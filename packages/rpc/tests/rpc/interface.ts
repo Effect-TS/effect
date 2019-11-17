323c35e8e801577daae16aa5359edb663d0e6bbc
Michael Arnaldi <arnaldimichael@gmail.com>
2019-11-17 15:38:48 +0000

    comment on rpc/test

--- packages/rpc/tests/rpc/interface.ts
+++ packages/rpc/tests/rpc/interface.ts
@@ -1,6 +1,15 @@
 import * as T from "@matechs/effect";
 import { ChildContext, Tracer } from "@matechs/tracing/lib";
 
+/*
+  this is an alternative way to provide a fake module definition that
+  can be used for client & server utility generation, in the classical approach
+  you would generate client from server module definition but this while
+  being more in line with traditional module definition impose a dependency
+  between client and server that you might not want (depending on how you
+  structure your app)
+ */
+
 export const printerDef = {
   printer: {
     print: {} as (s: string) => T.Effect<T.NoEnv, T.NoErr, void>
