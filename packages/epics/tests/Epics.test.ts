5aa572f2463d048dd204db84c26bd97c1078fa07
Michael Arnaldi <arnaldimichael@gmail.com>
2019-11-27 19:54:13 +0000

    improve exmple in epics test

--- packages/epics/tests/Epics.test.ts
+++ packages/epics/tests/Epics.test.ts
@@ -15,22 +15,38 @@ interface UserFetched extends Action<any> {
   user: User;
 }
 
+interface UserFetchFailed extends Action<any> {
+  type: "USER_FETCH_FAILED";
+  error: string;
+}
+
 interface FetchUser extends Action<any> {
   type: "FETCH_USER";
   id: string;
 }
 
-type MyAction = FetchUser | UserFetched;
+type MyAction = FetchUser | UserFetched | UserFetchFailed;
+
+type State = { user: Op.Option<User>; error: Op.Option<string> };
+
+function reducer(
+  state: State = { user: Op.none, error: Op.none },
+  action: MyAction
+): State {
+  switch (action.type) {
+    case "USER_FETCHED": {
+      return { ...state, user: Op.some(action.user), error: Op.none };
+    }
+    case "USER_FETCH_FAILED": {
+      return { ...state, user: Op.none, error: Op.some(action.error) };
+    }
+  }
+  return state;
+}
 
 function isFetchUser(x: MyAction): x is FetchUser {
   return x.type === "FETCH_USER";
 }
-
-function isUserFetched(x: MyAction): x is UserFetched {
-  return x.type === "USER_FETCHED";
-}
-
-type State = { user: Op.Option<User> };
 
 type Config = {
   config: {
@@ -38,26 +54,15 @@ type Config = {
   };
 };
 
-function reducer(state: State = { user: Op.none }, action: MyAction): State {
-  switch (action.type) {
-    case "USER_FETCHED": {
-      return { user: Op.some(action.user) };
-    }
-  }
-  return state;
-}
-
-describe("Epics", () => {
-  jest.setTimeout(5000);
-
-  it("should use redux-observable", async () => {
-    const fetchUser: Ep.Epic<Config, MyAction, State> = _ => action$ =>
-      Do(S.streamMonad)
-        .bind("action", S.filterRefineWith(isFetchUser)(action$))
-        .bindL("fetched", ({ action: { id } }) =>
-          S.encaseEffect(
-            T.access(
-              ({ config: { prefix } }: Config): MyAction => ({
+const fetchUser: Ep.Epic<Config, MyAction, State> = _ => action$ =>
+  Do(S.streamMonad)
+    .bind("action", S.filterRefineWith(isFetchUser)(action$))
+    .bindL("fetched", ({ action: { id } }) =>
+      S.encaseEffect(
+        T.accessM(({ config: { prefix } }: Config) =>
+          T.alt_(prefix === "prefix")(
+            T.sync(
+              (): MyAction => ({
                 type: "USER_FETCHED",
                 user: {
                   id,
@@ -65,10 +70,23 @@ describe("Epics", () => {
                 }
               })
             )
+          )(
+            T.sync(
+              (): MyAction => ({
+                type: "USER_FETCH_FAILED",
+                error: "wrong prefix"
+              })
+            )
           )
         )
-        .return(s => s.fetched);
+      )
+    )
+    .return(s => s.fetched);
 
+describe("Epics", () => {
+  jest.setTimeout(5000);
+
+  it("should use redux-observable", async () => {
     const module = pipe(
       T.noEnv,
       T.mergeEnv({ config: { prefix: "prefix" } } as Config)
@@ -102,8 +120,47 @@ describe("Epics", () => {
     await T.runToPromise(T.delay(T.unit, 10));
 
     assert.deepEqual(updates, [
-      { user: Op.none },
-      { user: Op.some({ id: "test", prefix: "prefix" }) }
+      { user: Op.none, error: Op.none },
+      { user: Op.some({ id: "test", prefix: "prefix" }), error: Op.none }
+    ]);
+  });
+
+  it("should use redux-observable (fail case)", async () => {
+    const module = pipe(
+      T.noEnv,
+      T.mergeEnv({ config: { prefix: "prefix-wrong" } } as Config)
+    );
+
+    const rootEpic = combineEpics(Ep.embed(module, fetchUser));
+
+    const epicMiddleware = createEpicMiddleware<
+      MyAction,
+      MyAction,
+      State,
+      State
+    >();
+    const store = createStore(
+      combineReducers({
+        reducer
+      }),
+      applyMiddleware(epicMiddleware)
+    );
+
+    const updates: State[] = [];
+
+    store.subscribe(() => {
+      updates.push(store.getState().reducer);
+    });
+
+    epicMiddleware.run(rootEpic);
+
+    store.dispatch({ id: "test", type: "FETCH_USER" } as FetchUser);
+
+    await T.runToPromise(T.delay(T.unit, 10));
+
+    assert.deepEqual(updates, [
+      { user: Op.none, error: Op.none },
+      { user: Op.none, error: Op.some("wrong prefix") }
     ]);
   });
 });
