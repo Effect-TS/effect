9fd25b7d48a78de56017f6e6b468da83f2a5ab1f
Michael Arnaldi <arnaldimichael@gmail.com>
2019-11-27 01:39:51 +0000

    add coverage

--- packages/rxjs/tests/RxJS.test.ts
+++ packages/rxjs/tests/RxJS.test.ts
@@ -6,6 +6,7 @@ import * as E from "fp-ts/lib/Either";
 import * as A from "fp-ts/lib/Array";
 
 import * as assert from "assert";
+import { raise } from "waveguide/lib/exit";
 
 describe("RxJS", () => {
   it("should encaseObservable", async () => {
@@ -15,5 +16,23 @@ describe("RxJS", () => {
     const r = await T.runToPromise(p);
 
     assert.deepEqual(r, A.range(0, 9));
+  });
+
+  it("should encaseObservable - complete", async () => {
+    const s = O.encaseObservable(Rx.from([0, 1, 2]), E.toError);
+    const p = S.collectArray(s);
+
+    const r = await T.runToPromise(p);
+
+    assert.deepEqual(r, A.range(0, 2));
+  });
+
+  it("should encaseObservable - error", async () => {
+    const s = O.encaseObservable(Rx.throwError(new Error("error")), E.toError);
+    const p = S.collectArray(S.take(s, 10));
+
+    const r = await T.runToPromiseExit(p);
+
+    assert.deepEqual(r, raise(new Error("error")));
   });
 });
