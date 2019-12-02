f17bde3af34f1b6a538e83eb9b353a715858c831
Michael Arnaldi <arnaldimichael@gmail.com>
2019-12-02 11:24:34 +0000

    remove deepmerge (#52)
    * remove deepmerge
    
    * add ci
    
    * add tests

---
+++ packages/effect/src/utils/merge.ts
@@ -0,0 +1,20 @@
+export function isObject(item: unknown) {
+  return item && typeof item === "object" && !Array.isArray(item);
+}
+
+export function mergeDeep<A, B>(target: A, source: B): A & B {
+  const c = target as any;
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!c[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(c[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return target as any;
+}
