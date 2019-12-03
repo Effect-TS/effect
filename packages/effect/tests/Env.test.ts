c02dcd06704f82ea6f8ea9ad51e6f692531ebae2
Michael Arnaldi <arnaldimichael@gmail.com>
2019-12-03 01:04:57 +0000

    add more tests

---
+++ packages/effect/tests/Env.test.ts
@@ -0,0 +1,46 @@
+import * as T from "../src/effect";
+import { Do } from "fp-ts-contrib/lib/Do";
+import * as assert from "assert";
+import { array } from "fp-ts/lib/Array";
+
+interface Env {
+  foo: string;
+}
+
+describe("Env", () => {
+  it("env should work", async () => {
+    const res = await T.runToPromise(
+      Do(T.effect)
+        .bindL("a", () =>
+          T.provideAll<Env>({ foo: "a" })(
+            T.delay(
+              T.access(({ foo }: Env) => foo),
+              100
+            )
+          )
+        )
+        .bindL("b", () =>
+          T.provideAll<Env>({ foo: "b" })(T.access(({ foo }: Env) => foo))
+        )
+        .return(s => `${s.a} - ${s.b}`)
+    );
+
+    assert.deepEqual(res, "a - b");
+  });
+
+  it("env should work - par", async () => {
+    const res = await T.runToPromise(
+      array.sequence(T.parEffect)([
+        T.provideAll<Env>({ foo: "a" })(
+          T.delay(
+            T.access(({ foo }: Env) => foo),
+            100
+          )
+        ),
+        T.provideAll<Env>({ foo: "b" })(T.access(({ foo }: Env) => foo))
+      ])
+    );
+
+    assert.deepEqual(res.join(" - "), "a - b");
+  });
+});
