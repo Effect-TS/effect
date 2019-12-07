f9ac9daaec0ec728b9768d5f2a03243231653a97
Michael Arnaldi <arnaldimichael@gmail.com>
2019-12-07 22:19:07 +0000

    continue work on http

---
+++ packages/http-client-libcurl/src/index.ts
@@ -0,0 +1,127 @@
+import { effect as T } from "@matechs/effect";
+import * as H from "@matechs/http-client";
+import bodyParser from "body-parser";
+import express from "express";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/pipeable";
+import * as R from "fp-ts/lib/Record";
+import * as C from "node-libcurl";
+import path from "path";
+
+const certfile = path.join(__dirname, "../cacert-2019-11-27.pem");
+
+export const libcurl: H.Http = {
+  http: {
+    request: <I, E, O>(
+      method: H.Method,
+      url: string,
+      headers: Record<string, string> = {},
+      body: I
+    ) =>
+      T.async(done => {
+        const req = new C.Curl();
+
+        req.setOpt("URL", url);
+        req.setOpt("CAINFO", certfile);
+        req.setOpt("FOLLOWLOCATION", 1);
+        req.setOpt("VERBOSE", 0);
+        req.setOpt("SSL_VERIFYHOST", 2);
+        req.setOpt("SSL_VERIFYPEER", 1);
+
+        const jsonBody = body ? JSON.stringify(body) : "";
+
+        switch (method) {
+          case H.Method.GET:
+            break;
+          case H.Method.POST:
+            if (jsonBody.length > 0) {
+              req.setOpt("POSTFIELDS", jsonBody);
+            }
+            req.setOpt("CUSTOMREQUEST", "POST");
+            req.setOpt(C.Curl.option.HTTPHEADER, [
+              "Content-Type: application/json",
+              ...pipe(
+                headers,
+                R.collect((k, v) => `${k}: ${v}`)
+              )
+            ]);
+            break;
+          default:
+            break;
+        }
+
+        req
+          .on("error", e => {
+            done(
+              E.left({
+                _tag: H.HttpErrorReason.Request,
+                error: e
+              })
+            );
+          })
+          .on("end", (statusCode, body, headers) => {
+            const bodyStr = body.toString();
+
+            if (statusCode >= 200 && statusCode < 300) {
+              done(
+                E.right({
+                  status: statusCode,
+                  body: bodyStr.length > 0 ? JSON.parse(bodyStr) : undefined,
+                  headers:
+                    headers.length > 0
+                      ? typeof headers[0] !== "number"
+                        ? headers[0]
+                        : {}
+                      : {}
+                })
+              );
+            } else {
+              done(
+                E.left({
+                  _tag: H.HttpErrorReason.Response,
+                  response: {
+                    status: statusCode,
+                    body: bodyStr.length > 0 ? JSON.parse(bodyStr) : undefined,
+                    headers:
+                      headers.length > 0
+                        ? typeof headers[0] !== "number"
+                          ? headers[0]
+                          : {}
+                        : {}
+                  }
+                })
+              );
+            }
+          });
+
+        req.perform();
+
+        // tslint:disable-next-line: no-empty
+        return () => {};
+      })
+  }
+};
+
+const app = express();
+
+app.post("/post", bodyParser.json(), (req, res) => {
+  res.send(req.body);
+});
+
+const s = app.listen(4001);
+
+T.run(
+  libcurl.http.request(
+    H.Method.POST,
+    "http://127.0.0.1:4001/post",
+    {},
+    {
+      foo: "bar"
+    }
+  ),
+  r => {
+    console.log(r);
+
+    s.close();
+  }
+);
--- packages/http-client/src/index.ts
+++ packages/http-client/src/index.ts
@@ -10,12 +10,6 @@ export enum Method {
 
 export type Headers = Record<string, string>;
 
-export interface Request<Body> {
-  method: Method;
-  body?: Body;
-  headers: Headers;
-}
-
 export interface Response<Body> {
   body?: Body;
   headers: Headers;
@@ -23,65 +17,103 @@ export interface Response<Body> {
 }
 
 export interface HttpResponseError<ErrorBody> {
-  _tag: "HttpResponseError";
+  _tag: HttpErrorReason.Response;
   response: Response<ErrorBody>;
 }
 
 export interface HttpRequestError {
-  _tag: "HttpRequestError";
+  _tag: HttpErrorReason.Request;
   error: Error;
+}
+
+export enum HttpErrorReason {
+  Request,
+  Response
 }
 
 export type HttpError<ErrorBody> =
   | HttpRequestError
   | HttpResponseError<ErrorBody>;
 
+export function foldHttpError<A, B, ErrorBody>(
+  onError: (e: Error) => A,
+  onResponseError: (e: Response<ErrorBody>) => B
+): (err: HttpError<ErrorBody>) => A | B {
+  return err => {
+    switch (err._tag) {
+      case HttpErrorReason.Request:
+        return onError(err.error);
+      case HttpErrorReason.Response:
+        return onResponseError(err.response);
+    }
+  };
+}
+
 export interface Http {
   http: {
     request: <I, E, O>(
       method: Method,
       url: string,
+      headers?: Record<string, string>,
       body?: I
-    ) => T.Effect<T.NoEnv, HttpError<E>, O>;
+    ) => T.Effect<T.NoEnv, HttpError<E>, Response<O>>;
   };
 }
 
 export function request<I, E, O>(
   method: Method,
   url: string,
+  headers: Record<string, string> = {},
   body?: I
 ): T.Effect<Http, HttpError<E>, O> {
-  return T.accessM(({ http }: Http) => http.request(method, url, body));
+  return T.accessM(({ http }: Http) =>
+    http.request(method, url, headers, body)
+  );
 }
 
-export function Get<E, O>(url: string): T.Effect<Http, HttpError<E>, O> {
-  return T.accessM(({ http }: Http) => http.request(Method.GET, url));
+export function Get<E, O>(
+  url: string,
+  headers: Record<string, string> = {}
+): T.Effect<Http, HttpError<E>, O> {
+  return T.accessM(({ http }: Http) => http.request(Method.GET, url, headers));
 }
 
 export function Post<I, E, O>(
   url: string,
+  headers: Record<string, string> = {},
   body?: I
 ): T.Effect<Http, HttpError<E>, O> {
-  return T.accessM(({ http }: Http) => http.request(Method.POST, url, body));
+  return T.accessM(({ http }: Http) =>
+    http.request(Method.POST, url, headers, body)
+  );
 }
 
 export function Patch<I, E, O>(
   url: string,
+  headers: Record<string, string> = {},
   body?: I
 ): T.Effect<Http, HttpError<E>, O> {
-  return T.accessM(({ http }: Http) => http.request(Method.PATCH, url, body));
+  return T.accessM(({ http }: Http) =>
+    http.request(Method.PATCH, url, headers, body)
+  );
 }
 
 export function Put<I, E, O>(
   url: string,
+  headers: Record<string, string> = {},
   body?: I
 ): T.Effect<Http, HttpError<E>, O> {
-  return T.accessM(({ http }: Http) => http.request(Method.PUT, url, body));
+  return T.accessM(({ http }: Http) =>
+    http.request(Method.PUT, url, headers, body)
+  );
 }
 
 export function Delete<I, E, O>(
   url: string,
+  headers: Record<string, string> = {},
   body?: I
 ): T.Effect<Http, HttpError<E>, O> {
-  return T.accessM(({ http }: Http) => http.request(Method.PUT, url, body));
+  return T.accessM(({ http }: Http) =>
+    http.request(Method.PUT, url, headers, body)
+  );
 }
