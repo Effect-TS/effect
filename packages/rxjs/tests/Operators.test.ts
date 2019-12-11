bc6be702b1a91d9a800f65059294b87a66e79f95
Michael Arnaldi <arnaldimichael@gmail.com>
2019-12-11 15:07:53 +0000

    observable error test

--- packages/rxjs/tests/Operators.test.ts
+++ packages/rxjs/tests/Operators.test.ts
@@ -44,4 +44,31 @@ describe("Operators", () => {
     assert.deepEqual(numbers, []);
     assert.deepEqual(errors, ["error"]);
   });
+
+  it("chainEffect observable error", async () => {
+    const numbers: number[] = [];
+    const errors: any[] = [];
+
+    Rx.concat(Rx.from([0, 1, 2, 3]), Rx.throwError("error"))
+      .pipe(filter(n => n % 2 === 0))
+      .pipe(
+        O.chainEffect(n =>
+          T.sync(() => {
+            numbers.push(n);
+          })
+        )
+      )
+      .pipe(
+        catchError(e => {
+          errors.push(e);
+          return Rx.EMPTY;
+        })
+      )
+      .subscribe();
+
+    await T.runToPromise(T.delay(T.unit, 10));
+
+    assert.deepEqual(numbers, [0, 2]);
+    assert.deepEqual(errors, ["error"]);
+  });
 });
