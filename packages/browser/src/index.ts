84d71d6a97867d151e81437e83d665b1005daaf1
Michael Arnaldi <arnaldimichael@gmail.com>
2019-12-12 21:31:13 +0000

    add session & local storage

---
+++ packages/browser/src/index.ts
@@ -0,0 +1,60 @@
+import { effect as T } from "@matechs/effect";
+import * as O from "fp-ts/lib/Option";
+
+export const localStorageEnv: unique symbol = Symbol();
+export const sessionStorageEnv: unique symbol = Symbol();
+
+interface GenericStorage<Env> {
+  length: T.Effect<Env, T.NoErr, number>;
+  clear: T.Effect<Env, T.NoErr, void>;
+  getItem(key: string): T.Effect<Env, T.NoErr, O.Option<string>>;
+  key(index: number): T.Effect<Env, T.NoErr, O.Option<string>>;
+  removeItem(key: string): T.Effect<Env, T.NoErr, void>;
+  setItem(key: string, value: string): T.Effect<Env, T.NoErr, void>;
+}
+
+export interface StorageEnv {
+  [localStorageEnv]: GenericStorage<T.NoEnv>;
+  [sessionStorageEnv]: GenericStorage<T.NoEnv>;
+}
+
+function getStorageImpl(storage: Storage): GenericStorage<T.NoEnv> {
+  return {
+    length: T.sync(() => storage.length),
+    clear: T.sync(() => storage.clear()),
+    getItem: key => T.sync(() => O.fromNullable(storage.getItem(key))),
+    key: index => T.sync(() => O.fromNullable(storage.key(index))),
+    removeItem: key => T.sync(() => storage.removeItem(key)),
+    setItem: (key, value) => T.sync(() => storage.setItem(key, value))
+  };
+}
+
+export function storageEnv(session: Storage, local: Storage): StorageEnv {
+  return {
+    [sessionStorageEnv]: getStorageImpl(session),
+    [localStorageEnv]: getStorageImpl(local)
+  };
+}
+
+function getStorageFunctions(
+  sym: typeof sessionStorageEnv | typeof localStorageEnv
+): GenericStorage<StorageEnv> {
+  return {
+    length: T.accessM(({ [sym]: s }: StorageEnv) => s.length),
+    clear: T.accessM(({ [sym]: s }: StorageEnv) => s.clear),
+    getItem: key => T.accessM(({ [sym]: s }: StorageEnv) => s.getItem(key)),
+    key: index => T.accessM(({ [sym]: s }: StorageEnv) => s.key(index)),
+    removeItem: key =>
+      T.accessM(({ [sym]: s }: StorageEnv) => s.removeItem(key)),
+    setItem: (key, value) =>
+      T.accessM(({ [sym]: s }: StorageEnv) => s.setItem(key, value))
+  };
+}
+
+export const sessionStore: GenericStorage<StorageEnv> = getStorageFunctions(
+  sessionStorageEnv
+);
+
+export const localStore: GenericStorage<StorageEnv> = getStorageFunctions(
+  localStorageEnv
+);
---
+++ packages/browser/tests/Browser.test.ts
@@ -0,0 +1,76 @@
+import { effect as T } from "@matechs/effect";
+import * as B from "../src";
+import * as assert from "assert";
+import { some } from "fp-ts/lib/Option";
+
+class MockStorage implements Storage {
+  [name: string]: any;
+
+  get length() {
+    return this.st.length;
+  }
+
+  // tslint:disable-next-line: no-empty
+  constructor(private st: Array<[string, string]>) {}
+
+  clear(): void {
+    this.st = [];
+  }
+  getItem(key: string): string | null {
+    return this.st.find(x => x[0] === key)?.[1] || null;
+  }
+  key(index: number): string | null {
+    return this.st[index]?.[0];
+  }
+  removeItem(key: string): void {
+    const i = this.st.findIndex(x => x[0] === key);
+
+    if (i >= 0) {
+      this.st = this.st.filter((_, i2) => i2 !== i);
+    }
+  }
+  setItem(key: string, value: string): void {
+    this.st.push([key, value]);
+  }
+}
+
+const session = new MockStorage([]);
+const local = new MockStorage([]);
+
+function run<E, A>(eff: T.Effect<B.StorageEnv, E, A>) {
+  return T.runToPromise(T.provideAll(B.storageEnv(session, local))(eff));
+}
+
+describe("Browser", () => {
+  it("local storage", async () => {
+    await run(B.localStore.setItem("foo", "bar"));
+    const l = await run(B.localStore.length);
+    const k = await run(B.localStore.key(0));
+    await run(B.localStore.removeItem("foo"));
+    const l2 = await run(B.localStore.length);
+    await run(B.localStore.setItem("foo", "bar"));
+    await run(B.localStore.clear);
+    const l3 = await run(B.localStore.length);
+
+    assert.deepEqual(l, 1);
+    assert.deepEqual(k, some("foo"));
+    assert.deepEqual(l2, 0);
+    assert.deepEqual(l3, 0);
+  });
+
+  it("session storage", async () => {
+    await run(B.localStore.setItem("foo", "bar"));
+    const l = await run(B.localStore.length);
+    const k = await run(B.localStore.key(0));
+    await run(B.localStore.removeItem("foo"));
+    const l2 = await run(B.localStore.length);
+    await run(B.localStore.setItem("foo", "bar"));
+    await run(B.localStore.clear);
+    const l3 = await run(B.localStore.length);
+
+    assert.deepEqual(l, 1);
+    assert.deepEqual(k, some("foo"));
+    assert.deepEqual(l2, 0);
+    assert.deepEqual(l3, 0);
+  });
+});
