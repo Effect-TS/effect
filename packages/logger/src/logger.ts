83907c41545d7c758ea3383f1e68befe240a436c
Michael Arnaldi <arnaldimichael@gmail.com>
2019-12-28 20:32:44 +0000

    Logger (#96)
    * logger module with console implementation
    
    * add instance to implement lower order env
    
    * use instance in console logger
    
    * improve logger config

---
+++ packages/logger/src/logger.ts
@@ -0,0 +1,37 @@
+import { effect as T, freeEnv as F } from "@matechs/effect";
+
+export const loggerEnv: unique symbol = Symbol();
+
+export interface Meta {
+  [k: string]: any;
+}
+
+export const loggerM = F.define({
+  [loggerEnv]: {
+    silly: F.fn<(message: string, meta?: Meta) => T.UIO<void>>(),
+    debug: F.fn<(message: string, meta?: Meta) => T.UIO<void>>(),
+    verbose: F.fn<(message: string, meta?: Meta) => T.UIO<void>>(),
+    http: F.fn<(message: string, meta?: Meta) => T.UIO<void>>(),
+    info: F.fn<(message: string, meta?: Meta) => T.UIO<void>>(),
+    warn: F.fn<(message: string, meta?: Meta) => T.UIO<void>>(),
+    error: F.fn<(message: string, meta?: Meta) => T.UIO<void>>()
+  }
+});
+
+export type Logger = F.TypeOf<typeof loggerM>;
+
+export type Level = keyof Logger[typeof loggerEnv];
+
+export const severity: Record<Level, number> = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+};
+
+export const {
+  [loggerEnv]: { silly, debug, verbose, http, info, warn, error }
+} = F.access(loggerM);
---
+++ packages/logger/tests/Logger.test.ts
@@ -0,0 +1,102 @@
+import { effect as T } from "@matechs/effect";
+import * as L from "../src";
+import { pipe } from "fp-ts/lib/pipeable";
+import * as assert from "assert";
+import { isDone } from "@matechs/effect/lib/exit";
+
+// tslint:disable-next-line: no-empty
+const empty = () => {};
+
+const withEnv = T.provideAll(L.console.consoleLogger());
+
+describe("Logger", () => {
+  const info = jest.spyOn(console, "info");
+  const warn = jest.spyOn(console, "warn");
+  const debug = jest.spyOn(console, "debug");
+  const error = jest.spyOn(console, "error");
+
+  info.mockImplementation(empty);
+  warn.mockImplementation(empty);
+  debug.mockImplementation(empty);
+  error.mockImplementation(empty);
+
+  it("use logger", async () => {
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.info("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(info.mock.calls.length, 1);
+
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.http("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(info.mock.calls.length, 2);
+
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.debug("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(debug.mock.calls.length, 1);
+
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.silly("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(debug.mock.calls.length, 2);
+
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.verbose("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(debug.mock.calls.length, 3);
+
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.warn("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(warn.mock.calls.length, 1);
+
+    assert.deepEqual(
+      isDone(await T.runToPromiseExit(pipe(L.logger.error("ok"), withEnv))),
+      true
+    );
+
+    assert.deepEqual(error.mock.calls.length, 1);
+
+    assert.deepEqual(
+      isDone(
+        await T.runToPromiseExit(
+          pipe(L.logger.error("ok", { foo: "ok" }), withEnv)
+        )
+      ),
+      true
+    );
+
+    assert.deepEqual(error.mock.calls.length, 2);
+
+    assert.deepEqual(
+      isDone(
+        await T.runToPromiseExit(
+          pipe(
+            L.logger.debug("ok"),
+            T.provideAll(
+              L.console.consoleLogger(
+                T.pure<L.console.Config>({ level: "info" })
+              )
+            )
+          )
+        )
+      ),
+      true
+    );
+
+    assert.deepEqual(debug.mock.calls.length, 3);
+  });
+});
