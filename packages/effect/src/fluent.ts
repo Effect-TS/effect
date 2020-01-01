5f656ce96c11e1b4cd70c4117843686a23e2c29c
Michael Arnaldi <arnaldimichael@gmail.com>
2020-01-01 01:37:34 +0000

    improve types

--- packages/effect/src/fluent.ts
+++ packages/effect/src/fluent.ts
@@ -1,37 +1,36 @@
 import * as T from "./effect";
 import { pipe } from "fp-ts/lib/pipeable";
 
-export type Rem<R, R2> = R extends R2 & infer R3 ? R3 : R;
+export type Strip<R, R2> = R extends R2 & infer R3 ? R3 : R;
 
-export class FluentImpl<R, E, A> {
+export class Fluent<R, E, A> {
   constructor(readonly t: T.Effect<R, E, A>) {}
-
-  chain: <R2, E2, A2>(
-    f: (s: A) => T.Effect<R2, E2, A2>
-  ) => FluentImpl<R & R2, E | E2, A2> = f =>
-    new FluentImpl(T.effect.chain(this.t, f));
-
-  chainError: <R2, E2, A2>(
-    f: (r: E) => T.Effect<R2, E2, A2>
-  ) => FluentImpl<R & R2, E2, A | A2> = f =>
-    new FluentImpl(T.effect.chainError(this.t, f));
-
-  tap: <R2, E2, A2>(
-    f: (s: A) => T.Effect<R2, E2, A2>
-  ) => FluentImpl<R & R2, E | E2, A> = f =>
-    new FluentImpl(T.effect.chainTap(this.t, f));
 
   done: () => T.Effect<R, E, A> = () => this.t;
 
-  provideS<R2 extends object>(r: R2): FluentImpl<Rem<R, R2>, E, A> {
-    return new FluentImpl(T.provideS(r)(this.t) as any);
-  }
+  chain: <R2, E2, A2>(
+    f: (s: A) => T.Effect<R2, E2, A2>
+  ) => Fluent<R & R2, E | E2, A2> = f => new Fluent(T.effect.chain(this.t, f));
 
-  provide(r: R): FluentImpl<unknown, E, A> {
-    return new FluentImpl(pipe(this.t, T.provideS(r)));
-  }
+  chainError: <R2, E2, A2>(
+    f: (r: E) => T.Effect<R2, E2, A2>
+  ) => Fluent<R & R2, E2, A | A2> = f =>
+    new Fluent(T.effect.chainError(this.t, f));
+
+  tap: <R2, E2, A2>(
+    f: (s: A) => T.Effect<R2, E2, A2>
+  ) => Fluent<R & R2, E | E2, A> = f =>
+    new Fluent(T.effect.chainTap(this.t, f));
+
+  provideS: <R2 extends Partial<R>>(
+    r: R2
+  ) => Fluent<Strip<R, R2>, E, A> = r =>
+    new Fluent(T.provideS(r)(this.t) as any);
+
+  provide: (r: R) => Fluent<unknown, E, A> = r =>
+    new Fluent(pipe(this.t, T.provideS(r)));
 }
 
-export function fluent<R, E, A>(eff: T.Effect<R, E, A>): FluentImpl<R, E, A> {
-  return new FluentImpl<R, E, A>(eff);
+export function fluent<R, E, A>(eff: T.Effect<R, E, A>): Fluent<R, E, A> {
+  return new Fluent<R, E, A>(eff);
 }
