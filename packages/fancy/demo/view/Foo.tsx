5dca3292088cbd233c6a19504b878425e40a3ba4
Michael Arnaldi <arnaldimichael@gmail.com>
2020-02-05 20:18:31 +0000

    ensure compatibility with router

--- packages/fancy/demo/view/Foo.tsx
+++ packages/fancy/demo/view/Foo.tsx
@@ -4,8 +4,6 @@ import * as O from "fp-ts/lib/Option";
 import { pipe } from "fp-ts/lib/pipeable";
 import { App } from "../src/app";
 import * as S from "../src/state";
-import { UpdateOrganisations } from "./UpdateOrganisations";
-import { MemoInput } from "./MemoInput";
 import { ShowDate } from "./ShowDate";
 import { UpdateDate } from "./UpdateDate";
 import * as R from "../../lib";
@@ -14,40 +12,27 @@ import Link from "next/link";
 // alpha
 /* istanbul ignore file */
 
-export const Home = App.view(() =>
+export const Foo = App.view(() =>
   pipe(
     sequenceS(T.effect)({
       UpdateDate,
-      UpdateOrganisations,
-      ShowDate,
-      MemoInput
+      ShowDate
     }),
     T.map(
-      ({
-        UpdateDate,
-        ShowDate,
-        UpdateOrganisations,
-        MemoInput
-      }): React.FC<R.StateP<S.AppState>> => ({ state }) => (
+      ({ UpdateDate, ShowDate }): React.FC<R.StateP<S.AppState>> => ({
+        state
+      }) => (
         <>
           <ShowDate />
           <UpdateDate />
-          <UpdateOrganisations />
           {pipe(
             state,
             S.orgsL.get,
             O.map(orgs => <div>{orgs}</div>),
             O.toNullable
           )}
-          {pipe(
-            state,
-            S.errorL.get,
-            O.map(error => <div>{error}</div>),
-            O.toNullable
-          )}
-          <MemoInput />
-          <Link href={"/foo"}>
-            <a>foo</a>
+          <Link href={"/"}>
+            <a>home</a>
           </Link>
         </>
       )
