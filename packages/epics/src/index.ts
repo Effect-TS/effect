6cffea203c02b8d150d1b47497e756c5a5110f37
Michael Arnaldi <arnaldimichael@gmail.com>
2020-02-13 15:41:35 +0100

    Combine Epics (#124)
    * combine epics
    
    * improve uniontointersection

--- packages/epics/src/index.ts
+++ packages/epics/src/index.ts
@@ -1,9 +1,10 @@
-import { effect as T } from "@matechs/effect";
+import { effect as T, freeEnv as F } from "@matechs/effect";
 import * as S from "@matechs/effect/lib/stream";
 import * as R from "@matechs/rxjs";
-
+import * as A from "fp-ts/lib/Array";
 import { Action } from "redux";
 import * as Rxo from "redux-observable";
+import { pipe } from "fp-ts/lib/pipeable";
 
 export type Epic<R, State, A extends Action<any>> = (
   current: State
@@ -14,15 +15,36 @@ function toNever(_: any): never {
   return undefined as never;
 }
 
-export function embed<R, State, A extends Action>(
-  epic: Epic<R, State, A>
-): (r: R) => Rxo.Epic<A, A, State> {
-  return (r: R) => (action$, state$) =>
-    R.runToObservable(
-      T.provideAll(r)(
-        R.toObservable(epic(state$.value)(R.encaseObservable(action$, toNever)))
+type Env<K> = K extends Epic<infer R, any, any> ? R : never;
+type Sta<K> = K extends Epic<any, infer S, any> ? S : never;
+type Act<K> = K extends Epic<any, any, infer A> ? A : never;
+
+export function embed<EPS extends Epic<any, any, any>[]>(
+  ...epics: EPS
+): (
+  r: F.UnionToIntersection<
+    Env<Exclude<typeof epics[number], Epic<T.NoEnv, any, any>>>
+  >
+) => Rxo.Epic<Act<EPS[number]>, Act<EPS[number]>, Sta<EPS[number]>> {
+  return (
+    r: F.UnionToIntersection<
+      Env<Exclude<typeof epics[number], Epic<T.NoEnv, any, any>>>
+    >
+  ) =>
+    Rxo.combineEpics(
+      ...pipe(
+        epics,
+        A.map(epic => (action$: any, state$: any) =>
+          R.runToObservable(
+            T.provideAll(r)(
+              R.toObservable(
+                epic(state$.value)(R.encaseObservable(action$, toNever) as any)
+              )
+            )
+          )
+        )
       )
-    );
+    ) as any;
 }
 
 export function epic<S, A extends Action>(): <R>(
