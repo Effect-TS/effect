882bfcd42ebec49800e04dfa5753aa31b12e334f
Michael Arnaldi <arnaldimichael@gmail.com>
2020-02-17 17:51:19 +0000

    cover missing case

--- packages/effect/tests/Retry.test.ts
+++ packages/effect/tests/Retry.test.ts
@@ -2,6 +2,7 @@ import { effect as T, retry as R, exit as E } from "../src";
 import * as assert from "assert";
 import { limitRetries } from "retry-ts";
 import { function as F } from "fp-ts";
+import { raise } from "../src/original/exit";
 
 describe("Retry", () => {
   it("should retry", async () => {
@@ -12,5 +13,15 @@ describe("Retry", () => {
     );
 
     assert.deepEqual(await T.runToPromise(program), 0);
+  });
+
+  it("should retry & fail", async () => {
+    const program: T.IO<string, number> = R.retrying(
+      limitRetries(2),
+      ({ iterNumber }) => (iterNumber > 3 ? T.pure(0) : T.raiseError("error")),
+      F.flow(E.isRaise, T.pure)
+    );
+
+    assert.deepEqual(await T.runToPromiseExit(program), raise("error"));
   });
 });
