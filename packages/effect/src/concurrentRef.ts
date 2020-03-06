980803c6506629592abdcc50971311faaa9e2db4
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2020-03-06 16:56:42 +0100

    add concurrentRef tests

---
+++ packages/effect/src/concurrentRef.ts
@@ -0,0 +1,83 @@
+import { function as F } from "fp-ts";
+import * as T from "./effect";
+import { makeSemaphore } from "./semaphore";
+import { pipe } from "fp-ts/lib/pipeable";
+
+export interface ConcurrentRef<A> {
+  /**
+   * Get the current value of the ConcurrentRef
+   */
+  readonly get: T.Effect<T.NoEnv, never, A>;
+  /**
+   * Set the current value of the ConcurrentRef
+   * @param a
+   */
+  set<R>(a: T.Effect<R, never, A>): T.Effect<R, never, A>;
+  /**
+   * Update the current value of the ConcurrentRef with an effect.
+   * Produces the new value
+   * @param f
+   */
+  update<R>(f: F.FunctionN<[A], T.Effect<R, never, A>>): T.Effect<R, never, A>;
+  /**
+   * Update the current value of a ConcurrentRef with an effect.
+   *
+   * This function may return a second value of type B that will be produced on complete
+   * @param f
+   */
+  modify<R, B>(
+    f: F.FunctionN<[A], T.Effect<R, never, readonly [B, A]>>
+  ): T.Effect<R, never, B>;
+}
+
+/**
+ * Creates an IO that will allocate a ConcurrentRef.
+ */
+export const makeConcurrentRef = <A>(
+  initial: A
+): T.Effect<T.NoEnv, never, ConcurrentRef<A>> =>
+  pipe(
+    makeSemaphore(1),
+    T.map(semaphore => {
+      let value = initial;
+
+      const get = T.sync(() => value);
+
+      const set = <R>(a: T.Effect<R, never, A>): T.Effect<R, never, A> =>
+        semaphore.withPermit(
+          T.effect.map(a, a => {
+            const prev = value;
+            value = a;
+            return prev;
+          })
+        );
+
+      const update = <R>(
+        f: F.FunctionN<[A], T.Effect<R, never, A>>
+      ): T.Effect<R, never, A> =>
+        semaphore.withPermit(
+          T.effect.map(f(value), v => {
+            value = v;
+            return v;
+          })
+        );
+
+      const modify = <R, B>(
+        f: F.FunctionN<[A], T.Effect<R, never, readonly [B, A]>>
+      ): T.Effect<R, never, B> =>
+        semaphore.withPermit(
+          T.effect.map(f(value), v => {
+            const [b, a] = v;
+            value = a;
+            return b;
+          })
+        );
+
+      return {
+        get,
+        set,
+        update,
+        modify
+      };
+    })
+  );
--- packages/effect/src/index.ts
+++ packages/effect/src/index.ts
@@ -4,13 +4,14 @@ import * as sink from "./stream/sink";
 import * as managed from "./managed";
 import * as queue from "./queue";
 import * as ref from "./ref";
+import * as concurrentRef from "./concurrentRef";
 import * as semaphore from "./semaphore";
 import * as exit from "./exit";
 import * as streameither from "./streameither";
 import * as freeEnv from "./freeEnv";
 import * as list from "./list";
 import * as utils from "./utils/types";
-import * as retry from "./retry"
+import * as retry from "./retry";
 
 export {
   effect,
@@ -18,6 +19,7 @@ export {
   managed,
   queue,
   ref,
+  concurrentRef,
   semaphore,
   exit,
   streameither,
