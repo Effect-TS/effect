d34d6403de9f11a8a0a60f582fcfdbbf4347129c
Michael Arnaldi <arnaldimichael@gmail.com>
2020-03-27 15:51:30 +0000

    alias Arb

--- packages/test/src/fc.ts
+++ packages/test/src/fc.ts
@@ -25,7 +25,9 @@ export const provideGenerator = T.provideSW<RandomGen>()(
   }
 }));
 
-export const arb = <T0>(a: Arbitrary<T0>) =>
+export interface Arb<R, A> extends S.Stream<R, never, A> {}
+
+export const arb = <T0>(a: Arbitrary<T0>): Arb<RandomGen, T0> =>
   S.fromSource(
     M.encaseEffect(T.access((_: RandomGen) => T.sync(() => some(a.generate(_[RandomGenURI].randomGenerator).value))))
   );
@@ -42,8 +44,8 @@ export const propertyM = (iterations: number) => <NER extends Record<string, S.S
     S.drain
   );
 
-export const property = (iterations: number) => <NER extends Record<string, S.Stream<any, any, any>>>(
-  r: EnforceNonEmptyRecord<NER> & Record<string, S.Stream<any, any, any>>
+export const property = (iterations: number) => <NER extends Record<string, Arb<any, any>>>(
+  r: EnforceNonEmptyRecord<NER> & Record<string, Arb<any, any>>
 ) => (f: (_: { [K in keyof NER]: [NER[K]] extends [S.Stream<any, any, infer A>] ? A : never }) => void) =>
   pipe(
     sequenceS(S.stream)<NER>(r),
