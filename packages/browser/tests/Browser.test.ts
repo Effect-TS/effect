a1550715e502e9222b27e842c5904b21562fcac0
Michael Arnaldi <arnaldimichael@gmail.com>
2020-03-28 13:10:44 +0000

    move browser test

--- packages/browser/tests/Browser.test.ts
+++ packages/browser/tests/Browser.test.ts
@@ -1,80 +1,54 @@
 import { effect as T } from "@matechs/effect";
 import * as B from "../src";
-import * as assert from "assert";
 import { some } from "fp-ts/lib/Option";
+import * as M from "@matechs/test-jest";
+import { MockStorage } from "./mock";
+import { Do } from "fp-ts-contrib/lib/Do";
 
-class MockStorage implements Storage {
-  [name: string]: any;
-
-  get length() {
-    return this.st.length;
-  }
-
-  // tslint:disable-next-line: no-empty
-  constructor(private st: Array<[string, string]>) {}
-
-  clear(): void {
-    this.st = [];
-  }
-  getItem(key: string): string | null {
-    return this.st.find(x => x[0] === key)?.[1] || null;
-  }
-  key(index: number): string | null {
-    return this.st[index]?.[0];
-  }
-  removeItem(key: string): void {
-    const i = this.st.findIndex(x => x[0] === key);
-
-    if (i >= 0) {
-      this.st = this.st.filter((_, i2) => i2 !== i);
-    }
-  }
-  setItem(key: string, value: string): void {
-    this.st.push([key, value]);
-  }
-}
+const browserSpec = M.suite("browser")(
+  M.testM(
+    "use local storage",
+    Do(T.effect)
+      .do(B.localStore.setItem("foo", "bar"))
+      .bind("l", B.localStore.length)
+      .bind("f", B.localStore.getItem("foo"))
+      .bind("k", B.localStore.key(0))
+      .do(B.localStore.removeItem("foo"))
+      .bind("l2", B.localStore.length)
+      .do(B.localStore.setItem("foo", "bar"))
+      .do(B.localStore.clear)
+      .bind("l3", B.localStore.length)
+      .return(({ f, k, l, l2, l3 }) => {
+        M.assert.deepEqual(l, 1);
+        M.assert.deepEqual(k, some("foo"));
+        M.assert.deepEqual(f, some("bar"));
+        M.assert.deepEqual(l2, 0);
+        M.assert.deepEqual(l3, 0);
+      })
+  ),
+  M.testM(
+    "use session storage",
+    Do(T.effect)
+      .do(B.sessionStore.setItem("foo", "bar"))
+      .bind("l", B.sessionStore.length)
+      .bind("f", B.sessionStore.getItem("foo"))
+      .bind("k", B.sessionStore.key(0))
+      .do(B.sessionStore.removeItem("foo"))
+      .bind("l2", B.sessionStore.length)
+      .do(B.sessionStore.setItem("foo", "bar"))
+      .do(B.sessionStore.clear)
+      .bind("l3", B.sessionStore.length)
+      .return(({ f, k, l, l2, l3 }) => {
+        M.assert.deepEqual(l, 1);
+        M.assert.deepEqual(k, some("foo"));
+        M.assert.deepEqual(f, some("bar"));
+        M.assert.deepEqual(l2, 0);
+        M.assert.deepEqual(l3, 0);
+      })
+  )
+);
 
 const session = new MockStorage([]);
 const local = new MockStorage([]);
 
-function run<E, A>(eff: T.Effect<B.SessionStorageEnv & B.LocalStorageEnv, E, A>) {
-  return T.runToPromise(T.provideAll(B.storageEnv(session, local))(eff));
-}
-
-describe("Browser", () => {
-  it("local storage", async () => {
-    await run(B.localStore.setItem("foo", "bar"));
-    const l = await run(B.localStore.length);
-    const f = await run(B.localStore.getItem("foo"));
-    const k = await run(B.localStore.key(0));
-    await run(B.localStore.removeItem("foo"));
-    const l2 = await run(B.localStore.length);
-    await run(B.localStore.setItem("foo", "bar"));
-    await run(B.localStore.clear);
-    const l3 = await run(B.localStore.length);
-
-    assert.deepEqual(l, 1);
-    assert.deepEqual(k, some("foo"));
-    assert.deepEqual(f, some("bar"));
-    assert.deepEqual(l2, 0);
-    assert.deepEqual(l3, 0);
-  });
-
-  it("session storage", async () => {
-    await run(B.sessionStore.setItem("foo", "bar"));
-    const l = await run(B.sessionStore.length);
-    const f = await run(B.sessionStore.getItem("foo"));
-    const k = await run(B.sessionStore.key(0));
-    await run(B.sessionStore.removeItem("foo"));
-    const l2 = await run(B.sessionStore.length);
-    await run(B.sessionStore.setItem("foo", "bar"));
-    await run(B.sessionStore.clear);
-    const l3 = await run(B.sessionStore.length);
-
-    assert.deepEqual(l, 1);
-    assert.deepEqual(k, some("foo"));
-    assert.deepEqual(l2, 0);
-    assert.deepEqual(l3, 0);
-    assert.deepEqual(f, some("bar"));
-  });
-});
+M.run(browserSpec)(T.provideS(B.storageEnv(session, local)));
