ddb587504b157d67d66b1dae4ad12d4b464dc492
Michael Arnaldi <arnaldimichael@gmail.com>
2020-03-29 20:59:16 +0100

    use test in logger

--- packages/logger/tests/Logger.test.ts
+++ packages/logger/tests/Logger.test.ts
@@ -1,102 +1,57 @@
 import { effect as T } from "@matechs/effect";
 import * as L from "../src";
-import { pipe } from "fp-ts/lib/pipeable";
-import * as assert from "assert";
-import { isDone } from "@matechs/effect/lib/exit";
+import * as M from "@matechs/test-jest";
+import { Do } from "fp-ts-contrib/lib/Do";
 
 // tslint:disable-next-line: no-empty
 const empty = () => {};
 
-const withEnv = T.provideAll(L.console.consoleLogger());
-
-describe("Logger", () => {
-  const info = jest.spyOn(console, "info");
-  const warn = jest.spyOn(console, "warn");
-  const debug = jest.spyOn(console, "debug");
-  const error = jest.spyOn(console, "error");
-
-  info.mockImplementation(empty);
-  warn.mockImplementation(empty);
-  debug.mockImplementation(empty);
-  error.mockImplementation(empty);
-
-  it("use logger", async () => {
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.info("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(info.mock.calls.length, 1);
-
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.http("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(info.mock.calls.length, 2);
-
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.debug("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(debug.mock.calls.length, 1);
-
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.silly("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(debug.mock.calls.length, 2);
-
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.verbose("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(debug.mock.calls.length, 3);
-
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.warn("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(warn.mock.calls.length, 1);
-
-    assert.deepEqual(
-      isDone(await T.runToPromiseExit(pipe(L.logger.error("ok"), withEnv))),
-      true
-    );
-
-    assert.deepEqual(error.mock.calls.length, 1);
-
-    assert.deepEqual(
-      isDone(
-        await T.runToPromiseExit(
-          pipe(L.logger.error("ok", { foo: "ok" }), withEnv)
+const loggerSpec = M.suite("Logger")(
+  M.mockedTestM("use logger")(() => ({
+    info: jest.spyOn(console, "info").mockImplementation(empty),
+    warn: jest.spyOn(console, "warn").mockImplementation(empty),
+    debug: jest.spyOn(console, "debug").mockImplementation(empty),
+    error: jest.spyOn(console, "error").mockImplementation(empty),
+  }))(({ useMockM }) =>
+    Do(T.effect)
+      .do(L.logger.info("ok"))
+      .do(L.logger.http("ok"))
+      .do(L.logger.debug("ok"))
+      .do(L.logger.silly("ok"))
+      .do(L.logger.verbose("ok"))
+      .do(L.logger.warn("ok"))
+      .do(L.logger.error("ok"))
+      .do(L.logger.error("ok", { foo: "ok" }))
+      .do(
+        useMockM(({ info, debug, error, warn }) =>
+          T.sync(() => {
+            M.assert.deepEqual(info.mock.calls.length, 2);
+            M.assert.deepEqual(debug.mock.calls.length, 3);
+            M.assert.deepEqual(error.mock.calls.length, 2);
+            M.assert.deepEqual(warn.mock.calls.length, 1);
+          })
         )
-      ),
-      true
-    );
-
-    assert.deepEqual(error.mock.calls.length, 2);
-
-    assert.deepEqual(
-      isDone(
-        await T.runToPromiseExit(
-          pipe(
-            L.logger.debug("ok"),
-            T.provideAll(
-              L.console.consoleLogger(
-                T.pure<L.console.Config>({ level: "info" })
-              )
-            )
-          )
+      )
+      .done()
+      .fluent()
+      .provideS(L.console.consoleLogger())
+  ),
+  M.mockedTestM("use logger with level")(() => ({
+    debug: jest.spyOn(console, "debug").mockImplementation(empty),
+  }))(({ useMockM }) =>
+    Do(T.effect)
+      .do(L.logger.debug("ok"))
+      .do(
+        useMockM(({ debug }) =>
+          T.sync(() => {
+            M.assert.deepEqual(debug.mock.calls.length, 0);
+          })
         )
-      ),
-      true
-    );
+      )
+      .done()
+      .fluent()
+      .provideS(L.console.consoleLogger(T.pure({ level: "warn" })))
+  )
+);
 
-    assert.deepEqual(debug.mock.calls.length, 3);
-  });
-});
+M.run(loggerSpec)();
