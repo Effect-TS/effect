2fbf6064e2a3e2a680e3ab04a0f13b42b40dd102
Michael Arnaldi <arnaldimichael@gmail.com>
2020-03-30 01:37:19 +0100

    add async in test

--- packages/effect/tests/Eff.test.ts
+++ packages/effect/tests/Eff.test.ts
@@ -4,6 +4,7 @@ import { pipe } from "fp-ts/lib/pipeable";
 
 describe("Eff", () => {
   it("should compose sync", () => {
+    // T.SyncEff<{ n: number; }, never, number>
     const program = pipe(
       T.access((_: { n: number }) => _.n),
       T.chain((n) => T.sync(() => n + 1)),
@@ -22,6 +23,7 @@ describe("Eff", () => {
   });
 
   it("should compose sync using fluent", () => {
+    // T.SyncEff<{ n: number; }, never, number>
     const program = T.retype(
       T.accessEnvironment<{ n: number }>()
         .fluent()
@@ -34,6 +36,26 @@ describe("Eff", () => {
         n: 1,
       }),
       T.runSync
+    );
+
+    assert.deepEqual(result, EX.done(2));
+  });
+
+  it("should compose async using fluent", async () => {
+    // T.AsyncEff<{ n: number; }, never, number>
+    const program = T.retype(
+      T.accessEnvironment<{ n: number }>()
+        .fluent()
+        .chain((_) => T.sync(() => _.n + 1))
+        .flow(T.shiftBefore)
+    );
+
+    const result = await pipe(
+      program,
+      T.provideAll({
+        n: 1,
+      }),
+      T.runToPromiseExit
     );
 
     assert.deepEqual(result, EX.done(2));
