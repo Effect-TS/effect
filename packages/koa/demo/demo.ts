0d64a7dfbc451f1fe11aa9e1e25127dcddffefd6
Patrick Roza <contact@patrickroza.com>
2020-04-10 00:42:35 +0200

    Formatting fix.

--- packages/koa/demo/demo.ts
+++ packages/koa/demo/demo.ts
@@ -6,29 +6,35 @@ import { pipe } from "fp-ts/lib/pipeable";
 const program = KOA.withApp(
   Do(T.effect)
     .do(
-      KOA.router(R => Do(T.effect)
-        .do(R.route(
-          "get",
-          "/",
-          T.pure(
-            KOA.routeResponse(200, {
-              message: "OK"
-            })
-          )))
-        .do(R.route(
-          "get",
-          "/test2",
-          T.pure(
-            KOA.routeResponse(200, {
-              message: "OK"
-            })
-          ))
-        )
-        .return(() => void 0)
+      KOA.router((R) =>
+        Do(T.effect)
+          .do(
+            R.route(
+              "get",
+              "/",
+              T.pure(
+                KOA.routeResponse(200, {
+                  message: "OK"
+                })
+              )
+            )
+          )
+          .do(
+            R.route(
+              "get",
+              "/test2",
+              T.pure(
+                KOA.routeResponse(200, {
+                  message: "OK"
+                })
+              )
+            )
+          )
+          .return(() => void 0)
       )
     )
     .bind("server", KOA.bind(8081))
-    .return(s => s.server)
+    .return((s) => s.server)
 );
 
 const envLive = pipe(T.noEnv, T.mergeEnv(KOA.koa));
@@ -36,20 +42,20 @@ const envLive = pipe(T.noEnv, T.mergeEnv(KOA.koa));
 T.run(
   T.provideAll(envLive)(program),
   E.fold(
-    server => {
+    (server) => {
       process.on("SIGINT", () => {
-        server.close(err => {
+        server.close((err) => {
           process.exit(err ? 2 : 0);
         });
       });
       process.on("SIGTERM", () => {
-        server.close(err => {
+        server.close((err) => {
           process.exit(err ? 2 : 0);
         });
       });
     },
-    e => console.error(e),
-    e => console.error(e),
+    (e) => console.error(e),
+    (e) => console.error(e),
     () => console.error("interrupted")
   )
 );
