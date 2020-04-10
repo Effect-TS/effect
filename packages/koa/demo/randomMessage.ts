c0c8df232ff14eb9f0945e91a0bdbaf04fb489a7
Patrick Roza <contact@patrickroza.com>
2020-04-10 11:49:29 +0200

    add random message.

---
+++ packages/koa/demo/randomMessage.ts
@@ -0,0 +1,37 @@
+import { effect as T, freeEnv as F } from "@matechs/effect";
+
+/**
+ * Definition of a calculator module
+ */
+
+export const RandomMessageURI = "@uris-free/RandomMessage";
+
+const RandomMessage_ = F.define({
+  [RandomMessageURI]: {
+    hitMe: F.fn<() => T.UIO<string>>()
+  }
+});
+
+export interface RandomMessage extends F.TypeOf<typeof RandomMessage_> {}
+
+export const RandomMessage = F.opaque<RandomMessage>()(RandomMessage_);
+
+/**
+ * Access helpers
+ */
+export const { hitMe } = F.access(RandomMessage)[RandomMessageURI];
+
+/**
+ * Implementation
+ */
+
+export const env = {
+  [RandomMessageURI]: {
+    // tslint:disable-next-line: no-bitwise
+    hitMe: () => T.sync(() => messages[~~(Math.random() * messages.length)])
+  }
+};
+
+export const provideRandomMessage = F.implement(RandomMessage)(env);
+
+const messages = ["Hi", "Bye", "Good day", "Good evening"];
