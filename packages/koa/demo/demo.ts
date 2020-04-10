483c91aa197db073b44df6d5451410cdb7d93933
Patrick Roza <contact@patrickroza.com>
2020-04-10 10:55:14 +0200

    Use more composable approach.

--- packages/koa/demo/demo.ts
+++ packages/koa/demo/demo.ts
@@ -2,35 +2,32 @@ import { effect as T, exit as E } from "@matechs/effect";
 import * as KOA from "../src";
 import { Do } from "fp-ts-contrib/lib/Do";
 import { pipe } from "fp-ts/lib/pipeable";
+import { sequenceT } from "fp-ts/lib/Apply";
 
 const program = KOA.withApp(
   Do(T.effect)
     .do(
-      KOA.router((R) =>
-        Do(T.effect)
-          .do(
-            R.route(
-              "get",
-              "/",
-              T.pure(
-                KOA.routeResponse(200, {
-                  message: "OK"
-                })
-              )
+      KOA.withRouter(
+        sequenceT(T.effect)(
+          KOA.route(
+            "get",
+            "/",
+            T.pure(
+              KOA.routeResponse(200, {
+                message: "OK"
+              })
+            )
+          ),
+          KOA.route(
+            "get",
+            "/test2",
+            T.pure(
+              KOA.routeResponse(200, {
+                message: "OK"
+              })
             )
           )
-          .do(
-            R.route(
-              "get",
-              "/test2",
-              T.pure(
-                KOA.routeResponse(200, {
-                  message: "OK"
-                })
-              )
-            )
-          )
-          .return(() => void 0)
+        )
       )
     )
     .bind("server", KOA.bind(8081))
