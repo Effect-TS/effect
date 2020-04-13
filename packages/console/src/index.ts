9ca98645a972f683d88937f9d39624e6e630b4b6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-04-13 22:27:53 +0100

    migrate console to eff sync

--- packages/console/src/index.ts
+++ packages/console/src/index.ts
@@ -1,28 +1,30 @@
-import { effect as T, freeEnv as F } from "@matechs/effect";
+import { eff as EFF, freeEnv as F } from "@matechs/effect";
 
 export const consoleURI = "@matechs/console/consoleURI";
 
+type Void = EFF.Eff<never, unknown, never, void>;
+
 const Console_ = F.define({
   [consoleURI]: {
-    assert: F.fn<(value: any, message?: string, ...optionalParams: any[]) => T.UIO<void>>(),
-    time: F.fn<(label?: string) => T.UIO<void>>(),
-    clear: F.cn<T.UIO<void>>(),
-    info: F.fn<(message?: any, ...optionalParams: any[]) => T.UIO<void>>(),
-    count: F.fn<(label?: string) => T.UIO<void>>(),
-    countReset: F.fn<(label?: string) => T.UIO<void>>(),
-    debug: F.fn<(message?: any, ...optionalParams: any[]) => T.UIO<void>>(),
-    dir: F.fn<(obj: any, options?: NodeJS.InspectOptions) => T.UIO<void>>(),
-    dirxml: F.fn<(...data: any[]) => T.UIO<void>>(),
-    error: F.fn<(message?: any, ...optionalParams: any[]) => T.UIO<void>>(),
-    group: F.fn<(...label: any[]) => T.UIO<void>>(),
-    groupCollapsed: F.fn<(...label: any[]) => T.UIO<void>>(),
-    groupEnd: F.cn<T.UIO<void>>(),
-    log: F.fn<(message?: any, ...optionalParams: any[]) => T.UIO<void>>(),
-    table: F.fn<(tabularData: any, properties?: string[]) => T.UIO<void>>(),
-    timeEnd: F.fn<(label?: string) => T.UIO<void>>(),
-    timeLog: F.fn<(label?: string, ...data: any[]) => T.UIO<void>>(),
-    trace: F.fn<(message?: any, ...optionalParams: any[]) => T.UIO<void>>(),
-    warn: F.fn<(message?: any, ...optionalParams: any[]) => T.UIO<void>>()
+    assert: F.fn<(value: any, message?: string, ...optionalParams: any[]) => Void>(),
+    time: F.fn<(label?: string) => Void>(),
+    clear: F.cn<Void>(),
+    info: F.fn<(message?: any, ...optionalParams: any[]) => Void>(),
+    count: F.fn<(label?: string) => Void>(),
+    countReset: F.fn<(label?: string) => Void>(),
+    debug: F.fn<(message?: any, ...optionalParams: any[]) => Void>(),
+    dir: F.fn<(obj: any, options?: NodeJS.InspectOptions) => Void>(),
+    dirxml: F.fn<(...data: any[]) => Void>(),
+    error: F.fn<(message?: any, ...optionalParams: any[]) => Void>(),
+    group: F.fn<(...label: any[]) => Void>(),
+    groupCollapsed: F.fn<(...label: any[]) => Void>(),
+    groupEnd: F.cn<Void>(),
+    log: F.fn<(message?: any, ...optionalParams: any[]) => Void>(),
+    table: F.fn<(tabularData: any, properties?: string[]) => Void>(),
+    timeEnd: F.fn<(label?: string) => Void>(),
+    timeLog: F.fn<(label?: string, ...data: any[]) => Void>(),
+    trace: F.fn<(message?: any, ...optionalParams: any[]) => Void>(),
+    warn: F.fn<(message?: any, ...optionalParams: any[]) => Void>()
   }
 });
 
@@ -30,29 +32,31 @@ export interface Console extends F.TypeOf<typeof Console_> {}
 
 export const Console = F.opaque<Console>()(Console_);
 
-export const provideConsole = F.implement(Console)({
+export const provideConsoleEff = F.implementEff(Console)({
   [consoleURI]: {
-    assert: (v, m, ...o) => T.sync(() => console.assert(v, m, ...o)),
-    clear: T.sync(() => console.clear()),
-    count: (l) => T.sync(() => console.count(l)),
-    countReset: (l) => T.sync(() => console.countReset(l)),
-    debug: (m, ...o) => T.sync(() => console.debug(m, ...o)),
-    dir: (ob, op) => T.sync(() => console.dir(ob, op)),
-    dirxml: (...d) => T.sync(() => console.dirxml(...d)),
-    error: (m, ...o) => T.sync(() => console.error(m, ...o)),
-    group: (...l) => T.sync(() => console.group(...l)),
-    groupCollapsed: (...l) => T.sync(() => console.groupCollapsed(...l)),
-    groupEnd: T.sync(() => console.groupEnd()),
-    info: (m, ...o) => T.sync(() => console.info(m, ...o)),
-    log: (m, ...o) => T.sync(() => console.log(m, ...o)),
-    table: (t, p) => T.sync(() => console.table(t, p)),
-    time: (l) => T.sync(() => console.time(l)),
-    timeEnd: (l) => T.sync(() => console.timeEnd(l)),
-    timeLog: (l, ...d) => T.sync(() => console.timeLog(l, ...d)),
-    trace: (m, ...o) => T.sync(() => console.trace(m, ...o)),
-    warn: (m, ...o) => T.sync(() => console.warn(m, ...o))
+    assert: (v, m, ...o) => EFF.sync(() => console.assert(v, m, ...o)),
+    clear: EFF.sync(() => console.clear()),
+    count: (l) => EFF.sync(() => console.count(l)),
+    countReset: (l) => EFF.sync(() => console.countReset(l)),
+    debug: (m, ...o) => EFF.sync(() => console.debug(m, ...o)),
+    dir: (ob, op) => EFF.sync(() => console.dir(ob, op)),
+    dirxml: (...d) => EFF.sync(() => console.dirxml(...d)),
+    error: (m, ...o) => EFF.sync(() => console.error(m, ...o)),
+    group: (...l) => EFF.sync(() => console.group(...l)),
+    groupCollapsed: (...l) => EFF.sync(() => console.groupCollapsed(...l)),
+    groupEnd: EFF.sync(() => console.groupEnd()),
+    info: (m, ...o) => EFF.sync(() => console.info(m, ...o)),
+    log: (m, ...o) => EFF.sync(() => console.log(m, ...o)),
+    table: (t, p) => EFF.sync(() => console.table(t, p)),
+    time: (l) => EFF.sync(() => console.time(l)),
+    timeEnd: (l) => EFF.sync(() => console.timeEnd(l)),
+    timeLog: (l, ...d) => EFF.sync(() => console.timeLog(l, ...d)),
+    trace: (m, ...o) => EFF.sync(() => console.trace(m, ...o)),
+    warn: (m, ...o) => EFF.sync(() => console.warn(m, ...o))
   }
 });
+
+export const provideConsole = EFF.providerToEffect(provideConsoleEff);
 
 export const {
   [consoleURI]: {
