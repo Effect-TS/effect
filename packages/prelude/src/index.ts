ea1ca254cb27562b817f68a0a18e62008f22213d
Michael Arnaldi <arnaldimichael@gmail.com>
2020-04-16 12:37:43 +0100

    re-export commonly used components

--- packages/prelude/src/index.ts
+++ packages/prelude/src/index.ts
@@ -6,6 +6,14 @@ import {
   streameither as SE
 } from "@matechs/effect";
 import * as F from "fp-ts/lib/function";
+import * as eq from "fp-ts/lib/Eq";
+import * as show from "fp-ts/lib/Show";
+import * as semigroup from "fp-ts/lib/Semigroup";
+import * as monoid from "fp-ts/lib/Monoid";
+import * as map from "fp-ts/lib/Map";
+import * as set from "fp-ts/lib/Set";
+import * as tree from "fp-ts/lib/Tree";
+import * as magma from "fp-ts/lib/Magma";
 import * as E from "./either";
 import * as A from "./array";
 import * as O from "./option";
@@ -19,6 +27,7 @@ export { E };
 export { Service };
 export { F };
 export { A };
+export { eq, show, semigroup, monoid, tree, map, set, magma };
 
 export class Pipe<A> {
   constructor(private readonly _: A) {
