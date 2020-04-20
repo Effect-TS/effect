6e96779dbf635f6565eb6ade8e18840fd98ee7cf
Michael Arnaldi <arnaldimichael@gmail.com>
2020-04-20 19:32:42 +0100

    fix coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,14 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
-  collectCoverageFrom: ["packages/**/src/**/*.ts"],
+  collectCoverageFrom: [
+    "packages/**/src/**/*.ts",
+    "packages_be/**/src/**/*.ts",
+    "packages_fe/**/src/**/*.ts",
+    "packages_http/**/src/**/*.ts",
+    "packages_inc/**/src/**/*.ts",
+    "packages_sys/**/src/**/*.ts"
+  ],
   setupFiles: ["./jest-setup.ts"],
   modulePathIgnorePatterns: ["dtslint"],
   verbose: true
