1ea029163813ac1ec3134171cad133592cb96cf1
Michael Arnaldi <arnaldimichael@gmail.com>
2020-04-22 18:10:13 +0100

    Features/aio (#207)
    * All in one package
    
    * re-export in retry
    
    * remove dtslint from aio

---
+++ packages/aio/src/types.ts
@@ -0,0 +1,51 @@
+import * as BooleanFromString from "io-ts-types/lib/BooleanFromString";
+import * as DateFromISOString from "io-ts-types/lib/DateFromISOString";
+import * as DateFromNumber from "io-ts-types/lib/DateFromNumber";
+import * as DateFromUnixTime from "io-ts-types/lib/DateFromUnixTime";
+import * as IntFromString from "io-ts-types/lib/IntFromString";
+import * as NonEmptyString from "io-ts-types/lib/NonEmptyString";
+import * as NumberFromString from "io-ts-types/lib/NumberFromString";
+import * as UUID from "io-ts-types/lib/UUID";
+import * as clone from "io-ts-types/lib/clone";
+import * as date from "io-ts-types/lib/date";
+import * as either from "io-ts-types/lib/either";
+import * as fromNewtype from "io-ts-types/lib/fromNewtype";
+import * as fromNullable from "io-ts-types/lib/fromNullable";
+import * as fromRefinement from "io-ts-types/lib/fromRefinement";
+import * as getLenses from "io-ts-types/lib/getLenses";
+import * as mapOutput from "io-ts-types/lib/mapOutput";
+import * as nonEmptyArray from "io-ts-types/lib/nonEmptyArray";
+import * as option from "io-ts-types/lib/option";
+import * as optionFromNullable from "io-ts-types/lib/optionFromNullable";
+import * as regexp from "io-ts-types/lib/regexp";
+import * as setFromArray from "io-ts-types/lib/setFromArray";
+import * as withFallback from "io-ts-types/lib/withFallback";
+import * as withMessage from "io-ts-types/lib/withMessage";
+import * as withValidate from "io-ts-types/lib/withValidate";
+
+export {
+  BooleanFromString,
+  DateFromISOString,
+  DateFromNumber,
+  DateFromUnixTime,
+  IntFromString,
+  NonEmptyString,
+  NumberFromString,
+  UUID,
+  clone,
+  date,
+  either,
+  fromNewtype,
+  fromNullable,
+  fromRefinement,
+  getLenses,
+  mapOutput,
+  nonEmptyArray,
+  option,
+  optionFromNullable,
+  regexp,
+  setFromArray,
+  withFallback,
+  withMessage,
+  withValidate
+};
