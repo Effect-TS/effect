9ee5896d503239889d68d2a328d0fbc2f2610702
Michael Arnaldi <arnaldimichael@gmail.com>
2020-04-28 00:35:09 +0100

    upgrade morphic - add effect matcher

--- packages/aio/src/morphic.ts
+++ packages/aio/src/morphic.ts
@@ -1,3 +1,5 @@
+import { T } from "@matechs/prelude";
+
 import {
   AsOpaque,
   AsUOpaque,
@@ -11,9 +13,80 @@ import { FastCheckURI } from "@morphic-ts/fastcheck-interpreters/lib/config";
 import { modelFastCheckInterpreter as fc } from "@morphic-ts/fastcheck-interpreters/lib/interpreters";
 import { IoTsURI } from "@morphic-ts/io-ts-interpreters/lib/config";
 import { ShowURI } from "@morphic-ts/show-interpreters/lib/config";
+import { MorphADT, AOfTypes, AOfMorhpADT } from "@morphic-ts/batteries/lib/usage/tagged-union";
+import { ProgramURI } from "@morphic-ts/batteries/lib/usage/ProgramType";
+import { InterpreterURI } from "@morphic-ts/batteries/lib/usage/InterpreterResult";
+import { AType, EType, RType } from "@morphic-ts/batteries/lib/usage/utils";
 
-export { AsOpaque, AsUOpaque, M, Summoner, UM, summonFor, EqURI, IoTsURI, FastCheckURI, ShowURI };
+export {
+  AsOpaque,
+  AsUOpaque,
+  M,
+  Summoner,
+  UM,
+  summonFor,
+  EqURI,
+  IoTsURI,
+  FastCheckURI,
+  ShowURI,
+  AType,
+  EType,
+  RType,
+  AOfMorhpADT
+};
 
 export const arb = <Env, E, A>(F: M<Env, E, A>) => (
   _: { [k in "FastCheckURI" & keyof Env]: Env[k] }
 ) => F.derive(fc<Env>())(_).arb;
+
+type Cases<Record, R> = { [key in keyof Record]: (v: Record[key]) => R };
+
+type ValueByKeyByTag<Union extends Record<any, any>, Tags extends keyof Union = keyof Union> = {
+  [Tag in Tags]: { [Key in Union[Tag]]: Union extends { [r in Tag]: Key } ? Union : never };
+};
+
+export interface MatcherWiden<A, Tag extends keyof A>
+  extends MatcherWidenIntern<A, ValueByKeyByTag<A>[Tag]> {}
+
+interface MatcherWidenIntern<A, Record> {
+  <M extends Cases<Record, any>>(match: M): (
+    a: A
+  ) => ReturnType<M[keyof M]> extends T.Effect<infer S, infer R, infer E, infer A>
+    ? T.Effect<S, R, E, A>
+    : ReturnType<M[keyof M]> extends infer K
+    ? K
+    : never;
+  <
+    M extends Partial<Cases<Record, any>>,
+    D extends (_: { [k in keyof Record]: Record[k] }[Exclude<keyof Record, keyof M>]) => any
+  >(
+    match: M,
+    def: D
+  ): (
+    a: A
+  ) =>
+    | (ReturnType<NonNullable<M[keyof M]>> extends infer R ? R : never)
+    | (unknown extends ReturnType<D> ? never : ReturnType<D>) extends T.Effect<
+    infer S,
+    infer R,
+    infer E,
+    infer A
+  >
+    ? T.Effect<S, R, E, A>
+    :
+        | (ReturnType<NonNullable<M[keyof M]>> extends infer R ? R : never)
+        | (unknown extends ReturnType<D> ? never : ReturnType<D>);
+}
+
+export const match = <
+  Types extends {
+    [k in keyof Types]: [any, any];
+  },
+  Tag extends string,
+  ProgURI extends ProgramURI,
+  InterpURI extends InterpreterURI,
+  R
+>(
+  _: MorphADT<Types, Tag, ProgURI, InterpURI, R>
+): MatcherWiden<AOfTypes<Types>, Tag> => (match: any, def?: any) => (a: any): any =>
+  (match[a[_.tag]] || def)(a);
--- packages/aio/tests/Morphic.test.ts
+++ packages/aio/tests/Morphic.test.ts
@@ -1,4 +1,4 @@
-import { MO } from "../src";
+import { MO, T, Ex, pipe, combineProviders } from "../src";
 import * as J from "@matechs/test-jest";
 
 interface Config {
@@ -8,7 +8,7 @@ interface Config {
   };
 }
 
-const { summon } = MO.summonFor<Config>({});
+const { summon, tagged } = MO.summonFor<Config>({});
 
 const Person = summon((F) =>
   F.interface(
@@ -27,6 +27,51 @@ const PersonArb = MO.arb(Person)({
   }
 });
 
+const A_ = summon((F) =>
+  F.interface(
+    {
+      _tag: F.stringLiteral("A"),
+      foo: F.string()
+    },
+    "A"
+  )
+);
+
+interface A extends MO.AType<typeof A_> {}
+interface A_ extends MO.EType<typeof A_> {}
+const A = MO.AsOpaque<A_, A>()(A_);
+
+const B_ = summon((F) =>
+  F.interface(
+    {
+      _tag: F.stringLiteral("B"),
+      bar: F.string()
+    },
+    "B"
+  )
+);
+
+interface B extends MO.AType<typeof B_> {}
+interface B_ extends MO.EType<typeof B_> {}
+const B = MO.AsOpaque<B_, B>()(B_);
+
+const C_ = summon((F) =>
+  F.interface(
+    {
+      _tag: F.stringLiteral("C"),
+      baz: F.string()
+    },
+    "C"
+  )
+);
+
+interface C extends MO.AType<typeof C_> {}
+interface C_ extends MO.EType<typeof C_> {}
+const C = MO.AsOpaque<C_, C>()(C_);
+
+const Tagged = tagged("_tag")({ A, B, C });
+const TaggedMatch = MO.match(Tagged);
+
 const MorphicSuite = J.suite("Morphic")(
   J.testM(
     "generate persons",
@@ -37,7 +82,100 @@ const MorphicSuite = J.suite("Morphic")(
       expect(pers.firstName.substr(0, 2)).toStrictEqual("f_");
       expect(pers.lastName.substr(0, 2)).toStrictEqual("l_");
     })
+  ),
+  J.testM(
+    "match",
+    T.sync(() => {
+      const matcher = TaggedMatch(
+        {
+          A: (a) => a._tag
+        },
+        (bc) => bc._tag
+      );
+
+      J.assert.deepStrictEqual(matcher(Tagged.of.A({ foo: "ok" })), "A");
+      J.assert.deepStrictEqual(matcher(Tagged.of.B({ bar: "ok" })), "B");
+      J.assert.deepStrictEqual(matcher(Tagged.of.C({ baz: "ok" })), "C");
+    })
+  ),
+  J.testM(
+    "match - 2",
+    T.sync(() => {
+      const matcher = TaggedMatch({
+        A: (a) => T.sync(() => a._tag),
+        B: (b) => T.sync(() => b._tag),
+        C: (c) => T.sync(() => c._tag)
+      });
+
+      J.assert.deepStrictEqual(T.runSync(matcher(Tagged.of.A({ foo: "ok" }))), Ex.done("A"));
+      J.assert.deepStrictEqual(T.runSync(matcher(Tagged.of.B({ bar: "ok" }))), Ex.done("B"));
+      J.assert.deepStrictEqual(T.runSync(matcher(Tagged.of.C({ baz: "ok" }))), Ex.done("C"));
+    })
+  ),
+  J.testM(
+    "match - 3",
+    T.sync(() => {
+      const matcher = TaggedMatch(
+        {
+          A: (a) => T.sync(() => a._tag),
+          B: (b) => T.sync(() => b._tag)
+        },
+        (c) => T.access((_: { t: string }) => c._tag)
+      );
+
+      J.assert.deepStrictEqual(
+        T.runSync(
+          pipe(
+            matcher(Tagged.of.A({ foo: "ok" })),
+            T.provide({
+              t: ""
+            })
+          )
+        ),
+        Ex.done("A")
+      );
+      J.assert.deepStrictEqual(
+        T.runSync(
+          pipe(
+            matcher(Tagged.of.B({ bar: "ok" })),
+            T.provide({
+              t: ""
+            })
+          )
+        ),
+        Ex.done("B")
+      );
+      J.assert.deepStrictEqual(
+        T.runSync(
+          pipe(
+            matcher(Tagged.of.C({ baz: "ok" })),
+            T.provide({
+              t: ""
+            })
+          )
+        ),
+        Ex.done("C")
+      );
+    })
+  ),
+  J.testM(
+    "match - 4",
+    pipe(
+      TaggedMatch(
+        {
+          A: (a) => T.sync(() => a._tag),
+          B: (b) => T.shiftAfter(T.sync(() => b._tag))
+        },
+        (c) => T.access((_: { t: string }) => c._tag)
+      )(Tagged.of.A({ foo: "ok" })),
+      T.map((res) => J.assert.deepStrictEqual(res, "A"))
+    )
   )
 );
 
-J.run(MorphicSuite)(J.provideGenerator);
+J.run(MorphicSuite)(
+  combineProviders()
+    .with(J.provideGenerator)
+    .with(T.provide({ t: "ok" }))
+    .done()
+);
