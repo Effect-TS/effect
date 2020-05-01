8e900c7f5b6d7a3fa1f0b85a48c485c3358e7bb2
Michael Arnaldi <arnaldimichael@gmail.com>
2020-05-01 18:12:59 +0100

    improve iots-types-exports

--- packages/aio/src/types.ts
+++ packages/aio/src/types.ts
@@ -1,31 +1,53 @@
-import * as BooleanFromString from "io-ts-types/lib/BooleanFromString";
-import * as DateFromISOString from "io-ts-types/lib/DateFromISOString";
-import * as DateFromNumber from "io-ts-types/lib/DateFromNumber";
-import * as DateFromUnixTime from "io-ts-types/lib/DateFromUnixTime";
-import * as IntFromString from "io-ts-types/lib/IntFromString";
-import * as NonEmptyString from "io-ts-types/lib/NonEmptyString";
-import * as NumberFromString from "io-ts-types/lib/NumberFromString";
-import * as UUID from "io-ts-types/lib/UUID";
-import * as clone from "io-ts-types/lib/clone";
-import * as date from "io-ts-types/lib/date";
-import * as either from "io-ts-types/lib/either";
-import * as fromNewtype from "io-ts-types/lib/fromNewtype";
-import * as fromNullable from "io-ts-types/lib/fromNullable";
-import * as fromRefinement from "io-ts-types/lib/fromRefinement";
-import * as getLenses from "io-ts-types/lib/getLenses";
-import * as mapOutput from "io-ts-types/lib/mapOutput";
-import * as nonEmptyArray from "io-ts-types/lib/nonEmptyArray";
-import * as option from "io-ts-types/lib/option";
-import * as optionFromNullable from "io-ts-types/lib/optionFromNullable";
-import * as regexp from "io-ts-types/lib/regexp";
-import * as setFromArray from "io-ts-types/lib/setFromArray";
-import * as withFallback from "io-ts-types/lib/withFallback";
-import * as withMessage from "io-ts-types/lib/withMessage";
-import * as withValidate from "io-ts-types/lib/withValidate";
+import { BooleanFromString, BooleanFromStringC } from "io-ts-types/lib/BooleanFromString";
+import { DateFromISOString, DateFromISOStringC } from "io-ts-types/lib/DateFromISOString";
+import { DateFromNumber, DateFromNumberC } from "io-ts-types/lib/DateFromNumber";
+import { DateFromUnixTime, DateFromUnixTimeC } from "io-ts-types/lib/DateFromUnixTime";
+import { IntFromString, IntFromStringC } from "io-ts-types/lib/IntFromString";
+import {
+  NonEmptyString,
+  NonEmptyStringBrand,
+  NonEmptyStringC
+} from "io-ts-types/lib/NonEmptyString";
+import { NumberFromString, NumberFromStringC } from "io-ts-types/lib/NumberFromString";
+import { UUID, UUIDBrand } from "io-ts-types/lib/UUID";
+import { clone } from "io-ts-types/lib/clone";
+import { DateC, date } from "io-ts-types/lib/date";
+import { EitherC, either } from "io-ts-types/lib/either";
+import { fromNewtype } from "io-ts-types/lib/fromNewtype";
+import { fromNullable } from "io-ts-types/lib/fromNullable";
+import { fromRefinement } from "io-ts-types/lib/fromRefinement";
+import { ExactHasLenses, HasLenses, getLenses } from "io-ts-types/lib/getLenses";
+import { mapOutput } from "io-ts-types/lib/mapOutput";
+import { NonEmptyArrayC, nonEmptyArray } from "io-ts-types/lib/nonEmptyArray";
+import { OptionC, option } from "io-ts-types/lib/option";
+import { OptionFromNullableC, optionFromNullable } from "io-ts-types/lib/optionFromNullable";
+import { RegExpC, regexp } from "io-ts-types/lib/regexp";
+import { SetFromArrayC, setFromArray } from "io-ts-types/lib/setFromArray";
+import { withFallback } from "io-ts-types/lib/withFallback";
+import { withMessage } from "io-ts-types/lib/withMessage";
+import { withValidate } from "io-ts-types/lib/withValidate";
 
 export {
   BooleanFromString,
+  BooleanFromStringC,
   DateFromISOString,
+  DateC,
+  DateFromISOStringC,
+  DateFromNumberC,
+  DateFromUnixTimeC,
+  EitherC,
+  ExactHasLenses,
+  HasLenses,
+  IntFromStringC,
+  NonEmptyArrayC,
+  NonEmptyStringBrand,
+  NonEmptyStringC,
+  NumberFromStringC,
+  OptionC,
+  OptionFromNullableC,
+  RegExpC,
+  SetFromArrayC,
+  UUIDBrand,
   DateFromNumber,
   DateFromUnixTime,
   IntFromString,
