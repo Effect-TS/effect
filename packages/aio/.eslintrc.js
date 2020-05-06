408ac1fe07fd7d5b174cbdb5cc58de874bf114d7
Patrick Roza <contact@patrickroza.com>
2020-05-06 18:46:06 +0200

    Switch to ESLint

---
+++ packages/aio/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      typescript: {
+        "alwaysTryTypes": true
+      }, // this loads <rootdir>/tsconfig.json to eslint
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    // "plugin:jest/recommended", // disabled - because we use expect outside it/test blocks of jest.
+    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  plugins: [
+    "import",
+    "sort-destructure-keys",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_"}],
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      {
+      "allowSingleExtends": true
+      }
+  ],
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "sort-destructure-keys/sort-destructure-keys": "error", // Mainly to sort render props
+    "import/no-default-export": "error",
+    "import/no-unresolved": "error",
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroups: [{ pattern: "@/**", group: "external", position:"after" },],
+        alphabetize: {order: "asc"},
+        groups: ["builtin", "external", "parent", "sibling", "index"],
+      },
+    ],
+    "object-shorthand": "error",
+    "prefer-destructuring": "warn",
+  },
+};
