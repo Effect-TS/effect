447d69e06f84eb82d24d6b2c9b072d07dff191ab
Patrick Roza <contact@patrickroza.com>
2020-05-06 21:21:17 +0200

    Remove dupe ignore and cleanup config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
-module.exports =  {
-  "ignorePatterns": ["dtslint/", "lib/", "es6/"],
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+module.exports = {
+  ignorePatterns: ["dtslint/", "lib/", "es6/"],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
-//    tsconfigRootDir: __dirname,
-//    project: './tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
   settings: {
     "import/parsers": {
@@ -14,15 +12,13 @@ module.exports =  {
     "import/resolver": {
       typescript: {
         "alwaysTryTypes": true
-      }, // this loads <rootdir>/tsconfig.json to eslint
+      },
     },
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    // "plugin:jest/recommended", // disabled - because we use expect outside it/test blocks of jest.
     'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
@@ -39,21 +35,19 @@ module.exports =  {
     "@typescript-eslint/no-empty-interface": [
       "error",
       {
-      "allowSingleExtends": true
+        "allowSingleExtends": true
       }
-  ],
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "sort-destructure-keys/sort-destructure-keys": "error", // Mainly to sort render props
+    ],
     "@typescript-eslint/camelcase": "off",
+    "sort-destructure-keys/sort-destructure-keys": "error",
     "import/no-unresolved": "error",
     "no-irregular-whitespace": "off",
     "import/order": [
       "error",
       {
         "newlines-between": "always",
-        pathGroups: [{ pattern: "@/**", group: "external", position:"after" },],
-        alphabetize: {order: "asc"},
+        pathGroups: [{ pattern: "@/**", group: "external", position: "after" },],
+        alphabetize: { order: "asc" },
         groups: ["builtin", "external", "parent", "sibling", "index"],
       },
     ],
