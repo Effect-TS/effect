4b197033a8fba54b612ad620e7455d74ed89e900
Patrick Roza <contact@patrickroza.com>
2020-05-06 18:57:07 +0200

    turn off unused vars

--- packages_http/apollo/.eslintrc.js
+++ packages_http/apollo/.eslintrc.js
@@ -31,7 +31,7 @@ module.exports =  {
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_"}],
+    "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/interface-name-prefix": "off",
