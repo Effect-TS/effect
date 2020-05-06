4d78d033849e8c60fa7e87c5d7db67d3124068aa
Patrick Roza <contact@patrickroza.com>
2020-05-06 18:53:52 +0200

    Disable requiring-type-checking rules for now.

--- packages_http/http-client-libcurl/.eslintrc.js
+++ packages_http/http-client-libcurl/.eslintrc.js
@@ -3,8 +3,8 @@ module.exports =  {
   parserOptions: {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json',
+//    tsconfigRootDir: __dirname,
+//    project: './tsconfig.json',
   },
   settings: {
     "import/parsers": {
@@ -20,7 +20,7 @@ module.exports =  {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
     // "plugin:jest/recommended", // disabled - because we use expect outside it/test blocks of jest.
     'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
