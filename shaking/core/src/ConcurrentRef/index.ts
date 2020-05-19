a482ca43801165ae25cf13aa3696f6999844d3f6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-05-19 18:20:44 +0100

    Continue with Single file Modules

--- shaking/core/src/ConcurrentRef/index.ts
+++ shaking/core/src/ConcurrentRef/index.ts
@@ -1,1 +1,89 @@
-export { ConcurrentRef, makeConcurrentRef } from "./concurrentRef"
+import * as T from "../Effect"
+import type { FunctionN } from "../Function"
+import { makeSemaphore } from "../Semaphore"
+
+export interface ConcurrentRef<A> {
+  /**
+   * Get the current value of the ConcurrentRef
+   */
+  readonly get: T.Async<A>
+  /**
+   * Set the current value of the ConcurrentRef
+   * @param a
+   */
+  set<R>(a: T.AsyncRE<R, never, A>): T.AsyncRE<R, never, A>
+  /**
+   * Update the current value of the ConcurrentRef with an effect.
+   * Produces the new value
+   * @param f
+   */
+  update<R>(f: FunctionN<[A], T.AsyncRE<R, never, A>>): T.AsyncRE<R, never, A>
+  /**
+   * Update the current value of a ConcurrentRef with an effect.
+   *
+   * This function may return a second value of type B that will be produced on complete
+   * @param f
+   */
+  modify<R, B>(
+    f: FunctionN<[A], T.AsyncRE<R, never, readonly [B, A]>>
+  ): T.AsyncRE<R, never, B>
+}
+
+/**
+ * Creates an IO that will allocate a ConcurrentRef.
+ */
+export const makeConcurrentRef = <A>(initial: A): T.Sync<ConcurrentRef<A>> =>
+  T.map_(makeSemaphore(1), (semaphore) => {
+    let value = initial
+
+    const get = T.sync(() => value)
+
+    const set = <R>(a: T.AsyncRE<R, never, A>): T.AsyncRE<R, never, A> =>
+      semaphore.withPermit(
+        T.map_(a, (a) => {
+          const prev = value
+          value = a
+          return prev
+        })
+      )
+
+    const update = <R>(
+      f: FunctionN<[A], T.AsyncRE<R, never, A>>
+    ): T.AsyncRE<R, never, A> =>
+      semaphore.withPermit(
+        T.map_(
+          T.chain_(
+            T.sync(() => value),
+            f
+          ),
+          (v) => {
+            value = v
+            return v
+          }
+        )
+      )
+
+    const modify = <R, B>(
+      f: FunctionN<[A], T.AsyncRE<R, never, readonly [B, A]>>
+    ): T.AsyncRE<R, never, B> =>
+      semaphore.withPermit(
+        T.map_(
+          T.chain_(
+            T.sync(() => value),
+            f
+          ),
+          (v) => {
+            const [b, a] = v
+            value = a
+            return b
+          }
+        )
+      )
+
+    return {
+      get,
+      set,
+      update,
+      modify
+    }
+  })
--- shaking/core/src/Deferred/index.ts
+++ shaking/core/src/Deferred/index.ts
@@ -1,13 +1,130 @@
-export {
-  Async,
-  AsyncE,
-  AsyncR,
-  AsyncRE,
-  Deferred,
-  DeferredImpl,
-  Sync,
-  SyncE,
-  SyncR,
-  SyncRE,
-  makeDeferred
-} from "./deferred"
+import * as T from "../Effect"
+import type { Exit, Cause } from "../Exit"
+import { Completable, makeCompletable } from "../Support/Completable"
+
+export interface Deferred<S, R, E, A> {
+  /**
+   * Wait for this deferred to complete.
+   *
+   * This Stack will produce the value set by done, raise the error set by error or interrupt
+   */
+  readonly wait: T.AsyncRE<R, E, A>
+  /**
+   * Interrupt any waitersa on this Deferred
+   */
+  interrupt: T.Sync<void>
+  /**
+   * Complete this Deferred with a value
+   *
+   * Any waiters will receive it
+   * @param a
+   */
+  done(a: A): T.Sync<void>
+  /**
+   *
+   * @param e Complete this deferred with an error
+   *
+   * Any waiters will produce an error
+   */
+  error(e: E): T.Sync<void>
+
+  /**
+   * Complete this Deferred with an abort
+   *
+   * Any waiters will produce an error
+   * @param e
+   */
+  abort(e: unknown): T.Sync<void>
+
+  /**
+   * Complete this deferred with the given cuase
+   * @param c
+   */
+  cause(c: Cause<E>): T.Sync<void>
+
+  /**
+   * complete this Defered with the provide exit status
+   * @param e
+   */
+  complete(e: Exit<E, A>): T.Sync<void>
+
+  /**
+   * Set this deferred with the result of source
+   * @param source
+   */
+  from(source: T.Effect<S, R, E, A>): T.Effect<S, unknown, never, void>
+}
+
+export type Async<A> = Deferred<unknown, unknown, never, A>
+export type AsyncE<E, A> = Deferred<unknown, unknown, E, A>
+export type AsyncR<R, A> = Deferred<unknown, R, never, A>
+export type AsyncRE<R, E, A> = Deferred<unknown, R, E, A>
+
+export type Sync<A> = Deferred<never, unknown, never, A>
+export type SyncE<E, A> = Deferred<never, unknown, E, A>
+export type SyncR<R, A> = Deferred<never, R, never, A>
+export type SyncRE<R, E, A> = Deferred<never, R, E, A>
+
+export class DeferredImpl<S, R, E, A> implements Deferred<S, R, E, A> {
+  wait: T.AsyncRE<R, E, A>
+  interrupt: T.Sync<void>
+  c: Completable<T.Effect<S, R, E, A>>
+
+  constructor(readonly r: R) {
+    this.c = makeCompletable()
+
+    this.wait = T.flatten(
+      T.asyncTotal<T.Effect<S, R, E, A>>((callback) => this.c.listen(callback))
+    )
+
+    this.interrupt = T.sync(() => {
+      this.c.complete(T.raiseInterrupt)
+    })
+  }
+
+  done(a: A): T.Sync<void> {
+    return T.sync(() => {
+      this.c.complete(T.pure(a))
+    })
+  }
+
+  error(e: E): T.Sync<void> {
+    return T.sync(() => {
+      this.c.complete(T.raiseError(e))
+    })
+  }
+
+  abort(e: unknown): T.Sync<void> {
+    return T.sync(() => {
+      this.c.complete(T.raiseAbort(e))
+    })
+  }
+
+  cause(e: Cause<E>): T.Sync<void> {
+    return T.sync(() => {
+      this.c.complete(T.raised(e))
+    })
+  }
+
+  complete(exit: Exit<E, A>): T.Sync<void> {
+    return T.sync(() => {
+      this.c.complete(T.completed(exit))
+    })
+  }
+
+  from(source: T.Effect<S, R, E, A>): T.Effect<S, unknown, never, void> {
+    const completed = T.chain_(T.result(T.provide(this.r as R)(source)), (e) =>
+      this.complete(e)
+    )
+    return T.onInterrupted_(completed, this.interrupt)
+  }
+}
+
+export function makeDeferred<S, R, E, A, E2 = never>(): T.Effect<
+  never,
+  R,
+  E2,
+  Deferred<S, R, E, A>
+> {
+  return T.access((r: R) => new DeferredImpl(r))
+}
