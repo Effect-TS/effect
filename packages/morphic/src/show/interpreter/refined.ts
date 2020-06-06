df036c7caf32976f959e89f7cf43d9f648e41779
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-06 12:25:35 +0100

    Config for Refined

--- packages/morphic/src/show/interpreter/refined.ts
+++ packages/morphic/src/show/interpreter/refined.ts
@@ -2,21 +2,36 @@ import { memo } from "../../utils"
 import { showApplyConfig } from "../config"
 import { ShowType, ShowURI } from "../hkt"
 
+import { introduce } from "@matechs/core/Function"
+import type { Show } from "@matechs/core/Show"
 import type { AnyEnv } from "@matechs/morphic-alg/config"
 import type { MatechsAlgebraRefined1 } from "@matechs/morphic-alg/refined"
+
+declare module "@matechs/morphic-alg/refined" {
+  interface RefinedConfig<E, A, B> {
+    [ShowURI]: {
+      show: Show<A>
+    }
+  }
+}
 
 export const showRefinedInterpreter = memo(
   <Env extends AnyEnv>(): MatechsAlgebraRefined1<ShowURI, Env> => ({
     _F: ShowURI,
     refined: (getShow, _ref, name, config) => (env) =>
-      new ShowType(
-        showApplyConfig(config)(
-          {
-            show: (x) => `<${name}>(${getShow(env).show.show(x)})`
-          },
-          env,
-          {}
-        )
+      introduce(getShow(env).show)(
+        (show) =>
+          new ShowType(
+            showApplyConfig(config)(
+              {
+                show: (x) => `<${name}>(${show.show(x)})`
+              },
+              env,
+              {
+                show
+              }
+            )
+          )
       )
   })
 )
