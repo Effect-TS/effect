689f86188a646e97487e802d96ecc00f20f08523
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-08 17:33:46 +0100

    Promote fast equals to dependency and remove leftover

--- packages/morphic/src/eq/interpreter/unknown.ts
+++ packages/morphic/src/eq/interpreter/unknown.ts
@@ -4,13 +4,8 @@ import { memo } from "../../utils"
 import { eqApplyConfig } from "../config"
 import { EqType, EqURI } from "../hkt"
 
-import type { Eq } from "@matechs/core/Eq"
 import type { AnyEnv } from "@matechs/morphic-alg/config"
 import type { MatechsAlgebraUnknown1 } from "@matechs/morphic-alg/unknown"
-
-export interface CustomizeUnknown<RC> {
-  compare: "default-circular" | "default-non-circular" | ((env: RC) => Eq<unknown>)
-}
 
 export const eqUnknownInterpreter = memo(
   <Env extends AnyEnv>(): MatechsAlgebraUnknown1<EqURI, Env> => ({
