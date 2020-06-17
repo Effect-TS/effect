d1c86f8efd46579514dbeadf9e2760dee2e05695
Jascha <jascha.smacka@akquinet.de>
2020-06-17 09:09:57 +0200

    #273  Add some tests and fix issue (breaking)

--- packages/effect/src/Effect/index.ts
+++ packages/effect/src/Effect/index.ts
@@ -602,7 +602,7 @@ export const filterOrElse: {
 } = <E, A>(predicate: Predicate<A>, onFalse: (a: A) => E) => <S, R>(
   ma: Effect<S, R, E, A>
 ): Effect<S, R, E, A> =>
-  chain_(ma, (a) => (predicate(a) ? completed(raise(onFalse(a))) : completed(done(a))))
+  chain_(ma, (a) => (predicate(a) ? completed(done(a)) : completed(raise(onFalse(a)))))
 
 export const fromPredicate: {
   <E, A, B extends A>(refinement: Refinement<A, B>, onFalse: (a: A) => E): (
