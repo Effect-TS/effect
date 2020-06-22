06e7bd98ecc7a6627bebbf27b4ef5e54a0951d74
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-22 18:09:56 +0100

    WIP: Improve run function names

--- src/Eff/Effect/runtime.ts
+++ src/Eff/Effect/runtime.ts
@@ -4,9 +4,7 @@ import { failed } from "../Cause/failed"
 import { pretty } from "../Cause/pretty"
 // exit
 import { FiberFailure } from "../Errors"
-import { die } from "../Exit/die"
 import { Exit } from "../Exit/exit"
-import { flatten } from "../Exit/flatten"
 // fiber
 import { FiberContext } from "../Fiber/context"
 import { newFiberId } from "../Fiber/id"
@@ -16,7 +14,7 @@ import { Callback } from "../Fiber/state"
 import * as Scope from "../Scope"
 import * as Supervisor from "../Supervisor"
 
-import { Effect, Async } from "./effect"
+import { Async, Effect } from "./effect"
 
 // empty function
 const empty = () => {
@@ -26,10 +24,7 @@ const empty = () => {
 /**
  * Runs effect until completion, calling cb with the eventual exit state
  */
-export const unsafeRunAsync = <S, E, A>(
-  _: Effect<S, {}, E, A>,
-  cb?: Callback<E, A>
-) => {
+export const runAsync = <S, E, A>(_: Effect<S, {}, E, A>, cb?: Callback<E, A>) => {
   const initialIS = interruptible
   const fiberId = newFiberId()
   const scope = Scope.unsafeMakeScope<Exit<E, A>>()
@@ -56,9 +51,11 @@ export interface CancelMain {
  * triggers cancellation of the process, in case errors are found process will
  * exit with a status of 2 and cause will be pretty printed, if interruption
  * is found without errors the cause is pretty printed and process exits with
- * status 0. In the success scenario process exits with status 0 witout any log
+ * status 0. In the success scenario process exits with status 0 witout any log.
+ *
+ * Note: this should be used only in node.js as it depends on process.exit
  */
-export const unsafeRunMain = <S, E>(effect: Effect<S, {}, E, void>): CancelMain => {
+export const runMain = <S, E>(effect: Effect<S, {}, E, void>): CancelMain => {
   const initialIS = interruptible
   const fiberId = newFiberId()
   const scope = Scope.unsafeMakeScope<Exit<E, void>>()
@@ -78,22 +75,22 @@ export const unsafeRunMain = <S, E>(effect: Effect<S, {}, E, void>): CancelMain 
       case "Failure": {
         if (died(exit.cause) || failed(exit.cause)) {
           console.error(pretty(exit.cause))
-          //process.exit(2)
+          process.exit(2)
         } else {
           console.log(pretty(exit.cause))
-          //process.exit(0)
+          process.exit(0)
         }
         break
       }
       // eslint-disable-next-line no-fallthrough
       case "Success": {
-        //process.exit(0)
+        process.exit(0)
       }
     }
   })
 
   return () => {
-    unsafeRunAsync(context.interruptAs(fiberId))
+    runAsync(context.interruptAs(fiberId))
   }
 }
 
@@ -106,7 +103,7 @@ export type AsyncCancel<E, A> = Async<Exit<E, A>>
  * Runs effect until completion returing a cancel effecr that when executed
  * triggers cancellation of the process
  */
-export const unsafeRunAsyncCancelable = <S, E, A>(
+export const runAsyncCancel = <S, E, A>(
   _: Effect<S, {}, E, A>,
   cb?: Callback<E, A>
 ): AsyncCancel<E, A> => {
@@ -134,7 +131,7 @@ export const unsafeRunAsyncCancelable = <S, E, A>(
  * Run effect as a Promise, throwing a FiberFailure containing the cause of exit
  * in case of error.
  */
-export const unsafeRunPromise = <S, E, A>(_: Effect<S, {}, E, A>): Promise<A> => {
+export const runPromise = <S, E, A>(_: Effect<S, {}, E, A>): Promise<A> => {
   const initialIS = interruptible
   const fiberId = newFiberId()
   const scope = Scope.unsafeMakeScope<Exit<E, A>>()
@@ -168,10 +165,39 @@ export const unsafeRunPromise = <S, E, A>(_: Effect<S, {}, E, A>): Promise<A> =>
 }
 
 /**
- * Run effect as a Promise, throwing a FiberFailure containing the cause of exit
+ * Run effect as a Promise of the Exit state
  * in case of error.
  */
-export const unsafeRunSync = <E, A>(_: Effect<never, {}, E, A>): Exit<E, A> => {
+export const runPromiseExit = <S, E, A>(
+  _: Effect<S, {}, E, A>
+): Promise<Exit<E, A>> => {
+  const initialIS = interruptible
+  const fiberId = newFiberId()
+  const scope = Scope.unsafeMakeScope<Exit<E, A>>()
+  const supervisor = Supervisor.none
+
+  const context = new FiberContext<E, A>(
+    fiberId,
+    {},
+    initialIS,
+    new Map(),
+    supervisor,
+    scope
+  )
+
+  context.evaluateNow(_.asInstruction)
+
+  return new Promise((res) => {
+    context.runAsync((exit) => {
+      res(exit)
+    })
+  })
+}
+
+/**
+ * Run effect as a synchronously, returning the full exit state
+ */
+export const runSyncExit = <E, A>(_: Effect<never, {}, E, A>): Exit<E, A> => {
   const initialIS = interruptible
   const fiberId = newFiberId()
   const scope = Scope.unsafeMakeScope<Exit<E, A>>()
@@ -193,12 +219,43 @@ export const unsafeRunSync = <E, A>(_: Effect<never, {}, E, A>): Exit<E, A> => {
   if (state._tag === "Done") {
     return state.value
   } else {
-    unsafeRunAsync(context.interruptAs(fiberId), (e) => {
-      console.error(
-        "Bug: unsafeRunSync did not complete, interrupted with:",
-        flatten(e)
-      )
-    })
-    return die("Bug: unsafeRunSync did not complete")
+    throw new Error("Fatal(Bug): runSyncExit called with async")
+  }
+}
+
+/**
+ * Run effect synchronously, throwing a FiberFailure containing the cause of exit
+ * in case of error.
+ */
+export const runSync = <E, A>(_: Effect<never, {}, E, A>): A => {
+  const initialIS = interruptible
+  const fiberId = newFiberId()
+  const scope = Scope.unsafeMakeScope<Exit<E, A>>()
+  const supervisor = Supervisor.none
+
+  const context = new FiberContext<E, A>(
+    fiberId,
+    {},
+    initialIS,
+    new Map(),
+    supervisor,
+    scope
+  )
+
+  context.evaluateNow(_.asInstruction)
+
+  const state = context.state.get
+
+  if (state._tag === "Done") {
+    switch (state.value._tag) {
+      case "Success": {
+        return state.value.value
+      }
+      case "Failure": {
+        throw new FiberFailure(state.value.cause)
+      }
+    }
+  } else {
+    throw new Error("Fatal(Bug): runSyncExit called with async")
   }
 }
--- src/Eff/demo2.ts
+++ src/Eff/demo2.ts
@@ -1,6 +1,6 @@
 import * as T from "./Effect"
 
-const cancel = T.unsafeRunMain(
+const cancel = T.runMain(
   T.bracket_(
     T.succeedNow(1),
     () => T.die("error"),
