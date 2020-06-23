69f260a7612791e81892fa24ee026ad11e4669af
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-23 18:37:59 +0100

    WIP: Deterministic Random, Remove constraints

--- packages/effect/src/Eff/Effect/accessM.ts
+++ packages/effect/src/Eff/Effect/accessM.ts
@@ -1,10 +1,9 @@
-import { AnyEnv } from "./AnyEnv"
 import { Effect } from "./effect"
 import { IRead } from "./primitives"
 
 /**
  * Effectfully accesses the environment of the effect.
  */
-export const accessM = <S, R, E, A, R0 extends AnyEnv = {}>(
+export const accessM = <R0, S, R, E, A>(
   f: (_: R0) => Effect<S, R, E, A>
 ): Effect<S, R & R0, E, A> => new IRead(f)
