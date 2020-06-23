ed76b009a04f14fa1819d10c2230e3a6a5c9aa03
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-23 19:16:36 +0100

    WIP: Reduce Random Env Footprint

--- src/Eff/Random/index.ts
+++ src/Eff/Random/index.ts
@@ -1,7 +1,8 @@
-import { provideAll } from "../Effect"
 import { accessM } from "../Effect/accessM"
-import { Sync, Effect } from "../Effect/effect"
+import { chain_ } from "../Effect/chain_"
+import { Effect } from "../Effect/effect"
 import { effectTotal } from "../Effect/effectTotal"
+import { provideAll_ } from "../Effect/provideAll_"
 
 import { PRNG } from "./Alea"
 
@@ -9,42 +10,14 @@ export const RandomURI = "@matechs/core/Eff/RandomURI"
 
 export interface Random {
   [RandomURI]: {
-    next: Sync<number>
-    nextBoolean: Sync<boolean>
-    nextIntBetween(low: number, high: number): Sync<number>
-    nextInt: Sync<number>
-    nextDouble: Sync<number>
-    nextRange(low: number, high: number): Sync<number>
-    setSeed: (seed: string) => Sync<void>
-    withSeed: (
-      seed: string
-    ) => <S, R, E, A>(_: Effect<S, R, E, A>) => Effect<S, R, E, A>
+    prng: PRNG
   }
 }
 
 export const randomLive = (seed = String(Math.random())): Random => {
-  let prng = new PRNG(seed)
-
   return {
     [RandomURI]: {
-      next: effectTotal(() => prng.next()),
-      nextBoolean: effectTotal(() => prng.next() > 0.5),
-      nextIntBetween: (high, low) =>
-        effectTotal(() => Math.floor((high - low + 1) * prng.next() + low)),
-      nextInt: effectTotal(() => prng.int32()),
-      nextDouble: effectTotal(() => prng.double()),
-      nextRange: (high, low) => effectTotal(() => (high - low) * prng.next() + low),
-      setSeed: (seed) =>
-        effectTotal(() => {
-          prng = new PRNG(seed)
-        }),
-      withSeed: (seed: string) => <S, R, E, A>(effect: Effect<S, R, E, A>) =>
-        accessM((r: R) =>
-          provideAll({
-            ...r,
-            ...randomLive(seed)
-          })(effect)
-        )
+      prng: new PRNG(seed)
     }
   }
 }
@@ -55,28 +28,39 @@ export const defaultRandom =
 
 export const next =
   /*#__PURE__*/
-  accessM((_: Random) => _[RandomURI].next)
+  accessM((_: Random) => effectTotal(() => _[RandomURI].prng.next()))
 
 export const nextBoolean =
   /*#__PURE__*/
-  accessM((_: Random) => _[RandomURI].nextBoolean)
+  chain_(next, (n) => effectTotal(() => n > 0.5))
 
 export const nextIntBetween = (low: number, high: number) =>
-  accessM((_: Random) => _[RandomURI].nextIntBetween(low, high))
+  chain_(next, (n) => effectTotal(() => Math.floor((high - low + 1) * n + low)))
 
 export const nextInt =
   /*#__PURE__*/
-  accessM((_: Random) => _[RandomURI].nextInt)
+  accessM((_: Random) => effectTotal(() => _[RandomURI].prng.int32()))
 
 export const nextDouble =
   /*#__PURE__*/
-  accessM((_: Random) => _[RandomURI].nextDouble)
+  accessM((_: Random) => effectTotal(() => _[RandomURI].prng.double()))
 
 export const nextRange = (low: number, high: number) =>
-  accessM((_: Random) => _[RandomURI].nextRange(low, high))
+  chain_(next, (n) => effectTotal(() => (high - low) * n + low))
 
 export const setSeed = (seed: string) =>
-  accessM((_: Random) => _[RandomURI].setSeed(seed))
+  accessM((_: Random) =>
+    effectTotal(() => {
+      _[RandomURI].prng.setSeed(seed)
+    })
+  )
 
-export const withSeed = (seed: string) => <S, R, E, A>(effect: Effect<S, R, E, A>) =>
-  accessM((_: Random) => _[RandomURI].withSeed(seed)(effect))
+export const withSeed = (seed: string) => <S, R, E, A>(
+  effect: Effect<S, R & Random, E, A>
+) =>
+  accessM((r: R) =>
+    provideAll_(effect, {
+      ...r,
+      ...randomLive(seed)
+    })
+  )
