bd0fc5e9463b55bd45394669724d040e6b0c1157
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-24 15:14:58 +0100

    WIP: Fix demo code

--- packages/core/src/Eff/_demos/demo5.ts
+++ packages/core/src/Eff/_demos/demo5.ts
@@ -2,21 +2,25 @@ import { pipe } from "../../Function"
 import * as T from "../Effect"
 import * as M from "../Managed"
 
-const managedExample = M.makeExit(
-  T.accessM(({ foo }: { foo: string }) => T.succeedNow(foo))
-)((s, e) =>
-  T.accessM((_: { bar: string }) =>
-    T.effectTotal(() => {
-      console.log("release:", s, _.bar, e)
-    })
+const managedExample = pipe(
+  T.accessM(({ foo }: { foo: string }) => T.succeedNow(foo)),
+  M.makeExit((s, e) =>
+    T.accessM((_: { bar: string }) =>
+      T.effectTotal(() => {
+        console.log("release:", s, _.bar, e)
+      })
+    )
   )
 )
 
-const use = M.use_(managedExample, (s) =>
-  T.effectTotal(() => {
-    console.log("use:", s)
-    return `used: ${s}`
-  })
+pipe(
+  managedExample,
+  M.use((s) =>
+    T.effectTotal(() => {
+      console.log("use:", s)
+      return `used: ${s}`
+    })
+  ),
+  T.provideAll({ foo: "foo", bar: "bar" }),
+  T.runMain
 )
-
-pipe(use, T.provideAll({ foo: "foo", bar: "bar" }), T.runMain)
