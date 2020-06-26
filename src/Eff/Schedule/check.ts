d317e0ecf3ecfcf2dabb3efecdc59fd13de40474
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-26 14:42:08 +0100

    WIP: Schedule check

---
+++ src/Eff/Schedule/check.ts
@@ -0,0 +1,26 @@
+import { chain_ } from "../Effect/chain_"
+import { Effect } from "../Effect/effect"
+import { fail } from "../Effect/fail"
+
+import { Schedule } from "./schedule"
+import { updated_ } from "./updated_"
+
+/**
+ * Peeks at the output produced by this schedule, executes some action, and
+ * then continues the schedule or not based on the specified state predicate.
+ */
+export const check_ = <S, R, A, B, S2, R2>(
+  self: Schedule<S, R, A, B>,
+  f: (a: A, b: B) => Effect<S2, R2, never, boolean>
+): Schedule<S | S2, R & R2, A, B> =>
+  updated_(self, (upd) => (a, s) =>
+    chain_(f(a, self.extract(a, s)), (b) => (b ? fail(undefined) : upd(a, s)))
+  )
+
+/**
+ * Peeks at the output produced by this schedule, executes some action, and
+ * then continues the schedule or not based on the specified state predicate.
+ */
+export const check = <A, B, S2, R2>(
+  f: (a: A, b: B) => Effect<S2, R2, never, boolean>
+) => <S, R>(self: Schedule<S, R, A, B>) => check_(self, f)
