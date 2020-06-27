525a8d5cb2b3c3f848e71cc2c8e3ed60606d7375
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-27 14:12:09 +0100

    WIP: Schedule ensuring / split / first. Effect tapError

--- packages/core/src/Eff/Schedule/split.ts
+++ packages/core/src/Eff/Schedule/split.ts
@@ -1,3 +1,4 @@
+import { zipPar_ } from "../Effect"
 import { zip_ } from "../Effect/zip_"
 
 import { Schedule, ScheduleClass } from "./schedule"
@@ -18,6 +19,6 @@ export const split_ = <S, R, A, B, S2, R2, A2, B2>(
 ): Schedule<unknown, R & R2, [A, A2], [B, B2]> =>
   new ScheduleClass<unknown, R & R2, [any, any], [A, A2], [B, B2]>(
     zip_(self.initial, that.initial),
-    ([a0, a1], [s0, s1]) => zip_(self.update(a0, s0), that.update(a1, s1)),
+    ([a0, a1], [s0, s1]) => zipPar_(self.update(a0, s0), that.update(a1, s1)),
     ([a0, a1], [s0, s1]) => [self.extract(a0, s0), that.extract(a1, s1)]
   )
