7a209c35c5a6592fd5fa24029b5b175b3899b5f2
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-27 23:18:07 +0100

    WIP: Schedule onDecision

--- packages/core/src/Eff/Schedule/updated_.ts
+++ packages/core/src/Eff/Schedule/updated_.ts
@@ -6,10 +6,10 @@ import { Schedule } from "./schedule"
  * Returns a new schedule with the update function transformed by the
  * specified update transformer.
  */
-export const updated_ = <S, R, ST, A, B, S2, R2>(
+export const updated_ = <S, R, ST, A, B, S2, R2, A1 extends A>(
   self: Schedule<S, R, ST, A, B>,
   f: (
     update: (a: A, s: ST) => Effect<S, R, void, any>
-  ) => (a: A, s: ST) => Effect<S2, R2, void, any>
-): Schedule<S | S2, R & R2, ST, A, B> =>
-  new Schedule<S | S2, R & R2, any, A, B>(self.initial, f(self.update), self.extract)
+  ) => (a: A1, s: ST) => Effect<S2, R2, void, any>
+): Schedule<S | S2, R & R2, ST, A1, B> =>
+  new Schedule<S | S2, R & R2, any, A1, B>(self.initial, f(self.update), self.extract)
