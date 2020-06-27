6e05b2ad80b7297c583d04516397b226cfda7b54
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-27 20:57:38 +0100

    WIP: Effect tapBoth

---
+++ src/Eff/Effect/tapBoth_.ts
@@ -0,0 +1,27 @@
+import * as E from "../../Either"
+import { failureOrCause } from "../Cause"
+
+import { chain_ } from "./chain_"
+import { Effect } from "./effect"
+import { foldCauseM_ } from "./foldCauseM_"
+import { halt } from "./halt"
+
+/**
+ * Returns an effect that effectfully "peeks" at the failure or success of
+ * this effect.
+ */
+export const tapBoth_ = <S, R, E, A, S2, R2, E2, S3, R3, E3>(
+  self: Effect<S, R, E, A>,
+  f: (e: E) => Effect<S2, R2, E2, any>,
+  g: (a: A) => Effect<S3, R3, E3, any>
+) =>
+  foldCauseM_(
+    self,
+    (c) =>
+      E.fold_(
+        failureOrCause(c),
+        (e) => chain_(f(e), () => halt(c)),
+        (_) => halt(c)
+      ),
+    g
+  )
