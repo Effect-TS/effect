4c8d18fbc6ba2c20f3e73d5bf134ef9ceba30948
Michael Arnaldi <arnaldimichael@gmail.com>
2020-06-30 21:55:30 +0100

    WIP: Schedule Fibonacci

--- packages/core/src/Eff/_demos/demo8.ts
+++ packages/core/src/Eff/_demos/demo8.ts
@@ -1,14 +1,38 @@
-import { pipe } from "../../Function"
+import { pipe, introduce } from "../../Function"
 import * as T from "../Effect"
 import * as S from "../Schedule"
 
 let i = 0
 
 // exponential capped
-const policy = pipe(
-  S.exponential(100),
-  S.untilOutput((n) => n > 5000)
-)
+export const exponentialCapped = (o?: {
+  base?: number
+  factor?: number
+  cap?: number
+}) =>
+  introduce({
+    base: o?.base || 100,
+    factor: o?.factor || 2,
+    cap: o?.cap || 5000
+  })(({ base, cap, factor }) =>
+    pipe(S.exponential(base, factor), S.both(S.duration(cap)))
+  )
+
+export const fibonacciCapped = (o?: { base?: number; cap?: number }) =>
+  introduce({
+    base: o?.base || 100,
+    cap: o?.cap || 5000
+  })(({ base, cap }) =>
+    pipe(
+      S.fibonacci(base),
+      S.both(S.duration(cap)),
+      S.tapOutput((n) =>
+        T.effectTotal(() => {
+          console.log(n)
+        })
+      )
+    )
+  )
 
 const program = pipe(
   T.suspend(() => {
@@ -16,7 +40,7 @@ const program = pipe(
     const r = Math.random()
     return r > 0.1 ? T.fail(`err: ${r}`) : T.succeedNow(i)
   }),
-  T.retry(policy),
+  T.retry(exponentialCapped({ base: 1000 })),
   T.chain((n) =>
     T.effectTotal(() => {
       console.log(n)
