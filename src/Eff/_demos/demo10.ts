acd7039c41a6ec77d063fda485fe62f78341082e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-01 19:04:37 +0100

    WIP: Has Improvements

--- src/Eff/_demos/demo10.ts
+++ src/Eff/_demos/demo10.ts
@@ -37,7 +37,7 @@ export class LiveConsole extends Console {
 
   putStrLn: (s: string) => T.Sync<void> = (s) =>
     T.chain_(this.format.formatString(s), (f) =>
-      T.provideServiceM(HasFormat)(T.succeedNow(this.format))(
+      T.provideService(HasFormat)(this.format)(
         T.effectTotal(() => {
           console.log(f)
         })
