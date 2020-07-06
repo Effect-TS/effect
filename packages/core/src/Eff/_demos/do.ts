a5e54e978684bd0d338f07e1f2f58a2732ed2c8a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-06 23:22:10 +0100

    WIP: add let to demo

--- packages/core/src/Eff/_demos/do.ts
+++ packages/core/src/Eff/_demos/do.ts
@@ -1,14 +1,16 @@
 import { pipe } from "../../Function"
 import * as T from "../Effect"
 
-pipe(
+const program = pipe(
   T.of,
   T.bind("foo", () => T.succeedNow(1)),
   T.merge(() => T.sequenceS({ bar: T.succeedNow(2), baz: T.succeedNow(3) })),
+  T.let("red", () => 4),
   T.tap((s) =>
     T.effectTotal(() => {
       console.log(s)
     })
-  ),
-  T.runMain
+  )
 )
+
+T.runMain(program)
