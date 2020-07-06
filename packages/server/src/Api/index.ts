35d1490530c0271034df321dadae1ef3fd59f0ce
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-06 16:20:26 +0100

    WIP: Improve Server API

---
+++ packages/server/src/Api/index.ts
@@ -0,0 +1,44 @@
+import * as R from "../Router"
+import {
+  accessConfigM,
+  config,
+  server,
+  Server,
+  accessRouteInputM,
+  body,
+  body_,
+  getBody,
+  next,
+  params,
+  params_,
+  response,
+  response_,
+  status
+} from "../Server"
+
+import * as T from "@matechs/core/Eff/Effect"
+import * as Has from "@matechs/core/Eff/Has"
+import * as L from "@matechs/core/Eff/Layer"
+
+export const makeServer = <K>(has: Has.Augumented<Server, K>) => {
+  return {
+    accessRouteInputM,
+    body,
+    body_,
+    getBody,
+    next,
+    params,
+    params_,
+    response,
+    response_,
+    status,
+    has,
+    child: R.child(has),
+    accessConfigM: accessConfigM(has),
+    accessServerM: T.accessServiceM(has),
+    route: R.route(has),
+    use: R.use(has),
+    config: config(has),
+    server: L.using_(R.root(has), server(has))
+  }
+}
