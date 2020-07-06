b0aeb91b34c897ecd08c5dc4f6e34835e17be5bc
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-06 22:29:30 +0100

    WIP: Pipeable do

---
+++ packages/core/src/Eff/Effect/do.ts
@@ -0,0 +1,40 @@
+import { pipe } from "../../Function"
+
+import { chain } from "./chain"
+import { Effect } from "./effect"
+import { map } from "./map"
+import { succeedNow } from "./succeedNow"
+
+const bind = <S, R, E, A, K, N extends string>(
+  tag: Exclude<N, keyof K>,
+  f: (_: K) => Effect<S, R, E, A>
+) => <S2, R2, E2>(
+  mk: Effect<S2, R2, E2, K>
+): Effect<S | S2, R & R2, E | E2, K & { [k in N]: A }> =>
+  pipe(
+    mk,
+    chain((k) =>
+      pipe(
+        k,
+        f,
+        map((a): K & { [k in N]: A } => ({ ...k, [tag]: a } as any))
+      )
+    )
+  )
+
+const let_ = <S, R, E, A, K, N extends string>(
+  tag: Exclude<N, keyof K>,
+  f: (_: K) => A
+) => <S2, R2, E2>(
+  mk: Effect<S2, R2, E2, K>
+): Effect<S | S2, R & R2, E | E2, K & { [k in N]: A }> =>
+  pipe(
+    mk,
+    map((k): K & { [k in N]: A } => ({ ...k, [tag]: f(k) } as any))
+  )
+
+const of =
+  /*#__PURE__*/
+  succeedNow({})
+
+export { let_ as let, bind, of }
