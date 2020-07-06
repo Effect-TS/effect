fcffa56c52b475b7e488d2f787b52007bdf6422d
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-07 00:04:14 +0100

    WIP: Improve API

--- packages/core/src/Eff/_demos/do.ts
+++ packages/core/src/Eff/_demos/do.ts
@@ -4,7 +4,7 @@ import * as T from "../Effect"
 const program = pipe(
   T.of,
   T.bind("foo", () => T.succeedNow(1)),
-  T.merge(() => T.sequenceS({ bar: T.succeedNow(2), baz: T.succeedNow(3) })),
+  T.bindAll(() => ({ bar: T.succeedNow(2), baz: T.succeedNow(3) })),
   T.let("red", () => 4),
   T.tap((s) =>
     T.effectTotal(() => {
