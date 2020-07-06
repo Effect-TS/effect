dad3576437261e343ae52f7e44811e80ea95c524
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-07 00:04:14 +0100

    WIP: Improve API

--- src/Eff/Effect/sequenceS.ts
+++ src/Eff/Effect/sequenceS.ts
@@ -1,6 +1,7 @@
 import { SOf, EnvOf } from "../../Base/Apply"
 import * as R from "../../Record"
 
+import { chain_ } from "./chain_"
 import { Effect } from "./effect"
 import { foreachParN_ } from "./foreachParN_"
 import { foreachPar_ } from "./foreachPar_"
@@ -34,6 +35,116 @@ export function sequenceS<NER extends Record<string, Effect<any, any, any, any>>
       return res
     }
   ) as any
+}
+
+export function bindAll<
+  K,
+  NER extends Record<string, Effect<any, any, any, any>> &
+    { [k in keyof K & keyof NER]?: never }
+>(
+  r: (k: K) => EnforceNonEmptyRecord<NER> & Record<string, Effect<any, any, any, any>>
+): <S, R, E>(
+  s: Effect<S, R, E, K>
+) => Effect<
+  SOf<NER>,
+  EnvOf<NER>,
+  {
+    [K in keyof NER]: [NER[K]] extends [Effect<any, any, infer E, any>] ? E : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [Effect<any, any, any, infer A>] ? A : never
+  }
+> {
+  return (s) =>
+    chain_(s, (k) =>
+      map_(
+        foreach_(
+          R.collect_(r(k), (k, v) => [k, v] as const),
+          ([_, e]) => map_(e, (a) => [_, a] as const)
+        ),
+        (values) => {
+          const res = {}
+          values.forEach(([k, v]) => {
+            res[k] = v
+          })
+          return res
+        }
+      )
+    ) as any
+}
+
+export function bindAllPar<
+  K,
+  NER extends Record<string, Effect<any, any, any, any>> &
+    { [k in keyof K & keyof NER]?: never }
+>(
+  r: (k: K) => EnforceNonEmptyRecord<NER> & Record<string, Effect<any, any, any, any>>
+): <S, R, E>(
+  s: Effect<S, R, E, K>
+) => Effect<
+  SOf<NER>,
+  EnvOf<NER>,
+  {
+    [K in keyof NER]: [NER[K]] extends [Effect<any, any, infer E, any>] ? E : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [Effect<any, any, any, infer A>] ? A : never
+  }
+> {
+  return (s) =>
+    chain_(s, (k) =>
+      map_(
+        foreachPar_(
+          R.collect_(r(k), (k, v) => [k, v] as const),
+          ([_, e]) => map_(e, (a) => [_, a] as const)
+        ),
+        (values) => {
+          const res = {}
+          values.forEach(([k, v]) => {
+            res[k] = v
+          })
+          return res
+        }
+      )
+    ) as any
+}
+
+export function bindAllParN(
+  n: number
+): <
+  K,
+  NER extends Record<string, Effect<any, any, any, any>> &
+    { [k in keyof K & keyof NER]?: never }
+>(
+  r: (k: K) => EnforceNonEmptyRecord<NER> & Record<string, Effect<any, any, any, any>>
+) => <S, R, E>(
+  s: Effect<S, R, E, K>
+) => Effect<
+  SOf<NER>,
+  EnvOf<NER>,
+  {
+    [K in keyof NER]: [NER[K]] extends [Effect<any, any, infer E, any>] ? E : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [Effect<any, any, any, infer A>] ? A : never
+  }
+> {
+  return (r) => (s) =>
+    chain_(s, (k) =>
+      map_(
+        foreachParN_(n)(
+          R.collect_(r(k), (k, v) => [k, v] as const),
+          ([_, e]) => map_(e, (a) => [_, a] as const)
+        ),
+        (values) => {
+          const res = {}
+          values.forEach(([k, v]) => {
+            res[k] = v
+          })
+          return res
+        }
+      )
+    ) as any
 }
 
 export function sequenceSPar<NER extends Record<string, Effect<any, any, any, any>>>(
