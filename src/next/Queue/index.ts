bb4c7124fb80d2b8835205ea64e2801dafc58cc3
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-21 18:00:20 +0100

    improve queue

--- src/next/Queue/index.ts
+++ src/next/Queue/index.ts
@@ -1,2 +1,15 @@
-export { Queue } from "./queue"
+export { Queue, XQueue } from "./xqueue"
 export { makeSliding, makeDropping, makeUnbounded, makeBounded } from "./make"
+export {
+  takeBetween,
+  awaitShutdown,
+  capacity,
+  isShutdown,
+  offer,
+  offerAll,
+  shutdown,
+  size,
+  take,
+  takeAll,
+  takeAllUpTo
+} from "./api"
