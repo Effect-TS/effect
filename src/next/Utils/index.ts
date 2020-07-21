090523a4843a29be02188d008962e75cd3a86479
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-21 12:35:23 +0100

    introduce pattern matecher

--- src/next/Utils/index.ts
+++ src/next/Utils/index.ts
@@ -12,3 +12,32 @@ export type KOf<T> = [T] extends [{ [_U]: infer URI }]
   : T
 
 export type Erase<R, K> = R & K extends K & infer R1 ? R1 : R
+
+export const pattern: <N extends string>(
+  n: N
+) => <
+  X extends { [k in N]: string },
+  K extends { [k in X[N]]: (_: Extract<X, { _tag: k }>) => any }
+>(
+  _: K
+) => (m: X) => ReturnType<K[keyof K]> = (n) => (_) => (m) => {
+  return _[m[n]](m as any)
+}
+
+export const patternDef: <N extends string>(
+  n: N
+) => <X extends { [k in N]: string }, T extends X[N], A, B>(
+  t: T,
+  f: (_: Extract<X, { _tag: T }>) => A,
+  d: (_: Exclude<X, { _tag: T }>) => B
+) => (m: X) => A | B = (n) => (t, f, d) => (m) => {
+  return m[n] === t ? f(m as any) : d(m as any)
+}
+
+export const matchTag =
+  /*#__PURE__*/
+  pattern("_tag")
+
+export const matchTagDef =
+  /*#__PURE__*/
+  patternDef("_tag")
