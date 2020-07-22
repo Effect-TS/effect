fee4e7c78b457a61330bf2fb55c3fe59c55b4d92
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-22 20:06:05 +0100

    continue refM

---
+++ demo/demo19.ts
@@ -0,0 +1,20 @@
+import { pipe } from "../src/Function"
+import * as T from "../src/next/Effect"
+import * as R from "../src/next/RefM"
+
+pipe(
+  T.of,
+  T.bind("ref", () => R.makeRefM(1)),
+  T.bind("n", ({ ref }) =>
+    pipe(
+      ref,
+      R.getAndUpdate((n) => T.succeedNow(n + 1))
+    )
+  ),
+  T.chain(({ n }) =>
+    T.effectTotal(() => {
+      console.log(n)
+    })
+  ),
+  T.runMain
+)
---
+++ src/next/RefM/index.ts
@@ -0,0 +1,24 @@
+export {
+  RefM,
+  Atomic,
+  RefMRE,
+  Derived,
+  DerivedAll,
+  RefME,
+  RefMR,
+  XRefM,
+  concrete
+} from "./XRefM"
+
+export {
+  modify,
+  getAndUpdateSome,
+  getAndUpdate,
+  getAndSet,
+  makeRefM,
+  updateSomeAndGet,
+  updateSome,
+  updateAndGet,
+  update,
+  modifySome
+} from "./api"
