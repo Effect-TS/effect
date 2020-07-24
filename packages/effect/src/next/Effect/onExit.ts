4360748707afc5bd55458078c3079392e7f44e05
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-24 23:53:20 +0100

    layer memoization

---
+++ packages/effect/src/next/Effect/onExit.ts
@@ -0,0 +1,24 @@
+import { Exit } from "../Exit/exit"
+
+import { bracketExit_ } from "./bracketExit_"
+import { Effect } from "./effect"
+import { unit } from "./unit"
+
+export const onExit_ = <S, R, E, A, S2, R2, E2>(
+  self: Effect<S, R, E, A>,
+  cleanup: (exit: Exit<E, A>) => Effect<S2, R2, E2, any>
+): Effect<S | S2, R & R2, E | E2, A> =>
+  bracketExit_(
+    unit,
+    () => self,
+    (_, e) => cleanup(e)
+  )
+
+export const onExit = <E, A, S2, R2, E2>(
+  cleanup: (exit: Exit<E, A>) => Effect<S2, R2, E2, any>
+) => <S, R>(self: Effect<S, R, E, A>): Effect<S | S2, R & R2, E | E2, A> =>
+  bracketExit_(
+    unit,
+    () => self,
+    (_, e) => cleanup(e)
+  )
