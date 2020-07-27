a60ba1977963f45fbf47b4e37c0b400a69beb79b
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-27 23:30:35 +0100

    fix type

--- src/next/Effect/toManaged.ts
+++ src/next/Effect/toManaged.ts
@@ -1,9 +1,12 @@
 import { fromEffect } from "../Managed/fromEffect"
 import { makeExit_ } from "../Managed/makeExit_"
+import { Managed } from "../Managed/managed"
 
 import { Effect } from "./effect"
 
 export const toManaged = <A = unknown, S = never, R = unknown>(
   release?: (a: A) => Effect<S, R, never, any>
-) => <S, R, E, A1 extends A>(self: Effect<S, R, E, A1>) =>
+) => <S1, R1, E1, A1 extends A>(
+  self: Effect<S1, R1, E1, A1>
+): Managed<S | S1, R1 & R, E1, A1> =>
   release ? makeExit_(self, (a) => release(a)) : fromEffect(self)
