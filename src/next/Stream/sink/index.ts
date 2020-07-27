be633156764892030f4cc8162bfd071da4914feb
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-27 15:34:37 +0100

    add important note on sink semantic

--- src/next/Stream/sink/index.ts
+++ src/next/Stream/sink/index.ts
@@ -5,6 +5,17 @@ import * as R from "../../Ref"
 import * as T from "../internal/effect"
 import * as M from "../internal/managed"
 
+// Important notes while writing sinks and combinators:
+// - What return values for sinks mean:
+//   Effect.unit - "need more values"
+//   Effect.fail([Either.Right(z), l]) - "ended with z and emit leftover l"
+//   Effect.fail([Either.Left(e), l]) - "failed with e and emit leftover l"
+// - Result of processing of the stream using the sink must not depend on how the stream is chunked
+//   (chunking-invariance)
+//   pipe(stream, run(sink), Effect.either) === pipe(stream, chunkN(1), run(sink), Effect.either)
+// - Sinks should always end when receiving a `None`. It is a defect to not end with some
+//   sort of result (even a failure) when receiving a `None`.
+// - Sinks can assume they will not be pushed again after emitting a value.
 export class Sink<S, R, E, I, L, Z> {
   constructor(readonly push: M.Managed<S, R, never, Push<S, R, E, I, L, Z>>) {}
 }
