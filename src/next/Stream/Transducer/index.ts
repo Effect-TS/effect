cb3d0aef28c172d4eec60f1ed57719112f7f4c9a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-28 17:31:05 +0100

    keep separated type level identity

--- src/next/Stream/Transducer/index.ts
+++ src/next/Stream/Transducer/index.ts
@@ -9,14 +9,16 @@ import * as M from "../_internal/managed"
 //
 //   Stated differently, after a first push(None), all subsequent push(None) must
 //   result in empty [].
-export interface Transducer<S, R, E, I, O>
-  extends M.Managed<
-    S,
-    R,
-    never,
-    (c: O.Option<A.Array<I>>) => T.Effect<S, R, E, A.Array<O>>
-  > {}
-
+export class Transducer<S, R, E, I, O> {
+  constructor(
+    readonly push: M.Managed<
+      S,
+      R,
+      never,
+      (c: O.Option<A.Array<I>>) => T.Effect<S, R, E, A.Array<O>>
+    >
+  ) {}
+}
 /**
  * Contract notes for transducers:
  * - When a None is received, the transducer must flush all of its internal state
@@ -32,4 +34,4 @@ export const transducer = <S, R, E, I, O, S1, R1>(
     never,
     (c: O.Option<A.Array<I>>) => T.Effect<S1, R1, E, A.Array<O>>
   >
-): Transducer<S | S1, R & R1, E, I, O> => push
+) => new Transducer<S | S1, R & R1, E, I, O>(push)
