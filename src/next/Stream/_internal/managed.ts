700284931179f8a5cbb2089e3603fe34f30b4919
Michael Arnaldi <arnaldimichael@gmail.com>
2020-07-30 22:15:55 +0100

    restructure next managed

--- src/next/Stream/_internal/managed.ts
+++ src/next/Stream/_internal/managed.ts
@@ -1,15 +1,21 @@
+export {
+  chain,
+  effectTotal,
+  ensuring,
+  finalizerRef,
+  foldCauseM,
+  fork,
+  map,
+  mapM,
+  mapM_,
+  succeedNow,
+  tap,
+  useNow,
+  zip,
+  zipWith,
+  zip_
+} from "../../Managed/core"
+
+export { bind, let, merge, of } from "../../Managed/do"
+
 export { Managed } from "../../Managed/managed"
-export { bind, merge, let, of } from "../../Managed/do"
-export { map } from "../../Managed/map"
-export { tap } from "../../Managed/tap"
-export { zip, zip_ } from "../../Managed/zip"
-export { zipWith } from "../../Managed/zipWith"
-export { mapM, mapM_ } from "../../Managed/mapM_"
-export { useNow } from "../../Managed/use_"
-export { succeedNow } from "../../Managed/succeedNow"
-export { chain } from "../../Managed/chain"
-export { finalizerRef } from "../../Managed/finalizerRef"
-export { effectTotal } from "../../Managed/effectTotal"
-export { ensuring } from "../../Managed/ensuring"
-export { foldCauseM } from "../../Managed/foldCauseM"
-export { fork } from "../../Managed/fork"
