d447974fbc10832e6f6ae23f408673d0933b0934
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-06 23:30:31 +0100

    improve

--- packages/effect/src/next/Prelude/Array/index.ts
+++ packages/effect/src/next/Prelude/Array/index.ts
@@ -1,8 +1,11 @@
 import * as A from "../../../Array"
 import { pipe } from "../../../Function"
-import { succeed } from "../Any"
+import { Any1, succeed } from "../Any"
+import { Applicative1 } from "../Applicative"
+import { AssociativeBoth1 } from "../AssociativeBoth"
+import { AssociativeFlatten1 } from "../AssociativeFlatten"
 import * as C from "../Closure"
-import { Covariant } from "../Covariant"
+import * as COVA from "../Covariant"
 import { Derive11 } from "../Derive"
 import * as Eq from "../Equal"
 import { HKT } from "../HKT"
@@ -21,18 +24,86 @@ export type URI = typeof URI
 
 declare module "../HKT" {
   interface URItoKind<A> {
-    [URI]: readonly A[]
+    [URI]: A.Array<A>
   }
 }
 
 /**
- * @category traversable
+ * The `Closure` for `Sum<Array<A>>`.
  */
+export function SumClosure<A>(): C.Closure<Sum<readonly A[]>> {
+  return pipe(Sum<readonly A[]>(), (SumArray) =>
+    C.make((l, r) => SumArray.wrap([...SumArray.unwrap(l), ...SumArray.unwrap(r)]))
+  )
+}
 
-export const Traversable: Traversable1<URI> = {
+/**
+ * The `Closure` for `Array<A>`.
+ */
+export function Closure<A>(): C.Closure<readonly A[]> {
+  return C.make((x, y) => [...x, ...y])
+}
+
+/**
+ * The `Identity` for `Array<A>`.
+ */
+export function Identity<A>(): I.Identity<readonly A[]> {
+  return I.make<readonly A[]>([], Closure<A>().combine)
+}
+
+/**
+ * The `Any` instance for `Array<A>`.
+ */
+export const Any: Any1<URI> = {
   URI,
-  map: (f) => (fa) => fa.map(f),
-  foreach: <G>(G: IB.IdentityBoth<G> & Covariant<G>) => <A, B>(
+  any: () => []
+}
+
+/**
+ * The `Covariant` instance for `Array<A>`.
+ */
+export const Covariant: COVA.Covariant1<URI> = {
+  URI,
+  map: A.map
+}
+
+/**
+ * The `Covariant` instance for `Array<A>`.
+ */
+export const AssociativeFlatten: AssociativeFlatten1<URI> = {
+  URI,
+  flatten: A.flatten
+}
+
+export const AssociativeBoth: AssociativeBoth1<URI> = {
+  URI,
+  both: (fb) => (fa) => A.zip_(fa, fb)
+}
+
+/**
+ * The `Monad` instance for `Array<A>`.
+ */
+export const Applicative: Applicative1<URI> = {
+  ...Any,
+  ...Covariant,
+  ...AssociativeBoth
+}
+
+/**
+ * The `Monad` instance for `Array<A>`.
+ */
+export const Monad: M.Monad1<URI> = {
+  ...Any,
+  ...Covariant,
+  ...AssociativeFlatten
+}
+
+/**
+ * The `Traversable` instance for `Array`.
+ */
+export const Traversable: Traversable1<URI> = {
+  ...Covariant,
+  foreach: <G>(G: IB.IdentityBoth<G> & COVA.Covariant<G>) => <A, B>(
     f: (a: A) => HKT<G, B>
   ) => (fa: readonly A[]): HKT<G, readonly B[]> =>
     A.reduce_(fa, succeed(G)([] as readonly B[]), (b, a) =>
@@ -45,40 +116,7 @@ export const Traversable: Traversable1<URI> = {
 }
 
 /**
- * @category closure
- */
-
-export function SumClosure<A>(): C.Closure<Sum<readonly A[]>> {
-  return pipe(Sum<readonly A[]>(), (SumArray) =>
-    C.make((l, r) => SumArray.wrap([...SumArray.unwrap(l), ...SumArray.unwrap(r)]))
-  )
-}
-
-export function Closure<A>(): C.Closure<readonly A[]> {
-  return C.make((x, y) => [...x, ...y])
-}
-
-/**
- * @category identity
- */
-
-export function Identity<A>(): I.Identity<readonly A[]> {
-  return I.make<readonly A[]>([], Closure<A>().combine)
-}
-
-/**
- * @category monad
- */
-
-export const Monad: M.Monad1<URI> = {
-  URI,
-  any: () => [],
-  map: (f) => (fa) => fa.map(f),
-  flatten: (ffa) => A.flatten(ffa)
-}
-
-/**
- * @category equal
+ * The `Derive<Array, Equal>` instance for `Equal<Array<A>>`.
  */
 export const Equal: Derive11<URI, Eq.URI> = {
   derive: (eq) => (y) => (x) =>
