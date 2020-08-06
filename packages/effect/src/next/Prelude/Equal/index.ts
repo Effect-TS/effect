8c0f0f7d74d77747239e4ac668e7c3440dcd9966
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-06 18:24:06 +0100

    continue

--- packages/effect/src/next/Prelude/Equal/index.ts
+++ packages/effect/src/next/Prelude/Equal/index.ts
@@ -1,4 +1,5 @@
 import { AssociativeBoth1 } from "../AssociativeBoth"
+import { AssociativeEither1 } from "../AssociativeEither"
 import { Contravariant1 } from "../Contravariant"
 import { IdentityBoth1 } from "../IdentityBoth"
 
@@ -22,6 +23,18 @@ export function make<A>(f: (x: A, y: A) => boolean): Equal<A> {
 export const AssociativeBoth: AssociativeBoth1<URI> = {
   URI,
   both: (fb) => (fa) => make(([x0, x1], [y0, y1]) => fa(x0, y0) && fb(x1, y1))
+}
+
+export const AssociativeEither: AssociativeEither1<URI> = {
+  URI,
+  either: (fb) => (fa) =>
+    make((ex, ey) =>
+      ex._tag === "Left" && ey._tag === "Left"
+        ? fa(ex.left, ey.left)
+        : ex._tag === "Right" && ey._tag === "Right"
+        ? fb(ex.right, ey.right)
+        : false
+    )
 }
 
 export const Contravariant: Contravariant1<URI> = {
