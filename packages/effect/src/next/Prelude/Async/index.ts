5d40437a7e925dc9a72da014a14af7e0df8c8153
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-06 18:07:18 +0100

    fix naming

--- packages/effect/src/next/Prelude/Async/index.ts
+++ packages/effect/src/next/Prelude/Async/index.ts
@@ -3,42 +3,38 @@ import { Applicative3 } from "../Applicative"
 import { Contravariant3 } from "../Contravariant"
 import { Covariant3 } from "../Covariant"
 
-export const ContravariantURI = "ContravariantAsync"
-export type ContravariantURI = typeof ContravariantURI
+export const AsyncEnvURI = "AsyncEnv"
+export type AsyncEnvURI = typeof AsyncEnvURI
 
-export const CovariantURI = "CovariantAsync"
-export type CovariantURI = typeof CovariantURI
-
-export const ApplicativeURI = "ApplicativeAsync"
-export type ApplicativeURI = typeof ApplicativeURI
+export const AsyncURI = "Async"
+export type AsyncURI = typeof AsyncURI
 
 declare module "../HKT" {
   interface URItoKind3<R, E, A> {
-    [ContravariantURI]: S.AsyncRE<A, E, R>
-    [CovariantURI]: S.AsyncRE<R, E, A>
-    [ApplicativeURI]: S.AsyncRE<R, E, A>
+    [AsyncEnvURI]: S.AsyncRE<A, E, R>
+    [AsyncURI]: S.AsyncRE<R, E, A>
   }
 }
 
-export const Contravariant: Contravariant3<ContravariantURI> = {
-  URI: ContravariantURI,
+export const Contravariant: Contravariant3<AsyncEnvURI> = {
+  URI: AsyncEnvURI,
   contramap: S.provideSome
 }
 
-export const Covariant: Covariant3<CovariantURI> = {
-  URI: CovariantURI,
+export const Covariant: Covariant3<AsyncURI> = {
+  URI: AsyncURI,
   map: S.map
 }
 
-export const Applicative: Applicative3<ApplicativeURI> = {
-  URI: ApplicativeURI,
+export const Applicative: Applicative3<AsyncURI> = {
+  URI: AsyncURI,
   any: () => S.of,
   both: S.zip,
   map: S.map
 }
 
-export const ApplicativePar: Applicative3<ApplicativeURI> = {
-  URI: ApplicativeURI,
+export const ApplicativePar: Applicative3<AsyncURI> = {
+  URI: AsyncURI,
   any: () => S.of,
   both: (fb) => (fa) => S.zipPar_(fa, fb),
   map: S.map
