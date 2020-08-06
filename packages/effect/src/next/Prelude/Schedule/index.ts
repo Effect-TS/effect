e43bd8440a717c3d0d927ee1ddb0c7bc2a2520f0
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-06 17:50:14 +0100

    schedule applicative

--- packages/effect/src/next/Prelude/Schedule/index.ts
+++ packages/effect/src/next/Prelude/Schedule/index.ts
@@ -1,4 +1,6 @@
+import { tuple } from "../../../Function"
 import * as S from "../../Schedule"
+import { Applicative4 } from "../Applicative"
 import { Contravariant4 } from "../Contravariant"
 import { Covariant4 } from "../Covariant"
 
@@ -8,10 +10,14 @@ export type ContravariantURI = typeof ContravariantURI
 export const CovariantURI = "CovariantSchedule"
 export type CovariantURI = typeof CovariantURI
 
+export const ApplicativeURI = "ApplicativeSchedule"
+export type ApplicativeURI = typeof ApplicativeURI
+
 declare module "../HKT" {
   interface URItoKind4<S, R, E, A> {
     [ContravariantURI]: S.Schedule<S, R, A, E>
     [CovariantURI]: S.Schedule<S, R, E, A>
+    [ApplicativeURI]: S.Schedule<S, R, E, A>
   }
 }
 
@@ -24,3 +30,12 @@ export const Covariant: Covariant4<CovariantURI> = {
   URI: CovariantURI,
   map: S.map
 }
+
+export const Applicative: Applicative4<ApplicativeURI> = {
+  URI: ApplicativeURI,
+  map: S.map,
+  any: () => S.succeed({}),
+  both: (fb) => (fa) => S.contramap_(S.both_(fa, fb), (e) => tuple(e, e))
+}
+
+export { succeed, tapOutput, run, delayed } from "../../Schedule"
