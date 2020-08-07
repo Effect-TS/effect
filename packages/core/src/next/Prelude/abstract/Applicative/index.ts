b9626e91ac4ce24998c61c3ab1b4fd7787679a28
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-07 23:00:25 +0100

    generic sequenceSF

--- packages/core/src/next/Prelude/abstract/Applicative/index.ts
+++ packages/core/src/next/Prelude/abstract/Applicative/index.ts
@@ -1,3 +1,7 @@
+import * as A from "../../../../Array"
+import { pipe, tuple } from "../../../../Function"
+import { UnionToIntersection } from "../../../Utils"
+import { succeedF } from "../Any"
 import {
   CovariantF,
   Covariant1,
@@ -7,7 +11,21 @@ import {
   Covariant5,
   Covariant6
 } from "../Covariant"
-import { URIS, URIS2, URIS3, URIS4, URIS5, URIS6 } from "../HKT"
+import {
+  HKT,
+  Kind,
+  Kind2,
+  Kind3,
+  Kind4,
+  Kind5,
+  Kind6,
+  URIS,
+  URIS2,
+  URIS3,
+  URIS4,
+  URIS5,
+  URIS6
+} from "../HKT"
 import {
   IdentityBothF,
   IdentityBoth1,
@@ -57,4 +75,196 @@ export function makeApplicative<URI>(
     URI,
     ..._
   })
+}
+
+type EnforceNonEmptyRecord<R> = keyof R extends never ? never : R
+
+export function sequenceSF<F extends URIS6>(
+  F: Applicative6<F>
+): <S, NER extends Record<string, Kind6<F, any, any, S, any, any, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => Kind6<
+  F,
+  {
+    [K in keyof NER]: [NER[K]] extends [
+      Kind6<F, infer X, infer In, infer S, infer S, infer E, infer A>
+    ]
+      ? X
+      : never
+  }[keyof NER],
+  UnionToIntersection<
+    {
+      [K in keyof NER]: [NER[K]] extends [
+        Kind6<F, infer X, infer In, infer S, infer R, infer E, infer A>
+      ]
+        ? unknown extends In
+          ? never
+          : In
+        : never
+    }[keyof NER]
+  >,
+  S,
+  UnionToIntersection<
+    {
+      [K in keyof NER]: [NER[K]] extends [
+        Kind6<F, infer X, infer In, infer S, infer R, infer E, infer A>
+      ]
+        ? unknown extends R
+          ? never
+          : R
+        : never
+    }[keyof NER]
+  >,
+  {
+    [K in keyof NER]: [NER[K]] extends [
+      Kind6<F, infer X, infer In, infer S, infer S, infer E, infer A>
+    ]
+      ? E
+      : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [
+      Kind6<F, infer X, infer In, infer S, infer S, infer E, infer A>
+    ]
+      ? A
+      : never
+  }
+>
+export function sequenceSF<F extends URIS5>(
+  F: Applicative5<F>
+): <S, NER extends Record<string, Kind5<F, any, S, any, any, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => Kind5<
+  F,
+  UnionToIntersection<
+    {
+      [K in keyof NER]: [NER[K]] extends [
+        Kind5<F, infer In, infer S, infer R, infer E, infer A>
+      ]
+        ? unknown extends In
+          ? never
+          : In
+        : never
+    }[keyof NER]
+  >,
+  S,
+  UnionToIntersection<
+    {
+      [K in keyof NER]: [NER[K]] extends [
+        Kind5<F, infer In, infer S, infer R, infer E, infer A>
+      ]
+        ? unknown extends R
+          ? never
+          : R
+        : never
+    }[keyof NER]
+  >,
+  {
+    [K in keyof NER]: [NER[K]] extends [
+      Kind5<F, infer In, infer S, infer S, infer E, infer A>
+    ]
+      ? E
+      : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [
+      Kind5<F, infer In, infer S, infer S, infer E, infer A>
+    ]
+      ? A
+      : never
+  }
+>
+export function sequenceSF<F extends URIS4>(
+  F: Applicative4<F>
+): <S, NER extends Record<string, Kind4<F, S, any, any, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => Kind4<
+  F,
+  S,
+  UnionToIntersection<
+    {
+      [K in keyof NER]: [NER[K]] extends [Kind4<F, infer S, infer R, infer E, infer A>]
+        ? unknown extends R
+          ? never
+          : R
+        : never
+    }[keyof NER]
+  >,
+  {
+    [K in keyof NER]: [NER[K]] extends [Kind4<F, infer S, infer S, infer E, infer A>]
+      ? E
+      : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [Kind4<F, infer S, infer S, infer E, infer A>]
+      ? A
+      : never
+  }
+>
+export function sequenceSF<F extends URIS3>(
+  F: Applicative3<F>
+): <NER extends Record<string, Kind3<F, any, any, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => Kind3<
+  F,
+  UnionToIntersection<
+    {
+      [K in keyof NER]: [NER[K]] extends [Kind3<F, infer R, infer E, infer A>]
+        ? unknown extends R
+          ? never
+          : R
+        : never
+    }[keyof NER]
+  >,
+  {
+    [K in keyof NER]: [NER[K]] extends [Kind3<F, infer S, infer E, infer A>] ? E : never
+  }[keyof NER],
+  {
+    [K in keyof NER]: [NER[K]] extends [Kind3<F, infer S, infer E, infer A>] ? A : never
+  }
+>
+export function sequenceSF<F extends URIS2>(
+  F: Applicative2<F>
+): <NER extends Record<string, Kind2<F, any, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => Kind2<
+  F,
+  {
+    [K in keyof NER]: [NER[K]] extends [Kind2<F, infer E, infer A>] ? E : never
+  }[keyof NER],
+  { [K in keyof NER]: [NER[K]] extends [Kind2<F, infer E, infer A>] ? A : never }
+>
+export function sequenceSF<F extends URIS>(
+  F: Applicative1<F>
+): <NER extends Record<string, Kind<F, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => Kind<F, { [K in keyof NER]: [NER[K]] extends [Kind<F, infer A>] ? A : never }>
+export function sequenceSF<F>(
+  F: ApplicativeF<F>
+): <NER extends Record<string, HKT<F, any>>>(
+  r: EnforceNonEmptyRecord<NER>
+) => HKT<F, { [K in keyof NER]: [NER[K]] extends [HKT<F, infer A>] ? A : never }>
+export function sequenceSF<F>(
+  F: ApplicativeF<F>
+): (r: Record<string, HKT<F, any>>) => HKT<F, Record<string, any>> {
+  return (r) =>
+    pipe(
+      A.reduce_(
+        Object.keys(r).map((k) => tuple(k, r[k])),
+        succeedF(F)([] as readonly [string, any][]),
+        (b, a) =>
+          pipe(
+            b,
+            F.both(a[1]),
+            F.map(([x, y]) => [...x, tuple(a[0], y)])
+          )
+      ),
+      F.map((a) => {
+        const res = {}
+        a.forEach(([k, v]) => {
+          res[k] = v
+        })
+        return res
+      })
+    )
 }
