6f3eac771266b79da48d3f70cf1d0991ffe7600e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-07 10:42:44 +0100

    represent compositions are intersections

--- packages/core/src/next/Prelude/abstract/IdentityEither/index.ts
+++ packages/core/src/next/Prelude/abstract/IdentityEither/index.ts
@@ -14,28 +14,16 @@ import { None, None1, None2, None3, None4, None5, None6 } from "../None"
  * A binary operator that combines two values of types `F[A]` and `F[B]` to
  * produce an `F[Either[A, B]]` with an identity value.
  */
-export interface IdentityEither<F> extends AssociativeEither<F>, None<F> {}
+export type IdentityEither<F> = AssociativeEither<F> & None<F>
 
-export interface IdentityEither1<F extends URIS>
-  extends AssociativeEither1<F>,
-    None1<F> {}
+export type IdentityEither1<F extends URIS> = AssociativeEither1<F> & None1<F>
 
-export interface IdentityEither2<F extends URIS2>
-  extends AssociativeEither2<F>,
-    None2<F> {}
+export type IdentityEither2<F extends URIS2> = AssociativeEither2<F> & None2<F>
 
-export interface IdentityEither3<F extends URIS3>
-  extends AssociativeEither3<F>,
-    None3<F> {}
+export type IdentityEither3<F extends URIS3> = AssociativeEither3<F> & None3<F>
 
-export interface IdentityEither4<F extends URIS4>
-  extends AssociativeEither4<F>,
-    None4<F> {}
+export type IdentityEither4<F extends URIS4> = AssociativeEither4<F> & None4<F>
 
-export interface IdentityEither5<F extends URIS5>
-  extends AssociativeEither5<F>,
-    None5<F> {}
+export type IdentityEither5<F extends URIS5> = AssociativeEither5<F> & None5<F>
 
-export interface IdentityEither6<F extends URIS6>
-  extends AssociativeEither6<F>,
-    None6<F> {}
+export type IdentityEither6<F extends URIS6> = AssociativeEither6<F> & None6<F>
