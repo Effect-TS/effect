4fe10cc6090e6009cc8df548cbf6a1f884710051
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-07 10:42:44 +0100

    represent compositions are intersections

--- src/next/Prelude/abstract/IdentityFlatten/index.ts
+++ src/next/Prelude/abstract/IdentityFlatten/index.ts
@@ -14,28 +14,16 @@ import { URIS, URIS2, URIS3, URIS4, URIS5, URIS6 } from "../HKT"
  * A binary operator that combines two values of types `F[A]` and `F[B]` to
  * produce an `F[(A, B)]` with an identity.
  */
-export interface IdentityFlatten<F> extends AssociativeFlatten<F>, Any<F> {}
+export type IdentityFlatten<F> = AssociativeFlatten<F> & Any<F>
 
-export interface IdentityFlatten1<F extends URIS>
-  extends AssociativeFlatten1<F>,
-    Any1<F> {}
+export type IdentityFlatten1<F extends URIS> = AssociativeFlatten1<F> & Any1<F>
 
-export interface IdentityFlatten2<F extends URIS2>
-  extends AssociativeFlatten2<F>,
-    Any2<F> {}
+export type IdentityFlatten2<F extends URIS2> = AssociativeFlatten2<F> & Any2<F>
 
-export interface IdentityFlatten3<F extends URIS3>
-  extends AssociativeFlatten3<F>,
-    Any3<F> {}
+export type IdentityFlatten3<F extends URIS3> = AssociativeFlatten3<F> & Any3<F>
 
-export interface IdentityFlatten4<F extends URIS4>
-  extends AssociativeFlatten4<F>,
-    Any4<F> {}
+export type IdentityFlatten4<F extends URIS4> = AssociativeFlatten4<F> & Any4<F>
 
-export interface IdentityFlatten5<F extends URIS5>
-  extends AssociativeFlatten5<F>,
-    Any5<F> {}
+export type IdentityFlatten5<F extends URIS5> = AssociativeFlatten5<F> & Any5<F>
 
-export interface IdentityFlatten6<F extends URIS6>
-  extends AssociativeFlatten6<F>,
-    Any6<F> {}
+export type IdentityFlatten6<F extends URIS6> = AssociativeFlatten6<F> & Any6<F>
