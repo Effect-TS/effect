283794a2a19af75b2369c4a8bbe0c3abdb835786
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-08 20:38:44 +0100

    generic do

--- packages/core/src/next/Prelude/abstract/Monad/index.ts
+++ packages/core/src/next/Prelude/abstract/Monad/index.ts
@@ -1,5 +1,6 @@
+import { pipe } from "../../../../Function"
 import { CovariantK, CovariantF } from "../Covariant"
-import { URIS } from "../HKT"
+import { HKT, HKT6, Kind, URIS } from "../HKT"
 import { IdentityFlattenK, IdentityFlattenF } from "../IdentityFlatten"
 
 export type MonadF<F> = IdentityFlattenF<F> & CovariantF<F>
@@ -14,4 +15,81 @@ export function makeMonad<URI>(URI: URI): (_: Omit<MonadF<URI>, "URI">) => Monad
     URI,
     ..._
   })
+}
+
+export function doF<F extends URIS>(
+  F: MonadK<F>
+): <In, S = In>() => Kind<F, never, In, S, unknown, never, {}>
+export function doF<F>(
+  F: MonadF<F>
+): <In, S = In>() => HKT6<F, never, In, S, unknown, never, {}>
+export function doF<F>(F: MonadF<F>): () => HKT<F, {}> {
+  return () =>
+    pipe(
+      F.any(),
+      F.map(() => ({}))
+    )
+}
+
+export function bindF<F extends URIS>(
+  F: MonadK<F>
+): <X, I, S, R, E, A, K, N extends string>(
+  tag: Exclude<N, keyof K>,
+  f: (_: K) => Kind<F, X, I, S, R, E, A>
+) => <X2, I2, R2, E2>(
+  mk: Kind<F, X2, I2, S, R2, E2, K>
+) => Kind<F, X | X2, I & I2, S, R & R2, E | E2, K & { [k in N]: A }>
+export function bindF<F>(
+  F: MonadF<F>
+): <X, I, S, R, E, A, K, N extends string>(
+  tag: Exclude<N, keyof K>,
+  f: (_: K) => HKT6<F, X, I, S, R, E, A>
+) => <X2, I2, R2, E2>(
+  mk: HKT6<F, X2, I2, S, R2, E2, K>
+) => HKT6<F, X | X2, I & I2, S, R & R2, E | E2, K & { [k in N]: A }>
+export function bindF<F>(F: MonadF<F>) {
+  return <X, I, S, R, E, A, K, N extends string>(
+    tag: Exclude<N, keyof K>,
+    f: (_: K) => HKT6<F, X, I, S, R, E, A>
+  ) => <X2, I2, R2, E2>(
+    mk: HKT6<F, X2, I2, S, R2, E2, K>
+  ): HKT6<F, X | X2, I & I2, S, R & R2, E | E2, K & { [k in N]: A }> =>
+    pipe(
+      mk,
+      chainF(F)((k) =>
+        pipe(
+          f(k),
+          F.map((a) =>
+            Object.assign({}, k, { [tag]: a } as {
+              [k in N]: A
+            })
+          )
+        )
+      )
+    )
+}
+
+export function chainF<F extends URIS>(
+  F: MonadK<F>
+): <X, I, S, R, E, A, B>(
+  f: (_: A) => HKT6<F, X, I, S, R, E, B>
+) => <X2, I2, R2, E2>(
+  mk: Kind<F, X2, I2, S, R2, E2, A>
+) => Kind<F, X2 | X, I & I2, S, R & R2, E2 | E, B>
+export function chainF<F>(
+  F: MonadF<F>
+): <X, I, S, R, E, A, B>(
+  f: (_: A) => HKT6<F, X, I, S, R, E, B>
+) => <X2, I2, R2, E2>(
+  mk: HKT6<F, X2, I2, S, R2, E2, A>
+) => HKT6<F, X2 | X, I & I2, S, R & R2, E2 | E, B>
+export function chainF<F>(F: MonadF<F>) {
+  return <X, I, S, R, E, A, B>(f: (_: A) => HKT6<F, X, I, S, R, E, B>) => <
+    X2,
+    I2,
+    R2,
+    E2
+  >(
+    mk: HKT6<F, X2, I2, S, R2, E2, A>
+  ): HKT6<F, X | X2, I & I2, S, R & R2, E | E2, B> => pipe(mk, F.map(f), F.flatten)
 }
