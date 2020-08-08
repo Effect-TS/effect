d8c465922d0761d49a8a830f3e3d3a88860c94c3
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-08 11:00:17 +0100

    fix tapCause

--- src/next/Effect/tapCause.ts
+++ src/next/Effect/tapCause.ts
@@ -1,7 +1,7 @@
 import { Cause } from "../Cause/cause"
 
-import { foldCauseM_, succeed } from "./core"
 import { Effect } from "./effect"
+import { tapCause_ } from "./tapCause_"
 
 /**
  * Returns an effect that effectually "peeks" at the cause of the failure of
@@ -13,4 +13,4 @@ export const tapCause = <S, R, E, E2>(f: (e: Cause<E2>) => Effect<S, R, E, any>)
   A2
 >(
   effect: Effect<S2, R2, E2, A2>
-) => foldCauseM_(effect, f, succeed)
+) => tapCause_(effect, f)
