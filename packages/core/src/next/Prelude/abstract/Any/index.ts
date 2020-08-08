a97a12ca8d71ab0e896dfe2ad6a8713999f110ae
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-08 17:24:29 +0100

    implementation utility

--- packages/core/src/next/Prelude/abstract/Any/index.ts
+++ packages/core/src/next/Prelude/abstract/Any/index.ts
@@ -7,20 +7,40 @@ import { HasURI, HKT, HKT6, Kind, URIS } from "../HKT"
  */
 export interface AnyF<F> extends HasURI<F> {
   readonly Any: "Any"
-  readonly any: <In, S = In>() => HKT6<F, never, In, S, unknown, never, unknown>
+  readonly any: <In, S = In, X = never, R = unknown, E = never>() => HKT6<
+    F,
+    X,
+    In,
+    S,
+    R,
+    E,
+    unknown
+  >
 }
 
 export interface AnyK<F extends URIS> extends HasURI<F> {
   readonly Any: "Any"
-  readonly any: <In, S = In>() => Kind<F, never, In, S, unknown, never, unknown>
+  readonly any: <In, S = In, X = never, R = unknown, E = never>() => Kind<
+    F,
+    X,
+    In,
+    S,
+    R,
+    E,
+    unknown
+  >
 }
 
 export function succeedF<F extends URIS>(
   F: AnyK<F> & CovariantK<F>
-): <A, In, S = In>(a: A) => Kind<F, never, In, S, unknown, never, A>
+): <A, In, S = In, X = never, R = unknown, E = never>(
+  a: A
+) => Kind<F, X, In, S, R, E, A>
 export function succeedF<F>(
   F: AnyF<F> & CovariantF<F>
-): <A, In, S = In>(a: A) => HKT6<F, never, In, S, unknown, never, A>
+): <A, In, S = In, X = never, R = unknown, E = never>(
+  a: A
+) => HKT6<F, X, In, S, R, E, A>
 export function succeedF<F>(F: AnyF<F> & CovariantF<F>): <A>(a: A) => HKT<F, A> {
   return (a) =>
     pipe(
