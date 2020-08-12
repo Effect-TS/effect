636202691eaffa6595d0b88bd18a9fb6f90bd448
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-12 16:33:53 +0100

    improve demo

--- packages/preview/demo/demo20.ts
+++ packages/preview/demo/demo20.ts
@@ -1,18 +1,34 @@
 import * as A from "../src/Array"
+import * as T from "../src/EffectAsync"
 import * as E from "../src/Either"
 import { pipe } from "../src/Function"
-import * as R from "../src/Reader"
+import { nextRange } from "../src/Random"
+import { makeSemaphore, withPermit } from "../src/Semaphore"
 
 pipe(
-  10,
-  A.makeBy((n) => `item: ${n}`),
-  A.separateWithKeysF(R.Applicative)((s, k) =>
-    R.access((r: number) =>
-      k > 5 ? E.left(`s: {${s}} l: ${k + r}`) : E.right(`s: {${s}} r: ${k - r}`)
+  makeSemaphore(3),
+  T.chain((r) =>
+    pipe(
+      10,
+      A.makeBy((n) => `item: ${n}`),
+      A.separateWithKeysF(T.ApplicativePar)((s, k) =>
+        pipe(
+          nextRange(2000, 5000),
+          T.chain((d) =>
+            pipe(
+              T.access((r: { n: number }) =>
+                k > 5
+                  ? E.left(`s: {${s}} l: ${k + r.n}`)
+                  : E.right(`s: {${s}} r: ${k - r.n}`)
+              ),
+              T.delay(d),
+              withPermit(r)
+            )
+          )
+        )
+      ),
+      T.provide({ n: 100 })
     )
   ),
-  R.runEnv(100),
-  (x) => {
-    console.log(x)
-  }
-)
+  T.runPromise
+).then(console.log)
