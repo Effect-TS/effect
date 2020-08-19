b2978bb60a4ce219d66cd4a92f09c930b7fb6cfa
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-19 16:58:28 +0100

    add witherable with index

---
+++ packages/core/src/Prelude/WitherableWithIndex/index.ts
@@ -0,0 +1,146 @@
+import { Applicative } from "../Combined"
+import {
+  Auto,
+  Base,
+  G_,
+  IndexFor,
+  Kind,
+  OrE,
+  OrI,
+  OrK,
+  OrN,
+  OrR,
+  OrS,
+  OrX,
+  UG_,
+  URIS
+} from "../HKT"
+
+import { Option } from "@effect-ts/system/Option"
+
+export interface WitherWithIndex<F extends URIS, C = Auto> {
+  <G extends URIS, GC = Auto>(F: Applicative<G, GC>): <
+    GN extends string,
+    GK,
+    GSIO,
+    GX,
+    GI,
+    GS,
+    GR,
+    GE,
+    A,
+    B,
+    FN extends string,
+    FK
+  >(
+    f: (
+      k: IndexFor<F, OrN<C, FN>, OrK<C, FK>>,
+      a: A
+    ) => Kind<
+      G,
+      OrN<GC, GN>,
+      OrK<GC, GK>,
+      GSIO,
+      GSIO,
+      OrX<GC, GX>,
+      OrI<GC, GI>,
+      OrS<GC, GS>,
+      OrR<GC, GR>,
+      OrE<GC, GE>,
+      Option<B>
+    >
+  ) => <FSI, FSO, FX, FI, FS, FR, FE>(
+    ta: Kind<
+      F,
+      OrN<C, FN>,
+      OrK<C, FK>,
+      FSI,
+      FSO,
+      OrX<C, FX>,
+      OrI<C, FI>,
+      OrS<C, FS>,
+      OrR<C, FR>,
+      OrE<C, FE>,
+      A
+    >
+  ) => Kind<
+    G,
+    OrN<GC, GN>,
+    OrK<GC, GK>,
+    GSIO,
+    GSIO,
+    OrX<GC, GX>,
+    OrI<GC, GI>,
+    OrS<GC, GS>,
+    OrR<GC, GR>,
+    OrE<GC, GE>,
+    Kind<
+      F,
+      OrN<C, FN>,
+      OrK<C, FK>,
+      FSI,
+      FSO,
+      OrX<C, FX>,
+      OrI<C, FI>,
+      OrS<C, FS>,
+      OrR<C, FR>,
+      OrE<C, FE>,
+      B
+    >
+  >
+}
+
+export interface WitherableWithIndex<F extends URIS, C = Auto> extends Base<F> {
+  readonly compactWithIndexF: WitherWithIndex<F, C>
+}
+
+export function implementCompactWithIndexF<F extends URIS, C = Auto>(): (
+  i: <FN extends string, FK, FSI, FSO, FX, FI, FS, FR, FE, A, B>(_: {
+    A: A
+    B: B
+    FN: FN
+    FK: FK
+    FSI: FSI
+    FSO: FSO
+    FX: FX
+    FI: FI
+    FS: FS
+    FR: FR
+    FE: FE
+  }) => (
+    G: Applicative<UG_>
+  ) => (
+    f: (k: IndexFor<F, OrN<C, FN>, OrK<C, FK>>, a: A) => G_<Option<B>>
+  ) => (
+    ta: Kind<
+      F,
+      OrN<C, FN>,
+      OrK<C, FK>,
+      FSI,
+      FSO,
+      OrX<C, FX>,
+      OrI<C, FI>,
+      OrS<C, FS>,
+      OrR<C, FR>,
+      OrE<C, FE>,
+      A
+    >
+  ) => G_<
+    Kind<
+      F,
+      OrN<C, FN>,
+      OrK<C, FK>,
+      FSI,
+      FSO,
+      OrX<C, FX>,
+      OrI<C, FI>,
+      OrS<C, FS>,
+      OrR<C, FR>,
+      OrE<C, FE>,
+      B
+    >
+  >
+) => WitherWithIndex<F, C>
+export function implementCompactWithIndexF() {
+  return (i: any) => i()
+}
