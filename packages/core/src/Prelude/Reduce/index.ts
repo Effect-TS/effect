65e39a65a604acda9dbe767bba2a2edbf84e4e46
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-19 13:20:52 +0100

    rename key to index

--- packages/core/src/Prelude/Reduce/index.ts
+++ packages/core/src/Prelude/Reduce/index.ts
@@ -1,9 +1,5 @@
 import { URIS, Auto, Base, Kind, OrN, OrK, OrX, OrI, OrS, OrR, OrE } from "../HKT"
 
-/**
- * An associative binary operator that combines two values of types `F[A]`
- * and `F[B]` to produce an `F[(A, B)]`.
- */
 export interface Reduce<F extends URIS, C = Auto> extends Base<F> {
   readonly reduce: <A, B>(
     b: B,
