4717df3f1cb24599c84f8ec08e93f1e30c73f18e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-19 14:46:58 +0100

    add inverse & ord

---
+++ packages/core/src/Classic/Ordering/index.ts
@@ -0,0 +1,23 @@
+import { pipe } from "../../Function"
+import { typeDef, TypeOf, newtype } from "../../Newtype"
+
+const Ordering_ = typeDef<"lt" | "eq" | "gt">()("@newtype/Ordering")
+
+export interface Ordering extends TypeOf<typeof Ordering_> {}
+
+export const Ordering = newtype<Ordering>()(Ordering_)
+
+export const toNumber = (o: Ordering) =>
+  pipe(Ordering.unwrap(o), (o) => {
+    switch (o) {
+      case "eq": {
+        return 0
+      }
+      case "gt": {
+        return 1
+      }
+      case "lt": {
+        return -1
+      }
+    }
+  })
