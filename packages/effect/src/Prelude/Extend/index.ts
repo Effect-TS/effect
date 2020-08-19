19a4ab869e60c0b37505fe793da309d3328c7928
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-19 22:28:18 +0100

    add extend

---
+++ packages/effect/src/Prelude/Extend/index.ts
@@ -0,0 +1,47 @@
+import { Auto, Base, Kind, OrE, OrI, OrK, OrN, OrR, OrS, OrX, URIS } from "../HKT"
+
+export interface Extend<F extends URIS, C = Auto> extends Base<F> {
+  readonly extend: <N extends string, K, SI, SO, X, I, S, R, E, A, B>(
+    f: (
+      _: Kind<
+        F,
+        OrN<C, N>,
+        OrK<C, K>,
+        SI,
+        SO,
+        OrX<C, X>,
+        OrI<C, I>,
+        OrS<C, S>,
+        OrR<C, R>,
+        OrE<C, E>,
+        A
+      >
+    ) => B
+  ) => (
+    fa: Kind<
+      F,
+      OrN<C, N>,
+      OrK<C, K>,
+      SI,
+      SO,
+      OrX<C, X>,
+      OrI<C, I>,
+      OrS<C, S>,
+      OrR<C, R>,
+      OrE<C, E>,
+      A
+    >
+  ) => Kind<
+    F,
+    OrN<C, N>,
+    OrK<C, K>,
+    SI,
+    SO,
+    OrX<C, X>,
+    OrI<C, I>,
+    OrS<C, S>,
+    OrR<C, R>,
+    OrE<C, E>,
+    B
+  >
+}
