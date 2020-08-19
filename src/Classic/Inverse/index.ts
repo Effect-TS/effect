2929ea2a64fecb52361e4f9d08aeb3146055aef7
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-19 14:46:58 +0100

    add inverse & ord

---
+++ src/Classic/Inverse/index.ts
@@ -0,0 +1,31 @@
+import { Identity } from "../Identity"
+
+export interface Inverse<A> extends Identity<A> {
+  /**
+   * Returns a right inverse for the given `A` value, such that when
+   * the value is combined with the inverse (on the right hand side),
+   * the identity element is returned.
+   */
+  readonly inverse: (r: A) => (l: A) => A
+}
+
+export const InverseURI = "Inverse"
+export type InverseURI = typeof InverseURI
+
+declare module "../../Prelude/HKT" {
+  interface URItoKind<N extends string, K, SI, SO, X, I, S, R, E, A> {
+    [InverseURI]: Inverse<A>
+  }
+}
+
+export function makeInverse<A>(
+  identity: A,
+  combine: (y: A) => (x: A) => A,
+  inverse: (r: A) => (l: A) => A
+): Inverse<A> {
+  return {
+    combine,
+    identity,
+    inverse
+  }
+}
