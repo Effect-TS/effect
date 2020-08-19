9f4503da1441150325a8d1781be0196071702c74
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-19 16:10:04 +0100

    improve demo

--- demo/06.ts
+++ demo/06.ts
@@ -4,14 +4,14 @@ import * as R from "../src/Classic/Record"
 import { pipe } from "../src/Function"
 
 const ValidationApplicative = E.getValidationApplicative(
-  makeAssociative<string>((r) => (l) => `(${l})(${r})`)
+  makeAssociative<string>((r) => (l) => `${l} | ${r}`)
 )
 
 const traverse = R.foreachWithIndexF(ValidationApplicative)
 
 const result = pipe(
-  { a: 0, b: 1, c: 2 },
-  traverse((k, n) => (n > 3 && k !== "c" ? E.left("bad") : E.right("good")))
+  { a: 0, b: 1, c: 2, d: 4, e: 5, g: 6 },
+  traverse((k, n) => (n > 3 && k !== "c" ? E.left(`bad: ${k}`) : E.right("good")))
 )
 
 console.log(result)
