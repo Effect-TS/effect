e59198edca25ea7d4c1aedec29bac925e9c27e68
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-20 21:01:02 +0100

    demo custom params

---
+++ demo/11.ts
@@ -0,0 +1,33 @@
+import type * as M from "@effect-ts/system/Map"
+
+import { identity } from "../src/Function"
+import * as P from "../src/Prelude"
+import type * as H from "../src/Prelude/HKT"
+import * as T from "../src/Pure"
+
+type State<K, V> = M.Map<K, V>
+
+export interface Store<K, V, A>
+  extends T.XPure<State<K, V>, State<K, V>, unknown, never, A> {}
+
+export const URI = "Store"
+export type URI = typeof URI
+
+type SK = "SK"
+type SV = "SV"
+type Params<K, V> = H.CT<SK, K> & H.CT<SV, V>
+
+declare module "../src/Prelude/HKT" {
+  export interface URItoKind<D, N extends string, K, SI, SO, X, I, S, R, E, A> {
+    [URI]: Store<H.Custom<D, SK>, H.Custom<D, SV>, A>
+  }
+}
+
+export const getMonad = <K, V>() =>
+  P.instance<P.Monad<[URI], Params<K, V>>>({
+    any: T.Any.any,
+    flatten: (ffa) => T.chain_(ffa, identity),
+    map: T.map
+  })
+
+export const K = getMonad<string, number>()
