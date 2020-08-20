733eb8daed67a33d425e21f95fbad05ebdf7e27f
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-20 20:05:21 +0100

    variadic

--- src/Classic/EitherT/index.ts
+++ src/Classic/EitherT/index.ts
@@ -3,12 +3,12 @@ import type { Erase } from "@effect-ts/system/Utils"
 import { identity, pipe } from "../../Function"
 import type { Monad } from "../../Prelude"
 import { succeedF } from "../../Prelude/DSL"
-import type { RestrictedKindURI, UnionURI, V } from "../../Prelude/HKT"
+import type { UnionURI, V } from "../../Prelude/HKT"
 import { instance } from "../../Prelude/HKT"
-import type { Auto, F_, UF_ } from "../../Prelude/HKT/hkt"
+import type { Auto, F_, UF_, URIS } from "../../Prelude/HKT/hkt"
 import * as E from "../Either"
 
-export function getEitherM<F extends RestrictedKindURI, C>(
+export function getEitherM<F extends URIS, C>(
   M: Monad<F, C>
 ): Monad<UnionURI<E.EitherURI, F>, Erase<C, Auto> & V<"E", "+">>
 export function getEitherM(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI]> {
