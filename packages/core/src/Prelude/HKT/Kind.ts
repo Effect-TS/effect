870acf598d032e9bb138f3ed19508cf32adbd5bc
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-20 18:58:04 +0100

    up to 5 layers

--- packages/core/src/Prelude/HKT/Kind.ts
+++ packages/core/src/Prelude/HKT/Kind.ts
@@ -1,13 +1,16 @@
 import type { BaseURIS, URItoKind } from "./hkt"
 
-export type URIS = [BaseURIS, BaseURIS?, BaseURIS?, BaseURIS?]
-export type RestrictedKindURI = [BaseURIS, BaseURIS?, BaseURIS?]
+export type URIS = [BaseURIS, BaseURIS?, BaseURIS?, BaseURIS?, BaseURIS?]
+export type RestrictedKindURI = [BaseURIS, BaseURIS?, BaseURIS?, BaseURIS?]
 
 export type UnionURI<G extends BaseURIS, F extends RestrictedKindURI> = F extends [
   BaseURIS,
   BaseURIS,
+  BaseURIS,
   BaseURIS
 ]
+  ? [F[0], F[1], F[2], F[3], G]
+  : F extends [BaseURIS, BaseURIS, BaseURIS]
   ? [F[0], F[1], F[2], G]
   : F extends [BaseURIS, BaseURIS]
   ? [F[0], F[1], G]
@@ -27,7 +30,53 @@ export type Kind<
   R,
   E,
   A
-> = URI extends [BaseURIS, BaseURIS, BaseURIS, BaseURIS]
+> = URI extends [BaseURIS, BaseURIS, BaseURIS, BaseURIS, BaseURIS]
+  ? URItoKind<
+      N,
+      K,
+      SI,
+      SO,
+      X,
+      I,
+      S,
+      R,
+      E,
+      URItoKind<
+        N,
+        K,
+        SI,
+        SO,
+        X,
+        I,
+        S,
+        R,
+        E,
+        URItoKind<
+          N,
+          K,
+          SI,
+          SO,
+          X,
+          I,
+          S,
+          R,
+          E,
+          URItoKind<
+            N,
+            K,
+            SI,
+            SO,
+            X,
+            I,
+            S,
+            R,
+            E,
+            URItoKind<N, K, SI, SO, X, I, S, R, E, A>[URI[4]]
+          >[URI[3]]
+        >[URI[2]]
+      >[URI[1]]
+    >[URI[0]]
+  : URI extends [BaseURIS, BaseURIS, BaseURIS, BaseURIS]
   ? URItoKind<
       N,
       K,
