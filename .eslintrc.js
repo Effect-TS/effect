db78058c1753b6be7f598c856f62bc8a14b9d572
Nikita Volodin <volodin.n@gmail.com>
2020-08-20 09:48:33 -0400

    sort eslint rules alphabetically

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,28 +25,31 @@ module.exports = {
   ],
   plugins: ["import", "sort-destructure-keys", "simple-import-sort"],
   rules: {
+    // eslint built-in rules, sorted alphabetically
+    "no-fallthrough": "off",
+    "no-irregular-whitespace": "off",
+    "object-shorthand": "error",
+    "prefer-destructuring": "off",
+    "sort-imports": "off",
+
+    // all other rules, sorted alphabetically
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/camelcase": "off",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "sort-destructure-keys/sort-destructure-keys": "error",
-    "simple-import-sort/sort": "error",
-    "import/no-unresolved": "error",
-    "no-irregular-whitespace": "off",
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
+    "import/no-unresolved": "error",
     "import/order": "off",
-    "sort-imports": "off",
-    "object-shorthand": "error",
-    "prefer-destructuring": "off",
-    "no-fallthrough": "off"
+    "simple-import-sort/sort": "error",
+    "sort-destructure-keys/sort-destructure-keys": "error"
   }
 }
