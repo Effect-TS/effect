50b8d115848b8a86fa15293b293ecd58bca4a277
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-21 00:49:50 +0100

    allow for custom index in eitherT

--- packages/effect/src/Classic/EitherT/index.ts
+++ packages/effect/src/Classic/EitherT/index.ts
@@ -6,12 +6,17 @@ import { succeedF } from "../../Prelude/DSL"
 import type { UnionURI, V } from "../../Prelude/HKT"
 import { instance } from "../../Prelude/HKT"
 import type { Auto, F_, UF_, URIS } from "../../Prelude/HKT/hkt"
+import type { IndexedURI } from "../../Prelude/HKT/Kind"
+import type { Par } from "../../Prelude/HKT/variance"
 import * as E from "../Either"
 
-export function getEitherM<F extends URIS, C>(
+function getEitherM_<F extends URIS, P extends Par, C>(
   M: Monad<F, C>
-): Monad<UnionURI<E.EitherURI, F>, Erase<C, Auto> & V<"E", "+">>
-export function getEitherM(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI]> {
+): Monad<
+  UnionURI<P extends "E" ? E.EitherURI : IndexedURI<E.EitherURI, "E", P>, F>,
+  Erase<C, Auto> & V<P, "+">
+>
+function getEitherM_(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI]> {
   return instance({
     any: () => succeedF(M)(E.right({})),
     flatten: (ffa) =>
@@ -19,4 +24,8 @@ export function getEitherM(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI]> {
     map: <A, B>(f: (a: A) => B) => <E>(fa: F_<E.Either<E, A>>): F_<E.Either<E, B>> =>
       pipe(fa, M.map(E.map(f)))
   })
+}
+
+export function getEitherM<P extends Par = "E">(_?: P) {
+  return <F extends URIS, C>(M: Monad<F, C>) => getEitherM_<F, P, C>(M)
 }
