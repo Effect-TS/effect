9247fe67c729da5ac8466ceff3cad4055fe29510
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-21 22:34:50 +0100

    fail returns never on A

--- packages/effect/src/Prelude/FX/Fail/index.ts
+++ packages/effect/src/Prelude/FX/Fail/index.ts
@@ -8,9 +8,8 @@ export interface Fail<F extends HKT.URIS, C = HKT.Auto> extends HKT.Base<F, C> {
     I = HKT.INIT<F, C, "I">,
     S = HKT.INIT<F, C, "S">,
     R = HKT.INIT<F, C, "R">,
-    E = HKT.INIT<F, C, "E">,
-    A = never
+    E = HKT.INIT<F, C, "E">
   >(
     e: HKT.AccessType<F, C, "E", X, I, S, R, E>
-  ) => HKT.KindFix<F, C, never, never, SI, SO, X, I, S, R, E, A>
+  ) => HKT.KindFix<F, C, never, never, SI, SO, X, I, S, R, E, never>
 }
