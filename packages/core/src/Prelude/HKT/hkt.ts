534b816c7a8a7a3780200c10f5d33d0804c7d385
Nikita Volodin <volodin.n@gmail.com>
2020-08-21 00:45:55 -0400

    move base interfaces into separate file

--- packages/core/src/Prelude/HKT/hkt.ts
+++ packages/core/src/Prelude/HKT/hkt.ts
@@ -77,19 +77,3 @@ export type URISL0 = keyof URItoKind<
   any,
   any
 >
-
-export interface Auto {
-  readonly Auto: unique symbol
-}
-
-export interface Base<F, C = Auto> {
-  F: F
-  C: C
-}
-
-export interface CompositionBase2<F, G, CF = Auto, CG = Auto> {
-  F: F
-  G: G
-  CF: CF
-  CG: CG
-}
