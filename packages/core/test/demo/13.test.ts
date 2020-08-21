2a9d73c97024d51a2c12f015856707b701885a48
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-21 11:20:04 +0100

    add invariant transformer

---
+++ packages/core/test/demo/13.test.ts
@@ -0,0 +1,22 @@
+import { pipe } from "@effect-ts/system/Function"
+
+import * as A from "../../src/Classic/Array"
+import * as E from "../../src/Classic/Either"
+import * as EitherT from "../../src/Classic/EitherT"
+import * as InvariantT from "../../src/Classic/InvariantT"
+import { chainF } from "../../src/Prelude/DSL"
+import * as R from "../../src/Pure/Reader"
+import * as ReaderT from "../../src/Pure/ReaderT"
+
+test("10", () => {
+  const M = pipe(A.Monad, EitherT.monad(), ReaderT.monad(), InvariantT.monad("E"))
+
+  pipe(
+    R.access((k: number) => A.range(0, 10).map((n) => E.right(n + k))),
+    chainF(M)((n) => R.succeed([E.right(n + 1)])),
+    R.runEnv(10),
+    (x) => {
+      console.log(x)
+    }
+  )
+})
