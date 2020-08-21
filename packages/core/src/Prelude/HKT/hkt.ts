edcf83b779e5e577bd278a19876a4607a110735e
Nikita Volodin <volodin.n@gmail.com>
2020-08-21 00:41:12 -0400

    reorder hkt

--- packages/core/src/Prelude/HKT/hkt.ts
+++ packages/core/src/Prelude/HKT/hkt.ts
@@ -1,22 +1,20 @@
+export const UF_ = "F_"
+export type UF_ = typeof UF_
 export interface F_<A> {
   URI: UF_
   A: A
 }
 
-export const UF_ = "F_"
-
-export type UF_ = typeof UF_
-
+export const UF__ = "F__"
+export type UF__ = typeof UF__
 export interface F__<E, A> {
   URI: UF__
   E: () => E
   A: A
 }
 
-export const UF__ = "F__"
-
-export type UF__ = typeof UF__
-
+export const UF___ = "F___"
+export type UF___ = typeof UF___
 export interface F___<R, E, A> {
   URI: UF___
   R: (_: R) => void
@@ -24,10 +22,8 @@ export interface F___<R, E, A> {
   A: A
 }
 
-export const UF___ = "F___"
-
-export type UF___ = typeof UF___
-
+export const UF____ = "F____"
+export type UF____ = typeof UF____
 export interface F____<S, R, E, A> {
   URI: UF____
   S: S
@@ -36,33 +32,15 @@ export interface F____<S, R, E, A> {
   A: A
 }
 
-export const UF____ = "F____"
-
-export type UF____ = typeof UF____
-
 export const UG_ = "G_"
-
 export type UG_ = typeof UG_
-
 export interface G_<A> {
   URI: UG_
   A: A
 }
 
-export interface URItoKind<D, N extends string, K, SI, SO, X, I, S, R, E, A> {
-  [UF_]: F_<A>
-  [UG_]: G_<A>
-  [UF__]: F__<E, A>
-  [UF___]: F___<R, E, A>
-  [UF____]: F____<S, R, E, A>
-}
-
-export interface URItoIndex<N extends string, K> {}
-
 export const HKTFullURI = "HKTFullURI"
-
 export type HKTFullURI = typeof HKTFullURI
-
 export interface HKTFull<K, SI, SO, X, I, S, R, E, A> {
   URI: HKTFullURI
   K: () => K
@@ -75,6 +53,16 @@ export interface HKTFull<K, SI, SO, X, I, S, R, E, A> {
   E: () => E
   A: A
 }
+
+export interface URItoKind<D, N extends string, K, SI, SO, X, I, S, R, E, A> {
+  [UF_]: F_<A>
+  [UG_]: G_<A>
+  [UF__]: F__<E, A>
+  [UF___]: F___<R, E, A>
+  [UF____]: F____<S, R, E, A>
+}
+
+export interface URItoIndex<N extends string, K> {}
 
 export type URISL0 = keyof URItoKind<
   any,
