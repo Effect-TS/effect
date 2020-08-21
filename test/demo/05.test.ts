397855f224f39020f7415a513b64f3489f61d7bb
Nikita Volodin <volodin.n@gmail.com>
2020-08-21 00:26:31 -0400

    move all demos as tests

---
+++ test/demo/05.test.ts
@@ -0,0 +1,20 @@
+import { constant, pipe } from "@effect-ts/system/Function"
+
+import * as E from "../../src/Classic/Either"
+import * as R from "../../src/Classic/Record"
+import * as X from "../../src/Pure"
+
+test("05", () => {
+  pipe(
+    { a: 0, b: 1, c: 2, d: 3 },
+    R.separateF(X.Applicative)((n) =>
+      X.succeed(constant(n < 2 ? E.left(n) : E.right(n)))
+    ),
+    X.chain((r) =>
+      X.sync(() => {
+        console.log(r)
+      })
+    ),
+    X.runIO
+  )
+})
---
+++ test/demo/08.test.ts
@@ -0,0 +1,38 @@
+import { pipe } from "@effect-ts/system/Function"
+
+import { makeAssociative } from "../../src/Classic/Associative"
+import * as T from "../../src/Effect"
+import * as DSL from "../../src/Prelude/DSL"
+
+test("08", () => {
+  const ValidationApplicative = T.getValidationApplicative(
+    makeAssociative<string>((r) => (l) => `${l} | ${r}`)
+  )
+
+  const validationSequenceS = DSL.sequenceSF(ValidationApplicative)
+
+  const result = validationSequenceS({
+    a: T.succeed(0),
+    b: T.succeed(1),
+    c: T.succeed(2),
+    d: T.fail("d"),
+    e: T.fail("e"),
+    f: T.access((_: { foo: string }) => 3),
+    g: T.access((_: { bar: string }) => 3)
+  })
+
+  pipe(
+    result,
+    T.either,
+    T.chain((e) =>
+      T.effectTotal(() => {
+        console.log(e)
+      })
+    ),
+    T.provideAll({
+      foo: "foo",
+      bar: "bar"
+    }),
+    T.runSync
+  )
+})
