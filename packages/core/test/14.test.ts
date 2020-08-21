929df439e87b226bd032719ead3679668023464f
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-21 16:25:32 +0100

    adapt fx access/provide

--- packages/core/test/14.test.ts
+++ packages/core/test/14.test.ts
@@ -4,7 +4,9 @@ import { makeAssociative } from "../src/Classic/Associative"
 import * as E from "../src/Classic/Either"
 import * as EitherT from "../src/Classic/EitherT"
 import { getValidationF, sequenceSF } from "../src/Prelude/DSL"
+import * as IO from "../src/XPure/IO"
 import * as R from "../src/XPure/Reader"
+import * as ReaderT from "../src/XPure/ReaderT"
 
 /**
  * Silly example of Reader[_-, Either[_+, _+]] using transformers
@@ -12,13 +14,21 @@ import * as R from "../src/XPure/Reader"
  * (all is already inglobated in XPure so a direct XPure interpretation would be much better)
  */
 
-const Monad = pipe(R.Monad, EitherT.monad("X"))
-const Applicative = pipe(R.Applicative, EitherT.applicative("X"))
-const Run = pipe(R.Covariant, EitherT.run("X"))
-const Fail = pipe(R.Monad, EitherT.fail("X"))
+const Monad = pipe(IO.Monad, EitherT.monad("X"), ReaderT.monad("I"))
+const Applicative = pipe(
+  IO.Applicative,
+  EitherT.applicative("X"),
+  ReaderT.applicative()
+)
+const Run = pipe(IO.Covariant, EitherT.run("X"), ReaderT.run("I"))
+const Fail = pipe(IO.Monad, EitherT.fail("X"), ReaderT.fail("I"))
+const Access = pipe(IO.Monad, EitherT.monad("X"), ReaderT.access("I"))
+const Provide = pipe(IO.Monad, EitherT.monad("X"), ReaderT.provide("I"))
 
 export const __ = Fail.fail
 export const ___ = Run.run
+export const ____ = Access.access
+export const _____ = Provide.provide
 
 const getValidation = getValidationF({
   ...Monad,
@@ -34,12 +44,13 @@ const StringValidation = getValidation(
 const validate = sequenceSF(StringValidation)
 
 export const result = validate({
-  a: R.succeed(E.left("foo")),
-  b: R.succeed(E.left("bar"))
+  a: R.succeed(IO.succeed(E.left("foo"))),
+  b: R.succeed(IO.succeed(E.left("bar"))),
+  c: Access.access((r: number) => r + 1)
 })
 
 test("14", () => {
-  pipe(result, R.run, (x) => {
+  pipe(result, Provide.provide(2), R.run, IO.run, (x) => {
     console.log(x)
   })
 })
