acfd6d5aaac2d0b8ecb3bd749b79fdc2af5d56b1
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-21 19:24:04 +0100

    alias variance definition

--- packages/core/src/Classic/InvariantT/index.ts
+++ packages/core/src/Classic/InvariantT/index.ts
@@ -1,24 +1,22 @@
 import type { Erase } from "@effect-ts/system/Utils"
 
 import * as P from "../../Prelude"
-import type { URIS } from "../../Prelude/HKT"
+import type * as HKT from "../../Prelude/HKT"
 import type { Par } from "../../Prelude/HKT/variance"
 
-type Strip<
-  C,
-  P extends Par,
-  A extends "+" | "-" | "_",
-  B extends "+" | "-" | "_"
-> = Erase<Erase<C, P.V<P, A>>, P.V<P, B>>
+type Strip<C, P extends Par, A extends HKT.Variance, B extends HKT.Variance> = Erase<
+  Erase<C, P.V<P, A>>,
+  P.V<P, B>
+>
 
 export function monad<P extends Par>(_: P) {
-  return <F extends URIS, C>(
+  return <F extends HKT.URIS, C>(
     M: P.Monad<F, C>
   ): P.Monad<F, Strip<C, P, "+", "-"> & P.V<P, "_">> => P.instance(M)
 }
 
 export function applicative<P extends Par>(_: P) {
-  return <F extends URIS, C>(
+  return <F extends HKT.URIS, C>(
     M: P.Applicative<F, C>
   ): P.Applicative<F, Strip<C, P, "+", "-"> & P.V<P, "_">> => P.instance(M)
 }
