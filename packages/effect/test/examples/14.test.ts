e1a174b3cd94523a0aa8c8266c8d6020d8158e8e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-22 13:57:58 +0100

    improve demo, fix branch

--- packages/effect/test/examples/14.test.ts
+++ packages/effect/test/examples/14.test.ts
@@ -1,81 +1,107 @@
+// NOTE: namespaces are used to scope things in the demo, in real usage don't use namespaces!
+
+/* eslint-disable @typescript-eslint/no-namespace */
+
 import { pipe } from "@effect-ts/system/Function"
 
 import { makeAssociative } from "../../src/Classic/Associative"
 import * as EitherT from "../../src/Classic/EitherT"
 import * as DSL from "../../src/Prelude/DSL"
+import * as S from "../../src/Prelude/Selective"
 import * as IO from "../../src/XPure/IO"
-import * as R from "../../src/XPure/Reader"
+import * as Reader from "../../src/XPure/Reader"
 import * as ReaderT from "../../src/XPure/ReaderT"
 
 //
 // IO[Either[X, A]]
 //
 
-const IOEMonad = pipe(IO.Monad, EitherT.monad("X"))
-const IOEApplicative = pipe(IO.Applicative, EitherT.applicative("X"))
-const IOERun = pipe(IO.Covariant, EitherT.run("X"))
-const IOEFail = pipe(IO.Monad, EitherT.fail("X"))
+namespace IOEither {
+  export const Monad = pipe(IO.Monad, EitherT.monad("X"))
+  export const Applicative = pipe(IO.Applicative, EitherT.applicative("X"))
+  export const Run = pipe(IO.Covariant, EitherT.run("X"))
+  export const Fail = pipe(IO.Monad, EitherT.fail("X"))
+}
 
 //
 // Reader[I, IO[Either[X, A]]]
 //
 
-const Monad = pipe(IOEMonad, ReaderT.monad("I"))
-const Applicative = pipe(IOEApplicative, ReaderT.applicative("I"))
-const Run = pipe(IOERun, ReaderT.run("I"))
-const Fail = pipe(IOEFail, ReaderT.fail("I"))
-const Access = pipe(IOEMonad, ReaderT.access("I"))
-const Provide = pipe(IOEMonad, ReaderT.provide("I"))
+namespace ReaderIOEither {
+  export const Monad = pipe(IOEither.Monad, ReaderT.monad("I"))
+  export const Applicative = pipe(IOEither.Applicative, ReaderT.applicative("I"))
+  export const Run = pipe(IOEither.Run, ReaderT.run("I"))
+  export const Fail = pipe(IOEither.Fail, ReaderT.fail("I"))
+  export const Access = pipe(IOEither.Monad, ReaderT.access("I"))
+  export const Provide = pipe(IOEither.Monad, ReaderT.provide("I"))
 
-export const { access, any, both, fail, flatten, map, provide, run } = {
-  ...Monad,
-  ...Applicative,
-  ...Run,
-  ...Fail,
-  ...Access,
-  ...Provide
+  export const { access, any, both, fail, flatten, map, provide, run } = {
+    ...Monad,
+    ...Applicative,
+    ...Run,
+    ...Fail,
+    ...Access,
+    ...Provide
+  }
+
+  export const succeed = DSL.succeedF(Monad)
+  export const accessM = DSL.accessMF({ ...Monad, ...Access })
+
+  export const Do = DSL.doF(Monad)()
+  export const bind = DSL.bindF(Monad)
+
+  export const provideSome = DSL.provideSomeF({
+    ...Monad,
+    ...Access,
+    ...Provide
+  })
+
+  export const getValidation = DSL.getValidationF({
+    ...Monad,
+    ...Applicative,
+    ...Run,
+    ...Fail
+  })
+
+  export const StringValidation = getValidation(
+    makeAssociative<string>((l) => (r) => `${l}, ${r}`)
+  )
+
+  export const validate = DSL.sequenceSF(StringValidation)
 }
 
-export const succeed = DSL.succeedF(Monad)
-export const accessM = DSL.accessMF({ ...Monad, ...Access })
+//
+// Reader[I, IO[A]]
+//
 
-export const Do = DSL.doF(Monad)()
-export const bind = DSL.bindF(Monad)
+namespace ReaderIO {
+  export const RIOMonad = pipe(IO.Monad, ReaderT.monad("I"))
+  export const RIOApplicative = pipe(IO.Applicative, ReaderT.applicative("I"))
+  export const RIOSelective = S.getSelectMonad({ ...RIOMonad, ...RIOApplicative })
 
-export const provideSome = DSL.provideSomeF({
-  ...Monad,
-  ...Access,
-  ...Provide
-})
+  export const succeed = DSL.succeedF(RIOMonad)
 
-export const getValidation = DSL.getValidationF({
-  ...Monad,
-  ...Applicative,
-  ...Run,
-  ...Fail
-})
-
-const StringValidation = getValidation(
-  makeAssociative<string>((l) => (r) => `${l}, ${r}`)
-)
-
-const validate = DSL.sequenceSF(StringValidation)
+  export const branch = S.getBranch(RIOSelective)
+}
 
 test("14", () => {
   pipe(
-    Do,
-    bind("x", () => succeed("0")),
-    bind("y", () => succeed("1")),
-    bind("v", () =>
-      validate({
-        a: fail("foo"),
-        b: fail("bar"),
-        c: access((r: number) => r + 1)
+    ReaderIOEither.Do,
+    ReaderIOEither.bind("x", () => ReaderIOEither.succeed("0")),
+    ReaderIOEither.bind("y", () => ReaderIOEither.succeed("1")),
+    ReaderIOEither.bind("v", () =>
+      ReaderIOEither.validate({
+        a: ReaderIOEither.fail("foo"),
+        b: ReaderIOEither.fail("bar"),
+        c: ReaderIOEither.access((r: number) => r + 1)
       })
     ),
-    map(({ v: { a, b, c }, x, y }) => `${a}${b}${c}${x}${y}`),
-    provide(2),
-    R.run,
+    ReaderIOEither.map(({ v: { a, b, c }, x, y }) => `${a}${b}${c}${x}${y}`),
+    ReaderIO.branch(
+      ReaderIO.succeed((s) => `error: ${s}`),
+      ReaderIO.succeed((s) => `success: ${s}`)
+    ),
+    Reader.runEnv(2),
     IO.run,
     (x) => {
       console.log(x)
