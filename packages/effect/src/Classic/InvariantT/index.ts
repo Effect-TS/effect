96beda845aaaf050cfb452adb0d2cd3bb64f1811
Nikita Volodin <volodin.n@gmail.com>
2020-08-22 00:44:36 -0400

    simplify InvariantT variance erasure

--- packages/effect/src/Classic/InvariantT/index.ts
+++ packages/effect/src/Classic/InvariantT/index.ts
@@ -4,19 +4,15 @@ import * as P from "../../Prelude"
 import type * as HKT from "../../Prelude/HKT"
 import type { Par } from "../../Prelude/HKT/variance"
 
-type Strip<C, P extends Par, A extends HKT.Variance, B extends HKT.Variance> = Erase<
-  Erase<C, P.V<P, A>>,
-  P.V<P, B>
->
-
 export function monad<P extends Par>(_: P) {
   return <F extends HKT.URIS, C>(
     M: P.Monad<F, C>
-  ): P.Monad<F, Strip<C, P, "+", "-"> & P.V<P, "_">> => P.instance(M)
+  ): P.Monad<F, Erase<C, P.V<P, "+"> & P.V<P, "-">> & P.V<P, "_">> => P.instance(M)
 }
 
 export function applicative<P extends Par>(_: P) {
   return <F extends HKT.URIS, C>(
     M: P.Applicative<F, C>
-  ): P.Applicative<F, Strip<C, P, "+", "-"> & P.V<P, "_">> => P.instance(M)
+  ): P.Applicative<F, Erase<C, P.V<P, "+"> & P.V<P, "-">> & P.V<P, "_">> =>
+    P.instance(M)
 }
