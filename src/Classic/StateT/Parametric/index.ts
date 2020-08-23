515d75d4e1508b1c63b0bc7c667d2359ddbdd59a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-24 00:38:56 +0100

    progressively replace Fix at the top

--- src/Classic/StateT/Parametric/index.ts
+++ src/Classic/StateT/Parametric/index.ts
@@ -1,27 +1,33 @@
+import type { Erase } from "@effect-ts/system/Utils"
+
 import * as StateT from "../"
 import type { StateInURI, StateOutURI } from "../../../Modules"
 import type { Monad } from "../../../Prelude"
-import type { URI, URIS } from "../../../Prelude/HKT"
 import * as HKT from "../../../Prelude/HKT"
+
+export type V<C, S> = HKT.Unfix<Erase<HKT.Strip<C, "S">, HKT.Auto>, "S"> &
+  HKT.Fix<"S", S>
 
 export type PState = "S"
 
-export interface PSIn<S> extends URI<StateInURI, HKT.Fix<"S", S>> {}
+export interface PSIn<S> extends HKT.URI<StateInURI, HKT.Fix<"S", S>> {}
 
-export interface PSOut<S> extends URI<StateOutURI, HKT.Fix<"S", S>> {}
+export interface PSOut<S> extends HKT.URI<StateOutURI, HKT.Fix<"S", S>> {}
 
-export type ParametricStateT<F extends URIS, S> = HKT.PrependURI<
+export type ParametricStateT<F extends HKT.URIS, S> = HKT.PrependURI<
   PSIn<S>,
   HKT.AppendURI<PSOut<S>, F>
 >
 
 export function monad<S>() {
-  return <F extends URIS, C>(M: Monad<F, C>) => getMonad_<F, C, S>(M)
+  return <F extends HKT.URIS, C>(M: Monad<F, C>) => getMonad_<F, C, S>(M)
 }
 
-function getMonad_<F extends URIS, C, S>(
+function getMonad_<F extends HKT.URIS, C, S>(
   M: Monad<F, C>
-): Monad<ParametricStateT<F, S>, C>
-function getMonad_<S>(M: Monad<[HKT.UF_]>): Monad<ParametricStateT<[HKT.UF_], S>> {
+): Monad<ParametricStateT<F, S>, V<C, S>>
+function getMonad_<S>(
+  M: Monad<[HKT.UF_]>
+): Monad<ParametricStateT<[HKT.UF_], S>, V<HKT.Auto, S>> {
   return HKT.instance(StateT.monad(M))
 }
