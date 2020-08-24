3345f7a4715a2035ebd7db8f6b688d6b69529964
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-24 14:05:02 +0100

    improve clean param

--- packages/core/src/Classic/EitherT/index.ts
+++ packages/core/src/Classic/EitherT/index.ts
@@ -1,23 +1,29 @@
-import type { Erase } from "@effect-ts/system/Utils"
-
-import { identity, pipe } from "../../Function"
+import { pipe } from "../../Function"
 import type { Any, Applicative, Covariant, Monad } from "../../Prelude"
 import { succeedF } from "../../Prelude/DSL"
 import type { Fail, Run } from "../../Prelude/FX"
-import type { Auto, F_, UF_, URIS } from "../../Prelude/HKT"
+import type { F_, UF_, URIS } from "../../Prelude/HKT"
 import * as HKT from "../../Prelude/HKT"
 import * as E from "../Either"
 
-export type EitherTVariance<C> = Erase<HKT.Strip<C, "E">, Auto> & HKT.V<"E", "+">
+export type V<C> = HKT.CleanParam<C, "E"> & HKT.V<"E", "+">
 
 export function monad<F extends URIS, C>(
   M: Monad<F, C>
-): Monad<HKT.AppendURI<F, E.EitherURI>, EitherTVariance<C>>
-export function monad(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI]> {
+): Monad<HKT.AppendURI<F, E.EitherURI>, V<C>>
+export function monad(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI], HKT.V<"E", "+">> {
   return HKT.instance({
     any: () => succeedF(M)(E.right({})),
-    flatten: (ffa) =>
-      pipe(ffa, M.map(E.fold((e) => succeedF(M)(E.left(e)), identity)), M.flatten),
+    flatten: <E, A, E2>(
+      ffa: F_<E.Either<E2, F_<E.Either<E, A>>>>
+    ): F_<E.Either<E | E2, A>> =>
+      pipe(
+        ffa,
+        M.map(
+          E.fold((e) => succeedF(M)(E.widenE<E>()(E.left(e))), M.map(E.widenE<E2>()))
+        ),
+        M.flatten
+      ),
     map: <A, B>(f: (a: A) => B) => <E>(fa: F_<E.Either<E, A>>): F_<E.Either<E, B>> =>
       pipe(fa, M.map(E.map(f)))
   })
@@ -25,15 +31,17 @@ export function monad(M: Monad<[UF_]>): Monad<[UF_, E.EitherURI]> {
 
 export function applicative<F extends URIS, C>(
   M: Applicative<F, C>
-): Applicative<HKT.AppendURI<F, E.EitherURI>, EitherTVariance<C>>
-export function applicative(M: Applicative<[UF_]>): Applicative<[UF_, E.EitherURI]> {
+): Applicative<HKT.AppendURI<F, E.EitherURI>, V<C>>
+export function applicative(
+  M: Applicative<[UF_]>
+): Applicative<[UF_, E.EitherURI], HKT.V<"E", "+">> {
   return HKT.instance({
     any: () => succeedF(M)(E.right({})),
     map: <A, B>(f: (a: A) => B) => <E>(fa: F_<E.Either<E, A>>): F_<E.Either<E, B>> =>
       pipe(fa, M.map(E.map(f))),
-    both: <E2, B>(fb: F_<E.Either<E2, B>>) => <A>(
-      fa: F_<E.Either<E2, A>>
-    ): F_<E.Either<E2, readonly [A, B]>> =>
+    both: <E2, B>(fb: F_<E.Either<E2, B>>) => <E, A>(
+      fa: F_<E.Either<E, A>>
+    ): F_<E.Either<E2 | E, readonly [A, B]>> =>
       pipe(
         fa,
         M.both(fb),
@@ -44,8 +52,8 @@ export function applicative(M: Applicative<[UF_]>): Applicative<[UF_, E.EitherUR
 
 export function run<F extends URIS, C>(
   M: Covariant<F, C>
-): Run<HKT.AppendURI<F, E.EitherURI>, EitherTVariance<C>>
-export function run(M: Covariant<[UF_]>): Run<[UF_, E.EitherURI]> {
+): Run<HKT.AppendURI<F, E.EitherURI>, V<C>>
+export function run(M: Covariant<[UF_]>): Run<[UF_, E.EitherURI], HKT.V<"E", "+">> {
   return HKT.instance({
     either: <E, A>(fa: F_<E.Either<E, A>>): F_<E.Either<never, E.Either<E, A>>> => {
       return pipe(fa, M.map(E.Run.either))
@@ -55,8 +63,10 @@ export function run(M: Covariant<[UF_]>): Run<[UF_, E.EitherURI]> {
 
 export function fail<F extends URIS, C>(
   M: Any<F, C> & Covariant<F, C>
-): Fail<HKT.AppendURI<F, E.EitherURI>, EitherTVariance<C>>
-export function fail(M: Any<[UF_]> & Covariant<[UF_]>): Fail<[UF_, E.EitherURI]> {
+): Fail<HKT.AppendURI<F, E.EitherURI>, V<C>>
+export function fail(
+  M: Any<[UF_]> & Covariant<[UF_]>
+): Fail<[UF_, E.EitherURI], HKT.V<"E", "+">> {
   return HKT.instance({
     fail: <E, A = never>(e: E): F_<E.Either<E, A>> =>
       succeedF(M)(E.widenA<A>()(E.left(e)))
--- packages/core/src/Prelude/HKT/fix.ts
+++ packages/core/src/Prelude/HKT/fix.ts
@@ -20,13 +20,17 @@ export type OrFix<P extends Param, A, B> = A extends Fix<P, infer X>
     : X
   : B
 
-export type Unfix<C, P extends Param> = {
-  [K in Exclude<keyof C, "Fix">]: C[K]
-} &
-  {
-    [K in keyof C & "Fix"]: {
-      [KK in Exclude<keyof C[K], P>]: C[K][KK]
-    }
-  }
+export type Unfix<C, P extends Param> = (Exclude<keyof C, "Fix"> extends never
+  ? unknown
+  : {
+      [K in Exclude<keyof C, "Fix">]: C[K]
+    }) &
+  (keyof C & "Fix" extends never
+    ? unknown
+    : {
+        [K in keyof C & "Fix"]: {
+          [KK in Exclude<keyof C[K], P>]: C[K][KK]
+        }
+      })
 
 export type CleanParam<C, P extends Param> = Unfix<Erase<Strip<C, P>, Auto>, P>
