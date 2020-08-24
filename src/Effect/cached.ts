207025a317fff156aa722b1eb7ce563e6f5d6dbb
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-24 23:20:40 +0100

    add comments

--- src/Effect/cached.ts
+++ src/Effect/cached.ts
@@ -13,8 +13,30 @@ import { map } from "./map"
 import { tap } from "./tap"
 import { toPromise } from "./toPromise"
 
+/**
+ * Returns an effect that, if evaluated, will return the cached result of
+ * this effect. Cached results will expire after `timeToLive` duration.
+ */
 export function cached(ttl: number) {
   return <S, R, E, A>(fa: Effect<S, R, E, A>) => cached_(fa, ttl)
+}
+
+/**
+ * Returns an effect that, if evaluated, will return the cached result of
+ * this effect. Cached results will expire after `timeToLive` duration.
+ */
+export function cached_<S, R, E, A>(
+  fa: Effect<S, R, E, A>,
+  ttl: number
+): SyncR<R & Has<Clock>, AsyncE<E, A>> {
+  return pipe(
+    of,
+    bind("r", () => environment<R & Has<Clock>>()),
+    bind("cache", () =>
+      RefM.makeRefM<O.Option<readonly [number, P.Promise<E, A>]>>(O.none)
+    ),
+    map(({ cache, r }) => provideAll(r)(get(fa, ttl, cache)))
+  )
 }
 
 function compute<S, R, E, A>(fa: Effect<S, R, E, A>, ttl: number, start: number) {
@@ -50,19 +72,5 @@ function get<S, R, E, A>(
         )
       )
     )
-  )
-}
-
-export function cached_<S, R, E, A>(
-  fa: Effect<S, R, E, A>,
-  ttl: number
-): SyncR<R & Has<Clock>, AsyncE<E, A>> {
-  return pipe(
-    of,
-    bind("r", () => environment<R & Has<Clock>>()),
-    bind("cache", () =>
-      RefM.makeRefM<O.Option<readonly [number, P.Promise<E, A>]>>(O.none)
-    ),
-    map(({ cache, r }) => provideAll(r)(get(fa, ttl, cache)))
   )
 }
