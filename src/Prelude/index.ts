fa73f26e68916b8ef86c7ef14a08f51c98c60660
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-25 12:40:58 +0100

    improve exports

--- src/Prelude/index.ts
+++ src/Prelude/index.ts
@@ -1,97 +1,35 @@
-export { Commutative, CommutativeURI, makeCommutative } from "../Classic/Commutative"
-export { Any } from "./Any"
-export { AssociativeBoth } from "./AssociativeBoth"
-export { AssociativeEither } from "./AssociativeEither"
-export { AssociativeFlatten } from "./AssociativeFlatten"
-export {
-  Applicative,
-  IdentityBoth,
-  IdentityEither,
-  IdentityFlatten,
-  Monad
-} from "./Combined"
-export { CommutativeBoth } from "./CommutativeBoth"
-export { CommutativeEither } from "./CommutativeEither"
-export { Compact } from "./Compact"
-export { Compactable } from "./Compactable"
-export { Contravariant, getContravariantComposition } from "./Contravariant"
-export { Covariant, CovariantComposition, getCovariantComposition } from "./Covariant"
-export { CovariantWithIndex } from "./CovariantWithIndex"
-export { Derive } from "./Derive"
-export { Extend } from "./Extend"
-export { Foldable } from "./Foldable"
-export { FoldableWithIndex } from "./FoldableWithIndex"
-export { FoldMap, FoldMapFn } from "./FoldMap"
-export { FoldMapWithIndex, FoldMapWithIndexFn } from "./FoldMapWithIndex"
+export * from "../Classic/Commutative"
+export * from "./Any"
+export * from "./AssociativeBoth"
+export * from "./AssociativeEither"
+export * from "./AssociativeFlatten"
+export * from "./Combined"
+export * from "./CommutativeBoth"
+export * from "./CommutativeEither"
+export * from "./Compact"
+export * from "./Compactable"
+export * from "./Contravariant"
+export * from "./Covariant"
+export * from "./CovariantWithIndex"
+export * from "./Derive"
+export * from "./Extend"
+export * from "./Foldable"
+export * from "./FoldableWithIndex"
+export * from "./FoldMap"
+export * from "./FoldMapWithIndex"
+export * from "./HKT"
+export * from "./Invariant"
+export * from "./None"
+export * from "./Reduce"
+export * from "./ReduceRight"
+export * from "./ReduceRightWithIndex"
+export * from "./ReduceWithIndex"
+export * from "./Separate"
+export * from "./Traversable"
+export * from "./TraversableWithIndex"
+export * from "./Wiltable"
+export * from "./WiltableWithIndex"
+export * from "./Witherable"
+export * from "./WitherableWithIndex"
+
 export * as FX from "./FX"
-export {
-  Auto,
-  Base,
-  CompositionBase2,
-  Fix,
-  F_,
-  F__,
-  F___,
-  F____,
-  G_,
-  HKTFull,
-  HKTFullURI,
-  IndexFor,
-  InferA,
-  InferE,
-  InferI,
-  InferK,
-  InferN,
-  InferR,
-  InferS,
-  InferX,
-  Initial,
-  instance,
-  Intro,
-  ConcreteKind as Kind,
-  Mix,
-  MixStruct,
-  OrFix,
-  OrNever,
-  UF_,
-  UF__,
-  UF___,
-  UF____,
-  UG_,
-  URIS,
-  URItoIndex,
-  URItoKind,
-  V
-} from "./HKT"
-export { Invariant } from "./Invariant"
-export { None } from "./None"
-export { Reduce } from "./Reduce"
-export { ReduceRight } from "./ReduceRight"
-export { ReduceRightWithIndex, ReduceRightWithIndexFn } from "./ReduceRightWithIndex"
-export { ReduceWithIndex, ReduceWithIndexFn } from "./ReduceWithIndex"
-export { Separate } from "./Separate"
-export {
-  Foreach,
-  ForeachComposition,
-  getTraversableComposition,
-  implementForeachF,
-  Traversable,
-  TraversableComposition
-} from "./Traversable"
-export {
-  ForeachWithIndex,
-  implementForeachWithIndexF,
-  TraversableWithIndex
-} from "./TraversableWithIndex"
-export { implementSeparateF, Wilt, Wiltable } from "./Wiltable"
-export {
-  implementSeparateWithIndexF,
-  WiltableWithIndex,
-  WiltWithIndex
-} from "./WiltableWithIndex"
-export { implementCompactF, Wither, Witherable } from "./Witherable"
-export {
-  implementCompactWithIndexF,
-  WitherableWithIndex,
-  WitherWithIndex
-} from "./WitherableWithIndex"
