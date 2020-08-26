b9811adeedee731710ff90199e3b6a71d56f2bb3
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-26 13:40:54 +0100

    add forkAllUnit and fromFiber

---
+++ packages/system/src/Effect/fromFiber.ts
@@ -0,0 +1,11 @@
+import * as Fiber from "../Fiber"
+import { chain_, effectTotal } from "./core"
+import type { AsyncE } from "./effect"
+
+/**
+ * Creates a `ZIO` value that represents the exit value of the specified
+ * fiber.
+ */
+export function fromFiber<E, A>(fiber: () => Fiber.Fiber<E, A>): AsyncE<E, A> {
+  return chain_(effectTotal(fiber), Fiber.join)
+}
