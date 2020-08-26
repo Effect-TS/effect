da5f60e07de3041b5041b1b6d4031cda9c7e9ad8
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-26 12:51:31 +0100

    ass firstSuccessOf

---
+++ packages/system/src/Effect/firstSuccessOf.ts
@@ -0,0 +1,17 @@
+import * as A from "../Array"
+import * as NEA from "../NonEmptyArray"
+import type { Effect } from "./effect"
+import { orElse_ } from "./orElse_"
+
+/**
+ * Returns an effect that yields the value of the first
+ * effect to succeed.
+ */
+export function firstSuccessOf<S, R, E, A>(
+  effects: NEA.NonEmptyArray<Effect<S, R, E, A>>
+) {
+  const first = NEA.head(effects)
+  const rest = NEA.tail(effects)
+
+  return A.reduce_(rest, first, (b, a) => orElse_(b, () => a))
+}
