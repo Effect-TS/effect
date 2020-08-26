1699c6ada3315f5a24ba4fa6691488268c1c2ab6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-26 13:48:19 +0100

    add fromFiberM

--- packages/system/src/Effect/fromFiber.ts
+++ packages/system/src/Effect/fromFiber.ts
@@ -1,6 +1,6 @@
 import * as Fiber from "../Fiber"
 import { chain_, effectTotal } from "./core"
-import type { AsyncE } from "./effect"
+import type { AsyncE, AsyncRE, Effect } from "./effect"
 
 /**
  * Creates a `Effect` value that represents the exit value of the specified
@@ -8,4 +8,14 @@ import type { AsyncE } from "./effect"
  */
 export function fromFiber<E, A>(fiber: () => Fiber.Fiber<E, A>): AsyncE<E, A> {
   return chain_(effectTotal(fiber), Fiber.join)
+}
+
+/**
+ * Creates a `Effect` value that represents the exit value of the specified
+ * fiber.
+ */
+export function fromFiberM<S, R, E, E2, A>(
+  fiber: Effect<S, R, E, Fiber.Fiber<E2, A>>
+): AsyncRE<R, E | E2, A> {
+  return chain_(fiber, Fiber.join)
 }
