ad4c048cd52135f45ef2bc2722a76131bef8d4b4
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-27 15:50:58 +0100

    add iterate

---
+++ src/Effect/iterate.ts
@@ -0,0 +1,26 @@
+import { chain_, succeed } from "./core"
+import type { Effect } from "./effect"
+
+/**
+ * Iterates with the specified effectual function. The moral equivalent of:
+ *
+ * ```
+ * let s = initial
+ *
+ * while (cont(s)) {
+ *   s = body(s)
+ * }
+ *
+ * return s
+ * ```
+ */
+export function iterate<Z>(initial: Z) {
+  return (cont: (z: Z) => boolean) => <S, R, E>(
+    body: (z: Z) => Effect<S, R, E, Z>
+  ): Effect<S, R, E, Z> => {
+    if (cont(initial)) {
+      return chain_(body(initial), (z2) => iterate(z2)(cont)(body))
+    }
+    return succeed(initial)
+  }
+}
