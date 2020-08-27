011931e050598f73a1f27fb190ac75c47639981a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-27 16:08:09 +0100

    add loopUnit

--- src/Effect/loop.ts
+++ src/Effect/loop.ts
@@ -1,5 +1,5 @@
 import { pipe } from "../Function"
-import { chain_, succeed } from "./core"
+import { chain_, succeed, unit } from "./core"
 import type { Effect } from "./effect"
 import { map } from "./map"
 
@@ -32,5 +32,29 @@ export function loop<Z>(initial: Z) {
       )
     }
     return succeed([])
+  }
+}
+
+/**
+ * Loops with the specified effectual function purely for its effects. The
+ * moral equivalent of:
+ *
+ * ```
+ * var s = initial
+ *
+ * while (cont(s)) {
+ *   body(s)
+ *   s = inc(s)
+ * }
+ * ```
+ */
+export function loopUnit<Z>(initial: Z) {
+  return (cont: (z: Z) => boolean, inc: (z: Z) => Z) => <S, R, E>(
+    body: (z: Z) => Effect<S, R, E, any>
+  ): Effect<S, R, E, void> => {
+    if (cont(initial)) {
+      return chain_(body(initial), () => loop(inc(initial))(cont, inc)(body))
+    }
+    return unit
   }
 }
