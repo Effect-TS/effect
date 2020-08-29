ef0e79831c8499a2e7e8d0f1ab10147a236f6a62
Nikita Volodin <volodin.n@gmail.com>
2020-08-28 22:57:31 -0400

    split Inverse into separate files

---
+++ src/Classic/Inverse/operations.ts
@@ -0,0 +1,13 @@
+import type { Inverse } from "./index"
+
+export function makeInverse<A>(
+  identity: A,
+  combine: (y: A) => (x: A) => A,
+  inverse: (r: A) => (l: A) => A
+): Inverse<A> {
+  return {
+    combine,
+    identity,
+    inverse
+  }
+}
