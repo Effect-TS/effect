cc015c81ea59d9728ea805a15ef4a5d7b19a359a
Nikita Volodin <volodin.n@gmail.com>
2020-08-28 23:16:34 -0400

    split Commutative into separate files

--- packages/core/src/Classic/Commutative/index.ts
+++ packages/core/src/Classic/Commutative/index.ts
@@ -1,16 +1,3 @@
-import type { Associative } from "../Associative"
-
+export * from "./definition"
 export { CommutativeURI } from "../../Modules"
-
-/**
- * The `Commutative[A]` type class describes a commutative binary operator
- * for a type `A`. For example, addition for integers.
- */
-export interface Commutative<A> extends Associative<A> {
-  readonly commute: (y: A) => (x: A) => A
-}
-
-export const makeCommutative = <A>(f: (r: A) => (l: A) => A): Commutative<A> => ({
-  combine: f,
-  commute: (y) => (x) => f(x)(y)
-})
+export * from "./operations"
