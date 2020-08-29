699ff8cbcb78eea6df4b9fa0b8ca65a9725ef1c1
Nikita Volodin <volodin.n@gmail.com>
2020-08-28 23:16:34 -0400

    split Commutative into separate files

---
+++ packages/effect/src/Classic/Commutative/definition.ts
@@ -0,0 +1,9 @@
+import type { Associative } from "../Associative"
+
+/**
+ * The `Commutative[A]` type class describes a commutative binary operator
+ * for a type `A`. For example, addition for integers.
+ */
+export interface Commutative<A> extends Associative<A> {
+  readonly commute: (y: A) => (x: A) => A
+}
