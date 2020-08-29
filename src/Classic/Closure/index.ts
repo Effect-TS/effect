1d1998011ae834f7dfa1fe0b5b27d0f30e0570e9
Nikita Volodin <volodin.n@gmail.com>
2020-08-28 23:21:24 -0400

    split Closure into separate files

--- src/Classic/Closure/index.ts
+++ src/Classic/Closure/index.ts
@@ -1,12 +1,3 @@
+export * from "./definition"
 export { ClosureURI } from "../../Modules"
-
-/**
- * Base combine
- */
-export interface Closure<A> {
-  combine(r: A): (l: A) => A
-}
-
-export const makeClosure = <A>(f: (l: A, r: A) => A): Closure<A> => ({
-  combine: (r) => (l) => f(l, r)
-})
+export * from "./operations"
