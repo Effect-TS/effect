02163fbab01ddbf7a8272bd357a47524d9d33638
Nikita Volodin <volodin.n@gmail.com>
2020-08-29 15:00:57 -0400

    add Stream absolve

---
+++ packages/effect/src/Stream/Stream/absolve.ts
@@ -0,0 +1,10 @@
+import type { Either } from "../../Either"
+import { fold } from "../../Either"
+import { chain } from "./chain"
+import type { Stream } from "./definitions"
+import { fail } from "./fail"
+import { succeed } from "./succeed"
+
+export const absolve: <S, R, E, E2, O>(
+  xs: Stream<S, R, E, Either<E2, O>>
+) => Stream<S, R, E | E2, O> = chain(fold(fail, succeed))
