24d56b3bbccf02d53884f3a7f2264d75adec6471
Nikita Volodin <volodin.n@gmail.com>
2020-08-28 23:24:29 -0400

    split Associative into separate files

---
+++ packages/effect/src/Classic/Associative/definition.ts
@@ -0,0 +1,8 @@
+import type { Closure } from "../Closure"
+
+/**
+ * The `Associative[A]` type class describes an associative binary operator
+ * for a type `A`. For example, addition for integers, and string
+ * concatenation for strings.
+ */
+export interface Associative<A> extends Closure<A> {}
