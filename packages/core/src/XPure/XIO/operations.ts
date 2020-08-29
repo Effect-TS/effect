703967868444d6462359accff3038173d5347e84
Nikita Volodin <volodin.n@gmail.com>
2020-08-29 00:59:36 -0400

    split XIO into separate files

---
+++ packages/core/src/XPure/XIO/operations.ts
@@ -0,0 +1,42 @@
+import * as F from "@effect-ts/system/XPure"
+
+import type { XIO } from "./definition"
+
+/**
+ * Lift a sync (non failable) computation
+ */
+export const sync: <A>(f: () => A) => XIO<A> = F.sync
+
+/**
+ * Combines this computation with the specified computation.
+ */
+export const zip: <B>(fb: XIO<B>) => <A>(fa: XIO<A>) => XIO<readonly [A, B]> = F.zip
+
+/**
+ * Extends this computation with another computation that depends on the
+ * result of this computation by running the first computation, using its
+ * result to generate a second computation, and running that computation.
+ */
+export const chain: <A, B>(f: (a: A) => XIO<B>) => (self: XIO<A>) => XIO<B> = F.chain
+
+/**
+ * `map` can be used to turn functions `(a: A) => B` into functions `(fa: F<A>) => F<B>`
+ *  whose argument and return types use the type constructor `F` to represent
+ *  some computational context.
+ */
+export const map: <A, B>(f: (a: A) => B) => <R>(self: XIO<A>) => XIO<B> = F.map
+
+/**
+ * Succeed with a value A
+ */
+export const succeed: <A>(a: A) => XIO<A> = (a) => F.succeed(() => a)
+
+/**
+ * Run the computation
+ */
+export const run = <A>(self: XIO<A>): A => F.runIO(self)
+
+/**
+ * Returns a computation that effectfully "peeks" at the success of this one.
+ */
+export const tap: <A>(f: (a: A) => XIO<any>) => (self: XIO<A>) => XIO<A> = F.tap
