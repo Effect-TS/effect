7078d6fb69475e76d253c7dc69d58cebd22b8a1b
Nikita Volodin <volodin.n@gmail.com>
2020-08-28 23:19:02 -0400

    split Ordering into separate files

--- packages/core/src/Classic/Ordering/index.ts
+++ packages/core/src/Classic/Ordering/index.ts
@@ -1,24 +1,2 @@
-import { pipe } from "../../Function"
-import type { TypeOf } from "../../Newtype"
-import { newtype, typeDef } from "../../Newtype"
-
-const Ordering_ = typeDef<"lt" | "eq" | "gt">()("@newtype/Ordering")
-
-export interface Ordering extends TypeOf<typeof Ordering_> {}
-
-export const Ordering = newtype<Ordering>()(Ordering_)
-
-export const toNumber = (o: Ordering) =>
-  pipe(Ordering.unwrap(o), (o) => {
-    switch (o) {
-      case "eq": {
-        return 0
-      }
-      case "gt": {
-        return 1
-      }
-      case "lt": {
-        return -1
-      }
-    }
-  })
+export * from "./definition"
+export * from "./operations"
