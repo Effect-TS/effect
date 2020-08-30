e7f21c4ed26b47f30639a67c96b8f9eaa42a1d70
Nikita Volodin <volodin.n@gmail.com>
2020-08-29 23:59:52 -0400

    add Stream/Push halt()

--- packages/system/src/Stream/Push/index.ts
+++ packages/system/src/Stream/Push/index.ts
@@ -1,5 +1,6 @@
 import * as T from "../_internal/effect"
-import type * as A from "../../Array"
+import * as A from "../../Array"
+import type { Cause } from "../../Cause"
 import * as E from "../../Either"
 import type * as O from "../../Option"
 
@@ -18,3 +19,8 @@ export const fail = <E, I>(
   e: E,
   leftover: A.Array<I>
 ): T.SyncE<[E.Either<E, never>, A.Array<I>], never> => T.fail([E.left(e), leftover])
+
+export const halt = <E>(
+  c: Cause<E>
+): T.SyncE<[E.Either<E, never>, A.Array<never>], never> =>
+  T.mapError_(T.halt(c), (e) => [E.left(e), A.empty])
