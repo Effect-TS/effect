ff11c92ee7465ef23c0bae46b66380d582317f42
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-31 10:07:12 +0100

    add doc to accessStream

--- packages/effect/src/Stream/Stream/accessStream.ts
+++ packages/effect/src/Stream/Stream/accessStream.ts
@@ -2,6 +2,9 @@ import { chain } from "./chain"
 import type { Stream } from "./definitions"
 import { environment } from "./environment"
 
+/**
+ * Accesses the environment of the stream in the context of a stream.
+ */
 export function accessStream<S, R, E, A>(
   f: (r: R) => Stream<S, R, E, A>
 ): Stream<S, R, E, A> {
