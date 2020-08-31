457a93f39c5370383794886542e2a6bef882e982
Nikita Volodin <volodin.n@gmail.com>
2020-08-30 22:32:56 -0400

    add docs to methods

--- src/Stream/Stream/finalizer.ts
+++ src/Stream/Stream/finalizer.ts
@@ -3,6 +3,9 @@ import { unit } from "../../Effect"
 import { bracket } from "./bracket"
 import type { SyncR } from "./definitions"
 
+/**
+ * Creates a one-element stream that never fails and executes the finalizer when it ends.
+ */
 export function finalizer<R>(finalizer: EffSyncR<R, unknown>): SyncR<R, unknown> {
   return bracket((_) => finalizer)(unit)
 }
