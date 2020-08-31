b387fcb0af3fb50703ae4b9699b749b6ff7acd6d
Michael Arnaldi <arnaldimichael@gmail.com>
2020-08-31 14:01:15 +0100

    avoid potential self-reference in sync cases

--- packages/system/src/Stream/Stream/concatAll.ts
+++ packages/system/src/Stream/Stream/concatAll.ts
@@ -2,13 +2,50 @@ import * as T from "../_internal/effect"
 import * as M from "../_internal/managed"
 import type { Array } from "../../Array"
 import * as Cause from "../../Cause"
-import { toManaged } from "../../Effect"
 import { pipe } from "../../Function"
 import type { Option } from "../../Option"
 import { fold_ } from "../../Option"
-import { getAndUpdate, makeRef } from "../../Ref"
+import type { Ref } from "../../Ref"
+import { getAndUpdate, makeManagedRef } from "../../Ref"
 import * as Pull from "../Pull"
 import { Stream } from "./definitions"
+
+function go<S, R, E, O>(
+  streams: Array<Stream<S, R, E, O>>,
+  chunkSize: number,
+  currIndex: Ref<number>,
+  currStream: Ref<T.Effect<S, R, Option<E>, Array<O>>>,
+  switchStream: (
+    x: M.Managed<S, R, never, T.Effect<S, R, Option<E>, Array<O>>>
+  ) => T.Effect<S, R, never, T.Effect<S, R, Option<E>, Array<O>>>
+): T.Effect<S, R, Option<E>, Array<O>> {
+  return pipe(
+    currStream.get,
+    T.flatten,
+    T.catchAllCause((x) =>
+      fold_(
+        Cause.sequenceCauseOption(x),
+        () =>
+          pipe(
+            currIndex,
+            getAndUpdate((x) => x + 1),
+            T.chain((i) =>
+              i >= chunkSize
+                ? Pull.end
+                : pipe(
+                    switchStream(streams[i].proc),
+                    T.chain(currStream.set),
+                    T.zipSecond(
+                      go(streams, chunkSize, currIndex, currStream, switchStream)
+                    )
+                  )
+            )
+          ),
+        Pull.halt
+      )
+    )
+  )
+}
 
 /**
  * Concatenates all of the streams in the chunk to one stream.
@@ -20,40 +57,16 @@ export function concatAll<S, R, E, O>(
   return new Stream(
     pipe(
       M.of,
-      M.bind("currIndex", () => toManaged()(makeRef(0))),
+      M.bind("currIndex", () => makeManagedRef(0)),
       M.bind("currStream", () =>
-        toManaged()(makeRef<T.Effect<S, R, Option<E>, Array<O>>>(Pull.end))
+        makeManagedRef<T.Effect<S, R, Option<E>, Array<O>>>(Pull.end)
       ),
       M.bind("switchStream", () =>
         M.switchable<S, R, never, T.Effect<S, R, Option<E>, Array<O>>>()
       ),
-      M.map(({ currIndex, currStream, switchStream }) => {
-        const go: T.Effect<S, R, Option<E>, Array<O>> = pipe(
-          currStream.get,
-          T.flatten,
-          T.catchAllCause((x) =>
-            fold_(
-              Cause.sequenceCauseOption(x),
-              () =>
-                pipe(
-                  currIndex,
-                  getAndUpdate((x) => x + 1),
-                  T.chain((i) =>
-                    i >= chunkSize
-                      ? Pull.end
-                      : pipe(
-                          switchStream(streams[i].proc),
-                          T.chain(currStream.set),
-                          T.zipSecond(go)
-                        )
-                  )
-                ),
-              Pull.halt
-            )
-          )
-        )
-        return go
-      })
+      M.map(({ currIndex, currStream, switchStream }) =>
+        go(streams, chunkSize, currIndex, currStream, switchStream)
+      )
     )
   )
 }
