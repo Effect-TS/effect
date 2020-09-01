5281c97ec5033c3117f08252e137ab6acbed83cd
Michael Arnaldi <arnaldimichael@gmail.com>
2020-09-01 18:47:55 +0100

    fix hasURI

--- packages/core/src/Classic/IndexedT/index.ts
+++ packages/core/src/Classic/IndexedT/index.ts
@@ -113,6 +113,95 @@ export interface Indexed<F extends [IxURI, ...HKT.URIS], C extends IxC<any, any>
     HKT.Mix<C, "X", [E2, E]>,
     B
   >
+
+  chain<
+    IO extends C[IxURI]["_O"],
+    N2 extends string,
+    K2,
+    Q2,
+    W2,
+    X2,
+    I2,
+    S2,
+    R2,
+    E2,
+    A,
+    B
+  >(
+    f: (a: A) => HKT.Kind<F, IxCT<C, IO, IO>, N2, K2, Q2, W2, X2, I2, S2, R2, E2, B>
+  ): <N extends string, II extends C[IxURI]["_I"], K, Q, W, X, I, S, R, E>(
+    fa: HKT.Kind<
+      F,
+      IxCT<C, II, IO>,
+      HKT.Intro<C, "N", N2, N>,
+      HKT.Intro<C, "K", K2, K>,
+      HKT.Intro<C, "Q", Q2, Q>,
+      HKT.Intro<C, "W", W2, W>,
+      HKT.Intro<C, "X", X2, X>,
+      HKT.Intro<C, "I", I2, I>,
+      HKT.Intro<C, "S", S2, S>,
+      HKT.Intro<C, "R", R2, R>,
+      HKT.Intro<C, "E", E2, E>,
+      A
+    >
+  ) => HKT.Kind<
+    F,
+    IxCT<C, II, IO>,
+    HKT.Mix<C, "N", [N2, N]>,
+    HKT.Mix<C, "K", [K2, K]>,
+    HKT.Mix<C, "Q", [Q2, Q]>,
+    HKT.Mix<C, "W", [W2, W]>,
+    HKT.Mix<C, "X", [X2, X]>,
+    HKT.Mix<C, "I", [I2, I]>,
+    HKT.Mix<C, "S", [S2, S]>,
+    HKT.Mix<C, "R", [R2, R]>,
+    HKT.Mix<C, "X", [E2, E]>,
+    B
+  >
+
+  chainLower<N2 extends string, K2, Q2, W2, X2, I2, S2, R2, E2, A, B>(
+    f: (a: A) => HKT.Kind<Inner<F>, C, N2, K2, Q2, W2, X2, I2, S2, R2, E2, B>
+  ): <
+    N extends string,
+    II extends C[IxURI]["_I"],
+    IO extends C[IxURI]["_O"],
+    K,
+    Q,
+    W,
+    X,
+    I,
+    S,
+    R,
+    E
+  >(
+    fa: HKT.Kind<
+      F,
+      IxCT<C, II, IO>,
+      HKT.Intro<C, "N", N2, N>,
+      HKT.Intro<C, "K", K2, K>,
+      HKT.Intro<C, "Q", Q2, Q>,
+      HKT.Intro<C, "W", W2, W>,
+      HKT.Intro<C, "X", X2, X>,
+      HKT.Intro<C, "I", I2, I>,
+      HKT.Intro<C, "S", S2, S>,
+      HKT.Intro<C, "R", R2, R>,
+      HKT.Intro<C, "E", E2, E>,
+      A
+    >
+  ) => HKT.Kind<
+    F,
+    IxCT<C, II, IO>,
+    HKT.Mix<C, "N", [N2, N]>,
+    HKT.Mix<C, "K", [K2, K]>,
+    HKT.Mix<C, "Q", [Q2, Q]>,
+    HKT.Mix<C, "W", [W2, W]>,
+    HKT.Mix<C, "X", [X2, X]>,
+    HKT.Mix<C, "I", [I2, I]>,
+    HKT.Mix<C, "S", [S2, S]>,
+    HKT.Mix<C, "R", [R2, R]>,
+    HKT.Mix<C, "X", [E2, E]>,
+    B
+  >
 }
 
 export interface Ix<I, O, A> {
@@ -159,6 +248,17 @@ function indexed_<_I, _O>(
     lift: <II extends _I, IO extends _I>() => <A>(
       fa: HKT.F_<A>
     ): Ix<II, IO, HKT.F_<A>> => makeIx<II, IO>()(fa),
-    lower: () => (fa) => fa._A
+    lower: () => (fa) => fa._A,
+    chain: <IO extends _O, A, B>(f: (a: A) => Ix<IO, IO, HKT.F_<B>>) => <II extends _I>(
+      fa: Ix<II, IO, HKT.F_<A>>
+    ): Ix<II, IO, HKT.F_<B>> =>
+      pipe(
+        fa._A,
+        chainF(F)((a) => f(a)._A),
+        makeIx<II, IO>()
+      ),
+    chainLower: <A, B>(f: (a: A) => HKT.F_<B>) => <II extends _I, IO extends _O>(
+      fa: Ix<II, IO, HKT.F_<A>>
+    ): Ix<II, IO, HKT.F_<B>> => pipe(fa._A, chainF(F)(f), makeIx<II, IO>())
   })
 }
