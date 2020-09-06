10efc3567abe92bd5f4a0b4b151adb80fc22d7de
Nikita Volodin <volodin.n@gmail.com>
2020-09-06 16:34:52 -0400

    add Stream fromQueueWithShutdown()

---
+++ packages/system/src/Stream/Stream/fromQueueWithShutdown.ts
@@ -0,0 +1,13 @@
+import type { XQueue } from "../../Queue"
+import type { AsyncRE } from "./definitions"
+import { ensuringFirst_ } from "./ensuringFirst"
+import { fromQueue } from "./fromQueue"
+
+/**
+ * Creates a stream from a {@link XQueue} of values. The queue will be shutdown once the stream is closed.
+ */
+export function fromQueueWithShutdown<R, E, O>(
+  queue: XQueue<never, R, unknown, E, never, O>
+): AsyncRE<R, E, O> {
+  return ensuringFirst_(fromQueue(queue), queue.shutdown)
+}
