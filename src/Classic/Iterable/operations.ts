eac656bf290de1a5aef6e1b5e3da356b32c80ff9
Michael Arnaldi <arnaldimichael@gmail.com>
2020-09-23 14:01:43 +0100

    rename of to do

--- src/Classic/Iterable/operations.ts
+++ src/Classic/Iterable/operations.ts
@@ -1,5 +1,5 @@
 import { pipe } from "@effect-ts/system/Function"
-import { concat, never, of, reduce } from "@effect-ts/system/Iterable"
+import * as I from "@effect-ts/system/Iterable"
 
 import type { IterableURI } from "../../Modules"
 import * as P from "../../Prelude"
@@ -8,11 +8,11 @@ import { succeedF } from "../../Prelude/DSL"
 export * from "@effect-ts/system/Iterable"
 
 export const foreachF = P.implementForeachF<[IterableURI]>()((_) => (G) => (f) =>
-  reduce(succeedF(G)(never as Iterable<typeof _.B>), (b, a) =>
+  I.reduce(succeedF(G)(I.never as Iterable<typeof _.B>), (b, a) =>
     pipe(
       b,
       G.both(f(a)),
-      G.map(([x, y]) => concat(x, of(y)))
+      G.map(([x, y]) => I.concat(x, I.of(y)))
     )
   )
 )
