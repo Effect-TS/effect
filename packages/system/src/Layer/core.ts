86f6c71f82d60668b29015ead457d22a41fc2a76
Michael Arnaldi <arnaldimichael@gmail.com>
2020-09-27 12:48:02 +0100

    continue with layer instances

--- packages/system/src/Layer/core.ts
+++ packages/system/src/Layer/core.ts
@@ -16,93 +16,106 @@ export { Layer } from "./Layer"
 
 export type AsyncR<R, A> = Layer<unknown, R, never, A>
 
-export const pure = <T>(has: T.Tag<T>) => (resource: T) =>
-  new Layer<never, unknown, never, T.Has<T>>(
-    T.managedChain_(T.fromEffect(T.succeedNow(resource)), (a) => environmentFor(has, a))
-  )
-
-export const prepare = <T>(has: T.Tag<T>) => <S, R, E, A extends T>(
-  acquire: T.Effect<S, R, E, A>
-) => ({
-  open: <S1, R1, E1>(open: (_: A) => T.Effect<S1, R1, E1, any>) => ({
-    release: <S2, R2>(release: (_: A) => T.Effect<S2, R2, never, any>) =>
-      fromManaged(has)(
-        T.managedChain_(
-          T.makeExit_(acquire, (a) => release(a)),
-          (a) => T.fromEffect(T.map_(open(a), () => a))
-        )
+export function pure<T>(has: T.Tag<T>) {
+  return (resource: T) =>
+    new Layer<never, unknown, never, T.Has<T>>(
+      T.managedChain_(T.fromEffect(T.succeedNow(resource)), (a) =>
+        environmentFor(has, a)
       )
-  }),
-  release: <S2, R2>(release: (_: A) => T.Effect<S2, R2, never, any>) =>
-    fromManaged(has)(T.makeExit_(acquire, (a) => release(a)))
-})
+    )
+}
 
-export const create = <T>(has: T.Tag<T>) => ({
-  fromEffect: fromEffect(has),
-  fromManaged: fromManaged(has),
-  pure: pure(has),
-  prepare: prepare(has)
-})
+export function prepare<T>(has: T.Tag<T>) {
+  return <S, R, E, A extends T>(acquire: T.Effect<S, R, E, A>) => ({
+    open: <S1, R1, E1>(open: (_: A) => T.Effect<S1, R1, E1, any>) => ({
+      release: <S2, R2>(release: (_: A) => T.Effect<S2, R2, never, any>) =>
+        fromManaged(has)(
+          T.managedChain_(
+            T.makeExit_(acquire, (a) => release(a)),
+            (a) => T.fromEffect(T.map_(open(a), () => a))
+          )
+        )
+    }),
+    release: <S2, R2>(release: (_: A) => T.Effect<S2, R2, never, any>) =>
+      fromManaged(has)(T.makeExit_(acquire, (a) => release(a)))
+  })
+}
 
-export const fromEffect = <T>(has: T.Tag<T>) => <S, R, E>(
-  resource: T.Effect<S, R, E, T>
-) =>
-  new Layer<S, R, E, T.Has<T>>(
-    T.managedChain_(T.fromEffect(resource), (a) => environmentFor(has, a))
-  )
+export function create<T>(has: T.Tag<T>) {
+  return {
+    fromEffect: fromEffect(has),
+    fromManaged: fromManaged(has),
+    pure: pure(has),
+    prepare: prepare(has)
+  }
+}
 
-export const fromManaged = <T>(has: T.Tag<T>) => <S, R, E>(
-  resource: T.Managed<S, R, E, T>
-) =>
-  new Layer<S, R, E, T.Has<T>>(T.managedChain_(resource, (a) => environmentFor(has, a)))
+export function fromEffect<T>(has: T.Tag<T>) {
+  return <S, R, E>(resource: T.Effect<S, R, E, T>) =>
+    new Layer<S, R, E, T.Has<T>>(
+      T.managedChain_(T.fromEffect(resource), (a) => environmentFor(has, a))
+    )
+}
 
-export const fromRawManaged = <S, R, E, A>(resource: T.Managed<S, R, E, A>) =>
-  new Layer<S, R, E, A>(resource)
+export function fromManaged<T>(has: T.Tag<T>) {
+  return <S, R, E>(resource: T.Managed<S, R, E, T>) =>
+    new Layer<S, R, E, T.Has<T>>(
+      T.managedChain_(resource, (a) => environmentFor(has, a))
+    )
+}
 
-export const fromRawEffect = <S, R, E, A>(resource: T.Effect<S, R, E, A>) =>
-  new Layer<S, R, E, A>(T.fromEffect(resource))
+export function fromRawManaged<S, R, E, A>(resource: T.Managed<S, R, E, A>) {
+  return new Layer<S, R, E, A>(resource)
+}
 
-export const zip_ = <S, R, E, A, S2, R2, E2, A2>(
+export function fromRawEffect<S, R, E, A>(resource: T.Effect<S, R, E, A>) {
+  return new Layer<S, R, E, A>(T.fromEffect(resource))
+}
+
+export function zip_<S, R, E, A, S2, R2, E2, A2>(
   left: Layer<S, R, E, A>,
   right: Layer<S2, R2, E2, A2>
-) =>
-  new Layer<S | S2, R & R2, E | E2, readonly [A, A2]>(
+) {
+  return new Layer<S | S2, R & R2, E | E2, readonly [A, A2]>(
     T.managedChain_(left.build, (l) =>
       T.managedChain_(right.build, (r) =>
         T.fromEffect(T.effectTotal(() => tuple(l, r)))
       )
     )
   )
+}
 
-export const zip = <S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) => <S, R, E, A>(
-  left: Layer<S, R, E, A>
-) => zip_(left, right)
+export function zip<S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) {
+  return <S, R, E, A>(left: Layer<S, R, E, A>) => zip_(left, right)
+}
 
-export const merge_ = <S, R, E, A, S2, R2, E2, A2>(
+export function merge_<S, R, E, A, S2, R2, E2, A2>(
   left: Layer<S, R, E, A>,
   right: Layer<S2, R2, E2, A2>
-) =>
-  new Layer<S | S2, R & R2, E | E2, A & A2>(
+) {
+  return new Layer<S | S2, R & R2, E | E2, A & A2>(
     T.managedChain_(left.build, (l) =>
       T.managedChain_(right.build, (r) =>
         T.fromEffect(T.effectTotal(() => ({ ...l, ...r })))
       )
     )
   )
+}
 
-export const merge = <S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) => <S, R, E, A>(
-  left: Layer<S, R, E, A>
-) => merge_(left, right)
+export function merge<S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) {
+  return <S, R, E, A>(left: Layer<S, R, E, A>) => merge_(left, right)
+}
 
-export const using = <S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) => <S, R, E, A>(
-  left: Layer<S, R, E, A>
-) => using_<S, R, E, A, S2, R2, E2, A2>(left, right)
+export function using<S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) {
+  return <S, R, E, A>(left: Layer<S, R, E, A>) =>
+    using_<S, R, E, A, S2, R2, E2, A2>(left, right)
+}
 
-export const using_ = <S, R, E, A, S2, R2, E2, A2>(
+export function using_<S, R, E, A, S2, R2, E2, A2>(
   left: Layer<S, R, E, A>,
   right: Layer<S2, R2, E2, A2>
-) =>
-  new Layer<S | S2, Erase<R, A2> & R2, E | E2, A & A2>(
+) {
+  return new Layer<S | S2, Erase<R, A2> & R2, E | E2, A & A2>(
     T.managedChain_(right.build, (a2) =>
       T.managedMap_(
         T.managedProvideSome_(left.build, (r0: R) => ({
@@ -113,16 +126,18 @@ export const using_ = <S, R, E, A, S2, R2, E2, A2>(
       )
     )
   )
+}
 
-export const consuming = <S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) => <S, R, E, A>(
-  left: Layer<S, R & A2, E, A>
-) => consuming_<S, R, E, A, S2, R2, E2, A2>(left, right)
+export function consuming<S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) {
+  return <S, R, E, A>(left: Layer<S, R & A2, E, A>) =>
+    consuming_<S, R, E, A, S2, R2, E2, A2>(left, right)
+}
 
-export const consuming_ = <S, R, E, A, S2, R2, E2, A2>(
+export function consuming_<S, R, E, A, S2, R2, E2, A2>(
   left: Layer<S, R & A2, E, A>,
   right: Layer<S2, R2, E2, A2>
-) =>
-  new Layer<S | S2, R & R2, E | E2, A & A2>(
+) {
+  return new Layer<S | S2, R & R2, E | E2, A & A2>(
     T.managedChain_(right.build, (a2) =>
       T.managedMap_(
         T.managedProvideSome_(left.build, (r0: R & R2) => ({
@@ -133,21 +148,23 @@ export const consuming_ = <S, R, E, A, S2, R2, E2, A2>(
       )
     )
   )
+}
 
-export const mergePar = <S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) => <S, R, E, A>(
-  left: Layer<S, R, E, A>
-) => mergePar_(left, right)
+export function mergePar<S2, R2, E2, A2>(right: Layer<S2, R2, E2, A2>) {
+  return <S, R, E, A>(left: Layer<S, R, E, A>) => mergePar_(left, right)
+}
 
-export const mergePar_ = <S, R, E, A, S2, R2, E2, A2>(
+export function mergePar_<S, R, E, A, S2, R2, E2, A2>(
   left: Layer<S, R, E, A>,
   right: Layer<S2, R2, E2, A2>
-) =>
-  new Layer<unknown, R & R2, E | E2, A & A2>(
+) {
+  return new Layer<unknown, R & R2, E | E2, A & A2>(
     T.managedChain_(
       T.managedZipWithPar_(left.build, right.build, (a, b) => [a, b] as const),
       ([l, r]) => T.fromEffect(T.effectTotal(() => ({ ...l, ...r })))
     )
   )
+}
 
 export type MergeS<Ls extends Layer<any, any, any, any>[]> = {
   [k in keyof Ls]: [Ls[k]] extends [Layer<infer X, any, any, any>] ? X : never
@@ -177,35 +194,39 @@ export type MergeA<Ls extends Layer<any, any, any, any>[]> = UnionToIntersection
   }[number]
 >
 
-export const all = <Ls extends Layer<any, any, any, any>[]>(
+export function all<Ls extends Layer<any, any, any, any>[]>(
   ...ls: Ls & { 0: Layer<any, any, any, any> }
-): Layer<MergeS<Ls>, MergeR<Ls>, MergeE<Ls>, MergeA<Ls>> =>
-  new Layer(
+): Layer<MergeS<Ls>, MergeR<Ls>, MergeE<Ls>, MergeA<Ls>> {
+  return new Layer(
     T.managedMap_(
       T.managedForeach_(ls, (l) => l.build),
       (ps) => reduce_(ps, {} as any, (b, a) => ({ ...b, ...a }))
     )
   )
+}
 
-export const allPar = <Ls extends Layer<any, any, any, any>[]>(
+export function allPar<Ls extends Layer<any, any, any, any>[]>(
   ...ls: Ls & { 0: Layer<any, any, any, any> }
-): Layer<unknown, MergeR<Ls>, MergeE<Ls>, MergeA<Ls>> =>
-  new Layer(
+): Layer<unknown, MergeR<Ls>, MergeE<Ls>, MergeA<Ls>> {
+  return new Layer(
     T.managedMap_(
       T.foreachPar_(ls, (l) => l.build),
       (ps) => reduce_(ps, {} as any, (b, a) => ({ ...b, ...a }))
     )
   )
+}
 
-export const allParN = (n: number) => <Ls extends Layer<any, any, any, any>[]>(
-  ...ls: Ls & { 0: Layer<any, any, any, any> }
-): Layer<unknown, MergeR<Ls>, MergeE<Ls>, MergeA<Ls>> =>
-  new Layer(
-    T.managedMap_(
-      T.foreachParN_(n)(ls, (l) => l.build),
-      (ps) => reduce_(ps, {} as any, (b, a) => ({ ...b, ...a }))
+export function allParN(n: number) {
+  return <Ls extends Layer<any, any, any, any>[]>(
+    ...ls: Ls & { 0: Layer<any, any, any, any> }
+  ): Layer<unknown, MergeR<Ls>, MergeE<Ls>, MergeA<Ls>> =>
+    new Layer(
+      T.managedMap_(
+        T.foreachParN_(n)(ls, (l) => l.build),
+        (ps) => reduce_(ps, {} as any, (b, a) => ({ ...b, ...a }))
+      )
     )
-  )
+}
 
 function environmentFor<T>(
   has: T.Tag<T>,
@@ -222,25 +243,29 @@ function environmentFor<T>(has: T.Tag<T>, a: T): T.Managed<never, unknown, never
 /**
  * Type level bound to make sure a layer is complete
  */
-export const main = <S, E, A>(layer: Layer<S, DefaultEnv, E, A>) => layer
+export function main<S, E, A>(layer: Layer<S, DefaultEnv, E, A>) {
+  return layer
+}
 
 /**
  * Embed the requird environment in a region
  */
-export const region = <K, T>(h: T.Tag<T.Region<T, K>>) => <S, R, E>(
-  _: Layer<S, R, E, T>
-): Layer<S, R, E, T.Has<T.Region<T, K>>> =>
-  pipe(
-    fromRawEffect(T.access((r: T): T.Has<T.Region<T, K>> => ({ [h.key]: r } as any))),
-    consuming(_)
-  )
+export function region<K, T>(h: T.Tag<T.Region<T, K>>) {
+  return <S, R, E>(_: Layer<S, R, E, T>): Layer<S, R, E, T.Has<T.Region<T, K>>> =>
+    pipe(
+      fromRawEffect(T.access((r: T): T.Has<T.Region<T, K>> => ({ [h.key]: r } as any))),
+      consuming(_)
+    )
+}
 
 /**
  * Converts a layer to a managed runtime
  */
-export const toRuntime = <S, R, E, A>(
+export function toRuntime<S, R, E, A>(
   _: Layer<S, R, E, A>
-): Managed<S, R, E, Runtime<A>> => T.managedMap_(_.build, makeRuntime)
+): Managed<S, R, E, Runtime<A>> {
+  return T.managedMap_(_.build, makeRuntime)
+}
 
 /**
  * A default memoMap is included in DefaultEnv,
@@ -258,18 +283,64 @@ export const memoMap = create(HasMemoMap).fromEffect(
 /**
  * Memoize the current layer using a MemoMap
  */
-export const memo = <S, R, E, A>(
+export function memo<S, R, E, A>(
   layer: Layer<S, R, E, A>
-): Layer<unknown, T.Has<MemoMap> & R, E, A> =>
-  pipe(
+): Layer<unknown, T.Has<MemoMap> & R, E, A> {
+  return pipe(
     T.fromEffect(readService(HasMemoMap)),
     T.managedChain((m) => m.getOrElseMemoize(layer)),
     fromRawManaged
   )
+}
 
 /**
  * Returns a fresh version of a potentially memoized layer,
  * note that this will override the memoMap for the layer and its children
  */
-export const fresh = <S, R, E, A>(layer: Layer<S, R, E, A>): Layer<S, R, E, A> =>
-  pipe(layer, consuming(memoMap))
+export function fresh<S, R, E, A>(layer: Layer<S, R, E, A>): Layer<S, R, E, A> {
+  return pipe(layer, consuming(memoMap))
+}
+
+/**
+ * Maps the output of the layer using f
+ */
+export function map<A, B>(f: (a: A) => B) {
+  return <S, R, E>(fa: Layer<S, R, E, A>): Layer<S, R, E, B> => map_(fa, f)
+}
+
+/**
+ * Maps the output of the layer using f
+ */
+export function map_<S, R, E, A, B>(
+  fa: Layer<S, R, E, A>,
+  f: (a: A) => B
+): Layer<S, R, E, B> {
+  return new Layer(T.managedMap_(fa.build, f))
+}
+
+/**
+ * Chains the output of the layer using f
+ */
+export function chain<S2, R2, E2, A, B>(f: (a: A) => Layer<S2, R2, E2, B>) {
+  return <S, R, E>(fa: Layer<S, R, E, A>): Layer<S | S2, R & R2, E | E2, B> =>
+    chain_(fa, f)
+}
+
+/**
+ * Chains the output of the layer using f
+ */
+export function chain_<S, R, E, A, S2, R2, E2, B>(
+  fa: Layer<S, R, E, A>,
+  f: (a: A) => Layer<S2, R2, E2, B>
+) {
+  return new Layer(T.managedChain_(fa.build, (x) => f(x).build))
+}
+
+/**
+ * Flatten `Layer<S, R, E, Layer<S2, R2, E2, A>>`
+ */
+export function flatten<S, R, E, A, S2, R2, E2, B>(
+  ffa: Layer<S, R, E, Layer<S2, R2, E2, B>>
+): Layer<S | S2, R & R2, E | E2, B> {
+  return new Layer(T.managedChain_(ffa.build, (i) => i.build))
+}
