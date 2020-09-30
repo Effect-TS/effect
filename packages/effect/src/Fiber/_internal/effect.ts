c0a4d8634e4c6cd928de5802d384be47e0e34da5
Michael Arnaldi <arnaldimichael@gmail.com>
2020-09-30 19:39:09 +0100

    continue

--- packages/effect/src/Fiber/_internal/effect.ts
+++ packages/effect/src/Fiber/_internal/effect.ts
@@ -1,7 +1,22 @@
 export { asUnit } from "../../Effect/asUnit"
-export { chain_, halt, succeed, unit } from "../../Effect/core"
+export { bracket_ } from "../../Effect/bracket_"
+export {
+  chain,
+  chain_,
+  effectTotal,
+  halt,
+  succeed,
+  suspend,
+  unit
+} from "../../Effect/core"
+export { forkDaemon } from "../../Effect/core-scope"
+export { die } from "../../Effect/die"
 export { done } from "../../Effect/done"
-export { IO, UIO } from "../../Effect/effect"
+export { Effect, IO, RIO, UIO, _I } from "../../Effect/effect"
+export { effectAsync } from "../../Effect/effectAsync"
+export { effectAsyncInterrupt } from "../../Effect/effectAsyncInterrupt"
+export { effectMaybeAsyncInterrupt } from "../../Effect/effectMaybeAsyncInterrupt"
+export { fail } from "../../Effect/fail"
 export { fiberId } from "../../Effect/fiberId"
 export { foldLeft, foldLeft_ } from "../../Effect/foldLeft"
 export { foreach } from "../../Effect/foreach"
@@ -9,10 +24,11 @@ export { foreachPar } from "../../Effect/foreachPar"
 export { foreachPar_ } from "../../Effect/foreachPar_"
 export { foreachUnit_ } from "../../Effect/foreachUnit_"
 export { foreach_ } from "../../Effect/foreach_"
+export { interruptAs } from "../../Effect/interruptAs"
 export { map } from "../../Effect/map"
 export { map_ } from "../../Effect/map_"
+export type { IFold, Instruction, IRaceWith } from "../../Effect/primitives"
 export { result } from "../../Effect/result"
-export { forkDaemon } from "../../Effect/core-scope"
 export { tap_ } from "../../Effect/tap_"
 export { zipWithPar_ } from "../../Effect/zipWithPar_"
 export { zipWith_ } from "../../Effect/zipWith_"
