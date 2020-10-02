9118d308b086d2bc2d8c75dece409405889887f9
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-02 22:37:04 +0100

    discriminate associative/closure

--- packages/core/src/Classic/Commutative/operations.ts
+++ packages/core/src/Classic/Commutative/operations.ts
@@ -1,6 +1,8 @@
 import type { Commutative } from "./definition"
 
 export const makeCommutative = <A>(f: (r: A) => (l: A) => A): Commutative<A> => ({
+  Associative: "Associative",
+  Commutative: "Commutative",
   combine: f,
   commute: (y) => (x) => f(x)(y)
 })
