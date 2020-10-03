c89f7a07d8296f66f79777f7eb16b0bc0476b65c
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-03 22:48:33 +0100

    add common ops

--- src/Classic/Ordering/operations.ts
+++ src/Classic/Ordering/operations.ts
@@ -2,8 +2,8 @@ import { pipe } from "@effect-ts/system/Function"
 
 import { Ordering } from "./definition"
 
-export const toNumber = (o: Ordering) =>
-  pipe(Ordering.unwrap(o), (o) => {
+export function toNumber(o: Ordering) {
+  return pipe(Ordering.unwrap(o), (o) => {
     switch (o) {
       case "eq": {
         return 0
@@ -16,3 +16,16 @@ export const toNumber = (o: Ordering) =>
       }
     }
   })
+}
+
+export function fromNumber(n: number) {
+  if (n < 0) {
+    return Ordering.wrap("lt")
+  }
+  if (n > 0) {
+    return Ordering.wrap("gt")
+  }
+  if (n === 0) {
+    return Ordering.wrap("eq")
+  }
+}
