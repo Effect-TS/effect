5efada6b3d9e029579076a33d9aa2054d664b8bd
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-04 17:42:14 +0100

    stream merge functions

--- packages/system/src/Effect/toManaged.ts
+++ packages/system/src/Effect/toManaged.ts
@@ -2,9 +2,7 @@ import { fromEffect, makeExit_ } from "../Managed/core"
 import type { Managed } from "../Managed/managed"
 import type { Effect } from "./effect"
 
-export function toManaged<A = unknown, S = never, R = unknown>(
-  release?: (a: A) => Effect<R, never, any>
-) {
-  return <R1, E1, A1 extends A>(self: Effect<R1, E1, A1>): Managed<R1 & R, E1, A1> =>
+export function toManaged<A, R = unknown>(release?: (a: A) => Effect<R, never, any>) {
+  return <R1, E1>(self: Effect<R1, E1, A>): Managed<R1 & R, E1, A> =>
     release ? makeExit_(self, (a) => release(a)) : fromEffect(self)
 }
---
+++ packages/system/src/Stream/Stream/mergeWith.ts
@@ -0,0 +1,160 @@
+import * as A from "../../Array"
+import type { Cause } from "../../Cause"
+import { sequenceCauseOption } from "../../Cause"
+import * as T from "../../Effect"
+import type { Either } from "../../Either"
+import { left, right } from "../../Either"
+import * as Exit from "../../Exit"
+import * as Fiber from "../../Fiber"
+import { identity, pipe } from "../../Function"
+import * as M from "../../Managed"
+import * as O from "../../Option"
+import * as RefM from "../../RefM"
+import * as H from "../Handoff"
+import type * as Pull from "../Pull"
+import * as Take from "../Take"
+import { Stream } from "./definitions"
+
+export type TerminationStrategy = "Left" | "Right" | "Both" | "Either"
+
+/**
+ * Merges this stream and the specified stream together to a common element
+ * type with the specified mapping functions.
+ *
+ * New produced stream will terminate when both specified stream terminate if
+ * no termination strategy is specified.
+ */
+export function mergeWith_<R, E, A, R1, E1, B, C, C1>(
+  self: Stream<R, E, A>,
+  that: Stream<R1, E1, B>,
+  l: (a: A) => C,
+  r: (b: B) => C1,
+  strategy: TerminationStrategy = "Both"
+): Stream<R1 & R, E | E1, C | C1> {
+  return new Stream(
+    pipe(
+      M.do,
+      M.bind("handoff", () => M.fromEffect(H.make<Take.Take<E | E1, C | C1>>())),
+      M.bind("done", () => M.fromEffect(RefM.makeRefM<O.Option<boolean>>(O.none))),
+      M.bind("chunksL", () => self.proc),
+      M.bind("chunksR", () => that.proc),
+      M.let(
+        "handler",
+        ({ done, handoff }) => (
+          pull: Pull.Pull<R & R1, E | E1, C | C1>,
+          terminate: boolean
+        ) =>
+          pipe(
+            done.get,
+            T.chain((o) => {
+              if (o._tag === "Some" && o.value) {
+                return T.succeed(false)
+              } else {
+                return pipe(
+                  pull,
+                  T.result,
+                  T.chain((exit) =>
+                    pipe(
+                      done,
+                      RefM.modify((o) => {
+                        const causeOrChunk = pipe(
+                          exit,
+                          Exit.fold(
+                            (c): Either<O.Option<Cause<E | E1>>, A.Array<C | C1>> =>
+                              left(sequenceCauseOption(c)),
+                            right
+                          )
+                        )
+
+                        if (o._tag === "Some" && o.value) {
+                          return T.succeed([false, o])
+                        } else if (causeOrChunk._tag === "Right") {
+                          return pipe(
+                            handoff,
+                            H.offer(
+                              <Take.Take<E | E1, C | C1>>Take.chunk(causeOrChunk.right)
+                            ),
+                            T.as([true, o])
+                          )
+                        } else if (
+                          causeOrChunk._tag === "Left" &&
+                          causeOrChunk.left._tag === "Some"
+                        ) {
+                          return pipe(
+                            handoff,
+                            H.offer(
+                              <Take.Take<E | E1, C | C1>>(
+                                Take.halt(causeOrChunk.left.value)
+                              )
+                            ),
+                            T.as([false, O.some(true)])
+                          )
+                        } else if (
+                          causeOrChunk._tag === "Left" &&
+                          causeOrChunk.left._tag === "None" &&
+                          (terminate || o._tag === "Some")
+                        ) {
+                          return pipe(
+                            handoff,
+                            H.offer(<Take.Take<E | E1, C | C1>>Take.end),
+                            T.as([false, O.some(true)])
+                          )
+                        } else {
+                          return T.succeed([false, O.some(false)])
+                        }
+                      })
+                    )
+                  )
+                )
+              }
+            }),
+            T.repeatWhile(identity),
+            T.fork,
+            T.interruptible,
+            T.toManaged(Fiber.interrupt)
+          )
+      ),
+      M.tap(({ chunksL, handler }) =>
+        handler(
+          pipe(chunksL, T.map(A.map(l))),
+          strategy === "Left" || strategy === "Either"
+        )
+      ),
+      M.tap(({ chunksR, handler }) =>
+        handler(
+          pipe(chunksR, T.map(A.map(r))),
+          strategy === "Right" || strategy === "Either"
+        )
+      ),
+      M.map(({ done, handoff }) =>
+        pipe(
+          T.do,
+          T.bind("done", () => done.get),
+          T.bind("take", (s) =>
+            s.done._tag === "Some" && s.done.value
+              ? pipe(handoff, H.poll, T.some)
+              : pipe(handoff, H.take)
+          ),
+          T.bind("result", ({ take }) => Take.done(take)),
+          T.map(({ result }) => result)
+        )
+      )
+    )
+  )
+}
+
+/**
+ * Merges this stream and the specified stream together to a common element
+ * type with the specified mapping functions.
+ *
+ * New produced stream will terminate when both specified stream terminate if
+ * no termination strategy is specified.
+ */
+export function mergeWith<R, E, A, R1, E1, B, C, C1>(
+  that: Stream<R1, E1, B>,
+  l: (a: A) => C,
+  r: (b: B) => C1,
+  strategy: TerminationStrategy = "Both"
+) {
+  return (self: Stream<R, E, A>) => mergeWith_(self, that, l, r, strategy)
+}
