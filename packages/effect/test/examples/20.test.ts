03ebf971fb0687f7b2013e74883ed9e84b21d273
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-04 20:18:02 +0100

    implement tupled

---
+++ packages/effect/test/examples/20.test.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import { pipe } from "@effect-ts/system/Function"
+
+import * as E from "../../src/Classic/Either"
+import * as ReaderT from "../../src/Classic/ReaderT"
+import * as DSL from "../../src/Prelude/DSL"
+
+namespace ReaderEither {
+  export const Monad = ReaderT.monad(E.Monad)
+  export const Access = ReaderT.access(E.Monad)
+  export const Fail = ReaderT.fail({ ...E.Monad, ...E.Fail })
+  export const Applicative = ReaderT.applicative(E.Applicative)
+
+  export const chain = DSL.chainF(Monad)
+  export const succeed = DSL.succeedF(Monad)
+
+  export const { access } = Access
+  export const { fail } = Fail
+
+  export const tupled = DSL.tupledF(Applicative)
+}
+
+test("20", () => {
+  expect(
+    pipe(
+      ReaderEither.tupled(
+        ReaderEither.access((n: { n: number }) => n.n),
+        ReaderEither.access((s: { s: string }) => s.s)
+      ),
+      (f) =>
+        f({
+          n: 1,
+          s: "s"
+        }),
+      (e) => e
+    )
+  ).toEqual(E.right([1, "s"]))
+})
