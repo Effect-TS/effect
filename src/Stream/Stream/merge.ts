8ba6e1aba022d60af0c6abc138000960d89da826
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-04 17:42:14 +0100

    stream merge functions

---
+++ src/Stream/Stream/merge.ts
@@ -0,0 +1,119 @@
+import type { Either } from "../../Either"
+import { left, right } from "../../Either"
+import type { Stream } from "./definitions"
+import type { TerminationStrategy } from "./mergeWith"
+import { mergeWith_ } from "./mergeWith"
+
+/**
+ * Merges this stream and the specified stream together.
+ *
+ * New produced stream will terminate when both specified stream terminate if no termination
+ * strategy is specified.
+ */
+export function merge_<R, E, A, R1, E1, B>(
+  self: Stream<R, E, A>,
+  that: Stream<R1, E1, B>,
+  strategy: TerminationStrategy = "Both"
+): Stream<R1 & R, E | E1, A | B> {
+  return mergeWith_(
+    self,
+    that,
+    (a): A | B => a,
+    (b) => b,
+    strategy
+  )
+}
+
+/**
+ * Merges this stream and the specified stream together. New produced stream will
+ * terminate when either stream terminates.
+ */
+export function mergeTerminateEither_<R, E, A, R1, E1, B>(
+  self: Stream<R, E, A>,
+  that: Stream<R1, E1, B>
+): Stream<R1 & R, E | E1, A | B> {
+  return merge_(self, that, "Either")
+}
+
+/**
+ * Merges this stream and the specified stream together. New produced stream will
+ * terminate when this stream terminates.
+ */
+export function mergeTerminateLeft_<R, E, A, R1, E1, B>(
+  self: Stream<R, E, A>,
+  that: Stream<R1, E1, B>
+): Stream<R1 & R, E | E1, A | B> {
+  return merge_(self, that, "Left")
+}
+
+/**
+ * Merges this stream and the specified stream together. New produced stream will
+ * terminate when the specified stream terminates.
+ */
+export function mergeTerminateRight_<R, E, A, R1, E1, B>(
+  self: Stream<R, E, A>,
+  that: Stream<R1, E1, B>
+): Stream<R1 & R, E | E1, A | B> {
+  return merge_(self, that, "Right")
+}
+
+/**
+ * Merges this stream and the specified stream together to produce a stream of
+ * eithers.
+ */
+export function mergeEither_<R, E, A, R1, E1, B>(
+  self: Stream<R, E, A>,
+  that: Stream<R1, E1, B>,
+  strategy: TerminationStrategy = "Both"
+): Stream<R & R1, E | E1, Either<A, B>> {
+  return mergeWith_(self, that, left, right, strategy)
+}
+
+/**
+ * Merges this stream and the specified stream together.
+ *
+ * New produced stream will terminate when both specified stream terminate if no termination
+ * strategy is specified.
+ */
+export function merge<R1, E1, B>(
+  that: Stream<R1, E1, B>,
+  strategy: TerminationStrategy = "Both"
+) {
+  return <R, E, A>(self: Stream<R, E, A>) => merge_(self, that, strategy)
+}
+
+/**
+ * Merges this stream and the specified stream together to produce a stream of
+ * eithers.
+ */
+export function mergeEither<R1, E1, B>(
+  that: Stream<R1, E1, B>,
+  strategy: TerminationStrategy = "Both"
+) {
+  return <R, E, A>(self: Stream<R, E, A>): Stream<R & R1, E | E1, Either<A, B>> =>
+    mergeEither_(self, that, strategy)
+}
+
+/**
+ * Merges this stream and the specified stream together. New produced stream will
+ * terminate when either stream terminates.
+ */
+export function mergeTerminateEither<R1, E1, B>(that: Stream<R1, E1, B>) {
+  return <R, E, A>(self: Stream<R, E, A>) => merge_(self, that, "Either")
+}
+
+/**
+ * Merges this stream and the specified stream together. New produced stream will
+ * terminate when this stream terminates.
+ */
+export function mergeTerminateLeft<R1, E1, B>(that: Stream<R1, E1, B>) {
+  return <R, E, A>(self: Stream<R, E, A>) => merge_(self, that, "Left")
+}
+
+/**
+ * Merges this stream and the specified stream together. New produced stream will
+ * terminate when the specified stream terminates.
+ */
+export function mergeTerminateRight<R1, E1, B>(that: Stream<R1, E1, B>) {
+  return <R, E, A>(self: Stream<R, E, A>) => merge_(self, that, "Right")
+}
