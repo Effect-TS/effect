f6f6b54eb9a6b97bac24a86a6085721a333d9da0
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-05 21:53:20 +0100

    add small test

---
+++ packages/monocle/test/lens.test.ts
@@ -0,0 +1,32 @@
+import { some } from "@effect-ts/core/Classic/Option"
+import { pipe } from "@effect-ts/core/Function"
+
+import * as L from "../src/Lens"
+import * as O from "../src/Optional"
+
+interface Person {
+  name: {
+    first?: string
+    last?: string
+  }
+}
+
+describe("Lens", () => {
+  it("access first", () => {
+    const firstName = pipe(
+      L.id<Person>(),
+      L.prop("name"),
+      L.asOptional,
+      O.prop("first"),
+      O.fromNullable
+    )
+
+    expect(
+      pipe(
+        <Person>{ name: { first: "Mike", last: "Arnaldi" } },
+        firstName.set("Updated"),
+        firstName.getOption
+      )
+    ).toEqual(some("Updated"))
+  })
+})
