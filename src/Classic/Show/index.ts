1e1f28f6468bd5b89431401fdb0e214bb9f6685a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-05 16:24:50 +0100

    add show instances

--- src/Classic/Show/index.ts
+++ src/Classic/Show/index.ts
@@ -1,18 +1,2 @@
-export { ShowURI } from "../../Modules"
-
-/**
- * `Show[A]` provides implicit evidence that values of type `A` have a total
- * ordering.
- */
-export interface Show<A> {
-  readonly show: (x: A) => string
-}
-
-/**
- * Creates Show[A] from equals & compare functions
- */
-export function makeShow<A>(show: (x: A) => string): Show<A> {
-  return {
-    show
-  }
-}
+export * from "./definitions"
+export * from "./operations"
