2b22a5b9a1e8069fde59a41ecb9d17a2e39b1eeb
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-06 23:37:33 +0100

    encode type lambas

--- packages/effect/src/Prelude/AssociativeCompose/index.ts
+++ packages/effect/src/Prelude/AssociativeCompose/index.ts
@@ -1,4 +1,5 @@
 import type * as HKT from "../HKT"
+import type { AlternativeURI } from "../HKT/hkt"
 
 export interface AssociativeCompose<F extends HKT.URIS, TC = HKT.Auto>
   extends HKT.Base<F, TC> {
@@ -15,7 +16,7 @@ export interface AssociativeCompose<F extends HKT.URIS, TC = HKT.Auto>
     R = HKT.Initial<TC, "R">,
     E = HKT.Initial<TC, "E">
   >(
-    ab: HKT.Kind<F, TC, N, K, Q, W, X, B, S, R, E, C>
+    ab: HKT.Kind<F, TC, N, K, Q, W, X, B, S, R, E, C, AlternativeURI["Category"]>
   ) => <
     A,
     N2 extends string = HKT.Initial<TC, "N">,
@@ -23,7 +24,6 @@ export interface AssociativeCompose<F extends HKT.URIS, TC = HKT.Auto>
     Q2 = HKT.Initial<TC, "Q">,
     W2 = HKT.Initial<TC, "W">,
     X2 = HKT.Initial<TC, "X">,
-    I2 = HKT.Initial<TC, "I">,
     S2 = HKT.Initial<TC, "S">,
     R2 = HKT.Initial<TC, "R">,
     E2 = HKT.Initial<TC, "E">
@@ -40,7 +40,8 @@ export interface AssociativeCompose<F extends HKT.URIS, TC = HKT.Auto>
       HKT.Intro<TC, "S", S, S2>,
       HKT.Intro<TC, "R", R, R2>,
       HKT.Intro<TC, "E", E, E2>,
-      B
+      B,
+      AlternativeURI["Category"]
     >
   ) => HKT.Kind<
     F,
@@ -54,6 +55,7 @@ export interface AssociativeCompose<F extends HKT.URIS, TC = HKT.Auto>
     HKT.Mix<TC, "S", [S, S2]>,
     HKT.Mix<TC, "R", [R, R2]>,
     HKT.Mix<TC, "E", [E, E2]>,
-    C
+    C,
+    AlternativeURI["Category"]
   >
 }
