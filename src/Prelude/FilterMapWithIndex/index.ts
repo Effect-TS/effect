02a4b3904e08bca400970b1e2c65f18beab743a8
0x706b <peter@peterkrol.com>
2020-10-07 07:43:55 -0400

    Make requested changes

---
+++ src/Prelude/FilterMapWithIndex/index.ts
@@ -0,0 +1,14 @@
+import type { Option } from "../../Classic/Option"
+import type * as HKT from "../HKT"
+
+export interface FilterMapWithIndex<F extends HKT.URIS, C = HKT.Auto>
+  extends HKT.Base<F, C> {
+  readonly filterMapWithIndex: <N extends string, K, A, B>(
+    f: (
+      k: HKT.IndexFor<F, HKT.OrFix<"N", C, N>, HKT.OrFix<"K", C, K>>,
+      a: A
+    ) => Option<B>
+  ) => <Q, W, X, I, S, R, E>(
+    fa: HKT.Kind<F, C, N, K, Q, W, X, I, S, R, E, A>
+  ) => HKT.Kind<F, C, N, K, Q, W, X, I, S, R, E, B>
+}
