a40132dd3f0f0a6073b11841c7bf18bfc179e233
0x706b <peter@peterkrol.com>
2020-10-07 07:43:55 -0400

    Make requested changes

---
+++ packages/core/src/Prelude/PartitionMapWithIndex/index.ts
@@ -0,0 +1,18 @@
+import type { Either } from "../../Classic/Either"
+import type { Separated } from "../../Utils"
+import type * as HKT from "../HKT"
+
+export interface PartitionMapWithIndex<F extends HKT.URIS, C = HKT.Auto>
+  extends HKT.Base<F, C> {
+  readonly partitionMapWithIndex: <N extends string, K, A, B, B1>(
+    f: (
+      k: HKT.IndexFor<F, HKT.OrFix<"N", C, N>, HKT.OrFix<"K", C, K>>,
+      a: A
+    ) => Either<B, B1>
+  ) => <Q, W, X, I, S, R, E>(
+    fa: HKT.Kind<F, C, N, K, Q, W, X, I, S, R, E, A>
+  ) => Separated<
+    HKT.Kind<F, C, N, K, Q, W, X, I, S, R, E, B>,
+    HKT.Kind<F, C, N, K, Q, W, X, I, S, R, E, B1>
+  >
+}
