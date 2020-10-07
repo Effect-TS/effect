e194cbc0446194aa74bdfc34087423fc726f90b1
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-07 13:09:37 +0100

    Show interpreter

--- src/Classic/Option/operations.ts
+++ src/Classic/Option/operations.ts
@@ -1,11 +1,18 @@
 import * as O from "@effect-ts/system/Option"
 
 import type { Equal } from "../Equal"
+import type { Show } from "../Show"
 
 export function getEqual<A>(E: Equal<A>): Equal<O.Option<A>> {
   return {
     equals: (y) => (x) =>
       x === y ||
       (O.isNone(x) ? O.isNone(y) : O.isNone(y) ? false : E.equals(y.value)(x.value))
+  }
+}
+
+export function getShow<A>(S: Show<A>): Show<O.Option<A>> {
+  return {
+    show: (ma) => (O.isNone(ma) ? "none" : `some(${S.show(ma.value)})`)
   }
 }
