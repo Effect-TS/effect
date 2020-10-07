e468dc34c53183dd8abfc00d8e76919766c9ed1c
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-07 22:29:10 +0100

    Reduce FilterableWithIndex Dependencies, add Array instanced

--- src/Prelude/FilterableWithIndex/index.ts
+++ src/Prelude/FilterableWithIndex/index.ts
@@ -1,17 +1,13 @@
-import type { Compactable } from "../Compactable"
-import type { CovariantWithIndex } from "../CovariantWithIndex"
 import type { FilterMapWithIndex } from "../FilterMapWithIndex"
 import type { FilterWithIndex } from "../FilterWithIndex"
 import type * as HKT from "../HKT"
 import type { PartitionMapWithIndex } from "../PartitionMapWithIndex"
 import type { PartitionWithIndex } from "../PartitionWithIndex"
 
-export type FilterableWithIndex<F extends HKT.URIS, C = HKT.Auto> = CovariantWithIndex<
+export type FilterableWithIndex<F extends HKT.URIS, C = HKT.Auto> = FilterWithIndex<
   F,
   C
 > &
-  Compactable<F, C> &
-  FilterWithIndex<F, C> &
   PartitionWithIndex<F, C> &
   FilterMapWithIndex<F, C> &
   PartitionMapWithIndex<F, C>
