458f8332a8e9afabac33b0ef5f92802f86f63f01
0x706b <peter@peterkrol.com>
2020-10-07 16:02:38 -0400

    Fix typo

--- packages/core/src/Prelude/FilterMap/index.ts
+++ packages/core/src/Prelude/FilterMap/index.ts
@@ -2,7 +2,7 @@ import type { Option } from "../../Classic/Option"
 import type * as HKT from "../HKT"
 
 export interface FilterMap<F extends HKT.URIS, C = HKT.Auto> extends HKT.Base<F, C> {
-  readonly filtermap: <A, B>(
+  readonly filterMap: <A, B>(
     f: (a: A) => Option<B>
   ) => <N extends string, K, Q, W, X, I, S, R, E>(
     fa: HKT.Kind<F, C, N, K, Q, W, X, I, S, R, E, A>
