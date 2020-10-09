9a16aaa63083e0e6a97655d298f7d8c3e83af2cd
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-09 01:06:05 +0100

    remove duplicated of sandbox-unsandbox

--- src/Effect/cause.ts
+++ src/Effect/cause.ts
@@ -1,9 +1,7 @@
 import type { Cause } from "../Cause/cause"
 import { Empty } from "../Cause/cause"
-import { foldCauseM_, halt, succeed } from "./core"
+import { foldCauseM_, succeed } from "./core"
 import type { Effect, RIO } from "./effect"
-import { fail } from "./fail"
-import { foldM_ } from "./foldM_"
 
 /**
  * Returns an effect that succeeds with the cause of failure of this effect,
@@ -11,20 +9,4 @@ import { foldM_ } from "./foldM_"
  */
 export function cause<R, E, A>(effect: Effect<R, E, A>): RIO<R, Cause<E>> {
   return foldCauseM_(effect, succeed, () => succeed(Empty))
-}
-
-/**
- * Returns the full exit cause in the error channel
- */
-export function causeAsError<R, E, A>(effect: Effect<R, E, A>): Effect<R, Cause<E>, A> {
-  return foldCauseM_(effect, fail, succeed)
-}
-
-/**
- * Opposite of causeAsError
- */
-export function errorFromCause<R, E, A>(
-  effect: Effect<R, Cause<E>, A>
-): Effect<R, E, A> {
-  return foldM_(effect, halt, succeed)
 }
