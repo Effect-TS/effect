ad6868aae43e2907c8ded19fe8a85791ff0890d9
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-09 12:10:07 +0100

    add get

--- packages/effect/src/Effect/get.ts
+++ packages/effect/src/Effect/get.ts
@@ -1,17 +1,18 @@
+import { map as mapCause } from "../Cause"
+import { flow } from "../Function"
 import type { Option } from "../Option"
-import { fold, none } from "../Option"
-import { chain_, succeed } from "./core"
+import { fold, none, some } from "../Option"
+import { foldCauseM_, halt, succeed } from "./core"
 import type { Effect } from "./effect"
 import { fail } from "./fail"
 
 /**
  * Unwraps the optional success of this effect, but can fail with an None value.
  */
-export function get<R, A>(
-  self: Effect<R, never, Option<A>>
-): Effect<R, Option<never>, A> {
-  return chain_(
+export function get<R, E, A>(self: Effect<R, E, Option<A>>): Effect<R, Option<E>, A> {
+  return foldCauseM_(
     self,
+    flow(mapCause(some), halt),
     fold(() => fail(none), succeed)
   )
 }
