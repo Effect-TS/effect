ab08e13887ff048e938d85b90fc46ad92b18e305
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-12 15:09:34 +0100

    mix integrations at run to minimise size in Async

---
+++ packages/core/src/Effect/Integrations/index.ts
@@ -0,0 +1,31 @@
+import * as T from "@effect-ts/system/Effect"
+import { some } from "@effect-ts/system/Option"
+
+import * as A from "../../Classic/Async"
+
+function asyncToEffectIntegration() {
+  if (A.currentIntegration.get._tag === "None") {
+    A.currentIntegration.set(
+      some(<R, E, A>(self: A.Async<R, E, A>) =>
+        T.accessM((r: R) =>
+          T.effectAsyncInterrupt<R, E, A>((cb) => {
+            const int = A.runAsync(self, r, (ex) => {
+              if (ex._tag === "Success") {
+                cb(T.succeed(ex.a))
+              } else if (ex._tag === "Failure") {
+                cb(T.fail(ex.e))
+              } else {
+                cb(T.interrupt)
+              }
+            })
+            return T.effectTotal(() => {
+              int()
+            })
+          })
+        )
+      )
+    )
+  }
+}
+
+asyncToEffectIntegration()
