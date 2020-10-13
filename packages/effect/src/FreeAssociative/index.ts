aa5b3e0efbe42d962e9760bcebbde73bba5bcc7e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-13 18:56:54 +0100

    add instances

--- packages/effect/src/FreeAssociative/index.ts
+++ packages/effect/src/FreeAssociative/index.ts
@@ -1,3 +1,4 @@
+import type { Endomorphism, Predicate } from "../Function"
 import { Stack } from "../Stack"
 
 export class Empty {
@@ -14,10 +15,42 @@ export class Concat<A> {
   constructor(readonly left: FreeAssociative<A>, readonly right: FreeAssociative<A>) {}
 }
 
-export type FreeAssociative<A> = Empty | Element<A> | Concat<A>
+export class Filter<A> {
+  readonly _tag = "Filter"
+  constructor(readonly self: FreeAssociative<A>, readonly f: Predicate<A>) {}
+}
+
+export class Map<A> {
+  readonly _tag = "Map"
+  constructor(readonly self: FreeAssociative<A>, readonly f: Endomorphism<A>) {}
+}
+
+export type FreeAssociative<A> = Empty | Element<A> | Concat<A> | Filter<A> | Map<A>
 
 export function init<A>(): FreeAssociative<A> {
   return new Empty()
+}
+
+export function of<A>(a: A): FreeAssociative<A> {
+  return new Element(a)
+}
+
+export function filter<A>(
+  f: Predicate<A>
+): (_: FreeAssociative<A>) => FreeAssociative<A> {
+  return (_) => new Filter(_, f)
+}
+
+export function map<A, B>(
+  f: (a: A) => B
+): (_: FreeAssociative<A>) => FreeAssociative<B> {
+  return (_) => new Map(_, f as any) as any
+}
+
+export function concat<A>(
+  r: FreeAssociative<A>
+): (l: FreeAssociative<A>) => FreeAssociative<A> {
+  return (l) => new Concat(l, r)
 }
 
 export function append<A>(a: A): (_: FreeAssociative<A>) => FreeAssociative<A> {
@@ -28,10 +61,14 @@ export function prepend<A>(a: A): (_: FreeAssociative<A>) => FreeAssociative<A> 
   return (_) => new Concat(new Element(a), _)
 }
 
+export type Ops<A> = Filter<A> | Map<A>
+
 export function toArray<A>(_: FreeAssociative<A>): readonly A[] {
   const as = <A[]>[]
   let current: FreeAssociative<A> | undefined = _
   let stack: Stack<FreeAssociative<A>> | undefined = undefined
+  let ops: Stack<Ops<A>> | undefined = undefined
+
   while (typeof current !== "undefined") {
     switch (current._tag) {
       case "Empty": {
@@ -39,8 +76,42 @@ export function toArray<A>(_: FreeAssociative<A>): readonly A[] {
         break
       }
       case "Element": {
-        as.push(current.element)
+        if (typeof ops !== "undefined") {
+          let currentOp: Stack<Ops<A>> | undefined = ops
+          let drop = false
+          let cv = current.element
+          while (typeof currentOp !== "undefined" && !drop) {
+            switch (currentOp.value._tag) {
+              case "Filter": {
+                if (!currentOp.value.f(cv)) {
+                  drop = true
+                }
+                break
+              }
+              case "Map": {
+                cv = currentOp.value.f(cv)
+                break
+              }
+            }
+            currentOp = currentOp.previous
+          }
+          if (!drop) {
+            as.push(cv)
+          }
+        } else {
+          as.push(current.element)
+        }
         current = undefined
+        break
+      }
+      case "Filter": {
+        ops = new Stack(current, ops)
+        current = current.self
+        break
+      }
+      case "Map": {
+        ops = new Stack(current, ops)
+        current = current.self
         break
       }
       case "Concat": {
