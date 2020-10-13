5067b6f3471e340bf5b240d7f5053596b5ab70cd
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-13 13:20:18 +0100

    make foreachParN in line with zio

--- packages/system/src/Effect/foreachParN_.ts
+++ packages/system/src/Effect/foreachParN_.ts
@@ -1,8 +1,18 @@
-import { pipe } from "../Function"
-import { makeSemaphore, withPermit } from "../Semaphore/semaphore"
-import { chain } from "./core"
+import { makeBy_ } from "../Array"
+import { pipe, tuple } from "../Function"
+import * as P from "../Promise"
+import * as Q from "../Queue"
+import { foldCauseM } from "."
+import { bracket } from "./bracket"
+import { collectAllUnit } from "./collectAllUnit"
+import { chain, fork } from "./core"
+import * as D from "./do"
 import type { Effect } from "./effect"
-import { foreachPar_ } from "./foreachPar_"
+import { foreach } from "./foreach"
+import { foreachUnit } from "./foreachUnit"
+import { forever } from "./forever"
+import { map } from "./map"
+import { tap } from "./tap"
 
 /**
  * Applies the functionw `f` to each element of the `Iterable<A>` in parallel,
@@ -16,7 +26,57 @@ export function foreachParN_(n: number) {
     f: (a: A) => Effect<R, E, B>
   ): Effect<R, E, readonly B[]> =>
     pipe(
-      makeSemaphore(n),
-      chain((s) => foreachPar_(as, (a) => withPermit(s)(f(a))))
+      Q.makeBounded<readonly [P.Promise<E, B>, A]>(n),
+      bracket(
+        (q) =>
+          pipe(
+            D.do,
+            D.bind("pairs", () =>
+              pipe(
+                as,
+                foreach((a) =>
+                  pipe(
+                    P.make<E, B>(),
+                    map((p) => tuple(p, a))
+                  )
+                )
+              )
+            ),
+            tap(({ pairs }) => pipe(pairs, foreachUnit(q.offer), fork)),
+            tap(({ pairs }) =>
+              pipe(
+                makeBy_(n, () =>
+                  pipe(
+                    q.take,
+                    chain(([p, a]) =>
+                      pipe(
+                        f(a),
+                        foldCauseM(
+                          (c) =>
+                            pipe(
+                              pairs,
+                              foreach(([promise]) => pipe(promise, P.halt(c)))
+                            ),
+                          (b) => pipe(p, P.succeed(b))
+                        )
+                      )
+                    ),
+                    forever,
+                    fork
+                  )
+                ),
+                collectAllUnit
+              )
+            ),
+            D.bind("res", ({ pairs }) =>
+              pipe(
+                pairs,
+                foreach(([p]) => P.await(p))
+              )
+            ),
+            map(({ res }) => res)
+          ),
+        (q) => q.shutdown
+      )
     )
 }
