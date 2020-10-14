013fb65b6b6cae432cebac4e8138aa5adef5158e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-14 12:52:29 +0100

    fixes

--- src/Classic/FreeAssociative/instances.ts
+++ src/Classic/FreeAssociative/instances.ts
@@ -1,17 +1,14 @@
 import * as FA from "@effect-ts/system/FreeAssociative"
 
-import { flow } from "../../Function"
 import type { FreeAssociativeURI } from "../../Modules"
 import * as P from "../../Prelude"
 import { makeAssociative } from "../Associative"
 import { makeIdentity } from "../Identity"
-import * as O from "../Option"
 
-export const getAssociative = <A>() =>
-  makeAssociative<FA.FreeAssociative<A>>((r) => (l) => new FA.Concat(l, r))
+export const getAssociative = <A>() => makeAssociative<FA.FreeAssociative<A>>(FA.concat)
 
 export const getIdentity = <A>() =>
-  makeIdentity<FA.FreeAssociative<A>>(FA.init<A>(), (r) => (l) => new FA.Concat(l, r))
+  makeIdentity<FA.FreeAssociative<A>>(FA.init<A>(), FA.concat)
 
 export const Covariant = P.instance<P.Covariant<[FreeAssociativeURI]>>({
   map: FA.map
@@ -22,10 +19,5 @@ export const Filter = P.instance<P.Filter<[FreeAssociativeURI]>>({
 })
 
 export const FilterMap = P.instance<P.FilterMap<[FreeAssociativeURI]>>({
-  filterMap: (f) =>
-    flow(
-      FA.map(f),
-      FA.filter(O.isSome),
-      FA.map((x) => (x as O.Some<any>).value)
-    )
+  filterMap: FA.filterMap
 })
