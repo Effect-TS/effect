7f8ff809cd05c19c50d9da4d0f7215ae4f795d5e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-16 22:37:45 +0100

    add compute from ts-toolbelt

--- packages/effect/src/Utils/index.ts
+++ packages/effect/src/Utils/index.ts
@@ -52,3 +52,5 @@ export type _A<T> = [T] extends [{ ["_A"]: () => infer A }] ? A : never
 export type _R<T> = [T] extends [{ ["_R"]: (_: infer R) => void }] ? R : never
 
 export type _E<T> = [T] extends [{ ["_E"]: () => infer E }] ? E : never
+
+export * from "./tool"
