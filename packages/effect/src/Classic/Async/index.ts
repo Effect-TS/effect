af4da590d890d90ea176cd1cfed3aa88218b917b
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-16 12:06:28 +0100

    improve async

--- packages/effect/src/Classic/Async/index.ts
+++ packages/effect/src/Classic/Async/index.ts
@@ -2,14 +2,18 @@
  * The Async Problem
  */
 
-import type { Effect, Instruction } from "@effect-ts/system/Effect"
-import { _I, FFI, notIimplementedFFI } from "@effect-ts/system/Effect"
+import type { Cause } from "@effect-ts/system/Cause"
+import type { Effect, EffectURI, FFI, Instruction } from "@effect-ts/system/Effect"
 import { AtomicReference } from "@effect-ts/system/Support/AtomicReference"
 
-import { pipe } from "../../Function"
+import { flow, identity, pipe, tuple as mkTuple } from "../../Function"
+import type { AsyncURI } from "../../Modules"
+import * as P from "../../Prelude"
+import * as DSL from "../../Prelude/DSL"
 import type { UnionToIntersection } from "../../Utils"
-import type { Option } from "../Option"
-import { none } from "../Option"
+import * as E from "../Either"
+import type { Has, Tag } from "../Has"
+import * as O from "../Option"
 
 /**
  * During the last part of day-2 when we started to test async code we noticed
@@ -48,14 +52,44 @@ import { none } from "../Option"
  */
 
 export const currentIntegration = new AtomicReference<
-  Option<<R, E, A>(_: Async<R, E, A>) => Effect<R, E, A>>
->(none)
+  O.Option<<R, E, A>(_: Async<R, E, A>) => Effect<R, E, A>>
+>(O.none)
 
-export class Async<R, E, A> extends FFI<R, E, A> {
-  constructor(readonly f: (_: InterruptionState, r: R) => CancelablePromise<E, A>) {
-    super()
+export class IFail<E> {
+  readonly _tag = "Fail"
+  readonly _S1!: (_: unknown) => void
+  readonly _S2!: () => never;
+
+  readonly ["_U"]!: EffectURI;
+  readonly ["_E"]!: () => E;
+  readonly ["_A"]!: () => never;
+  readonly ["_R"]!: (_: unknown) => void
+
+  constructor(readonly cause: Cause<E>) {}
+
+  get ["_I"](): Instruction {
+    return this as any
   }
-  get [_I](): Instruction {
+}
+
+export const notIimplementedFFI = new IFail({
+  _tag: "Die",
+  value: new Error("not supported")
+})
+
+export class Async<R, E, A> implements FFI<R, E, A> {
+  readonly _tag = "FFI"
+  readonly _S1!: (_: unknown) => void
+  readonly _S2!: () => never;
+
+  readonly ["_U"]!: EffectURI;
+  readonly ["_E"]!: () => E;
+  readonly ["_A"]!: () => A;
+  readonly ["_R"]!: (_: R) => void
+
+  constructor(readonly f: (_: InterruptionState, r: R) => CancelablePromise<E, A>) {}
+
+  get ["_I"](): Instruction {
     const ci = currentIntegration.get
     if (ci._tag === "Some") {
       return ci.value(this)["_I"]
@@ -112,16 +146,16 @@ export type Rejection<E> = Failure<E> | Interrupt
 
 export type Exit<E, A> = Rejection<E> | Success<A>
 
-export const failure = <E>(e: E): Rejection<E> => ({
+export const failExit = <E>(e: E): Rejection<E> => ({
   _tag: "Failure",
   e
 })
 
-export const interrupt: Rejection<never> = {
+export const interruptExit: Rejection<never> = {
   _tag: "Interrupt"
 }
 
-export const success = <A>(a: A): Exit<never, A> => ({
+export const successExit = <A>(a: A): Exit<never, A> => ({
   _tag: "Success",
   a
 })
@@ -187,7 +221,7 @@ export class CancelablePromise<E, A> {
 
   readonly interrupt = () => {
     // triggeres a promise rejection on the current promise with an interrupt exit
-    this.rejection?.(interrupt)
+    this.rejection?.(interruptExit)
   }
 }
 
@@ -225,7 +259,7 @@ export class Tracer {
   async wait(): Promise<Exit<any, any>[]> {
     const t = await Promise.all(
       Array.from(this.running).map((p) =>
-        p.then((a) => success(a)).catch((e) => Promise.resolve(e))
+        p.then((a) => successExit(a)).catch((e) => Promise.resolve(e))
       )
     )
     return await new Promise((r) => {
@@ -250,13 +284,15 @@ export const tracingContext = new Tracer()
 
 // runs the cancellable promise in a tracing context
 // folds the result to an Exit state
-function runCancelablePromise<E, A>(
+async function runCancelablePromise<E, A>(
   task: CancelablePromise<E, A>
 ): Promise<Exit<E, A>> {
-  return tracingContext
-    .traced(task.promise)()
-    .then((a) => success(a))
-    .catch((e: Rejection<E>) => Promise.resolve(e))
+  try {
+    const a = await tracingContext.traced(task.promise)()
+    return successExit(a)
+  } catch (e) {
+    return await Promise.resolve(e)
+  }
 }
 
 // runs as a Promise of an Exit
@@ -305,23 +341,37 @@ export const accessM = <R, R1, E, A>(
 ): Async<R & R1, E, A> =>
   new Async((is, r) => new CancelablePromise(() => f(r).f(is, r).promise(), is))
 
+// reads a value from the environment and transforms it monadically using f
+export const provide = <R>(r: R) => <E, A>(fa: Async<R, E, A>): Async<unknown, E, A> =>
+  new Async((is, _: unknown) => new CancelablePromise(() => fa.f(is, r).promise(), is))
+
 // construct a Task from an error
 export const fail = <E>(e: E): Async<unknown, E, never> =>
-  new Async((is) => new CancelablePromise(() => Promise.reject(failure(e)), is))
+  new Async((is) => new CancelablePromise(() => Promise.reject(failExit(e)), is))
 
 // construct an empty task of {}
 // useful to combine with bind & assign
-export const of = succeed({})
+const of = succeed({})
+
+export { of as do }
 
 // construct a Task from a promise that can fail
 // need to specify the action to take on error
-export const fromPromise = <E>(onError: (u: unknown) => E) => <A>(
+export const fromPromise = <A>(p: () => Promise<A>): Async<unknown, unknown, A> =>
+  new Async(
+    (is) =>
+      new CancelablePromise(() => p().catch((e) => Promise.reject(failExit(e))), is)
+  )
+
+// construct a Task from a promise that can fail
+// need to specify the action to take on error
+export const fromPromiseMap = <E>(onError: (u: unknown) => E) => <A>(
   p: () => Promise<A>
 ): Async<unknown, E, A> =>
   new Async(
     (is) =>
       new CancelablePromise(
-        () => p().catch((e) => Promise.reject(failure(onError(e)))),
+        () => p().catch((e) => Promise.reject(failExit(onError(e)))),
         is
       )
   )
@@ -336,16 +386,15 @@ export const fromTryCatch = <E>(onError: (u: unknown) => E) => <A>(
         try {
           return Promise.resolve(p())
         } catch (e) {
-          return Promise.reject(failure(onError(e)))
+          return Promise.reject(failExit(onError(e)))
         }
       }, is)
   )
 }
 
 // construct a Task from a promise that cannot fail
-export const fromNonFailingPromise = <A>(
-  p: () => Promise<A>
-): Async<unknown, never, A> => new Async((is) => new CancelablePromise(p, is))
+export const fromTask = <A>(p: () => Promise<A>): Async<unknown, never, A> =>
+  new Async((is) => new CancelablePromise(p, is))
 
 // represent a callback
 export type Cb<A> = (a: A) => void
@@ -364,7 +413,7 @@ export const fromCallback = <E = never, A = void>(
                 res(result)
               },
               (err) => {
-                rej(failure(err))
+                rej(failExit(err))
               }
             )
           }),
@@ -458,13 +507,13 @@ export const result = <R, E, A>(task: Async<R, E, A>): Async<R, never, Exit<E, A
   pipe(
     task,
     fold(
-      (e) => succeed(failure(e)),
-      (a) => succeed(success(a))
+      (e) => succeed(failExit(e)),
+      (a) => succeed(successExit(a))
     )
   )
 
 // like Promise.all
-export function all<R, E, A>(a: Async<R, E, A>[]): Async<R, E, A[]> {
+export function collectAll<R, E, A>(a: Async<R, E, A>[]): Async<R, E, A[]> {
   return new Async(
     (is, r) =>
       new CancelablePromise(() => Promise.all(a.map((p) => p.f(is, r).promise())), is)
@@ -472,7 +521,7 @@ export function all<R, E, A>(a: Async<R, E, A>[]): Async<R, E, A[]> {
 }
 
 // like Promise.all for tuples
-export function sequence<Tasks extends Async<any, any, any>[]>(
+export function tuple<Tasks extends Async<any, any, any>[]>(
   ...tasks: Tasks & { 0: Async<any, any, any> }
 ): Async<
   UnionToIntersection<
@@ -489,13 +538,13 @@ export function sequence<Tasks extends Async<any, any, any>[]>(
   }[number],
   { [k in keyof Tasks]: [Tasks[k]] extends [Async<any, any, infer A>] ? A : never }
 > {
-  return all(tasks) as any
+  return collectAll(tasks) as any
 }
 
 // like Promise.all + map on steroids
 export const foreach = <R, A, E1, B>(f: (a: A) => Async<R, E1, B>) => (
   as: Iterable<A>
-) => all(Array.from(as).map(f))
+) => collectAll(Array.from(as).map(f))
 
 // binds the output of a computation to a variable
 // useful for imperative style, like using async/await
@@ -514,13 +563,15 @@ export const bind = <K extends string>(k: K) => <R, S, E1, A1>(
 
 // binds the result of a function to a variable
 // useful for imperative style, like using async/await
-export const assign = <K extends string>(k: K) => <S, A1>(f: (s: S) => A1) => <R, E>(
+const assign = <K extends string>(k: K) => <S, A1>(f: (s: S) => A1) => <R, E>(
   self: Async<R, E, S>
 ): Async<R, E, S & { [k in K]: A1 }> =>
   pipe(
     self,
     map((s) => pipe(f(s), (a1) => ({ ...s, [k]: a1 } as S & { [k in K]: A1 })))
   )
+
+export { assign as let }
 
 // sleeps
 export const sleep = (ms: number) =>
@@ -555,7 +606,7 @@ export const onInterrupt = <R, E1, A1>(f: () => Async<R, E1, A1>) => <R2, E, A>(
                 case "Interrupt": {
                   await f().f(new InterruptionState(), r).promise()
 
-                  return await Promise.reject(failure(e))
+                  return await Promise.reject(failExit(e))
                 }
               }
             }),
@@ -579,7 +630,7 @@ export const fromInterruptibleCallback = <E = never, A = void, E2 = never, A2 = 
                   res(result)
                 },
                 (err) => {
-                  rej(failure(err))
+                  rej(failExit(err))
                 }
               )
             }),
@@ -592,3 +643,112 @@ export const fromInterruptibleCallback = <E = never, A = void, E2 = never, A2 = 
       onInterrupt(() => finalizer)
     ).f(is, {})
   })
+
+export type V = P.V<"R", "-"> & P.V<"E", "+">
+
+export const Covariant = P.instance<P.Covariant<[AsyncURI], V>>({
+  map
+})
+
+export const Any = P.instance<P.Any<[AsyncURI], V>>({
+  any: () => succeed({})
+})
+
+export const zip: <R2, E2, B>(
+  fb: Async<R2, E2, B>
+) => <R, E, A>(fa: Async<R, E, A>) => Async<R2 & R, E2 | E, readonly [A, B]> = (fb) => (
+  fa
+) =>
+  pipe(
+    fa,
+    chain((a) =>
+      pipe(
+        fb,
+        map((b) => mkTuple(a, b))
+      )
+    )
+  )
+
+export const AssociativeBoth = P.instance<P.AssociativeBoth<[AsyncURI], V>>({
+  both: zip
+})
+
+export const AssociativeFlatten = P.instance<P.AssociativeFlatten<[AsyncURI], V>>({
+  flatten: (fa) => pipe(fa, chain(identity))
+})
+
+export const IdentityBoth = P.instance<P.IdentityBoth<[AsyncURI], V>>({
+  ...Any,
+  ...AssociativeBoth
+})
+
+export const IdentityFlatten = P.instance<P.IdentityFlatten<[AsyncURI], V>>({
+  ...Any,
+  ...AssociativeFlatten
+})
+
+export const Applicative = P.instance<P.Applicative<[AsyncURI], V>>({
+  ...Covariant,
+  ...IdentityBoth
+})
+
+export const Monad = P.instance<P.Monad<[AsyncURI], V>>({
+  ...Covariant,
+  ...IdentityFlatten
+})
+
+export const Fail = P.instance<P.FX.Fail<[AsyncURI], V>>({
+  fail
+})
+
+export const Run = P.instance<P.FX.Run<[AsyncURI], V>>({
+  either: flow(
+    map(E.right),
+    handle((e) => succeed(E.left(e)))
+  )
+})
+
+export const either: <A, R, E>(fa: Async<R, E, A>) => Async<R, never, E.Either<E, A>> =
+  Run.either
+
+export const getValidation = DSL.getValidationF({
+  ...Monad,
+  ...Run,
+  ...Applicative,
+  ...Fail
+})
+
+export const Provide = P.instance<P.FX.Provide<[AsyncURI], V>>({
+  provide
+})
+
+export const Access = P.instance<P.FX.Access<[AsyncURI], V>>({
+  access
+})
+
+export const accessServiceM: <Service>(
+  H: Tag<Service>
+) => <R, E, A>(
+  f: (_: Service) => Async<R, E, A>
+) => Async<R & Has<Service>, E, A> = DSL.accessServiceMF({ ...Monad, ...Access })
+
+export const accessService: <Service>(
+  H: Tag<Service>
+) => <A>(f: (_: Service) => A) => Async<Has<Service>, never, A> = (tag) => (f) =>
+  accessServiceM(tag)((_) => succeed(f(_)))
+
+export const provideService: <Service>(
+  H: Tag<Service>
+) => (
+  S: Service
+) => <R, E, A>(
+  fa: Async<R & Has<Service>, E, A>
+) => Async<R, E, A> = DSL.provideServiceF({ ...Monad, ...Provide, ...Access })
+
+export const provideSome: <R, R2>(
+  f: (_: R2) => R
+) => <E, A>(fa: Async<R, E, A>) => Async<R2, E, A> = DSL.provideSomeF({
+  ...Monad,
+  ...Access,
+  ...Provide
+})
