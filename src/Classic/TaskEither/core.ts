bdb25dac537d16bda56909f6096f76c6e64175d5
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-17 22:25:42 +0100

    continue with generators

---
+++ src/Classic/TaskEither/core.ts
@@ -0,0 +1,45 @@
+import { chainF, doF, getValidationF, structF, succeedF } from "../../Prelude/DSL"
+import type { Either } from "../Either"
+import * as ET from "../EitherT"
+import * as T from "../Task"
+
+export type TaskEither<E, A> = T.Task<Either<E, A>>
+
+export const Monad = ET.monad(T.Monad)
+
+export const Applicative = ET.applicative(T.Applicative)
+
+export const ApplicativePar = ET.applicative(T.ApplicativePar)
+
+export const Fail = ET.fail(T.Applicative)
+
+export const Run = ET.run(T.Applicative)
+
+export const { any, flatten, map } = Monad
+
+export const { both: zip } = Applicative
+
+export const { both: zipPar } = ApplicativePar
+
+export const { fail } = Fail
+
+export const { either } = Run
+
+export const getValidation = getValidationF({
+  ...Monad,
+  ...Applicative,
+  ...Fail,
+  ...Run
+})
+
+export const chain = chainF(Monad)
+
+const do_ = doF(Monad)
+
+export { do_ as do }
+
+export const struct = structF(Applicative)
+
+export const structPar = structF(ApplicativePar)
+
+export const succeed = succeedF(Monad)
---
+++ src/Classic/TaskEither/gen.ts
@@ -0,0 +1,71 @@
+import { NoSuchElementException } from "@effect-ts/system/GlobalExceptions"
+import type { _E } from "@effect-ts/system/Utils"
+
+import type { Either } from "../Either"
+import type { Option } from "../Option"
+import type { TaskEither } from "./core"
+import * as T from "./core"
+
+export class GenTaskEither<E, A> {
+  readonly _E!: () => E
+  readonly _A!: () => A
+
+  constructor(readonly effect: TaskEither<E, A>) {}
+
+  *[Symbol.iterator](): Generator<GenTaskEither<E, A>, A, any> {
+    return yield this
+  }
+}
+
+function isOption(u: unknown): u is Option<unknown> {
+  return (
+    typeof u === "object" &&
+    u != null &&
+    "_tag" in u &&
+    (u["_tag"] === "Some" || u["_tag"] === "None")
+  )
+}
+
+function isEither(u: unknown): u is Either<unknown, unknown> {
+  return (
+    typeof u === "object" &&
+    u != null &&
+    "_tag" in u &&
+    (u["_tag"] === "Some" || u["_tag"] === "None")
+  )
+}
+
+const adapter = (_: any) => {
+  return isOption(_)
+    ? new GenTaskEither(
+        _._tag === "Some" ? T.succeed(_.value) : T.fail(new NoSuchElementException())
+      )
+    : isEither(_)
+    ? new GenTaskEither(_._tag === "Left" ? T.fail(_.left) : T.succeed(_.right))
+    : new GenTaskEither(_)
+}
+
+export function gen<Eff extends GenTaskEither<any, any>, EEff extends _E<Eff>, AEff>(
+  f: (i: {
+    <A>(_: Option<A>): GenTaskEither<NoSuchElementException, A>
+    <E, A>(_: Either<E, A>): GenTaskEither<E, A>
+    <E, A>(_: TaskEither<E, A>): GenTaskEither<E, A>
+  }) => Generator<Eff, AEff, any>
+): TaskEither<EEff, AEff> {
+  const iterator = f(adapter as any)
+  const state = iterator.next()
+
+  function run(
+    state: IteratorYieldResult<Eff> | IteratorReturnResult<AEff>
+  ): TaskEither<any, AEff> {
+    if (state.done) {
+      return T.succeed(state.value)
+    }
+    return T.chain((val) => {
+      const next = iterator.next(val)
+      return run(next)
+    })(state.value["effect"])
+  }
+
+  return run(state)
+}
