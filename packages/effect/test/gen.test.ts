b4faa3c95f3dbd8c2debd838190eb75e58cb8d6d
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-17 19:03:59 +0100

    remove try/catch stupid perf and untyped

--- packages/effect/test/gen.test.ts
+++ packages/effect/test/gen.test.ts
@@ -46,49 +46,6 @@ describe("Generator", () => {
 
     expect(result).toEqual(Ex.fail("12 should be lower then x"))
   })
-  it("try/catch", async () => {
-    const result = await T.runPromiseExit(
-      T.gen(function* (_) {
-        try {
-          return yield* _(T.fail(1))
-        } catch (e) {
-          return yield* _(T.succeed((e as number) + 1))
-        }
-      })
-    )
-
-    expect(result).toEqual(Ex.succeed(2))
-  })
-  it("try/catch defects", async () => {
-    const result = await T.runPromiseExit(
-      T.gen(function* (_) {
-        try {
-          yield* _(T.die(new Error("error")))
-        } catch (e) {
-          return yield* _(T.succeed((e as number) + 1))
-        }
-      })
-    )
-
-    expect(result).toEqual(Ex.die(new Error("error")))
-  })
-  it("try/catch use try", async () => {
-    const result = await T.runPromiseExit(
-      T.gen(function* (_) {
-        try {
-          yield* _(
-            T.try(() => {
-              throw new Error("error")
-            })
-          )
-        } catch (e) {
-          return yield* _(T.succeed("gotcha"))
-        }
-      })
-    )
-
-    expect(result).toEqual(Ex.succeed("gotcha"))
-  })
   it("mix types", async () => {
     class SumTooBig {
       readonly _tag = "SumTooBig"
