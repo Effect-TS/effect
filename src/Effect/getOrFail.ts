0b8a7ea5cab84290f44b8e10d67a7d75b2aecf25
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-19 14:44:47 +0100

    fix getFail signature

--- src/Effect/getOrFail.ts
+++ src/Effect/getOrFail.ts
@@ -1,25 +1,23 @@
 import { NoSuchElementException } from "../GlobalExceptions"
 import * as O from "../Option"
-import { chain_, effectTotal, succeed, suspend } from "./core"
+import { succeed } from "./core"
 import type { IO } from "./effect"
 import { fail } from "./fail"
 
 /**
  * Lifts an Option into an Effect, if the option is not defined it fails with NoSuchElementException.
  */
-export function getOrFail<A>(v: () => O.Option<A>): IO<NoSuchElementException, A> {
-  return suspend(() => O.fold_(v(), () => fail(new NoSuchElementException()), succeed))
+export function getOrFail<A>(v: O.Option<A>): IO<NoSuchElementException, A> {
+  return O.fold_(v, () => fail(new NoSuchElementException()), succeed)
 }
 
 /**
  * Lifts an Option into a IO, if the option is not defined it fails with Unit.
  */
-export function getOrFailUnit<A>(v: () => O.Option<A>): IO<void, A> {
-  return chain_(
-    effectTotal(v),
-    O.fold(
-      () => fail(undefined),
-      (a) => succeed(a)
-    )
+export function getOrFailUnit<A>(v: O.Option<A>): IO<void, A> {
+  return O.fold_(
+    v,
+    () => fail(undefined),
+    (a) => succeed(a)
   )
 }
