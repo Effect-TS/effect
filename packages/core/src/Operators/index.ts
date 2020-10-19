e0a9b5c089dfb49a4dce88c15cbb636f55a0d3f3
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-19 12:53:30 +0100

    add fake flow

--- packages/core/src/Operators/index.ts
+++ packages/core/src/Operators/index.ts
@@ -1,4 +1,10 @@
 interface Object {
+  /**
+   * To be used like
+   * ```ts
+   * T.succeed(1)["|>"](T.map(n => n + 1))["|>"](T.map(n => n + 2))
+   * ```
+   */
   ["|>"]<Self, Result>(this: Self, next: (value: Self) => Result): Result
 }
 
@@ -7,4 +13,21 @@ Object.prototype["|>"] = function <Self, Result>(
   next: (value: Self) => Result
 ): Result {
   return next(this)
+}
+
+interface Function {
+  /**
+   * This operator does not work with generic parameters
+   */
+  [">>"]<Args extends any[], A, Result>(
+    this: (...args: Args) => A,
+    next: (value: A) => Result
+  ): (...args: Args) => Result
+}
+
+Function.prototype[">>"] = function <Args extends any[], A, Result>(
+  this: (...args: Args) => A,
+  next: (value: A) => Result
+): (...args: Args) => Result {
+  return (...args) => next(this(...args))
 }
