01b7bda5260af59996cc61295ad979349061eab5
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-22 13:26:31 +0100

    begin

--- packages/system/src/Effect/gen.ts
+++ packages/system/src/Effect/gen.ts
@@ -48,6 +48,16 @@ const adapter = (_: any, __?: any) => {
   return new GenEffect(_)
 }
 
+export function gen<AEff>(): <Eff extends GenEffect<any, any, any>>(
+  f: (i: {
+    <A>(_: Tag<A>): GenEffect<Has<A>, never, A>
+    <E, A>(_: Option<A>, onNone: () => E): GenEffect<unknown, E, A>
+    <A>(_: Option<A>): GenEffect<unknown, NoSuchElementException, A>
+    <E, A>(_: Either<E, A>): GenEffect<unknown, E, A>
+    <R, E, A>(_: Effect<R, E, A>): GenEffect<R, E, A>
+    <R, E, A>(_: Managed<R, E, A>): GenEffect<R, E, A>
+  }) => Generator<Eff, AEff, any>
+) => Effect<_R<Eff>, _E<Eff>, AEff>
 export function gen<Eff extends GenEffect<any, any, any>, AEff>(
   f: (i: {
     <A>(_: Tag<A>): GenEffect<Has<A>, never, A>
@@ -57,38 +67,61 @@ export function gen<Eff extends GenEffect<any, any, any>, AEff>(
     <R, E, A>(_: Effect<R, E, A>): GenEffect<R, E, A>
     <R, E, A>(_: Managed<R, E, A>): GenEffect<R, E, A>
   }) => Generator<Eff, AEff, any>
-): Effect<_R<Eff>, _E<Eff>, AEff> {
-  return suspend(() => {
-    const iterator = f(adapter as any)
-    const state = iterator.next()
+): Effect<_R<Eff>, _E<Eff>, AEff>
+export function gen(...args: any[]): any {
+  function gen_<Eff extends GenEffect<any, any, any>, AEff>(
+    f: (i: any) => Generator<Eff, AEff, any>
+  ): Effect<_R<Eff>, _E<Eff>, AEff> {
+    return suspend(() => {
+      const iterator = f(adapter as any)
+      const state = iterator.next()
 
-    function run(
-      rm: ReleaseMap,
-      state: IteratorYieldResult<Eff> | IteratorReturnResult<AEff>
-    ): Effect<any, any, AEff> {
-      if (state.done) {
-        return succeed(state.value)
-      }
-      return chain_(
-        state.value["effect"] instanceof Managed
-          ? map_(
-              provideSome_(state.value["effect"]["effect"], (r0) => tuple(r0, rm)),
-              ([_, a]) => a
-            )
-          : state.value["effect"],
-        (val) => {
-          const next = iterator.next(val)
-          return run(rm, next)
+      function run(
+        rm: ReleaseMap,
+        state: IteratorYieldResult<Eff> | IteratorReturnResult<AEff>
+      ): Effect<any, any, AEff> {
+        if (state.done) {
+          return succeed(state.value)
         }
-      )
-    }
+        return chain_(
+          state.value["effect"] instanceof Managed
+            ? map_(
+                provideSome_(state.value["effect"]["effect"], (r0) => tuple(r0, rm)),
+                ([_, a]) => a
+              )
+            : state.value["effect"],
+          (val) => {
+            const next = iterator.next(val)
+            return run(rm, next)
+          }
+        )
+      }
 
-    return chain_(makeReleaseMap, (rm) =>
-      bracketExit_(
-        unit,
-        () => run(rm, state),
-        (_, e) => releaseAll(e, sequential)(rm)
+      return chain_(makeReleaseMap, (rm) =>
+        bracketExit_(
+          unit,
+          () => run(rm, state),
+          (_, e) => releaseAll(e, sequential)(rm)
+        )
       )
-    )
-  })
+    })
+  }
+
+  if (args.length === 0) {
+    return (f: any) => gen_(f)
+  }
+  return gen_(args[0])
+}
+
+export function genA<AEff>() {
+  return <Eff extends GenEffect<any, any, any>>(
+    f: (i: {
+      <A>(_: Tag<A>): GenEffect<Has<A>, never, A>
+      <E, A>(_: Option<A>, onNone: () => E): GenEffect<unknown, E, A>
+      <A>(_: Option<A>): GenEffect<unknown, NoSuchElementException, A>
+      <E, A>(_: Either<E, A>): GenEffect<unknown, E, A>
+      <R, E, A>(_: Effect<R, E, A>): GenEffect<R, E, A>
+      <R, E, A>(_: Managed<R, E, A>): GenEffect<R, E, A>
+    }) => Generator<Eff, AEff, any>
+  ): Effect<_R<Eff>, _E<Eff>, AEff> => gen(f)
 }
