79b5329a8a28bfdb860be7b007707d3cb7037f45
curiosityfoundation <71806010+curiosityfoundation@users.noreply.github.com>
2020-10-22 02:42:54 -0700

    filter added to system/stream (#332)
    * filter added to system/stream
    
    * linted
    
    * filter added to index
    
    Co-authored-by: Alfred Young <alfred@codenaturally.com>

---
+++ packages/effect/src/Stream/Stream/filter.ts
@@ -0,0 +1,17 @@
+import * as Array_ from "../../Array"
+import type { Predicate } from "../../Function"
+import { pipe } from "../../Function"
+import type { Stream } from "./definitions"
+import { mapChunks } from "./mapChunks"
+
+/**
+ * Applies the predicate to each element and allows passing elements 
+ * to reach the output of this stream.
+ */
+export const filter = <O>(f: Predicate<O>) => <R, E>(
+  self: Stream<R, E, O>
+): Stream<R, E, O> =>
+  pipe(
+    self,
+    mapChunks((o) => Array_.chain_(o, (o) => (f(o) ? [o] : [])))
+  )
