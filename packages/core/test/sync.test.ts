71507d0380300d85f1b3567140605b943dadf221
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-22 23:07:01 +0100

    continue

--- packages/core/test/sync.test.ts
+++ packages/core/test/sync.test.ts
@@ -12,9 +12,15 @@ interface Bar {
   bar: string
 }
 
+interface Baz {
+  baz: string
+}
+
 const Foo = has<Foo>()
 
 const Bar = has<Bar>()
+
+const Baz = has<Baz>()
 
 const FooLive = L.fromSync(Foo)(
   Sy.succeed<Foo>({ foo: "foo" })
@@ -24,21 +30,31 @@ const BarLive = L.fromSync(Bar)(
   Sy.succeed<Bar>({ bar: "bar" })
 )
 
-const AppLive = FooLive["+++"](BarLive)
+const BazLive = L.fromSync(Baz)(
+  Sy.gen(function* (_) {
+    const { foo } = yield* _(Foo)
+    const { bar } = yield* _(Bar)
+
+    return {
+      baz: `${foo} - ${bar}`
+    }
+  })
+)
+
+const AppLive = BazLive["<+<"](FooLive["+++"](BarLive))
 
 describe("SyncLayer", () => {
   it("use foo & bar", () => {
     const result = pipe(
       Sy.gen(function* (_) {
-        const { foo } = yield* _(Foo)
-        const { bar } = yield* _(Bar)
+        const { baz } = yield* _(Baz)
 
-        return `${foo} - ${bar}`
+        return `ok: ${baz}`
       }),
       L.provideSyncLayer(AppLive),
       Sy.run
     )
 
-    expect(result).toEqual(`foo - bar`)
+    expect(result).toEqual(`ok: foo - bar`)
   })
 })
