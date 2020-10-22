684a6a22c378144c466c92a564e740911b7dca47
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-22 10:45:48 +0100

    fix format and filter

--- packages/system/src/Stream/Stream/filter.ts
+++ packages/system/src/Stream/Stream/filter.ts
@@ -1,17 +1,24 @@
-import * as Array_ from "../../Array"
+import * as A from "../../Array"
 import type { Predicate } from "../../Function"
 import { pipe } from "../../Function"
 import type { Stream } from "./definitions"
 import { mapChunks } from "./mapChunks"
 
 /**
- * Applies the predicate to each element and allows passing elements 
+ * Applies the predicate to each element and allows passing elements
  * to reach the output of this stream.
  */
-export const filter = <O>(f: Predicate<O>) => <R, E>(
-  self: Stream<R, E, O>
-): Stream<R, E, O> =>
-  pipe(
-    self,
-    mapChunks((o) => Array_.chain_(o, (o) => (f(o) ? [o] : [])))
-  )
+export function filter<O>(f: Predicate<O>) {
+  return <R, E>(self: Stream<R, E, O>): Stream<R, E, O> => filter_(self, f)
+}
+
+/**
+ * Applies the predicate to each element and allows passing elements
+ * to reach the output of this stream.
+ */
+export function filter_<R, E, O>(
+  self: Stream<R, E, O>,
+  f: Predicate<O>
+): Stream<R, E, O> {
+  return pipe(self, mapChunks(A.filter(f)))
+}
