bd0f89b705d4fe9c8f00d0ff196569a1b20108f6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-24 22:31:02 +0100

    add methods

--- src/Effect/absorbWith.ts
+++ src/Effect/absorbWith.ts
@@ -1,5 +1,5 @@
 import { squash } from "../Cause"
-import { flow, pipe } from "../Function"
+import { flow, identity, pipe } from "../Function"
 import { succeed } from "./core"
 import type { Effect } from "./effect"
 import { fail } from "./fail"
@@ -20,4 +20,12 @@ export function absorbWith<E>(f: (e: E) => unknown) {
  */
 export function absorbWith_<R, A, E>(fa: Effect<R, E, A>, f: (e: E) => unknown) {
   return pipe(fa, sandbox, foldM(flow(squash(f), fail), succeed))
+}
+
+/**
+ * Attempts to convert defects into a failure, throwing away all information
+ * about the cause of the failure.
+ */
+export function absord<R, E, A>(self: Effect<R, E, A>) {
+  return absorbWith_(self, identity)
 }
