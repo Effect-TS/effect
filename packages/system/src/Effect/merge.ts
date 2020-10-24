96ba9edff821438df9b76c28dc28431ac6215929
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-24 22:49:58 +0100

    add merge

---
+++ packages/system/src/Effect/merge.ts
@@ -0,0 +1,11 @@
+import { succeed } from "./core"
+import type { Effect } from "./effect"
+import { foldM_ } from "./foldM_"
+
+/**
+ * Returns a new effect where the error channel has been merged into the
+ * success channel to their common combined type.
+ */
+export function merge<R, E, A>(self: Effect<R, E, A>): Effect<R, never, E | A> {
+  return foldM_(self, succeed, succeed)
+}
