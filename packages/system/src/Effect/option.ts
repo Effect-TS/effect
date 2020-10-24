be8b864288fbeee024039137a2b8b998ac9e715b
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-24 23:34:55 +0100

    fix types

---
+++ packages/system/src/Effect/option.ts
@@ -0,0 +1,15 @@
+import * as O from "../Option"
+import { succeed } from "./core"
+import type { Effect, RIO } from "./effect"
+import { foldM_ } from "./foldM_"
+
+/**
+ * Converts an option on errors into an option on values.
+ */
+export function option<R, E, A>(self: Effect<R, E, A>): RIO<R, O.Option<A>> {
+  return foldM_(
+    self,
+    () => succeed(O.none),
+    (a) => succeed(O.some(a))
+  )
+}
