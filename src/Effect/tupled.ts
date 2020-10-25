3f656fdf8cc16b24c1249c6bff10d49d85fccd59
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-25 14:59:23 +0000

    improve signatures

--- src/Effect/tupled.ts
+++ src/Effect/tupled.ts
@@ -1,26 +1,10 @@
 import { identity } from "../Function"
 import type { NonEmptyArray } from "../NonEmptyArray"
-import type { UnionToIntersection } from "../Utils"
+import type { _E, _R } from "../Utils"
 import type { Effect } from "./effect"
 import { foreach_ } from "./foreach_"
 import { foreachPar_ } from "./foreachPar_"
 import { foreachParN_ } from "./foreachParN_"
-
-export type TupleR<
-  T extends NonEmptyArray<Effect<any, any, any>>
-> = UnionToIntersection<
-  {
-    [K in keyof T]: [T[K]] extends [Effect<infer R, any, any>]
-      ? unknown extends R
-        ? never
-        : R
-      : never
-  }[number]
->
-
-export type TupleE<T extends NonEmptyArray<Effect<any, any, any>>> = {
-  [K in keyof T]: [T[K]] extends [Effect<any, infer E, any>] ? E : never
-}[number]
 
 export type TupleA<T extends NonEmptyArray<Effect<any, any, any>>> = {
   [K in keyof T]: [T[K]] extends [Effect<any, any, infer A>] ? A : never
@@ -31,7 +15,7 @@ export type TupleA<T extends NonEmptyArray<Effect<any, any, any>>> = {
  */
 export function tuple<T extends NonEmptyArray<Effect<any, any, any>>>(
   ...t: T
-): Effect<TupleR<T>, TupleE<T>, TupleA<T>> {
+): Effect<_R<T[number]>, _E<T[number]>, TupleA<T>> {
   return foreach_(t, identity) as any
 }
 
@@ -40,7 +24,7 @@ export function tuple<T extends NonEmptyArray<Effect<any, any, any>>>(
  */
 export function tuplePar<T extends NonEmptyArray<Effect<any, any, any>>>(
   ...t: T
-): Effect<TupleR<T>, TupleE<T>, TupleA<T>> {
+): Effect<_R<T[number]>, _E<T[number]>, TupleA<T>> {
   return foreachPar_(t, identity) as any
 }
 
@@ -52,6 +36,6 @@ export function tupleParN(
   n: number
 ): <T extends NonEmptyArray<Effect<any, any, any>>>(
   ...t: T
-) => Effect<TupleR<T>, TupleE<T>, TupleA<T>> {
+) => Effect<_R<T[number]>, _E<T[number]>, TupleA<T>> {
   return ((...t: Effect<any, any, any>[]) => foreachParN_(n)(t, identity)) as any
 }
