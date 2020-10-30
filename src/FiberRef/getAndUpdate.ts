86d6a49d7a510928ba5959c8b131b5d1ca3ae898
Nikita Volodin <volodin.n@gmail.com>
2020-10-30 19:29:28 -0400

    simplify existing methods

--- src/FiberRef/getAndUpdate.ts
+++ src/FiberRef/getAndUpdate.ts
@@ -1,13 +1,7 @@
-import { pipe } from "../Function"
-import type { FiberRef } from "./fiberRef"
 import { modify } from "./modify"
 
 /**
  * Atomically modifies the `FiberRef` with the specified function and returns
  * the old value.
  */
-export const getAndUpdate = <A>(f: (a: A) => A) => (fiberRef: FiberRef<A>) =>
-  pipe(
-    fiberRef,
-    modify((v) => [v, f(v)])
-  )
+export const getAndUpdate = <A>(f: (a: A) => A) => modify<A, A>((v) => [v, f(v)])
--- src/FiberRef/update.ts
+++ src/FiberRef/update.ts
@@ -1,13 +1,6 @@
-import type { UIO } from "../Effect/effect"
-import { pipe } from "../Function"
-import type { FiberRef } from "./fiberRef"
 import { modify } from "./modify"
 
 /**
  * Atomically modifies the `FiberRef` with the specified function.
  */
-export const update = <A>(f: (a: A) => A) => (fiberRef: FiberRef<A>): UIO<void> =>
-  pipe(
-    fiberRef,
-    modify((v) => [undefined, f(v)])
-  )
+export const update = <A>(f: (a: A) => A) => modify<A, void>((v) => [undefined, f(v)])
