c5d09e1db21892838187d7507149a98b92fc51dc
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-03 19:34:12 +0000

    begin stack safety of recursions

--- src/Fiber/status.ts
+++ src/Fiber/status.ts
@@ -1,3 +1,4 @@
+import * as S from "../Sync"
 import type { FiberID } from "./id"
 
 export type Status = Done | Finishing | Running | Suspended
@@ -33,43 +34,55 @@ export function isDone(s: Status): boolean {
   return s._tag === "Done"
 }
 
+export function withInterruptingSafe(b: boolean) {
+  return (s: Status): S.UIO<Status> => {
+    return S.gen(function* (_) {
+      switch (s._tag) {
+        case "Done": {
+          return s
+        }
+        case "Finishing": {
+          return new Finishing(b)
+        }
+        case "Running": {
+          return new Running(b)
+        }
+        case "Suspended": {
+          return new Suspended(
+            yield* _(withInterruptingSafe(b)(s.previous)),
+            s.interruptible,
+            s.epoch,
+            s.blockingOn
+          )
+        }
+      }
+    })
+  }
+}
+
 export function withInterrupting(b: boolean) {
-  return (s: Status): Status => {
+  return (s: Status) => S.run(withInterruptingSafe(b)(s))
+}
+
+export const toFinishing = (s: Status): Status => {
+  return S.run(toFinishingSafe(s))
+}
+
+export const toFinishingSafe = (s: Status): S.UIO<Status> => {
+  return S.gen(function* (_) {
     switch (s._tag) {
       case "Done": {
         return s
       }
       case "Finishing": {
-        return new Finishing(b)
+        return s
       }
       case "Running": {
-        return new Running(b)
+        return s
       }
       case "Suspended": {
-        return new Suspended(
-          withInterrupting(b)(s.previous),
-          s.interruptible,
-          s.epoch,
-          s.blockingOn
-        )
+        return yield* _(toFinishingSafe(s.previous))
       }
     }
-  }
-}
-
-export const toFinishing = (s: Status): Status => {
-  switch (s._tag) {
-    case "Done": {
-      return s
-    }
-    case "Finishing": {
-      return s
-    }
-    case "Running": {
-      return s
-    }
-    case "Suspended": {
-      return toFinishing(s.previous)
-    }
-  }
+  })
 }
