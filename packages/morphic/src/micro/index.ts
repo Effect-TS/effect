bc2b50a63390cd9f2c5ec1cf9961c25b9be32a45
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-03 11:41:20 +0000

    fix export

--- packages/morphic/src/micro/index.ts
+++ packages/morphic/src/micro/index.ts
@@ -15,7 +15,7 @@ export type { AType, EType, RType } from "./Batteries/usage/utils"
 //
 // URIS
 //
-//export { FastCheckURI } from "./FastCheck/hkt"
+export { FastCheckURI } from "./FastCheck/base"
 //export { GuardURI } from "./Guard/hkt"
 //export { DecoderURI } from "./Decoder/hkt"
 //export { EncoderURI } from "./Encoder/hkt"
@@ -26,7 +26,7 @@ export { EqURI } from "./Equal/base"
 //
 // Threading configs
 //
-//export {} from "./FastCheck/interpreter/configs"
+export {} from "./FastCheck/interpreter/configs"
 //export {} from "./Guard/interpreter/configs"
 //export {} from "./Decoder/interpreter/configs"
 //export {} from "./Encoder/interpreter/configs"
