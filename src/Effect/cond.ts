8aa1a6aac852afb6e908b34845de969eda6ea5bd
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-04 14:01:47 +0000

    add condM

--- src/Effect/cond.ts
+++ src/Effect/cond.ts
@@ -1,3 +1,4 @@
+import type { RIO } from "."
 import { chain_, effectTotal } from "./core"
 import type { Effect } from "./effect"
 import { fail } from "./fail"
@@ -22,4 +23,29 @@ export function cond_<E, A>(
   onFalse: () => E
 ): Effect<unknown, E, A> {
   return b ? effectTotal(onTrue) : chain_(effectTotal(onFalse), fail)
+}
+
+/**
+ * Evaluate the predicate,
+ * return the given A as success if predicate returns true,
+ * and the given E as error otherwise
+ */
+export function condM_<R, R2, E, A>(
+  b: boolean,
+  onTrue: RIO<R, A>,
+  onFalse: RIO<R2, E>
+): Effect<R & R2, E, A> {
+  return b ? onTrue : chain_(onFalse, fail)
+}
+
+/**
+ * Evaluate the predicate,
+ * return the given A as success if predicate returns true,
+ * and the given E as error otherwise
+ */
+export function condM<R, R2, E, A>(
+  onTrue: RIO<R, A>,
+  onFalse: RIO<R2, E>
+): (b: boolean) => Effect<R & R2, E, A> {
+  return (b) => condM_(b, onTrue, onFalse)
 }
