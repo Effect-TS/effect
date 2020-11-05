171b5d4a3dfb315d9c0a18938e013314e3d11b97
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-05 14:49:58 +0000

    add methods

--- src/Managed/methods/api.ts
+++ src/Managed/methods/api.ts
@@ -1,14 +1,18 @@
 import type { Cause } from "../../Cause"
 import * as T from "../../Effect"
 import * as E from "../../Either"
-import { pipe, tuple } from "../../Function"
+import { flow, pipe, tuple } from "../../Function"
 import * as O from "../../Option"
 import * as P from "../../Promise"
-import { map_, mapM_ } from "../core"
+import { fail, map_, mapM_ } from "../core"
+import { fromEffect } from "../fromEffect"
 import type { UIO } from "../managed"
 import { Managed } from "../managed"
+import { succeed } from "../succeed"
 import { absolve } from "./absolve"
+import { foldM_ } from "./foldM_"
 import { releaseMap } from "./releaseMap"
+import { suspend } from "./suspend"
 
 /**
  * Unwraps the optional success of this effect, but can fail with None value.
@@ -75,5 +79,78 @@ export function memoize<R, E, A>(self: Managed<R, E, A>): UIO<Managed<R, E, A>> 
 
       return pipe(complete, T.andThen(P.await(promise)), T.toManaged())
     })
+  )
+}
+
+/**
+ * Returns a new effect where the error channel has been merged into the
+ * success channel to their common combined type.
+ */
+export function merge<R, E, A>(self: Managed<R, E, A>) {
+  return foldM_(self, succeed, succeed)
+}
+
+/**
+ * Returns the managed resulting from mapping the success of this managed to unit.
+ */
+export const unit = suspend(() => fromEffect(T.unit))
+
+/**
+ * Requires the option produced by this value to be `None`.
+ */
+export function none<R, E, A>(
+  self: Managed<R, E, O.Option<A>>
+): Managed<R, O.Option<E>, void> {
+  return foldM_(
+    self,
+    flow(O.some, fail),
+    O.fold(
+      () => unit,
+      () => fail(O.none)
+    )
+  )
+}
+
+/**
+ * Folds over the failure value or the success value to yield an effect that
+ * does not fail, but succeeds with the value returned by the left or right
+ * function passed to `fold`.
+ */
+export function fold_<R, E, A, B, C>(
+  self: Managed<R, E, A>,
+  onFail: (e: E) => B,
+  onSuccess: (a: A) => C
+) {
+  return foldM_(self, flow(onFail, succeed), flow(onSuccess, succeed))
+}
+
+/**
+ * Folds over the failure value or the success value to yield an effect that
+ * does not fail, but succeeds with the value returned by the left or right
+ * function passed to `fold`.
+ */
+export function fold<E, A, B, C>(onFail: (e: E) => B, onSuccess: (a: A) => C) {
+  return <R>(self: Managed<R, E, A>) => fold_(self, onFail, onSuccess)
+}
+
+/**
+ * Executes this effect, skipping the error but returning optionally the success.
+ */
+export function option<R, E, A>(
+  self: Managed<R, E, A>
+): Managed<R, never, O.Option<A>> {
+  return fold_(self, () => O.none, O.some)
+}
+
+/**
+ * Converts an option on errors into an option on values.
+ */
+export function optional<R, E, A>(
+  self: Managed<R, O.Option<E>, A>
+): Managed<R, E, O.Option<A>> {
+  return foldM_(
+    self,
+    O.fold(() => succeed(O.none), fail),
+    flow(O.some, succeed)
   )
 }
