a0fd8fdb00402238881e56ce7e93cedca593a80f
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-07 12:45:18 +0000

    add methods

--- packages/system/src/Effect/eventually.ts
+++ packages/system/src/Effect/eventually.ts
@@ -4,6 +4,6 @@ import { orElse_ } from "./orElse"
 /**
  * Returns an effect that ignores errors and runs repeatedly until it eventually succeeds.
  */
-export function eventually<R, E, A>(fa: Effect<R, E, A>): Effect<R, E, A> {
+export function eventually<R, E, A>(fa: Effect<R, E, A>): Effect<R, never, A> {
   return orElse_(fa, () => eventually(fa))
 }
