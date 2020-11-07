98253ca06085fc821b2b09580a9efd082f47e75d
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-07 18:59:34 +0000

    improve provide and add managed methods

--- packages/effect/src/Effect/provideSomeLayer.ts
+++ packages/effect/src/Effect/provideSomeLayer.ts
@@ -1,15 +1,14 @@
-import type { Layer } from "../Layer/core"
-import { build } from "../Layer/definitions"
+import * as L from "../Layer"
 import { use_ } from "../Managed/core"
+import { provideAll_ } from "./core"
 import type { Effect } from "./effect"
-import { provideSome_ } from "./provideSome"
 
 /**
  * Provides a layer to the given effect
  */
-export function provideSomeLayer<R, E, A>(layer: Layer<R, E, A>) {
-  return <R1, E1, A1>(eff: Effect<R1 & A, E1, A1>): Effect<R & R1, E | E1, A1> =>
-    use_(build(layer), (p) => provideSome_(eff, (r: R & R1) => ({ ...r, ...p })))
+export function provideSomeLayer<R, E, A>(layer: L.Layer<R, E, A>) {
+  return <R1, E1, A1>(self: Effect<R1 & A, E1, A1>): Effect<R & R1, E | E1, A1> =>
+    provideLayer_(self, layer["+++"](L.identity()))
 }
 
 /**
@@ -17,7 +16,24 @@ export function provideSomeLayer<R, E, A>(layer: Layer<R, E, A>) {
  */
 export function provideSomeLayer_<R, E, A, R1, E1, A1>(
   eff: Effect<R1 & A, E1, A1>,
-  layer: Layer<R, E, A>
+  layer: L.Layer<R, E, A>
 ): Effect<R & R1, E | E1, A1> {
-  return provideSomeLayer_(eff, layer)
+  return provideSomeLayer(layer)(eff)
+}
+
+/**
+ * Provides a layer to the given effect
+ */
+export function provideLayer_<R, E, A, E1, A1>(
+  self: Effect<A, E1, A1>,
+  layer: L.Layer<R, E, A>
+): Effect<R, E | E1, A1> {
+  return use_(L.build(layer), (p) => provideAll_(self, p))
+}
+
+/**
+ * Provides a layer to the given effect
+ */
+export function provideLayer<R, E, A, E1, A1>(layer: L.Layer<R, E, A>) {
+  return (self: Effect<A, E1, A1>) => provideLayer_(self, layer)
 }
