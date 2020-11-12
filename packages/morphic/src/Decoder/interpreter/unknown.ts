8d4377001a5eb37b23b41a9c3bdfa51aeb2424ca
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-12 12:14:27 +0000

    restore unknown behaviour

--- packages/morphic/src/Decoder/interpreter/unknown.ts
+++ packages/morphic/src/Decoder/interpreter/unknown.ts
@@ -1,24 +1,16 @@
 import * as T from "@effect-ts/core/Sync"
 
 import type { UnknownURI } from "../../Algebra/Unknown"
-import { isUnknownRecord } from "../../Guard/interpreter/common"
 import { interpreter } from "../../HKT"
 import { decoderApplyConfig, DecoderType, DecoderURI } from "../base"
-import { fail, makeDecoder } from "../common"
+import { makeDecoder } from "../common"
 
 export const decoderUnknownInterpreter = interpreter<DecoderURI, UnknownURI>()(() => ({
   _F: DecoderURI,
   unknown: (cfg) => (env) =>
     new DecoderType(
       decoderApplyConfig(cfg?.conf)(
-        makeDecoder(
-          (u, c) =>
-            isUnknownRecord(u)
-              ? T.succeed(u)
-              : fail(u, c, `${typeof u} is not a record`),
-          "unknown",
-          cfg?.name || "unknown"
-        ) as any,
+        makeDecoder(T.succeed, "unknown", cfg?.name || "unknown"),
         env,
         {}
       )
