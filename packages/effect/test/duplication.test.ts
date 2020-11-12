b80cd6762b4d769d3b50de2b32484ef98f5c2ff9
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-12 17:58:36 +0000

    fix record foreach

--- packages/effect/test/duplication.test.ts
+++ packages/effect/test/duplication.test.ts
@@ -1,4 +1,7 @@
 import * as A from "../src/Classic/Array"
+import * as R from "../src/Classic/Record"
+import * as T from "../src/Effect"
+import { pipe } from "../src/Function"
 import * as S from "../src/Sync"
 
 const x: <A>(a: A.Array<A>) => S.UIO<A.Array<A>> = A.foreachF(S.Applicative)(S.succeed)
@@ -6,8 +9,29 @@ const x: <A>(a: A.Array<A>) => S.UIO<A.Array<A>> = A.foreachF(S.Applicative)(S.s
 const res = S.run(x([0, 1, 2]))
 const res1 = S.run(x(res))
 
+const encodeField = T.succeed
+const encodeRecord = R.foreachF(T.Applicative)(encodeField)
+const encodeArray = pipe(
+  [{ x: "a" }, { x: "b" }, { x: "c" }],
+  A.foreachF(T.Applicative)(encodeRecord)
+)
+
 describe("Dupe Bug", () => {
   it("not dupe", () => {
     expect(res1).toEqual([0, 1, 2])
+  })
+  it("not dupe 2", () => {
+    expect(S.run(x([{ value: "A" }, { value: "B" }, { value: "C" }]))).toEqual([
+      { value: "A" },
+      { value: "B" },
+      { value: "C" }
+    ])
+  })
+  it("not repeat", async () => {
+    expect(await T.runPromise(encodeArray)).toEqual([
+      { x: "a" },
+      { x: "b" },
+      { x: "c" }
+    ])
   })
 })
