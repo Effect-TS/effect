cadee80da43945c9d14267e4b13b3356d52e5ff9
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-13 09:45:17 +0000

    add managed methods

--- packages/effect/src/Managed/methods/sandbox.ts
+++
@@ -1,9 +0,0 @@
-import * as T from "../deps"
-import { Managed } from "../managed"
-
-/**
- * Exposes the full cause of failure of this effect.
- */
-export function sandbox<R, E, A>(self: Managed<R, E, A>) {
-  return new Managed(T.sandbox(self.effect))
-}
