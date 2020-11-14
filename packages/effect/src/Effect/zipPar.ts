02589ab3d1afb5961f778f839481994d4b57494a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-14 16:21:39 +0000

    managed zip methods

--- packages/effect/src/Effect/zipPar.ts
+++ packages/effect/src/Effect/zipPar.ts
@@ -1,10 +1,9 @@
 import type { Effect } from "./effect"
-import { zipWithPar_ } from "./zipWithPar_"
+import { zipPar_ } from "./zipPar_"
 
 /**
  * Parallely zips this effects
  */
 export function zipPar<R2, E2, A2>(b: Effect<R2, E2, A2>) {
-  return <R, E, A>(a: Effect<R, E, A>): Effect<R & R2, E | E2, [A, A2]> =>
-    zipWithPar_(a, b, (a, b) => [a, b])
+  return <R, E, A>(a: Effect<R, E, A>): Effect<R & R2, E | E2, [A, A2]> => zipPar_(a, b)
 }
