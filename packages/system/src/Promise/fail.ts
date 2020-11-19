d8da83154f71ea46113e89bdcbed29051a352081
Roger Cyr <roger@rogercyr.com>
2020-11-19 23:36:23 +0100

    Added mergeAllUnbounded and its dependencies

--- packages/system/src/Promise/fail.ts
+++ packages/system/src/Promise/fail.ts
@@ -6,5 +6,11 @@ import type { Promise } from "./promise"
  * Fails the promise with the specified error, which will be propagated to all
  * fibers waiting on the value of the promise.
  */
-export const fail = <E>(e: E) => <A>(promise: Promise<E, A>) =>
+export const fail_ = <E, A>(promise: Promise<E, A>, e: E) =>
   completeWith<E, A>(effectFail(e))(promise)
+
+/**
+ * Fails the promise with the specified error, which will be propagated to all
+ * fibers waiting on the value of the promise.
+ */
+export const fail = <E>(e: E) => <A>(promise: Promise<E, A>) => fail_(promise, e)
