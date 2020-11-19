675593b72093c64244be8456058f6a7cfa58fa9a
Roger Cyr <roger@rogercyr.com>
2020-11-20 00:36:01 +0100

    Added mergeAllUnbounded and its dependencies (#404)
    * Added mergeAllUnbounded and its dependencies
    
    * fix auto-import issue

--- src/Stream/Stream/map.ts
+++ src/Stream/Stream/map.ts
@@ -5,10 +5,18 @@ import { mapChunks } from "./mapChunks"
 /**
  * Transforms the chunks emitted by this stream.
  */
-export const map = <O, O2>(f: (_: O) => O2) => <R, E>(
-  self: Stream<R, E, O>
+export const map_ = <R, E, O, O2>(
+  self: Stream<R, E, O>,
+  f: (_: O) => O2
 ): Stream<R, E, O2> =>
   pipe(
     self,
     mapChunks((o) => o.map(f))
   )
+
+/**
+ * Transforms the chunks emitted by this stream.
+ */
+export const map = <O, O2>(f: (_: O) => O2) => <R, E>(
+  self: Stream<R, E, O>
+): Stream<R, E, O2> => map_(self, f)
