c4f197f3503eeb39e2a82eeb241d34c03dcf4d4b
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-20 23:32:46 +0000

    avoid loop

--- packages/effect/src/Tracing/index.ts
+++ packages/effect/src/Tracing/index.ts
@@ -11,14 +11,28 @@ export function traceF_<F extends Function>(f: F, _trace: string): F {
  * Trace F as the first of inputs
  */
 export function traceAs(...refs: any[]) {
-  return <F extends Function>(f: F): F => {
-    for (const r of refs) {
-      if (r && "$trace" in r) {
-        const g = ((...args: any[]) => f(...args)) as any
-        g["$trace"] = r["$trace"]
-        return g
+  switch (arguments.length) {
+    case 1: {
+      return <F extends Function>(f: F): F => {
+        if ("$trace" in refs[0]) {
+          const g = ((...args: any[]) => f(...args)) as any
+          g["$trace"] = refs[0]["$trace"]
+          return g
+        }
+        return f
       }
     }
-    return f
+    default: {
+      return <F extends Function>(f: F): F => {
+        for (const r of refs) {
+          if (r && "$trace" in r) {
+            const g = ((...args: any[]) => f(...args)) as any
+            g["$trace"] = r["$trace"]
+            return g
+          }
+        }
+        return f
+      }
+    }
   }
 }
