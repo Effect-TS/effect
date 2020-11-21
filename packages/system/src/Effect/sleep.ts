da41f37601107aadf7f42901ccded3c60bdf0f2a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-21 18:47:43 +0000

    trace sleep

--- packages/system/src/Effect/sleep.ts
+++ packages/system/src/Effect/sleep.ts
@@ -1,8 +1,13 @@
 import { sleep as clockSleep } from "../Clock"
+import { foldTraced_, traceF_ } from "../Tracing"
+import { suspend } from "./core"
 
 /**
  * Sleeps for `ms` milliseconds
+ *
+ * @module Effect
+ * @trace replace 0
  */
 export function sleep(ms: number) {
-  return clockSleep(ms)
+  return foldTraced_(ms, (d, t) => suspend(traceF_(() => clockSleep(d), t)))
 }
