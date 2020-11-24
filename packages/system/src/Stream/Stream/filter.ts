b45c289a320f66fcfa2c8dc32793c0dc404894a3
Roger Cyr <roger@rogercyr.com>
2020-11-24 17:14:13 +0100

    Added refinement version to Stream filter

--- packages/system/src/Stream/Stream/filter.ts
+++ packages/system/src/Stream/Stream/filter.ts
@@ -1,5 +1,5 @@
 import * as A from "../../Array"
-import type { Predicate } from "../../Function"
+import type { Predicate, Refinement } from "../../Function"
 import { pipe } from "../../Function"
 import type { Stream } from "./definitions"
 import { mapChunks } from "./mapChunks"
@@ -8,7 +8,15 @@ import { mapChunks } from "./mapChunks"
  * Applies the predicate to each element and allows passing elements
  * to reach the output of this stream.
  */
-export function filter<O>(f: Predicate<O>) {
+export function filter<O, O1 extends O>(
+  f: Refinement<O, O1>
+): <R, E>(self: Stream<R, E, O>) => Stream<R, E, O1>
+export function filter<O>(
+  f: Predicate<O>
+): <R, E>(self: Stream<R, E, O>) => Stream<R, E, O>
+export function filter<O>(
+  f: Predicate<O>
+): <R, E>(self: Stream<R, E, O>) => Stream<R, E, O> {
   return <R, E>(self: Stream<R, E, O>): Stream<R, E, O> => filter_(self, f)
 }
 
@@ -16,6 +24,14 @@ export function filter<O>(f: Predicate<O>) {
  * Applies the predicate to each element and allows passing elements
  * to reach the output of this stream.
  */
+export function filter_<R, E, O, O1 extends O>(
+  self: Stream<R, E, O>,
+  f: Refinement<O, O1>
+): Stream<R, E, O1>
+export function filter_<R, E, O>(
+  self: Stream<R, E, O>,
+  f: Predicate<O>
+): Stream<R, E, O>
 export function filter_<R, E, O>(
   self: Stream<R, E, O>,
   f: Predicate<O>
