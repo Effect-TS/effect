3cca2fb31833d56d4b92fa05feee03b2724cbaed
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-25 17:33:24 +0000

    continue with data first

--- src/Effect/tap.ts
+++ src/Effect/tap.ts
@@ -1,12 +1,25 @@
-import { chain } from "./core"
+import { chain, chain_ } from "./core"
 import type { Effect } from "./effect"
 import { map } from "./map"
+import { map_ } from "./map_"
 
 /**
  * Returns an effect that effectfully "peeks" at the success of this effect.
+ *
+ * @dataFirst tap_
  */
 export function tap<A, R, E>(
   f: (_: A) => Effect<R, E, any>
 ): <E2, R2>(_: Effect<R2, E2, A>) => Effect<R & R2, E | E2, A> {
   return chain((a: A) => map(() => a)(f(a)))
+}
+
+/**
+ * Returns an effect that effectfully "peeks" at the success of this effect.
+ */
+export function tap_<E2, R2, A, R, E>(
+  _: Effect<R2, E2, A>,
+  f: (_: A) => Effect<R, E, any>
+) {
+  return chain_(_, (a: A) => map_(f(a), () => a))
 }
--- src/Managed/do.ts
+++ src/Managed/do.ts
@@ -2,11 +2,59 @@ import { chain_, map_ } from "./core"
 import type { Managed } from "./managed"
 import { succeed } from "./succeed"
 
-const bind = <R, E, A, K, N extends string>(
+/**
+ * @dataFirst bind_
+ */
+export function bind<R, E, A, K, N extends string>(
   tag: Exclude<N, keyof K>,
   f: (_: K) => Managed<R, E, A>
-) => <R2, E2>(mk: Managed<R2, E2, K>): Managed<R & R2, E | E2, K & { [k in N]: A }> =>
-  chain_(mk, (k) => map_(f(k), (a): K & { [k in N]: A } => ({ ...k, [tag]: a } as any)))
+) {
+  return <R2, E2>(
+    mk: Managed<R2, E2, K>
+  ): Managed<
+    R & R2,
+    E | E2,
+    K &
+      {
+        [k in N]: A
+      }
+  > =>
+    chain_(mk, (k) =>
+      map_(
+        f(k),
+        (
+          a
+        ): K &
+          {
+            [k in N]: A
+          } => ({ ...k, [tag]: a } as any)
+      )
+    )
+}
+export function bind_<R2, E2, R, E, A, K, N extends string>(
+  mk: Managed<R2, E2, K>,
+  tag: Exclude<N, keyof K>,
+  f: (_: K) => Managed<R, E, A>
+): Managed<
+  R & R2,
+  E | E2,
+  K &
+    {
+      [k in N]: A
+    }
+> {
+  return chain_(mk, (k) =>
+    map_(
+      f(k),
+      (
+        a
+      ): K &
+        {
+          [k in N]: A
+        } => ({ ...k, [tag]: a } as any)
+    )
+  )
+}
 
 const let_ = <A, K, N extends string>(tag: Exclude<N, keyof K>, f: (_: K) => A) => <
   R2,
@@ -18,4 +66,4 @@ const let_ = <A, K, N extends string>(tag: Exclude<N, keyof K>, f: (_: K) => A) 
 
 const do_ = succeed({})
 
-export { let_ as let, bind, do_ as do }
+export { let_ as let, do_ as do }
