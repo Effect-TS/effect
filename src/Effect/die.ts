b6e2693b5ca4900d62cc869963761730405690ff
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-25 23:36:35 +0000

    continue selective tracing

--- src/Effect/die.ts
+++ src/Effect/die.ts
@@ -5,6 +5,8 @@ import { halt } from "./core"
  * Returns an effect that dies with the specified `unknown`.
  * This method can be used for terminating a fiber because a defect has been
  * detected in the code.
+ *
+ * @trace
  */
 export function die(e: unknown) {
   return halt(Die(e))
