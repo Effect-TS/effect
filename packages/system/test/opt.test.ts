a9afc3e208962d1864ecd6188e4de9af52eb17f6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-26 00:16:44 +0000

    continue

--- packages/system/test/opt.test.ts
+++ packages/system/test/opt.test.ts
@@ -13,10 +13,9 @@ describe("Optimizations", () => {
         T.chain((n) => T.succeed(n + 1)),
         T.chain((n) => T.succeed(n + 1)),
         T.tap(T.fail),
-        T.foldM(
-          (n) => T.succeed(n),
-          (n) => T.succeed(n + 1)
-        ),
+        T.catchAll(function handle(n) {
+          return T.succeed(n)
+        }),
         T.andThen(T.checkExecutionTraces(T.succeed)),
         T.runPromise
       )
@@ -25,7 +24,7 @@ describe("Optimizations", () => {
       "packages/system/test/opt.test.ts:13:24:anonymous",
       "packages/system/test/opt.test.ts:14:24:anonymous",
       "packages/system/src/Effect/fail.ts:10:31:fail",
-      "packages/system/test/opt.test.ts:17:18:anonymous",
+      "packages/system/test/opt.test.ts:16:39:handle",
       "packages/system/src/Effect/core.ts:275:61:succeed"
     ])
   })
