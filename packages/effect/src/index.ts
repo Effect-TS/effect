1aa1fde750788e4b1e1d29c44b44657061bac249
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-27 18:38:31 +0000

    trace call

---
+++ packages/effect/src/index.ts
@@ -0,0 +1,58 @@
+export function traceAs<F extends Function>(g: any, f: F): F {
+  if (g["$trace"]) {
+    f["$trace"] = g["$trace"]
+  }
+  return f
+}
+
+const traces = new Map<string, string>()
+
+export function traceCall<T>(call: () => T, name: string, trace: string) {
+  traces.set(name, trace)
+  const x = call()
+  traces.delete(name)
+  return x
+}
+
+export function traceAsFrom<F extends Function>(
+  name: string,
+  g: any,
+  f: F,
+  clean = true
+): F {
+  const x = traces.get(name)
+  if (x) {
+    f["$trace"] = x
+    if (clean) {
+      traces.delete(name)
+    }
+  } else if (g["$trace"]) {
+    f["$trace"] = g["$trace"]
+  }
+  return f
+}
+
+export function traceFrom<F extends Function>(name: string, f: F, clean = true): F {
+  const x = traces.get(name)
+  if (x) {
+    f["$trace"] = x
+    if (clean) {
+      traces.delete(name)
+    }
+  }
+  return f
+}
+
+export function accessCallTrace(name: string, clean = true) {
+  const x = traces.get(name)
+
+  if (clean) {
+    traces.delete(name)
+  }
+
+  if (x) {
+    return x
+  }
+
+  return undefined
+}
