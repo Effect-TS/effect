410aaa543f9005095ce2eca3a782567b697c0cd2
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-28 17:41:48 +0000

    optimise ringbuffer

--- src/Support/RingBuffer/index.ts
+++ src/Support/RingBuffer/index.ts
@@ -1,29 +1,29 @@
 import * as L from "../../List"
+import { DoublyLinkedList } from "../DublyLinkedList"
 
 export class RingBuffer<T> {
-  private values = L.empty<T>()
+  private values = new DoublyLinkedList<T>()
 
   constructor(readonly size: number) {}
 
   push(value: T) {
     if (this.values.length >= this.size) {
-      this.pop()
+      this.values.shift()
     }
-    this.values = L.append_(this.values, value)
+    this.values.add(value)
     return this.values
   }
 
   pop() {
-    this.values = L.drop_(this.values, 1)
+    this.values.pop()
     return this.values
   }
 
-  popHead() {
-    this.values = L.dropLast_(this.values, 1)
-    return this.values
-  }
-
-  get list() {
-    return this.values
+  get list(): L.List<T> {
+    const l = L.emptyPushable<T>()
+    this.values.forEach((t) => {
+      L.push(t, l)
+    })
+    return l
   }
 }
