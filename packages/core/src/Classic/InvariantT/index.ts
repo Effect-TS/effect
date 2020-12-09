69185c15c93991290700478f76e5e8a4c4f9be82
Michael Arnaldi <arnaldimichael@gmail.com>
2020-12-09 22:05:26 +0000

    improve clean

--- packages/core/src/Classic/InvariantT/index.ts
+++ packages/core/src/Classic/InvariantT/index.ts
@@ -4,11 +4,11 @@ import type * as HKT from "../../Prelude/HKT"
 export function monad<P extends HKT.Param>(_: P) {
   return <F extends HKT.URIS, C>(
     M: P.Monad<F, C>
-  ): P.Monad<F, HKT.Strip<C, P> & P.V<P, "_">> => P.instance(M)
+  ): P.Monad<F, HKT.CleanParam<C, P> & P.V<P, "_">> => P.instance(M)
 }
 
 export function applicative<P extends HKT.Param>(_: P) {
   return <F extends HKT.URIS, C>(
     M: P.Applicative<F, C>
-  ): P.Applicative<F, HKT.Strip<C, P> & P.V<P, "_">> => P.instance(M)
+  ): P.Applicative<F, HKT.CleanParam<C, P> & P.V<P, "_">> => P.instance(M)
 }
