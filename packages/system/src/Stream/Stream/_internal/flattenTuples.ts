59f431bdfa2dab63beececb09317af09624ab37b
Roger Cyr <roger@rogercyr.com>
2020-12-21 15:26:30 +0100

    make eslint happy. Fixed flattenTuples type

--- packages/system/src/Stream/Stream/_internal/flattenTuples.ts
+++ packages/system/src/Stream/Stream/_internal/flattenTuples.ts
@@ -2,8 +2,12 @@ import type * as A from "../../../Array"
 
 type RecursiveTuples<T> = readonly [T | RecursiveTuples<T>, T]
 
+function isTuple(t: unknown): t is readonly [unknown, unknown] {
+  return Array.isArray(t) && t.length === 2
+}
+
 export function flattenTuples<T>([a, b]: RecursiveTuples<T>): A.Array<T> {
-  if (Array.isArray(a)) {
+  if (isTuple(a)) {
     return [...flattenTuples(a), b]
   }
 
