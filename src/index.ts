533eae27752021b5c36104e6f3f4888701f6744e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-12-31 15:31:38 +0000

    improve tracing

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,23 @@
+import { isTracingEnabled } from "./Global"
+
 export function traceAs<F extends Function>(g: any, f: F): F {
-  if (g["$trace"]) {
+  if (g["$trace"] && isTracingEnabled()) {
     f["$trace"] = g["$trace"]
   }
   return f
 }
 
-const traces = new Map<string, string>()
-
-export function traceCall<T>(call: () => T, name: string, trace: string) {
-  traces.set(name, trace)
-  const x = call()
-  traces.delete(name)
-  return x
-}
-
-export function traceAsFrom<F extends Function>(
-  name: string,
-  g: any,
-  f: F,
-  clean = true
-): F {
-  const x = traces.get(name)
-  if (x) {
-    f["$trace"] = x
-    if (clean) {
-      traces.delete(name)
-    }
-  } else if (g["$trace"]) {
-    f["$trace"] = g["$trace"]
+export function traceCall<F extends Function>(call: F, trace: string): F {
+  if (!isTracingEnabled()) {
+    return call
   }
-  return f
-}
-
-export function traceFrom<F extends Function>(name: string, f: F, clean = true): F {
-  const x = traces.get(name)
-  if (x) {
-    f["$trace"] = x
-    if (clean) {
-      traces.delete(name)
-    }
-  }
-  return f
-}
-
-export function accessCallTrace(name: string, clean = true) {
-  const x = traces.get(name)
-
-  if (clean) {
-    traces.delete(name)
-  }
-
-  if (x) {
+  return ((...args: any[]) => {
+    const y = call["$trace"]
+    call["$trace"] = trace
+    const x = call(...args)
+    call["$trace"] = y
     return x
-  }
-
-  return undefined
+  }) as any
 }
 
 export * from "./Global"
