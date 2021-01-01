95f7cc9d62fdcd65e54b49557f954ea2ae5471b7
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-01 18:18:14 +0000

    fixes for traceCall

--- test/demo.test.ts
+++ test/demo.test.ts
@@ -1,4 +1,8 @@
+import "@effect-ts/tracing-utils/Enable"
+
+import { pretty } from "../src/Cause"
 import * as T from "../src/Effect"
+import { assertsFailure } from "../src/Exit"
 import { flow, pipe } from "../src/Function"
 
 const addOne = (n: number) => T.effectTotal(() => n + 1)
@@ -12,4 +16,19 @@ it("should not break", async () => {
       T.runPromise
     )
   ).toEqual(0)
+})
+
+it("should trace generator", async () => {
+  const exit = await pipe(
+    T.gen(function* (_) {
+      const a = yield* _(T.succeed(1))
+      const b = yield* _(T.succeed(2))
+      return a + b
+    }),
+    T.chain(T.succeed),
+    T.chain(T.fail),
+    T.runPromiseExit
+  )
+  assertsFailure(exit)
+  console.log(pretty(exit.cause))
 })
