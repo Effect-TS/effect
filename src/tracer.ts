f26773923e57929a03bf621ee10f7a7bd975a27a
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-09 19:00:50 +0000

    work in progress

--- src/tracer.ts
+++ src/tracer.ts
@@ -39,6 +39,8 @@ export default function tracer(
   const tracingOn = !(_opts?.tracing === false)
   const checker = _program.getTypeChecker()
 
+  checker
+
   const moduleMap = _opts?.moduleMap || {}
   const moduleMapKeys = Object.keys(moduleMap).map((k) => [k, new RegExp(k)] as const)
 
@@ -48,10 +50,9 @@ export default function tracer(
 
       return (sourceFile: ts.SourceFile) => {
         const sourceFullText = sourceFile.getFullText()
-        const traceF = factory.createUniqueName("trace")
-        const traced = new Set<string>()
+        const traced = factory.createIdentifier("traceCall")
+        const traceFrom = factory.createIdentifier("traceFrom")
         const fileVar = factory.createUniqueName("fileName")
-        const traceVar = factory.createUniqueName("$trace")
         const tracing = factory.createUniqueName("tracing")
 
         const isModule =
@@ -60,6 +61,12 @@ export default function tracer(
         if (!isModule) {
           return sourceFile
         }
+
+        const tracedIdentifier = factory.createPropertyAccessExpression(tracing, traced)
+        const traceFromIdentifier = factory.createPropertyAccessExpression(
+          tracing,
+          traceFrom
+        )
 
         const regions = sourceFullText
           .split("\n")
@@ -75,55 +82,37 @@ export default function tracer(
           })
           .filter(([x]) => x.length > 0)
 
-        function traceCall(node: ts.Expression, isTracing: boolean) {
-          const symbol = checker.getTypeAtLocation(node).getSymbol()
+        const { fileName } = sourceFile
 
-          const traceCallTags =
-            symbol
-              ?.getDeclarations()
-              ?.map((e) => {
-                try {
-                  return ts
-                    .getAllJSDocTags(
-                      e,
-                      (t): t is ts.JSDocTag => t.tagName.getText() === "traceCall"
-                    )
-                    .map((e) => e.comment)
-                } catch {
-                  return []
-                }
-              })
-              .reduce((flatten, entry) => flatten.concat(entry), []) || []
+        let finalName = path.relative(process.cwd(), fileName)
 
-          const shouldTrace = traceCallTags.length > 0 && isTracing
-
-          if (shouldTrace) {
-            const nodeEnd = sourceFile.getLineAndCharacterOfPosition(node.getEnd())
-            return factory.createCallExpression(
-              factory.createPropertyAccessExpression(
-                tracing,
-                factory.createIdentifier("traceCall")
-              ),
-              undefined,
-              [
-                node,
-                factory.createBinaryExpression(
-                  fileVar,
-                  factory.createToken(ts.SyntaxKind.PlusToken),
-                  factory.createStringLiteral(
-                    `:${nodeEnd.line + 1}:${nodeEnd.character + 1}:${
-                      ts.isIdentifier(node)
-                        ? node.getText()
-                        : ts.isPropertyAccessExpression(node)
-                        ? node.name.getText()
-                        : "unknown"
-                    }`
-                  )
-                )
-              ]
+        function getTrace(node: ts.Node, pos: "start" | "end") {
+          const nodeStart = sourceFile.getLineAndCharacterOfPosition(
+            pos === "start" ? node.getStart() : node.getEnd()
+          )
+          return factory.createBinaryExpression(
+            fileVar,
+            factory.createToken(ts.SyntaxKind.PlusToken),
+            factory.createStringLiteral(
+              `:${nodeStart.line + 1}:${nodeStart.character + 1}`
             )
-          }
-          return node
+          )
+        }
+
+        function traceChild(
+          tags: Record<string, (string | undefined)[]>,
+          i: number,
+          factory: ts.NodeFactory,
+          traceFromIdentifier: ts.PropertyAccessExpression,
+          getTrace: (node: ts.Node, pos: "start" | "end") => ts.BinaryExpression,
+          x: ts.Expression
+        ): ts.Expression {
+          return tags["trace"] && tags["trace"].includes(`${i}`)
+            ? factory.createCallExpression(traceFromIdentifier, undefined, [
+                getTrace(x, "start"),
+                ts.visitEachChild(x, visitor, ctx)
+              ])
+            : ts.visitEachChild(x, visitor, ctx)
         }
 
         function visitor(node: ts.Node): ts.VisitResult<ts.Node> {
@@ -132,230 +121,97 @@ export default function tracer(
           const isTracing =
             tracingOn && checkRegionAt(regions, nodeStart.line, nodeStart.character)
 
-          if (ts.isCallExpression(node)) {
-            const symbol = checker.getTypeAtLocation(node.expression).getSymbol()
+          if (ts.isCallExpression(node) && isTracing) {
+            const signature = checker.getResolvedSignature(node)
 
-            const overloadDeclarations = checker
-              .getResolvedSignature(node)
-              ?.getDeclaration()
+            const entries: (readonly [string, string | undefined])[] =
+              signature?.getJsDocTags().map((t) => [t.name, t.text] as const) || []
+            const tags: Record<string, (string | undefined)[]> = {}
 
-            const traceRetTagsOverload = overloadDeclarations
-              ? (() => {
-                  try {
-                    return ts
-                      .getAllJSDocTags(
-                        overloadDeclarations,
-                        (t): t is ts.JSDocTag => t.tagName.getText() === "traceRet"
-                      )
-                      .map((e) => e.comment)
-                      .filter((s): s is string => s != null)
-                  } catch {
-                    return undefined
-                  }
-                })()
-              : undefined
-
-            const traceRetTagsMain =
-              symbol
-                ?.getDeclarations()
-                ?.map((e) => {
-                  try {
-                    return ts
-                      .getAllJSDocTags(
-                        e,
-                        (t): t is ts.JSDocTag => t.tagName.getText() === "traceRet"
-                      )
-                      .map((e) => e.comment)
-                  } catch {
-                    return []
-                  }
-                })
-                .reduce((flatten, entry) => flatten.concat(entry), []) || []
-
-            const traceRetTags = new Set([
-              ...traceRetTagsMain,
-              ...(traceRetTagsOverload || [])
-            ])
-
-            const shouldTrace = traceRetTags.size > 0 && isTracing
-
-            const child_ = ts.visitEachChild(node, visitor, ctx)
-            const child = factory.createCallExpression(
-              traceCall(child_.expression, isTracing),
-              child_.typeArguments,
-              child_.arguments.map((ex) => traceCall(ex, isTracing))
-            )
-
-            if (shouldTrace) {
-              return factory.createCallExpression(traceF, undefined, [
-                child,
-                factory.createStringLiteral(
-                  `:${nodeStart.line + 1}:${nodeStart.character + 1}:${
-                    ts.isVariableDeclaration(node.parent)
-                      ? node.parent.name.getText()
-                      : ts.isPropertyDeclaration(node.parent)
-                      ? node.parent.name.getText()
-                      : "anonymous"
-                  }`
-                )
-              ])
-            } else {
-              return child
+            for (const entry of entries) {
+              if (!tags[entry[0]]) {
+                tags[entry[0]] = []
+              }
+              tags[entry[0]].push(entry[1])
             }
-          }
 
-          if (ts.isClassDeclaration(node) && node.name) {
-            if (isTracing) {
-              const members: [ts.Identifier, ts.StringLiteral][] = []
-
-              node.members
-                .filter((t) => !ts.isConstructorDeclaration(t))
-                .forEach((m) => {
-                  const { character, line } = sourceFile.getLineAndCharacterOfPosition(
-                    m.getStart()
+            if (signature && tags["trace"] && tags["trace"].includes("call")) {
+              return factory.createCallExpression(
+                factory.createCallExpression(tracedIdentifier, undefined, [
+                  ts.visitEachChild(node.expression, visitor, ctx),
+                  factory.createArrayLiteralExpression(
+                    [
+                      getTrace(node.expression, "end"),
+                      ...node.arguments.map((x) => getTrace(x, "start"))
+                    ],
+                    false
                   )
-                  if (m.name && ts.isIdentifier(m.name) && ts.isMethodDeclaration(m)) {
-                    members.push([
-                      m.name,
-                      factory.createStringLiteral(
-                        `:${line + 1}:${
-                          character + 1 + m.name.getText().length
-                        }:${m.name.getText()}`
-                      )
-                    ])
-                  }
-                })
+                ]),
+                undefined,
+                node.arguments.map((x, i) =>
+                  traceChild(tags, i, factory, traceFromIdentifier, getTrace, x)
+                )
+              )
+            }
 
-              const cl = ts.visitEachChild(node, visitor, ctx)
+            if (signature && tags["trace"] && tags["trace"].includes("args")) {
+              return factory.updateCallExpression(
+                node,
+                node.expression,
+                node.typeArguments,
+                node.arguments.map((x, i) => {
+                  const symbol = checker.getSymbolAtLocation(x)
+                  const declaration = symbol?.valueDeclaration
+                  if (declaration) {
+                    if (ts.isFunctionDeclaration(declaration)) {
+                      const argumentTags =
+                        symbol?.getJsDocTags().map((t) => t.name) || []
 
-              return factory.createClassDeclaration(
-                cl.decorators,
-                cl.modifiers,
-                cl.name,
-                cl.typeParameters,
-                cl.heritageClauses,
-                cl.members.map((m) => {
-                  if (ts.isConstructorDeclaration(m) && m.body) {
-                    return factory.updateConstructorDeclaration(
-                      m,
-                      m.decorators,
-                      m.modifiers,
-                      m.parameters,
-                      factory.updateBlock(m.body, [
-                        ...m.body.statements,
-                        ...members.map(([id, trace]) => {
-                          return factory.createExpressionStatement(
-                            factory.createBinaryExpression(
-                              factory.createElementAccessExpression(
-                                factory.createPropertyAccessExpression(
-                                  factory.createThis(),
-                                  id
-                                ),
-                                traceVar
-                              ),
-                              factory.createToken(ts.SyntaxKind.EqualsToken),
-                              factory.createBinaryExpression(
-                                fileVar,
-                                factory.createToken(ts.SyntaxKind.PlusToken),
-                                trace
-                              )
+                      if (argumentTags.includes("trace")) {
+                        return factory.createCallExpression(
+                          tracedIdentifier,
+                          undefined,
+                          [
+                            traceChild(
+                              tags,
+                              i,
+                              factory,
+                              traceFromIdentifier,
+                              getTrace,
+                              x
+                            ),
+                            factory.createArrayLiteralExpression(
+                              [
+                                getTrace(x, "end"),
+                                ...declaration.parameters.map(() => getTrace(x, "end"))
+                              ],
+                              false
                             )
-                          )
-                        })
-                      ])
-                    )
+                          ]
+                        )
+                      }
+                    }
                   }
-                  return m
+
+                  return traceChild(tags, i, factory, traceFromIdentifier, getTrace, x)
                 })
               )
             }
-          }
 
-          if (ts.isArrowFunction(node)) {
-            if (isTracing) {
-              const { character, line } = sourceFile.getLineAndCharacterOfPosition(
-                node.body?.getStart() || node.getStart()
+            if (signature && tags["trace"] && tags["trace"]) {
+              return factory.updateCallExpression(
+                node,
+                ts.visitEachChild(node.expression, visitor, ctx),
+                node.typeArguments,
+                node.arguments.map((x, i) =>
+                  traceChild(tags, i, factory, traceFromIdentifier, getTrace, x)
+                )
               )
-
-              return factory.createCallExpression(traceF, undefined, [
-                ts.visitEachChild(node, visitor, ctx),
-                factory.createStringLiteral(
-                  `:${line + 1}:${character + 1}:${
-                    ts.isVariableDeclaration(node.parent)
-                      ? node.parent.name.getText()
-                      : ts.isPropertyDeclaration(node.parent)
-                      ? node.parent.name.getText()
-                      : "anonymous"
-                  }`
-                )
-              ])
-            }
-          }
-
-          if (ts.isFunctionExpression(node)) {
-            if (isTracing) {
-              if ((node.name && !traced.has(node.name.getText())) || !node.name) {
-                if (node.name) {
-                  traced.add(node.name.getText())
-                }
-                const { character, line } = sourceFile.getLineAndCharacterOfPosition(
-                  node.body?.getStart() || node.getStart()
-                )
-
-                return factory.createCallExpression(traceF, undefined, [
-                  ts.visitEachChild(node, visitor, ctx),
-                  factory.createStringLiteral(
-                    `:${line + 1}:${character + 1}:${
-                      node.name?.getText() ||
-                      (ts.isVariableDeclaration(node.parent)
-                        ? node.parent.name.getText()
-                        : "anonymous")
-                    }`
-                  )
-                ])
-              }
-            }
-          }
-
-          if (ts.isFunctionDeclaration(node)) {
-            if (isTracing) {
-              if ((node.name && !traced.has(node.name.getText())) || !node.name) {
-                if (node.name) {
-                  traced.add(node.name.getText())
-                }
-
-                const { character, line } = sourceFile.getLineAndCharacterOfPosition(
-                  node.body?.getStart() || node.getStart()
-                )
-
-                const name = node.name
-
-                if (name) {
-                  return [
-                    ts.visitEachChild(node, visitor, ctx),
-                    factory.createBinaryExpression(
-                      factory.createElementAccessExpression(name, traceVar),
-                      factory.createToken(ts.SyntaxKind.EqualsToken),
-                      factory.createBinaryExpression(
-                        fileVar,
-                        factory.createToken(ts.SyntaxKind.PlusToken),
-                        factory.createStringLiteral(
-                          `:${line + 1}:${character + 1}:${name.getText()}`
-                        )
-                      )
-                    )
-                  ]
-                }
-              }
             }
           }
 
           return ts.visitEachChild(node, visitor, ctx)
         }
-
-        const { fileName } = sourceFile
-
-        let finalName = path.relative(process.cwd(), fileName)
 
         for (const k of moduleMapKeys) {
           const matches = finalName.match(k[1])
@@ -384,73 +240,6 @@ export default function tracer(
           )
         )
 
-        const traceNode = factory.createVariableStatement(
-          undefined,
-          factory.createVariableDeclarationList(
-            [
-              factory.createVariableDeclaration(
-                traceVar,
-                undefined,
-                undefined,
-                factory.createStringLiteral("$trace")
-              )
-            ],
-            ts.NodeFlags.Const
-          )
-        )
-
-        const traceFNode = factory.createFunctionDeclaration(
-          undefined,
-          undefined,
-          undefined,
-          traceF,
-          undefined,
-          [
-            factory.createParameterDeclaration(
-              undefined,
-              undefined,
-              undefined,
-              factory.createIdentifier("f"),
-              undefined,
-              undefined,
-              undefined
-            ),
-            factory.createParameterDeclaration(
-              undefined,
-              undefined,
-              undefined,
-              factory.createIdentifier("t"),
-              undefined,
-              undefined,
-              undefined
-            )
-          ],
-          undefined,
-          factory.createBlock(
-            [
-              factory.createIfStatement(
-                factory.createIdentifier("f"),
-                factory.createExpressionStatement(
-                  factory.createBinaryExpression(
-                    factory.createElementAccessExpression(
-                      factory.createIdentifier("f"),
-                      traceVar
-                    ),
-                    factory.createToken(ts.SyntaxKind.EqualsToken),
-                    factory.createBinaryExpression(
-                      fileVar,
-                      factory.createToken(ts.SyntaxKind.PlusToken),
-                      factory.createIdentifier("t")
-                    )
-                  )
-                )
-              ),
-              factory.createReturnStatement(factory.createIdentifier("f"))
-            ],
-            true
-          )
-        )
-
         if (tracingOn) {
           const visited = ts.visitEachChild(sourceFile, visitor, ctx)
 
@@ -465,9 +254,7 @@ export default function tracer(
               ),
               factory.createStringLiteral("@effect-ts/tracing-utils")
             ),
-            traceNode,
             fileNode,
-            traceFNode,
             ...visited.statements
           ])
         }
