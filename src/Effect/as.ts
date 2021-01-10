cd8bafa8fa1517cfa6413f663ebbd21e142714c5
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-10 15:59:33 +0000

    continue

--- src/Effect/as.ts
+++ src/Effect/as.ts
@@ -1,16 +1,29 @@
+// tracing: off
+import { accessCallTrace, traceCall, traceFrom } from "@effect-ts/tracing-utils"
+
 import type { Effect } from "./effect"
 import { map_ } from "./map"
 
 /**
  * Maps the success value of this effect to the specified constant value.
+ *
+ * @trace call
  */
 export function as_<R, E, A, B>(self: Effect<R, E, A>, b: B) {
-  return map_(self, () => b)
+  const trace = accessCallTrace()
+  return map_(
+    self,
+    traceFrom(trace, () => b)
+  )
 }
 
 /**
  * Maps the success value of this effect to the specified constant value.
+ *
+ * @dataFirst as_
+ * @trace call
  */
 export function as<B>(b: B) {
-  return <R, E, A>(self: Effect<R, E, A>) => map_(self, () => b)
+  const trace = accessCallTrace()
+  return traceCall(<R, E, A>(self: Effect<R, E, A>) => as_(self, b), trace)
 }
