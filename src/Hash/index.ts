28ff4c0a55ecdbcdf585020d083e622e0b37bd3c
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-11 12:03:04 +0000

    move hash

--- src/Hash/index.ts
+++ src/Hash/index.ts
@@ -4,3 +4,18 @@
 export interface Hash<A> {
   readonly hash: (x: A) => number
 }
+
+/**
+ * Get 32 bit hash of string.
+ *
+ * Based on:
+ * http://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+ */
+export function string(str: string) {
+  let hash = 0
+  for (let i = 0, len = str.length; i < len; ++i) {
+    const c = str.charCodeAt(i)
+    hash = ((hash << 5) - hash + c) | 0
+  }
+  return hash
+}
