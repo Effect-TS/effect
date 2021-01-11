65990e352826af6022276970c5ea4589f4d93fb5
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-11 02:27:40 +0000

    restart

--- packages/system/test/persistent.test.ts
+++ packages/system/test/persistent.test.ts
@@ -1,4 +1,6 @@
 import { pipe, tuple } from "../src/Function"
+import type { Equatable } from "../src/Persistent/_internal/Structural"
+import { equalsSymbol } from "../src/Persistent/_internal/Structural"
 import * as HM from "../src/Persistent/HashMap"
 
 describe("HashMap", () => {
@@ -32,5 +34,32 @@ describe("HashMap", () => {
       })
     )
     expect(map).toEqual(HM.fromObject({ 0: "ok", 1: "ko", 2: "no", 3: "oo", 4: "oo" }))
+  })
+  it("use hash-map 4", () => {
+    class Index implements Equatable {
+      constructor(readonly a: number, readonly b: number) {}
+
+      [equalsSymbol](other: any): boolean {
+        return other instanceof Index && other.a === this.a && other.b === this.b
+      }
+    }
+    interface Value {
+      c: number
+      d: number
+    }
+    const map = pipe(
+      HM.empty<Index, Value>(),
+      HM.set<Index, Value>(new Index(0, 1), { c: 0, d: 0 }),
+      HM.set<Index, Value>(new Index(0, 1), { c: 1, d: 1 }),
+      HM.set<Index, Value>(new Index(1, 1), { c: 0, d: 0 }),
+      HM.set<Index, Value>(new Index(10, 1), { c: 3, d: 2 }),
+      HM.set<Index, Value>(new Index(10, 1), { c: 3, d: 3 })
+    )
+
+    console.log(Array.from(map))
+
+    expect(HM.get_(map, new Index(0, 1))).toEqual({ c: 1, d: 1 })
+    expect(HM.get_(map, new Index(1, 1))).toEqual({ c: 0, d: 0 })
+    //expect(HM.get_(map, new Index(1, 1))).toEqual({ c: 0, d: 0 })
   })
 })
