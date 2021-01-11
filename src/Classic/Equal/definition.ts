2661e250bebfefa20524336f900548610e59436d
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-11 15:00:42 +0000

    reindex

--- src/Classic/Equal/definition.ts
+++ src/Classic/Equal/definition.ts
@@ -1,10 +1,1 @@
-/**
- * `Equal[A]` provides implicit evidence that two values of type `A` can be
- * compared for equality.
- */
-export interface Equal<A> {
-  /**
-   * Returns whether two values of type `A` are equal.
-   */
-  readonly equals: (y: A) => (x: A) => boolean
-}
+export * from "@effect-ts/system/Equal"
