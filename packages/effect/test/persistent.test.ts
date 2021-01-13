c00e86d10d33d000e98c25fc43941bdeb810ca53
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-13 22:06:14 +0000

    relax set operations

--- packages/effect/test/persistent.test.ts
+++ packages/effect/test/persistent.test.ts
@@ -3,6 +3,7 @@ import { pipe, tuple } from "../src/Function"
 import * as Hash from "../src/Hash"
 import * as O from "../src/Option"
 import * as HM from "../src/Persistent/HashMap"
+import * as HS from "../src/Persistent/HashSet"
 
 describe("HashMap", () => {
   it("use hash-map 4", () => {
@@ -114,6 +115,46 @@ describe("HashMap", () => {
     expect(Array.from(j)).toEqual([
       [x, new Value(0, 3)],
       [x, new Value(0, 2)]
+    ])
+  })
+  it("keySet", () => {
+    class Index {
+      constructor(readonly a: number, readonly b: number) {}
+    }
+    class Value {
+      constructor(readonly c: number, readonly d: number) {}
+    }
+    const eqIndex: Equal<Index> = {
+      equals: (y) => (x) => x === y || (x.a === y.a && x.b === y.b)
+    }
+    const hashIndex: Hash.Hash<Index> = {
+      hash: (x) => Hash.string(`${x.a}-${x.b}`)
+    }
+    const makeMap = () =>
+      HM.make<Index, Value>({
+        ...eqIndex,
+        ...hashIndex
+      })
+    const map = pipe(
+      makeMap(),
+      HM.set(new Index(0, 0), new Value(0, 0)),
+      HM.set(new Index(0, 0), new Value(1, 1)),
+      HM.set(new Index(1, 1), new Value(2, 2)),
+      HM.set(new Index(1, 1), new Value(3, 3)),
+      HM.set(new Index(0, 0), new Value(4, 4)),
+      HM.remove(new Index(1, 1)),
+      HM.mutate((m) => {
+        HM.set_(m, new Index(2, 2), new Value(5, 5))
+        HM.set_(m, new Index(3, 3), new Value(6, 6))
+      })
+    )
+    const keys = HM.keySet(map)
+    const diff = HS.difference_(keys, [new Index(2, 2)])
+    expect(Array.from(diff)).toEqual([new Index(0, 0), new Index(3, 3)])
+    expect(Array.from(keys)).toEqual([
+      new Index(0, 0),
+      new Index(2, 2),
+      new Index(3, 3)
     ])
   })
 })
