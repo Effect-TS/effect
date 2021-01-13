b6feeb8b96bbbf042962d25de049b16711ac10f5
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-13 15:36:56 +0000

    continue apis

--- src/Hash/index.ts
+++ src/Hash/index.ts
@@ -1,3 +1,5 @@
+import { PCGRandom } from "../Persistent/HashMap/Random"
+
 /**
  * `Hash[A]` provides a way to hash a value
  */
@@ -18,4 +20,28 @@ export function string(str: string) {
     hash = ((hash << 5) - hash + c) | 0
   }
   return hash
+}
+
+const RANDOM = new PCGRandom(13)
+const CACHE = new WeakMap<Object, number>()
+
+function randomInt() {
+  return RANDOM.integer(0x7fffffff)
+}
+
+export function randomHash(key: any) {
+  if (typeof key === "number") {
+    return key
+  }
+  const hash = CACHE.get(key)
+  if (hash) {
+    return hash
+  }
+  const h = randomInt()
+  CACHE.set(key, h)
+  return h
+}
+
+export function combineHash(a: number, b: number): number {
+  return (a * 53) ^ b
 }
