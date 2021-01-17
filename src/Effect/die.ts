fdc565975b61ff917408b12d9cd466bcd1eb6581
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-17 14:50:31 +0000

    die trace call

--- src/Effect/die.ts
+++ src/Effect/die.ts
@@ -1,3 +1,6 @@
+// tracing: off
+import { accessCallTrace, traceFrom } from "@effect-ts/tracing-utils"
+
 import * as C from "../Cause/cause"
 import { haltWith } from "./core"
 
@@ -5,7 +8,10 @@ import { haltWith } from "./core"
  * Returns an effect that dies with the specified `unknown`.
  * This method can be used for terminating a fiber because a defect has been
  * detected in the code.
+ *
+ * @trace call
  */
 export function die(e: unknown) {
-  return haltWith((trace) => C.traced(C.die(e), trace()))
+  const trace = accessCallTrace()
+  return haltWith(traceFrom(trace, (trace) => C.traced(C.die(e), trace())))
 }
