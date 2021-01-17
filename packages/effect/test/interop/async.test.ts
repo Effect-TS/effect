313e5fea02add9e6003dd962940e2dff69e4e75f
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-17 00:23:39 +0000

    remove unsupported interop

--- packages/effect/test/interop/async.test.ts
+++ packages/effect/test/interop/async.test.ts
@@ -2,7 +2,6 @@ import { succeed } from "@effect-ts/system/Exit"
 import { pipe } from "@effect-ts/system/Function"
 
 import * as A from "../../src/Async"
-import { successExit } from "../../src/Async"
 import * as T from "../../src/Effect"
 import * as S from "../../src/Sync"
 
@@ -26,15 +25,5 @@ describe("Async/Effect interop", () => {
     )
 
     expect(result).toEqual(succeed(1))
-  })
-  it("async should execute sync natively", async () => {
-    const result = await pipe(
-      S.accessM((_: { foo: string }) => S.succeed(_.foo.length)),
-      A.chain((n) => A.sync(() => n + 1)),
-      A.provideAll({ foo: "" }),
-      A.runPromiseExit
-    )
-
-    expect(result).toEqual(successExit(1))
   })
 })
