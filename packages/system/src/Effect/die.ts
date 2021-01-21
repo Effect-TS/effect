7cadb592ff41aee038376bcac07b3511f936bf1f
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-21 18:02:16 +0000

    add lazy variants

--- packages/system/src/Effect/die.ts
+++ packages/system/src/Effect/die.ts
@@ -1,5 +1,5 @@
 // tracing: off
-import { accessCallTrace, traceFrom } from "@effect-ts/tracing-utils"
+import { accessCallTrace, traceAs, traceFrom } from "@effect-ts/tracing-utils"
 
 import * as C from "../Cause/cause"
 import { haltWith } from "./core"
@@ -14,4 +14,15 @@ import { haltWith } from "./core"
 export function die(e: unknown) {
   const trace = accessCallTrace()
   return haltWith(traceFrom(trace, (trace) => C.traced(C.die(e), trace())))
+}
+
+/**
+ * Returns an effect that dies with the specified `unknown`.
+ * This method can be used for terminating a fiber because a defect has been
+ * detected in the code.
+ *
+ * @trace 0
+ */
+export function dieWith(e: () => unknown) {
+  return haltWith(traceAs(e, (trace) => C.traced(C.die(e), trace())))
 }
