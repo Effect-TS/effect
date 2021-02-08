3db4d123cd47f59932a671de710db102e99077e5
Michael Arnaldi <arnaldimichael@gmail.com>
2021-02-08 11:10:58 +0000

    collect in zio - map/chainOrFail in effect

--- packages/system/src/Managed/methods/api.ts
+++ packages/system/src/Managed/methods/api.ts
@@ -2498,3 +2498,56 @@ export function unwrap<R, E, A>(
 ): Managed<R, E, A> {
   return flatten(fromEffect(fa))
 }
+
+/**
+ * Fail with `e` if the supplied `PartialFunction` does not match, otherwise
+ * continue with the returned value.
+ */
+export function chainOrFail_<E1, A, R2, E2, B, R, E>(
+  self: Managed<R, E, A>,
+  e: E1,
+  f: (a: A) => O.Option<Managed<R2, E2, B>>
+) {
+  return chain_(
+    self,
+    (a): Managed<R2, E1 | E2, B> => O.fold_(f(a), () => fail(e), identity)
+  )
+}
+
+/**
+ * Fail with `e` if the supplied `PartialFunction` does not match, otherwise
+ * continue with the returned value.
+ *
+ * @dataFirst chainOrFail_
+ */
+export function chainOrFail<E1, A, R2, E2, B>(
+  e: E1,
+  f: (a: A) => O.Option<Managed<R2, E2, B>>
+) {
+  return <R, E>(self: Managed<R, E, A>) => chainOrFail_(self, e, f)
+}
+
+/**
+ * Fail with `e` if the supplied `PartialFunction` does not match, otherwise
+ * succeed with the returned value.
+ */
+export function mapOrFail_<E1, A, B, R, E>(
+  self: Managed<R, E, A>,
+  e: E1,
+  f: (a: A) => O.Option<B>
+) {
+  return chain_(
+    self,
+    (a): Managed<unknown, E1, B> => O.fold_(f(a), () => fail(e), succeed)
+  )
+}
+
+/**
+ * Fail with `e` if the supplied `PartialFunction` does not match, otherwise
+ * succeed with the returned value.
+ *
+ * @dataFirst mapOrFail_
+ */
+export function mapOrFail<E1, A, B>(e: E1, f: (a: A) => O.Option<B>) {
+  return <R, E>(self: Managed<R, E, A>) => mapOrFail_(self, e, f)
+}
