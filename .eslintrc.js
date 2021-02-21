06e94128624e86f11d72dd905eb82cb30a5f837b
Michael Arnaldi <arnaldimichael@gmail.com>
2021-02-21 12:14:05 +0000

    updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,8 +20,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "prettier"
   ],
   plugins: ["import", "sort-destructure-keys", "simple-import-sort", "codegen"],
   rules: {
