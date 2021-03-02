02c6de21334bcbc5755ef5b1b9d0471431cf8800
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-02 13:55:30 +0000

    remove sync from either gen

--- packages/system/src/Either/gen.ts
+++ packages/system/src/Either/gen.ts
@@ -1,9 +1,7 @@
 import { NoSuchElementException } from "../GlobalExceptions"
 import type { Option } from "../Option"
-import type { Sync } from "../Sync/core"
-import { runEither, runEitherEnv } from "../Sync/core"
 import type { _E } from "../Utils"
-import { isOption, isSync } from "../Utils"
+import { isOption } from "../Utils"
 import type { Either } from "./core"
 import { chain_, left, right } from "./core"
 
@@ -19,12 +17,6 @@ export class GenEither<E, A> {
 }
 
 function adapter(_: any, __?: any) {
-  if (isSync(_)) {
-    if (__) {
-      return new GenEither(runEitherEnv(__)(_))
-    }
-    return new GenEither(runEither(_))
-  }
   return isOption(_)
     ? new GenEither(
         _._tag === "Some"
@@ -36,8 +28,6 @@ function adapter(_: any, __?: any) {
 
 export function gen<EBase, AEff>(): <Eff extends GenEither<EBase, any>>(
   f: (i: {
-    <E, A>(_: Sync<unknown, E, A>): GenEither<E, A>
-    <R, E, A>(_: Sync<R, E, A>, r: R): GenEither<E, A>
     <E, A>(_: Option<A>, onNone: () => E): GenEither<E, A>
     <A>(_: Option<A>): GenEither<NoSuchElementException, A>
     <E, A>(_: Either<E, A>): GenEither<E, A>
@@ -45,8 +35,6 @@ export function gen<EBase, AEff>(): <Eff extends GenEither<EBase, any>>(
 ) => Either<_E<Eff>, AEff>
 export function gen<AEff>(): <Eff extends GenEither<any, any>>(
   f: (i: {
-    <E, A>(_: Sync<unknown, E, A>): GenEither<E, A>
-    <R, E, A>(_: Sync<R, E, A>, r: R): GenEither<E, A>
     <E, A>(_: Option<A>, onNone: () => E): GenEither<E, A>
     <A>(_: Option<A>): GenEither<NoSuchElementException, A>
     <E, A>(_: Either<E, A>): GenEither<E, A>
@@ -54,8 +42,6 @@ export function gen<AEff>(): <Eff extends GenEither<any, any>>(
 ) => Either<_E<Eff>, AEff>
 export function gen<Eff extends GenEither<any, any>, AEff>(
   f: (i: {
-    <E, A>(_: Sync<unknown, E, A>): GenEither<E, A>
-    <R, E, A>(_: Sync<R, E, A>, r: R): GenEither<E, A>
     <E, A>(_: Option<A>, onNone: () => E): GenEither<E, A>
     <A>(_: Option<A>): GenEither<NoSuchElementException, A>
     <E, A>(_: Either<E, A>): GenEither<E, A>
