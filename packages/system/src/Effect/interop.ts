4277fedde143a39f71a59db7895239b8ffe55d60
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-03 14:10:07 +0000

    remove useless fromSync

--- packages/system/src/Effect/interop.ts
+++ packages/system/src/Effect/interop.ts
@@ -1,11 +1,9 @@
 import * as Async from "../Async"
 import * as IO from "../IO"
-import * as Sync from "../Sync"
 import { accessM, effectTotal, succeed } from "./core"
 import type { Effect } from "./effect"
 import { effectAsyncInterrupt } from "./effectAsyncInterrupt"
 import { fail } from "./fail"
-import { fromEither } from "./fromEither"
 import { interrupt } from "./interruption"
 
 /**
@@ -42,11 +40,4 @@ export function fromAsync<R, E, A>(async: Async.Async<R, E, A>): Effect<R, E, A>
  */
 export function fromIO<A>(io: IO.IO<A>): Effect<unknown, never, A> {
   return effectTotal(() => IO.run(io))
-}
-
-/**
- * Lift Sync into Effect
- */
-export function fromSync<R, E, A>(sync: Sync.Sync<R, E, A>): Effect<R, E, A> {
-  return accessM((r: R) => fromEither(() => Sync.runEitherEnv(r)(sync)))
 }
