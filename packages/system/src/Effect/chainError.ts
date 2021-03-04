fefbce1f8fa848d165f5361c17d81ff5b8dc235f
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-04 17:23:47 +0000

    continue

--- packages/system/src/Effect/chainError.ts
+++ packages/system/src/Effect/chainError.ts
@@ -1,3 +1,5 @@
+// tracing: off
+
 import { chain } from "./core"
 import type { Effect, RIO } from "./effect"
 import { flipWith_ } from "./flipWith"
@@ -5,6 +7,9 @@ import { flipWith_ } from "./flipWith"
 /**
  * Creates a composite effect that represents this effect followed by another
  * one that may depend on the error produced by this one.
+ *
+ * @dataFirst chainError_
+ * @trace 0
  */
 export function chainError<E, R2, E2>(f: (e: E) => RIO<R2, E2>) {
   return <R, A>(self: Effect<R, E, A>) => chainError_(self, f)
@@ -13,6 +18,8 @@ export function chainError<E, R2, E2>(f: (e: E) => RIO<R2, E2>) {
 /**
  * Creates a composite effect that represents this effect followed by another
  * one that may depend on the error produced by this one.
+ *
+ * @trace 1
  */
 export function chainError_<R, E, A, R2, E2>(
   self: Effect<R, E, A>,
