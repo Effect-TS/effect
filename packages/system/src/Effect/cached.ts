26246e67957981e37f4e4b8e7e4d810c4e864b69
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-07 12:45:57 +0000

    continue

--- packages/system/src/Effect/cached.ts
+++ packages/system/src/Effect/cached.ts
@@ -1,7 +1,5 @@
 // tracing: off
 
-import { accessCallTrace, traceCall } from "@effect-ts/tracing-utils"
-
 import type { Clock } from "../Clock"
 import { pipe } from "../Function"
 import type { Has } from "../Has"
@@ -13,27 +11,22 @@ import { map } from "./map"
  * Returns an effect that, if evaluated, will return the cached result of
  * this effect. Cached results will expire after `timeToLive` duration.
  *
- * @trace call
  * @dataFirst cached_
  */
 export function cached(ttl: number) {
-  const trace = accessCallTrace()
-  return traceCall(<R, E, A>(fa: Effect<R, E, A>) => cached_(fa, ttl), trace)
+  return <R, E, A>(fa: Effect<R, E, A>) => cached_(fa, ttl)
 }
 
 /**
  * Returns an effect that, if evaluated, will return the cached result of
  * this effect. Cached results will expire after `timeToLive` duration.
- *
- * @trace call
  */
 export function cached_<R, E, A>(
   fa: Effect<R, E, A>,
   ttl: number
 ): RIO<R & Has<Clock>, IO<E, A>> {
-  const trace = accessCallTrace()
   return pipe(
-    traceCall(cachedInvalidate_, trace)(fa, ttl),
+    cachedInvalidate_(fa, ttl),
     map(([cachedEffect, _]) => cachedEffect)
   )
 }
