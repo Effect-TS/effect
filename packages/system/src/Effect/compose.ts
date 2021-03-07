fad0e3a155c8122fc2d64d7e2368693ad3888d4a
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-07 13:27:51 +0000

    continue

--- packages/system/src/Effect/compose.ts
+++ packages/system/src/Effect/compose.ts
@@ -1,7 +1,5 @@
 // tracing: off
 
-import { accessCallTrace, traceCall, traceFrom } from "@effect-ts/tracing-utils"
-
 import { chain_, provideAll_ } from "./core"
 import type { Effect } from "./effect"
 
@@ -9,11 +7,9 @@ import type { Effect } from "./effect"
  * Uses the output of `that` to provide to `self`
  *
  * @dataFirst compose_
- * @trace call
  */
 export function compose<A, E1, B>(that: Effect<A, E1, B>) {
-  const trace = accessCallTrace()
-  return <R, E>(self: Effect<R, E, A>) => traceCall(compose_, trace)(self, that)
+  return <R, E>(self: Effect<R, E, A>) => compose_(self, that)
 }
 
 /**
@@ -25,9 +21,5 @@ export function compose_<A, E1, B, R, E>(
   self: Effect<R, E, A>,
   that: Effect<A, E1, B>
 ) {
-  const trace = accessCallTrace()
-  return chain_(
-    self,
-    traceFrom(trace, (r) => provideAll_(that, r))
-  )
+  return chain_(self, (r) => provideAll_(that, r))
 }
