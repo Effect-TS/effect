495d23d03c56a8c49038b9ae31e5ee21e97e54ac
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-12 12:47:57 +0000

    continue

--- packages/system/src/Effect/fail.ts
+++ packages/system/src/Effect/fail.ts
@@ -1,27 +1,20 @@
 // tracing: off
 
-import { accessCallTrace, traceAs, traceFrom } from "@effect-ts/tracing-utils"
-
 import * as C from "../Cause/cause"
 import { haltWith } from "./core"
 
 /**
  * Returns an effect that models failure with the specified error.
  * The moral equivalent of `throw` for pure code.
- *
- * @trace call
  */
-export function fail<E>(e: E) {
-  const trace = accessCallTrace()
-  return haltWith(traceFrom(trace, (trace) => C.traced(C.fail(e), trace())))
+export function fail<E>(e: E, __trace?: string) {
+  return haltWith((trace) => C.traced(C.fail(e), trace()), __trace)
 }
 
 /**
  * Returns an effect that models failure with the specified error.
  * The moral equivalent of `throw` for pure code.
- *
- * @trace 0
  */
-export function failWith<E>(e: () => E) {
-  return haltWith(traceAs(e, (trace) => C.traced(C.fail(e()), trace())))
+export function failWith<E>(e: () => E, __trace?: string) {
+  return haltWith((trace) => C.traced(C.fail(e()), trace()), __trace)
 }
