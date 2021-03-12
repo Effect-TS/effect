e5e4deb6fc61a78123bdfe1c4b591026d9011fa3
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-12 12:35:36 +0000

    continue

--- packages/system/src/Effect/asUnit.ts
+++ packages/system/src/Effect/asUnit.ts
@@ -6,6 +6,6 @@ import type { Effect } from "./effect"
 /**
  * Ignores the result of the effect replacing it with a void
  */
-export function asUnit<R, E, X>(self: Effect<R, E, X>) {
-  return chain_(self, () => unit)
+export function asUnit<R, E, X>(self: Effect<R, E, X>, __trace?: string) {
+  return chain_(self, () => unit, __trace)
 }
