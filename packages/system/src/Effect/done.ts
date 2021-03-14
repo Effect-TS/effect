22f6ded40bb5f68a5da89c2d34b6678277b88cb1
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-14 12:19:44 +0000

    fix

--- packages/system/src/Effect/done.ts
+++ packages/system/src/Effect/done.ts
@@ -2,11 +2,12 @@
 
 import type { Exit } from "../Exit/exit"
 import { halt, succeed } from "./core"
+import type { Effect } from "./effect"
 
 /**
  * Returns an effect from a `Exit` value.
  */
-export function done<E, A>(exit: Exit<E, A>, __trace?: string) {
+export function done<E, A>(exit: Exit<E, A>, __trace?: string): Effect<unknown, E, A> {
   switch (exit._tag) {
     case "Success": {
       return succeed(exit.value, __trace)
