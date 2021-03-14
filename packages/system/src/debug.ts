90c3c8ae7776c5e1ad5bf8f519bde74f33c4f64b
Michael Arnaldi <arnaldimichael@gmail.com>
2021-03-14 17:01:30 +0000

    continue

--- packages/system/src/debug.ts
+++ packages/system/src/debug.ts
@@ -2,15 +2,21 @@ import "@effect-ts/tracing-utils/Enable"
 
 import * as T from "./Effect"
 import { prettyTrace } from "./Fiber"
-import { pipe } from "./Function"
+import { literal } from "./Function"
 
-pipe(
-  T.tupleParN(2)(T.succeed(0), T.succeed(1), T.fail(2)),
-  T.andThen(T.trace),
-  T.chain((t) =>
-    T.effectTotal(() => {
-      console.log(prettyTrace(t))
-    })
-  ),
-  T.runPromise
-).catch(console.error)
+T.succeed("no")
+  ["|>"](
+    T.filterOrElse(
+      (a): a is "ok" => a === "ok",
+      () => T.succeed(literal("no"))
+    )
+  )
+  ["|>"](T.andThen(T.trace))
+  ["|>"](
+    T.chain((trace) =>
+      T.effectTotal(() => {
+        console.log(prettyTrace(trace))
+      })
+    )
+  )
+  ["|>"]((x) => T.runPromise(x).catch(console.error))
