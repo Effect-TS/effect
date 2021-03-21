13d147d866446f85a5738ba7c6cf98bd0aab19d2
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-03-21 15:05:21 -0400

    Add basic renderers

--- packages/printer/src/Core/Render/index.ts
+++ packages/printer/src/Core/Render/index.ts
@@ -2,7 +2,10 @@
 
 import * as IO from "@effect-ts/core/IO"
 
+import type { Doc } from "../Doc"
 import type { DocStream } from "../DocStream"
+import type { LayoutOptions } from "../Layout"
+import * as Layout from "../Layout"
 
 function renderRec<A>(x: DocStream<A>): IO.IO<string> {
   return IO.gen(function* (_) {
@@ -39,6 +42,36 @@ function renderRec<A>(x: DocStream<A>): IO.IO<string> {
 // operations
 // -------------------------------------------------------------------------------------
 
-export const render = <A>(stream: DocStream<A>): string => {
+export function render<A>(stream: DocStream<A>): string {
   return IO.run(renderRec(stream))
+}
+
+export function renderPretty_<A>(doc: Doc<A>, options: LayoutOptions): string {
+  return render(Layout.pretty_(options, doc))
+}
+
+/**
+ * @dataFirst renderPretty_
+ */
+export function renderPretty(options: LayoutOptions) {
+  return <A>(doc: Doc<A>): string => renderPretty_(doc, options)
+}
+
+export function renderPrettyDefault<A>(doc: Doc<A>): string {
+  return render(Layout.pretty_(Layout.defaultLayoutOptions, doc))
+}
+
+export function renderSmart_<A>(doc: Doc<A>, options: LayoutOptions) {
+  return render(Layout.smart_(options, doc))
+}
+
+/**
+ * @dataFirst renderSmart_
+ */
+export function renderSmart(options: LayoutOptions) {
+  return <A>(doc: Doc<A>): string => renderSmart_(doc, options)
+}
+
+export function renderSmartDefault<A>(doc: Doc<A>): string {
+  return render(Layout.smart_(Layout.defaultLayoutOptions, doc))
 }
