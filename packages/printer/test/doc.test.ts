07c94a6fa4f55bf3969182f0b2c20f054a378aa6
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-03-23 21:35:34 -0400

    Remove definition tests for Doc module

--- packages/printer/test/doc.test.ts
+++ packages/printer/test/doc.test.ts
@@ -1,6 +1,6 @@
 import type { Array } from "@effect-ts/core/Array"
 import * as A from "@effect-ts/core/Array"
-import { constant, identity, pipe } from "@effect-ts/system/Function"
+import { constant, pipe } from "@effect-ts/system/Function"
 
 import type { Doc } from "../src/Core/Doc"
 import * as D from "../src/Core/Doc"
@@ -8,116 +8,6 @@ import * as PW from "../src/Core/PageWidth"
 import * as R from "../src/Core/Render"
 
 describe("Doc", () => {
-  describe("definition", () => {
-    it("Fail", () => {
-      const doc = new D.Fail(identity)
-
-      expect(doc).toBeInstanceOf(D.Fail)
-      expect(doc.id).toBeDefined()
-    })
-
-    it("Char", () => {
-      const doc = new D.Char("a", identity)
-
-      expect(doc).toBeInstanceOf(D.Char)
-      expect(doc.id).toBeDefined()
-      expect(doc.char).toBe("a")
-    })
-
-    it("Text", () => {
-      const doc = new D.Text("foo", identity)
-
-      expect(doc).toBeInstanceOf(D.Text)
-      expect(doc.id).toBeDefined()
-      expect(doc.text).toBe("foo")
-    })
-
-    it("FlatAlt", () => {
-      const doc = new D.FlatAlt(new D.Char("a", identity), new D.Char("b", identity))
-
-      expect(doc).toBeInstanceOf(D.FlatAlt)
-      expect(doc.left).toBeInstanceOf(D.Char)
-      expect(doc.left).toHaveProperty("char", "a")
-      expect(doc.right).toBeInstanceOf(D.Char)
-      expect(doc.right).toHaveProperty("char", "b")
-    })
-
-    it("Cat", () => {
-      const doc = new D.Cat(new D.Char("a", identity), new D.Char("b", identity))
-
-      expect(doc).toBeInstanceOf(D.Cat)
-      expect(doc.left).toBeInstanceOf(D.Char)
-      expect(doc.left).toHaveProperty("char", "a")
-      expect(doc.right).toBeInstanceOf(D.Char)
-      expect(doc.right).toHaveProperty("char", "b")
-    })
-
-    it("Nest", () => {
-      const doc = new D.Nest(4, new D.Char("a", identity))
-
-      expect(doc).toBeInstanceOf(D.Nest)
-      expect(doc.doc).toBeInstanceOf(D.Char)
-      expect(doc.doc).toHaveProperty("char", "a")
-      expect(doc.indent).toBe(4)
-    })
-
-    it("Union", () => {
-      const doc = new D.Union(new D.Char("a", identity), new D.Char("b", identity))
-
-      expect(doc).toBeInstanceOf(D.Union)
-      expect(doc.left).toBeInstanceOf(D.Char)
-      expect(doc.left).toHaveProperty("char", "a")
-      expect(doc.right).toBeInstanceOf(D.Char)
-      expect(doc.right).toHaveProperty("char", "b")
-    })
-
-    it("Column", () => {
-      const doc = new D.Column((pos) => new D.Text(`${pos}`, identity))
-      const reactedDoc = doc.react(4)
-
-      expect(doc).toBeInstanceOf(D.Column)
-      expect(doc.react).toBeDefined()
-      expect(reactedDoc).toBeInstanceOf(D.Text)
-      expect(reactedDoc).toHaveProperty("text", "4")
-    })
-
-    it("WithPageWidth", () => {
-      const react = (w: PW.PageWidth): D.Doc<never> =>
-        w._tag === "AvailablePerLine"
-          ? new D.Text(`${w.lineWidth}`, identity)
-          : new D.Char("a", identity)
-      const doc = new D.WithPageWidth(react)
-      const availablePerLineDoc = doc.react(PW.availablePerLine(80, 1))
-      const unboundedDoc = doc.react(PW.unbounded)
-
-      expect(doc).toBeInstanceOf(D.WithPageWidth)
-      expect(doc.react).toBeDefined()
-      expect(availablePerLineDoc).toBeInstanceOf(D.Text)
-      expect(availablePerLineDoc).toHaveProperty("text", "80")
-      expect(unboundedDoc).toBeInstanceOf(D.Char)
-      expect(unboundedDoc).toHaveProperty("char", "a")
-    })
-
-    it("Nesting", () => {
-      const doc = new D.Nesting((l: number): Doc<never> => new D.Text(`${l}`, identity))
-      const reactedDoc = doc.react(4)
-
-      expect(doc).toBeInstanceOf(D.Nesting)
-      expect(doc.react).toBeDefined()
-      expect(reactedDoc).toBeInstanceOf(D.Text)
-      expect(reactedDoc).toHaveProperty("text", "4")
-    })
-
-    it("Annotated", () => {
-      const doc = new D.Annotated(1, new D.Char("a", identity))
-
-      expect(doc).toBeInstanceOf(D.Annotated)
-      expect(doc.annotation).toBe(1)
-      expect(doc.doc).toBeInstanceOf(D.Char)
-      expect(doc.doc).toHaveProperty("char", "a")
-    })
-  })
-
   describe("constructors", () => {
     it("fail", () => {
       expect(D.fail).toBeInstanceOf(D.Fail)
