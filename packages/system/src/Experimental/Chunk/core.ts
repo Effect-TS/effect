439a612950cbb455ddab95bdedef004a922bf195
Michael Arnaldi <arnaldimichael@gmail.com>
2021-04-09 00:11:08 +0100

    feat(chunk): implement take and drop

--- packages/system/src/Experimental/Chunk/core.ts
+++ packages/system/src/Experimental/Chunk/core.ts
@@ -629,3 +629,59 @@ export function equals_<A>(self: Chunk<A>, that: Chunk<A>): boolean {
 export function equals<A>(that: Chunk<A>): (self: Chunk<A>) => boolean {
   return (self) => equals_(self, that)
 }
+
+/**
+ * Takes the first n elements
+ */
+export function take_<A>(self: Chunk<A>, n: number): Chunk<A> {
+  return self.take(n)
+}
+
+/**
+ * Takes the first n elements
+ *
+ * @dataFirst take_
+ */
+export function take(n: number): <A>(self: Chunk<A>) => Chunk<A> {
+  return (self) => self.take(n)
+}
+
+/**
+ * Drops the first n elements
+ */
+export function drop_<A>(self: Chunk<A>, n: number): Chunk<A> {
+  concrete(self)
+  if (n <= 0) {
+    return self
+  } else if (n >= self.length) {
+    return _Empty
+  } else {
+    const len = self.length
+    switch (self._typeId) {
+      case EmptyTypeId: {
+        return _Empty
+      }
+      case SliceTypeId: {
+        return new Slice(self.chunk, self.offset + n, self.length - n)
+      }
+      case SingletonTypeId: {
+        if (n > 0) {
+          return _Empty
+        }
+        return self
+      }
+      default: {
+        return new Slice(self, n, len - n)
+      }
+    }
+  }
+}
+
+/**
+ * Drops the first n elements
+ *
+ * @dataFirst drop_
+ */
+export function drop(n: number): <A>(self: Chunk<A>) => Chunk<A> {
+  return (self) => self.take(n)
+}
