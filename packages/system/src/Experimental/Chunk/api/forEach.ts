d4cb6885e051aabefc8db93b5ac2d5a3709380de
Michael Arnaldi <arnaldimichael@gmail.com>
2021-04-09 23:00:38 +0100

    feat(chunk): implement forEach, partitionMap

---
+++ packages/system/src/Experimental/Chunk/api/forEach.ts
@@ -0,0 +1,30 @@
+import type * as Chunk from "../core"
+
+/**
+ * Iterate over the chunk applying f
+ */
+export function forEach_<A, U>(self: Chunk.Chunk<A>, f: (a: A) => U): void {
+  const iterator = self.arrayLikeIterator()
+  let next = iterator.next()
+
+  while (!next.done) {
+    const array = next.value
+    const len = array.length
+    let i = 0
+    while (i < len) {
+      const a = array[i]!
+      f(a)
+      i++
+    }
+    next = iterator.next()
+  }
+}
+
+/**
+ * Iterate over the chunk applying f
+ *
+ * @dataFirst forEach_
+ */
+export function forEach<A, U>(f: (a: A) => U): (self: Chunk.Chunk<A>) => void {
+  return (self) => forEach_(self, f)
+}
