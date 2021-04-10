c1e67d23453a9772ce07002a93938865cf0a8785
Michael Arnaldi <arnaldimichael@gmail.com>
2021-04-11 00:55:31 +0100

    perf(streams): extract loop in chunkN

--- packages/system/src/Stream/Stream/chunkN.ts
+++ packages/system/src/Stream/Stream/chunkN.ts
@@ -15,42 +15,40 @@ class State<X> {
   constructor(public buffer: A.Chunk<X>, public done: boolean) {}
 }
 
+function emitOrAccumulate<R, E, O>(
+  buffer: A.Chunk<O>,
+  done: boolean,
+  ref: Ref.Ref<State<O>>,
+  pull: T.Effect<R, O.Option<E>, A.Chunk<O>>,
+  n: number
+): T.Effect<R, O.Option<E>, A.Chunk<O>> {
+  if (A.size(buffer) < n) {
+    if (done) {
+      if (A.isEmpty(buffer)) {
+        return Pull.end
+      } else {
+        return T.zipRight_(ref.set(new State(A.empty(), true)), Pull.emitChunk(buffer))
+      }
+    } else {
+      return T.foldM_(
+        pull,
+        O.fold(() => emitOrAccumulate(buffer, true, ref, pull, n), Pull.fail),
+        (ch) => emitOrAccumulate(A.concat_(buffer, ch), false, ref, pull, n)
+      )
+    }
+  } else {
+    const [chunk, leftover] = A.splitAt_(buffer, n)
+
+    return T.zipRight_(ref.set(new State(leftover, done)), Pull.emitChunk(chunk))
+  }
+}
+
 /**
  * Re-chunks the elements of the stream into chunks of
  * `n` elements each.
  * The last chunk might contain less than `n` elements
  */
 export function chunkN_<R, E, O>(self: Stream<R, E, O>, n: number): Stream<R, E, O> {
-  const emitOrAccumulate = (
-    buffer: A.Chunk<O>,
-    done: boolean,
-    ref: Ref.Ref<State<O>>,
-    pull: T.Effect<R, O.Option<E>, A.Chunk<O>>
-  ): T.Effect<R, O.Option<E>, A.Chunk<O>> => {
-    if (A.size(buffer) < n) {
-      if (done) {
-        if (A.isEmpty(buffer)) {
-          return Pull.end
-        } else {
-          return T.zipRight_(
-            ref.set(new State(A.empty(), true)),
-            Pull.emitChunk(buffer)
-          )
-        }
-      } else {
-        return T.foldM_(
-          pull,
-          O.fold(() => emitOrAccumulate(buffer, true, ref, pull), Pull.fail),
-          (ch) => emitOrAccumulate(A.concat_(buffer, ch), false, ref, pull)
-        )
-      }
-    } else {
-      const [chunk, leftover] = A.splitAt_(buffer, n)
-
-      return T.zipRight_(ref.set(new State(leftover, done)), Pull.emitChunk(chunk))
-    }
-  }
-
   if (n < 1) {
     return halt(C.die(new C.IllegalArgumentException("chunkN: n must be at least 1")))
   } else {
@@ -62,7 +60,7 @@ export function chunkN_<R, E, O>(self: Stream<R, E, O>, n: number): Stream<R, E,
         ),
         M.bind("p", () => self.proc),
         M.let("pull", ({ p, ref }) =>
-          T.chain_(ref.get, (s) => emitOrAccumulate(s.buffer, s.done, ref, p))
+          T.chain_(ref.get, (s) => emitOrAccumulate(s.buffer, s.done, ref, p, n))
         ),
         M.map(({ pull }) => pull)
       )
