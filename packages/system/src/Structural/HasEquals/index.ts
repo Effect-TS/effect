58a9ea4748a838f61663ae28c17d36c5af3e3812
Michael Arnaldi <arnaldimichael@gmail.com>
2021-04-19 22:26:48 +0100

    fix(equals): use sameValueZeroEqual

--- packages/system/src/Structural/HasEquals/index.ts
+++ packages/system/src/Structural/HasEquals/index.ts
@@ -2,7 +2,7 @@
 
 import { hash } from "../HasHash"
 import { createComparator } from "./comparator"
-import { createCircularEqualCreator } from "./utils"
+import { createCircularEqualCreator, sameValueZeroEqual } from "./utils"
 
 export const equalsSym = Symbol()
 
@@ -25,12 +25,12 @@ export const deepEquals = createComparator(
 )
 
 export function equals(a: unknown, b: unknown): boolean {
-  if (hash(a) !== hash(b)) {
+  if (!sameValueZeroEqual(hash(a), hash(b))) {
     return false
   } else if (hasEquals(a)) {
     return a[equalsSym](b)
   } else if (hasEquals(b)) {
     return b[equalsSym](a)
   }
-  return Object.is(a, b)
+  return sameValueZeroEqual(a, b)
 }
