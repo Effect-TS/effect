22cb0d5b1091b938c945af205086e7839c5d50fd
Patrick Roza <contact@patrickroza.com>
2021-05-03 16:25:03 +0200

    feat(core): Add `forEach`and friends to Sync.

---
+++ packages/system/src/Sync/excl-forEach.ts
@@ -0,0 +1,125 @@
+// tracing: off
+
+import * as ChunkFilterMap from "../Collections/Immutable/Chunk/api/filterMap"
+import * as Chunk from "../Collections/Immutable/Chunk/core"
+import { identity } from "../Function"
+import type * as O from "../Option"
+import type { Sync } from "./core"
+import * as core from "./core"
+
+/**
+ * Applies the function `f` to each element of the `Iterable<A>` and
+ * returns the results in a new `readonly B[]`.
+ *
+ * For a parallel version of this method, see `forEachPar`.
+ * If you do not need the results, see `forEachUnit` for a more efficient implementation.
+ */
+export function forEach_<A, R, E, B>(
+  as: Iterable<A>,
+  f: (a: A) => Sync<R, E, B>
+): Sync<R, E, Chunk.Chunk<B>> {
+  return core.suspend(() => {
+    const acc: B[] = []
+
+    return core.map_(
+      forEachUnit_(as, (a) =>
+        core.map_(f(a), (b) => {
+          acc.push(b)
+        })
+      ),
+      () => Chunk.from(acc)
+    )
+  })
+}
+
+/**
+ * Applies the function `f` to each element of the `Iterable<A>` and
+ * returns the results in a new `readonly B[]`.
+ *
+ * For a parallel version of this method, see `forEachPar`.
+ * If you do not need the results, see `forEachUnit` for a more efficient implementation.
+ *
+ * @dataFirst forEach_
+ */
+export function forEach<A, R, E, B>(f: (a: A) => Sync<R, E, B>) {
+  return (as: Iterable<A>) => forEach_(as, f)
+}
+
+function forEachUnitLoop<R, E, A, X>(
+  iterator: Iterator<A, any, undefined>,
+  f: (a: A) => Sync<R, E, X>
+): Sync<R, E, void> {
+  const next = iterator.next()
+  return next.done
+    ? core.unit
+    : core.chain_(f(next.value), () => forEachUnitLoop(iterator, f))
+}
+
+/**
+ * Applies the function `f` to each element of the `Iterable<A>` and runs
+ * produced Syncs sequentially.
+ *
+ * Equivalent to `asUnit(forEach(as, f))`, but without the cost of building
+ * the list of results.
+ */
+export function forEachUnit_<R, E, A, X>(
+  as: Iterable<A>,
+  f: (a: A) => Sync<R, E, X>,
+  __trace?: string
+): Sync<R, E, void> {
+  return core.suspend(() => forEachUnitLoop(as[Symbol.iterator](), f))
+}
+
+/**
+ * Applies the function `f` to each element of the `Iterable<A>` and runs
+ * produced Syncs sequentially.
+ *
+ * Equivalent to `asUnit(forEach(as, f))`, but without the cost of building
+ * the list of results.
+ *
+ * @dataFirst forEachUnit_
+ */
+export function forEachUnit<R, E, A, X>(
+  f: (a: A) => Sync<R, E, X>,
+  __trace?: string
+): (as: Iterable<A>) => Sync<R, E, void> {
+  return (as) => forEachUnit_(as, f)
+}
+
+/**
+ * Evaluate each Sync in the structure from left to right, and collect the
+ * results. For a parallel version, see `collectAllPar`.
+ */
+export function collectAll<R, E, A>(as: Iterable<Sync<R, E, A>>) {
+  return forEach_(as, identity)
+}
+
+/**
+ * Evaluate each Sync in the structure from left to right, and discard the
+ * results. For a parallel version, see `collectAllUnitPar`.
+ */
+export function collectAllUnit<R, E, A>(as: Iterable<Sync<R, E, A>>, __trace?: string) {
+  return forEachUnit_(as, identity)
+}
+
+/**
+ * Evaluate each Sync in the structure with `collectAll`, and collect
+ * the results with given partial function.
+ */
+export function collectAllWith_<R, E, A, B>(
+  as: Iterable<Sync<R, E, A>>,
+  pf: (a: A) => O.Option<B>,
+  __trace?: string
+): Sync<R, E, Chunk.Chunk<B>> {
+  return core.map_(collectAll(as), ChunkFilterMap.filterMap(pf))
+}
+
+/**
+ * Evaluate each Sync in the structure with `collectAll`, and collect
+ * the results with given partial function.
+ *
+ * @dataFirst collectAllWith_
+ */
+export function collectAllWith<A, B>(pf: (a: A) => O.Option<B>) {
+  return <R, E>(as: Iterable<Sync<R, E, A>>) => collectAllWith_(as, pf)
+}
--- packages/system/src/Sync/index.ts
+++ packages/system/src/Sync/index.ts
@@ -3,6 +3,9 @@
 import "../Operator"
 
 export * from "./core"
+export * from "./excl-forEach"
+export * from "./collect"
 export * from "./has"
 export * from "./do"
 export * from "./gen"
+export * from "./optional"
