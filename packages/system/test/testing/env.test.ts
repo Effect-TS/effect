e5a28dea5ef314d7653324aa07672966c7868c29
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-11 19:38:13 +0100

    feat(test): include deterministic rand

--- packages/system/test/testing/env.test.ts
+++ packages/system/test/testing/env.test.ts
@@ -2,6 +2,7 @@ import * as T from "../../src/Effect"
 import * as Ex from "../../src/Exit"
 import * as F from "../../src/Fiber"
 import { pipe } from "../../src/Function"
+import * as Rand from "../../src/Random"
 import * as FPT from "../../src/Testing/FibersPerTest"
 import { TestClock } from "../../src/Testing/TestClock"
 import { TestEnvironment } from "../../src/Testing/TestEnvironment"
@@ -14,6 +15,10 @@ it("test env", async () => {
       const sleeping = yield* _(T.fork(T.sleep(10_000)))
 
       yield* _(TC.adjust(10_000))
+
+      const rand = yield* _(Rand.nextInt)
+
+      expect(rand).equals(3700301897)
 
       return yield* _(F.join(sleeping))
     }),
