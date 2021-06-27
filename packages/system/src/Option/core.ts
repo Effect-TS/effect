60743b110e6a8c68c26ae60eb8aab26d2af18a5b
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-27 17:21:39 +0100

    feat(option): improve option

--- packages/system/src/Option/core.ts
+++ packages/system/src/Option/core.ts
@@ -47,7 +47,9 @@ export const none: Option<never> = new None()
 /**
  * Constructs none
  */
-export const emptyOf: <A>() => Option<A> = () => none
+export function emptyOf<A>(): Option<A> {
+  return none
+}
 
 /**
  * Constructs Some(A)
@@ -59,157 +61,156 @@ export function some<A>(a: A): Option<A> {
 /**
  * Classic applicative
  */
-export const ap_: <A, B>(fab: Option<(a: A) => B>, fa: Option<A>) => Option<B> = (
-  mab,
-  ma
-) => (isNone(mab) ? none : isNone(ma) ? none : some(mab.value(ma.value)))
+export function ap_<A, B>(fab: Option<(a: A) => B>, fa: Option<A>): Option<B> {
+  return isNone(fab) ? none : isNone(fa) ? none : some(fab.value(fa.value))
+}
 
 /**
  * Classic applicative
+ *
+ * @dataFirst ap_
  */
-export const ap: <A>(fa: Option<A>) => <B>(fab: Option<(a: A) => B>) => Option<B> =
-  (fa) => (fab) =>
-    ap_(fab, fa)
+export function ap<A>(fa: Option<A>) {
+  return <B>(fab: Option<(a: A) => B>): Option<B> => ap_(fab, fa)
+}
 
 /**
  * Zips `Option[A]` and `Option[B]` into `Option[(A, B)]
  */
-export const zip_: <A, B>(fa: Option<A>, fb: Option<B>) => Option<Tp.Tuple<[A, B]>> = (
-  fa,
-  fb
-) => chain_(fa, (a) => map_(fb, (b) => Tp.tuple(a, b)))
+export function zip_<A, B>(fa: Option<A>, fb: Option<B>): Option<Tp.Tuple<[A, B]>> {
+  return chain_(fa, (a) => map_(fb, (b) => Tp.tuple(a, b)))
+}
 
 /**
  * Zips `Option[A]` and `Option[B]` into `Option[(A, B)]
+ *
+ * @dataFirst zip_
  */
-export const zip: <B>(fb: Option<B>) => <A>(fa: Option<A>) => Option<Tp.Tuple<[A, B]>> =
-  (fb) => (fa) => zip_(fa, fb)
+export function zip<B>(fb: Option<B>) {
+  return <A>(fa: Option<A>): Option<Tp.Tuple<[A, B]>> => zip_(fa, fb)
+}
+
+/**
+ * Apply both and return first
+ *
+ * @dataFirst zipFirst_
+ */
+export function zipFirst<B>(fb: Option<B>) {
+  return <A>(fa: Option<A>): Option<A> => zipFirst_(fa, fb)
+}
 
 /**
  * Apply both and return first
  */
-export const zipFirst: <B>(fb: Option<B>) => <A>(fa: Option<A>) => Option<A> =
-  (fb) => (fa) =>
-    ap_(
-      map_(fa, (a) => () => a),
-      fb
-    )
+export function zipFirst_<A, B>(fa: Option<A>, fb: Option<B>): Option<A> {
+  return ap_(
+    map_(fa, (a) => () => a),
+    fb
+  )
+}
+
+/**
+ * Apply both and return second
+ *
+ * @dataFirst zipSecond_
+ */
+export function zipSecond<B>(fb: Option<B>) {
+  return <A>(fa: Option<A>): Option<B> => zipSecond_(fa, fb)
+}
 
 /**
  * Apply both and return second
  */
-export const zipSecond =
-  <B>(fb: Option<B>) =>
-  <A>(fa: Option<A>): Option<B> =>
-    ap_(
-      map_(fa, () => (b: B) => b),
-      fb
-    )
+export function zipSecond_<A, B>(fa: Option<A>, fb: Option<B>): Option<B> {
+  return ap_(
+    map_(fa, () => (b: B) => b),
+    fb
+  )
+}
 
 /**
  * Builds a new option constructed using the value of self
  */
-export const chain_: <A, B>(self: Option<A>, f: (a: A) => Option<B>) => Option<B> = (
-  ma,
-  f
-) => (isNone(ma) ? none : f(ma.value))
+export function chain_<A, B>(self: Option<A>, f: (a: A) => Option<B>): Option<B> {
+  return isNone(self) ? none : f(self.value)
+}
 
 /**
  * Builds a new option constructed using the value of self
+ *
+ * @dataFirst chain_
  */
-export const chain: <A, B>(f: (a: A) => Option<B>) => (self: Option<A>) => Option<B> =
-  (f) => (ma) =>
-    chain_(ma, f)
+export function chain<A, B>(f: (a: A) => Option<B>) {
+  return (self: Option<A>): Option<B> => chain_(self, f)
+}
+
+/**
+ * Like chain but ignores the constructed outout
+ *
+ * @dataFirst tap_
+ */
+export function tap<A>(f: (a: A) => Option<any>) {
+  return (ma: Option<A>): Option<A> => chain_(ma, (a) => map_(f(a), () => a))
+}
 
 /**
  * Like chain but ignores the constructed outout
  */
-export const tap =
-  <A>(f: (a: A) => Option<any>) =>
-  (ma: Option<A>): Option<A> =>
-    chain_(ma, (a) => map_(f(a), () => a))
-
-/**
- * Like chain but ignores the constructed outout
- */
-export const tap_ = <A>(ma: Option<A>, f: (a: A) => Option<any>): Option<A> =>
-  chain_(ma, (a) => map_(f(a), () => a))
+export function tap_<A>(ma: Option<A>, f: (a: A) => Option<any>): Option<A> {
+  return chain_(ma, (a) => map_(f(a), () => a))
+}
 
 /**
  * Flattens nested options
  */
-export const flatten = <A>(fa: Option<Option<A>>): Option<A> => chain_(fa, identity)
+export function flatten<A>(fa: Option<Option<A>>): Option<A> {
+  return chain_(fa, identity)
+}
 
 /**
  * Wraps this option into a second one
  */
-export const duplicate: <A>(ma: Option<A>) => Option<Option<A>> = (ma) =>
-  isNone(ma) ? none : some(ma)
+export function duplicate<A>(ma: Option<A>): Option<Option<A>> {
+  return isNone(ma) ? none : some(ma)
+}
 
 /**
  * Returns `true` if the predicate is satisfied by the wrapped value
  *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     exists(n => n > 0)
- *   ),
- *   true
- * )
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     exists(n => n > 1)
- *   ),
- *   false
- * )
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     exists(n => n > 0)
- *   ),
- *   false
- * )
+ * @dataFirst exists_
  */
 export function exists<A>(predicate: Predicate<A>): (ma: Option<A>) => boolean {
   return (ma) => (isNone(ma) ? false : predicate(ma.value))
 }
 
 /**
- * Apply `Option[A] => B` in case self is some returning `Option[B]`
+ * Returns `true` if the predicate is satisfied by the wrapped value
  */
-export const extend =
-  <A, B>(f: (fa: Option<A>) => B) =>
-  (self: Option<A>): Option<B> =>
-    extend_(self, f)
+export function exists_<A>(ma: Option<A>, predicate: Predicate<A>): boolean {
+  return isNone(ma) ? false : predicate(ma.value)
+}
+
+/**
+ * Apply `Option[A] => B` in case self is some returning `Option[B]`
+ *
+ * @dataFirst extend_
+ */
+export function extend<A, B>(f: (fa: Option<A>) => B) {
+  return (self: Option<A>): Option<B> => extend_(self, f)
+}
 
 /**
  * Apply `Option[A] => B` in case self is some returning `Option[B]`
  */
-export const extend_ = <A, B>(self: Option<A>, f: (fa: Option<A>) => B): Option<B> =>
-  isNone(self) ? none : some(f(self))
+export function extend_<A, B>(self: Option<A>, f: (fa: Option<A>) => B): Option<B> {
+  return isNone(self) ? none : some(f(self))
+}
 
 /**
  * Takes a default value, a function, and an `Option` value, if the `Option` value is `None` the default value is
  * returned, otherwise the function is applied to the value inside the `Some` and the result is returned.
  *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     fold(() => 'a none', a => `a some containing ${a}`)
- *   ),
- *   'a some containing 1'
- * )
- *
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     fold(() => 'a none', a => `a some containing ${a}`)
- *   ),
- *   'a none'
- * )
+ * @dataFirst fold_
  */
 export function fold<A, B, C>(
   onNone: () => B,
@@ -221,23 +222,6 @@ export function fold<A, B, C>(
 /**
  * Takes a default value, a function, and an `Option` value, if the `Option` value is `None` the default value is
  * returned, otherwise the function is applied to the value inside the `Some` and the result is returned.
- *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     fold(() => 'a none', a => `a some containing ${a}`)
- *   ),
- *   'a some containing 1'
- * )
- *
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     fold(() => 'a none', a => `a some containing ${a}`)
- *   ),
- *   'a none'
- * )
  */
 export function fold_<A, B, C>(
   ma: Option<A>,
@@ -250,17 +234,13 @@ export function fold_<A, B, C>(
 /**
  * Constructs `Option[A]` from `Either[E, A]` discarding `E`
  */
-export const fromEither: <E, A>(ma: Either<E, A>) => Option<A> = (ma) =>
-  ma._tag === "Left" ? none : some(ma.right)
+export function fromEither<E, A>(ma: Either<E, A>): Option<A> {
+  return ma._tag === "Left" ? none : some(ma.right)
+}
 
 /**
  * Constructs a new `Option` from a nullable type. If the value is `null` or `undefined`, returns `None`, otherwise
  * returns the value wrapped in a `Some`
- *
- * @example
- * assert.deepStrictEqual(fromNullable(undefined), none)
- * assert.deepStrictEqual(fromNullable(null), none)
- * assert.deepStrictEqual(fromNullable(1), some(1))
  */
 export function fromNullable<A>(a: A): Option<NonNullable<A>> {
   return a == null ? none : some(a as NonNullable<A>)
@@ -269,20 +249,42 @@ export function fromNullable<A>(a: A): Option<NonNullable<A>> {
 /**
  * Returns a smart constructor based on the given predicate
  *
- * @example
- * import { none, some, fromPredicate } from '@matechs/core/Option'
- *
- * const getOption = fromPredicate((n: number) => n >= 0)
- *
- * assert.deepStrictEqual(getOption(-1), none)
- * assert.deepStrictEqual(getOption(1), some(1))
+ * @dataFirst fromPredicate_
  */
 export function fromPredicate<A, B extends A>(
   refinement: Refinement<A, B>
 ): (a: A) => Option<B>
+/**
+ * Returns a smart constructor based on the given predicate
+ *
+ * @dataFirst fromPredicate_
+ */
 export function fromPredicate<A>(predicate: Predicate<A>): (a: A) => Option<A>
+/**
+ * Returns a smart constructor based on the given predicate
+ *
+ * @dataFirst fromPredicate_
+ */
 export function fromPredicate<A>(predicate: Predicate<A>): (a: A) => Option<A> {
   return (a) => (predicate(a) ? some(a) : none)
+}
+
+/**
+ * Returns a smart constructor based on the given predicate
+ */
+export function fromPredicate_<A, B extends A>(
+  a: A,
+  refinement: Refinement<A, B>
+): Option<B>
+/**
+ * Returns a smart constructor based on the given predicate
+ */
+export function fromPredicate_<A>(a: A, predicate: Predicate<A>): Option<A>
+/**
+ * Returns a smart constructor based on the given predicate
+ */
+export function fromPredicate_<A>(a: A, predicate: Predicate<A>): Option<A> {
+  return predicate(a) ? some(a) : none
 }
 
 /**
@@ -295,21 +297,7 @@ export function getLeft<E, A>(ma: Either<E, A>): Option<E> {
 /**
  * Extracts the value out of the structure, if it exists. Otherwise returns the given default value
  *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     getOrElse(() => 0)
- *   ),
- *   1
- * )
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     getOrElse(() => 0)
- *   ),
- *   0
- * )
+ * @dataFirst getOrElse_
  */
 export function getOrElse<B>(onNone: () => B): <A>(ma: Option<A>) => A | B {
   return (o) => getOrElse_(o, onNone)
@@ -317,29 +305,15 @@ export function getOrElse<B>(onNone: () => B): <A>(ma: Option<A>) => A | B {
 
 /**
  * Extracts the value out of the structure, if it exists. Otherwise returns the given default value
+ *
+ * @dataFirst getOrElseS_
  */
-export function getOrElseInv<B>(onNone: () => B): (ma: Option<B>) => B {
+export function getOrElseS<B>(onNone: () => B): (ma: Option<B>) => B {
   return getOrElse(onNone)
 }
 
 /**
  * Extracts the value out of the structure, if it exists. Otherwise returns the given default value
- *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     getOrElse(() => 0)
- *   ),
- *   1
- * )
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     getOrElse(() => 0)
- *   ),
- *   0
- * )
  */
 export function getOrElse_<A, B>(ma: Option<A>, onNone: () => B): A | B {
   return ma._tag === "None" ? onNone() : ma.value
@@ -348,22 +322,13 @@ export function getOrElse_<A, B>(ma: Option<A>, onNone: () => B): A | B {
 /**
  * Extracts the value out of the structure, if it exists. Otherwise returns the given default value
  */
-export function getOrElseInv_<A>(ma: Option<A>, onNone: () => A): A {
+export function getOrElseS_<A>(ma: Option<A>, onNone: () => A): A {
   return getOrElse_(ma, onNone)
 }
 
 /**
  * Returns a `Refinement` (i.e. a custom type guard) from a `Option` returning function.
  * This function ensures that a custom type guard definition is type-safe.
- *
- * ```ts
- * type A = { type: 'A' }
- * type B = { type: 'B' }
- * type C = A | B
- *
- * const isA = (c: C): c is A => c.type === 'B' // <= typo but typescript doesn't complain
- * const isA = getRefinement<C, A>(c => (c.type === 'B' ? some(c) : none)) // static error: Type '"B"' is not assignable to type '"A"'
- * ```
  */
 export function getRefinement<A, B extends A>(
   getOption: (a: A) => Option<B>
@@ -380,10 +345,6 @@ export function getRight<E, A>(ma: Either<E, A>): Option<A> {
 
 /**
  * Returns `true` if the option is `None`, `false` otherwise
- *
- * @example
- * assert.strictEqual(isNone(some(1)), false)
- * assert.strictEqual(isNone(none), true)
  */
 export function isNone<A>(fa: Option<A>): fa is None {
   return fa._tag === "None"
@@ -391,10 +352,6 @@ export function isNone<A>(fa: Option<A>): fa is None {
 
 /**
  * Returns `true` if the option is an instance of `Some`, `false` otherwise
- *
- * @example
- * assert.strictEqual(isSome(some(1)), true)
- * assert.strictEqual(isSome(none), false)
  */
 export function isSome<A>(fa: Option<A>): fa is Some<A> {
   return fa._tag === "Some"
@@ -403,53 +360,21 @@ export function isSome<A>(fa: Option<A>): fa is Some<A> {
 /**
  * Use `A => B` to transform `Option[A]` to `Option[B]`
  */
-export const map_: <A, B>(fa: Option<A>, f: (a: A) => B) => Option<B> = (ma, f) =>
-  isNone(ma) ? none : some(f(ma.value))
+export function map_<A, B>(ma: Option<A>, f: (a: A) => B): Option<B> {
+  return isNone(ma) ? none : some(f(ma.value))
+}
 
 /**
  * Use `A => B` to transform `Option[A]` to `Option[B]`
+ *
+ * @dataFirst map_
  */
-export const map: <A, B>(f: (a: A) => B) => (fa: Option<A>) => Option<B> =
-  (f) => (fa) =>
-    map_(fa, f)
+export function map<A, B>(f: (a: A) => B) {
+  return (fa: Option<A>): Option<B> => map_(fa, f)
+}
 
 /**
  * This is `chain` + `fromNullable`, useful when working with optional values
- *
- * @example
- * interface Employee {
- *   company?: {
- *     address?: {
- *       street?: {
- *         name?: string
- *       }
- *     }
- *   }
- * }
- *
- * const employee1: Employee = { company: { address: { street: { name: 'high street' } } } }
- *
- * assert.deepStrictEqual(
- *   pipe(
- *     fromNullable(employee1.company),
- *     mapNullable(company => company.address),
- *     mapNullable(address => address.street),
- *     mapNullable(street => street.name)
- *   ),
- *   some('high street')
- * )
- *
- * const employee2: Employee = { company: { address: { street: {} } } }
- *
- * assert.deepStrictEqual(
- *   pipe(
- *     fromNullable(employee2.company),
- *     mapNullable(company => company.address),
- *     mapNullable(address => address.street),
- *     mapNullable(street => street.name)
- *   ),
- *   none
- * )
  */
 export function mapNullable<A, B>(
   f: (a: A) => B | null | undefined
@@ -459,22 +384,6 @@ export function mapNullable<A, B>(
 
 /**
  * Extracts the value out of the structure, if it exists. Otherwise returns `null`.
- *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     toNullable
- *   ),
- *   1
- * )
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     toNullable
- *   ),
- *   null
- * )
  */
 export function toNullable<A>(ma: Option<A>): A | null {
   return isNone(ma) ? null : ma.value
@@ -482,22 +391,6 @@ export function toNullable<A>(ma: Option<A>): A | null {
 
 /**
  * Extracts the value out of the structure, if it exists. Otherwise returns `undefined`.
- *
- * @example
- * assert.strictEqual(
- *   pipe(
- *     some(1),
- *     toUndefined
- *   ),
- *   1
- * )
- * assert.strictEqual(
- *   pipe(
- *     none,
- *     toUndefined
- *   ),
- *   undefined
- * )
  */
 export function toUndefined<A>(ma: Option<A>): A | undefined {
   return isNone(ma) ? undefined : ma.value
@@ -506,15 +399,6 @@ export function toUndefined<A>(ma: Option<A>): A | undefined {
 /**
  * Transforms an exception into an `Option`. If `f` throws, returns `None`, otherwise returns the output wrapped in
  * `Some`
- *
- * @example
- * assert.deepStrictEqual(
- *   tryCatch(() => {
- *     throw new Error()
- *   }),
- *   none
- * )
- * assert.deepStrictEqual(tryCatch(() => 1), some(1))
  */
 export function tryCatch<A>(f: Lazy<A>): Option<A> {
   try {
