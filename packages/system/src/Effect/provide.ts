ef3ca5f2be3a61ad5a3b927d8c783c4b0dab8b6c
Michael Arnaldi <arnaldimichael@gmail.com>
2021-07-01 14:22:04 +0100

    feat(system): expose missing data first

--- packages/system/src/Effect/provide.ts
+++ packages/system/src/Effect/provide.ts
@@ -6,8 +6,22 @@ import { provideSome_ } from "./provideSome"
 /**
  * Provides some of the environment required to run this effect,
  * leaving the remainder `R0` and combining it automatically using spread.
+ *
+ * @dataFirst provide_
  */
 export function provide<R>(r: R, __trace?: string) {
   return <E, A, R0>(next: Effect<R & R0, E, A>): Effect<R0, E, A> =>
-    provideSome_(next, (r0: R0) => ({ ...r0, ...r }), __trace)
+    provide_(next, r, __trace)
+}
+
+/**
+ * Provides some of the environment required to run this effect,
+ * leaving the remainder `R0` and combining it automatically using spread.
+ */
+export function provide_<E, A, R0, R>(
+  next: Effect<R & R0, E, A>,
+  r: R,
+  __trace?: string
+): Effect<R0, E, A> {
+  return provideSome_(next, (r0: R0) => ({ ...r0, ...r }), __trace)
 }
--- packages/system/src/Effect/sandboxWith.ts
+++ packages/system/src/Effect/sandboxWith.ts
@@ -8,10 +8,24 @@ import { unsandbox } from "./unsandbox"
 /**
  * Companion helper to `sandbox`. Allows recovery, and partial recovery, from
  * errors and defects alike.
+ *
+ * @dataFirst sandboxWith_
  */
 export function sandboxWith<R, E, A, E2>(
   f: (_: Effect<R, Cause<E>, A>) => Effect<R, Cause<E2>, A>,
   __trace?: string
 ) {
-  return (self: Effect<R, E, A>) => unsandbox(f(sandbox(self)), __trace)
+  return (self: Effect<R, E, A>) => sandboxWith_(self, f, __trace)
+}
+
+/**
+ * Companion helper to `sandbox`. Allows recovery, and partial recovery, from
+ * errors and defects alike.
+ */
+export function sandboxWith_<R, E, A, E2>(
+  self: Effect<R, E, A>,
+  f: (_: Effect<R, Cause<E>, A>) => Effect<R, Cause<E2>, A>,
+  __trace?: string
+) {
+  return unsandbox(f(sandbox(self)), __trace)
 }
