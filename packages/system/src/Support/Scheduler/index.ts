616585af7e8826ef5a2eca150f6f7f2a24bcc5d5
Michael Arnaldi <arnaldimichael@gmail.com>
2021-09-21 08:16:16 +0100

    perf(system): improve scheduler

--- packages/system/src/Support/Scheduler/index.ts
+++ packages/system/src/Support/Scheduler/index.ts
@@ -1,4 +1,21 @@
 import type { Lazy } from "../../Function"
+import { DoublyLinkedList } from "../DoublyLinkedList"
 
-export const defaultScheduler: (thunk: Lazy<void>) => void =
+let isRunning = false
+const tasks = new DoublyLinkedList<Lazy<void>>()
+
+export const immediate: (thunk: Lazy<void>) => void =
   typeof setImmediate !== "undefined" ? setImmediate : (x) => setTimeout(x, 0)
+
+export const defaultScheduler: (thunk: Lazy<void>) => void = (thunk) => {
+  tasks.add(thunk)
+  if (!isRunning) {
+    isRunning = true
+    immediate(() => {
+      while (tasks.length > 0) {
+        tasks.shift()!()
+      }
+      isRunning = false
+    })
+  }
+}
