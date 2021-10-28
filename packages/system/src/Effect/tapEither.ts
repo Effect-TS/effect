510802de5a853042c8b0447a5fb42798d85b619a
Michael Arnaldi <arnaldimichael@gmail.com>
2021-10-28 08:51:58 +0100

    fix(system): fix tapEither

--- packages/system/src/Effect/tapEither.ts
+++ packages/system/src/Effect/tapEither.ts
@@ -1,20 +1,29 @@
 // ets_tracing: off
 
-import type * as CS from "../Cause"
-import * as E from "../Either"
-import * as Ex from "../Exit"
-import { tapExit_ } from "."
+import type { Either } from "../Either"
+import { pipe } from "../Function"
+import * as core from "./core"
 import type { Effect } from "./effect"
+import { either } from "./either"
+import { fromEither } from "./fromEither"
 
 /**
  * Returns an effect that effectfully "peeks" at the result of this effect as an `Either`.
  */
 export function tapEither_<R, R1, E, E1, A, A1>(
   self: Effect<R, E, A>,
-  f: (exit: E.Either<CS.Cause<E>, A>) => Effect<R1, E1, A1>,
+  f: (exit: Either<E, A>) => Effect<R1, E1, A1>,
   __trace?: string
 ): Effect<R & R1, E | E1, A> {
-  return tapExit_(self, (exit) => f(Ex.fold_(exit, E.left, E.right)), __trace)
+  return pipe(
+    either(self),
+    core.chain((exit) =>
+      pipe(
+        f(exit),
+        core.chain(() => fromEither(() => exit))
+      )
+    )
+  )
 }
 
 /**
@@ -23,7 +32,7 @@ export function tapEither_<R, R1, E, E1, A, A1>(
  * @ets_data_first tapEither_
  */
 export function tapEither<R1, E, E1, A, A1>(
-  f: (exit: E.Either<CS.Cause<E>, A>) => Effect<R1, E1, A1>,
+  f: (exit: Either<E, A>) => Effect<R1, E1, A1>,
   __trace?: string
 ) {
   return <R>(self: Effect<R, E, A>) => tapEither_(self, f, __trace)
