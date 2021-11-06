9003db230e4153503d7764d2a96c01284cbddd51
Roger <roger@rogercyr.com>
2021-11-06 02:10:55 -0400

    chore(exp-stream): update Stream zipWithLatest

--- packages/system/src/Experimental/Stream/_internal/api/zipWithLatest.ts
+++ packages/system/src/Experimental/Stream/_internal/api/zipWithLatest.ts
@@ -1,13 +1,25 @@
 // ets_tracing: off
 
 import * as CK from "../../../../Collections/Immutable/Chunk"
+import * as Tp from "../../../../Collections/Immutable/Tuple"
 import * as T from "../../../../Effect"
 import * as E from "../../../../Either"
+import * as F from "../../../../Fiber"
 import { pipe } from "../../../../Function"
-import * as O from "../../../../Option"
-import * as CH from "../../Channel"
-import * as MH from "../../Channel/_internal/mergeHelpers"
-import * as C from "../core"
+import * as M from "../../../../Managed"
+import type * as O from "../../../../Option"
+import * as Ref from "../../../../Ref"
+import type * as C from "../core"
+import * as Chain from "./chain"
+import * as Concat from "./concat"
+import * as FromChunk from "./fromChunk"
+import * as FromEffect from "./fromEffect"
+import * as FromEffectOption from "./fromEffectOption"
+import * as FromPull from "./fromPull"
+import * as MapEffect from "./mapEffect"
+import * as MergeEither from "./mergeEither"
+import * as RepeatEffectOption from "./repeatEffectOption"
+import * as ToPull from "./toPull"
 
 /**
  * Zips the two streams so that when a value is emitted by either of the two streams,
@@ -21,78 +33,95 @@ export function zipWithLatest_<R, R1, E, E1, A, A1, A2>(
   that: C.Stream<R1, E1, A1>,
   f: (a: A, a1: A1) => A2
 ): C.Stream<R & R1, E | E1, A2> {
-  const mergedChannel: CH.Channel<
-    R & R1,
-    unknown,
-    unknown,
-    unknown,
-    E | E1,
-    O.Option<E.Either<A, A1>>,
-    any
-  > = pipe(
-    self.channel,
-    CH.mapOut((_) => O.map_(CK.last(_), E.left)),
-    CH.mergeWith(
-      pipe(
-        that.channel,
-        CH.mapOut((_) => O.map_(CK.last(_), E.right))
+  const pullNonEmpty = <R, E, O>(
+    pull: T.Effect<R, O.Option<E>, CK.Chunk<O>>
+  ): T.Effect<R, O.Option<E>, CK.Chunk<O>> =>
+    T.chain_(pull, (chunk) =>
+      CK.isEmpty(chunk) ? pullNonEmpty(pull) : T.succeed(chunk)
+    )
+
+  return FromPull.fromPull(
+    pipe(
+      M.do,
+      M.bind("left", () => M.map_(ToPull.toPull(self), pullNonEmpty)),
+      M.bind("right", () => M.map_(ToPull.toPull(that), pullNonEmpty)),
+      M.bind("pull", ({ left, right }) =>
+        ToPull.toPull(
+          Chain.chain_(
+            FromEffectOption.fromEffectOption(
+              T.raceWith_(
+                left,
+                right,
+                (leftDone, rightFiber) =>
+                  T.zipWith_(T.done(leftDone), F.join(rightFiber), (l, r) =>
+                    Tp.tuple(l, r, true)
+                  ),
+                (rightDone, leftFiber) =>
+                  T.zipWith_(T.done(rightDone), F.join(leftFiber), (r, l) =>
+                    Tp.tuple(l, r, false)
+                  )
+              ) as T.Effect<
+                R & R1,
+                O.Some<E | E1>,
+                Tp.Tuple<[CK.Chunk<A>, CK.Chunk<A1>, boolean]>
+              >
+            ),
+            ({ tuple: [l, r, leftFirst] }) =>
+              Chain.chain_(
+                FromEffect.fromEffect(
+                  Ref.makeRef(
+                    Tp.tuple(
+                      CK.unsafeGet_(l, CK.size(l) - 1),
+                      CK.unsafeGet_(r, CK.size(r) - 1)
+                    )
+                  )
+                ),
+                (latest) =>
+                  Concat.concat_(
+                    FromChunk.fromChunk(
+                      leftFirst
+                        ? CK.map_(r, (_) => f(CK.unsafeGet_(l, CK.size(l) - 1), _))
+                        : CK.map_(l, (_) => f(_, CK.unsafeGet_(r, CK.size(r) - 1)))
+                    ),
+                    pipe(
+                      RepeatEffectOption.repeatEffectOption(left),
+                      MergeEither.mergeEither(
+                        RepeatEffectOption.repeatEffectOption(right)
+                      ),
+                      MapEffect.mapEffect(
+                        E.fold(
+                          (leftChunk) =>
+                            Ref.modify_(latest, ({ tuple: [_, rightLatest] }) =>
+                              Tp.tuple(
+                                CK.map_(leftChunk, (_) => f(_, rightLatest)),
+                                Tp.tuple(
+                                  CK.unsafeGet_(leftChunk, CK.size(leftChunk) - 1),
+                                  rightLatest
+                                )
+                              )
+                            ),
+                          (rightChunk) =>
+                            Ref.modify_(latest, ({ tuple: [leftLatest, _] }) =>
+                              Tp.tuple(
+                                CK.map_(rightChunk, (_) => f(leftLatest, _)),
+                                Tp.tuple(
+                                  leftLatest,
+                                  CK.unsafeGet_(rightChunk, CK.size(rightChunk) - 1)
+                                )
+                              )
+                            )
+                        )
+                      ),
+                      Chain.chain(FromChunk.fromChunk)
+                    )
+                  )
+              )
+          )
+        )
       ),
-      (exit) => MH.done(T.done(exit)),
-      (exit) => MH.done(T.done(exit))
+      M.map(({ pull }) => pull)
     )
   )
-
-  const writer = (
-    lastLeft: O.Option<A>,
-    lastRight: O.Option<A1>
-  ): CH.Channel<
-    R1,
-    E | E1,
-    O.Option<E.Either<A, A1>>,
-    unknown,
-    E | E1,
-    CK.Chunk<A2>,
-    void
-  > =>
-    CH.readWith(
-      (val) => {
-        if (O.isSome(val)) {
-          if (E.isLeft(val.value)) {
-            const a1 = val.value.left
-
-            if (O.isSome(lastRight)) {
-              const a2 = lastRight.value
-
-              return CH.zipRight_(
-                CH.write(CK.single(f(a1, a2))),
-                writer(O.some(a1), lastRight)
-              )
-            } else {
-              return writer(O.some(a1), lastRight)
-            }
-          } else {
-            const a2 = val.value.right
-
-            if (O.isSome(lastLeft)) {
-              const a1 = lastLeft.value
-
-              return CH.zipRight_(
-                CH.write(CK.single(f(a1, a2))),
-                writer(lastLeft, O.some(a2))
-              )
-            } else {
-              return writer(lastLeft, O.some(a2))
-            }
-          }
-        } else {
-          return writer(lastLeft, lastRight)
-        }
-      },
-      (err) => CH.fail(err),
-      (_) => CH.unit
-    )
-
-  return new C.Stream(mergedChannel[">>>"](writer(O.none, O.none)))
 }
 
 /**
