1c04adde176f33908e4bc85b38015c90ba0d8b79
Harry Kightlinger <harry.kightlinger@automox.com>
2021-11-11 13:57:53 -0800

    Added Sync derive utilities

---
+++ packages/system/src/Sync/derive.ts
@@ -0,0 +1,111 @@
+// ets_tracing: off
+
+import type { AnyService, Has, Tag } from "../Has"
+import type { Sync } from "./core"
+import * as has from "./has"
+
+export type ShapeFn<T> = Pick<
+  T,
+  {
+    [k in keyof T]: T[k] extends (
+      ...args: infer ARGS
+    ) => Sync<infer R, infer E, infer A>
+      ? ((...args: ARGS) => Sync<R, E, A>) extends T[k]
+        ? k
+        : never
+      : never
+  }[keyof T]
+>
+
+export type ShapeCn<T> = Pick<
+  T,
+  {
+    [k in keyof T]: T[k] extends Sync<any, any, any> ? k : never
+  }[keyof T]
+>
+
+export type DerivedLifted<
+  T extends AnyService,
+  Fns extends keyof ShapeFn<T>,
+  Cns extends keyof ShapeCn<T>,
+  Values extends keyof T
+> = {
+  [k in Fns]: T[k] extends (...args: infer ARGS) => Sync<infer R, infer E, infer A>
+    ? (...args: ARGS) => Sync<R & Has<T>, E, A>
+    : never
+} & {
+  [k in Cns]: T[k] extends Sync<infer R, infer E, infer A>
+    ? Sync<R & Has<T>, E, A>
+    : never
+} & {
+  [k in Values]: Sync<Has<T>, never, T[k]>
+}
+
+export function deriveLifted<T extends AnyService>(
+  H: Tag<T>
+): <
+  Fns extends keyof ShapeFn<T> = never,
+  Cns extends keyof ShapeCn<T> = never,
+  Values extends keyof T = never
+>(
+  functions: Fns[],
+  effects: Cns[],
+  values: Values[]
+) => DerivedLifted<T, Fns, Cns, Values> {
+  return (functions, constants, values) => {
+    const ret = {} as any
+
+    for (const k of functions) {
+      ret[k] = (...args: any[]) => has.accessServiceM(H)((h) => h[k](...args))
+    }
+
+    for (const k of constants) {
+      ret[k] = has.accessServiceM(H)((h) => h[k])
+    }
+
+    for (const k of values) {
+      ret[k] = has.accessService(H)((h) => h[k])
+    }
+
+    return ret as any
+  }
+}
+
+export type DerivedAccessM<T extends AnyService, Gens extends keyof T> = {
+  [k in Gens]: <R_, E_, A_>(
+    f: (_: T[k]) => Sync<R_, E_, A_>,
+    __trace?: string
+  ) => Sync<R_ & Has<T>, E_, A_>
+}
+
+export function deriveAccessM<T extends AnyService>(
+  H: Tag<T>
+): <Gens extends keyof T = never>(generics: Gens[]) => DerivedAccessM<T, Gens> {
+  return (generics) => {
+    const ret = {} as any
+
+    for (const k of generics) {
+      ret[k] = (f: any) => has.accessServiceM(H)((h) => f(h[k]))
+    }
+
+    return ret as any
+  }
+}
+
+export type DerivedAccess<T extends AnyService, Gens extends keyof T> = {
+  [k in Gens]: <A_>(f: (_: T[k]) => A_, __trace?: string) => Sync<Has<T>, never, A_>
+}
+
+export function deriveAccess<T extends AnyService>(
+  H: Tag<T>
+): <Gens extends keyof T = never>(generics: Gens[]) => DerivedAccess<T, Gens> {
+  return (generics) => {
+    const ret = {} as any
+
+    for (const k of generics) {
+      ret[k] = (f: any) => has.accessService(H)((h) => f(h[k]))
+    }
+
+    return ret as any
+  }
+}
---
+++ packages/system/test/derive-sync.test.ts
@@ -0,0 +1,86 @@
+import { pipe } from "../src/Function"
+import { service, tag } from "../src/Has"
+import * as Sy from "../src/Sync"
+
+// module definition
+export const CalculatorId = Symbol()
+
+export function LiveCalculator() {
+  return service(CalculatorId, {
+    factor: 2,
+    factorFun: (): number => 3,
+    base: Sy.succeed(1),
+    add: (x: number, y: number) => Sy.succeedWith(() => x + y),
+    mul: (x: number, y: number) => Sy.succeedWith(() => x * y),
+    gen: <A>(a: A) => Sy.succeedWith(() => a)
+  })
+}
+
+export interface Calculator extends ReturnType<typeof LiveCalculator> {}
+
+// module tag
+export const Calculator = tag<Calculator>(CalculatorId)
+
+// lifted functions
+export const { add, base, factor, mul } = Sy.deriveLifted(Calculator)(
+  ["add", "mul"],
+  ["base"],
+  ["factor"]
+)
+
+// accessM functions
+export const { gen } = Sy.deriveAccessM(Calculator)(["gen"])
+
+// access functions
+export const { factorFun } = Sy.deriveAccess(Calculator)(["factorFun", "gen"])
+
+// program
+const program = pipe(
+  Sy.zip_(
+    gen((_) => _(1)),
+    factorFun((_) => _())
+  ),
+  Sy.chain((_) =>
+    pipe(
+      Sy.do,
+      Sy.bind("b", () => base),
+      Sy.bind("f", () => factor),
+      Sy.bind("_", () => Sy.succeed(_))
+    )
+  ),
+  Sy.chain(
+    ({
+      b,
+      f,
+      _: {
+        tuple: [x, y]
+      }
+    }) => Sy.chain_(add(b, f), (k) => Sy.succeed(k + x + y))
+  ),
+  Sy.chain((sum) => mul(sum, 3))
+)
+
+export function MockCalculator(): Calculator {
+  return {
+    serviceId: CalculatorId,
+    add: () => Sy.succeed(0),
+    mul: () => Sy.succeed(0),
+    base: Sy.succeed(0),
+    factor: 0,
+    gen: Sy.succeed,
+    factorFun: () => 0
+  }
+}
+
+describe("Derive Access", () => {
+  it("should use derived access functions", async () => {
+    expect(
+      await pipe(program, Sy.provideService(Calculator)(LiveCalculator()), Sy.run)
+    ).toEqual(21)
+  })
+  it("should use mock", async () => {
+    expect(
+      await pipe(program, Sy.provideService(Calculator)(MockCalculator()), Sy.run)
+    ).toEqual(0)
+  })
+})
