0adf085a417615063d9f73be258679c8188d7018
Maxwell Brown <maxwellbrown1990@gmail.com>
2021-11-12 15:08:55 -0500

    feat(string): add implementation of stripMargin to string module

---
+++ packages/core/test/string.test.ts
@@ -0,0 +1,28 @@
+import * as String from "../src/String"
+
+describe("String", () => {
+  it("should strip a leading prefix from each line", () => {
+    const actual = String.stripMargin(
+      `|
+       |Hello,
+       |World!
+       |`
+    )
+    const expected = "\nHello,\nWorld!\n"
+
+    expect(actual).toBe(expected)
+  })
+
+  it("should strip a leading prefix from each line using a margin character", () => {
+    const actual = String.stripMarginWith_(
+      `$
+       $Hello,
+       $World!
+       $`,
+      "$"
+    )
+    const expected = "\nHello,\nWorld!\n"
+
+    expect(actual).toBe(expected)
+  })
+})
