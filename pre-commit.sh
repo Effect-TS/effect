5608f51c5b16e5d622a64f143d0ada78a53dcac3
Michael Arnaldi <arnaldimichael@gmail.com>
2021-11-21 10:57:46 +0000

    chore: upgrade yarn setup

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-if yarn yarn-deduplicate -fl; then
-    echo "No duplicates found. Pursuing..."
-else
-    echo "ERROR: Lockfile contains duplicates!"
-    echo "deduplicating..."
-    yarn yarn-deduplicate
-    yarn
-    echo "deduplication finished"
-    exit 1
-fi
+yarn dedupe -c
