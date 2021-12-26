52f97aa416830987a9de190cd385edab40695cd7
Harry Kightlinger <harry.kightlinger@automox.com>
2021-12-26 12:45:43 -0800

    feat(Async): Added struct and structPar support for Async effect type

---
+++ packages/system/test/struct-async.test.ts
@@ -0,0 +1,25 @@
+import * as As from "../src/Async"
+
+describe("Async", () => {
+  it("struct", async () => {
+    expect(
+      await As.runPromise(
+        As.struct({
+          zero: As.succeed(0),
+          one: As.succeed(1)
+        })
+      )
+    ).toEqual({ zero: 0, one: 1 })
+  })
+  it("structPar", async () => {
+    let value = 0
+    expect(
+      await As.runPromise(
+        As.structPar({
+          one: As.delay(10)(As.succeedWith(() => value)),
+          zero: As.succeedWith(() => value++)
+        })
+      )
+    ).toEqual({ zero: 0, one: 1 })
+  })
+})
