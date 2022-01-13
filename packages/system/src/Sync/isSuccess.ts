c48d8923ba1b67336e8e7c205939bfa664070bed
Bálint Fülöp <bfulop@mac.com>
2022-01-13 10:01:30 +0100

    feat(sync): added missing isSuccess and isFailure for Sync effect type

---
+++ packages/system/src/Sync/isSuccess.ts
@@ -0,0 +1,15 @@
+// ets_tracing: off
+
+import type { Sync } from "./core"
+import { fold_ } from "./core"
+
+/**
+ * Returns whether this effect is a success.
+ */
+export function isSuccess<R, E, A>(self: Sync<R, E, A>) {
+  return fold_(
+    self,
+    () => false,
+    () => true
+  )
+}
