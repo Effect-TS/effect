b37fc55810885cb30e02581bc123f420bc2b0a64
Bálint Fülöp <bfulop@mac.com>
2022-01-13 10:28:14 +0100

    feat(sync): export and test isFailure

--- packages/system/test/isuccess-failure-sync.test.ts
+++ packages/system/test/isuccess-failure-sync.test.ts
@@ -2,13 +2,24 @@ import { pipe } from "@effect-ts/system/Function"
 
 import * as Sy from "../src/Sync"
 
-describe("Checking failure or success in Sync", () => {
-  it("should be true in case of success", async () => {
+describe("Checking success in Sync", () => {
+  it("should be true in case of success", () => {
     const result = pipe(Sy.succeed("ok"), Sy.isSuccess, Sy.run)
     expect(result).toBe(true)
   })
-  it("should be false in case of handled failure", async () => {
+  it("should be false in case of handled failure", () => {
     const result = pipe(Sy.fail("error"), Sy.isSuccess, Sy.run)
     expect(result).toBe(false)
+  })
+})
+
+describe("Checking failure in Sync", () => {
+  it("should be false in case of success", () => {
+    const result = pipe(Sy.succeed("ok"), Sy.isFailure, Sy.run)
+    expect(result).toBe(false)
+  })
+  it("should be true in case of handled failure", () => {
+    const result = pipe(Sy.fail("error"), Sy.isFailure, Sy.run)
+    expect(result).toBe(true)
   })
 })
