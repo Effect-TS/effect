193c79a32dca0c2a6f4317b9dca4f34053cf0e1d
Wessel van der Veen <wesselvanderveen@gmail.com>
2022-01-14 19:21:46 +0100

    refactor(exp-channel): remove obsolete m.gen

--- packages/system/src/Experimental/Stream/Channel/api/mergeWith.ts
+++ packages/system/src/Experimental/Stream/Channel/api/mergeWith.ts
@@ -71,24 +71,16 @@ export function mergeWith_<
     M.bind("pullL", ({ queueReader }) => ToPull.toPull(queueReader[">>>"](self))),
     M.bind("pullR", ({ queueReader }) => ToPull.toPull(queueReader[">>>"](that))),
     M.chain(({ input, pullL, pullR, queueReader }) =>
-      M.gen(function* (_) {
-        const transplant = yield* _(
-          M.fromEffect(
-            T.transplant((graft) =>
-              T.succeed({
-                pullL: graft(pullL),
-                pullR: graft(pullR)
-              })
-            )
-          )
+      T.toManaged(
+        T.transplant((graft) =>
+          T.succeed({
+            input,
+            pullL: graft(pullL),
+            pullR: graft(pullR),
+            queueReader
+          })
         )
-        return {
-          input,
-          pullL: transplant.pullL,
-          pullR: transplant.pullR,
-          queueReader
-        }
-      })
+      )
     ),
     M.map(({ input, pullL, pullR }) => {
       type MergeState = MH.MergeState<
