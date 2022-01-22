20132035c643eaf0e3ad0232778023bee01e56d9
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-22 21:42:43 +0000

    chore: fix tsc path, begin index removal

---
+++ packages/system-next/src/Cause.ts
@@ -0,0 +1,3 @@
+export * from "./Cause/definition"
+export * from "./Cause/errors"
+export * from "./Cause/operations"
---
+++ packages/system-next/src/Clock.ts
@@ -0,0 +1,6 @@
+// codegen:start {preset: barrel, include: ./Clock/*.ts}
+export * from "./Clock/Clock"
+export * from "./Clock/LiveClock"
+export * from "./Clock/ProxyClock"
+export * from "./Clock/TestClock"
+// codegen:end
