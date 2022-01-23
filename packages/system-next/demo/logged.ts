faffb1facbe7e82ffcf4e0aa8855c3da0e082f20
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-23 13:41:26 +0000

    chore: implement more zip operators

--- packages/system-next/demo/logged.ts
+++ packages/system-next/demo/logged.ts
@@ -1,11 +1,17 @@
-import * as T from "../src/Effect"
+import { Effect } from "../src/Effect"
 import * as LogLevel from "../src/LogLevel"
 
-export const numbers = T.succeed(0) + T.succeed(1) + T.succeed(2)
+export const numbers = Effect.succeed(0) + Effect.succeed(1) + Effect.succeed(2)
+export const numbersPar = Effect.succeed(0) & Effect.succeed(1) & Effect.succeed(2)
 
-export const program = numbers.flatMap(
-  ({ tuple: [a, b, c] }) =>
-    T.log(`yay: ${a}`) > T.logInfo(`ok: ${b}`) > T.logWarning(`maybe: ${c}`)
+export const program = (numbers + numbersPar).flatMap(
+  ({ tuple: [a, b, c, d, e, f] }) =>
+    Effect.log(`yay: ${a}`) >
+    Effect.logInfo(`ok: ${b}`) >
+    Effect.logWarning(`maybe: ${c}`) >
+    Effect.log(`yay: ${d}`) >
+    Effect.logInfo(`ok: ${e}`) >
+    Effect.logWarning(`maybe: ${f}`)
 )
 
 program.apply(LogLevel.locally(LogLevel.All)).unsafeRunPromise()
--- packages/system-next/src/Effect/operations/zip.ts
+++ packages/system-next/src/Effect/operations/zip.ts
@@ -25,28 +25,3 @@ export function zip<R2, E2, A2>(b: Effect<R2, E2, A2>, __trace?: string) {
   return <R, E, A>(a: Effect<R, E, A>): Effect<R & R2, E | E2, Tp.Tuple<[A, A2]>> =>
     zip_(a, b, __trace)
 }
-
-/**
- * Sequentially zips this effect with the specified effect
- *
- * @ets operator ets/Effect +
- * @ets fluent ets/Effect zipFlatten
- */
-export function zipFlatten_<R, E, A, R2, E2, A2>(
-  self: Effect<R, E, A>,
-  that: Effect<R2, E2, A2>,
-  __etsTrace?: string
-): Effect<R & R2, E | E2, Tp.MergeTuple<A, A2>> {
-  return map_(zip_(self, that), ({ tuple: [_a, _b] }) => Tp.mergeTuple(_a, _b))
-}
-
-/**
- * Sequentially zips this effect with the specified effect
- *
- * @ets_data_first zipFlatten_
- */
-export function zipFlatten<R2, E2, A2>(that: Effect<R2, E2, A2>, __etsTrace?: string) {
-  return <R, E, A>(
-    self: Effect<R, E, A>
-  ): Effect<R & R2, E | E2, Tp.MergeTuple<A, A2>> => zipFlatten_(self, that)
-}
