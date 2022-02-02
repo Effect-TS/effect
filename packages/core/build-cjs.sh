8c25f0e91f9f6b6a7dcbd3e16bb3ad0f8f9fc535
Michael Arnaldi <arnaldimichael@gmail.com>
2022-02-02 17:49:56 +0000

    chore: update cjs build

--- packages/core/build-cjs.sh
+++ packages/core/build-cjs.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-yarn ets:esbuild ./build/esm/**/*.js --format=cjs --outdir=./build/cjs
+yarn ets:esbuild `find build/esm \\( -name '*.js' \\)` --platform=node --target=node10.4 --format=cjs --outdir=build/cjs --log-level=error
