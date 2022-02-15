803def9d5ea93f17c18b8881b1df4049b341f670
Michael Arnaldi <arnaldimichael@gmail.com>
2022-02-15 14:33:29 +0000

    chore: detect circular deps in esm built

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
     "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "import/first": "error",
-    "import/no-cycle": "error",
+    "import/no-cycle": "off",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
     "import/no-unresolved": "error",
