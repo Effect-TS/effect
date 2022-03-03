7d39a7abe73ce2d504f50ed4e7c9e173a539184f
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-03-03 21:09:10 +0000

    chore(core): add metrics to fiber context

--- packages/core/src/support/Scheduler.ts
+++ packages/core/src/support/Scheduler.ts
@@ -1,18 +1,4 @@
 import type { Lazy } from "../data/Function"
-import { DoublyLinkedList } from "./DoublyLinkedList"
 
-let isRunning = false
-const tasks = new DoublyLinkedList<Lazy<void>>()
-
-export const defaultScheduler: (thunk: Lazy<void>) => void = (thunk) => {
-  tasks.add(thunk)
-  if (!isRunning) {
-    isRunning = true
-    Promise.resolve().then(() => {
-      while (tasks.length > 0) {
-        tasks.shift()!()
-      }
-      isRunning = false
-    })
-  }
-}
+export const defaultScheduler: (thunk: Lazy<void>) => void =
+  typeof setImmediate === "undefined" ? (thunk) => setTimeout(thunk, 0) : setImmediate
