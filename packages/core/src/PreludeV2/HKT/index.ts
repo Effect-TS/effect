e20876c74fb04a611096fd73d7bba2812a34f53e
Edouard Penin <e.peninb@gmail.com>
2022-03-24 21:20:28 +0100

    feat(StateT): Move to PV2

--- packages/core/src/PreludeV2/HKT/index.ts
+++ packages/core/src/PreludeV2/HKT/index.ts
@@ -1,3 +1,5 @@
+import type { UnionToIntersection } from "@effect-ts/core/Utils"
+
 export declare const URI: unique symbol
 
 export interface Typeclass<F extends HKT> {
@@ -62,4 +64,15 @@ export interface ComposeF<F extends HKT, G extends HKT> extends HKT {
  */
 export function instance<T>(_: T): T {
   return _ as any
+}
+
+export function intersect<As extends any[]>(
+  ...as: As
+): UnionToIntersection<As[number]> {
+  const y = {}
+  for (let i = 0; i < as.length; i++) {
+    Object.assign(y, as[i])
+  }
+  // @ts-expect-error
+  return y
 }
--- packages/core/src/StateT/index.ts
+++ packages/core/src/StateT/index.ts
@@ -2,4 +2,61 @@
 
 import "../Operator/index.js"
 
-export * from "./Classic/index.js"
+import { pipe } from "../Function"
+import * as P from "../PreludeV2/index.js"
+
+export interface StateT<F extends P.HKT, S> extends P.HKT {
+  readonly type: (
+    s: S
+  ) => P.Kind<F, this["X"], this["I"], this["R"], this["E"], readonly [S, this["A"]]>
+}
+
+export interface State<F extends P.HKT, S> extends P.Typeclass<StateT<F, S>> {
+  readonly get: P.Kind<StateT<F, S>, any, unknown, unknown, never, S>
+
+  readonly update: (
+    f: (s: S) => S
+  ) => <X, I, R, E, A>(
+    fa: P.Kind<StateT<F, S>, X, I, R, E, A>
+  ) => P.Kind<StateT<F, S>, X, I, R, E, A>
+
+  readonly runState: (
+    s: S
+  ) => <X, I, R, E, A>(
+    fa: P.Kind<StateT<F, S>, X, I, R, E, A>
+  ) => P.Kind<F, X, I, R, E, A>
+}
+
+export function stateT<S>() {
+  return <F extends P.HKT>(F_: P.Monad<F>) => {
+    const succeed = P.succeedF(F_, F_)
+    const monad: P.Monad<StateT<F, S>> = {
+      any: () => (s) => succeed([s, {} as unknown] as const),
+      map: (f) => (fa) => (s) =>
+        pipe(
+          fa(s),
+          F_.map(([s, a]) => [s, f(a)])
+        ),
+      flatten: (ffa) => (s) =>
+        pipe(
+          ffa(s),
+          F_.map(([s2, fa]) => fa(s2)),
+          F_.flatten
+        )
+    }
+    const state: State<F, S> = {
+      update: (f) => (fa) => (s) =>
+        pipe(
+          fa(s),
+          F_.map(([s, a]) => [f(s), a])
+        ),
+      get: (s) => succeed([s, s]),
+      runState: (s) => (fa) =>
+        pipe(
+          fa(s),
+          F_.map(([_, a]) => a)
+        )
+    }
+    return P.intersect(monad, state)
+  }
+}
