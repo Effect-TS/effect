67d60c1c79433f5ab754c864c7ef9cd9fe200105
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2022-04-01 00:19:03 +0200

    fix hashmap collision for equality (no repro - optimistic) (#1153)

--- packages/system/test/collections/hashmap.test.ts
+++ packages/system/test/collections/hashmap.test.ts
@@ -1,13 +1,37 @@
-import { hash } from "@effect-ts/system/Structural"
+import * as St from "@effect-ts/system/Structural"
 
 import * as HM from "../../src/Collections/Immutable/HashMap/index.js"
 import { pipe } from "../../src/Function/index.js"
 
+class Key implements St.HasEquals, St.HasHash {
+  constructor(readonly n: number) {}
+
+  get [St.hashSym](): number {
+    return St.hashNumber(this.n)
+  }
+
+  [St.equalsSym](u: unknown): boolean {
+    return u instanceof Key && this.n === u.n
+  }
+}
+
+class Value implements St.HasEquals, St.HasHash {
+  constructor(readonly s: string) {}
+
+  get [St.hashSym](): number {
+    return St.hashString(this.s)
+  }
+
+  [St.equalsSym](u: unknown): boolean {
+    return u instanceof Value && this.s === u.s
+  }
+}
+
 describe("HashMap", () => {
   it("hasHash_", () => {
-    expect(HM.hasHash_(pipe(HM.make<number, number>(), HM.set(0, 0)), 0, hash(0))).toBe(
-      true
-    )
+    expect(
+      HM.hasHash_(pipe(HM.make<number, number>(), HM.set(0, 0)), 0, St.hash(0))
+    ).toBe(true)
   })
   it("from", () => {
     expect(
@@ -16,5 +40,18 @@ describe("HashMap", () => {
         [3, 4]
       ])
     ).toEqual(pipe(HM.make<number, number>(), HM.set(1, 2), HM.set(3, 4)))
+  })
+
+  it("hashmapequals", () => {
+    const a = HM.from([
+      [new Key(0), new Value("a")],
+      [new Key(1), new Value("b")]
+    ])
+    const b = HM.from([
+      [new Key(0), new Value("a")],
+      [new Key(1), new Value("b")]
+    ])
+
+    expect(St.equals(a, b)).toBe(true)
   })
 })
