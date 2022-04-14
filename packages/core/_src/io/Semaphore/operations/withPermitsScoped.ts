7f0bfa7ab51b04891f19b72d35aaf0441c268c3a
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-04-14 19:19:45 +0000

    feat(@effect/core): make Semaphore its own datatype

---
+++ packages/core/_src/io/Semaphore/operations/withPermitsScoped.ts
@@ -0,0 +1,24 @@
+import { concreteSemaphore } from "@effect/core/io/Semaphore/operations/_internal/SemaphoreInternal";
+
+/**
+ * Returns a scoped effect that describes acquiring the specified number of
+ * permits and releasing them when the scope is closed.
+ *
+ * @tsplus fluent ets/Semaphore withPermitsScoped
+ */
+export function withPermitsScoped_(
+  self: Semaphore,
+  permits: number,
+  __tsplusTrace?: string
+): Effect.RIO<Has<Scope>, void> {
+  concreteSemaphore(self);
+  return self.semaphore.withPermitsScoped(permits);
+}
+
+/**
+ * Returns a scoped effect that describes acquiring the specified number of
+ * permits and releasing them when the scope is closed.
+ *
+ * @tsplus static ets/Semaphore/Aspects withPermitsScoped
+ */
+export const withPermitsScoped = Pipeable(withPermitsScoped_);
