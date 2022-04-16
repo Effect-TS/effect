149a73a279c09c42f250d9024e24b76a17123b60
Michael Arnaldi <arnaldimichael@gmail.com>
2022-04-16 23:05:19 +0000

    chore: update tsplus

--- packages/core/_test/io/Effect/absolve.test.ts
+++ packages/core/_test/io/Effect/absolve.test.ts
@@ -1,15 +1,12 @@
 describe.concurrent("Effect", () => {
   describe.concurrent("absolve", () => {
-    it("fluent/static method consistency", async () => {
-      const ioEither = Effect.succeed(Either.right("test"));
-      const program = Effect.Do()
-        .bind("abs1", () => ioEither.absolve())
-        .bind("abs2", () => Effect.absolve(ioEither));
-
-      const { abs1, abs2 } = await program.unsafeRunPromise();
-
-      assert.strictEqual(abs1, "test");
-      assert.strictEqual(abs2, "test");
-    });
+    it("fluent/static method consistency", () =>
+      Do(($) => {
+        const ioEither = Effect.succeed(Either.right("test"));
+        const abs1 = $(ioEither.absolve());
+        const abs2 = $(Effect.absolve(ioEither));
+        assert.strictEqual(abs1, "test");
+        assert.strictEqual(abs2, "test");
+      }).unsafeRunPromise());
   });
 });
