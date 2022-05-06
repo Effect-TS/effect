6697e9eaf38eb33092e08507c214033c65b88777
Peter Krol <peter@peterkrol.com>
2022-05-05 21:42:39 -0400

    fix(stream): emit elements eagerly

--- packages/core/_src/stream/Stream/operations/_internal/loopOnPartialChunksElements.ts
+++
@@ -1,9 +0,0 @@
-import { loopOnPartialChunks } from "@effect/core/stream/Stream/operations/_internal/loopOnPartialChunks";
-
-export function loopOnPartialChunksElements<R, E, A, R1, E1, A1>(
-  self: Stream<R, E, A>,
-  f: (a: A, emit: (a: A1) => Effect.UIO<void>) => Effect<R1, E1, void>,
-  __tsplusTrace?: string
-): Stream<R & R1, E | E1, A1> {
-  return loopOnPartialChunks(self, (chunk, emit) => Effect.forEachDiscard(chunk, (value) => f(value, emit)).as(true));
-}
--- packages/core/_src/stream/Stream/operations/mapEffect.ts
+++ packages/core/_src/stream/Stream/operations/mapEffect.ts
@@ -1,4 +1,4 @@
-import { loopOnPartialChunksElements } from "@effect/core/stream/Stream/operations/_internal/loopOnPartialChunksElements";
+import { concreteStream, StreamInternal } from "@effect/core/stream/Stream/operations/_internal/StreamInternal";
 
 /**
  * Maps over elements of the stream with the specified effectful function.
@@ -10,7 +10,10 @@ export function mapEffect_<R, E, A, R1, E1, B>(
   f: (a: A) => Effect<R1, E1, B>,
   __tsplusTrace?: string
 ): Stream<R & R1, E | E1, B> {
-  return loopOnPartialChunksElements(self, (a, emit) => f(a).flatMap(emit));
+  concreteStream(self);
+  return new StreamInternal(
+    self.channel >> loop(Chunk.empty<A>()[Symbol.iterator](), f)
+  );
 }
 
 /**
@@ -19,3 +22,25 @@ export function mapEffect_<R, E, A, R1, E1, B>(
  * @tsplus static ets/Stream/Aspects mapEffect
  */
 export const mapEffect = Pipeable(mapEffect_);
+
+function loop<R, E, A, R1, E1, A1>(
+  chunkIterator: Iterator<A>,
+  f: (a: A) => Effect<R1, E1, A1>
+): Channel<R & R1, E, Chunk<A>, unknown, E | E1, Chunk<A1>, unknown> {
+  const next = chunkIterator.next();
+  if (next.done) {
+    return Channel.readWithCause(
+      elem => loop(elem[Symbol.iterator](), f),
+      err => Channel.failCause(err),
+      done => Channel.succeed(done)
+    );
+  } else {
+    return Channel.unwrap(
+      f(next.value).map(
+        a1 =>
+          Channel.write(Chunk.single(a1)) >
+            loop<R, E, A, R1, E1, A1>(chunkIterator, f)
+      )
+    );
+  }
+}
