26aed052cbc14fe665fc23b8ae37912dd29e5be8
Michael Arnaldi <arnaldimichael@gmail.com>
2022-06-02 20:58:28 +0000

    fix(@effect/core): forEachEffect_ signature

---
+++ .changeset/curvy-planes-tan.md
@@ -0,0 +1,5 @@
+---
+"@effect/core": patch
+---
+
+Fix forEachEffect\_ signature
--- packages/core/_src/io/Effect/operations/forEachEffect.ts
+++ packages/core/_src/io/Effect/operations/forEachEffect.ts
@@ -8,7 +8,7 @@ export function forEachEffect_<R, E, A, R1, E1, B>(
   self: Effect<R, E, A>,
   f: (a: A) => Effect<R1, E1, B>,
   __tsplusTrace?: string
-): Effect<R | R1, E | E1, Option<B>> {
+): Effect<R | R1, E1, Option<B>> {
   return self.foldCauseEffect(
     () => Effect.none,
     (a) => f(a).map(Option.some)
