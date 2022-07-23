38f98209ff78486dcb0f717366ddb0b5446c2eb8
Michael Arnaldi <arnaldimichael@gmail.com>
2022-07-23 08:02:56 +0000

    chore(@effect/core): improve scheduler compatibility

--- packages/core/_src/support/Scheduler.ts
+++ packages/core/_src/support/Scheduler.ts
@@ -1,3 +1,5 @@
+import { DoublyLinkedList } from "@tsplus/stdlib/collections/mutable/DoublyLinkedList"
+
 export type Task = Lazy<void>
 
 export interface Scheduler {
@@ -5,38 +7,35 @@ export interface Scheduler {
 }
 
 export class DefaultScheduler {
-  readonly scheduled = {
-    running: false,
-    tasks: new DoublyLinkedList<Task>()
-  }
-
-  readonly queueMicrotask = typeof queueMicrotask === "function" ?
-    queueMicrotask :
-    (fn: () => void) => Promise.resolve(void 0).then(fn)
+  readonly running = new AtomicReference(false)
+  readonly tasks = new AtomicReference(new DoublyLinkedList<Task>())
+  readonly promise = Promise.resolve(void 0)
 
   starveInternal(depth: number) {
-    const toRun = this.scheduled.tasks
-    this.scheduled.tasks = new DoublyLinkedList()
+    const toRun = this.tasks.get
+    this.tasks.set(new DoublyLinkedList())
     toRun.forEach((task) => {
       task()
     })
-    if (this.scheduled.tasks.isEmpty) {
-      this.scheduled.running = false
+    if (this.tasks.get.isEmpty) {
+      this.running.set(false)
     } else {
       this.starve(depth)
     }
   }
+
   starve(depth = 0) {
     if (depth >= 2048) {
       setTimeout(() => this.starveInternal(0), 0)
     } else {
-      this.queueMicrotask(() => this.starveInternal(depth + 1))
+      this.promise.then(() => this.starveInternal(depth + 1))
     }
   }
+
   scheduleTask(task: Task) {
-    this.scheduled.tasks.add(task)
-    if (!this.scheduled.running) {
-      this.scheduled.running = true
+    this.tasks.get.add(task)
+    if (!this.running.get) {
+      this.running.set(true)
       this.starve()
     }
   }
@@ -45,23 +44,20 @@ export class DefaultScheduler {
 export const defaultScheduler: Scheduler = new DefaultScheduler()
 
 export class StagedScheduler {
-  readonly scheduled = {
-    tasks: new DoublyLinkedList<Task>()
-  }
-
+  readonly tasks = new DoublyLinkedList<Task>()
   readonly deferred = new AtomicReference(false)
 
   scheduleTask(task: Task) {
     if (this.deferred.get) {
       defaultScheduler.scheduleTask(task)
     } else {
-      this.scheduled.tasks.add(task)
+      this.tasks.add(task)
     }
   }
 
   flush() {
-    while (!this.scheduled.tasks.isEmpty) {
-      this.scheduled.tasks.shift()!()
+    while (!this.tasks.isEmpty) {
+      this.tasks.shift()!()
     }
     this.deferred.set(true)
   }
