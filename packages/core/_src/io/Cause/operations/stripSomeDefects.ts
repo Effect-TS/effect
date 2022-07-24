ad1ef9f13f83f9bbe8be9e9a1ffe469c3489dd56
Maxwell Brown <maxwellbrown1990@gmail.com>
2022-07-24 12:10:10 +0000

    feat(@effect/core): add Cause.stripSomeDefects

---
+++ packages/core/_src/io/Cause/operations/stripSomeDefects.ts
@@ -0,0 +1,44 @@
+import { Stackless } from "@effect/core/io/Cause/definition"
+
+/**
+ * Remove all `Die` causes that the specified partial function is defined at,
+ * returning `Some` with the remaining causes or `None` if there are no
+ * remaining causes.
+ *
+ * @tsplus static effect/core/io/Cause.Aspects stripSomeDefects
+ * @tsplus pipeable effect/core/io/Cause stripSomeDefects
+ */
+export function stripSomeDefects(pf: (defect: unknown) => unknown) {
+  return <E>(cause: Cause<E>): Maybe<Cause<E>> =>
+    cause.fold<E, Maybe<Cause<E>>>(
+      Maybe.some(Cause.empty),
+      (e, trace) => Maybe.some(Cause.fail(e, trace)),
+      (t, trace) => pf(t) ? Maybe.none : Maybe.some(Cause.die(t, trace)),
+      (fiberId, trace) => Maybe.some(Cause.interrupt(fiberId, trace)),
+      (x, y) => {
+        if (x.isSome() && y.isSome()) {
+          return Maybe.some(Cause.then(x.value, y.value))
+        }
+        if (x.isSome() && y.isNone()) {
+          return Maybe.some(x.value)
+        }
+        if (x.isNone() && y.isSome()) {
+          return Maybe.some(y.value)
+        }
+        return Maybe.none
+      },
+      (x, y) => {
+        if (x.isSome() && y.isSome()) {
+          return Maybe.some(Cause.both(x.value, y.value))
+        }
+        if (x.isSome() && y.isNone()) {
+          return Maybe.some(x.value)
+        }
+        if (x.isNone() && y.isSome()) {
+          return Maybe.some(y.value)
+        }
+        return Maybe.none
+      },
+      (causeOption, stackless) => causeOption.map((cause) => new Stackless(cause, stackless))
+    )
+}
