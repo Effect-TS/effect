3e246581cd0d5832ca2193800a61ca672339a137
Michael Arnaldi <arnaldimichael@gmail.com>
2022-08-25 13:25:46 +0000

    chore: fix retryOrElse inference

--- packages/core/_src/io/Effect/operations/retryOrElse.ts
+++ packages/core/_src/io/Effect/operations/retryOrElse.ts
@@ -6,8 +6,8 @@
  * @tsplus static effect/core/io/Effect.Aspects retryOrElse
  * @tsplus pipeable effect/core/io/Effect retryOrElse
  */
-export function retryOrElse<S, R1, E, A1, R2, E2, A2>(
-  policy: Schedule<S, R1, E, A1>,
+export function retryOrElse<S, R1, E extends E3, A1, R2, E2, A2, E3>(
+  policy: Schedule<S, R1, E3, A1>,
   orElse: (e: E, out: A1) => Effect<R2, E2, A2>
 ) {
   return <R, A>(self: Effect<R, E, A>): Effect<R | R1 | R2, E | E2, A | A2> =>
