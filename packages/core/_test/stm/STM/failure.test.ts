7ca1e5b4e229725d277fed3d9959c7af0fe66cf7
Michael Arnaldi <arnaldimichael@gmail.com>
2022-10-15 15:12:44 +0000

    chore: ensure Do works out of the box

--- packages/core/_test/stm/STM/failure.test.ts
+++ packages/core/_test/stm/STM/failure.test.ts
@@ -1,33 +1,21 @@
 describe.concurrent("STM", () => {
   describe.concurrent("Failure must", () => {
-    it("rollback full transaction", async () => {
-      const program = Effect.Do()
-        .bind("tRef", () => TRef.makeCommit(0))
-        .bind(
-          "either",
-          ({ tRef }) => (tRef.update((n) => n + 10) > STM.failSync("error")).commit.either
-        )
-        .bind("value", ({ tRef }) => tRef.get)
+    it("rollback full transaction", () =>
+      Do(($) => {
+        const tRef = $(TRef.make(0))
+        const either = $((tRef.update((n) => n + 10) > STM.failSync("error")).commit.either)
+        const value = $(tRef.get)
+        assert.isTrue(either == Either.left("error"))
+        assert.strictEqual(value, 0)
+      }).unsafeRunPromise())
 
-      const { either, value } = await program.unsafeRunPromise()
-
-      assert.isTrue(either == Either.left("error"))
-      assert.strictEqual(value, 0)
-    })
-
-    it("be ignored", async () => {
-      const program = Effect.Do()
-        .bind("tRef", () => TRef.makeCommit(0))
-        .bind(
-          "either",
-          ({ tRef }) => (tRef.update((n) => n + 10) > STM.failSync("error")).commit.ignore
-        )
-        .bind("value", ({ tRef }) => tRef.get)
-
-      const { either, value } = await program.unsafeRunPromise()
-
-      assert.isUndefined(either)
-      assert.strictEqual(value, 0)
-    })
+    it("be ignored", () =>
+      Do(($) => {
+        const tRef = $(TRef.make(0))
+        const either = $((tRef.update((n) => n + 10) > STM.failSync("error")).commit.ignore)
+        const value = $(tRef.get)
+        assert.isUndefined(either)
+        assert.strictEqual(value, 0)
+      }).unsafeRunPromise())
   })
 })
