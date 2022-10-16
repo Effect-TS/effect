e93aa8b4a3b2d82a43890d975c1235043a57dd36
Ihar Abukhouski <ihar.abukhouski@gmail.com>
2022-10-16 16:23:52 -0500

    Update MetricKey.ts
    Fixed typo

--- packages/core/_src/io/Metrics/MetricKey.ts
+++ packages/core/_src/io/Metrics/MetricKey.ts
@@ -4,7 +4,7 @@ export type MetricKeySym = typeof MetricKeySym
 /**
  * A `MetricKey` is a unique key associated with each metric. The key is based
  * on a combination of the metric type, the name and tags associated with the
- * metric, and any other information to describe a a metric, such as the
+ * metric, and any other information to describe a metric, such as the
  * boundaries of a histogram. In this way, it is impossible to ever create
  * different metrics with conflicting keys.
  *
