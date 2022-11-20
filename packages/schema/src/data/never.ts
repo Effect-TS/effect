b198a880de76bd016359f4eb67e9ad5ce6d91c5d
gcanti <giulio.canti@gmail.com>
2022-11-20 17:59:20 +0100

    /data/never as empty union

--- packages/schema/src/data/never.ts
+++ packages/schema/src/data/never.ts
@@ -1,13 +1,15 @@
 /**
  * @since 1.0.0
  */
-import * as DE from "@fp-ts/codec/DecodeError"
-import type * as D from "@fp-ts/codec/Decoder"
-import type * as G from "@fp-ts/codec/Guard"
+import * as A from "@fp-ts/codec/Arbitrary"
+import * as D from "@fp-ts/codec/Decoder"
+import type * as E from "@fp-ts/codec/Encoder"
+import * as G from "@fp-ts/codec/Guard"
 import * as I from "@fp-ts/codec/internal/common"
 import * as P from "@fp-ts/codec/Provider"
-import type * as S from "@fp-ts/codec/Schema"
-import * as O from "@fp-ts/data/Option"
+import * as S from "@fp-ts/codec/Schema"
+import * as Sh from "@fp-ts/codec/Show"
+import { identity } from "@fp-ts/data/Function"
 
 /**
  * @since 1.0.0
@@ -19,21 +21,39 @@ export const id = Symbol.for("@fp-ts/codec/data/never")
  */
 export const Provider: P.Provider = P.make(id, {
   [I.GuardId]: () => Guard,
+  [I.ArbitraryId]: () => Arbitrary,
   [I.DecoderId]: () => Decoder,
-  [I.JsonDecoderId]: () => Decoder
+  [I.JsonDecoderId]: () => Decoder,
+  [I.ShowId]: () => Show,
+  [I.JsonEncoderId]: () => Encoder
 })
 
 /**
  * @since 1.0.0
  */
-export const Schema: S.Schema<never> = I.declareSchema(id, O.none, Provider) as any
+export const Schema: S.Schema<never> = S.union()
 
 /**
  * @since 1.0.0
  */
-export const Guard: G.Guard<never> = I.makeGuard(Schema, (_u: unknown): _u is never => false)
+export const Guard: G.Guard<never> = G.union()
 
 /**
  * @since 1.0.0
  */
-export const Decoder: D.Decoder<unknown, never> = I.fromGuard(Guard, (u) => DE.notType("never", u))
+export const Decoder: D.Decoder<unknown, never> = D.union()
+
+/**
+ * @since 1.0.0
+ */
+export const Encoder: E.Encoder<never, never> = I.makeEncoder(Schema, identity)
+
+/**
+ * @since 1.0.0
+ */
+export const Arbitrary: A.Arbitrary<never> = A.union()
+
+/**
+ * @since 1.0.0
+ */
+export const Show: Sh.Show<never> = Sh.union()
