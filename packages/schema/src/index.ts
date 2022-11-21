8044c67a396c833e9df8561f7806283590b290dc
gcanti <giulio.canti@gmail.com>
2022-11-21 08:02:16 +0100

    export Guard module

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,6 +2,7 @@
  * @since 1.0.0
  */
 import * as decoder from "@fp-ts/codec/Decoder"
+import * as guard from "@fp-ts/codec/Guard"
 import * as schema from "@fp-ts/codec/Schema"
 
 export {
@@ -9,6 +10,10 @@ export {
    * @since 1.0.0
    */
   decoder,
+  /**
+   * @since 1.0.0
+   */
+  guard,
   /**
    * @since 1.0.0
    */
