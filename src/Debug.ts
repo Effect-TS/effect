74fa4086e5b26c5f20706a3209e6c8345e187bcc
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-23 22:20:59 +0000

    avoid skipLibCheck, isolate debug

---
+++ src/Debug.ts
@@ -0,0 +1,27 @@
+/**
+ * @since 2.0.0
+ */
+import { Debug, getCallTrace, nodeSourceMapExtractor, runtimeDebug, withCallTrace } from "@effect/io/Debug"
+
+export {
+  /**
+   * @since 2.0.0
+   */
+  Debug,
+  /**
+   * @since 2.0.0
+   */
+  getCallTrace,
+  /**
+   * @since 2.0.0
+   */
+  nodeSourceMapExtractor,
+  /**
+   * @since 2.0.0
+   */
+  runtimeDebug,
+  /**
+   * @since 2.0.0
+   */
+  withCallTrace
+}
