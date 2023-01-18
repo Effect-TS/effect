b6e7d3c5851e8d0c587ebdbb3fb78aff0d75bd97
Jesse Kelly <kellyjesse881@gmail.com>
2023-01-18 12:59:46 +0100

    feat: replaced mod with floatSafeRemainder

--- packages/schema/src/data/filter.ts
+++ packages/schema/src/data/filter.ts
@@ -54,6 +54,17 @@ export const greaterThanOrEqualTo = <A extends number>(
       })
     )
 
+// https://stackoverflow.com/questions/3966484/why-does-modulus-operator-return-fractional-number-in-javascript/31711034#31711034
+// https://github.com/colinhacks/zod/blob/5616f6b505090ebb1775d1d5567d3ee7baa5519d/src/types.ts#L915
+function floatSafeRemainder(val: number, step: number) {
+  const valDecCount = (val.toString().split(".")[1] || "").length
+  const stepDecCount = (step.toString().split(".")[1] || "").length
+  const decCount = valDecCount > stepDecCount ? valDecCount : stepDecCount
+  const valInt = parseInt(val.toFixed(decCount).replace(".", ""))
+  const stepInt = parseInt(step.toFixed(decCount).replace(".", ""))
+  return (valInt % stepInt) / Math.pow(10, decCount)
+}
+
 /**
  * @since 1.0.0
  */
@@ -64,9 +75,9 @@ export const multipleOf = <A extends number>(
   (self: Schema<A>): Schema<A> =>
     pipe(
       self,
-      I.filter((a): a is A => a % divisor === 0, {
+      I.filter((a): a is A => floatSafeRemainder(a, divisor) === 0, {
         description: `a number divisible by ${divisor}`,
-        jsonSchema: { multipleOf: divisor },
+        jsonSchema: { multipleOf: divisor < 0 ? -divisor : divisor }, // spec requires positive divisor
         ...annotationOptions
       })
     )
