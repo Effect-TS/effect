96a7c865e57ab5a5b3adb57233137ed56884bc69
Jesse Kelly <kellyjesse881@gmail.com>
2023-01-18 13:20:15 +0100

    chore: extended tests

--- packages/schema/test/data/filter/MultipleOf.ts
+++ packages/schema/test/data/filter/MultipleOf.ts
@@ -10,23 +10,31 @@ describe.concurrent("multipleOf", () => {
   })
 
   it("Guard", () => {
-    const schema = pipe(S.number, _.multipleOf(2))
+    const schema = pipe(S.number, _.multipleOf(-.2))
     const is = P.is(schema)
+    expect(is(-2.8)).toEqual(true)
     expect(is(-2)).toEqual(true)
-    expect(is(-1)).toEqual(false)
+    expect(is(-1.5)).toEqual(false)
     expect(is(0)).toEqual(true)
-    expect(is(1)).toEqual(false)
-    expect(is(2)).toEqual(true)
+    expect(is(1)).toEqual(true)
+    expect(is(2.6)).toEqual(true)
+    expect(is(3.1)).toEqual(false)
   })
 
   it("Decoder", () => {
-    const schema = _.multipleOf(.2)(S.number)
-    Util.expectDecodingSuccess(schema, 2)
-    Util.expectDecodingSuccess(schema, -.4)
+    const schema = _.multipleOf(2)(S.number)
+    Util.expectDecodingSuccess(schema, -4)
     Util.expectDecodingFailure(
       schema,
-      3.1,
-      `Expected a number divisible by 0.2, actual 3.1`
+      -3,
+      `Expected a number divisible by 2, actual -3`
+    )
+    Util.expectDecodingSuccess(schema, 0)
+    Util.expectDecodingSuccess(schema, 2)
+    Util.expectDecodingFailure(
+      schema,
+      2.5,
+      `Expected a number divisible by 2, actual 2.5`
     )
     Util.expectDecodingFailure(
       schema,
