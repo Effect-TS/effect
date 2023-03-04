2438092ce40fe2910998f28283b7867a33def3cf
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-03-04 08:21:16 -0500

    update to latest effect

---
+++ packages/cli/src/Exists.ts
@@ -0,0 +1,11 @@
+/**
+ * @since 1.0.0
+ */
+
+/**
+ * Describes whether the command-line application wants a file/directory to
+ * exist or not exist.
+ *
+ * @since 1.0.0
+ */
+export type Exists = "yes" | "no" | "either"
