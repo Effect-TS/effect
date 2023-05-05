92dfb8c046c5b229f3ff24d80b94ad1ccbb980ba
Giulio Canti <giulio.canti@gmail.com>
2023-05-05 18:44:45 +0200

    readme: fix annotation example (#270)

--- packages/schema/test/dev.ts
+++ packages/schema/test/dev.ts
@@ -1,11 +1,13 @@
 import { pipe } from "@effect/data/Function"
 import * as S from "@effect/schema/Schema"
-import * as TA from "@effect/schema/test/Arbitrary"
 
 describe.concurrent("dev", () => {
   it.skip("dev", async () => {
-    const schema = pipe(S.number, S.between(1, 10), S.int()) // hangs indefinitely
-    // const schema = pipe(S.number, S.int(), S.between(1, 10)) // hangs indefinitely
-    TA.propertyTo(schema)
+    const schema = pipe(
+      S.string,
+      S.nonEmpty(),
+      S.message(() => "bla")
+    )
+    console.log(S.parse(schema)(""))
   })
 })
