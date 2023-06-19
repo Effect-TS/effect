61e38ab2db76a062fee0bdc480332a2642d1b072
Tim <hello@timsmart.co>
2023-06-20 10:30:08 +1200

    add Console accessors (#12)

--- packages/platform-node/src/Console.ts
+++ packages/platform-node/src/Console.ts
@@ -1,16 +1,4 @@
 /**
  * @since 1.0.0
  */
-
-export {
-  /**
-   * @since 1.0.0
-   * @category tag
-   */
-  Console,
-  /**
-   * @since 1.0.0
-   * @category layer
-   */
-  layer
-} from "@effect/platform/Console"
+export * from "@effect/platform/Console"
--- packages/platform/src/internal/console.ts
+++ packages/platform/src/internal/console.ts
@@ -117,3 +117,67 @@ const consoleImpl = Console.of({
 
 /** @internal */
 export const layer = Layer.succeed(Console, consoleImpl)
+
+/** @internal */
+export const assert = (condition: boolean, ...args: ReadonlyArray<any>) =>
+  Effect.flatMap(Console, (_) => _.assert(condition, ...args))
+
+/** @internal */
+export const clear = () => Effect.flatMap(Console, (_) => _.clear())
+
+/** @internal */
+export const count = (label?: string) => Effect.flatMap(Console, (_) => _.count(label))
+
+/** @internal */
+export const countReset = (label?: string) => Effect.flatMap(Console, (_) => _.countReset(label))
+
+/** @internal */
+export const debug = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.debug(...args))
+
+/** @internal */
+export const dir = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.dir(...args))
+
+/** @internal */
+export const dirxml = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.dirxml(...args))
+
+/** @internal */
+export const error = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.error(...args))
+
+/** @internal */
+export const group = (options?: { label?: string; collapsed?: boolean }) =>
+  Effect.flatMap(Console, (_) => _.group(options))
+
+/** @internal */
+export const info = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.info(...args))
+
+/** @internal */
+export const log = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.log(...args))
+
+/** @internal */
+export const table = (tabularData: any, properties?: ReadonlyArray<string>) =>
+  Effect.flatMap(Console, (_) => _.table(tabularData, properties))
+
+/** @internal */
+export const time = (label?: string) => Effect.flatMap(Console, (_) => _.time(label))
+
+/** @internal */
+export const timeLog = (label?: string, ...args: ReadonlyArray<any>) =>
+  Effect.flatMap(Console, (_) => _.timeLog(label, ...args))
+
+/** @internal */
+export const trace = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.trace(...args))
+
+/** @internal */
+export const warn = (...args: ReadonlyArray<any>) => Effect.flatMap(Console, (_) => _.warn(...args))
+
+/** @internal */
+export const withGroup = (options?: { label?: string; collapsed?: boolean }) =>
+  <R, E, A>(
+    self: Effect.Effect<R, E, A>
+  ) => Effect.flatMap(Console, (_) => _.withGroup(options)(self))
+
+/** @internal */
+export const withTime = (label?: string) =>
+  <R, E, A>(
+    self: Effect.Effect<R, E, A>
+  ) => Effect.flatMap(Console, (_) => _.withTime(label)(self))
