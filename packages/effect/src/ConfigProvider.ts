17cf161b82cba41174e9a3b25a23ff7ef2b9bf8f
Giulio Canti <giulio.canti@gmail.com>
2023-07-13 19:40:42 +0200

    add Config/* modules (#1376)

---
+++ packages/effect/src/ConfigProvider.ts
@@ -0,0 +1,10 @@
+/**
+ * @since 2.0.0
+ *
+ * ```md
+ * - Docs: https://effect-ts.github.io/io/modules/Config/Provider.ts.html
+ * - Module: "@effect/io/Config/Provider"
+ * ```
+ */
+
+export * from "@effect/io/Config/Provider"
