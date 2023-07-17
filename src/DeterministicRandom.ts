2e9b54d0393c3f3c7e63ba2d6507d36074be0b51
Tim <hello@timsmart.co>
2023-07-17 12:18:47 +1200

    update /io and /data (#1379)

--- src/DeterministicRandom.ts
+++ src/DeterministicRandom.ts
@@ -2,9 +2,9 @@
  * @since 2.0.0
  *
  * ```md
- * - Docs: https://effect-ts.github.io/data/modules/Random.ts.html
- * - Module: "@effect/data/Random"
+ * - Docs: https://effect-ts.github.io/data/modules/DeterministicRandom.ts.html
+ * - Module: "@effect/data/DeterministicRandom"
  * ```
  */
 
-export * from "@effect/data/Random"
+export * from "@effect/data/DeterministicRandom"
