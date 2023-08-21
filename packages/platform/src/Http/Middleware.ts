c75c953c9ad576d5c44a192a6d53065c7cccf7c0
Tim <hello@timsmart.co>
2023-08-22 09:52:08 +1200

    remove Middleware.compose (#88)

--- packages/platform/src/Http/Middleware.ts
+++ packages/platform/src/Http/Middleware.ts
@@ -32,20 +32,6 @@ export const make: <M extends Middleware>(middleware: M) => M = internal.make
 
 /**
  * @since 1.0.0
- * @category combinators
- */
-export const compose: {
-  <B extends App.Default<any, any>, C extends App.Default<any, any>>(
-    that: (b: B) => C
-  ): <A extends App.Default<any, any>>(self: (a: A) => B) => (a: A) => C
-  <A extends App.Default<any, any>, B extends App.Default<any, any>, C extends App.Default<any, any>>(
-    self: (a: A) => B,
-    that: (b: B) => C
-  ): (a: A) => C
-} = internal.compose
-
-/**
- * @since 1.0.0
  * @category constructors
  */
 export const logger: <R, E>(httpApp: App.Default<R, E>) => App.Default<R, E> = internal.logger
--- packages/platform/src/internal/http/middleware.ts
+++ packages/platform/src/internal/http/middleware.ts
@@ -1,6 +1,5 @@
-import { dual } from "@effect/data/Function"
+import { flow } from "@effect/data/Function"
 import * as Effect from "@effect/io/Effect"
-import type * as App from "@effect/platform/Http/App"
 import * as Headers from "@effect/platform/Http/Headers"
 import type * as Middleware from "@effect/platform/Http/Middleware"
 import * as ServerRequest from "@effect/platform/Http/ServerRequest"
@@ -62,17 +61,4 @@ export const xForwardedHeaders = make((httpApp) =>
 )
 
 /** @internal */
-export const compose = dual<
-  <B extends App.Default<any, any>, C extends App.Default<any, any>>(
-    that: (b: B) => C
-  ) => <A extends App.Default<any, any>>(
-    self: (a: A) => B
-  ) => (a: A) => C,
-  <A extends App.Default<any, any>, B extends App.Default<any, any>, C extends App.Default<any, any>>(
-    self: (a: A) => B,
-    that: (b: B) => C
-  ) => (a: A) => C
->(2, (self, that) => (inApp) => that(self(inApp)))
-
-/** @internal */
-export const loggerTracer = compose(tracer, logger)
+export const loggerTracer = flow(tracer, logger)
