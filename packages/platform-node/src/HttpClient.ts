3875b4be7100e7a32471d266be30c3f3b0190e8f
Tim <hello@timsmart.co>
2023-08-25 15:08:57 +1200

    fix HttpClient doc links (#104)

--- packages/platform-node/src/HttpClient.ts
+++ packages/platform-node/src/HttpClient.ts
@@ -14,56 +14,56 @@ export {
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/Body](https://effect-ts.github.io/platform/platform/Http/Body.html)
+   * - Docs: [Http/Body](https://effect-ts.github.io/platform/platform/Http/Body.ts.html)
    * - Module: `@effect/platform/Http/Body`
    */
   body,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/Client](https://effect-ts.github.io/platform/platform/Http/Client.html)
+   * - Docs: [Http/Client](https://effect-ts.github.io/platform/platform/Http/Client.ts.html)
    * - Module: `@effect/platform/Http/Client`
    */
   client,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/ClientError](https://effect-ts.github.io/platform/platform/Http/ClientError.html)
+   * - Docs: [Http/ClientError](https://effect-ts.github.io/platform/platform/Http/ClientError.ts.html)
    * - Module: `@effect/platform/Http/ClientError`
    */
   error,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/Headers](https://effect-ts.github.io/platform/platform/Http/Headers.html)
+   * - Docs: [Http/Headers](https://effect-ts.github.io/platform/platform/Http/Headers.ts.html)
    * - Module: `@effect/platform/Http/Headers`
    */
   headers,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/NodeClient](https://effect-ts.github.io/platform/platform-node/Http/NodeClient.html)
+   * - Docs: [Http/NodeClient](https://effect-ts.github.io/platform/platform-node/Http/NodeClient.ts.html)
    * - Module: `@effect/platform-node/Http/NodeClient`
    */
   nodeClient,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/ClientRequest](https://effect-ts.github.io/platform/platform/Http/ClientRequest.html)
+   * - Docs: [Http/ClientRequest](https://effect-ts.github.io/platform/platform/Http/ClientRequest.ts.html)
    * - Module: `@effect/platform/Http/ClientRequest`
    */
   request,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/ClientResponse](https://effect-ts.github.io/platform/platform/Http/ClientResponse.html)
+   * - Docs: [Http/ClientResponse](https://effect-ts.github.io/platform/platform/Http/ClientResponse.ts.html)
    * - Module: `@effect/platform/Http/ClientResponse`
    */
   response,
   /**
    * @since 1.0.0
    *
-   * - Docs: [Http/UrlParams](https://effect-ts.github.io/platform/platform/Http/UrlParams.html)
+   * - Docs: [Http/UrlParams](https://effect-ts.github.io/platform/platform/Http/UrlParams.ts.html)
    * - Module: `@effect/platform/Http/UrlParams`
    */
   urlParams
