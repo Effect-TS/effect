42dc378bca29914a55465cc8fbb601c4f90ccfcd
Tim <hello@timsmart.co>
2023-08-29 15:17:57 +1200

    add SchemaStore and KeyValueStore.prefix (#120)

--- packages/platform/src/internal/keyValueStore.ts
+++ packages/platform/src/internal/keyValueStore.ts
@@ -1,11 +1,12 @@
 import * as Context from "@effect/data/Context"
-import { pipe } from "@effect/data/Function"
+import { dual, pipe } from "@effect/data/Function"
 import * as Option from "@effect/data/Option"
 import * as Effect from "@effect/io/Effect"
 import * as Layer from "@effect/io/Layer"
 import * as FileSystem from "@effect/platform/FileSystem"
 import type * as KeyValueStore from "@effect/platform/KeyValueStore"
 import * as Path from "@effect/platform/Path"
+import * as Schema from "@effect/schema/Schema"
 
 /** @internal */
 export const TypeId: KeyValueStore.TypeId = Symbol.for(
@@ -13,15 +14,15 @@ export const TypeId: KeyValueStore.TypeId = Symbol.for(
 ) as KeyValueStore.TypeId
 
 /** @internal */
-export const tag = Context.Tag<KeyValueStore.KeyValueStore>(TypeId)
+export const keyValueStoreTag = Context.Tag<KeyValueStore.KeyValueStore>(TypeId)
 
 /** @internal */
 export const make: (
   impl:
-    & Omit<KeyValueStore.KeyValueStore, KeyValueStore.TypeId | "has" | "modify" | "isEmpty">
+    & Omit<KeyValueStore.KeyValueStore, KeyValueStore.TypeId | "has" | "modify" | "isEmpty" | "forSchema">
     & Partial<KeyValueStore.KeyValueStore>
 ) => KeyValueStore.KeyValueStore = (impl) =>
-  tag.of({
+  keyValueStoreTag.of({
     [TypeId]: TypeId,
     has: (key) => Effect.map(impl.get(key), Option.isSome),
     isEmpty: Effect.map(impl.size, (size) => size === 0),
@@ -39,11 +40,83 @@ export const make: (
           )
         }
       ),
+    forSchema(schema) {
+      return makeSchemaStore(this, schema)
+    },
     ...impl
   })
 
 /** @internal */
-export const layerMemory = Layer.sync(tag, () => {
+export const prefix = dual<
+  (prefix: string) => <S extends KeyValueStore.KeyValueStore.AnyStore>(self: S) => S,
+  <S extends KeyValueStore.KeyValueStore.AnyStore>(self: S, prefix: string) => S
+>(
+  2,
+  ((self: KeyValueStore.KeyValueStore, prefix: string): KeyValueStore.KeyValueStore => ({
+    ...self,
+    get: (key) => self.get(`${prefix}${key}`),
+    set: (key, value) => self.set(`${prefix}${key}`, value),
+    remove: (key) => self.remove(`${prefix}${key}`),
+    has: (key) => self.has(`${prefix}${key}`),
+    modify: (key, f) => self.modify(`${prefix}${key}`, f)
+  })) as any
+)
+
+/** @internal */
+export const SchemaStoreTypeId: KeyValueStore.SchemaStoreTypeId = Symbol.for(
+  "@effect/platform/KeyValueStore/SchemaStore"
+) as KeyValueStore.SchemaStoreTypeId
+
+/** @internal */
+const makeSchemaStore = <I, A>(
+  store: KeyValueStore.KeyValueStore,
+  schema: Schema.Schema<I, A>
+): KeyValueStore.SchemaStore<A> => {
+  const jsonSchema = Schema.compose(Schema.ParseJson, schema)
+  const parse = Schema.parse(jsonSchema)
+  const encode = Schema.encode(jsonSchema)
+
+  const get = (key: string) =>
+    Effect.flatMap(
+      store.get(key),
+      Option.match({
+        onNone: () => Effect.succeedNone,
+        onSome: (value) => Effect.asSome(parse(value))
+      })
+    )
+
+  const set = (key: string, value: A) => Effect.flatMap(encode(value), (json) => store.set(key, json))
+
+  const modify = (key: string, f: (value: A) => A) =>
+    Effect.flatMap(
+      get(key),
+      (o) => {
+        if (Option.isNone(o)) {
+          return Effect.succeedNone
+        }
+        const newValue = f(o.value)
+        return Effect.as(
+          set(key, newValue),
+          Option.some(newValue)
+        )
+      }
+    )
+
+  return {
+    [SchemaStoreTypeId]: SchemaStoreTypeId,
+    get,
+    set,
+    modify,
+    remove: store.remove,
+    clear: store.clear,
+    size: store.size,
+    has: store.has,
+    isEmpty: store.isEmpty
+  }
+}
+
+/** @internal */
+export const layerMemory = Layer.sync(keyValueStoreTag, () => {
   const store = new Map<string, string>()
 
   return make({
@@ -58,7 +131,7 @@ export const layerMemory = Layer.sync(tag, () => {
 /** @internal */
 export const layerFileSystem = (directory: string) =>
   Layer.effect(
-    tag,
+    keyValueStoreTag,
     Effect.gen(function*(_) {
       const fs = yield* _(FileSystem.FileSystem)
       const path = yield* _(Path.Path)
@@ -91,3 +164,13 @@ export const layerFileSystem = (directory: string) =>
       })
     })
   )
+
+/** @internal */
+export const layerSchema = <I, A>(
+  schema: Schema.Schema<I, A>,
+  tagIdentifier?: unknown
+) => {
+  const tag = Context.Tag<KeyValueStore.SchemaStore<A>>(tagIdentifier)
+  const layer = Layer.effect(tag, Effect.map(keyValueStoreTag, (store) => store.forSchema(schema)))
+  return { tag, layer } as const
+}
--- packages/platform/test/KeyValueStore.test.ts
+++ packages/platform/test/KeyValueStore.test.ts
@@ -1,7 +1,9 @@
+import { identity } from "@effect/data/Function"
 import * as Option from "@effect/data/Option"
 import * as Effect from "@effect/io/Effect"
-import type * as Layer from "@effect/io/Layer"
+import * as Layer from "@effect/io/Layer"
 import * as KeyValueStore from "@effect/platform/KeyValueStore"
+import * as Schema from "@effect/schema/Schema"
 import { afterEach } from "vitest"
 
 export const testLayer = <E>(layer: Layer.Layer<never, E, KeyValueStore.KeyValueStore>) => {
@@ -87,3 +89,55 @@ export const testLayer = <E>(layer: Layer.Layer<never, E, KeyValueStore.KeyValue
 }
 
 describe("KeyValueStore / layerMemory", () => testLayer(KeyValueStore.layerMemory))
+
+describe("KeyValueStore / prefix", () => {
+  it("prefixes the keys", () =>
+    Effect.gen(function*(_) {
+      const store = yield* _(KeyValueStore.KeyValueStore)
+      const prefixed = KeyValueStore.prefix(store, "prefix/")
+
+      yield* _(prefixed.set("foo", "bar"))
+      yield* _(prefixed.modify("foo", (v) => v + "bar"))
+
+      expect(yield* _(prefixed.get("foo"))).toEqual(Option.some("barbar"))
+      expect(yield* _(prefixed.has("foo"))).toEqual(true)
+
+      expect(yield* _(store.get("prefix/foo"))).toEqual(Option.some("barbar"))
+      expect(yield* _(store.has("prefix/foo"))).toEqual(true)
+    }).pipe(
+      Effect.provideLayer(KeyValueStore.layerMemory),
+      Effect.runPromise
+    ))
+})
+
+class User extends Schema.Class({
+  name: Schema.string,
+  age: Schema.number
+}) {}
+const UserStore = KeyValueStore.layerSchema(User.schema())
+const runUserStore = <E, A>(effect: Effect.Effect<KeyValueStore.SchemaStore<User>, E, A>) =>
+  Effect.runPromise(Effect.provideLayer(effect, UserStore.layer.pipe(Layer.use(KeyValueStore.layerMemory))))
+
+describe("KeyValueStore / SchemaStore", () => {
+  it("encodes & decodes", () =>
+    runUserStore(Effect.gen(function*(_) {
+      const store = yield* _(UserStore.tag)
+      yield* _(store.set("foo", new User({ name: "foo", age: 42 })))
+      yield* _(store.modify("foo", (user) => new User({ ...user, age: 43 })))
+      const value = yield* _(store.get("foo"), Effect.flatMap(identity))
+
+      expect(value.name).toEqual("foo")
+      expect(value.age).toEqual(43)
+    })))
+
+  it("prefix", () =>
+    runUserStore(Effect.gen(function*(_) {
+      const store = yield* _(UserStore.tag)
+      const prefixed = KeyValueStore.prefix(store, "prefix/")
+      yield* _(prefixed.set("foo", new User({ name: "foo", age: 42 })))
+      const value = yield* _(store.get("prefix/foo"), Effect.flatMap(identity))
+
+      expect(value.name).toEqual("foo")
+      expect(value.age).toEqual(42)
+    })))
+})
