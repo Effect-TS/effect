9ca5488c33b997179dd2de36cf77703690ed903a
Tim <hello@timsmart.co>
2023-09-26 14:45:31 +1300

    update dependencies (#416)

--- packages/printer-ansi/src/index.ts
+++ packages/printer-ansi/src/index.ts
@@ -2,9 +2,27 @@
  * @since 1.0.0
  */
 
+/**
+ * @since 1.0.0
+ */
 export * as ansiDoc from "@effect/printer-ansi/AnsiDoc"
+/**
+ * @since 1.0.0
+ */
 export * as ansiRender from "@effect/printer-ansi/AnsiRender"
+/**
+ * @since 1.0.0
+ */
 export * as ansiStyle from "@effect/printer-ansi/AnsiStyle"
+/**
+ * @since 1.0.0
+ */
 export * as color from "@effect/printer-ansi/Color"
+/**
+ * @since 1.0.0
+ */
 export * as renderLayer from "@effect/printer-ansi/RenderLayer"
+/**
+ * @since 1.0.0
+ */
 export * as sgr from "@effect/printer-ansi/SGR"
--- packages/printer/src/index.ts
+++ packages/printer/src/index.ts
@@ -2,11 +2,35 @@
  * @since 1.0.0
  */
 
+/**
+ * @since 1.0.0
+ */
 export * as doc from "@effect/printer/Doc"
+/**
+ * @since 1.0.0
+ */
 export * as docStream from "@effect/printer/DocStream"
+/**
+ * @since 1.0.0
+ */
 export * as docTree from "@effect/printer/DocTree"
+/**
+ * @since 1.0.0
+ */
 export * as flatten from "@effect/printer/Flatten"
+/**
+ * @since 1.0.0
+ */
 export * as layout from "@effect/printer/Layout"
+/**
+ * @since 1.0.0
+ */
 export * as optimize from "@effect/printer/Optimize"
+/**
+ * @since 1.0.0
+ */
 export * as pageWidth from "@effect/printer/PageWidth"
+/**
+ * @since 1.0.0
+ */
 export * as render from "@effect/printer/Render"
