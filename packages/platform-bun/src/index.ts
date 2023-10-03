7875985bcc68f2fdc543631cee0815a5548a3643
Tim <hello@timsmart.co>
2023-10-04 11:42:40 +1300

    switch to build-utils prepare-v1 (#183)

--- packages/platform-bun/src/index.ts
+++ packages/platform-bun/src/index.ts
@@ -1,66 +1,66 @@
 /**
  * @since 1.0.0
  */
-export * as BunContext from "./BunContext"
+export * as BunContext from "@effect/platform-bun/BunContext"
 
 /**
  * @since 1.0.0
  */
-export * as Command from "./Command"
+export * as Command from "@effect/platform-bun/Command"
 
 /**
  * @since 1.0.0
  */
-export * as CommandExecutor from "./CommandExecutor"
+export * as CommandExecutor from "@effect/platform-bun/CommandExecutor"
 
 /**
  * @since 1.0.0
  */
-export * as Effectify from "./Effectify"
+export * as Effectify from "@effect/platform-bun/Effectify"
 
 /**
  * @since 1.0.0
  */
-export * as Error from "./Error"
+export * as Error from "@effect/platform-bun/Error"
 
 /**
  * @since 1.0.0
  */
-export * as FileSystem from "./FileSystem"
+export * as FileSystem from "@effect/platform-bun/FileSystem"
 
 /**
  * @since 1.0.0
  */
-export * as HttpClient from "./HttpClient"
+export * as HttpClient from "@effect/platform-bun/HttpClient"
 
 /**
  * @since 1.0.0
  */
-export * as HttpServer from "./HttpServer"
+export * as HttpServer from "@effect/platform-bun/HttpServer"
 
 /**
  * @since 1.0.0
  *
  * Also includes exports from [`@effect/platform/KeyValueStore`](https://effect-ts.github.io/platform/platform/KeyValueStore.ts.html).
  */
-export * as KeyValueStore from "./KeyValueStore"
+export * as KeyValueStore from "@effect/platform-bun/KeyValueStore"
 
 /**
  * @since 1.0.0
  */
-export * as Path from "./Path"
+export * as Path from "@effect/platform-bun/Path"
 
 /**
  * @since 1.0.0
  */
-export * as Runtime from "./Runtime"
+export * as Runtime from "@effect/platform-bun/Runtime"
 
 /**
  * @since 1.0.0
  */
-export * as Sink from "./Sink"
+export * as Sink from "@effect/platform-bun/Sink"
 
 /**
  * @since 1.0.0
  */
-export * as Stream from "./Stream"
+export * as Stream from "@effect/platform-bun/Stream"
--- packages/platform/src/index.ts
+++ packages/platform/src/index.ts
@@ -1,49 +1,49 @@
 /**
  * @since 1.0.0
  */
-export * as Command from "./Command"
+export * as Command from "@effect/platform/Command"
 
 /**
  * @since 1.0.0
  */
-export * as CommandExecutor from "./CommandExecutor"
+export * as CommandExecutor from "@effect/platform/CommandExecutor"
 
 /**
  * @since 1.0.0
  */
-export * as Effectify from "./Effectify"
+export * as Effectify from "@effect/platform/Effectify"
 
 /**
  * @since 1.0.0
  */
-export * as Error from "./Error"
+export * as Error from "@effect/platform/Error"
 
 /**
  * @since 1.0.0
  */
-export * as FileSystem from "./FileSystem"
+export * as FileSystem from "@effect/platform/FileSystem"
 
 /**
  * @since 1.0.0
  */
-export * as HttpClient from "./HttpClient"
+export * as HttpClient from "@effect/platform/HttpClient"
 
 /**
  * @since 1.0.0
  */
-export * as HttpServer from "./HttpServer"
+export * as HttpServer from "@effect/platform/HttpServer"
 
 /**
  * @since 1.0.0
  */
-export * as KeyValueStore from "./KeyValueStore"
+export * as KeyValueStore from "@effect/platform/KeyValueStore"
 
 /**
  * @since 1.0.0
  */
-export * as Path from "./Path"
+export * as Path from "@effect/platform/Path"
 
 /**
  * @since 1.0.0
  */
-export * as Runtime from "./Runtime"
+export * as Runtime from "@effect/platform/Runtime"
