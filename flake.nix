b2a88ecbc4b92c5cedad890b7165baf93cb65b8f
Sebastian Lorenz <fubhy@fubhy.com>
2023-10-03 08:50:56 +0200

    auto install pnpm version from package.json (#1441)

--- flake.nix
+++ flake.nix
@@ -17,6 +17,10 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      corepackEnable = pkgs.runCommand "corepack-enable" {} ''
+        mkdir -p $out/bin
+        ${pkgs.nodejs-18_x}/bin/corepack enable --install-directory $out/bin
+      '';
     in {
       formatter = pkgs.alejandra;
 
@@ -24,7 +28,7 @@
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs-18_x
-            nodePackages.pnpm
+            corepackEnable
           ];
         };
       };
