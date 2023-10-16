99a8daa6438dc361aab8cd5ac885aa4d48a122a4
Andrew Jarrett <ahrjarrett@gmail.com>
2023-10-15 22:16:05 -0500

    incorporate improvements suggested by @gcanti

--- src/Data.ts
+++ src/Data.ts
@@ -165,19 +165,19 @@ export const Structural: new<A>(
  * @category models
  */
 export type TaggedEnum<
-  A extends Record<string, Record<string, any>> & _,
-  _ extends ChildHasTagCheck<A> = ChildHasTagCheck<A>
+  A extends Record<string, Record<string, any>> & UntaggedChildren<A>
 > = keyof A extends infer Tag
   ? Tag extends keyof A ? Data<{ readonly [K in `_tag` | keyof A[Tag]]: K extends `_tag` ? Tag : A[Tag][K] }>
   : never
   : never
 
-type ChildHasTag<A> = `_tag` extends ChildKeys<A> ? true : false
+type ChildrenAreTagged<A> = keyof A extends infer K ? K extends keyof A ? "_tag" extends keyof A[K] ? true
+    : false
+  : never
+  : never
 
-type ChildKeys<A> = A[keyof A] extends infer M ? M extends M ? keyof M : never : never
-
-type ChildHasTagCheck<_> = [ChildHasTag<_>] extends [true]
-  ? `It looks like you're trying to create a tagged enum, but one or more of its members already has a \`_tag\` property.`
+type UntaggedChildren<A> = true extends ChildrenAreTagged<A>
+  ? "It looks like you're trying to create a tagged enum, but one or more of its members already has a `_tag` property."
   : unknown
 
 /**
