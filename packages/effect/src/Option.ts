8a5f82dfaa9eb78b7bd79fe1d2878cd288b08e15
Sebastian Lorenz <fubhy@fubhy.com>
2023-10-17 22:54:51 +0200

    Safe division (#1530)

--- packages/effect/src/Option.ts
+++ packages/effect/src/Option.ts
@@ -1155,7 +1155,7 @@ export const subtract: {
 export const divide: {
   (self: Option<number>, that: Option<number>): Option<number>
   (that: Option<number>): (self: Option<number>) => Option<number>
-} = lift2(N.divide)
+} = dual(2, (self: Option<number>, that: Option<number>): Option<number> => zipWith(self, that, N.divide).pipe(flatten))
 
 /**
  * Sum all numbers in an iterable of `Option<number>` ignoring the `None` values.
