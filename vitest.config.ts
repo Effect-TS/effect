4bed977406204ba2b0a4f07805ec6a88570077d2
Sebastian Lorenz <fubhy@fubhy.com>
2023-11-01 14:57:50 +0100

    simplify

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,8 @@
 /// <reference types="vitest" />
-import babel from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const babelConfig = require("./babel.config.json")
-
 export default defineConfig({
-  plugins: [babel({ babel: babelConfig })],
   test: {
     include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     exclude: ["./test/util.ts", "./test/utils/**/*.ts", "./test/**/*.init.ts"],
