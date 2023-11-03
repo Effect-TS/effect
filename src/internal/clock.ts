Effect-TS/effect

commit 57d8f17924e91e10617753382a91a3136043b421

Predicate: add hasProperty (+ internal refactoring to leverage it) (#1592)

src/internal/clock.ts
@@ -4,6 +4,7 @@ import * as Duration from "../Duration"
 import type * as Effect from "../Effect"
 import * as Either from "../Either"
 import { constFalse } from "../Function"
+import { hasProperty, isFunction } from "../Predicate"
 import * as core from "./core"
 import * as timeout from "./timeout"
 
@@ -54,10 +55,9 @@ const performanceNowNanos = (function() {
   return () => origin + BigInt(Math.round(performance.now() * 1_000_000))
 })()
 const processOrPerformanceNow = (function() {
-  const processHrtime =
-    typeof process === "object" && "hrtime" in process && typeof process.hrtime.bigint === "function" ?
-      process.hrtime :
-      undefined
+  const processHrtime = hasProperty(process, "hrtime") && isFunction(process.hrtime.bigint) ?
+    process.hrtime :
+    undefined
   if (!processHrtime) {
     return performanceNowNanos
   }

commit 11e49e50b98fec77766c49475863ca107cb5e3f6

fix regression in process.hrtime detection
introduced by #1592

src/internal/clock.ts
@@ -4,7 +4,6 @@ import * as Duration from "../Duration"
 import type * as Effect from "../Effect"
 import * as Either from "../Either"
 import { constFalse } from "../Function"
-import { hasProperty, isFunction } from "../Predicate"
 import * as core from "./core"
 import * as timeout from "./timeout"
 
@@ -55,9 +54,10 @@ const performanceNowNanos = (function() {
   return () => origin + BigInt(Math.round(performance.now() * 1_000_000))
 })()
 const processOrPerformanceNow = (function() {
-  const processHrtime = hasProperty(process, "hrtime") && isFunction(process.hrtime.bigint) ?
-    process.hrtime :
-    undefined
+  const processHrtime =
+    typeof process === "object" && "hrtime" in process && typeof process.hrtime.bigint === "function" ?
+      process.hrtime :
+      undefined
   if (!processHrtime) {
     return performanceNowNanos
   }