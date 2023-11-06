cbe009fa2fd43fb16c4ffda668bb70233b95575a
Patrick Roza <contact@patrickroza.com>
2023-11-06 13:10:08 +0100

    step1

--- src/Stream.ts
+++ src/Stream.ts
@@ -43,30 +43,65 @@ export const StreamTypeId: unique symbol = internal.StreamTypeId
  */
 export type StreamTypeId = typeof StreamTypeId
 
-/**
- * A `Stream<R, E, A>` is a description of a program that, when evaluated, may
- * emit zero or more values of type `A`, may fail with errors of type `E`, and
- * uses an context of type `R`. One way to think of `Stream` is as a
- * `Effect` program that could emit multiple values.
- *
- * `Stream` is a purely functional *pull* based stream. Pull based streams offer
- * inherent laziness and backpressure, relieving users of the need to manage
- * buffers between operators. As an optimization, `Stream` does not emit
- * single values, but rather an array of values. This allows the cost of effect
- * evaluation to be amortized.
- *
- * `Stream` forms a monad on its `A` type parameter, and has error management
- * facilities for its `E` type parameter, modeled similarly to `Effect` (with
- * some adjustments for the multiple-valued nature of `Stream`). These aspects
- * allow for rich and expressive composition of streams.
- *
- * @since 2.0.0
- * @category models
- */
-export interface Stream<R, E, A> extends Stream.Variance<R, E, A>, Pipeable {
-  [Unify.typeSymbol]?: unknown
-  [Unify.unifySymbol]?: StreamUnify<this>
-  [Unify.ignoreSymbol]?: StreamUnifyIgnore
+export * as Stream from "./Stream.js"
+
+declare module "./Stream.js" {
+  /**
+   * A `Stream<R, E, A>` is a description of a program that, when evaluated, may
+   * emit zero or more values of type `A`, may fail with errors of type `E`, and
+   * uses an context of type `R`. One way to think of `Stream` is as a
+   * `Effect` program that could emit multiple values.
+   *
+   * `Stream` is a purely functional *pull* based stream. Pull based streams offer
+   * inherent laziness and backpressure, relieving users of the need to manage
+   * buffers between operators. As an optimization, `Stream` does not emit
+   * single values, but rather an array of values. This allows the cost of effect
+   * evaluation to be amortized.
+   *
+   * `Stream` forms a monad on its `A` type parameter, and has error management
+   * facilities for its `E` type parameter, modeled similarly to `Effect` (with
+   * some adjustments for the multiple-valued nature of `Stream`). These aspects
+   * allow for rich and expressive composition of streams.
+   *
+   * @since 2.0.0
+   * @category models
+   */
+  export interface Stream<R, E, A> extends Stream.Variance<R, E, A>, Pipeable {
+    [Unify.typeSymbol]?: unknown
+    [Unify.unifySymbol]?: StreamUnify<this>
+    [Unify.ignoreSymbol]?: StreamUnifyIgnore
+  }
+
+  /**
+   * @since 2.0.0
+   */
+  export namespace Stream {
+    /**
+     * @since 2.0.0
+     * @category models
+     */
+    export interface Variance<R, E, A> {
+      readonly [StreamTypeId]: {
+        _R: (_: never) => R
+        _E: (_: never) => E
+        _A: (_: never) => A
+      }
+    }
+
+    /**
+     * @since 2.0.0
+     * @category models
+     */
+    export type DynamicTuple<T, N extends number> = N extends N ? number extends N ? Array<T> : DynamicTupleOf<T, N, []>
+      : never
+
+    /**
+     * @since 2.0.0
+     * @category models
+     */
+    export type DynamicTupleOf<T, N extends number, R extends Array<unknown>> = R["length"] extends N ? R
+      : DynamicTupleOf<T, N, [T, ...R]>
+  }
 }
 
 /**
@@ -102,37 +137,6 @@ declare module "./Effect.js" {
  */
 export interface StreamTypeLambda extends TypeLambda {
   readonly type: Stream<this["Out2"], this["Out1"], this["Target"]>
-}
-
-/**
- * @since 2.0.0
- */
-export declare namespace Stream {
-  /**
-   * @since 2.0.0
-   * @category models
-   */
-  export interface Variance<R, E, A> {
-    readonly [StreamTypeId]: {
-      _R: (_: never) => R
-      _E: (_: never) => E
-      _A: (_: never) => A
-    }
-  }
-
-  /**
-   * @since 2.0.0
-   * @category models
-   */
-  export type DynamicTuple<T, N extends number> = N extends N ? number extends N ? Array<T> : DynamicTupleOf<T, N, []>
-    : never
-
-  /**
-   * @since 2.0.0
-   * @category models
-   */
-  export type DynamicTupleOf<T, N extends number, R extends Array<unknown>> = R["length"] extends N ? R
-    : DynamicTupleOf<T, N, [T, ...R]>
 }
 
 /**
