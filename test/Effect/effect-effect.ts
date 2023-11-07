d487ce444ffd57bb01021e69c9be8efb061ecf23
Patrick Roza <contact@patrickroza.com>
2023-11-07 06:47:18 +0100

    int approach

--- test/Effect/effect-effect.ts
+++ test/Effect/effect-effect.ts
@@ -1,5 +1,8 @@
 import * as it from "effect-test/utils/extend"
-import { Effect } from "effect/Effect"
+// import * as Effect from "effect/Effect"
+import { Effect, Schedule } from "effect"
+
+import { Option } from "effect/Option"
 
 export type TestType = Effect<never, never, number>
 
@@ -8,3 +11,14 @@ it.effect("works", () =>
     .pipe(
       Effect.flatMap((i) => Effect.sync(() => console.log("the answer is: " + i)))
     ))
+
+export const a: Effect<string, string, void> = Effect.succeed("hello")
+
+export const effect = Effect.succeed("world").pipe(Effect.runSync)
+
+export const ao = Option.none()
+export const bo = Option.some(1)
+export const co = bo.pipe((_) => Option.map((_) => 2)(_))
+
+export const as = Schedule.identity()
+export const bs = as.pipe((_) => Schedule.map((_) => 2)(_))
