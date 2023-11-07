aa0b21671202e29fe3d5e9d296848a8d53d651ce
Patrick Roza <contact@patrickroza.com>
2023-11-07 21:33:05 +0100

    Revert "solution, art thou it?"
    This reverts commit d3894020242a0479555ed50344384164cfb0ed31.

--- src/Effect.ts
+++ src/Effect.ts
@@ -11,6 +11,7 @@ import type { Context } from "./Context.js"
 import type { Deferred } from "./Deferred.js"
 import type { Duration } from "./Duration.js"
 import type { Either } from "./Either.js"
+import type { Equal } from "./Equal.js"
 import type { Equivalence } from "./Equivalence.js"
 import type { ExecutionStrategy } from "./ExecutionStrategy.js"
 import type { Exit } from "./Exit.js"
@@ -38,6 +39,7 @@ import type { LogLevel } from "./LogLevel.js"
 import type { Metric } from "./Metric.js"
 import type { MetricLabel } from "./MetricLabel.js"
 import type { Option } from "./Option.js"
+import type { Pipeable } from "./Pipeable.js"
 import type { Predicate, Refinement } from "./Predicate.js"
 import type { Random } from "./Random.js"
 import type { Ref } from "./Ref.js"
@@ -55,9 +57,78 @@ import type { Tracer } from "./Tracer.js"
 import type { Concurrency } from "./Types.js"
 import type { Unify } from "./Unify.js"
 
-import "./Effect.helper.js"
+// eslint-disable-next-line import/no-cycle
+export * as Effect from "./Effect.helper.js"
 
-export * as Effect from "./Effect.js"
+declare module "./Effect.js" {
+  /**
+   * The `Effect` interface defines a value that lazily describes a workflow or job.
+   * The workflow requires some context `R`, and may fail with an error of type `E`,
+   * or succeed with a value of type `A`.
+   *
+   * `Effect` values model resourceful interaction with the outside world, including
+   * synchronous, asynchronous, concurrent, and parallel interaction. They use a
+   * fiber-based concurrency model, with built-in support for scheduling, fine-grained
+   * interruption, structured concurrency, and high scalability.
+   *
+   * To run an `Effect` value, you need a `Runtime`, which is a type that is capable
+   * of executing `Effect` values.
+   *
+   * @since 2.0.0
+   * @category models
+   */
+  export interface Effect<R, E, A> extends Effect.Variance<R, E, A>, Equal, Pipeable {
+    readonly [Unify.typeSymbol]?: unknown
+    readonly [Unify.unifySymbol]?: EffectUnify<this>
+    readonly [Unify.ignoreSymbol]?: EffectUnifyIgnore
+  }
+
+  export namespace Effect {
+    /**
+     * @since 2.0.0
+     * @category models
+     */
+    export interface Variance<R, E, A> {
+      readonly [EffectTypeId]: VarianceStruct<R, E, A>
+    }
+    /**
+     * @since 2.0.0
+     * @category models
+     */
+    export interface VarianceStruct<R, E, A> {
+      readonly _V: string
+      readonly _R: (_: never) => R
+      readonly _E: (_: never) => E
+      readonly _A: (_: never) => A
+    }
+    /**
+     * @since 2.0.0
+     * @category models
+     */
+    export type Unify<Ret extends Effect<any, any, any>> = Effect<
+      Context<Ret>,
+      Error<Ret>,
+      Success<Ret>
+    >
+    /**
+     * @since 2.0.0
+     * @category type-level
+     */
+    export type Context<T extends Effect<any, any, any>> = [T] extends [Effect<infer _R, infer _E, infer _A>] ? _R
+      : never
+    /**
+     * @since 2.0.0
+     * @category type-level
+     */
+    export type Error<T extends Effect<any, any, any>> = [T] extends [Effect<infer _R, infer _E, infer _A>] ? _E : never
+    /**
+     * @since 2.0.0
+     * @category type-level
+     */
+    export type Success<T extends Effect<any, any, any>> = [T] extends [Effect<infer _R, infer _E, infer _A>] ? _A
+      : never
+  }
+}
 
 // -------------------------------------------------------------------------------------
 // models
