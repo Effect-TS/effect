2ea0a0ff126660ed15f916000f2ea60e1cb50ea1
Patrick Roza <contact@patrickroza.com>
2023-11-08 22:01:35 +0100

    insane

--- src/Option.ts
+++ src/Option.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-cycle
-import type { None, Some } from "./impl/Option.js"
+import type { None, OptionUnify, Some, TypeId } from "./impl/Option.js"
+import type * as bla from "./impl/Option.js"
 
 export * from "./impl/Option.js"
 export * from "./internal/Jumpers/Option.js"
@@ -11,7 +12,26 @@ export * from "./internal/Jumpers/Option.js"
 export type Option<A> = None<A> | Some<A>
 
 export declare namespace Option {
-  // eslint-disable-next-line import/no-cycle
-  // @ts-expect-error
-  export type * from "./impl/Option.js"
+  const isSome: typeof bla.isSome
+  const isNone: typeof bla.isNone
+  const some: typeof bla.some
+  const none: typeof bla.none
+  const getOrElse: typeof bla.getOrElse
+  const fromNullable: typeof bla.fromNullable
+  const match: typeof bla.match
+  const map: typeof bla.map
+  const filter: typeof bla.filter
+  const getOrUndefined: typeof bla.getOrUndefined
+  const getRight: typeof bla.getRight
+  const flatMap: typeof bla.flatMap
+  const orElse: typeof bla.orElse
+  const all: typeof bla.all
+  const contains: typeof bla.contains
+  const getOrThrow: typeof bla.getOrThrow
+  const getOrThrowWith: typeof bla.getOrThrowWith
+  // TODO: the rest
+}
+
+export declare namespace Option {
+  export type { None, OptionUnify, Some, TypeId }
 }
