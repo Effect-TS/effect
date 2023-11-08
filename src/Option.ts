8a4de5d8850f68323f6c0d3b9f50741fae5615d2
Patrick Roza <contact@patrickroza.com>
2023-11-08 22:01:35 +0100

    test fallback export support without error

--- src/Option.ts
+++ src/Option.ts
@@ -1,5 +1,25 @@
 // eslint-disable-next-line import/no-cycle
+// import type { None, OptionUnify, Some, TypeId } from "./impl/Option.js"
 import type { None, Some } from "./impl/Option.js"
+// import {
+//   all,
+//   contains,
+//   filter,
+//   flatMap,
+//   fromNullable,
+//   getOrElse,
+//   getOrThrow,
+//   getOrThrowWith,
+//   getOrUndefined,
+//   getRight,
+//   isNone,
+//   isSome,
+//   map,
+//   match,
+//   none,
+//   orElse,
+//   some
+// } from "./impl/Option.js"
 
 export * from "./impl/Option.js"
 export * from "./internal/Jumpers/Option.js"
@@ -11,7 +31,32 @@ export * from "./internal/Jumpers/Option.js"
 export type Option<A> = None<A> | Some<A>
 
 export declare namespace Option {
-  // eslint-disable-next-line import/no-cycle
+  // opt1: much more convenient of course, but while it works, the compiler gives an error
   // @ts-expect-error
   export type * from "./impl/Option.js"
+
+  // opt2: have to manually (or automate process) these exports
+  // much less convenient, but it doesn't require silencing compiler
+  // export type { None, OptionUnify, Some, TypeId }
+
+  // export {
+  //   all,
+  //   contains,
+  //   filter,
+  //   flatMap,
+  //   fromNullable,
+  //   getOrElse,
+  //   getOrThrow,
+  //   getOrThrowWith,
+  //   getOrUndefined,
+  //   getRight,
+  //   isNone,
+  //   isSome,
+  //   map,
+  //   match,
+  //   none,
+  //   orElse,
+  //   some
+  //   // TODO: all the rest
+  // }
 }
