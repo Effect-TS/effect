90ad7efe79b866a22b4777ea85e867f84b4aa53a
Patrick Roza <contact@patrickroza.com>
2023-11-08 22:25:45 +0100

    wicked

--- src/Option.ts
+++ src/Option.ts
@@ -1,6 +1,24 @@
 // eslint-disable-next-line import/no-cycle
 import type { None, OptionUnify, Some, TypeId } from "./impl/Option.js"
-import type * as bla from "./impl/Option.js"
+import {
+  all,
+  contains,
+  filter,
+  flatMap,
+  fromNullable,
+  getOrElse,
+  getOrThrow,
+  getOrThrowWith,
+  getOrUndefined,
+  getRight,
+  isNone,
+  isSome,
+  map,
+  match,
+  none,
+  orElse,
+  some
+} from "./impl/Option.js"
 
 export * from "./impl/Option.js"
 export * from "./internal/Jumpers/Option.js"
@@ -12,26 +30,25 @@ export * from "./internal/Jumpers/Option.js"
 export type Option<A> = None<A> | Some<A>
 
 export declare namespace Option {
-  const isSome: typeof bla.isSome
-  const isNone: typeof bla.isNone
-  const some: typeof bla.some
-  const none: typeof bla.none
-  const getOrElse: typeof bla.getOrElse
-  const fromNullable: typeof bla.fromNullable
-  const match: typeof bla.match
-  const map: typeof bla.map
-  const filter: typeof bla.filter
-  const getOrUndefined: typeof bla.getOrUndefined
-  const getRight: typeof bla.getRight
-  const flatMap: typeof bla.flatMap
-  const orElse: typeof bla.orElse
-  const all: typeof bla.all
-  const contains: typeof bla.contains
-  const getOrThrow: typeof bla.getOrThrow
-  const getOrThrowWith: typeof bla.getOrThrowWith
-  // TODO: the rest
-}
-
-export declare namespace Option {
   export type { None, OptionUnify, Some, TypeId }
+
+  export {
+    all,
+    contains,
+    filter,
+    flatMap,
+    fromNullable,
+    getOrElse,
+    getOrThrow,
+    getOrThrowWith,
+    getOrUndefined,
+    getRight,
+    isNone,
+    isSome,
+    map,
+    match,
+    none,
+    orElse,
+    some
+  }
 }
