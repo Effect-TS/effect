dcc77a9f3587009eb7017335c3f5fe686721a65a
Patrick Roza <contact@patrickroza.com>
2023-11-10 11:08:21 +0100

    fix

--- src/Match.ts
+++ src/Match.ts
@@ -1,10 +1,14 @@
 /**
- * @since 1.0.0
-
  * @since 2.0.0
+ */
+
+/**
+ * @since 2.0.0
+ * @internal
  */
 export * from "./impl/Match.js"
 /**
  * @since 2.0.0
+ * @internal
  */
 export * from "./internal/Jumpers/Match.js"
