9fe33997b953c1d8124b8030280147e9b5924f22
Wayne Maurer <wayne@waynemaurer.net>
2023-11-15 19:52:57 +0100

    Improve refinement typings for ReadonlyArray.partition

--- src/ReadonlyArray.ts
+++ src/ReadonlyArray.ts
@@ -1688,14 +1688,14 @@ export const filter: {
 export const partition: {
   <C extends A, B extends A, A = C>(refinement: (a: A, i: number) => a is B): (
     self: Iterable<C>
-  ) => [Array<C>, Array<B>]
+  ) => [Array<Exclude<B, C>>, Array<B>]
   <B extends A, A = B>(
     predicate: (a: A, i: number) => boolean
   ): (self: Iterable<B>) => [Array<B>, Array<B>]
   <C extends A, B extends A, A = C>(
     self: Iterable<C>,
     refinement: (a: A, i: number) => a is B
-  ): [Array<C>, Array<B>]
+  ): [Array<Exclude<C, B>>, Array<B>]
   <B extends A, A = B>(
     self: Iterable<B>,
     predicate: (a: A, i: number) => boolean
