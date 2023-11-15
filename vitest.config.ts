5b5f354f616a4bd8317db5716618c18c01bbc39f
Sebastian Lorenz <fubhy@fubhy.com>
2023-11-15 14:57:25 +0100

    move test files to *.test.ts (#1653)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
-/// <reference types="vitest" />
-import path from "path"
+import * as path from "node:path"
 import { defineConfig } from "vite"
 
 export default defineConfig({
   test: {
-    include: ["./test/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: ["./test/util.ts", "./test/utils/**/*.ts", "./test/**/*.init.ts"],
+    include: ["test/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
     browser: {
       name: "chromium",
       provider: "playwright",
