a31fd91580abd4aacfbb556070ded42624eb619f
Tim <hello@timsmart.co>
2023-11-23 14:09:49 +1300

    allow omission of Scope type in R of Stream.asyncScoped (#1690)

--- packages/effect/src/internal/stream.ts
+++ packages/effect/src/internal/stream.ts
@@ -604,7 +604,7 @@ export const asyncOption = <R, E, A>(
 
 /** @internal */
 export const asyncScoped = <R, E, A>(
-  register: (emit: Emit.Emit<R, E, A, void>) => Effect.Effect<R, E, unknown>,
+  register: (emit: Emit.Emit<R, E, A, void>) => Effect.Effect<R | Scope.Scope, E, unknown>,
   outputBuffer = 16
 ): Stream.Stream<Exclude<R, Scope.Scope>, E, A> =>
   pipe(
