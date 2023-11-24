599243aecdef597ae072cebcf0a4a0e1dc3ce849
Tim <hello@timsmart.co>
2023-11-24 15:33:31 +1300

    remove prepare-v2 script (#1)

--- packages/experimental/src/Persistance.ts
+++ packages/experimental/src/Persistance.ts
@@ -1,9 +1,190 @@
-import type * as Effect from "effect/Effect"
-import type * as Option from "effect/Option"
+/**
+ * @since 1.0.0
+ */
+import type * as ParseResult from "@effect/schema/ParseResult"
+import * as Schema from "@effect/schema/Schema"
+import * as Context from "effect/Context"
+import * as Data from "effect/Data"
+import * as Effect from "effect/Effect"
+import * as Layer from "effect/Layer"
+import * as Option from "effect/Option"
 
-export interface Persistance {
-  readonly get: (key: string) => Effect.Effect<never, never, Option.Option<unknown>>
-  readonly getMany: (key: string) => Effect.Effect<never, never, Array<Option.Option<unknown>>>
-  readonly set: (key: string, value: unknown) => Effect.Effect<never, never, void>
-  readonly remove: (key: string) => Effect.Effect<never, never, void>
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface Persistance<A> {
+  readonly get: (key: string) => Effect.Effect<never, PersistanceError, Option.Option<A>>
+  readonly getMany: (key: Array<string>) => Effect.Effect<never, PersistanceError, Array<Option.Option<A>>>
+  readonly set: (key: string, value: A) => Effect.Effect<never, PersistanceError, void>
+  readonly remove: (key: string) => Effect.Effect<never, PersistanceError, void>
 }
+
+/**
+ * @since 1.0.0
+ */
+export const Persistance = <A>(_: Persistance<A>) => _
+
+/**
+ * @since 1.0.0
+ * @category errors
+ */
+export type PersistanceError = PersistanceSchemaError | PersistanceBackingError
+
+/**
+ * @since 1.0.0
+ * @category errors
+ */
+export class PersistanceSchemaError extends Data.TaggedError("PersistanceSchemaError")<{
+  readonly method: string
+  readonly errors: ParseResult.ParseError["errors"]
+}> {}
+
+/**
+ * @since 1.0.0
+ * @category errors
+ */
+export class PersistanceBackingError extends Data.TaggedError("PersistanceBackingError")<{
+  readonly method: string
+  readonly error: unknown
+}> {}
+
+/**
+ * @since 1.0.0
+ * @category type ids
+ */
+export const BackingPersistanceTypeId = Symbol.for("@effect/experimental/BackingPersistance")
+
+/**
+ * @since 1.0.0
+ * @category type ids
+ */
+export type BackingPersistanceTypeId = typeof BackingPersistanceTypeId
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface BackingPersistance {
+  readonly [BackingPersistanceTypeId]: BackingPersistanceTypeId
+  readonly make: (storeId: string) => Effect.Effect<never, never, Persistance<unknown>>
+}
+
+/**
+ * @since 1.0.0
+ * @category tags
+ */
+export const BackingPersistance: Context.Tag<BackingPersistance, BackingPersistance> = Context.Tag<BackingPersistance>(
+  "@effect/experimental/BackingPersistance"
+)
+
+/**
+ * @since 1.0.0
+ * @category type ids
+ */
+export const SchemaPersistanceTypeId = Symbol.for("@effect/experimental/SchemaPersistance")
+
+/**
+ * @since 1.0.0
+ * @category type ids
+ */
+export type SchemaPersistanceTypeId = typeof SchemaPersistanceTypeId
+
+/**
+ * @since 1.0.0
+ * @category models
+ */
+export interface SchemaPersistance {
+  readonly [SchemaPersistanceTypeId]: SchemaPersistanceTypeId
+  readonly make: <I, A>(storeId: string, schema: Schema.Schema<I, A>) => Effect.Effect<never, never, Persistance<A>>
+}
+
+/**
+ * @since 1.0.0
+ * @category tags
+ */
+export const SchemaPersistance: Context.Tag<SchemaPersistance, SchemaPersistance> = Context.Tag<SchemaPersistance>(
+  "@effect/experimental/SchemaPersistance"
+)
+
+/**
+ * @since 1.0.0
+ * @category layers
+ */
+export const layerSchema = Layer.effect(
+  SchemaPersistance,
+  Effect.gen(function*(_) {
+    const backing = yield* _(BackingPersistance)
+    return SchemaPersistance.of({
+      [SchemaPersistanceTypeId]: SchemaPersistanceTypeId,
+      make: <I, A>(storeId: string, schema: Schema.Schema<I, A>) =>
+        Effect.gen(function*(_) {
+          const storage = yield* _(backing.make(storeId))
+          const parse_ = Schema.parse(schema)
+          const parse = (method: string, value: unknown) =>
+            Effect.mapError(parse_(value), (_) => new PersistanceSchemaError({ method, errors: _.errors }))
+
+          const encode_ = Schema.encode(schema)
+          const encode = (method: string, value: A) =>
+            Effect.mapError(encode_(value), (_) => new PersistanceSchemaError({ method, errors: _.errors }))
+
+          return Persistance<A>({
+            get: (key) =>
+              Effect.flatMap(
+                storage.get(key),
+                Option.match({
+                  onNone: () => Effect.succeedNone,
+                  onSome: (_) => Effect.asSome(parse("get", _))
+                })
+              ),
+            getMany: (keys) =>
+              Effect.flatMap(
+                storage.getMany(keys),
+                Effect.forEach((result, i) =>
+                  Option.match(result, {
+                    onNone: () => Effect.succeedNone,
+                    onSome: (_) =>
+                      parse("getMany", _).pipe(
+                        Effect.tapError((_) => storage.remove(keys[i])),
+                        Effect.option
+                      )
+                  })
+                )
+              ),
+            set: (key, value) =>
+              encode("set", value).pipe(
+                Effect.flatMap((_) => storage.set(key, _))
+              ),
+            remove: storage.remove
+          })
+        })
+    })
+  })
+)
+
+/**
+ * @since 1.0.0
+ * @category layers
+ */
+export const layerMemory: Layer.Layer<never, never, BackingPersistance> = Layer.succeed(
+  BackingPersistance,
+  BackingPersistance.of({
+    [BackingPersistanceTypeId]: BackingPersistanceTypeId,
+    make: (_storeId) =>
+      Effect.sync(() => {
+        const map = new Map<string, unknown>()
+        return Persistance<unknown>({
+          get: (key) => Effect.sync(() => Option.fromNullable(map.get(key))),
+          getMany: (keys) => Effect.sync(() => keys.map((key) => Option.fromNullable(map.get(key)))),
+          set: (key, value) => Effect.sync(() => map.set(key, value)),
+          remove: (key) => Effect.sync(() => map.delete(key))
+        })
+      })
+  })
+)
+
+/**
+ * @since 1.0.0
+ * @category layers
+ */
+export const layerSchemaMemory: Layer.Layer<never, never, SchemaPersistance> = layerSchema.pipe(Layer.use(layerMemory))
---
+++ packages/experimental/src/RequestResolver.ts
@@ -0,0 +1,201 @@
+/**
+ * @since 1.0.0
+ */
+import * as Schema from "@effect/schema/Schema"
+import * as Cause from "effect/Cause"
+import * as Chunk from "effect/Chunk"
+import * as Deferred from "effect/Deferred"
+import type * as Duration from "effect/Duration"
+import * as Effect from "effect/Effect"
+import * as Either from "effect/Either"
+import { dual, pipe } from "effect/Function"
+import * as Option from "effect/Option"
+import * as PrimaryKey from "effect/PrimaryKey"
+import * as Queue from "effect/Queue"
+import * as ReadonlyArray from "effect/ReadonlyArray"
+import * as Ref from "effect/Ref"
+import * as Request from "effect/Request"
+import * as RequestResolver from "effect/RequestResolver"
+import type * as Scope from "effect/Scope"
+import * as Persistance from "./Persistance.js"
+
+interface DataLoaderItem<A extends Request.Request<any, any>> {
+  readonly request: A
+  readonly deferred: Deferred.Deferred<
+    Request.Request.Error<A>,
+    Request.Request.Success<A>
+  >
+}
+
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const dataLoader = dual<
+  (
+    options: {
+      readonly window: Duration.DurationInput
+      readonly maxBatchSize?: number
+    }
+  ) => <A extends Request.Request<any, any>>(
+    self: RequestResolver.RequestResolver<A, never>
+  ) => Effect.Effect<Scope.Scope, never, RequestResolver.RequestResolver<A, never>>,
+  <A extends Request.Request<any, any>>(
+    self: RequestResolver.RequestResolver<A, never>,
+    options: {
+      readonly window: Duration.DurationInput
+      readonly maxBatchSize?: number
+    }
+  ) => Effect.Effect<Scope.Scope, never, RequestResolver.RequestResolver<A, never>>
+>(2, <A extends Request.Request<any, any>>(
+  self: RequestResolver.RequestResolver<A, never>,
+  options: {
+    readonly window: Duration.DurationInput
+    readonly maxBatchSize?: number
+  }
+) =>
+  Effect.gen(function*(_) {
+    const queue = yield* _(Queue.unbounded<DataLoaderItem<A>>())
+    const batch = yield* _(Ref.make(Chunk.empty<DataLoaderItem<A>>()))
+    const takeOne = Effect.flatMap(Queue.take(queue), (item) => Ref.updateAndGet(batch, Chunk.append(item)))
+    const takeRest = takeOne.pipe(
+      Effect.repeatUntil(
+        (items) =>
+          options.maxBatchSize !== undefined &&
+          items.length >= options.maxBatchSize
+      ),
+      Effect.timeout(options.window),
+      Effect.zipRight(Ref.getAndSet(batch, Chunk.empty()))
+    )
+
+    yield* _(
+      takeOne,
+      Effect.zipRight(takeRest),
+      Effect.flatMap(
+        Effect.filter(({ deferred }) => Deferred.isDone(deferred), {
+          negate: true
+        })
+      ),
+      Effect.flatMap(
+        Effect.forEach(
+          ({ deferred, request }) =>
+            Effect.flatMap(Effect.exit(Effect.request(request, self)), (exit) => Deferred.complete(deferred, exit)),
+          { batching: true, discard: true }
+        )
+      ),
+      Effect.forever,
+      Effect.forkScoped
+    )
+
+    return RequestResolver.fromEffect((request: A) =>
+      Effect.flatMap(
+        Deferred.make<Request.Request.Error<A>, Request.Request.Success<A>>(),
+        (deferred) =>
+          Queue.offer(queue, { request, deferred }).pipe(
+            Effect.zipRight(Deferred.await(deferred)),
+            Effect.onInterrupt(() => Deferred.interrupt(deferred))
+          )
+      )
+    )
+  }))
+
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const persisted = dual<
+  <EI, EA, AI, AA>(
+    options: {
+      readonly storeId: string
+      readonly failureSchema: Schema.Schema<EI, EA>
+      readonly successSchema: Schema.Schema<AI, AA>
+    }
+  ) => <Req extends Request.Request<EA, AA> & { readonly _tag: string } & PrimaryKey.PrimaryKey>(
+    self: RequestResolver.RequestResolver<Req, never>
+  ) => Effect.Effect<Persistance.SchemaPersistance | Scope.Scope, never, RequestResolver.RequestResolver<Req, never>>,
+  <Req extends Request.Request<EA, AA> & { readonly _tag: string } & PrimaryKey.PrimaryKey, EI, EA, AI, AA>(
+    self: RequestResolver.RequestResolver<Req, never>,
+    options: {
+      readonly storeId: string
+      readonly failureSchema: Schema.Schema<EI, EA>
+      readonly successSchema: Schema.Schema<AI, AA>
+    }
+  ) => Effect.Effect<Persistance.SchemaPersistance | Scope.Scope, never, RequestResolver.RequestResolver<Req, never>>
+>(2, <Req extends Request.Request<EA, AA> & { readonly _tag: string } & PrimaryKey.PrimaryKey, EI, EA, AI, AA>(
+  self: RequestResolver.RequestResolver<Req, never>,
+  options: {
+    readonly storeId: string
+    readonly failureSchema: Schema.Schema<EI, EA>
+    readonly successSchema: Schema.Schema<AI, AA>
+  }
+): Effect.Effect<Persistance.SchemaPersistance | Scope.Scope, never, RequestResolver.RequestResolver<Req, never>> =>
+  Effect.gen(function*(_) {
+    const resultSchema = Schema.either(options.failureSchema, options.successSchema)
+    const storage = yield* _(
+      (yield* _(Persistance.SchemaPersistance)).make(options.storeId, resultSchema)
+    )
+    const requestKey = (request: Req) => `${request._tag}:${request[PrimaryKey.symbol]()}`
+
+    const partition = (requests: ReadonlyArray<Req>) =>
+      storage.getMany(requests.map(requestKey)).pipe(
+        Effect.map(
+          ReadonlyArray.partitionMap((_, i) =>
+            Option.match(_, {
+              onNone: () => Either.left(requests[i]),
+              onSome: (_) => Either.right([requests[i], _] as const)
+            })
+          )
+        ),
+        Effect.orElseSucceed(() => [requests, []] as const)
+      )
+
+    const set = (
+      request: Req,
+      result: Request.Request.Result<Req>
+    ): Effect.Effect<never, never, void> => {
+      const key = requestKey(request)
+      if (result._tag === "Failure") {
+        return Either.match(Cause.failureOrCause(result.cause), {
+          onLeft: (e) => Effect.ignoreLogged(storage.set(key, Either.left(e))),
+          onRight: (_cause) => Effect.unit
+        })
+      }
+      return Effect.ignoreLogged(storage.set(key, Either.right(result.value)))
+    }
+
+    return RequestResolver.makeBatched((requests: Array<Req>) =>
+      Effect.flatMap(partition(requests), ([remaining, results]) => {
+        const completeCached = Effect.forEach(
+          results,
+          ([request, result]) =>
+            Request.completeEffect(request, result as any) as Effect.Effect<
+              never,
+              never,
+              void
+            >,
+          { discard: true }
+        )
+        const completeUncached = pipe(
+          Effect.forEach(
+            remaining,
+            (request) => Effect.exit(Effect.request(request, self)),
+            { batching: true }
+          ),
+          Effect.flatMap((results) =>
+            Effect.forEach(
+              results,
+              (result, i) => {
+                const request = remaining[i]
+                return Effect.zipRight(
+                  set(request, result as any),
+                  Request.complete(request, result as any)
+                )
+              },
+              { discard: true }
+            )
+          )
+        )
+        return Effect.zipRight(completeCached, completeUncached)
+      })
+    )
+  }))
