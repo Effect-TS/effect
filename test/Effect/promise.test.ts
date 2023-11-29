a8cacde363f7a2e52cba7fe91636f95e3bfda757
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2023-11-28 18:55:38 -0600

    add more promise tests

--- test/Effect/promise.test.ts
+++ test/Effect/promise.test.ts
@@ -3,7 +3,60 @@ import * as timeout from "effect/internal/timeout"
 import { describe, expect, it } from "vitest"
 
 describe.concurrent("Effect", () => {
-  it("promise - success with AbortSignal", async () => {
+  it("promise - sync fail", async () => {
+    const effect = Effect.promise(() => {
+      throw "fail"
+      return Promise.resolve("hello")
+    })
+    const exit = await Effect.runPromiseExit(effect)
+    expect(exit._tag).toBe("Failure")
+  })
+
+  it("promise - failure, no AbortSignal", async () => {
+    const effect = Effect.promise<void>(() => {
+      return new Promise((_resolve, reject) => {
+        timeout.set(() => {
+          reject("error")
+        }, 5)
+      })
+    })
+    const exit = await Effect.runPromiseExit(effect)
+    expect(exit._tag).toBe("Failure")
+  })
+
+  it("promise - failure, AbortSignal", async () => {
+    let aborted = false
+    const effect = Effect.promise<void>((signal) => {
+      signal.addEventListener("abort", () => {
+        aborted = true
+      })
+      return new Promise((_resolve, reject) => {
+        timeout.set(() => {
+          reject("error")
+        }, 5)
+      })
+    })
+    const program = effect.pipe(
+      Effect.timeout("10 millis")
+    )
+    const exit = await Effect.runPromiseExit(program)
+    expect(exit._tag).toBe("Failure")
+    expect(aborted).toBe(false)
+  })
+
+  it("promise - success, no AbortSignal", async () => {
+    const effect = Effect.promise<void>(() => {
+      return new Promise((resolve) => {
+        timeout.set(() => {
+          resolve()
+        }, 100)
+      })
+    })
+    const exit = await Effect.runPromiseExit(effect)
+    expect(exit._tag).toBe("Success")
+  })
+
+  it("promise - success, AbortSignal", async () => {
     let aborted = false
     const effect = Effect.promise<void>((signal) => {
       signal.addEventListener("abort", () => {
--- test/Effect/tryPromise.test.ts
+++ test/Effect/tryPromise.test.ts
@@ -5,16 +5,16 @@ import * as timeout from "effect/internal/timeout"
 import { describe, expect, it } from "vitest"
 
 describe.concurrent("Effect", () => {
-  it("tryPromise - success, no catch, no AbortSignal", async () => {
-    const effect = Effect.tryPromise<number>(() =>
-      new Promise((resolve) => {
-        timeout.set(() => {
-          resolve(1)
-        }, 100)
-      })
-    )
-    const n = await Effect.runPromise(effect)
-    expect(n).toBe(1)
+  it("tryPromise - sync fail", async () => {
+    const effect = Effect.tryPromise({
+      try: () => {
+        throw "fail"
+        return Promise.resolve("hello")
+      },
+      catch: (e) => `caught: ${e}`
+    })
+    const either = await Effect.runPromise(Effect.either(effect))
+    expect(either).toStrictEqual(Either.left("caught: fail"))
   })
 
   it("tryPromise - failure, no catch, no AbortSignal", async () => {
@@ -29,6 +29,26 @@ describe.concurrent("Effect", () => {
     expect(either).toStrictEqual(Either.left(new Cause.UnknownException("error")))
   })
 
+  it("tryPromise - failure, no catch, AbortSignal", async () => {
+    let aborted = false
+    const effect = Effect.tryPromise<void>((signal) => {
+      signal.addEventListener("abort", () => {
+        aborted = true
+      })
+      return new Promise((_resolve, reject) => {
+        timeout.set(() => {
+          reject("error")
+        }, 50)
+      })
+    })
+    const program = effect.pipe(
+      Effect.timeout("100 millis")
+    )
+    const exit = await Effect.runPromiseExit(program)
+    expect(exit._tag).toBe("Failure")
+    expect(aborted).toBe(false)
+  })
+
   it("tryPromise - failure, catch, no AbortSignal", async () => {
     const effect = Effect.tryPromise({
       try: () =>
@@ -41,6 +61,41 @@ describe.concurrent("Effect", () => {
     })
     const either = await Effect.runPromise(Effect.either(effect))
     expect(either).toStrictEqual(Either.left(new Error("error")))
+  })
+
+  it("tryPromise - failure, catch, AbortSignal", async () => {
+    let aborted = false
+    const effect = Effect.tryPromise<void, Error>({
+      try: (signal) => {
+        signal.addEventListener("abort", () => {
+          aborted = true
+        })
+        return new Promise((_resolve, reject) => {
+          timeout.set(() => {
+            reject("error")
+          }, 50)
+        })
+      },
+      catch: () => new Error()
+    })
+    const program = effect.pipe(
+      Effect.timeout("100 millis")
+    )
+    const exit = await Effect.runPromiseExit(program)
+    expect(exit._tag).toBe("Failure")
+    expect(aborted).toBe(false)
+  })
+
+  it("tryPromise - success, no catch, no AbortSignal", async () => {
+    const effect = Effect.tryPromise<number>(() =>
+      new Promise((resolve) => {
+        timeout.set(() => {
+          resolve(1)
+        }, 100)
+      })
+    )
+    const n = await Effect.runPromise(effect)
+    expect(n).toBe(1)
   })
 
   it("tryPromise - success, no catch, AbortSignal", async () => {
@@ -61,6 +116,22 @@ describe.concurrent("Effect", () => {
     const exit = await Effect.runPromiseExit(program)
     expect(exit._tag).toBe("Success")
     expect(aborted).toBe(true)
+  })
+
+  it("tryPromise - success, catch, no AbortSignal", async () => {
+    const effect = Effect.tryPromise<number, Error>({
+      try: () => {
+        return new Promise((resolve) => {
+          timeout.set(() => {
+            resolve(1)
+          }, 100)
+        })
+      },
+      catch: () => new Error()
+    })
+
+    const n = await Effect.runPromise(effect)
+    expect(n).toBe(1)
   })
 
   it("tryPromise - success, catch, AbortSignal", async () => {
