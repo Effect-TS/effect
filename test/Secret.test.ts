ed7135d65f8e6ba7983940cb94b61bd713e7b351
Jesse Kelly <kellyjesse881@gmail.com>
2023-12-16 18:53:08 +0100

    added test

--- test/Secret.test.ts
+++ test/Secret.test.ts
@@ -2,13 +2,17 @@
  * @since 1.0.0
  */
 
+import { pipe } from "effect"
 import * as _ from "effect/Secret"
 import { describe, expect, it } from "vitest"
 
 describe("Secret", () => {
   it("map", () => {
     const secret = _.fromString("hello")
-    const newSecret = _.map(secret, (s) => `${s} world`)
-    expect(_.value(newSecret)).toBe("hello world")
+    const newSecret1 = _.map(secret, (s) => `${s} world`)
+    const newSecret2 = pipe(secret, _.map((s) => `${s} world`))
+
+    expect(_.value(newSecret1)).toBe("hello world")
+    expect(_.value(newSecret2)).toBe("hello world")
   })
 })
