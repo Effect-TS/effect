46d1a63b59ed32d1e5aeb321d205b333a7389cf4
gcanti <giulio.canti@gmail.com>
2024-01-09 09:18:16 +0100

    Schema: Arbitrary add `arbitrary` annotation

---
+++ .changeset/nine-buttons-watch.md
@@ -0,0 +1,5 @@
+---
+"@effect/schema": patch
+---
+
+Schema: Arbitrary add `arbitrary` annotation
--- packages/schema/src/Arbitrary.ts
+++ packages/schema/src/Arbitrary.ts
@@ -10,6 +10,7 @@ import * as AST from "./AST.js"
 import * as Internal from "./internal/ast.js"
 import * as filters from "./internal/filters.js"
 import * as hooks from "./internal/hooks.js"
+import * as InternalSchema from "./internal/schema.js"
 import * as Parser from "./Parser.js"
 import type * as Schema from "./Schema.js"
 
@@ -32,6 +33,15 @@ export const ArbitraryHookId: unique symbol = hooks.ArbitraryHookId
  * @since 1.0.0
  */
 export type ArbitraryHookId = typeof ArbitraryHookId
+
+/**
+ * @category annotations
+ * @since 1.0.0
+ */
+export const arbitrary =
+  <A>(handler: (...args: ReadonlyArray<Arbitrary<any>>) => Arbitrary<A>) =>
+  <I>(self: Schema.Schema<I, A>): Schema.Schema<I, A> =>
+    InternalSchema.make(AST.setAnnotation(self.ast, ArbitraryHookId, handler))
 
 /**
  * Returns a fast-check Arbitrary for the `A` type of the provided schema.
