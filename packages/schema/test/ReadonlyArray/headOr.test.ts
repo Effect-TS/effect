ff3bcb5f6f8b3f09a5887ff268c34c34e3485ef2
gcanti <giulio.canti@gmail.com>
2024-01-16 09:41:14 +0100

    Schema: add fallback arg to headOrFail and rename to headOr

--- packages/schema/test/ReadonlyArray/headOr.test.ts
+++ packages/schema/test/ReadonlyArray/headOr.test.ts
@@ -2,9 +2,9 @@ import * as S from "@effect/schema/Schema"
 import * as Util from "@effect/schema/test/util"
 import { describe, it } from "vitest"
 
-describe("ReadonlyArray > headOrFail", () => {
-  it("decoding", async () => {
-    const schema = S.headOrFail(S.number)
+describe("ReadonlyArray > headOr", () => {
+  it("decoding (without fallback)", async () => {
+    const schema = S.headOr(S.number)
     await Util.expectParseSuccess(schema, [1], 1)
     await Util.expectParseFailure(
       schema,
@@ -24,8 +24,23 @@ describe("ReadonlyArray > headOrFail", () => {
     )
   })
 
+  it("decoding (with fallback)", async () => {
+    const schema = S.headOr(S.number, () => 0)
+    await Util.expectParseSuccess(schema, [1], 1)
+    await Util.expectParseSuccess(schema, [], 0)
+    await Util.expectParseFailure(
+      schema,
+      ["a"],
+      `(ReadonlyArray<number> <-> number)
+└─ From side transformation failure
+   └─ ReadonlyArray<number>
+      └─ [0]
+         └─ Expected a number, actual "a"`
+    )
+  })
+
   it("encoding", async () => {
-    const schema = S.headOrFail(S.number)
+    const schema = S.headOr(S.number)
     await Util.expectEncodeSuccess(schema, 1, [1])
   })
 })
