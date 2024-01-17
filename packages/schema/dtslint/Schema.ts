463e67163adc6957d9b6f66b5f32c5b26afc0207
gcanti <giulio.canti@gmail.com>
2024-01-17 16:36:15 +0100

    more dtslint tests

--- packages/schema/dtslint/Schema.ts
+++ packages/schema/dtslint/Schema.ts
@@ -877,3 +877,17 @@ S.pluck(S.struct({ a: S.string, b: S.number }), "a")
 
 // $ExpectType Schema<{ readonly a: string; readonly b: number; }, string>
 pipe(S.struct({ a: S.string, b: S.number }), S.pluck("a"))
+
+// ---------------------------------------------
+// head
+// ---------------------------------------------
+
+// $ExpectType Schema<readonly number[], Option<number>>
+S.head(S.array(S.number))
+
+// ---------------------------------------------
+// headOr
+// ---------------------------------------------
+
+// $ExpectType Schema<readonly number[], number>
+S.headOr(S.array(S.number))
