23ec4ed5006eb7129e96e50ee32c1b616de04dc9
Tim Smart <tim.smart@arisechurch.com>
2024-01-17 20:18:12 +1300

    fix SerializedRunner

--- packages/platform/src/internal/workerRunner.ts
+++ packages/platform/src/internal/workerRunner.ts
@@ -163,13 +163,15 @@ export const layer = <I, R, E, O>(
 
 /** @internal */
 export const makeSerialized = <
+  R,
   I,
   A extends Schema.TaggedRequest.Any,
   const Handlers extends WorkerRunner.SerializedRunner.Handlers<A>
 >(
-  schema: Schema.Schema<never, I, A>,
+  schema: Schema.Schema<R, I, A>,
   handlers: Handlers
 ): Effect.Effect<
+  | R
   | WorkerRunner.PlatformRunner
   | Scope.Scope
   | WorkerRunner.SerializedRunner.HandlersContext<Handlers>,
@@ -179,7 +181,7 @@ export const makeSerialized = <
   Effect.gen(function*(_) {
     const scope = yield* _(Effect.scope)
     let context = Context.empty() as Context.Context<any>
-    const parseRequest = Schema.parse(schema)
+    const parseRequest = Schema.parse(schema) as (_: unknown) => Effect.Effect<never, never, A>
 
     return yield* _(make((request: A) => {
       const result = (handlers as any)[request._tag](request)
