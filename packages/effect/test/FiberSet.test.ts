40aadfba87bcb48a033e40021c74540ea96b33ee
Tim <hello@timsmart.co>
2024-01-19 09:17:26 +1300

    add Fiber{Set,Map}.runtime api (#1941)

--- packages/effect/test/FiberSet.test.ts
+++ packages/effect/test/FiberSet.test.ts
@@ -1,4 +1,4 @@
-import { Effect, Ref } from "effect"
+import { Effect, ReadonlyArray, Ref } from "effect"
 import * as it from "effect-test/utils/extend"
 import * as FiberSet from "effect/FiberSet"
 import { assert, describe } from "vitest"
@@ -11,14 +11,36 @@ describe("FiberSet", () => {
         Effect.gen(function*(_) {
           const set = yield* _(FiberSet.make())
           yield* _(
-            FiberSet.run(
-              set,
+            Effect.onInterrupt(
+              Effect.never,
+              () => Ref.update(ref, (n) => n + 1)
+            ).pipe(
+              FiberSet.run(set)
+            ),
+            Effect.replicateEffect(10)
+          )
+          yield* _(Effect.yieldNow())
+        }),
+        Effect.scoped
+      )
+
+      assert.strictEqual(yield* _(Ref.get(ref)), 10)
+    }))
+
+  it.effect("runtime", () =>
+    Effect.gen(function*(_) {
+      const ref = yield* _(Ref.make(0))
+      yield* _(
+        Effect.gen(function*(_) {
+          const set = yield* _(FiberSet.make())
+          const run = yield* _(FiberSet.runtime(set)<never>())
+          ReadonlyArray.range(1, 10).forEach(() =>
+            run(
               Effect.onInterrupt(
                 Effect.never,
                 () => Ref.update(ref, (n) => n + 1)
               )
-            ),
-            Effect.replicateEffect(10)
+            )
           )
           yield* _(Effect.yieldNow())
         }),
