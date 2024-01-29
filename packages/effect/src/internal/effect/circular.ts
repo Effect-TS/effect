966183c1d88f83e7d41b0358dc5087c94674bbb7
Milan Suk <Milansuk@email.cz>
2024-01-29 21:07:10 +0100

    use TimeoutException for timeout (#1995)

--- packages/effect/src/internal/effect/circular.ts
+++ packages/effect/src/internal/effect/circular.ts
@@ -399,14 +399,14 @@ export const supervised = dual<
 export const timeout = dual<
   (
     duration: Duration.DurationInput
-  ) => <R, E, A>(self: Effect.Effect<R, E, A>) => Effect.Effect<R, E | Cause.NoSuchElementException, A>,
+  ) => <R, E, A>(self: Effect.Effect<R, E, A>) => Effect.Effect<R, E | Cause.TimeoutException, A>,
   <R, E, A>(
     self: Effect.Effect<R, E, A>,
     duration: Duration.DurationInput
-  ) => Effect.Effect<R, E | Cause.NoSuchElementException, A>
+  ) => Effect.Effect<R, E | Cause.TimeoutException, A>
 >(2, (self, duration) =>
   timeoutFail(self, {
-    onTimeout: () => new core.NoSuchElementException(),
+    onTimeout: () => new core.TimeoutException(),
     duration
   }))
 
