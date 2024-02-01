f024aba22ac3e87f2dd7bad197175457678893ef
Tim <hello@timsmart.co>
2024-02-02 10:49:00 +1300

    rename imports in BunContext

--- packages/platform-bun/src/BunContext.ts
+++ packages/platform-bun/src/BunContext.ts
@@ -8,11 +8,11 @@ import type * as Terminal from "@effect/platform/Terminal"
 import type * as Worker from "@effect/platform/Worker"
 import { pipe } from "effect/Function"
 import * as Layer from "effect/Layer"
-import * as CommandExecutorBun from "./BunCommandExecutor.js"
-import * as FileSystemBun from "./BunFileSystem.js"
-import * as PathBun from "./BunPath.js"
-import * as TerminalBun from "./BunTerminal.js"
-import * as WorkerBun from "./BunWorker.js"
+import * as BunCommandExecutor from "./BunCommandExecutor.js"
+import * as BunFileSystem from "./BunFileSystem.js"
+import * as BunPath from "./BunPath.js"
+import * as BunTerminal from "./BunTerminal.js"
+import * as BunWorker from "./BunWorker.js"
 
 /**
  * @since 1.0.0
@@ -31,10 +31,10 @@ export type BunContext =
  */
 export const layer: Layer.Layer<never, never, BunContext> = pipe(
   Layer.mergeAll(
-    PathBun.layer,
-    CommandExecutorBun.layer,
-    TerminalBun.layer,
-    WorkerBun.layerManager
+    BunPath.layer,
+    BunCommandExecutor.layer,
+    BunTerminal.layer,
+    BunWorker.layerManager
   ),
-  Layer.provideMerge(FileSystemBun.layer)
+  Layer.provideMerge(BunFileSystem.layer)
 )
