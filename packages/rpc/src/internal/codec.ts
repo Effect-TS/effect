4314944bb7bf42b23abdb7c78e4820d112009e7c
gcanti <giulio.canti@gmail.com>
2024-02-01 14:34:47 +0100

    fix rpc package

--- packages/rpc/src/internal/codec.ts
+++ packages/rpc/src/internal/codec.ts
@@ -4,17 +4,17 @@ import * as Effect from "effect/Effect"
 import { RpcDecodeFailure, RpcEncodeFailure } from "../Error.js"
 
 /** @internal */
-export const decode = <R, I, A>(schema: Schema.Schema<R, I, A>) => {
+export const decode = <A, I, R>(schema: Schema.Schema<A, I, R>) => {
   const decode = Schema.decodeUnknown(schema)
   return (input: unknown): Effect.Effect<R, RpcDecodeFailure, A> =>
     Effect.mapError(decode(input, { errors: "all" }), (error) => RpcDecodeFailure({ error: error.error }))
 }
 
 /** @internal */
-export const encode: <R, I, A>(
-  schema: Schema.Schema<R, I, A>
-) => (input: A, options?: ParseOptions | undefined) => Effect.Effect<R, RpcEncodeFailure, I> = <R, I, A>(
-  schema: Schema.Schema<R, I, A>
+export const encode: <A, I, R>(
+  schema: Schema.Schema<A, I, R>
+) => (input: A, options?: ParseOptions | undefined) => Effect.Effect<R, RpcEncodeFailure, I> = <A, I, R>(
+  schema: Schema.Schema<A, I, R>
 ) => {
   const encode = Schema.encode(schema)
   return (input: A) =>
