d8755af9f19d995c461dc2b1df5dd13d53292709
Tim <hello@timsmart.co>
2024-02-02 15:50:08 +1300

    fix for encoding of Transferable schemas (#2027)

--- packages/platform/test/Transferable.test.ts
+++ packages/platform/test/Transferable.test.ts
@@ -2,6 +2,11 @@ import * as Transferable from "@effect/platform/Transferable"
 import { Schema } from "@effect/schema"
 import { assert, describe, test } from "vitest"
 
+const TransferableUint8Array = Transferable.schema(
+  Schema.Uint8ArrayFromSelf,
+  (_) => [_.buffer]
+)
+
 describe("Transferable", () => {
   test("calls symbol", () => {
     class Test implements Transferable.Transferable {
@@ -28,11 +33,32 @@ describe("Transferable", () => {
 
   test("schema Uint8Array", () => {
     const array = new Uint8Array([1, 2, 3])
+    const data = Schema.decodeSync(TransferableUint8Array)(array)
+    assert.deepEqual(Transferable.get(data), [array.buffer])
+  })
+
+  test("schema encode", () => {
     const schema = Transferable.schema(
-      Schema.Uint8ArrayFromSelf,
-      (_) => [_.buffer]
+      Schema.struct({
+        data: Schema.Uint8ArrayFromSelf
+      }),
+      (_) => [_.data.buffer]
     )
-    const data = Schema.decodeSync(schema)(array)
+    const array = new Uint8Array([1, 2, 3])
+    const data = schema({
+      data: array
+    })
+    assert.deepEqual(Transferable.get(data), [array.buffer])
+
+    const encoded = Schema.encodeSync(schema)(data)
+    assert.deepEqual(encoded, {
+      data: array
+    })
+  })
+
+  test("schema encode Uint8Array", () => {
+    const array = new Uint8Array([1, 2, 3])
+    const data = TransferableUint8Array(array)
     assert.deepEqual(Transferable.get(data), [array.buffer])
   })
 })
