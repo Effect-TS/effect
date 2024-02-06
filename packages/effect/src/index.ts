42ed26d4b82ec7d7ec3555ee69b33d14f8125ca9
Harish Subramanium <hsubra89@gmail.com>
2024-02-07 06:51:05 +1000

    Export `RateLimiter`

--- packages/effect/src/index.ts
+++ packages/effect/src/index.ts
@@ -899,3 +899,12 @@ export * as UpstreamPullStrategy from "./UpstreamPullStrategy.js"
  * @since 2.0.0
  */
 export * as Utils from "./Utils.js"
+
+/**
+ * Limits the number of calls to a resource to a maximum amount in some interval using the token bucket algorithm.
+ *
+ * Note that only the moment of starting the effect is rate limited: the number of concurrent executions is not bounded.
+ *
+ * @since 2.0.0
+ */
+export * as RateLimiter from "./RateLimiter.js"
