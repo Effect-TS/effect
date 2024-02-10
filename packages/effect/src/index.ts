fca60fe049b57fe6e5e324455b572c05c9c8e49e
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-02-10 13:52:53 -0500

    fix index file

--- packages/effect/src/index.ts
+++ packages/effect/src/index.ts
@@ -545,9 +545,11 @@ export * as Queue from "./Queue.js"
 export * as Random from "./Random.js"
 
 /**
- * Limits the number of calls to a resource to a maximum amount in some interval using the token bucket algorithm.
+ * Limits the number of calls to a resource to a maximum amount in some interval
+ * using the token-bucket algorithm.
  *
- * Note that only the moment of starting the effect is rate limited: the number of concurrent executions is not bounded.
+ * Note that only the moment of starting the effect is rate limited: the number
+ * of concurrent executions is not bounded.
  *
  * Calls are queued up in an unbounded queue until capacity becomes available.
  *
