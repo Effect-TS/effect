7506274f16d93809d25e84682184df2e5b3c0b7a
Harish Subramanium <hsubra89@gmail.com>
2024-02-12 07:20:43 +1000

    PR Feedback

--- packages/effect/src/internal/rateLimiter.ts
+++ packages/effect/src/internal/rateLimiter.ts
@@ -7,36 +7,8 @@ import type { Scope } from "../Scope.js"
 import * as SynchronizedRef from "../SynchronizedRef.js"
 
 /** @internal */
-export const makeWithCost = (limit: number, window: DurationInput): Effect.Effect<
-  (cost: number) => RateLimiter,
-  never,
-  Scope
-> =>
-  Effect.gen(function*(_) {
-    const scope = yield* _(Effect.scope)
-    const semaphore = yield* _(Effect.makeSemaphore(limit))
-    const ref = yield* _(SynchronizedRef.make(false))
-    const reset = SynchronizedRef.updateEffect(
-      ref,
-      (running) =>
-        running ? Effect.succeed(true) : Effect.sleep(window).pipe(
-          Effect.zipRight(SynchronizedRef.set(ref, false)),
-          Effect.zipRight(semaphore.releaseAll),
-          Effect.forkIn(scope),
-          Effect.interruptible,
-          Effect.as(true)
-        )
-    )
-
-    return (cost: number) => (effect) => {
-      const take = Effect.zipRight(semaphore.take(cost), reset)
-      return Effect.zipRight(take, effect)
-    }
-  })
-
-/** @internal */
-const defaultCost = globalValue(
-  Symbol.for("effect/RateLimiter/defaultCost"),
+const currentCost = globalValue(
+  Symbol.for("effect/RateLimiter/currentCost"),
   () => FiberRef.unsafeMake(1)
 )
 
@@ -62,11 +34,11 @@ export const make = (limit: number, window: DurationInput): Effect.Effect<
         )
     )
 
-    const cost = FiberRef.get(defaultCost).pipe(Effect.flatMap(semaphore.take))
+    const cost = FiberRef.get(currentCost).pipe(Effect.flatMap(semaphore.take))
     const take = Effect.zipRight(cost, reset)
 
     return (effect) => Effect.zipRight(take, effect)
   })
 
 /** @internal */
-export const withCost = (cost: number) => Effect.locallyWith(defaultCost, () => cost)
+export const withCost = (cost: number) => Effect.locally(currentCost, cost)
