33865a01e8748dbac9a64150411657550b6cd966
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-02-10 22:53:29 -0500

    simplify token-bucket rate limiter

--- packages/effect/src/internal/rateLimiter.ts
+++ packages/effect/src/internal/rateLimiter.ts
@@ -1,15 +1,11 @@
-import * as Chunk from "../Chunk.js"
-import * as Deferred from "../Deferred.js"
 import type { DurationInput } from "../Duration.js"
+import * as Duration from "../Duration.js"
 import * as Effect from "../Effect.js"
 import * as FiberRef from "../FiberRef.js"
 import { globalValue } from "../GlobalValue.js"
-import { nextPow2 } from "../Number.js"
 import * as Queue from "../Queue.js"
 import type * as RateLimiter from "../RateLimiter.js"
-import * as Ref from "../Ref.js"
 import type * as Scope from "../Scope.js"
-import * as Stream from "../Stream.js"
 import * as SynchronizedRef from "../SynchronizedRef.js"
 
 /** @internal */
@@ -32,58 +28,30 @@ export const make = ({
   }
 }
 
-const tokenBucket: (
-  tokens: number,
-  interval: DurationInput
-) => Effect.Effect<
+const tokenBucket = (limit: number, window: DurationInput): Effect.Effect<
   RateLimiter.RateLimiter,
   never,
   Scope.Scope
-> = (tokens: number, interval: DurationInput) => {
-  return Effect.gen(function*(_) {
-    const queue = yield* _(Queue.bounded<[Ref.Ref<boolean>, Effect.Effect<void>, cost: number]>(nextPow2(tokens)))
-
+> =>
+  Effect.gen(function*(_) {
+    const millisPerToken = Math.ceil(Duration.toMillis(window) / limit)
+    const queue = yield* _(Queue.bounded<void>(limit - 1))
     yield* _(
-      Stream.fromQueue(queue, { maxChunkSize: 1 }),
-      Stream.filterEffect(([interrupted]) => Effect.negate(Ref.get(interrupted))),
-      Stream.throttle({
-        strategy: "shape",
-        duration: interval,
-        cost: Chunk.reduce(0, (acc, [, , cost]) => acc + cost),
-        units: tokens
-      }),
-      Stream.mapEffect(([, effect]) => effect, { concurrency: "unbounded", unordered: true }),
-      Stream.runDrain,
-      Effect.interruptible,
-      Effect.forkScoped
+      queue.take,
+      Effect.zipRight(Effect.sleep(millisPerToken)),
+      Effect.forever,
+      Effect.forkScoped,
+      Effect.interruptible
     )
-
-    const apply = <A, E, R>(task: Effect.Effect<A, E, R>) =>
-      Effect.gen(function*(_) {
-        const cost = yield* _(FiberRef.get(currentCost))
-        const start = yield* _(Deferred.make<void>())
-        const done = yield* _(Deferred.make<void>())
-        const interrupted = yield* _(Ref.make(false))
-        const action = Deferred.succeed(start, void 0).pipe(
-          Effect.zipRight(Deferred.await(done))
-        )
-        const cleanup = Ref.set(interrupted, true).pipe(
-          Effect.zipRight(Deferred.succeed(done, void 0))
-        )
-        const offer = Queue.offer(queue, [interrupted, action, cost]).pipe(
-          Effect.onInterrupt(() => cleanup)
-        )
-        return yield* _(
-          Effect.acquireReleaseInterruptible(offer, () => cleanup),
-          Effect.zipRight(Deferred.await(start)),
-          Effect.zipRight(task),
-          Effect.scoped
-        )
-      })
-
-    return apply
+    const offer = Effect.flatMap(
+      FiberRef.get(currentCost),
+      (cost) =>
+        cost > 1
+          ? Queue.offerAll(queue, new Array(cost).fill(void 0))
+          : Queue.offer(queue, void 0)
+    )
+    return (effect) => Effect.zipRight(offer, effect)
   })
-}
 
 const fixedWindow = (limit: number, window: DurationInput): Effect.Effect<
   RateLimiter.RateLimiter,
