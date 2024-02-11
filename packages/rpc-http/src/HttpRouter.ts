ce0c2efb465096bca866b424a7e373d7a38ceceb
Tim <hello@timsmart.co>
2024-02-11 21:54:41 +1300

    add non-streaming handlers and resolvers to rpc (#2100)

--- packages/rpc-http/src/HttpRouter.ts
+++ packages/rpc-http/src/HttpRouter.ts
@@ -6,6 +6,7 @@ import type * as ServerError from "@effect/platform/Http/ServerError"
 import * as ServerRequest from "@effect/platform/Http/ServerRequest"
 import * as ServerResponse from "@effect/platform/Http/ServerResponse"
 import * as Router from "@effect/rpc/Router"
+import type { ParseError } from "@effect/schema/ParseResult"
 import * as Chunk from "effect/Chunk"
 import * as Effect from "effect/Effect"
 import * as Stream from "effect/Stream"
@@ -34,5 +35,21 @@ export const toHttpApp = <R extends Router.Router<any, any>>(self: R): App.Defau
         ),
         { contentType: "application/ndjson" }
       )
+  )
+}
+
+/**
+ * @since 1.0.0
+ * @category conversions
+ */
+export const toHttpAppEffect = <R extends Router.Router<any, any>>(self: R): App.Default<
+  Router.Router.Context<R>,
+  ServerError.RequestError | ParseError
+> => {
+  const handler = Router.toHandlerEffect(self)
+  return ServerRequest.ServerRequest.pipe(
+    Effect.flatMap((_) => _.json),
+    Effect.flatMap(handler),
+    Effect.map(ServerResponse.unsafeJson)
   )
 }
