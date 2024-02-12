1c6d18b422b0bd800f2ed036dba9cb78db296c03
Giulio Canti <giulio.canti@gmail.com>
2024-02-12 15:33:28 +0100

    Schema: add `transformation: false` option to `pluck` (#2108)

--- packages/schema/src/Schema.ts
+++ packages/schema/src/Schema.ts
@@ -1247,22 +1247,80 @@ export const omit = <A, Keys extends ReadonlyArray<keyof A>>(...keys: Keys) =>
 }
 
 /**
+ * Given a schema `Schema<A, I, R>` and a key `K`, this function extracts a specific field from the `A` type, producing a new schema that represents a transformation from the `I` type to `A[K]`.
+ *
+ * If the option `{ transformation: false }` is provided, the returned schema `Schema<A[K], I[K], R>` only represents the value of the field without any transformation.
+ *
+ * @example
+ * import * as S from "@effect/schema/Schema"
+ *
+ * // ---------------------------------------------
+ * // use case: pull out a single field from a
+ * // struct through a transformation
+ * // ---------------------------------------------
+ *
+ * const mytable = S.struct({
+ *   column1: S.NumberFromString,
+ *   column2: S.number
+ * })
+ *
+ * // const pullOutColumn1: S.Schema<number, {
+ * //     readonly column1: string;
+ * //     readonly column2: number;
+ * // }, never>
+ * const pullOutColumn1 = mytable.pipe(S.pluck("column1"))
+ *
+ * console.log(S.decode(S.array(pullOutColumn1))([{ column1: "1", column2: 100 }, { column1: "2", column2: 300 }]))
+ * // Output: { _id: 'Either', _tag: 'Right', right: [ 1, 2 ] }
+ *
+ * // ---------------------------------------------
+ * // use case: pull out a single field from a
+ * // struct (no transformation)
+ * // ---------------------------------------------
+ *
+ * // const pullOutColumn1Value: S.Schema<number, string, never>
+ * const pullOutColumn1Value = mytable.pipe(S.pluck("column1", { transformation: false }))
+ *
+ * console.log(S.decode(S.array(pullOutColumn1Value))(["1", "2"]))
+ * // Output: { _id: 'Either', _tag: 'Right', right: [ 1, 2 ] }
+ *
  * @category struct transformations
  * @since 1.0.0
  */
 export const pluck: {
+  <A, K extends keyof A>(
+    key: K,
+    options: { readonly transformation: false }
+  ): <I extends { [P in K]?: any }, R>(schema: Schema<A, I, R>) => Schema<A[K], I[K], R>
   <A, K extends keyof A>(key: K): <I, R>(schema: Schema<A, I, R>) => Schema<A[K], I, R>
+  <A, I extends { [P in K]?: any }, R, K extends keyof A>(
+    schema: Schema<A, I, R>,
+    key: K,
+    options: { readonly transformation: false }
+  ): Schema<A[K], I[K], R>
   <A, I, R, K extends keyof A>(schema: Schema<A, I, R>, key: K): Schema<A[K], I, R>
-} = dual(2, <A, I, R, K extends keyof A>(schema: Schema<A, I, R>, key: K): Schema<A[K], I, R> => {
-  const ps = AST.getPropertyKeyIndexedAccess(to(schema).ast, key)
-  const value = make<A[K], A[K], R>(ps.isOptional ? AST.createUnion([AST.undefinedKeyword, ps.type]) : ps.type)
-  return transform(
-    schema,
-    value,
-    (a) => a[key],
-    (ak) => ps.isOptional && ak === undefined ? {} : { [key]: ak } as any
-  )
-})
+} = dual(
+  (args) => isSchema(args[0]),
+  <A, I, R, K extends keyof A>(
+    schema: Schema<A, I, R>,
+    key: K,
+    options?: { readonly transformation: false }
+  ): Schema<A[K], I, R> => {
+    if (options && options.transformation == false) {
+      const ps = AST.getPropertyKeyIndexedAccess(schema.ast, key)
+      return make(ps.isOptional ? AST.createUnion([AST.undefinedKeyword, ps.type]) : ps.type)
+    } else {
+      const ps = AST.getPropertyKeyIndexedAccess(to(schema).ast, key)
+      const value = make<A[K], A[K], R>(ps.isOptional ? AST.createUnion([AST.undefinedKeyword, ps.type]) : ps.type)
+      return transform(
+        schema,
+        value,
+        (a) => a[key],
+        (ak) => ps.isOptional && ak === undefined ? {} : { [key]: ak } as any
+      )
+    }
+  }
+)
 
 /**
  * @category model
