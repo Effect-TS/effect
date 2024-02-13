8ddf2a1c0a095af5b5288ade00030a53ce0af01a
gcanti <giulio.canti@gmail.com>
2024-02-13 10:09:13 +0100

    add tests for unions and nested classes

--- packages/schema/test/Schema/Class.test.ts
+++ packages/schema/test/Schema/Class.test.ts
@@ -480,7 +480,15 @@ describe("Schema > Class", () => {
     )
   })
 
-  it("encode works with struct", () => {
+  it("encode works with struct", async () => {
     assert.doesNotThrow(() => S.encodeSync(Person)({ id: 1, name: "John" } as Person))
+    class A extends S.Class<A>()({
+      n: S.NumberFromString
+    }) {}
+    class B extends S.Class<B>()({
+      a: A
+    }) {}
+    await Util.expectEncodeSuccess(S.union(B, S.NumberFromString), 1, "1")
+    await Util.expectEncodeSuccess(B, { a: { n: 1 } }, { a: { n: "1" } })
   })
 })
