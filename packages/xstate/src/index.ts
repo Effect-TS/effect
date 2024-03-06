b633eb264fcd4a634389e17e2dd884d5b3a56684
Tim <hello@timsmart.co>
2024-03-06 17:09:34 +1300

    codegen

--- packages/xstate/src/index.ts
+++ packages/xstate/src/index.ts
@@ -6,14 +6,4 @@ export * as Actor from "./Actor.js"
 /**
  * @since 1.0.0
  */
-export * as Machine from "./Machine.js"
-
-/**
- * @since 1.0.0
- */
-export * as Procedure from "./Procedure.js"
-
-/**
- * @since 1.0.0
- */
 export * as Snapshot from "./Snapshot.js"
