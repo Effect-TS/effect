b9e2e028731bd6baec830f24507fe864cb1d3a2a
gcanti <giulio.canti@gmail.com>
2024-03-12 00:04:14 +0100

    refactor `propertySignature` and add `propertySignatureAnnotations`

---
+++ packages/schema/test/Schema/propertySignatureAnnotations.test.ts
@@ -0,0 +1,19 @@
+import * as AST from "@effect/schema/AST"
+import * as S from "@effect/schema/Schema"
+import { describe, expect, it } from "vitest"
+
+describe("Schema > propertySignatureAnnotations", () => {
+  it("should add property signature annotations to a schema", () => {
+    const schema = S.struct({
+      a: S.string.pipe(S.propertySignatureAnnotations({
+        title: "title",
+        [Symbol.for("custom-annotation")]: "custom-annotation-value"
+      }))
+    })
+    const ast: any = schema.ast
+    expect(ast.propertySignatures[0].annotations).toEqual({
+      [AST.TitleAnnotationId]: "title",
+      [Symbol.for("custom-annotation")]: "custom-annotation-value"
+    })
+  })
+})
