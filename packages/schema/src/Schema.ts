a33e6723cfdb7ffc24d37c30c7693b47ec8ccd8d
Tim <hello@timsmart.co>
2024-03-19 19:50:57 +1300

    Update packages/schema/src/Schema.ts
    Co-authored-by: Giulio Canti <giulio.canti@gmail.com>

--- packages/schema/src/Schema.ts
+++ packages/schema/src/Schema.ts
@@ -6191,7 +6191,7 @@ const makeClass = ({ Base, annotations, fields, fromSchema, identifier, kind, ta
   fromSchema?: Schema.Any | undefined
   tag?: { _tag: AST.LiteralValue } | undefined
   annotations?: Annotations.Schema<any> | undefined
-  toStringOverride?: (self: any) => string | undefined
+  toStringOverride?: ((self: any) => string) | undefined
 }): any => {
   const classSymbol = Symbol.for(`@effect/schema/${kind}/${identifier}`)
   const schema = fromSchema ?? struct(fields)
