487d486fef8f0ccbde9fb28a126e4abc72cc3174
Michael Arnaldi <michael.arnaldi@effectful.co>
2024-03-25 16:53:20 +0100

    improve test

--- packages/effect/test/Effect/query-repro.test.ts
+++ packages/effect/test/Effect/query-repro.test.ts
@@ -1,195 +1,46 @@
-import {
-  Cause,
-  Console,
-  Deferred,
-  Duration,
-  Effect,
-  Exit,
-  Fiber,
-  FiberId,
-  Layer,
-  ManagedRuntime,
-  Option,
-  RateLimiter,
-  ReadonlyArray,
-  Request,
-  RequestResolver,
-  Schedule
-} from "effect"
-import { inspect } from "util"
-import { it } from "vitest"
+import * as it from "effect-test/utils/extend"
+import * as Effect from "effect/Effect"
+import * as Layer from "effect/Layer"
+import * as Request from "effect/Request"
+import * as RequestResolver from "effect/RequestResolver"
+import { describe, expect } from "vitest"
 
-const logRequest = (name: string) => <A, E, R>(eff: Effect.Effect<A, E, R>) =>
-  Effect
-    .logInfo(`${name} [Start]`)
-    .pipe(
-      Effect.andThen(eff),
-      Effect.tapBoth({
-        onFailure: () => Effect.logInfo(`${name} [End fail]`),
-        onSuccess: () => Effect.logInfo(`${name} [End Success]`)
-      })
-    )
+export class FindIntraday extends Request.TaggedClass("FindIntraday")<string | null, never, { symbol: string }> {}
 
-function logCacheStats(c: Request.Cache, name: string) {
-  return Effect
-    .all({
-      stats: c.cacheStats,
-      entries: c.entries.pipe(
-        Effect.andThen(
-          Effect
-            .forEach(([k, v]) => Deferred.isDone(v.handle).pipe(Effect.andThen((isDone) => [k, isDone, v] as const)))
-        ),
-        Effect.andThen((entries) =>
-          entries.reduce((prev, cur) => {
-            return {
-              inflight: cur[1] ? prev.inflight : [...prev.inflight, cur[2]],
-              finished: cur[1] ? [...prev.finished, cur[2]] : prev.finished
-            }
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          }, { inflight: [] as Array<any>, finished: [] as Array<any> })
-        )
-      )
-    })
-    .pipe(Effect.andThen(({ entries, stats }) =>
-      Console
-        .log(
-          `$$$ cache stats [${name}]: ${
-            inspect(stats)
-          }. ${entries.finished.length} done, ${entries.inflight.length} in flight.`
-        )
-        .pipe(Effect.andThen(
-          entries
-              .inflight
-              .length
-            ? Console.log(`in-flight requests: ${JSON.stringify(entries.inflight, undefined, 2)}`)
-            : Effect.unit
-        ))
-    ))
-}
-
-export class FindIntraday extends Request.TaggedClass("FindIntraday")<
-  string | null,
-  never,
-  {
-    symbol: string
-  }
-> {}
-
-const make = Effect.gen(function*($) {
-  const rateLimit = yield* $(RateLimiter.make({ limit: 3, algorithm: "fixed-window", interval: Duration.seconds(1) }))
-  const rateLimit2 = yield* $(RateLimiter.make({ limit: 10, algorithm: "fixed-window", interval: Duration.seconds(1) }))
-
-  const intradayCache = yield* $(
-    Request.makeCache({ capacity: 500, timeToLive: "8 hours" }).pipe(
-      Effect.tap((c) =>
-        logCacheStats(c, "intraday2").pipe(
-          Effect.schedule(Schedule.spaced("15 seconds")),
-          Effect.interruptible,
-          Effect.forkScoped
-        )
-      )
-    )
+const make = Effect.sync(function() {
+  const getIntradayResolver = RequestResolver.makeBatched((requests: Array<FindIntraday>) =>
+    Effect.all(requests.map(Request.succeed(null)))
   )
 
-  const FindIntradayRequestResolver = RequestResolver
-    .makeBatched((requests: Array<FindIntraday>) =>
-      Effect
-        .delay(Effect.succeed([] as Array<string>), Duration.millis(25))
-        .pipe(rateLimit)
-        .pipe(
-          Effect
-            .andThen((intradays) =>
-              Effect.forEach(requests, (r) =>
-                Request.complete(
-                  r,
-                  Exit.succeed(
-                    ReadonlyArray
-                      .findFirst(intradays, (_) => _ === r.symbol)
-                      .pipe(Option.getOrNull)
-                  )
-                ), { discard: true })
-            ),
-          Effect.catchAllCause((cause) => {
-            console.log("catch all ", cause)
-            return Effect.forEach(requests, Request.complete(Exit.failCause(cause)), { discard: true })
-          }),
-          Effect.onInterrupt(() => {
-            console.log("oninterrupt ")
-            return Effect.forEach(requests, Request.complete(Exit.failCause(Cause.interrupt(FiberId.none))), {
-              discard: true
-            })
-          })
-        )
-    )
-    .pipe(
-      RequestResolver.batchN(100)
-    )
-
   const getIntraday = (symbol: string) =>
-    Effect
-      .request(new FindIntraday({ symbol }), FindIntradayRequestResolver)
-      .pipe(
-        Effect.withRequestCaching(true),
-        Effect.withRequestCache(intradayCache),
-        Effect.withRequestBatching(true),
-        logRequest(`MarketStackApiCache: getIntraDay#${symbol}`)
-      )
+    Effect.withRequestCaching(true)(
+      Effect.request(new FindIntraday({ symbol }), getIntradayResolver)
+    )
 
-  return { rateLimit, rateLimit2, getIntraday }
+  return { getIntraday }
 })
 
-export class Svc extends Effect.Tag("svc")<Svc, Effect.Effect.Success<typeof make>>() {
+class Svc extends Effect.Tag("svc")<Svc, Effect.Effect.Success<typeof make>>() {
   static readonly Live = Layer.scoped(Svc, make)
 }
 
 const getSub = (symbol: string) =>
-  Effect
-    .gen(function*($) {
-      const { getIntraday, rateLimit2 } = yield* $(Svc)
-      yield* $(
-        Effect
-          .all([
-            Effect
-              .delay(Effect.succeed(null), Duration.millis(25))
-              .pipe(rateLimit2),
-            getIntraday(symbol)
-          ], { concurrency: "inherit", batching: true })
-      )
-    })
+  Effect.all([
+    Effect.sleep("20 millis"),
+    Svc.getIntraday(symbol)
+  ], { concurrency: 2, batching: true })
 
-export const getItems = Effect
-  .forEach(
-    ReadonlyArray.range(1, 50).map((_) => `test_${_}`),
-    (symbol) => getSub(symbol),
-    {
-      concurrency: "inherit",
-      batching: true
-    }
-  )
+const getItems = getSub("test_1")
 
-const rt = ManagedRuntime.make(Svc.Live)
-
-it(
-  "help",
-  () =>
-    rt.runPromise(
-      Effect
-        .gen(function*($) {
-          const r1 = yield* $(Effect.fork(getItems))
-
-          yield* $(Effect.sleep("1 seconds"))
-          yield* $(Fiber.interrupt(r1))
-
-          const r2 = yield* $(Effect.fork(getItems))
-
-          yield* $(Effect.sleep("2 seconds"))
-          yield* $(Fiber.interrupt(r2))
-
-          yield* $(Effect.sleep("30 seconds"))
-          // during this sleep we will get cache dump with many listeners and non finished requests.
-
-          yield* $(getItems)
-        })
-    ),
-  { timeout: 60_000 }
-)
+describe("interruption", () => {
+  it.live("forEach interrupts residual requests", () =>
+    Effect.gen(function*($) {
+      const exit = yield* $(getItems.pipe(
+        Effect.timeout("10 millis"),
+        Effect.catchAll(() => getItems),
+        Effect.provide(Svc.Live),
+        Effect.exit
+      ))
+      expect(exit._tag).toEqual("Success")
+    }))
+})
