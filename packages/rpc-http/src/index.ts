b3bd604d97210b32da85268c9bf3f4ca496c4201
Tim <hello@timsmart.co>
2024-04-03 10:18:24 +1300

    codegen

--- packages/rpc-http/src/index.ts
+++ packages/rpc-http/src/index.ts
@@ -6,4 +6,9 @@ export * as HttpResolver from "./HttpResolver.js"
 /**
  * @since 1.0.0
  */
+export * as HttpResolverNoStream from "./HttpResolverNoStream.js"
+
+/**
+ * @since 1.0.0
+ */
 export * as HttpRouter from "./HttpRouter.js"
