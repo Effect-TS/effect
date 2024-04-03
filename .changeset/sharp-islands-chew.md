3c38fb6607279dbda76f484c28d26bfed7263228
Jesse Kelly <kellyjesse881@gmail.com>
2024-04-03 12:29:44 -0600

    make Logger.structuredLogger default for browser

---
+++ .changeset/sharp-islands-chew.md
@@ -0,0 +1,5 @@
+---
+"effect": patch
+---
+
+made Logger.structuredLogger the default for Browser.runMain
--- packages/platform-browser/src/internal/runtime.ts
+++ packages/platform-browser/src/internal/runtime.ts
@@ -1,16 +1,19 @@
 import type { RunMain } from "@effect/platform/Runtime"
 import * as Cause from "effect/Cause"
 import * as Effect from "effect/Effect"
+import * as Logger from "effect/Logger"
 
 /** @internal */
 export const runMain: RunMain = (
   effect,
   options
 ) => {
+  const _effect = Effect.provide(effect, Logger.structured);
+
   const fiber = Effect.runFork(
     options?.disableErrorReporting === true ?
-      effect :
-      Effect.tapErrorCause(effect, (cause) => {
+      _effect :
+      Effect.tapErrorCause(_effect, (cause) => {
         if (Cause.isInterruptedOnly(cause)) {
           return Effect.unit
         }
