4b4ad69659e613dec7793b43a592758f59700832
Jesse Kelly <kellyjesse881@gmail.com>
2024-04-04 12:28:16 -0600

    fixed logger replacement

--- packages/platform-browser/src/internal/runtime.ts
+++ packages/platform-browser/src/internal/runtime.ts
@@ -2,14 +2,28 @@ import type { RunMain } from "@effect/platform/Runtime"
 import * as Cause from "effect/Cause"
 import * as Effect from "effect/Effect"
 import * as Logger from "effect/Logger"
+import * as Layer from "effect/Layer"
+import * as HashSet from "effect/HashSet"
+import * as FiberRef from "effect/FiberRef"
+
+/** @internal */
+const useStructuredLogger = Layer.fiberRefLocallyScopedWith(FiberRef.currentLoggers, (loggers) =>  {
+  if(HashSet.has(loggers, Logger.defaultLogger)) {
+    const set = HashSet.remove(loggers, Logger.defaultLogger);
+    return HashSet.add(set, Logger.structuredLogger)
+  }
+
+  return loggers;
+})
+
 
 /** @internal */
 export const runMain: RunMain = (
   effect,
   options
 ) => {
-  const _effect = Effect.provide(effect, Logger.structured)
-
+  const _effect = Effect.provide(effect, useStructuredLogger)
+  
   const fiber = Effect.runFork(
     options?.disableErrorReporting === true ?
       _effect :
