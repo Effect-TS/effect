89413286d34d9a9d6d5c2a97b9bbab2d82167994
Jesse Kelly <kellyjesse881@gmail.com>
2024-04-04 17:59:45 -0600

    make Logger.structuredLogger default for browser (#2466)
    Co-authored-by: Tim <hello@timsmart.co>

--- packages/platform-browser/src/internal/runtime.ts
+++ packages/platform-browser/src/internal/runtime.ts
@@ -1,16 +1,31 @@
 import type { RunMain } from "@effect/platform/Runtime"
 import * as Cause from "effect/Cause"
 import * as Effect from "effect/Effect"
+import * as FiberRef from "effect/FiberRef"
+import * as HashSet from "effect/HashSet"
+import * as Logger from "effect/Logger"
+
+/** @internal */
+const useStructuredLogger = Effect.locallyWith(FiberRef.currentLoggers, (loggers) => {
+  if (HashSet.has(loggers, Logger.defaultLogger)) {
+    const set = HashSet.remove(loggers, Logger.defaultLogger)
+    return HashSet.add(set, Logger.withConsoleLog(Logger.structuredLogger))
+  }
+
+  return loggers
+})
 
 /** @internal */
 export const runMain: RunMain = (
   effect,
   options
 ) => {
+  const _effect = useStructuredLogger(effect)
+
   const fiber = Effect.runFork(
     options?.disableErrorReporting === true ?
-      effect :
-      Effect.tapErrorCause(effect, (cause) => {
+      _effect :
+      Effect.tapErrorCause(_effect, (cause) => {
         if (Cause.isInterruptedOnly(cause)) {
           return Effect.unit
         }
