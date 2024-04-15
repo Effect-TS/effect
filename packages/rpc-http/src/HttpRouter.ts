580d068331e946273301b30297823021eb71b84d
Tim <hello@timsmart.co>
2024-04-15 16:55:18 +1200

    ensure context is available in rpc http stream

--- packages/rpc-http/src/HttpRouter.ts
+++ packages/rpc-http/src/HttpRouter.ts
@@ -7,7 +7,9 @@ import * as ServerRequest from "@effect/platform/Http/ServerRequest"
 import * as ServerResponse from "@effect/platform/Http/ServerResponse"
 import * as Router from "@effect/rpc/Router"
 import * as Chunk from "effect/Chunk"
+import * as Context from "effect/Context"
 import * as Effect from "effect/Effect"
+import * as FiberRef from "effect/FiberRef"
 import * as Stream from "effect/Stream"
 
 /**
@@ -19,20 +21,19 @@ export const toHttpApp = <R extends Router.Router<any, any>>(self: R): App.Defau
   ServerError.RequestError
 > => {
   const handler = Router.toHandler(self)
-  return Effect.map(
-    Effect.flatMap(
-      ServerRequest.ServerRequest,
-      (request) => request.json
-    ),
-    (_) =>
+  return Effect.withFiberRuntime((fiber) => {
+    const context = fiber.getFiberRef(FiberRef.currentContext)
+    const request = Context.unsafeGet(context, ServerRequest.ServerRequest)
+    return Effect.map(request.json, (_) =>
       ServerResponse.stream(
         handler(_).pipe(
           Stream.chunks,
           Stream.map((_) => JSON.stringify(Chunk.toArray(_))),
           Stream.intersperse("\n"),
-          Stream.encodeText
+          Stream.encodeText,
+          Stream.provideContext(context)
         ),
         { contentType: "application/ndjson" }
-      )
-  )
+      ))
+  })
 }
