a1c7ab8ffedacd18c1fc784f4ff5844f79498b83
Jesse Kelly <kellyjesse881@gmail.com>
2024-04-16 18:11:56 -0600

    added Stream.fromEventListener (#2513)
    Co-authored-by: Tim <hello@timsmart.co>

---
+++ packages/effect/test/Stream/fromEventListener.test.ts
@@ -0,0 +1,23 @@
+import { Effect, Stream } from "effect"
+import * as it from "effect-test/utils/extend"
+import { describe } from "vitest"
+
+class TestTarget extends EventTarget {
+  emit() {
+    this.dispatchEvent(new Event("test-event"))
+  }
+}
+
+describe("Stream.fromEventListener", () => {
+  it.effect("emitted count", (ctx) =>
+    Effect.gen(function*(_) {
+      const target = new TestTarget()
+
+      const count = yield* _(
+        Stream.fromEventListener(target, "test-event"),
+        Stream.interruptWhen(Effect.sync(() => target.emit()).pipe(Effect.repeatN(2))),
+        Stream.runCount
+      )
+      ctx.expect(count).toEqual(3)
+    }))
+})
