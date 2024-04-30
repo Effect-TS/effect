060f5add5324ee7994cef33b4e224c9180509c52
RareBodhi <bodhi@looksrare.org>
2024-04-30 19:48:10 +0700

    Rm chunk differenceOption methods

--- packages/effect/test/Chunk.test.ts
+++ packages/effect/test/Chunk.test.ts
@@ -19,7 +19,6 @@ describe("Chunk", () => {
     expect(Chunk.containsWith).exist
     expect(Chunk.difference).exist
     expect(Chunk.differenceWith).exist
-    expect(Chunk.differenceOption).exist
     expect(Chunk.findFirst).exist
     expect(Chunk.findFirstIndex).exist
     expect(Chunk.findLast).exist
@@ -897,17 +896,5 @@ describe("Chunk", () => {
     expect(Chunk.difference(Chunk.empty(), curr)).toEqual(curr)
     expect(Chunk.difference(curr, Chunk.empty())).toEqual(Chunk.empty())
     expect(Chunk.difference(curr, curr)).toEqual(Chunk.empty())
-  })
-
-  it("differenceOption", () => {
-    const curr = Chunk.make(1, 3, 5, 7, 9)
-
-    const { none, some } = Option
-
-    expect(Chunk.differenceOption(some(Chunk.make(1, 2, 3, 4, 5)), curr)).toEqual(Chunk.make(7, 9))
-    expect(Chunk.differenceOption(some(Chunk.empty()), curr)).toEqual(curr)
-    expect(Chunk.differenceOption(none(), curr)).toEqual(curr)
-    expect(Chunk.differenceOption(some(curr), Chunk.empty())).toEqual(Chunk.empty())
-    expect(Chunk.differenceOption(some(curr), curr)).toEqual(Chunk.empty())
   })
 })
