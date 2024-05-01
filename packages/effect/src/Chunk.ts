f58bdfb7d11ef1921470ba989755ed209c411220
RareBodhi <bodhi@looksrare.org>
2024-05-01 09:17:02 +0700

    Changeset

--- packages/effect/src/Chunk.ts
+++ packages/effect/src/Chunk.ts
@@ -1392,7 +1392,7 @@ export const reduceRight: {
  * Creates a `Chunk` of values not included in the other given `Chunk` using the provided `isEquivalent` function.
  * The order and references of result values are determined by the first `Chunk`.
  *
- * @since 2.0.0
+ * @since 3.2.0
  */
 export const differenceWith = <A>(isEquivalent: (self: A, that: A) => boolean): {
   (that: Chunk<A>): (self: Chunk<A>) => Chunk<A>
