8d00cdcfd28059f92d3967e860f66ed54e668baa
Maxim Khramtsov <khraks.mamtsov@gmail.com>
2024-05-03 13:06:46 +0200

    Add do notation for Array (#2678)
    Co-authored-by: maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
    Co-authored-by: Tim <hello@timsmart.co>
    Co-authored-by: Giulio Canti <giulio.canti@gmail.com>

--- packages/effect/test/Array.test.ts
+++ packages/effect/test/Array.test.ts
@@ -1,4 +1,5 @@
 import { deepStrictEqual, double, strictEqual } from "effect-test/util"
+import * as Util from "effect-test/util"
 import * as RA from "effect/Array"
 import * as E from "effect/Either"
 import { identity, pipe } from "effect/Function"
@@ -1221,5 +1222,27 @@ describe("ReadonlyArray", () => {
     }
     const arr: ReadonlyArray<X> = [{ a: "a", b: 2 }, { a: "b", b: 1 }]
     expect(RA.sortWith(arr, (x) => x.b, Order.number)).toEqual([{ a: "b", b: 1 }, { a: "a", b: 2 }])
+  })
+
+  it("Do notation", () => {
+    const _do = RA.Do
+    Util.deepStrictEqual(_do, RA.of({}))
+
+    const doA = RA.bind(_do, "a", () => ["a"])
+    Util.deepStrictEqual(doA, RA.of({ a: "a" }))
+
+    const doAB = RA.bind(doA, "b", (x) => ["b", x.a + "b"])
+    Util.deepStrictEqual(doAB, [
+      { a: "a", b: "b" },
+      { a: "a", b: "ab" }
+    ])
+    const doABC = RA.let(doAB, "c", (x) => [x.a, x.b, x.a + x.b])
+    Util.deepStrictEqual(doABC, [
+      { a: "a", b: "b", c: ["a", "b", "ab"] },
+      { a: "a", b: "ab", c: ["a", "ab", "aab"] }
+    ])
+
+    const doABCD = RA.bind(doABC, "d", () => RA.empty())
+    Util.deepStrictEqual(doABCD, [])
   })
 })
