bbd6aeca3da1bf37e173821cfb58b2468119ceb9
Giulio Canti <giulio.canti@gmail.com>
2024-05-15 11:36:32 +0200

    `pick`: do not return a `ComposeTransformation` if none of the picked… (#2746)

--- packages/schema/test/AST/pick.test.ts
+++ packages/schema/test/AST/pick.test.ts
@@ -1,4 +1,4 @@
-import type * as AST from "@effect/schema/AST"
+import * as AST from "@effect/schema/AST"
 import * as S from "@effect/schema/Schema"
 import { describe, expect, it } from "vitest"
 
@@ -19,12 +19,26 @@ describe("pick", () => {
       expect(ast).toStrictEqual(S.compose(S.Struct({ a: S.NumberFromString }), S.Struct({ a: S.Number })).ast)
     })
 
-    it("TypeLiteralTransformation", async () => {
-      const schema = S.Struct({ a: S.optional(S.NumberFromString, { default: () => 0 }), b: S.Number })
-      const ast = schema.pipe(S.pick("a")).ast as AST.Transformation
-      expect(ast.from).toStrictEqual(S.Struct({ a: S.optional(S.NumberFromString) }).ast)
-      expect(ast.to).toStrictEqual(S.Struct({ a: S.Number }).ast)
-      expect(ast.transformation).toStrictEqual((schema.ast as AST.Transformation).transformation)
+    describe("TypeLiteralTransformation", () => {
+      it("picking keys with associated PropertySignatureTransformations", async () => {
+        const schema = S.Struct({ a: S.optional(S.NumberFromString, { default: () => 0 }), b: S.Number })
+        const pick = schema.pipe(S.pick("a"))
+        const ast = pick.ast as AST.Transformation
+        expect(ast.from).toStrictEqual(S.Struct({ a: S.optional(S.NumberFromString) }).ast)
+        expect(ast.to).toStrictEqual(S.Struct({ a: S.Number }).ast)
+        expect(ast.transformation).toStrictEqual((schema.ast as AST.Transformation).transformation)
+        expect(() => pick.pipe(S.extend(S.Struct({ c: S.Boolean })))).not.Throw()
+      })
+
+      it("picking keys without associated PropertySignatureTransformations", async () => {
+        const schema = S.Struct({ a: S.optional(S.NumberFromString, { default: () => 0 }), b: S.NumberFromString })
+        const pick = schema.pipe(S.pick("b"))
+        const ast = pick.ast as AST.TypeLiteral
+        expect(ast.propertySignatures).toStrictEqual([
+          new AST.PropertySignature("b", S.NumberFromString.ast, false, true)
+        ])
+        expect(() => pick.pipe(S.extend(S.Struct({ c: S.Boolean })))).not.Throw()
+      })
     })
 
     it("with SurrogateAnnotation", async () => {
