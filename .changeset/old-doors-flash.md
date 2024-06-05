e51036b09f69ed987dfa42867bb9fe359bc065ee
Tim <hello@timsmart.co>
2024-06-05 16:20:56 +1200

    parse URL instances when creating client requests (#2924)

---
+++ .changeset/old-doors-flash.md
@@ -0,0 +1,5 @@
+---
+"@effect/platform": minor
+---
+
+parse URL instances when creating client requests
--- packages/platform/test/Http/UrlParams.test.ts
+++ packages/platform/test/Http/UrlParams.test.ts
@@ -1,12 +1,12 @@
 import * as UrlParams from "@effect/platform/Http/UrlParams"
 import { assert, describe, it } from "@effect/vitest"
-import { Effect } from "effect"
+import { Effect, Option } from "effect"
 
 describe("UrlParams", () => {
   describe("makeUrl", () => {
     it.effect("makes a URL", () =>
       Effect.gen(function*(_) {
-        const url = yield* _(UrlParams.makeUrl("https://example.com/test", []))
+        const url = yield* _(UrlParams.makeUrl("https://example.com/test", [], Option.none()))
         assert.strictEqual(url.toString(), "https://example.com/test")
       }))
 
@@ -18,7 +18,7 @@ describe("UrlParams", () => {
           origin: "https://example.com",
           pathname: "/path/"
         } as Location
-        const url = yield* _(UrlParams.makeUrl("test", []))
+        const url = yield* _(UrlParams.makeUrl("test", [], Option.none()))
         assert.strictEqual(url.toString(), "https://example.com/path/test")
 
         globalThis.location = originalLocation
@@ -31,13 +31,13 @@ describe("UrlParams", () => {
         // `globalThis.location` is undefined
         // @ts-expect-error
         globalThis.location = undefined
-        let url = yield* _(UrlParams.makeUrl("https://example.com", []))
+        let url = yield* _(UrlParams.makeUrl("https://example.com", [], Option.none()))
         assert.strictEqual(url.toString(), "https://example.com/")
 
         // `location` is not in globalThis
         // @ts-expect-error
         delete globalThis.location
-        url = yield* _(UrlParams.makeUrl("http://example.com", []))
+        url = yield* _(UrlParams.makeUrl("http://example.com", [], Option.none()))
         assert.strictEqual(url.toString(), "http://example.com/")
 
         globalThis.location = originalLocation
@@ -48,21 +48,21 @@ describe("UrlParams", () => {
         const originalLocation = globalThis.location
 
         globalThis.location = { href: "" } as Location
-        const url1 = yield* _(UrlParams.makeUrl("https://example.com", []))
+        const url1 = yield* _(UrlParams.makeUrl("https://example.com", [], Option.none()))
         assert.strictEqual(url1.toString(), "https://example.com/")
 
         globalThis.location = {
           href: "",
           origin: "https://example.com"
         } as unknown as Location
-        const url2 = yield* _(UrlParams.makeUrl("https://example.com", []))
+        const url2 = yield* _(UrlParams.makeUrl("https://example.com", [], Option.none()))
         assert.strictEqual(url2.toString(), "https://example.com/")
 
         globalThis.location = {
           href: "",
           pathname: "example_path"
         } as unknown as Location
-        const url3 = yield* _(UrlParams.makeUrl("https://example.com", []))
+        const url3 = yield* _(UrlParams.makeUrl("https://example.com", [], Option.none()))
         assert.strictEqual(url3.toString(), "https://example.com/")
 
         globalThis.location = originalLocation
