e3178457de4248cf39ddbe9ddede900dcf6a5700
Giulio Canti <giulio.canti@gmail.com>
2024-06-06 19:41:39 +0200

    Redacted: add JSDocs

--- packages/effect/src/index.ts
+++ packages/effect/src/index.ts
@@ -615,6 +615,11 @@ export * as Record from "./Record.js"
 export * as RedBlackTree from "./RedBlackTree.js"
 
 /**
+ * The Redacted module provides functionality for handling sensitive information
+ * securely within your application. By using the `Redacted` data type, you can
+ * ensure that sensitive values are not accidentally exposed in logs or error
+ * messages.
+ *
  * @since 3.3.0
  */
 export * as Redacted from "./Redacted.js"
