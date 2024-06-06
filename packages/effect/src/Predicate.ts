7f9ae9114f1c34d2ffd827d1790e38fba18bbc46
Giulio Canti <giulio.canti@gmail.com>
2024-06-06 12:41:59 +0200

    Add documentation for `Predicate.implies`

--- packages/effect/src/Predicate.ts
+++ packages/effect/src/Predicate.ts
@@ -799,15 +799,55 @@ export const eqv: {
 } = dual(2, <A>(self: Predicate<A>, that: Predicate<A>): Predicate<A> => (a) => self(a) === that(a))
 
 /**
+ * Represents the logical implication combinator for predicates. In formal
+ * logic, the implication operator `->` denotes that if the first proposition
+ * (antecedent) is true, then the second proposition (consequent) must also be
+ * true. In simpler terms, `p implies q` can be interpreted as "if p then q". If
+ * the first predicate holds, then the second predicate must hold
+ * for the given context.
+ *
+ * In practical terms within TypeScript, `p implies q` is equivalent to `!p || (p && q)`.
+ *
+ * Note that if the antecedent is `false`, the result is `true` by default
+ * because the outcome of the consequent cannot be determined.
+ *
+ * This function is useful in situations where you need to enforce rules or
+ * constraints that are contingent on certain conditions.
+ * It proves especially helpful in defining property tests.
+ *
+ * The example below illustrates the transitive property of order using the
+ * `implies` function. In simple terms, if `a <= b` and `b <= c`, then `a <= c`
+ * must be true.
+ *
+ * @example
+ * import { Predicate } from "effect"
+ *
+ * type Triple = {
+ *   readonly a: number
+ *   readonly b: number
+ *   readonly c: number
+ * }
+ *
+ * const transitivity = Predicate.implies(
+ *   // antecedent
+ *   (input: Triple) => input.a <= input.b && input.b <= input.c,
+ *   // consequent
+ *   (input: Triple) => input.a <= input.c
+ * )
+ *
+ * assert.equal(transitivity({ a: 1, b: 2, c: 3 }), true)
+ * // antecedent is `false`, so the result is `true`
+ * assert.equal(transitivity({ a: 1, b: 0, c: 0 }), true)
+ *
  * @category combinators
  * @since 2.0.0
  */
 export const implies: {
-  <A>(that: Predicate<A>): (self: Predicate<A>) => Predicate<A>
-  <A>(self: Predicate<A>, that: Predicate<A>): Predicate<A>
+  <A>(consequent: Predicate<A>): (antecedent: Predicate<A>) => Predicate<A>
+  <A>(antecedent: Predicate<A>, consequent: Predicate<A>): Predicate<A>
 } = dual(
   2,
-  <A>(self: Predicate<A>, that: Predicate<A>): Predicate<A> => (a) => self(a) ? that(a) : true
+  <A>(antecedent: Predicate<A>, consequent: Predicate<A>): Predicate<A> => (a) => antecedent(a) ? consequent(a) : true
 )
 
 /**
