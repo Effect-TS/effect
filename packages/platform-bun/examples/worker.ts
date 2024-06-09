de791d750787ebd29b54e30db6ac8a33dbc2c944
Tim <hello@timsmart.co>
2024-06-10 09:56:30 +1200

    switch to class style tag

--- packages/platform-bun/examples/worker.ts
+++ packages/platform-bun/examples/worker.ts
@@ -2,33 +2,29 @@ import { Worker } from "@effect/platform"
 import { BunRuntime, BunWorker } from "@effect/platform-bun"
 import { Console, Context, Effect, Layer, Stream } from "effect"
 import * as OS from "node:os"
-import * as path from "node:path"
 
-interface MyWorkerPool {
-  readonly _: unique symbol
-}
+class MyWorkerPool extends Context.Tag("@app/MyWorkerPool")<
+  MyWorkerPool,
+  Worker.WorkerPool<number, never, number>
+>() {}
 
-const Pool = Context.GenericTag<MyWorkerPool, Worker.WorkerPool<number, never, number>>("@app/MyWorkerPool")
-const workerPath = path.resolve(__dirname, "./worker/range.ts")
-const PoolLive = Worker.makePoolLayer(Pool, {
+const PoolLive = Worker.makePoolLayer(MyWorkerPool, {
   size: OS.availableParallelism()
 }).pipe(
-  Layer.provide(BunWorker.layer(() => new globalThis.Worker(workerPath)))
+  Layer.provide(BunWorker.layer(() => new globalThis.Worker(`${__dirname}/worker/range.ts`)))
 )
 
-Effect.gen(function*(_) {
-  const pool = yield* _(Pool)
-  yield* _(
-    Effect.all([
-      pool.execute(5).pipe(
-        Stream.runForEach((_) => Console.log("worker 1", _))
-      ),
-      pool.execute(10).pipe(
-        Stream.runForEach((_) => Console.log("worker 2", _))
-      ),
-      pool.execute(15).pipe(
-        Stream.runForEach((_) => Console.log("worker 3", _))
-      )
-    ], { concurrency: "inherit" })
-  )
+Effect.gen(function*() {
+  const pool = yield* MyWorkerPool
+  yield* Effect.all([
+    pool.execute(5).pipe(
+      Stream.runForEach((_) => Console.log("worker 1", _))
+    ),
+    pool.execute(10).pipe(
+      Stream.runForEach((_) => Console.log("worker 2", _))
+    ),
+    pool.execute(15).pipe(
+      Stream.runForEach((_) => Console.log("worker 3", _))
+    )
+  ], { concurrency: "inherit" })
 }).pipe(Effect.provide(PoolLive), BunRuntime.runMain)
