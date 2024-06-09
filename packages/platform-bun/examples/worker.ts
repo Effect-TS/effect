639109691ff8339999952da9f52502fc169b55d2
datapedd <datapedd@proton.me>
2024-06-09 14:21:14 +0200

    fix for workspaces

--- packages/platform-bun/examples/worker.ts
+++ packages/platform-bun/examples/worker.ts
@@ -2,15 +2,18 @@ import { Worker } from "@effect/platform"
 import { BunRuntime, BunWorker } from "@effect/platform-bun"
 import { Console, Context, Effect, Layer, Stream } from "effect"
 import * as OS from "node:os"
+import * as path from "node:path"
 
 interface MyWorkerPool {
   readonly _: unique symbol
 }
+
 const Pool = Context.GenericTag<MyWorkerPool, Worker.WorkerPool<number, never, number>>("@app/MyWorkerPool")
+const workerPath = path.resolve(__dirname, "./worker/range.ts")
 const PoolLive = Worker.makePoolLayer(Pool, {
   size: OS.availableParallelism()
 }).pipe(
-  Layer.provide(BunWorker.layer(() => new globalThis.Worker("./examples/worker/range.ts")))
+  Layer.provide(BunWorker.layer(() => new globalThis.Worker(workerPath)))
 )
 
 Effect.gen(function*(_) {
