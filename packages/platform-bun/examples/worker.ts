49c6de3e125cdb4b73fcec4bf637552dad92b093
datapedd <datapedd@proton.me>
2024-06-09 23:56:54 +0200

    update example fix for workspaces (#2963)
    Co-authored-by: Tim <hello@timsmart.co>

--- packages/platform-bun/examples/worker.ts
+++ packages/platform-bun/examples/worker.ts
@@ -3,29 +3,28 @@ import { BunRuntime, BunWorker } from "@effect/platform-bun"
 import { Console, Context, Effect, Layer, Stream } from "effect"
 import * as OS from "node:os"
 
-interface MyWorkerPool {
-  readonly _: unique symbol
-}
-const Pool = Context.GenericTag<MyWorkerPool, Worker.WorkerPool<number, never, number>>("@app/MyWorkerPool")
-const PoolLive = Worker.makePoolLayer(Pool, {
+class MyWorkerPool extends Context.Tag("@app/MyWorkerPool")<
+  MyWorkerPool,
+  Worker.WorkerPool<number, never, number>
+>() {}
+
+const PoolLive = Worker.makePoolLayer(MyWorkerPool, {
   size: OS.availableParallelism()
 }).pipe(
-  Layer.provide(BunWorker.layer(() => new globalThis.Worker("./examples/worker/range.ts")))
+  Layer.provide(BunWorker.layer(() => new globalThis.Worker(`${__dirname}/worker/range.ts`)))
 )
 
-Effect.gen(function*(_) {
-  const pool = yield* _(Pool)
-  yield* _(
-    Effect.all([
-      pool.execute(5).pipe(
-        Stream.runForEach((_) => Console.log("worker 1", _))
-      ),
-      pool.execute(10).pipe(
-        Stream.runForEach((_) => Console.log("worker 2", _))
-      ),
-      pool.execute(15).pipe(
-        Stream.runForEach((_) => Console.log("worker 3", _))
-      )
-    ], { concurrency: "inherit" })
-  )
+Effect.gen(function*() {
+  const pool = yield* MyWorkerPool
+  yield* Effect.all([
+    pool.execute(5).pipe(
+      Stream.runForEach((_) => Console.log("worker 1", _))
+    ),
+    pool.execute(10).pipe(
+      Stream.runForEach((_) => Console.log("worker 2", _))
+    ),
+    pool.execute(15).pipe(
+      Stream.runForEach((_) => Console.log("worker 3", _))
+    )
+  ], { concurrency: "inherit" })
 }).pipe(Effect.provide(PoolLive), BunRuntime.runMain)
