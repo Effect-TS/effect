a26aea024a976452f30beb56a2f1ab48a5cc9958
ecyrbe <ecyrbe@gmail.com>
2024-06-19 23:10:08 +0200

    refactor(kysely): use common constructors

--- packages/sql-kysely/src/Mysql.ts
+++ packages/sql-kysely/src/Mysql.ts
@@ -2,14 +2,14 @@
  * @since 1.0.0
  */
 import { DummyDriver, MysqlAdapter, MysqlIntrospector, MysqlQueryCompiler } from "kysely"
-import { make as makeKysely } from "./internal/kysely.js"
+import { makeFromSql } from "./internal/kysely.js"
 
 /**
  * @since 1.0.0
  * @category constructors
  */
 export const make = <DB>() =>
-  makeKysely<DB>({
+  makeFromSql<DB>({
     createAdapter: () => new MysqlAdapter(),
     createDriver: () => new DummyDriver(),
     createIntrospector: (db) => new MysqlIntrospector(db),
