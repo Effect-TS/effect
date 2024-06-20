cdfcd746499ca291bdc2919e76e9b56632ec0eae
ecyrbe <ecyrbe@gmail.com>
2024-06-21 00:25:04 +0200

    feat(kysely): add support for plugins

--- packages/sql-kysely/src/Kysely.ts
+++ packages/sql-kysely/src/Kysely.ts
@@ -1,21 +1,12 @@
 /**
  * @since 1.0.0
  */
-import * as Context from "effect/Context"
-import * as Effect from "effect/Effect"
-import type { Dialect } from "kysely"
-import { makeFromDialect } from "./internal/kysely.js"
-
-/**
- * @since 1.0.0
- * @category tags
- */
-export class KyselyDialect extends Context.Tag("KyselyDialect")<KyselyDialect, Dialect>() {}
+import { makeWithExecute } from "./internal/kysely.js"
 
 /**
  * @since 1.0.0
  * @category constructors
  */
-export const make = <DB>() => Effect.map(KyselyDialect, makeFromDialect<DB>)
+export const make = makeWithExecute
 
 export type * from "./patch.types.js"
