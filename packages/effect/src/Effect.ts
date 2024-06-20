700312ff4c8677368510b00887d019aad848d3af
Laure Retru-Chavastel <laure.retruchavastel@gmail.com>
2024-06-20 03:36:30 +0200

    Add `Effect.liftPredicate` (#2956)
    Co-authored-by: Tim <hello@timsmart.co>

--- packages/effect/src/Effect.ts
+++ packages/effect/src/Effect.ts
@@ -2160,6 +2160,40 @@ export const uninterruptibleMask: <A, E, R>(
 ) => Effect<A, E, R> = core.uninterruptibleMask
 
 // -------------------------------------------------------------------------------------
+// lifting
+// -------------------------------------------------------------------------------------
+
+/**
+ * Transforms a `Predicate` function into an `Effect` returning the input value if the predicate returns `true`
+ * or failing with specified error if the predicate fails
+ *
+ * @param predicate - A `Predicate` function that takes in a value of type `A` and returns a boolean.
+ *
+ * @example
+ * import { Effect } from "effect"
+ *
+ * const isPositive = (n: number): boolean => n > 0
+ *
+ * // succeeds with `1`
+ * Effect.liftPredicate(1, isPositive, n => `${n} is not positive`)
+ *
+ * // fails with `"0 is not positive"`
+ * Effect.liftPredicate(0, isPositive, n => `${n} is not positive`)
+ *
+ * @category lifting
+ * @since 3.4.0
+ */
+export const liftPredicate: {
+  <A, B extends A, E>(
+    refinement: Refinement<NoInfer<A>, B>,
+    orFailWith: (a: NoInfer<A>) => E
+  ): (a: A) => Effect<B, E>
+  <A, E>(predicate: Predicate<NoInfer<A>>, orFailWith: (a: NoInfer<A>) => E): (a: A) => Effect<A, E>
+  <A, E, B extends A>(self: A, refinement: Refinement<A, B>, orFailWith: (a: A) => E): Effect<B, E>
+  <A, E>(self: A, predicate: Predicate<NoInfer<A>>, orFailWith: (a: NoInfer<A>) => E): Effect<A, E>
+} = effect.liftPredicate
+
+// -------------------------------------------------------------------------------------
 // mapping
 // -------------------------------------------------------------------------------------
 
