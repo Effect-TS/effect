d04f4bee77d215fc72b4d03a62e71a1d29259001
ecyrbe <ecyrbe@gmail.com>
2024-06-21 01:04:52 +0200

    refactor(kysely): use internal alias instead of named import

--- packages/sql-kysely/src/Sqlite.ts
+++ packages/sql-kysely/src/Sqlite.ts
@@ -3,14 +3,14 @@
  */
 import type { KyselyConfig } from "kysely"
 import { DummyDriver, SqliteAdapter, SqliteIntrospector, SqliteQueryCompiler } from "kysely"
-import { makeWithSql } from "./internal/kysely.js"
+import * as internal from "./internal/kysely.js"
 
 /**
  * @since 1.0.0
  * @category constructors
  */
 export const make = <DB>(config?: Omit<KyselyConfig, "dialect">) =>
-  makeWithSql<DB>({
+  internal.makeWithSql<DB>({
     ...config,
     dialect: {
       createAdapter: () => new SqliteAdapter(),
