fc641ef2aea63476f4fb6a96ba98411522f218fd
ecyrbe <ecyrbe@gmail.com>
2024-06-22 15:09:17 +0200

    fix(sql-d1): defect when using transactions

--- packages/sql-d1/src/D1Client.ts
+++ packages/sql-d1/src/D1Client.ts
@@ -15,7 +15,7 @@ import * as Duration from "effect/Duration"
 import * as Effect from "effect/Effect"
 import { identity } from "effect/Function"
 import * as Layer from "effect/Layer"
-import * as Scope from "effect/Scope"
+import type * as Scope from "effect/Scope"
 
 /**
  * @category type ids
@@ -169,18 +169,7 @@ export const make = (
     const connection = yield* _(makeConnection)
 
     const acquirer = semaphore.withPermits(1)(Effect.succeed(connection))
-    const transactionAcquirer = Effect.uninterruptibleMask((restore) =>
-      Effect.as(
-        Effect.zipRight(
-          restore(semaphore.take(1)),
-          Effect.tap(
-            Effect.scope,
-            (scope) => Scope.addFinalizer(scope, semaphore.release(1))
-          )
-        ),
-        connection
-      )
-    )
+    const transactionAcquirer = Effect.dieMessage("transactions are not supported in D1")
 
     return Object.assign(
       Client.make({
--- packages/sql-d1/test/Client.test.ts
+++ packages/sql-d1/test/Client.test.ts
@@ -1,15 +1,15 @@
 import { D1Client } from "@effect/sql-d1"
 import { assert, describe, it } from "@effect/vitest"
-import { Effect } from "effect"
+import { Cause, Effect } from "effect"
 import { D1Miniflare } from "./utils.js"
 
 describe("Client", () => {
   it.scoped("should handle queries without transactions", () =>
-    Effect.gen(function*(_) {
+    Effect.gen(function*() {
       const sql = yield* D1Client.D1Client
       yield* sql`CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)`
       yield* sql`INSERT INTO test (name) VALUES ('hello')`
-      let rows = yield* _(sql`SELECT * FROM test`)
+      let rows = yield* sql`SELECT * FROM test`
       assert.deepStrictEqual(rows, [{ id: 1, name: "hello" }])
       yield* sql`INSERT INTO test (name) VALUES ('world')`
       rows = yield* sql`SELECT * FROM test`
@@ -17,5 +17,25 @@ describe("Client", () => {
         { id: 1, name: "hello" },
         { id: 2, name: "world" }
       ])
+    }).pipe(Effect.provide(D1Miniflare.ClientLive)))
+
+  it.scoped("should handle queries with params without transactions", () =>
+    Effect.gen(function*() {
+      const sql = yield* D1Client.D1Client
+      yield* sql`CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)`
+      yield* sql`INSERT INTO test ${sql.insert({ name: "hello" })}`
+      const rows = yield* sql`SELECT * FROM test WHERE name = ${"hello"}`
+      assert.deepStrictEqual(rows, [{ id: 1, name: "hello" }])
+    }).pipe(Effect.provide(D1Miniflare.ClientLive)))
+
+  it.scoped("should defect on transactions", () =>
+    Effect.gen(function*() {
+      const sql = yield* D1Client.D1Client
+      yield* sql`CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)`
+      const res = yield* sql`INSERT INTO test ${sql.insert({ name: "hello" })}`.pipe(
+        sql.withTransaction,
+        Effect.catchAllDefect((defect) => Effect.succeed(defect))
+      )
+      assert.equal(Cause.isRuntimeException(res), true)
     }).pipe(Effect.provide(D1Miniflare.ClientLive)))
 })
