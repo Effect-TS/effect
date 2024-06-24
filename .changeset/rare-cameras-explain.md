c860f858ae9c60240174025293cdcbaccd905116
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2024-06-24 19:13:20 +0200

    Support unification for Micro module

---
+++ .changeset/rare-cameras-explain.md
@@ -0,0 +1,5 @@
+---
+"effect": minor
+---
+
+Support unification for Micro module
--- packages/effect/dtslint/Unify.ts
+++ packages/effect/dtslint/Unify.ts
@@ -1,4 +1,6 @@
+import type * as Effect from "effect/Effect"
 import * as Either from "effect/Either"
+import type * as Micro from "effect/Micro"
 import type * as Option from "effect/Option"
 import type * as Stream from "effect/Stream"
 import * as Unify from "effect/Unify"
@@ -23,7 +25,16 @@ Unify.unify(<N>(n: N) => Math.random() > 0 ? Either.right(n) : Either.left("ok")
 // $ExpectType Either<number, string>
 Unify.unify(Math.random() > 0 ? Either.right(10) : Either.left("ok"))
 
-// $ExpectType Stream<0 | 1, never, never>
+// $ExpectType Stream<0 | "a", "b" | 1, "c" | 2>
 export type SU = Unify.Unify<
-  Stream.Stream<0> | Stream.Stream<1>
+  Stream.Stream<0, 1, 2> | Stream.Stream<"a", "b", "c">
+>
+
+// $ExpectType Micro<0 | "a", "b" | 1, "c" | 2>
+export type MU = Unify.Unify<
+  Micro.Micro<0, 1, 2> | Micro.Micro<"a", "b", "c">
+>
+// $ExpectType Effect<0 | "a", "b" | 1, "c" | 2>
+export type EU = Unify.Unify<
+  Effect.Effect<0, 1, 2> | Effect.Effect<"a", "b", "c">
 >
