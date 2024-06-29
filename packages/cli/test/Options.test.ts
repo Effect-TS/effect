017a687b9b5219220b61aeabed621c7d61072cfd
David P. Novakovic <dpn@dpn.name>
2024-06-29 23:27:25 +1000

    lint: remove console log.

--- packages/cli/test/Options.test.ts
+++ packages/cli/test/Options.test.ts
@@ -425,7 +425,6 @@ describe("Options", () => {
     Effect.gen(function*(_) {
       const args = Array.make("-d", "key1=v1", "key2=v2=1+1", "--verbose")
       const result = yield* _(process(defs, args, CliConfig.defaultConfig))
-      console.log(result)
       expect(result).toEqual([["--verbose"], HashMap.make(["key1", "v1"], ["key2", "v2=1+1"])])
     }).pipe(runEffect))
 
