801a13345c5c89d940a89bee858661312daf4930
bowzee <dilame.bowzee@gmail.com>
2024-07-04 07:14:46 +0300

    test(Stream/raceAll): add async tests

--- packages/effect/test/Stream/racing.test.ts
+++ packages/effect/test/Stream/racing.test.ts
@@ -1,11 +1,14 @@
 import * as Chunk from "effect/Chunk"
 import * as Effect from "effect/Effect"
+import * as Fiber from "effect/Fiber"
+import * as Schedule from "effect/Schedule"
 import * as Stream from "effect/Stream"
 import * as it from "effect/test/utils/extend"
+import * as TestClock from "effect/TestClock"
 import { assert, describe } from "vitest"
 
 describe("Stream", () => {
-  it.effect("raceAll", () =>
+  it.effect("raceAll sync", () =>
     Effect.gen(function*($) {
       const result = yield* $(
         Stream.raceAll(
@@ -17,5 +20,38 @@ describe("Stream", () => {
         Effect.map(Chunk.toReadonlyArray)
       )
       assert.deepStrictEqual(result, [0, 1, 2, 3])
+    }))
+
+  it.effect("raceAll async", () =>
+    Effect.gen(function*($) {
+      const fiber = yield* $(
+        Stream.raceAll(
+          Stream.fromSchedule(Schedule.spaced("1 second")),
+          Stream.fromSchedule(Schedule.spaced("2 second"))
+        ),
+        Stream.take(5),
+        Stream.runCollect,
+        Effect.map(Chunk.toReadonlyArray),
+        Effect.fork
+      )
+      yield* TestClock.adjust("5 second")
+      const result = yield* Fiber.join(fiber)
+      assert.deepStrictEqual(result, [0, 1, 2, 3, 4])
+    }))
+
+  it.effect("raceAll combined sync + async", () =>
+    Effect.gen(function*($) {
+      const fiber = yield* $(
+        Stream.raceAll(
+          Stream.fromSchedule(Schedule.spaced("1 second")),
+          Stream.make(1, 2, 3)
+        ),
+        Stream.runCollect,
+        Effect.map(Chunk.toReadonlyArray),
+        Effect.fork
+      )
+      yield* TestClock.adjust("0 millis")
+      const result = yield* Fiber.join(fiber)
+      assert.deepStrictEqual(result, [1, 2, 3])
     }))
 })
