650b825a8d1d13f8b5c26b073e8282ec4a04102c
bowzee <dilame.bowzee@gmail.com>
2024-07-06 06:44:42 +0300

    feat(Stream/share): fix test

--- packages/effect/test/Stream/sharing.test.ts
+++ packages/effect/test/Stream/sharing.test.ts
@@ -1,15 +1,15 @@
-import * as Chunk from "effect/Chunk"
 import * as Effect from "effect/Effect"
 import * as Fiber from "effect/Fiber"
 import * as PubSub from "effect/PubSub"
 import * as Schedule from "effect/Schedule"
+import * as Sink from "effect/Sink"
 import * as Stream from "effect/Stream"
 import * as it from "effect/test/utils/extend"
 import * as TestClock from "effect/TestClock"
 import { assert, describe } from "vitest"
 
 describe("Stream", () => {
-  it.effect("share - values", () =>
+  it.scoped("share - values", () =>
     Effect.gen(function*($) {
       const source = [0, 1, 2, 3, 4]
       const sharedStream = Stream.fromIterable(source).pipe(
@@ -20,8 +20,7 @@ describe("Stream", () => {
         Effect.all([
           Stream.runCollect(sharedStream),
           Stream.runCollect(sharedStream)
-        ]),
-        Effect.scoped
+        ])
       )
       assert.deepStrictEqual(Array.from(result1), source)
       assert.deepStrictEqual(Array.from(result2), source)
@@ -40,8 +39,8 @@ describe("Stream", () => {
 
       const firstFiber = yield* sharedStream.pipe(
         Stream.take(1),
-        Stream.runCollect,
-        Effect.map(Chunk.toReadonlyArray),
+        Stream.runScoped(Sink.collectAll()),
+        Effect.map(Array.from),
         Effect.fork
       )
       yield* TestClock.adjust("1 second")
@@ -50,8 +49,8 @@ describe("Stream", () => {
 
       const secondFiber = yield* sharedStream.pipe(
         Stream.take(1),
-        Stream.runCollect,
-        Effect.map(Chunk.toReadonlyArray),
+        Stream.runScoped(Sink.collectAll()),
+        Effect.map(Array.from),
         Effect.fork
       )
       yield* TestClock.adjust("1 second")
