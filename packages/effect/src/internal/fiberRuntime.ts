2795edd32d5290fa62e3daf1a89f7d85089bdc23
Tim <hello@timsmart.co>
2024-07-09 09:31:28 +1200

    make Logger.prettyLogger the default logger (#3197)

--- packages/effect/src/internal/fiberRuntime.ts
+++ packages/effect/src/internal/fiberRuntime.ts
@@ -1406,7 +1406,7 @@ export const loggerWithConsoleError = <M, O>(self: Logger<M, O>): Logger<M, void
 /** @internal */
 export const defaultLogger: Logger<unknown, void> = globalValue(
   Symbol.for("effect/Logger/defaultLogger"),
-  () => loggerWithConsoleLog(internalLogger.stringLogger)
+  () => internalLogger.prettyLogger()
 )
 
 /** @internal */
@@ -1422,9 +1422,9 @@ export const logFmtLogger: Logger<unknown, void> = globalValue(
 )
 
 /** @internal */
-export const prettyLogger: Logger<unknown, void> = globalValue(
-  Symbol.for("effect/Logger/prettyLogger"),
-  () => internalLogger.prettyLogger()
+export const stringLogger: Logger<unknown, void> = globalValue(
+  Symbol.for("effect/Logger/stringLogger"),
+  () => loggerWithConsoleLog(internalLogger.stringLogger)
 )
 
 /** @internal */
