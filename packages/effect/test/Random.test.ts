4626de59c25b384216faa0be87bf0b8cd36357d0
Giacomo Randazzo <giacomoran@gmail.com>
2024-07-10 12:46:14 +0200

    export Random.make taking hashable values as seed (#3203)

--- packages/effect/test/Random.test.ts
+++ packages/effect/test/Random.test.ts
@@ -1,4 +1,4 @@
-import { Array, Chunk, Effect, Random } from "effect"
+import { Array, Chunk, Data, Effect, Random } from "effect"
 import * as it from "effect/test/utils/extend"
 import { assert, describe } from "vitest"
 
@@ -10,4 +10,19 @@ describe("Random", () => {
       assert.isTrue(Chunk.every(end, (n) => n !== undefined))
       assert.deepStrictEqual(start.sort(), Array.fromIterable(end).sort())
     }).pipe(Effect.repeatN(100)))
+
+  it.effect("make", () =>
+    Effect.gen(function*() {
+      const random0 = Random.make("foo")
+      const random1 = Random.make("foo")
+      const random2 = Random.make(Data.struct({ foo: "bar" }))
+      const random3 = Random.make(Data.struct({ foo: "bar" }))
+      const n0 = yield* random0.next
+      const n1 = yield* random1.next
+      const n2 = yield* random2.next
+      const n3 = yield* random3.next
+      assert.strictEqual(n0, n1)
+      assert.strictEqual(n2, n3)
+      assert.notStrictEqual(n0, n2)
+    }))
 })
