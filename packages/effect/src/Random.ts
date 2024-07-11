6e0a107518b2af800b5ccec04bf00b0ce2a3fcc7
Giulio Canti <giulio.canti@gmail.com>
2024-07-11 09:14:32 +0200

    Add example to Random.make (#3219)

--- packages/effect/src/Random.ts
+++ packages/effect/src/Random.ts
@@ -122,6 +122,21 @@ export const Random: Context.Tag<Random, Random> = internal.randomTag
 /**
  * Constructs the `Random` service, seeding the pseudo-random number generator
  * with an hash of the specified seed.
+ * This constructor is useful for generating predictable sequences of random values for specific use cases.
+ *
+ * Example uses:
+ * - Generating random UI data for visual tests.
+ * - Creating data that needs to change daily but remain the same throughout a single day, such as using a date as the seed.
+ *
+ * @param seed - The seed value used to initialize the generator.
+ *
+ * @example
+ * import { Effect, Random } from "effect"
+ *
+ * const random1 = Random.make("myseed")
+ * const random2 = Random.make("myseed")
+ *
+ * assert.equal(Effect.runSync(random1.next), Effect.runSync(random2.next))
  *
  * @since 3.5.0
  * @category constructors
