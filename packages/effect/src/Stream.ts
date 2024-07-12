1feadae2821388c031fead3ceac2670c87afe818
Leo Conforti <leo@leoconforti.us>
2024-07-12 00:33:03 -0500

    Update Stream.ts

--- packages/effect/src/Stream.ts
+++ packages/effect/src/Stream.ts
@@ -3679,7 +3679,7 @@ export const retry: {
 export const run: {
   <A2, A, E2, R2>(
     sink: Sink.Sink<A2, A, unknown, E2, R2>
-  ): <E, R>(self: Stream<A, E, R>) => Effect.Effect<A2, E2 | E, R2 | R>
+  ): <E, R>(self: Stream<A, E, R>) => Effect.Effect<A2, E2 | E, Exclude<R | R2, Scope.Scope>>
   <A, E, R, A2, E2, R2>(
     self: Stream<A, E, R>,
     sink: Sink.Sink<A2, A, unknown, E2, R2>
