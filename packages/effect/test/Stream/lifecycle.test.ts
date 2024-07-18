a5fbc4ebe3aae38ffec2de8ea1429e211a7c4274
Dmitry <dilame@users.noreply.github.com>
2024-07-19 01:15:41 +0300

    add Stream.onStart api (#3302)
    Co-authored-by: Tim <hello@timsmart.co>

---
+++ packages/effect/test/Stream/lifecycle.test.ts
@@ -0,0 +1,18 @@
+import * as Effect from "effect/Effect"
+import * as Stream from "effect/Stream"
+import * as it from "effect/test/utils/extend"
+import { assert, describe } from "vitest"
+
+describe("Stream", () => {
+  it.effect("onStart", () =>
+    Effect.gen(function*($) {
+      let counter = 0
+      const result = yield* $(
+        Stream.make(1, 1),
+        Stream.onStart(Effect.sync(() => counter++)),
+        Stream.runCollect
+      )
+      assert.strictEqual(counter, 1)
+      assert.deepStrictEqual(Array.from(result), [1, 1])
+    }))
+})
