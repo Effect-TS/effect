e25fa4c3bfbbb909f0790670cf5456ddf427012a
Tim <hello@timsmart.co>
2024-08-05 10:31:25 +1200

    add `propagateInterruption` option to Fiber{Handle,Set,Map} (#3407)

--- packages/effect/test/FiberHandle.test.ts
+++ packages/effect/test/FiberHandle.test.ts
@@ -1,4 +1,4 @@
-import { Effect, Exit, Ref } from "effect"
+import { Deferred, Effect, Exit, Fiber, Ref } from "effect"
 import * as FiberHandle from "effect/FiberHandle"
 import * as it from "effect/test/utils/extend"
 import { assert, describe } from "vitest"
@@ -73,5 +73,31 @@ describe("FiberHandle", () => {
       assert.isTrue(Exit.isInterrupted(yield* _(fiberB.await)))
       assert.isTrue(Exit.isInterrupted(yield* _(fiberC.await)))
       assert.strictEqual(fiberA.unsafePoll(), null)
+    }))
+
+  it.scoped("propagateInterruption: false", () =>
+    Effect.gen(function*() {
+      const handle = yield* FiberHandle.make()
+      const fiber = yield* FiberHandle.run(handle, Effect.never, {
+        propagateInterruption: false
+      })
+      yield* Effect.yieldNow()
+      yield* Fiber.interrupt(fiber)
+      assert.isFalse(yield* Deferred.isDone(handle.deferred))
+    }))
+
+  it.scoped("propagateInterruption: true", () =>
+    Effect.gen(function*() {
+      const handle = yield* FiberHandle.make()
+      const fiber = yield* FiberHandle.run(handle, Effect.never, {
+        propagateInterruption: true
+      })
+      yield* Effect.yieldNow()
+      yield* Fiber.interrupt(fiber)
+      assert.isTrue(Exit.isInterrupted(
+        yield* FiberHandle.join(handle).pipe(
+          Effect.exit
+        )
+      ))
     }))
 })
