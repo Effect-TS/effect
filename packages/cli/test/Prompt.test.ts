31c97d79ea3d618e025c36218fbe11b98dca4fa1
Christopher Dierkens <cjdierkens@gmail.com>
2024-08-17 12:17:23 -0400

    fix: remove cli app from prompt tests

--- packages/cli/test/Prompt.test.ts
+++ packages/cli/test/Prompt.test.ts
@@ -1,83 +1,54 @@
-import type * as CliApp from "@effect/cli/CliApp"
-import * as Command from "@effect/cli/Command"
 import * as Prompt from "@effect/cli/Prompt"
 import * as MockConsole from "@effect/cli/test/services/MockConsole"
 import * as MockTerminal from "@effect/cli/test/services/MockTerminal"
-import {} from "@effect/platform"
+import { } from "@effect/platform"
 import { Array, Effect } from "effect"
 import * as Console from "effect/Console"
 import * as Fiber from "effect/Fiber"
 import * as Layer from "effect/Layer"
 import { describe, expect, it } from "vitest"
+import { Terminal } from "../../platform/src/Terminal.js"
 
 const MainLive = Effect.gen(function*(_) {
   const console = yield* _(MockConsole.make)
   return Layer.mergeAll(
     Console.setConsole(console),
-    MockTerminal.layer
+    MockTerminal.layer,
   )
 }).pipe(Layer.unwrapEffect)
 
 const runEffect = <E, A>(
-  self: Effect.Effect<A, E, CliApp.CliApp.Environment>
+  self: Effect.Effect<A, E, Terminal>
 ): Promise<A> => Effect.provide(self, MainLive).pipe(Effect.runPromise)
 
 describe("Prompt", () => {
   describe("text", () => {
-    it("should return an empty string when `default` on `Prompt.TextOptions` is not provided", () =>
-      Effect.gen(function*(_) {
+    it("should use the prompt value when no default is provided", () =>
+      Effect.gen(function*() {
         const prompt = Prompt.text({
-          message: "This should not have a default"
+          message: "This does not have a default",
         })
 
-        const command = Command.prompt(
-          "prompt-command",
-          prompt,
-          (value) =>
-            Console.log(
-              `Prompt value: "${value}"`
-            )
-        )
-
-        const cli = Command.run(command, {
-          name: "Default Value App",
-          version: "0.0.1"
-        })
-
-        const fiber = yield* _(Effect.fork(cli([])))
-        yield* _(MockTerminal.inputKey("Enter"))
-        yield* _(Fiber.join(fiber))
-        const lines = yield* _(MockConsole.getLines({ stripAnsi: true }))
-        const result = Array.some(lines, (line) => line.includes("Prompt value: \"\""))
+        const fiber = yield* Effect.fork(prompt)
+        yield* MockTerminal.inputKey("enter")
+        yield* Fiber.join(fiber)
+        const lines = yield* MockConsole.getLines({ stripAnsi: true })
+        const result = Array.some(lines, (line) => line.includes("? This does not have a default › "))
         expect(result).toBe(true)
       }).pipe(runEffect))
 
-    it("should respect the `default` property on `Prompt.TextOptions`", () =>
-      Effect.gen(function*(_) {
+    it("should use the default value when the default is provided", () =>
+      Effect.gen(function*() {
         const prompt = Prompt.text({
           message: "This should have a default",
           default: "default-value"
         })
 
-        const command = Command.prompt(
-          "prompt-command",
-          prompt,
-          (value) =>
-            Console.log(
-              `Prompt value: "${value}"`
-            )
-        )
-
-        const cli = Command.run(command, {
-          name: "Default Value App",
-          version: "0.0.1"
-        })
-
-        const fiber = yield* _(Effect.fork(cli([])))
-        yield* _(MockTerminal.inputKey("Enter"))
-        yield* _(Fiber.join(fiber))
-        const lines = yield* _(MockConsole.getLines({ stripAnsi: true }))
-        const result = Array.some(lines, (line) => line.includes("Prompt value: \"default-value\""))
+        const fiber = yield* Effect.fork(prompt)
+        yield* MockTerminal.inputKey("enter")
+        yield* Fiber.join(fiber)
+        const lines = yield* MockConsole.getLines({ stripAnsi: true })
+        const result = Array.some(lines, (line) => line.includes("? This should have a default › default-value"))
         expect(result).toBe(true)
       }).pipe(runEffect))
   })
