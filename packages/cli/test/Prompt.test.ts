338e718a41210bee62ce33b6bfd7d85c79969723
Christopher Dierkens <cjdierkens@gmail.com>
2024-08-18 14:10:44 -0400

    chore: lint fix to add extra line after imports

--- packages/cli/test/Prompt.test.ts
+++ packages/cli/test/Prompt.test.ts
@@ -5,8 +5,6 @@ import * as Effect from "effect/Effect"
 import * as Fiber from "effect/Fiber"
 import { describe, expect, it } from "vitest"
 
-
-
 const runEffect = <E, A>(
   self: Effect.Effect<A, E, Terminal>
 ): Promise<A> => Effect.provide(self, MockTerminal.layer).pipe(Effect.runPromise)
@@ -16,14 +14,14 @@ describe("Prompt", () => {
     it("should use the prompt value when no default is provided", () =>
       Effect.gen(function*() {
         const prompt = Prompt.text({
-          message: "This does not have a default",
+          message: "This does not have a default"
         })
 
         const fiber = yield* Effect.fork(prompt)
         yield* MockTerminal.inputKey("enter")
         const result = yield* Fiber.join(fiber)
 
-        expect(result).toBe('')
+        expect(result).toBe("")
       }).pipe(runEffect))
 
     it("should use the default value when the default is provided", () =>
@@ -32,12 +30,12 @@ describe("Prompt", () => {
           message: "This should have a default",
           default: "default-value"
         })
-        
+
         const fiber = yield* Effect.fork(prompt)
         yield* MockTerminal.inputKey("enter")
         const result = yield* Fiber.join(fiber)
 
-        expect(result).toBe('default-value')
+        expect(result).toBe("default-value")
       }).pipe(runEffect))
   })
 })
