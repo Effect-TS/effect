c1406e6f4337397efa1ad6a936ad2b2250bfcca0
Tim <hello@timsmart.co>
2024-09-13 13:07:04 +1200

    changeset & example

--- packages/effect/src/Effect.ts
+++ packages/effect/src/Effect.ts
@@ -5430,6 +5430,23 @@ export const unsafeMakeLatch: (open?: boolean | undefined) => Latch = circular.u
 /**
  * @category latch
  * @since 3.8.0
+ * @example
+ * import { Effect } from "effect"
+ *
+ * Effect.gen(function*() {
+ *   // Create a latch, starting in the closed state
+ *   const latch = yield* Effect.makeLatch(false)
+ *
+ *   // Fork a fiber that logs "open sesame" when the latch is opened
+ *   const fiber = yield* Effect.log("open sesame").pipe(
+ *     latch.whenOpen,
+ *     Effect.fork
+ *   )
+ *
+ *   // Open the latch
+ *   yield* latch.open
+ *   yield* fiber.await
+ * })
  */
 export const makeLatch: (open?: boolean | undefined) => Effect<Latch, never, never> = circular.makeLatch
 
