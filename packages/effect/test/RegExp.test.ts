6982f979c0ed9e010860a83fffcbc2c4f8048f94
Ian D. Bollinger <ian.bollinger@gmail.com>
2024-09-20 00:14:43 -0400

    Add `isRegExp` type guard (#3647)

--- packages/effect/test/RegExp.test.ts
+++ packages/effect/test/RegExp.test.ts
@@ -2,6 +2,12 @@ import * as RegExp from "effect/RegExp"
 import { describe, expect, it } from "vitest"
 
 describe("RegExp", () => {
+  it("isRegExp", () => {
+    expect(RegExp.isRegExp(/a/)).toEqual(true)
+    expect(RegExp.isRegExp(null)).toEqual(false)
+    expect(RegExp.isRegExp("a")).toEqual(false)
+  })
+
   describe("escape", () => {
     it("should escape special characters correctly", () => {
       const testCases: Array<[string, string]> = [
