4b5ca7fa60276e318b949846d6e39d6c1753708c
Tim <hello@timsmart.co>
2024-09-25 15:48:51 +1200

    add support for watch mode in .runMain

---
+++ .changeset/curly-spoons-think.md
@@ -0,0 +1,5 @@
+---
+"@effect/platform-node-shared": patch
+---
+
+add support for watch mode in .runMain
--- packages/platform-node-shared/src/internal/runtime.ts
+++ packages/platform-node-shared/src/internal/runtime.ts
@@ -1,25 +1,34 @@
 import { makeRunMain } from "@effect/platform/Runtime"
+import { constVoid } from "effect/Function"
 
 /** @internal */
 export const runMain = makeRunMain(({
   fiber,
   teardown
 }) => {
-  const keepAlive = setInterval(() => {}, 2 ** 31 - 1)
+  const keepAlive = setInterval(constVoid, 2 ** 31 - 1)
+  let receivedSignal = false
 
   fiber.addObserver((exit) => {
+    if (!receivedSignal) {
+      process.removeListener("SIGINT", onSigint)
+      process.removeListener("SIGTERM", onSigint)
+    }
     clearInterval(keepAlive)
     teardown(exit, (code) => {
-      process.exit(code)
+      if (receivedSignal || code !== 0) {
+        process.exit(code)
+      }
     })
   })
 
   function onSigint() {
+    receivedSignal = true
     process.removeListener("SIGINT", onSigint)
     process.removeListener("SIGTERM", onSigint)
     fiber.unsafeInterruptAsFork(fiber.id())
   }
 
-  process.once("SIGINT", onSigint)
-  process.once("SIGTERM", onSigint)
+  process.on("SIGINT", onSigint)
+  process.on("SIGTERM", onSigint)
 })
