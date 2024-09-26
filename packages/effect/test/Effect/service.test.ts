89815e5086e913588db5f37f6a43b3bab31228bb
Michael Arnaldi <michael.arnaldi@effectful.co>
2024-09-26 19:03:30 +0200

    Implement Effect.Service as a Tag and Layer with Opaque Type and Support multiple layers in Effect.provide

---
+++ packages/effect/test/Effect/service.test.ts
@@ -0,0 +1,44 @@
+import * as Effect from "effect/Effect"
+import * as it from "effect/test/utils/extend"
+import { describe, expect } from "vitest"
+
+class Prefix extends Effect.Service<Prefix>()("Prefix", {
+  sync: () => ({
+    prefix: "PRE"
+  })
+}) {}
+
+class Postfix extends Effect.Service<Postfix>()("Postfix", {
+  sync: () => ({
+    postfix: "POST"
+  })
+}) {}
+
+const messages: Array<string> = []
+
+class Logger extends Effect.Service<Logger>()("Logger", {
+  effect: Effect.gen(function*() {
+    const { prefix } = yield* Prefix
+    const { postfix } = yield* Postfix
+    return {
+      info: (message: string) => Effect.sync(() => messages.push(`[${prefix}][${message}][${postfix}]`))
+    }
+  }),
+  dependencies: [Prefix, Postfix]
+}) {}
+
+describe("Effect", () => {
+  it.effect("Service correctly wires dependencies", () =>
+    Effect.gen(function*() {
+      const { _tag } = yield* Logger
+      expect(_tag).toEqual("Logger")
+      yield* Logger.info("Ok")
+      expect(messages).toEqual(["[PRE][Ok][POST]"])
+      const { prefix } = yield* Prefix
+      expect(prefix).toEqual("PRE")
+      const { postfix } = yield* Postfix
+      expect(postfix).toEqual("POST")
+    }).pipe(
+      Effect.provide([Logger, Prefix, Postfix])
+    ))
+})
