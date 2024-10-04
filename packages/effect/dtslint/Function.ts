d71b34863595227598f3ac903dc528893fbefb60
Patrick Roza <contact@patrickroza.com>
2024-10-04 21:16:14 +0200

    add test

--- packages/effect/dtslint/Function.ts
+++ packages/effect/dtslint/Function.ts
@@ -1,4 +1,5 @@
-import { flow, identity, pipe } from "effect/Function"
+import { identity, Option } from "effect"
+import { flow, pipe } from "effect/Function"
 
 // We should only have one error for the missing definition.
 const _x = (): number =>
@@ -12,6 +13,14 @@ const _x = (): number =>
 // We should only have one error for the missing definition.
 const _y = (): (n: number) => number =>
   flow(
+    // @ts-expect-error
+    add(1),
+    identity
+  )
+
+// We should only have one error for the missing definition.
+const _z = (): number =>
+  Option.some(1).pipe(
     // @ts-expect-error
     add(1),
     identity
