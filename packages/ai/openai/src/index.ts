7b2c626954944b0cd6609a7611897734b6586eec
Tim <hello@timsmart.co>
2024-10-11 13:27:56 +1300

    add OpenAiTokenizer module

--- packages/ai/openai/src/index.ts
+++ packages/ai/openai/src/index.ts
@@ -12,3 +12,13 @@ export * as OpenAiClient from "./OpenAiClient.js"
  * @since 1.0.0
  */
 export * as OpenAiCompletions from "./OpenAiCompletions.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as OpenAiConfig from "./OpenAiConfig.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as OpenAiTokenizer from "./OpenAiTokenizer.js"
