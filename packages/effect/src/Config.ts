1c7fcec925bf7040ca8e4d848a16c783389d3ea6
Maxim Khramtsov <khraks.mamtsov@gmail.com>
2024-10-24 22:01:33 +0200

    `Config.url` constructor (#3834)
    Co-authored-by: maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
    Co-authored-by: Tim <hello@timsmart.co>

--- packages/effect/src/Config.ts
+++ packages/effect/src/Config.ts
@@ -129,6 +129,14 @@ export const array: <A>(config: Config<A>, name?: string) => Config<Array<A>> = 
 export const boolean: (name?: string) => Config<boolean> = internal.boolean
 
 /**
+ * Constructs a config for a URL value.
+ *
+ * @since 3.11.0
+ * @category constructors
+ */
+export const url: (name?: string) => Config<URL> = internal.url
+
+/**
  * Constructs a config for a sequence of values.
  *
  * @since 2.0.0
