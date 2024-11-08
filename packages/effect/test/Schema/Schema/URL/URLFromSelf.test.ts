3fa0a45e588fe288895c44495b204ff6a7526d01
Maxim Khramtsov <khraks.mamtsov@gmail.com>
2024-11-08 13:40:15 +0100

    `URL` and `URLFromSelf` schemas (#3889)
    Co-authored-by: maksim.khramtsov <maksim.khramtsov@btsdigital.kz>

---
+++ packages/effect/test/Schema/Schema/URL/URLFromSelf.test.ts
@@ -0,0 +1,37 @@
+import * as Pretty from "effect/Pretty"
+import * as S from "effect/Schema"
+import * as Util from "effect/test/Schema/TestUtils"
+import { describe, expect, it } from "vitest"
+
+describe("URLFromSelf", () => {
+  const schema = S.URLFromSelf
+
+  it("property tests", () => {
+    Util.roundtrip(schema)
+  })
+
+  it("arbitrary", () => {
+    Util.expectArbitrary(S.URLFromSelf)
+  })
+
+  it("decoding", async () => {
+    await Util.expectDecodeUnknownSuccess(
+      schema,
+      new URL("https://effect.website"),
+      new URL("https://effect.website")
+    )
+  })
+
+  it("encoding", () => {
+    Util.expectEncodeSuccess(
+      schema,
+      new URL("https://effect.website"),
+      new URL("https://effect.website")
+    )
+  })
+
+  it("Pretty", () => {
+    const pretty = Pretty.make(schema)
+    expect(pretty(new URL("https://effect.website"))).toEqual(`https://effect.website/`)
+  })
+})
