b1a7685127bb82bf5810b2d40b3766cdc94b89ab
Marc Guilera <marcguilerap@gmail.com>
2024-12-05 18:39:31 +0100

    Update absurd function documentation (#4085)

--- packages/effect/src/Function.ts
+++ packages/effect/src/Function.ts
@@ -393,7 +393,7 @@ export const compose: {
  * The `absurd` function is a stub for cases where a value of type `never` is encountered in your code,
  * meaning that it should be impossible for this code to be executed.
  *
- * This function is particularly when it's necessary to specify that certain cases are impossible.
+ * This function is particularly useful when it's necessary to specify that certain cases are impossible.
  *
  * @since 2.0.0
  */
