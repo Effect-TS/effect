c0ba834d1995cf5a8b250e4780fd43f3e3881151
Titouan CREACH <titouancreach@users.noreply.github.com>
2024-12-06 12:49:14 +0100

    add Schema.headNonEmpty on Schema.NonEmptyArray (#3983)
    Co-authored-by: Giulio Canti <giulio.canti@gmail.com>

---
+++ .changeset/violet-spoons-compare.md
@@ -0,0 +1,5 @@
+---
+"effect": minor
+---
+
+Add Schema.headNonEmpty for Schema.NonEmptyArray
---
+++ packages/effect/test/Schema/Schema/Array/headNonEmpty.test.ts
@@ -0,0 +1,26 @@
+import * as S from "effect/Schema"
+import * as Util from "effect/test/Schema/TestUtils"
+import { describe, it } from "vitest"
+
+describe("headNonEmpty", () => {
+  it("decoding", async () => {
+    const schema = S.headNonEmpty(S.NonEmptyArray(S.NumberFromString))
+    await Util.expectDecodeUnknownSuccess(schema, ["1"], 1)
+    await Util.expectDecodeUnknownFailure(
+      schema,
+      ["a"],
+      `(readonly [NumberFromString, ...NumberFromString[]] <-> number | number)
+└─ Encoded side transformation failure
+   └─ readonly [NumberFromString, ...NumberFromString[]]
+      └─ [0]
+         └─ NumberFromString
+            └─ Transformation process failure
+               └─ Expected NumberFromString, actual "a"`
+    )
+  })
+
+  it("encoding", async () => {
+    const schema = S.headNonEmpty(S.NonEmptyArray(S.NumberFromString))
+    await Util.expectEncodeSuccess(schema, 1, ["1"])
+  })
+})
