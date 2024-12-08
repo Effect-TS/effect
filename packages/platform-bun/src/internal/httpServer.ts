62b2e74a840cba66761c198e35a251500d6e4bea
Hüseyin Berke Bütün <8071263+Zamion101@users.noreply.github.com>
2024-12-08 23:22:46 +0100

    Eslint apply `--fix`

--- packages/platform-bun/src/internal/httpServer.ts
+++ packages/platform-bun/src/internal/httpServer.ts
@@ -250,9 +250,9 @@ class ServerRequestImpl extends Inspectable.Class implements ServerRequest.HttpS
     return this.source.url
   }
   get remoteAddress(): Option.Option<string> {
-    return this.remoteAddressOverride 
-    ? Option.some(this.remoteAddressOverride) 
-    : this.bunServer.requestIP(this.source)
+    return this.remoteAddressOverride
+      ? Option.some(this.remoteAddressOverride)
+      : this.bunServer.requestIP(this.source)
       ? Option.some(this.bunServer.requestIP(this.source)!.address)
       : Option.none()
   }
