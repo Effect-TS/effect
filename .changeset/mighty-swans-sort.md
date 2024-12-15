b7a0a87827056dfe6b6f8c460631c054b15ffcd5
Tim <hello@timsmart.co>
2024-12-16 10:09:09 +1300

    add @effect/sql-sqlite-do for cloudflare durable object sqlite (#4141)

---
+++ .changeset/mighty-swans-sort.md
@@ -0,0 +1,5 @@
+---
+"@effect/sql-sqlite-do": minor
+---
+
+add @effect/sql-sqlite-do for cloudflare durable object sqlite
---
+++ packages/sql-sqlite-do/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @since 1.0.0
+ */
+export * as SqliteClient from "./SqliteClient.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as SqliteMigrator from "./SqliteMigrator.js"
