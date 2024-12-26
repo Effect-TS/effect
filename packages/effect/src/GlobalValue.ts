ccea9448a15c7a9f2e77bf98f45a3c1b3b593aa8
Michael Arnaldi <michael.arnaldi@effectful.co>
2024-12-26 22:01:46 +0100

    disable global value

--- packages/effect/src/GlobalValue.ts
+++ packages/effect/src/GlobalValue.ts
@@ -12,15 +12,6 @@
  *
  * @since 2.0.0
  */
-import * as version from "./internal/version.js"
-
-const globalStoreId = `effect/GlobalValue/globalStoreId/${version.getCurrentVersion()}`
-
-if (!(globalStoreId in globalThis)) {
-  ;(globalThis as any)[globalStoreId] = new Map()
-}
-
-const globalStore = (globalThis as any)[globalStoreId] as Map<unknown, any>
 
 /**
  * Retrieves or computes a global value associated with the given `id`. If the value for this `id`
@@ -46,8 +37,5 @@ const globalStore = (globalThis as any)[globalStoreId] as Map<unknown, any>
  * @since 2.0.0
  */
 export const globalValue = <A>(id: unknown, compute: () => A): A => {
-  if (!globalStore.has(id)) {
-    globalStore.set(id, compute())
-  }
-  return globalStore.get(id)!
+  return compute()
 }
