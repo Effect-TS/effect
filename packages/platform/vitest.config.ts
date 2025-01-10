c856cee1264a9587c6344a5e023ae44115b0b55f
Giulio Canti <giulio.canti@gmail.com>
2025-01-10 14:41:48 +0100

    comment out coverage code

--- packages/platform/vitest.config.ts
+++ packages/platform/vitest.config.ts
@@ -2,12 +2,12 @@ import { mergeConfig, type UserConfigExport } from "vitest/config"
 import shared from "../../vitest.shared.js"
 
 const config: UserConfigExport = {
-  test: {
-    coverage: {
-      reporter: ["html"],
-      include: ["src/OpenApi.ts", "src/Multipart.ts", "src/HttpApi*.ts"]
-    }
-  }
+  // test: {
+  //   coverage: {
+  //     reporter: ["html"],
+  //     include: ["src/OpenApi.ts", "src/Multipart.ts", "src/HttpApi*.ts"]
+  //   }
+  // }
 }
 
 export default mergeConfig(shared, config)
