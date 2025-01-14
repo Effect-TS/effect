e82dfc896663ad0871ec68db6192e2ff980dbf93
Chris Wilkinson <chris@prereview.org>
2025-01-14 13:44:03 +0000

    Add isHttpMethod refinement

---
+++ .changeset/long-waves-fetch.md
@@ -0,0 +1,5 @@
+---
+"@effect/platform": patch
+---
+
+Add isHttpMethod refinement
--- packages/platform/src/HttpMethod.ts
+++ packages/platform/src/HttpMethod.ts
@@ -33,3 +33,23 @@ export declare namespace HttpMethod {
  * @since 1.0.0
  */
 export const hasBody = (method: HttpMethod): boolean => method !== "GET" && method !== "HEAD" && method !== "OPTIONS"
+
+/**
+ * Tests if a value is a `HttpMethod`.
+ *
+ * @param input - The value to test.
+ *
+ * @example
+ * ```ts
+ * import { HttpMethod } from "@effect/platform"
+ *
+ * assert.deepStrictEqual(HttpMethod.isHttpMethod("GET"), true)
+ * assert.deepStrictEqual(HttpMethod.isHttpMethod("get"), false)
+ * assert.deepStrictEqual(HttpMethod.isHttpMethod(1), false)
+ * ```
+ *
+ * @since 1.0.0
+ * @category refinements
+ */
+export const isHttpMethod = (u: unknown): u is HttpMethod =>
+  typeof u === "string" && ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"].includes(u)
