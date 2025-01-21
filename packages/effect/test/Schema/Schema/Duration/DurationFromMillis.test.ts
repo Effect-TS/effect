c662fa33d892aff1c2b92ebf0de54e9448d430bb
Giulio Canti <giulio.canti@gmail.com>
2025-01-21 11:39:32 +0100

    Schema: fix `DurationFromMillis` jsdoc (#4310)

--- packages/effect/test/Schema/Schema/Duration/DurationFromMillis.test.ts
+++ packages/effect/test/Schema/Schema/Duration/DurationFromMillis.test.ts
@@ -15,12 +15,33 @@ describe("DurationFromMillis", () => {
     await Util.assertions.decoding.succeed(schema, 0, Duration.millis(0))
     await Util.assertions.decoding.succeed(schema, 1000, Duration.seconds(1))
     await Util.assertions.decoding.succeed(schema, 60 * 1000, Duration.minutes(1))
+    await Util.assertions.decoding.succeed(schema, 0.1, Duration.millis(0.1))
+
+    await Util.assertions.decoding.fail(
+      schema,
+      -1,
+      `DurationFromMillis
+└─ Encoded side transformation failure
+   └─ NonNegative
+      └─ Predicate refinement failure
+         └─ Expected a non-negative number to be decoded into a Duration, actual -1`
+    )
+    await Util.assertions.decoding.fail(
+      schema,
+      NaN,
+      `DurationFromMillis
+└─ Encoded side transformation failure
+   └─ NonNegative
+      └─ Predicate refinement failure
+         └─ Expected a non-negative number to be decoded into a Duration, actual NaN`
+    )
   })
 
   it("encoding", async () => {
     await Util.assertions.encoding.succeed(schema, Duration.infinity, Infinity)
     await Util.assertions.encoding.succeed(schema, Duration.seconds(5), 5000)
     await Util.assertions.encoding.succeed(schema, Duration.millis(5000), 5000)
+    await Util.assertions.encoding.succeed(schema, Duration.millis(0.1), 0.1)
     await Util.assertions.encoding.succeed(schema, Duration.nanos(5000n), 0.005)
   })
 })
