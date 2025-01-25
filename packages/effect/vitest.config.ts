8d900db119b410dc66a69967b282fda212d99cfd
Giulio Canti <giulio.canti@gmail.com>
2025-01-25 11:15:17 +0100

    commented-out test coverage configuration from vitest.config.ts

--- packages/effect/vitest.config.ts
+++ packages/effect/vitest.config.ts
@@ -2,12 +2,12 @@ import { mergeConfig, type UserConfigExport } from "vitest/config"
 import shared from "../../vitest.shared.js"
 
 const config: UserConfigExport = {
-  test: {
-    coverage: {
-      reporter: ["html"],
-      include: ["src/Cause.ts", "src/internal/cause.ts"]
-    }
-  }
+  // test: {
+  //   coverage: {
+  //     reporter: ["html"],
+  //     include: ["src/Cause.ts", "src/internal/cause.ts"]
+  //   }
+  // }
 }
 
 export default mergeConfig(shared, config)
