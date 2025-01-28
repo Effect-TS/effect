4da820cacf12bd16576d95f73e94f0e4f1496ec1
Giulio Canti <giulio.canti@gmail.com>
2025-01-28 19:52:55 +0100

    LogLevel

--- packages/effect/test/LogLevel.test.ts
+++ packages/effect/test/LogLevel.test.ts
@@ -1,15 +1,16 @@
 import * as LogLevel from "effect/LogLevel"
-import { describe, expect, it } from "vitest"
+import { strictEqual } from "effect/test/util"
+import { describe, it } from "vitest"
 
 describe("LogLevel", () => {
   it("fromLiteral", () => {
-    expect(LogLevel.fromLiteral("All")).toStrictEqual(LogLevel.All)
-    expect(LogLevel.fromLiteral("Debug")).toStrictEqual(LogLevel.Debug)
-    expect(LogLevel.fromLiteral("Error")).toStrictEqual(LogLevel.Error)
-    expect(LogLevel.fromLiteral("Fatal")).toStrictEqual(LogLevel.Fatal)
-    expect(LogLevel.fromLiteral("Info")).toStrictEqual(LogLevel.Info)
-    expect(LogLevel.fromLiteral("None")).toStrictEqual(LogLevel.None)
-    expect(LogLevel.fromLiteral("Trace")).toStrictEqual(LogLevel.Trace)
-    expect(LogLevel.fromLiteral("Warning")).toStrictEqual(LogLevel.Warning)
+    strictEqual(LogLevel.fromLiteral("All"), LogLevel.All)
+    strictEqual(LogLevel.fromLiteral("Debug"), LogLevel.Debug)
+    strictEqual(LogLevel.fromLiteral("Error"), LogLevel.Error)
+    strictEqual(LogLevel.fromLiteral("Fatal"), LogLevel.Fatal)
+    strictEqual(LogLevel.fromLiteral("Info"), LogLevel.Info)
+    strictEqual(LogLevel.fromLiteral("None"), LogLevel.None)
+    strictEqual(LogLevel.fromLiteral("Trace"), LogLevel.Trace)
+    strictEqual(LogLevel.fromLiteral("Warning"), LogLevel.Warning)
   })
 })
