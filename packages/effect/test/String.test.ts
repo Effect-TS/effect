2215077425b1ce0a1a53ed433e37da6b20bba018
Giulio Canti <giulio.canti@gmail.com>
2025-01-29 12:27:37 +0100

    String

--- packages/effect/test/String.test.ts
+++ packages/effect/test/String.test.ts
@@ -1,76 +1,73 @@
-import { pipe } from "effect/Function"
-import * as Option from "effect/Option"
-import * as Order from "effect/Order"
-import * as S from "effect/String"
-import { deepStrictEqual } from "effect/test/util"
-import { describe, expect, it } from "vitest"
+import { Order, pipe, String as S } from "effect"
+import { assertFalse, assertNone, assertSome, assertTrue, deepStrictEqual, strictEqual } from "effect/test/util"
+import { describe, it } from "vitest"
 
 describe("String", () => {
   it("isString", () => {
-    expect(S.isString("a")).toEqual(true)
-    expect(S.isString(1)).toEqual(false)
-    expect(S.isString(true)).toEqual(false)
+    assertTrue(S.isString("a"))
+    assertFalse(S.isString(1))
+    assertFalse(S.isString(true))
   })
 
   it("empty", () => {
-    expect(S.empty).toEqual("")
+    strictEqual(S.empty, "")
   })
 
   it("Equivalence", () => {
-    expect(S.Equivalence("a", "a")).toBe(true)
-    expect(S.Equivalence("a", "b")).toBe(false)
+    assertTrue(S.Equivalence("a", "a"))
+    assertFalse(S.Equivalence("a", "b"))
   })
 
   it("Order", () => {
     const lessThan = Order.lessThan(S.Order)
     const lessThanOrEqualTo = Order.lessThanOrEqualTo(S.Order)
-    expect(pipe("a", lessThan("b"))).toEqual(true)
-    expect(pipe("a", lessThan("a"))).toEqual(false)
-    expect(pipe("a", lessThanOrEqualTo("a"))).toEqual(true)
-    expect(pipe("b", lessThan("a"))).toEqual(false)
-    expect(pipe("b", lessThanOrEqualTo("a"))).toEqual(false)
+    assertTrue(pipe("a", lessThan("b")))
+    assertFalse(pipe("a", lessThan("a")))
+    assertTrue(pipe("a", lessThanOrEqualTo("a")))
+    assertFalse(pipe("b", lessThan("a")))
+    assertFalse(pipe("b", lessThanOrEqualTo("a")))
   })
 
   it("concat", () => {
-    expect(pipe("a", S.concat("b"))).toBe("ab")
+    strictEqual(pipe("a", S.concat("b")), "ab")
   })
 
   it("isEmpty", () => {
-    expect(S.isEmpty("")).toBe(true)
-    expect(S.isEmpty("a")).toBe(false)
+    assertTrue(S.isEmpty(""))
+    assertFalse(S.isEmpty("a"))
   })
 
   it("isNonEmpty", () => {
-    expect(S.isNonEmpty("")).toBe(false)
-    expect(S.isNonEmpty("a")).toBe(true)
+    assertFalse(S.isNonEmpty(""))
+    assertTrue(S.isNonEmpty("a"))
   })
 
   it("length", () => {
-    expect(S.length("")).toBe(0)
-    expect(S.length("a")).toBe(1)
-    expect(S.length("aaa")).toBe(3)
+    strictEqual(S.length(""), 0)
+    strictEqual(S.length("a"), 1)
+    strictEqual(S.length("aaa"), 3)
   })
 
   it("toUpperCase", () => {
-    expect(S.toUpperCase("a")).toBe("A")
+    strictEqual(S.toUpperCase("a"), "A")
   })
 
   it("toLowerCase", () => {
-    expect(S.toLowerCase("A")).toBe("a")
+    strictEqual(S.toLowerCase("A"), "a")
   })
 
   it("capitalize", () => {
-    expect(S.capitalize("")).toBe("")
-    expect(S.capitalize("abc")).toBe("Abc")
+    strictEqual(S.capitalize(""), "")
+    strictEqual(S.capitalize("abc"), "Abc")
   })
 
   it("uncapitalize", () => {
-    expect(S.uncapitalize("")).toBe("")
-    expect(S.uncapitalize("Abc")).toBe("abc")
+    strictEqual(S.uncapitalize(""), "")
+    strictEqual(S.uncapitalize("Abc"), "abc")
   })
 
   it("replace", () => {
-    expect(pipe("abc", S.replace("b", "d"))).toBe("adc")
+    strictEqual(pipe("abc", S.replace("b", "d")), "adc")
   })
 
   it("split", () => {
@@ -79,36 +76,36 @@ describe("String", () => {
   })
 
   it("trim", () => {
-    expect(pipe(" a ", S.trim)).toBe("a")
+    strictEqual(pipe(" a ", S.trim), "a")
   })
 
   it("trimStart", () => {
-    expect(pipe(" a ", S.trimStart)).toBe("a ")
+    strictEqual(pipe(" a ", S.trimStart), "a ")
   })
 
   it("trimEnd", () => {
-    expect(pipe(" a ", S.trimEnd)).toBe(" a")
+    strictEqual(pipe(" a ", S.trimEnd), " a")
   })
 
   it("includes", () => {
-    expect(pipe("abc", S.includes("b"))).toBe(true)
-    expect(pipe("abc", S.includes("d"))).toBe(false)
-    expect(pipe("abc", S.includes("b", 1))).toBe(true)
-    expect(pipe("abc", S.includes("a", 1))).toBe(false)
+    assertTrue(pipe("abc", S.includes("b")))
+    assertFalse(pipe("abc", S.includes("d")))
+    assertTrue(pipe("abc", S.includes("b", 1)))
+    assertFalse(pipe("abc", S.includes("a", 1)))
   })
 
   it("startsWith", () => {
-    expect(pipe("abc", S.startsWith("a"))).toBe(true)
-    expect(pipe("bc", S.startsWith("a"))).toBe(false)
-    expect(pipe("abc", S.startsWith("b", 1))).toBe(true)
-    expect(pipe("bc", S.startsWith("a", 1))).toBe(false)
+    assertTrue(pipe("abc", S.startsWith("a")))
+    assertFalse(pipe("bc", S.startsWith("a")))
+    assertTrue(pipe("abc", S.startsWith("b", 1)))
+    assertFalse(pipe("bc", S.startsWith("a", 1)))
   })
 
   it("endsWith", () => {
-    expect(pipe("abc", S.endsWith("c"))).toBe(true)
-    expect(pipe("ab", S.endsWith("c"))).toBe(false)
-    expect(pipe("abc", S.endsWith("b", 2))).toBe(true)
-    expect(pipe("abc", S.endsWith("c", 2))).toBe(false)
+    assertTrue(pipe("abc", S.endsWith("c")))
+    assertFalse(pipe("ab", S.endsWith("c")))
+    assertTrue(pipe("abc", S.endsWith("b", 2)))
+    assertFalse(pipe("abc", S.endsWith("c", 2)))
   })
 
   it("slice", () => {
@@ -116,135 +113,135 @@ describe("String", () => {
   })
 
   it("charCodeAt", () => {
-    expect(pipe("abc", S.charCodeAt(1))).toStrictEqual(Option.some(98))
-    expect(pipe("abc", S.charCodeAt(4))).toStrictEqual(Option.none())
+    assertSome(pipe("abc", S.charCodeAt(1)), 98)
+    assertNone(pipe("abc", S.charCodeAt(4)))
   })
 
   it("substring", () => {
-    expect(pipe("abcd", S.substring(1))).toBe("bcd")
-    expect(pipe("abcd", S.substring(1, 3))).toBe("bc")
+    strictEqual(pipe("abcd", S.substring(1)), "bcd")
+    strictEqual(pipe("abcd", S.substring(1, 3)), "bc")
   })
 
   it("at", () => {
-    expect(pipe("abc", S.at(1))).toStrictEqual(Option.some("b"))
-    expect(pipe("abc", S.at(4))).toStrictEqual(Option.none())
+    assertSome(pipe("abc", S.at(1)), "b")
+    assertNone(pipe("abc", S.at(4)))
   })
 
   it("charAt", () => {
-    expect(pipe("abc", S.charAt(1))).toStrictEqual(Option.some("b"))
-    expect(pipe("abc", S.charAt(4))).toStrictEqual(Option.none())
+    assertSome(pipe("abc", S.charAt(1)), "b")
+    assertNone(pipe("abc", S.charAt(4)))
   })
 
   it("codePointAt", () => {
-    expect(pipe("abc", S.codePointAt(1))).toStrictEqual(Option.some(98))
-    expect(pipe("abc", S.codePointAt(4))).toStrictEqual(Option.none())
+    assertSome(pipe("abc", S.codePointAt(1)), 98)
+    assertNone(pipe("abc", S.codePointAt(4)))
   })
 
   it("indexOf", () => {
-    expect(pipe("abbbc", S.indexOf("b"))).toStrictEqual(Option.some(1))
-    expect(pipe("abbbc", S.indexOf("d"))).toStrictEqual(Option.none())
+    assertSome(pipe("abbbc", S.indexOf("b")), 1)
+    assertNone(pipe("abbbc", S.indexOf("d")))
   })
 
   it("lastIndexOf", () => {
-    expect(pipe("abbbc", S.lastIndexOf("b"))).toStrictEqual(Option.some(3))
-    expect(pipe("abbbc", S.lastIndexOf("d"))).toStrictEqual(Option.none())
+    assertSome(pipe("abbbc", S.lastIndexOf("b")), 3)
+    assertNone(pipe("abbbc", S.lastIndexOf("d")))
   })
 
   it("localeCompare", () => {
-    expect(pipe("a", S.localeCompare("b"))).toBe(-1)
-    expect(pipe("b", S.localeCompare("a"))).toBe(1)
-    expect(pipe("a", S.localeCompare("a"))).toBe(0)
+    strictEqual(pipe("a", S.localeCompare("b")), -1)
+    strictEqual(pipe("b", S.localeCompare("a")), 1)
+    strictEqual(pipe("a", S.localeCompare("a")), 0)
   })
 
   it("match", () => {
-    expect(pipe("a", S.match(/a/))).toStrictEqual(Option.some(expect.arrayContaining(["a"])))
-    expect(pipe("a", S.match(/b/))).toStrictEqual(Option.none())
+    assertSome(pipe("a", S.match(/a/)), "a".match(/a/))
+    assertNone(pipe("a", S.match(/b/)))
   })
 
   it("matchAll", () => {
-    expect(Array.from(pipe("apple, banana", S.matchAll(/a[pn]/g)))).toHaveLength(3)
-    expect(Array.from(pipe("apple, banana", S.matchAll(/c/g)))).toHaveLength(0)
+    strictEqual(Array.from(pipe("apple, banana", S.matchAll(/a[pn]/g))).length, 3)
+    strictEqual(Array.from(pipe("apple, banana", S.matchAll(/c/g))).length, 0)
   })
 
   it("normalize", () => {
     const str = "\u1E9B\u0323"
-    expect(pipe(str, S.normalize())).toBe("\u1E9B\u0323")
-    expect(pipe(str, S.normalize("NFC"))).toBe("\u1E9B\u0323")
-    expect(pipe(str, S.normalize("NFD"))).toBe("\u017F\u0323\u0307")
-    expect(pipe(str, S.normalize("NFKC"))).toBe("\u1E69")
-    expect(pipe(str, S.normalize("NFKD"))).toBe("\u0073\u0323\u0307")
+    strictEqual(pipe(str, S.normalize()), "\u1E9B\u0323")
+    strictEqual(pipe(str, S.normalize("NFC")), "\u1E9B\u0323")
+    strictEqual(pipe(str, S.normalize("NFD")), "\u017F\u0323\u0307")
+    strictEqual(pipe(str, S.normalize("NFKC")), "\u1E69")
+    strictEqual(pipe(str, S.normalize("NFKD")), "\u0073\u0323\u0307")
   })
 
   it("padEnd", () => {
-    expect(pipe("a", S.padEnd(5))).toBe("a    ")
-    expect(pipe("a", S.padEnd(5, "_"))).toBe("a____")
+    strictEqual(pipe("a", S.padEnd(5)), "a    ")
+    strictEqual(pipe("a", S.padEnd(5, "_")), "a____")
   })
 
   it("padStart", () => {
-    expect(pipe("a", S.padStart(5))).toBe("    a")
-    expect(pipe("a", S.padStart(5, "_"))).toBe("____a")
+    strictEqual(pipe("a", S.padStart(5)), "    a")
+    strictEqual(pipe("a", S.padStart(5, "_")), "____a")
   })
 
   it("repeat", () => {
-    expect(pipe("a", S.repeat(3))).toBe("aaa")
+    strictEqual(pipe("a", S.repeat(3)), "aaa")
   })
 
   it("replaceAll", () => {
-    expect(pipe("ababb", S.replaceAll("b", "c"))).toBe("acacc")
-    expect(pipe("ababb", S.replaceAll(/ba/g, "cc"))).toBe("accbb")
+    strictEqual(pipe("ababb", S.replaceAll("b", "c")), "acacc")
+    strictEqual(pipe("ababb", S.replaceAll(/ba/g, "cc")), "accbb")
   })
 
   it("search", () => {
-    expect(pipe("ababb", S.search("b"))).toStrictEqual(Option.some(1))
-    expect(pipe("ababb", S.search(/abb/))).toStrictEqual(Option.some(2))
-    expect(pipe("ababb", S.search(/c/))).toStrictEqual(Option.none())
+    assertSome(pipe("ababb", S.search("b")), 1)
+    assertSome(pipe("ababb", S.search(/abb/)), 2)
+    assertNone(pipe("ababb", S.search(/c/)))
   })
 
   it("toLocaleLowerCase", () => {
     const locales = ["tr", "TR", "tr-TR", "tr-u-co-search", "tr-x-turkish"]
-    expect(pipe("\u0130", S.toLocaleLowerCase(locales))).toBe("i")
+    strictEqual(pipe("\u0130", S.toLocaleLowerCase(locales)), "i")
   })
 
   it("toLocaleUpperCase", () => {
     const locales = ["lt", "LT", "lt-LT", "lt-u-co-phonebk", "lt-x-lietuva"]
-    expect(pipe("i\u0307", S.toLocaleUpperCase(locales))).toBe("I")
+    strictEqual(pipe("i\u0307", S.toLocaleUpperCase(locales)), "I")
   })
 
   describe("takeLeft", () => {
     it("should take the specified number of characters from the left side of a string", () => {
-      expect(S.takeLeft("Hello, World!", 7)).toBe("Hello, ")
+      strictEqual(S.takeLeft("Hello, World!", 7), "Hello, ")
     })
 
     it("should return the string for `n` larger than the string length", () => {
       const string = "Hello, World!"
-      expect(S.takeLeft(string, 100)).toBe(string)
+      strictEqual(S.takeLeft(string, 100), string)
     })
 
     it("should return the empty string for a negative `n`", () => {
-      expect(S.takeLeft("Hello, World!", -1)).toBe("")
+      strictEqual(S.takeLeft("Hello, World!", -1), "")
     })
 
     it("should round down if `n` is a float", () => {
-      expect(S.takeLeft("Hello, World!", 5.5)).toBe("Hello")
+      strictEqual(S.takeLeft("Hello, World!", 5.5), "Hello")
     })
   })
 
   describe("takeRight", () => {
     it("should take the specified number of characters from the right side of a string", () => {
-      expect(S.takeRight("Hello, World!", 7)).toBe(" World!")
+      strictEqual(S.takeRight("Hello, World!", 7), " World!")
     })
 
     it("should return the string for `n` larger than the string length", () => {
       const string = "Hello, World!"
-      expect(S.takeRight(string, 100)).toBe(string)
+      strictEqual(S.takeRight(string, 100), string)
     })
 
     it("should return the empty string for a negative `n`", () => {
-      expect(S.takeRight("Hello, World!", -1)).toBe("")
+      strictEqual(S.takeRight("Hello, World!", -1), "")
     })
 
     it("should round down if `n` is a float", () => {
-      expect(S.takeRight("Hello, World!", 6.5)).toBe("World!")
+      strictEqual(S.takeRight("Hello, World!", 6.5), "World!")
     })
   })
 
@@ -255,13 +252,13 @@ describe("String", () => {
     |World!
     |`
       const result = S.stripMargin(string)
-      expect(result).toBe("\nHello,\nWorld!\n")
+      strictEqual(result, "\nHello,\nWorld!\n")
     })
 
     it("should strip a leading prefix from each line using a margin character", () => {
       const string = "\n$\n    $Hello,\r\n    $World!\n $"
       const result = S.stripMarginWith(string, "$")
-      expect(result).toBe("\n\nHello,\r\nWorld!\n")
+      strictEqual(result, "\n\nHello,\r\nWorld!\n")
     })
   })
 
