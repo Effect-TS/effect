c6a5291bb5086ed8d5396b789e823173d38abf7c
Giulio Canti <giulio.canti@gmail.com>
2025-01-29 13:02:20 +0100

    Tuple

--- packages/effect/test/Tuple.test.ts
+++ packages/effect/test/Tuple.test.ts
@@ -1,45 +1,43 @@
-import { pipe } from "effect/Function"
-import * as T from "effect/Tuple"
-import { describe, expect, it } from "vitest"
+import { pipe, Tuple } from "effect"
+import { deepStrictEqual, strictEqual } from "effect/test/util"
+import { describe, it } from "vitest"
 
 describe("Tuple", () => {
-  it("exports", () => {
-    expect(T.getOrder).exist
-    expect(T.getEquivalence).exist
-  })
-
   it("make", () => {
-    expect(T.make("a", 1, true)).toEqual(["a", 1, true])
+    deepStrictEqual(Tuple.make("a", 1, true), ["a", 1, true])
   })
 
   it("appendElement", () => {
-    expect(pipe(T.make("a", 1), T.appendElement(true))).toEqual(["a", 1, true])
+    deepStrictEqual(pipe(Tuple.make("a", 1), Tuple.appendElement(true)), ["a", 1, true])
   })
 
   it("getFirst", () => {
-    expect(T.getFirst(T.make("a", 1))).toEqual("a")
+    strictEqual(Tuple.getFirst(Tuple.make("a", 1)), "a")
   })
 
   it("getSecond", () => {
-    expect(T.getSecond(T.make("a", 1))).toEqual(1)
+    strictEqual(Tuple.getSecond(Tuple.make("a", 1)), 1)
   })
 
   it("mapBoth", () => {
-    expect(T.mapBoth(T.make("a", 1), {
-      onFirst: (s) => s + "!",
-      onSecond: (n) => n * 2
-    })).toEqual(["a!", 2])
+    deepStrictEqual(
+      Tuple.mapBoth(Tuple.make("a", 1), {
+        onFirst: (s) => s + "!",
+        onSecond: (n) => n * 2
+      }),
+      ["a!", 2]
+    )
   })
 
   it("map", () => {
-    expect(T.map(["a", 1, false], (x) => x.toString().toUpperCase())).toEqual(["A", "1", "FALSE"])
+    deepStrictEqual(Tuple.map(["a", 1, false], (x) => x.toString().toUpperCase()), ["A", "1", "FALSE"])
   })
 
   it("swap", () => {
-    expect(T.swap(T.make("a", 1))).toEqual([1, "a"])
+    deepStrictEqual(Tuple.swap(Tuple.make("a", 1)), [1, "a"])
   })
 
   it("at", () => {
-    expect(T.at([1, "hello", true], 1)).toEqual("hello")
+    deepStrictEqual(Tuple.at([1, "hello", true], 1), "hello")
   })
 })
