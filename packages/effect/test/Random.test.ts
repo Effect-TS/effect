6050915fa14b80b320c16f1bbd44d361bc3e60e0
Giulio Canti <giulio.canti@gmail.com>
2025-01-29 11:11:38 +0100

    Random

--- packages/effect/test/Random.test.ts
+++ packages/effect/test/Random.test.ts
@@ -1,6 +1,7 @@
 import { Array, Cause, Chunk, Data, Effect, Random } from "effect"
-import { expect, it } from "effect/test/utils/extend"
-import { assert, describe } from "vitest"
+import { assertTrue, deepStrictEqual, strictEqual } from "effect/test/util"
+import { it } from "effect/test/utils/extend"
+import { describe } from "vitest"
 
 describe("Random", () => {
   it.effect("integer is correctly distributed", () =>
@@ -10,14 +11,14 @@ describe("Random", () => {
       while (lastRandom < tenYearsMillis / 2) {
         lastRandom = yield* Random.nextIntBetween(0, tenYearsMillis)
       }
-      assert.isTrue(lastRandom >= tenYearsMillis / 2)
+      assertTrue(lastRandom >= tenYearsMillis / 2)
     }))
   it.effect("shuffle", () =>
     Effect.gen(function*() {
       const start = Array.range(0, 100)
       const end = yield* Random.shuffle(start)
-      assert.isTrue(Chunk.every(end, (n) => n !== undefined))
-      assert.deepStrictEqual(start.sort(), Array.fromIterable(end).sort())
+      assertTrue(Chunk.every(end, (n) => n !== undefined))
+      deepStrictEqual(start.sort(), Array.fromIterable(end).sort())
     }).pipe(Effect.repeatN(100)))
 
   it.effect("make", () =>
@@ -30,19 +31,22 @@ describe("Random", () => {
       const n1 = yield* random1.next
       const n2 = yield* random2.next
       const n3 = yield* random3.next
-      assert.strictEqual(n0, n1)
-      assert.strictEqual(n2, n3)
-      assert.notStrictEqual(n0, n2)
+      strictEqual(n0, n1)
+      strictEqual(n2, n3)
+      assertTrue(n0 !== n2)
     }))
 
   it.live("choice", () =>
     Effect.gen(function*() {
-      expect(yield* Random.choice([]).pipe(Effect.flip)).toEqual(new Cause.NoSuchElementException())
-      expect(yield* Random.choice([1])).toEqual(1)
+      deepStrictEqual(
+        yield* Random.choice([]).pipe(Effect.flip),
+        new Cause.NoSuchElementException("Cannot select a random element from an empty array")
+      )
+      strictEqual(yield* Random.choice([1]), 1)
 
       const randomItems = yield* Random.choice([1, 2, 3]).pipe(Array.replicate(100), Effect.all)
-      expect(Array.intersection(randomItems, [1, 2, 3]).length).toEqual(randomItems.length)
+      strictEqual(Array.intersection(randomItems, [1, 2, 3]).length, randomItems.length)
 
-      expect(yield* Random.choice(Chunk.fromIterable([1, 2, 3]))).oneOf([1, 2, 3])
+      assertTrue([1, 2, 3].includes(yield* Random.choice(Chunk.fromIterable([1, 2, 3]))))
     }))
 })
