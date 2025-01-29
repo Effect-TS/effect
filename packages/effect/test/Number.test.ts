61b1c5700f180c5d4bb55a91760d619814178fa2
Giulio Canti <giulio.canti@gmail.com>
2025-01-29 10:47:16 +0100

    Number

--- packages/effect/test/Number.test.ts
+++ packages/effect/test/Number.test.ts
@@ -1,147 +1,145 @@
-import { pipe } from "effect/Function"
-import * as Number from "effect/Number"
-import * as Option from "effect/Option"
-import { deepStrictEqual } from "effect/test/util"
-import { assert, describe, expect, it } from "vitest"
+import { Number, pipe } from "effect"
+import { assertFalse, assertNone, assertSome, assertTrue, strictEqual } from "effect/test/util"
+import { describe, it } from "vitest"
 
 describe("Number", () => {
   it("isNumber", () => {
-    expect(Number.isNumber(1)).toEqual(true)
-    expect(Number.isNumber("a")).toEqual(false)
-    expect(Number.isNumber(true)).toEqual(false)
+    assertTrue(Number.isNumber(1))
+    assertFalse(Number.isNumber("a"))
+    assertFalse(Number.isNumber(true))
   })
 
   it("sum", () => {
-    deepStrictEqual(pipe(1, Number.sum(2)), 3)
+    strictEqual(pipe(1, Number.sum(2)), 3)
   })
 
   it("multiply", () => {
-    deepStrictEqual(pipe(2, Number.multiply(3)), 6)
+    strictEqual(pipe(2, Number.multiply(3)), 6)
   })
 
   it("subtract", () => {
-    deepStrictEqual(pipe(3, Number.subtract(1)), 2)
+    strictEqual(pipe(3, Number.subtract(1)), 2)
   })
 
   it("divide", () => {
-    deepStrictEqual(pipe(6, Number.divide(2)), Option.some(3))
-    deepStrictEqual(pipe(6, Number.divide(0)), Option.none())
+    assertSome(pipe(6, Number.divide(2)), 3)
+    assertNone(pipe(6, Number.divide(0)))
   })
 
   it("unsafeDivide", () => {
-    deepStrictEqual(pipe(6, Number.unsafeDivide(2)), 3)
+    strictEqual(pipe(6, Number.unsafeDivide(2)), 3)
   })
 
   it("decrement", () => {
-    deepStrictEqual(Number.decrement(2), 1)
+    strictEqual(Number.decrement(2), 1)
   })
 
   it("Equivalence", () => {
-    expect(Number.Equivalence(1, 1)).toBe(true)
-    expect(Number.Equivalence(1, 2)).toBe(false)
+    assertTrue(Number.Equivalence(1, 1))
+    assertFalse(Number.Equivalence(1, 2))
   })
 
   it("Order", () => {
-    deepStrictEqual(Number.Order(1, 2), -1)
-    deepStrictEqual(Number.Order(2, 1), 1)
-    deepStrictEqual(Number.Order(2, 2), 0)
+    strictEqual(Number.Order(1, 2), -1)
+    strictEqual(Number.Order(2, 1), 1)
+    strictEqual(Number.Order(2, 2), 0)
   })
 
   it("sign", () => {
-    deepStrictEqual(Number.sign(0), 0)
-    deepStrictEqual(Number.sign(0.0), 0)
-    deepStrictEqual(Number.sign(-0.1), -1)
-    deepStrictEqual(Number.sign(-10), -1)
-    deepStrictEqual(Number.sign(10), 1)
-    deepStrictEqual(Number.sign(0.1), 1)
+    strictEqual(Number.sign(0), 0)
+    strictEqual(Number.sign(0.0), 0)
+    strictEqual(Number.sign(-0.1), -1)
+    strictEqual(Number.sign(-10), -1)
+    strictEqual(Number.sign(10), 1)
+    strictEqual(Number.sign(0.1), 1)
   })
 
   it("remainder", () => {
-    assert.deepStrictEqual(Number.remainder(2, 2), 0)
-    assert.deepStrictEqual(Number.remainder(3, 2), 1)
-    assert.deepStrictEqual(Number.remainder(4, 2), 0)
-    assert.deepStrictEqual(Number.remainder(2.5, 2), 0.5)
-    assert.deepStrictEqual(Number.remainder(-2, 2), -0)
-    assert.deepStrictEqual(Number.remainder(-3, 2), -1)
-    assert.deepStrictEqual(Number.remainder(-4, 2), -0)
-    assert.deepStrictEqual(Number.remainder(-2.8, -.2), -0)
-    assert.deepStrictEqual(Number.remainder(-2, -.2), -0)
-    assert.deepStrictEqual(Number.remainder(-1.5, -.2), -0.1)
-    assert.deepStrictEqual(Number.remainder(0, -.2), 0)
-    assert.deepStrictEqual(Number.remainder(1, -.2), 0)
-    assert.deepStrictEqual(Number.remainder(2.6, -.2), 0)
-    assert.deepStrictEqual(Number.remainder(3.1, -.2), 0.1)
+    strictEqual(Number.remainder(2, 2), 0)
+    strictEqual(Number.remainder(3, 2), 1)
+    strictEqual(Number.remainder(4, 2), 0)
+    strictEqual(Number.remainder(2.5, 2), 0.5)
+    strictEqual(Number.remainder(-2, 2), -0)
+    strictEqual(Number.remainder(-3, 2), -1)
+    strictEqual(Number.remainder(-4, 2), -0)
+    strictEqual(Number.remainder(-2.8, -.2), -0)
+    strictEqual(Number.remainder(-2, -.2), -0)
+    strictEqual(Number.remainder(-1.5, -.2), -0.1)
+    strictEqual(Number.remainder(0, -.2), 0)
+    strictEqual(Number.remainder(1, -.2), 0)
+    strictEqual(Number.remainder(2.6, -.2), 0)
+    strictEqual(Number.remainder(3.1, -.2), 0.1)
   })
 
   it("lessThan", () => {
-    assert.deepStrictEqual(Number.lessThan(2, 3), true)
-    assert.deepStrictEqual(Number.lessThan(3, 3), false)
-    assert.deepStrictEqual(Number.lessThan(4, 3), false)
+    assertTrue(Number.lessThan(2, 3))
+    assertFalse(Number.lessThan(3, 3))
+    assertFalse(Number.lessThan(4, 3))
   })
 
   it("lessThanOrEqualTo", () => {
-    assert.deepStrictEqual(Number.lessThanOrEqualTo(2, 3), true)
-    assert.deepStrictEqual(Number.lessThanOrEqualTo(3, 3), true)
-    assert.deepStrictEqual(Number.lessThanOrEqualTo(4, 3), false)
+    assertTrue(Number.lessThanOrEqualTo(2, 3))
+    assertTrue(Number.lessThanOrEqualTo(3, 3))
+    assertFalse(Number.lessThanOrEqualTo(4, 3))
   })
 
   it("greaterThan", () => {
-    assert.deepStrictEqual(Number.greaterThan(2, 3), false)
-    assert.deepStrictEqual(Number.greaterThan(3, 3), false)
-    assert.deepStrictEqual(Number.greaterThan(4, 3), true)
+    assertFalse(Number.greaterThan(2, 3))
+    assertFalse(Number.greaterThan(3, 3))
+    assertTrue(Number.greaterThan(4, 3))
   })
 
   it("greaterThanOrEqualTo", () => {
-    assert.deepStrictEqual(Number.greaterThanOrEqualTo(2, 3), false)
-    assert.deepStrictEqual(Number.greaterThanOrEqualTo(3, 3), true)
-    assert.deepStrictEqual(Number.greaterThanOrEqualTo(4, 3), true)
+    assertFalse(Number.greaterThanOrEqualTo(2, 3))
+    assertTrue(Number.greaterThanOrEqualTo(3, 3))
+    assertTrue(Number.greaterThanOrEqualTo(4, 3))
   })
 
   it("between", () => {
-    assert.deepStrictEqual(Number.between({ minimum: 0, maximum: 5 })(3), true)
-    assert.deepStrictEqual(Number.between({ minimum: 0, maximum: 5 })(-1), false)
-    assert.deepStrictEqual(Number.between({ minimum: 0, maximum: 5 })(6), false)
+    assertTrue(Number.between({ minimum: 0, maximum: 5 })(3))
+    assertFalse(Number.between({ minimum: 0, maximum: 5 })(-1))
+    assertFalse(Number.between({ minimum: 0, maximum: 5 })(6))
 
-    assert.deepStrictEqual(Number.between(3, { minimum: 0, maximum: 5 }), true)
+    assertTrue(Number.between(3, { minimum: 0, maximum: 5 }))
   })
 
   it("clamp", () => {
-    assert.deepStrictEqual(Number.clamp({ minimum: 0, maximum: 5 })(3), 3)
-    assert.deepStrictEqual(Number.clamp({ minimum: 0, maximum: 5 })(-1), 0)
-    assert.deepStrictEqual(Number.clamp({ minimum: 0, maximum: 5 })(6), 5)
+    strictEqual(Number.clamp({ minimum: 0, maximum: 5 })(3), 3)
+    strictEqual(Number.clamp({ minimum: 0, maximum: 5 })(-1), 0)
+    strictEqual(Number.clamp({ minimum: 0, maximum: 5 })(6), 5)
   })
 
   it("min", () => {
-    assert.deepStrictEqual(Number.min(2, 3), 2)
+    strictEqual(Number.min(2, 3), 2)
   })
 
   it("max", () => {
-    assert.deepStrictEqual(Number.max(2, 3), 3)
+    strictEqual(Number.max(2, 3), 3)
   })
 
   it("sumAll", () => {
-    assert.deepStrictEqual(Number.sumAll([2, 3, 4]), 9)
+    strictEqual(Number.sumAll([2, 3, 4]), 9)
   })
 
   it("multiplyAll", () => {
-    assert.deepStrictEqual(Number.multiplyAll([2, 0, 4]), 0)
-    assert.deepStrictEqual(Number.multiplyAll([2, 3, 4]), 24)
+    strictEqual(Number.multiplyAll([2, 0, 4]), 0)
+    strictEqual(Number.multiplyAll([2, 3, 4]), 24)
   })
 
   it("parse", () => {
-    assert.deepStrictEqual(Number.parse("NaN"), Option.some(NaN))
-    assert.deepStrictEqual(Number.parse("Infinity"), Option.some(Infinity))
-    assert.deepStrictEqual(Number.parse("-Infinity"), Option.some(-Infinity))
-    assert.deepStrictEqual(Number.parse("42"), Option.some(42))
-    assert.deepStrictEqual(Number.parse("a"), Option.none())
+    assertSome(Number.parse("NaN"), NaN)
+    assertSome(Number.parse("Infinity"), Infinity)
+    assertSome(Number.parse("-Infinity"), -Infinity)
+    assertSome(Number.parse("42"), 42)
+    assertNone(Number.parse("a"))
   })
 
   it("round", () => {
-    assert.deepStrictEqual(Number.round(1.1234, 2), 1.12)
-    assert.deepStrictEqual(Number.round(2)(1.1234), 1.12)
-    assert.deepStrictEqual(Number.round(0)(1.1234), 1)
-    assert.deepStrictEqual(Number.round(0)(1.1234), 1)
-    assert.deepStrictEqual(Number.round(1.567, 2), 1.57)
-    assert.deepStrictEqual(Number.round(2)(1.567), 1.57)
+    strictEqual(Number.round(1.1234, 2), 1.12)
+    strictEqual(Number.round(2)(1.1234), 1.12)
+    strictEqual(Number.round(0)(1.1234), 1)
+    strictEqual(Number.round(0)(1.1234), 1)
+    strictEqual(Number.round(1.567, 2), 1.57)
+    strictEqual(Number.round(2)(1.567), 1.57)
   })
 })
