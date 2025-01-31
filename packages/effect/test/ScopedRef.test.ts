5f77c91c55d279061cb85b1236ed97dca83706c8
Giulio Canti <giulio.canti@gmail.com>
2025-01-31 08:43:59 +0100

    ScopedRef.test.ts

--- packages/effect/test/ScopedRef.test.ts
+++ packages/effect/test/ScopedRef.test.ts
@@ -6,58 +6,58 @@ import { describe } from "vitest"
 
 describe("ScopedRef", () => {
   it.scoped("single set", () =>
-    Effect.gen(function*($) {
-      const counter = yield* $(Counter.make())
-      const ref = yield* $(ScopedRef.make(() => 0))
-      yield* $(ScopedRef.set(ref, counter.acquire()))
-      const result = yield* $(ScopedRef.get(ref))
+    Effect.gen(function*() {
+      const counter = yield* (Counter.make())
+      const ref = yield* (ScopedRef.make(() => 0))
+      yield* (ScopedRef.set(ref, counter.acquire()))
+      const result = yield* (ScopedRef.get(ref))
       strictEqual(result, 1)
     }))
   it.scoped("dual set", () =>
-    Effect.gen(function*($) {
-      const counter = yield* $(Counter.make())
-      const ref = yield* $(ScopedRef.make(() => 0))
-      yield* $(
+    Effect.gen(function*() {
+      const counter = yield* (Counter.make())
+      const ref = yield* (ScopedRef.make(() => 0))
+      yield* pipe(
         ScopedRef.set(ref, counter.acquire()),
         Effect.zipRight(ScopedRef.set(ref, counter.acquire()))
       )
-      const result = yield* $(ScopedRef.get(ref))
+      const result = yield* (ScopedRef.get(ref))
       strictEqual(result, 2)
     }))
   it.scoped("release on swap", () =>
-    Effect.gen(function*($) {
-      const counter = yield* $(Counter.make())
-      const ref = yield* $(ScopedRef.make(() => 0))
-      yield* $(
+    Effect.gen(function*() {
+      const counter = yield* (Counter.make())
+      const ref = yield* (ScopedRef.make(() => 0))
+      yield* pipe(
         ScopedRef.set(ref, counter.acquire()),
         Effect.zipRight(ScopedRef.set(ref, counter.acquire()))
       )
 
-      const acquired = yield* $(counter.acquired())
-      const released = yield* $(counter.released())
+      const acquired = yield* (counter.acquired())
+      const released = yield* (counter.released())
       strictEqual(acquired, 2)
       strictEqual(released, 1)
     }))
   it.scoped("double release on double swap", () =>
-    Effect.gen(function*($) {
-      const counter = yield* $(Counter.make())
-      const ref = yield* $(ScopedRef.make(() => 0))
-      yield* $(
+    Effect.gen(function*() {
+      const counter = yield* (Counter.make())
+      const ref = yield* (ScopedRef.make(() => 0))
+      yield* (
         pipe(
           ScopedRef.set(ref, counter.acquire()),
           Effect.zipRight(ScopedRef.set(ref, counter.acquire())),
           Effect.zipRight(ScopedRef.set(ref, counter.acquire()))
         )
       )
-      const acquired = yield* $(counter.acquired())
-      const released = yield* $(counter.released())
+      const acquired = yield* (counter.acquired())
+      const released = yield* (counter.released())
       strictEqual(acquired, 3)
       strictEqual(released, 2)
     }))
   it.effect("full release", () =>
-    Effect.gen(function*($) {
-      const counter = yield* $(Counter.make())
-      yield* $(
+    Effect.gen(function*() {
+      const counter = yield* (Counter.make())
+      yield* pipe(
         ScopedRef.make(() => 0),
         Effect.flatMap((ref) =>
           pipe(
@@ -68,8 +68,8 @@ describe("ScopedRef", () => {
         ),
         Effect.scoped
       )
-      const acquired = yield* $(counter.acquired())
-      const released = yield* $(counter.released())
+      const acquired = yield* (counter.acquired())
+      const released = yield* (counter.released())
       strictEqual(acquired, 3)
       strictEqual(released, 3)
     }))
