ca0b831b70c2af347d2e230f2918fbdf370a205f
Giulio Canti <giulio.canti@gmail.com>
2025-02-14 11:22:37 +0100

    Cause

--- packages/effect/dtslint/Cause.tst.ts
+++ packages/effect/dtslint/Cause.tst.ts
@@ -2,24 +2,25 @@ import type { Predicate } from "effect"
 import { Cause, hole, pipe } from "effect"
 import { describe, expect, it } from "tstyche"
 
-declare const err1: Cause.Cause<"err-1">
-declare const err2: Cause.Cause<"err-2">
+declare const cause1: Cause.Cause<"err-1">
+declare const cause2: Cause.Cause<"err-2">
 
 describe("Cause", () => {
   describe("andThen", () => {
     it("data first", () => {
-      expect(Cause.andThen(err1, err2)).type.toBe<Cause.Cause<"err-2">>()
-      expect(Cause.andThen(err1, () => err2)).type.toBe<Cause.Cause<"err-2">>()
+      expect(Cause.andThen(cause1, cause2)).type.toBe<Cause.Cause<"err-2">>()
+      expect(Cause.andThen(cause1, () => cause2)).type.toBe<Cause.Cause<"err-2">>()
     })
 
     it("data last", () => {
-      expect(err1.pipe(Cause.andThen(err2))).type.toBe<Cause.Cause<"err-2">>()
-      expect(err1.pipe(Cause.andThen(() => err2))).type.toBe<Cause.Cause<"err-2">>()
+      expect(cause1.pipe(Cause.andThen(cause2))).type.toBe<Cause.Cause<"err-2">>()
+      expect(cause1.pipe(Cause.andThen(() => cause2))).type.toBe<Cause.Cause<"err-2">>()
     })
   })
 
   it("filter", () => {
-    expect(Cause.filter(err1, hole<Predicate.Predicate<Cause.Cause<string>>>())).type.toBe<Cause.Cause<"err-1">>()
-    expect(pipe(err1, Cause.filter(hole<Predicate.Predicate<Cause.Cause<string>>>()))).type.toBe<Cause.Cause<"err-1">>()
+    const predicate = hole<Predicate.Predicate<Cause.Cause<string>>>()
+    expect(Cause.filter(cause1, predicate)).type.toBe<Cause.Cause<"err-1">>()
+    expect(pipe(cause1, Cause.filter(predicate))).type.toBe<Cause.Cause<"err-1">>()
   })
 })
