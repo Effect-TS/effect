71b6ac5619066701d28ef3513e2e6cc36451a2f3
Giulio Canti <giulio.canti@gmail.com>
2025-02-14 14:43:09 +0100

    Config

--- packages/effect/dtslint/Config.tst.ts
+++ packages/effect/dtslint/Config.tst.ts
@@ -3,8 +3,8 @@ import { describe, expect, it } from "tstyche"
 
 declare const string: Config.Config<string>
 declare const number: Config.Config<number>
-declare const stringArray: Array<Config.Config<string>>
-declare const numberRecord: Record<string, Config.Config<number>>
+declare const array: Array<Config.Config<string>>
+declare const record: Record<string, Config.Config<number>>
 
 describe("Config", () => {
   describe("all", () => {
@@ -19,17 +19,17 @@ describe("Config", () => {
     })
 
     it("array", () => {
-      expect(Config.all(stringArray)).type.toBe<Config.Config<Array<string>>>()
-      expect(pipe(stringArray, Config.all)).type.toBe<Config.Config<Array<string>>>()
+      expect(Config.all(array)).type.toBe<Config.Config<Array<string>>>()
+      expect(pipe(array, Config.all)).type.toBe<Config.Config<Array<string>>>()
     })
 
     it("record", () => {
-      expect(Config.all(numberRecord)).type.toBe<Config.Config<Record<string, number>>>()
-      expect(pipe(numberRecord, Config.all)).type.toBe<Config.Config<Record<string, number>>>()
+      expect(Config.all(record)).type.toBe<Config.Config<Record<string, number>>>()
+      expect(pipe(record, Config.all)).type.toBe<Config.Config<Record<string, number>>>()
     })
   })
 
-  it("Success", () => {
+  it("Config.Success helper type", () => {
     expect(hole<Config.Config.Success<typeof string>>()).type.toBe<string>()
     expect(hole<Config.Config.Success<typeof number>>()).type.toBe<number>()
     const config = Config.all({ a: string, b: number })
