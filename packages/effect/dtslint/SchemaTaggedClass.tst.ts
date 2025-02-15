e6b7f7836803216e5b7426e0e409c8c410aac15b
Giulio Canti <giulio.canti@gmail.com>
2025-02-15 18:31:37 +0100

    Schema.TaggedClass

---
+++ packages/effect/dtslint/SchemaTaggedClass.tst.ts
@@ -0,0 +1,18 @@
+import { Schema } from "effect"
+import { describe, expect, it } from "tstyche"
+
+describe("Schema.TaggedClass", () => {
+  it("Annotations as tuple", () => {
+    // @ts-expect-error
+    class _A extends Schema.TaggedClass<_A>()("A", { id: Schema.Number }, [
+      undefined,
+      undefined,
+      {
+        pretty: () => (x) => {
+          expect(x).type.toBe<{ readonly _tag: "A" } & { readonly id: number }>()
+          return ""
+        }
+      }
+    ]) {}
+  })
+})
