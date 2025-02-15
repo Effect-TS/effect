c5853f42dcee7f1b16a61ac6158918d2dba0ffc9
Giulio Canti <giulio.canti@gmail.com>
2025-02-15 09:58:27 +0100

    ManagedRuntime

---
+++ packages/effect/dtslint/ManagedRuntime.tst.ts
@@ -0,0 +1,14 @@
+import type { ManagedRuntime } from "effect"
+import { describe, expect, it } from "tstyche"
+
+declare const runtime: ManagedRuntime.ManagedRuntime<"context", "error">
+
+describe("ManagedRuntime", () => {
+  it("ManagedRuntime.Context type helper", () => {
+    expect<ManagedRuntime.ManagedRuntime.Context<typeof runtime>>().type.toBe<"context">()
+  })
+
+  it("ManagedRuntime.Error type helper", () => {
+    expect<ManagedRuntime.ManagedRuntime.Error<typeof runtime>>().type.toBe<"error">()
+  })
+})
