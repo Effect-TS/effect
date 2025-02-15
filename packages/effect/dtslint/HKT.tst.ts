02a3b5cf52036489d39ba8e04565f490d20b6c1c
Giulio Canti <giulio.canti@gmail.com>
2025-02-15 09:41:07 +0100

    HKT

---
+++ packages/effect/dtslint/HKT.tst.ts
@@ -0,0 +1,9 @@
+import type { HKT } from "effect"
+
+// issue #536
+export function testIssue536<F extends HKT.TypeLambda, G extends HKT.TypeLambda, R, W, E, A>(
+  x: HKT.Kind<F, R, W, E, A>
+): HKT.Kind<G, R, W, E, A> {
+  // @ts-expect-error
+  return x
+}
