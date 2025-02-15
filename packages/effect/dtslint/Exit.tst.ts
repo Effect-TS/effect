54cab0b9055e52e968ef23c331e6655bc1e2ec10
Giulio Canti <giulio.canti@gmail.com>
2025-02-15 09:13:43 +0100

    Exit

---
+++ packages/effect/dtslint/Exit.tst.ts
@@ -0,0 +1,43 @@
+import * as Exit from "effect/Exit"
+import { pipe } from "effect/Function"
+import * as Predicate from "effect/Predicate"
+import { describe, expect, it } from "tstyche"
+
+declare const number$string: Exit.Exit<number, string>
+declare const stringOrNumber$string: Exit.Exit<string | number, string>
+
+describe("Exit", () => {
+  it("exists", () => {
+    if (Exit.exists(stringOrNumber$string, Predicate.isString)) {
+      expect(stringOrNumber$string).type.toBe<Exit.Exit<string, never>>()
+    }
+    // TODO
+    // if (pipe(stringOrNumber$string, Exit.exists(Predicate.isString))) {
+    //   expect(stringOrNumber$string).type.toBe<Exit.Exit<string, never>>()
+    // }
+    if (Exit.exists(Predicate.isString)(stringOrNumber$string)) {
+      expect(stringOrNumber$string).type.toBe<Exit.Exit<string, never>>()
+    }
+
+    if (
+      pipe(
+        number$string,
+        Exit.exists((n) => {
+          expect(n).type.toBe<number>()
+          return true
+        })
+      )
+    ) {
+      expect(number$string).type.toBe<Exit.Exit<number, string>>()
+    }
+
+    if (
+      pipe(
+        number$string,
+        Exit.exists((_sn: string | number) => true)
+      )
+    ) {
+      expect(number$string).type.toBe<Exit.Exit<number, string>>()
+    }
+  })
+})
