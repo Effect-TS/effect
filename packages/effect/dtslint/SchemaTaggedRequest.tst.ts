09dde8b1ab40aab338d9be2e4901794d39c47e42
Giulio Canti <giulio.canti@gmail.com>
2025-02-15 18:37:27 +0100

    Schema.TaggedRequest

---
+++ packages/effect/dtslint/SchemaTaggedRequest.tst.ts
@@ -0,0 +1,46 @@
+import { Schema } from "effect"
+import { describe, expect, it } from "tstyche"
+
+describe("Schema.TaggedRequest", () => {
+  it("should expose fields, _tag, success and failure", () => {
+    class A extends Schema.TaggedRequest<A>()("A", {
+      failure: Schema.String,
+      success: Schema.Number,
+      payload: {
+        id: Schema.Number
+      }
+    }) {}
+
+    expect(A.fields)
+      .type.toBe<{ readonly _tag: Schema.tag<"A">; readonly id: typeof Schema.Number }>()
+
+    expect(A._tag)
+      .type.toBe<"A">()
+
+    expect(A.success)
+      .type.toBe<typeof Schema.Number>()
+
+    expect(A.failure)
+      .type.toBe<typeof Schema.String>()
+  })
+
+  it("Annotations as tuple", () => {
+    // @ts-expect-error
+    class _A extends Schema.TaggedRequest<_A>()("A", {
+      failure: Schema.String,
+      success: Schema.Number,
+      payload: {
+        id: Schema.Number
+      }
+    }, [
+      undefined,
+      undefined,
+      {
+        pretty: () => (x) => {
+          expect(x).type.toBe<{ readonly _tag: "A" } & { readonly id: number }>()
+          return ""
+        }
+      }
+    ]) {}
+  })
+})
