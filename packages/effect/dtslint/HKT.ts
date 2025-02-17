cf2b8308c15d0d2215b2bbdfecc2d28ec4c98cb9
Giulio Canti <giulio.canti@gmail.com>
2025-02-17 10:48:25 +0100

    remove old files

--- packages/effect/dtslint/HKT.ts
+++
@@ -1,9 +0,0 @@
-import type * as _ from "effect/HKT"
-
-// issue #536
-export function testIssue536<F extends _.TypeLambda, G extends _.TypeLambda, R, W, E, A>(
-  x: _.Kind<F, R, W, E, A>
-): _.Kind<G, R, W, E, A> {
-  // @ts-expect-error
-  return x
-}
--- packages/effect/dtslint/Runtime.ts
+++
@@ -1,4 +0,0 @@
-import type * as Runtime from "effect/Runtime"
-
-// $ExpectType { foo: string; }
-export type ContextOfRuntime = Runtime.Runtime.Context<Runtime.Runtime<{ foo: string }>>
