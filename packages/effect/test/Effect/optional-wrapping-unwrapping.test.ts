b95346bdeb756ba5a856a37903ef475560c89a2d
Vincent François <vincent.francois@inato.com>
2025-02-18 17:35:35 +0100

    Add `Either.transposeOption` (#4466)

--- packages/effect/test/Effect/optional-wrapping-unwrapping.test.ts
+++ packages/effect/test/Effect/optional-wrapping-unwrapping.test.ts
@@ -1,5 +1,6 @@
 import { assert, describe, it } from "@effect/vitest"
 import * as Effect from "effect/Effect"
+import * as Either from "effect/Either"
 import * as Option from "effect/Option"
 
 describe("Effect", () => {
@@ -13,6 +14,22 @@ describe("Effect", () => {
     it.effect("Some", () =>
       Effect.gen(function*() {
         const result = yield* Effect.transposeOption(Option.some(Effect.succeed(42)))
+        assert.deepStrictEqual(result, Option.some(42))
+      }))
+  })
+})
+
+describe("Either", () => {
+  describe("transposeOption", () => {
+    it.effect("None", () =>
+      Effect.gen(function*() {
+        const result = yield* Either.transposeOption(Option.none())
+        assert.ok(Option.isNone(result))
+      }))
+
+    it.effect("Some", () =>
+      Effect.gen(function*() {
+        const result = yield* Either.transposeOption(Option.some(Either.right(42)))
         assert.deepStrictEqual(result, Option.some(42))
       }))
   })
