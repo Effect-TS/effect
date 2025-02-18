780dec2c18e137f7156cfd135531876e1b3462d8
Sebastian Lorenz <fubhy@fubhy.com>
2025-02-18 17:32:14 +0100

    suppress warning for experimental features (#4470)

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,21 @@
 import { addEqualityTesters } from "@effect/vitest"
 
 addEqualityTesters()
+
+// Ignore warnings from usage of experimental features to declutter test output.
+const ignore = ["ExperimentalWarning"]
+const emitWarning = process.emitWarning
+process.emitWarning = (warning, ...args) => {
+  const [head] = args
+  if (head != null) {
+    if (typeof head === "string" && ignore.includes(head)) {
+      return
+    }
+
+    if (typeof head === "object" && ignore.includes(head.type)) {
+      return
+    }
+  }
+
+  return emitWarning(warning, ...args)
+}
