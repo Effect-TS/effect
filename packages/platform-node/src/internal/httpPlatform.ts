1d07549bb63771e452bac9def0dd37fae33d5fea
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2025-02-20 21:40:10 +0100

    chore: add default utf-8 charset to node file responses

--- packages/platform-node/src/internal/httpPlatform.ts
+++ packages/platform-node/src/internal/httpPlatform.ts
@@ -9,14 +9,28 @@ import * as Fs from "node:fs"
 import { Readable } from "node:stream"
 import * as FileSystem from "../NodeFileSystem.js"
 
+const getMimeTypeWithCharset = (path: string, charset = "utf-8") => {
+  const mimeType = Mime.getType(path)
+
+  if (mimeType) {
+    // in case of non-text types,
+    // the charset should be ignored by the client
+    return `${mimeType};charset=${charset}`
+  }
+
+  return null
+}
+
 /** @internal */
 export const make = Platform.make({
   fileResponse(path, status, statusText, headers, start, end, contentLength) {
     const stream = Fs.createReadStream(path, { start, end })
+
     return ServerResponse.raw(stream, {
       headers: {
         ...headers,
-        "content-type": headers["content-type"] ?? Mime.getType(path) ?? "application/octet-stream",
+        "content-type": headers["content-type"]
+          ?? getMimeTypeWithCharset(path) ?? "application/octet-stream",
         "content-length": contentLength.toString()
       },
       status,
@@ -28,7 +42,8 @@ export const make = Platform.make({
       headers: Headers.merge(
         headers,
         Headers.unsafeFromRecord({
-          "content-type": headers["content-type"] ?? Mime.getType(file.name) ?? "application/octet-stream",
+          "content-type": headers["content-type"]
+            ?? getMimeTypeWithCharset(file.name) ?? "application/octet-stream",
           "content-length": file.size.toString()
         })
       ),
