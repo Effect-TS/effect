62496662936800d192ae1383875342b617094521
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-02-26 14:08:55 -0500

    add latest claude

--- packages/ai/anthropic/src/Generated.ts
+++ packages/ai/anthropic/src/Generated.ts
@@ -14,17 +14,16 @@ export class MessagesPostParams extends S.Struct({
 }) {}
 
 export class ModelEnum extends S.Literal(
-  "claude-3-5-haiku-latest",
-  "claude-3-5-haiku-20241022",
+  "claude-3-7-sonnet-latest",
+  "claude-3-7-sonnet-20250219",
   "claude-3-5-sonnet-latest",
   "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-20240620",
+  "claude-3-5-haiku-latest",
+  "claude-3-5-haiku-20241022",
+  "claude-3-5-haiku-20240307",
   "claude-3-opus-latest",
-  "claude-3-opus-20240229",
-  "claude-3-sonnet-20240229",
-  "claude-3-haiku-20240307",
-  "claude-2.1",
-  "claude-2.0"
+  "claude-3-opus-20240229"
 ) {}
 
 export class Model extends S.Union(S.String, ModelEnum) {}
