211e67ede8d446f7dc76bfa39bda0bd9de8d67fb
Tim <hello@timsmart.co>
2025-03-01 21:31:10 +1300

    check if defined

--- packages/platform/src/internal/fetchHttpClient.ts
+++ packages/platform/src/internal/fetchHttpClient.ts
@@ -16,7 +16,7 @@ const fetch: Client.HttpClient = client.make((request, url, signal, fiber) => {
   const context = fiber.getFiberRef(FiberRef.currentContext)
   const fetch: typeof globalThis.fetch = context.unsafeMap.get(fetchTagKey) ?? globalThis.fetch
   const options: RequestInit = context.unsafeMap.get(requestInitTagKey) ?? {}
-  const headers = Headers.merge(Headers.fromInput(options.headers), request.headers)
+  const headers = options.headers ? Headers.merge(Headers.fromInput(options.headers), request.headers) : request.headers
   const send = (body: BodyInit | undefined) =>
     Effect.map(
       Effect.tryPromise({
