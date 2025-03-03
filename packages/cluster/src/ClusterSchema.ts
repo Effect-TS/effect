929bb71d0579de96b754135dcfbcabb1ef5d3c55
Tim <hello@timsmart.co>
2025-03-03 19:45:03 +1300

    disable message persistence by default

--- packages/cluster/src/ClusterSchema.ts
+++ packages/cluster/src/ClusterSchema.ts
@@ -2,12 +2,12 @@
  * @since 1.0.0
  */
 import * as Context from "effect/Context"
-import { constTrue } from "effect/Function"
+import { constFalse } from "effect/Function"
 
 /**
  * @since 1.0.0
  * @category Annotations
  */
 export class Persisted extends Context.Reference<Persisted>()("@effect/cluster/ClusterSchema/Persisted", {
-  defaultValue: constTrue
+  defaultValue: constFalse
 }) {}
