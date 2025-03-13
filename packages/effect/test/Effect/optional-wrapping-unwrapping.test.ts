3bea9764c6f4bd2a4ac41e0e832b2905e1bbf421
Laure Retru-Chavastel <laure.retruchavastel@gmail.com>
2025-03-13 14:45:55 +0100

    Add Effect.transposeMapOption (#4597)

--- packages/effect/test/Effect/optional-wrapping-unwrapping.test.ts
+++ packages/effect/test/Effect/optional-wrapping-unwrapping.test.ts
@@ -1,6 +1,7 @@
 import { assert, describe, it } from "@effect/vitest"
 import * as Effect from "effect/Effect"
 import * as Either from "effect/Either"
+import { pipe } from "effect/Function"
 import * as Option from "effect/Option"
 
 describe("Effect", () => {
@@ -16,6 +17,66 @@ describe("Effect", () => {
         const result = yield* Effect.transposeOption(Option.some(Effect.succeed(42)))
         assert.deepStrictEqual(result, Option.some(42))
       }))
+  })
+  describe("transposeMapOption", () => {
+    describe("None", () => {
+      it.effect("Success", () =>
+        Effect.gen(function*() {
+          const resultDataFirst = yield* Effect.transposeMapOption(Option.none(), () => Effect.succeed(42))
+          assert.ok(Option.isNone(resultDataFirst))
+
+          const resultDataLast = yield* pipe(
+            Option.none(),
+            Effect.transposeMapOption(() => Effect.succeed(42))
+          )
+          assert.ok(Option.isNone(resultDataLast))
+        }))
+      it.effect("Failure", () =>
+        Effect.gen(function*() {
+          const resultDataFirst = yield* Effect.transposeMapOption(Option.none(), () => Effect.fail("Error"))
+          assert.ok(Option.isNone(resultDataFirst))
+
+          const resultDataLast = yield* pipe(
+            Option.none(),
+            Effect.transposeMapOption(() => Effect.fail("Error"))
+          )
+          assert.ok(Option.isNone(resultDataLast))
+        }))
+    })
+
+    describe("Some", () => {
+      describe("None", () => {
+        it.effect("Success", () =>
+          Effect.gen(function*() {
+            const resultDataFirst = yield* Effect.transposeMapOption(Option.some(42), (value) =>
+              Effect.succeed(value * 2))
+            assert.deepStrictEqual(resultDataFirst, Option.some(84))
+
+            const resultDataLast = yield* pipe(
+              Option.some(42),
+              Effect.transposeMapOption((value) =>
+                Effect.succeed(value * 2)
+              )
+            )
+            assert.deepStrictEqual(resultDataLast, Option.some(84))
+          }))
+        it.effect("Failure", () =>
+          Effect.gen(function*() {
+            const resultDataFirst = yield* pipe(
+              Effect.transposeMapOption(Option.some(42), () => Effect.fail("error")),
+              Effect.flip
+            )
+            assert.equal(resultDataFirst, "error")
+
+            const resultDataLast = yield* pipe(
+              Option.some(42),
+              Effect.transposeMapOption(() => Effect.fail("error")),
+              Effect.flip
+            )
+            assert.equal(resultDataLast, "error")
+          }))
+      })
+    })
   })
 })
 
