4e53177472ae1235e4ea9e1eb1f9935b57f57564
vinassefranche <vincent.francois@inato.com>
2025-03-17 09:22:17 +0100

    Update tests

--- packages/effect/test/TestClock.test.ts
+++ packages/effect/test/TestClock.test.ts
@@ -4,18 +4,24 @@ import { deepStrictEqual } from "effect/test/util"
 
 describe("TestClock", () => {
   describe("setTime", () => {
-    const arbitraryDate = DateTime.unsafeMake("2023-12-31T11:00:00.000Z")
+    const arbitraryDateTime = DateTime.unsafeMake("2023-12-31T11:00:00.000Z")
     it.effect("should set the current Date using an Instant", () =>
       Effect.gen(function*() {
-        yield* TestClock.setTime(arbitraryDate)
+        yield* TestClock.setTime(arbitraryDateTime.epochMillis)
         const now = yield* DateTime.now
-        deepStrictEqual(now, arbitraryDate)
+        deepStrictEqual(now, arbitraryDateTime)
       }))
     it.effect("should set the current time using a DateTime", () =>
       Effect.gen(function*() {
-        yield* TestClock.setTime(arbitraryDate)
+        yield* TestClock.setTime(arbitraryDateTime)
         const now = yield* DateTime.now
-        deepStrictEqual(now, arbitraryDate)
+        deepStrictEqual(now, arbitraryDateTime)
+      }))
+    it.effect("should set the current time using a Date", () =>
+      Effect.gen(function*() {
+        yield* TestClock.setTime(DateTime.toDate(arbitraryDateTime))
+        const now = yield* DateTime.now
+        deepStrictEqual(now, arbitraryDateTime)
       }))
   })
 })
