b29026a47fa14b21ab7af22249c51da18188dd54
Maxim Khramtsov <khraks.mamtsov@gmail.com>
2025-04-02 14:13:56 +0200

    fix Effect.liftPredicate

--- packages/effect/dtslint/Effect.tst.ts
+++ packages/effect/dtslint/Effect.tst.ts
@@ -1256,7 +1256,7 @@ describe("Effect", () => {
     expect(pipe(
       primitiveNumberOrString,
       Effect.liftPredicate(Predicate.isString, (sn) => {
-        expect(sn).type.toBe<string | number>()
+        expect(sn).type.toBe<unknown>()
         return "b" as const
       })
     )).type.toBe<Effect.Effect<string, "b">>()
@@ -1264,6 +1264,15 @@ describe("Effect", () => {
       expect(sn).type.toBe<string | number>()
       return "b" as const
     })).type.toBe<Effect.Effect<string, "b">>()
+
+    expect(Effect.liftPredicate(hole<Predicate.Refinement<string | number, number>>(), (sn: string) => {
+      expect(sn).type.toBe<string | number>()
+      return "b" as const
+    })).type.toBe<(a: string | number) => Effect.Effect<number, "b">>()
+    expect(Effect.liftPredicate(Predicate.isString, (sn) => {
+      expect(sn).type.toBe<string | number>()
+      return "b" as const
+    })).type.toBe<(a: unknown) => Effect.Effect<string, "b">>()
 
     expect(pipe(
       primitiveNumberOrString,
@@ -1301,12 +1310,12 @@ describe("Effect", () => {
     expect(pipe(
       primitiveNumber,
       Effect.liftPredicate(predicateNumbersOrStrings, (n) => {
-        expect(n).type.toBe<number>()
+        expect(n).type.toBe<string | number>()
         return "b" as const
       })
     )).type.toBe<Effect.Effect<number, "b">>()
     expect(Effect.liftPredicate(primitiveNumber, predicateNumbersOrStrings, (n) => {
-      expect(n).type.toBe<number>()
+      expect(n).type.toBe<string | number>()
       return "b" as const
     })).type.toBe<Effect.Effect<number, "b">>()
 
