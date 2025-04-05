1e7528bc52de10dbca88cbe8c487eb86a57d7007
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-04-05 19:43:19 +0200

    test(effect/Number): add initial test scaffolding for Number
    Introduced a scaffold for testing the Number module in the effect
    package. Placeholder test cases using `it.todo` were added for
    various number operations and utilities to ensure comprehensive
    future coverage. This sets the groundwork for implementing and
    validating number-related functionality.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

---
+++ packages/effect/dtslint/Number.tst.ts
@@ -0,0 +1,54 @@
+import { Int, Number, pipe } from "effect"
+import { describe, expect, it } from "tstyche"
+
+describe("Number", () => {
+  it.todo("isNumber", () => {})
+
+  it.todo("sum", () => {})
+
+  it.todo("subtract", () => {})
+
+  it.todo("multiply", () => {})
+
+  it.todo("divide", () => {})
+
+  it.todo("unsafeDivide", () => {})
+
+  it.todo("increment", () => {})
+
+  it.todo("decrement", () => {})
+
+  it.todo("Equivalence", () => {})
+
+  it.todo("Order", () => {})
+
+  it.todo("lessThan", () => {})
+
+  it.todo("lessThanOrEqualTo", () => {})
+
+  it.todo("greaterThan", () => {})
+
+  it.todo("greaterThanOrEqualTo", () => {})
+
+  it.todo("between", () => {})
+
+  it.todo("clamp", () => {})
+
+  it.todo("min", () => {})
+
+  it.todo("max", () => {})
+
+  it.todo("sign", () => {})
+
+  it.todo("sumAll", () => {})
+
+  it.todo("multiplyAll", () => {})
+
+  it.todo("remainder", () => {})
+
+  it.todo("nextPow2", () => {})
+
+  it.todo("parse", () => {})
+
+  it.todo("round", () => {})
+})
