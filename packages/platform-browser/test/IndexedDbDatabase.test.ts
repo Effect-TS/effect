5ebed248871daba0a95965c09d9a25c2d15b30c1
SandroMaglione <lass.maglio@gmail.com>
2025-04-09 06:16:53 +0200

    `use` in `IndexedDbApi`

--- packages/platform-browser/test/IndexedDbDatabase.test.ts
+++ packages/platform-browser/test/IndexedDbDatabase.test.ts
@@ -27,36 +27,37 @@ const Db = IndexedDbVersion.make(Table)
 describe("IndexedDbDatabase", () => {
   it.effect("insert and read todos", () =>
     Effect.gen(function*() {
-      const { makeApi } = yield* IndexedDbQuery.IndexedDbApi
+      const { makeApi, use } = yield* IndexedDbQuery.IndexedDbApi
       const api = makeApi(Db)
       const todo = yield* api.getAll("todo")
+      const name = yield* use(async (database) => database.name)
+      const version = yield* use(async (database) => database.version)
+      const objectStoreNames = yield* use(async (database) => database.objectStoreNames)
+      assert.equal(name, "db")
+      assert.equal(version, 1)
       assert.deepStrictEqual(todo, [{ id: 1, title: "test", completed: false }])
+      assert.deepStrictEqual(Array.from(objectStoreNames), ["todo"])
     }).pipe(
       Effect.provide(
         IndexedDbQuery.layer.pipe(
           Layer.provide(
-            Layer.unwrapEffect(
-              Effect.gen(function*() {
-                return IndexedDbDatabase.layer(
-                  "db",
-                  IndexedDbMigration.make({
-                    fromVersion: IndexedDbVersion.makeEmpty,
-                    toVersion: Db,
-                    execute: (_, toQuery) =>
-                      Effect.gen(function*() {
-                        yield* toQuery.createObjectStore("todo")
-                        yield* toQuery.insert("todo", {
-                          id: 1,
-                          title: "test",
-                          completed: false
-                        })
-                      })
+            IndexedDbDatabase.layer(
+              "db",
+              IndexedDbMigration.make({
+                fromVersion: IndexedDbVersion.makeEmpty,
+                toVersion: Db,
+                execute: (_, toQuery) =>
+                  Effect.gen(function*() {
+                    yield* toQuery.createObjectStore("todo")
+                    yield* toQuery.insert("todo", {
+                      id: 1,
+                      title: "test",
+                      completed: false
+                    })
                   })
-                )
               })
-            )
-          ),
-          Layer.provide([layerFakeIndexedDb])
+            ).pipe(Layer.provide(layerFakeIndexedDb))
+          )
         )
       )
     ))
