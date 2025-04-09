81c3ec763707515b0d040706a87c8e180dd06474
SandroMaglione <lass.maglio@gmail.com>
2025-04-09 06:31:50 +0200

    migration sequence test

--- packages/platform-browser/test/IndexedDbDatabase.test.ts
+++ packages/platform-browser/test/IndexedDbDatabase.test.ts
@@ -12,21 +12,21 @@ import { indexedDB } from "fake-indexeddb"
 
 const layerFakeIndexedDb = Layer.succeed(IndexedDb.IndexedDb, IndexedDb.make({ indexedDB }))
 
-const Table = IndexedDbTable.make(
-  "todo",
-  Schema.Struct({
-    id: Schema.Number,
-    title: Schema.String,
-    completed: Schema.Boolean
-  }),
-  { keyPath: "id" }
-)
+describe.sequential("IndexedDbDatabase", () => {
+  it.effect("insert and read todos", () => {
+    const Table = IndexedDbTable.make(
+      "todo",
+      Schema.Struct({
+        id: Schema.Number,
+        title: Schema.String,
+        completed: Schema.Boolean
+      }),
+      { keyPath: "id" }
+    )
 
-const Db = IndexedDbVersion.make(Table)
+    const Db = IndexedDbVersion.make(Table)
 
-describe("IndexedDbDatabase", () => {
-  it.effect("insert and read todos", () =>
-    Effect.gen(function*() {
+    return Effect.gen(function*() {
       const { makeApi, use } = yield* IndexedDbQuery.IndexedDbApi
       const api = makeApi(Db)
       const todo = yield* api.getAll("todo")
@@ -37,6 +37,9 @@ describe("IndexedDbDatabase", () => {
       assert.equal(version, 1)
       assert.deepStrictEqual(todo, [{ id: 1, title: "test", completed: false }])
       assert.deepStrictEqual(Array.from(objectStoreNames), ["todo"])
+
+      // Close database to avoid errors when running other tests (blocked access)
+      yield* use(async (database) => database.close())
     }).pipe(
       Effect.provide(
         IndexedDbQuery.layer.pipe(
@@ -60,5 +63,89 @@ describe("IndexedDbDatabase", () => {
           )
         )
       )
-    ))
+    )
+  })
+
+  it.effect("migration sequence", () => {
+    const Table1 = IndexedDbTable.make(
+      "todo",
+      Schema.Struct({
+        id: Schema.Number,
+        title: Schema.String,
+        completed: Schema.Boolean
+      }),
+      { keyPath: "id" }
+    )
+
+    const Table2 = IndexedDbTable.make(
+      "todo",
+      Schema.Struct({
+        uuid: Schema.UUID,
+        title: Schema.String,
+        completed: Schema.Boolean
+      }),
+      { keyPath: "uuid" }
+    )
+
+    const Db1 = IndexedDbVersion.make(Table1)
+    const Db2 = IndexedDbVersion.make(Table2)
+    const uuid = "9535a059-a61f-42e1-a2e0-35ec87203c24"
+
+    return Effect.gen(function*() {
+      const { makeApi, use } = yield* IndexedDbQuery.IndexedDbApi
+      const api = makeApi(Db2)
+      const todo = yield* api.getAll("todo")
+      const name = yield* use(async (database) => database.name)
+      const version = yield* use(async (database) => database.version)
+      const objectStoreNames = yield* use(async (database) => database.objectStoreNames)
+      assert.equal(name, "db")
+      assert.equal(version, 2)
+      assert.deepStrictEqual(todo, [{ uuid, title: "test", completed: false }])
+      assert.deepStrictEqual(Array.from(objectStoreNames), ["todo"])
+
+      // Close database to avoid errors when running other tests (blocked access)
+      yield* use(async (database) => database.close())
+    }).pipe(
+      Effect.provide(
+        IndexedDbQuery.layer.pipe(
+          Layer.provide(
+            IndexedDbDatabase.layer(
+              "db",
+              IndexedDbMigration.make({
+                fromVersion: IndexedDbVersion.makeEmpty,
+                toVersion: Db1,
+                execute: (_, toQuery) =>
+                  Effect.gen(function*() {
+                    yield* toQuery.createObjectStore("todo")
+                    yield* toQuery.insert("todo", {
+                      id: 1,
+                      title: "test",
+                      completed: false
+                    })
+                  })
+              }),
+              IndexedDbMigration.make({
+                fromVersion: Db1,
+                toVersion: Db2,
+                execute: (fromQuery, toQuery) =>
+                  Effect.gen(function*() {
+                    const todo = yield* fromQuery.getAll("todo")
+                    yield* fromQuery.deleteObjectStore("todo")
+                    yield* toQuery.createObjectStore("todo")
+                    yield* toQuery.insertAll(
+                      "todo",
+                      todo.map((t) => ({
+                        uuid,
+                        title: t.title,
+                        completed: t.completed
+                      }))
+                    )
+                  })
+              })
+            ).pipe(Layer.provide(layerFakeIndexedDb))
+          )
+        )
+      )
+    )
+  })
 })
