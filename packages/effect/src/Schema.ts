92101b6a286991fd3a13182d056556e8dcad4f33
Alpha Tawanda Shuro <alphashuro@gmail.com>
2025-04-15 16:01:20 +0000

    fixup! feat (effect/Schema): add `taggedUnion`

--- packages/effect/src/Schema.ts
+++ packages/effect/src/Schema.ts
@@ -46,7 +46,7 @@ import * as record from "./Record.js"
 import * as redacted_ from "./Redacted.js"
 import * as Request from "./Request.js"
 import * as scheduler_ from "./Scheduler.js"
-import type { ParseOptions, LiteralValue } from "./SchemaAST.js"
+import type { LiteralValue, ParseOptions } from "./SchemaAST.js"
 import * as AST from "./SchemaAST.js"
 import * as sortedSet_ from "./SortedSet.js"
 import * as string_ from "./String.js"
@@ -8898,7 +8898,6 @@ const makeClass = <Fields extends Struct.Fields>(
 
   const [typeAnnotations, transformationAnnotations, encodedAnnotations] = getClassAnnotations(annotations)
 
-
   const declarationSurrogate = typeSchema_.annotations({
     identifier,
     ...typeAnnotations
@@ -10866,7 +10865,7 @@ export class ArrayFormatterIssue extends Struct({
  * @example
  * ```ts
  * import { Schema } from "effect"
- * 
+ *
  * const schema = Schema.taggedUnion({
  *   Circle: { radius: Schema.Number },
  *   Square: { sideLength: Schema.Number },
@@ -10880,21 +10879,12 @@ export class ArrayFormatterIssue extends Struct({
  */
 export function taggedUnion<
   Cases extends Record<string, Fields>,
-  Fields extends Struct.Fields,
->(
-  definitions: Cases,
-): Union<
-  {
-    [K in keyof Cases]: K extends LiteralValue
-      ? TaggedStruct<K, Cases[K]>
-      : never
-  }[keyof Cases][]
-> & {
-  [K in keyof Cases]: K extends LiteralValue
-    ? TaggedStruct<K, Cases[K]>
-    : never
-} {
-  const members = record.map(definitions, (fields, key) => TaggedStruct(key, fields))
+  Fields extends Struct.Fields
+>(cases: Cases):
+  & Union<Array<{ [K in keyof Cases]: K extends LiteralValue ? TaggedStruct<K, Cases[K]> : never }[keyof Cases]>>
+  & { [K in keyof Cases]: K extends LiteralValue ? TaggedStruct<K, Cases[K]> : never }
+{
+  const members = record.map(cases, (fields, key) => TaggedStruct(key, fields))
   const schema = Union(...record.values(members))
 
   return new Proxy(schema, {
@@ -10904,6 +10894,6 @@ export function taggedUnion<
       } else {
         return Reflect.get(target, key)
       }
-    },
+    }
   }) as any
 }
