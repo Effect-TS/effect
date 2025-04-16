24521f3a24ae49b918ce82882f0b1fe34615c35f
Tim <hello@timsmart.co>
2025-04-17 09:02:09 +1200

    wip

--- packages/workflow/src/WorkflowEngine.ts
+++ packages/workflow/src/WorkflowEngine.ts
@@ -3,6 +3,8 @@
  */
 import * as Context from "effect/Context"
 import type * as Effect from "effect/Effect"
+import type * as Exit from "effect/Exit"
+import type * as Option from "effect/Option"
 import type * as Schema from "effect/Schema"
 import type * as Activity from "./Activity.js"
 import type * as Workflow from "./Workflow.js"
@@ -30,6 +32,38 @@ export class WorkflowEngine extends Context.Tag("@effect/workflow/WorkflowEngine
       executionId: string,
       payload: object
     ) => Effect.Effect<unknown, unknown>
+
+    /**
+     * Tell a workflow to resume execution.
+     */
+    readonly resume: (
+      workflow: Workflow.Workflow<{}, Schema.Schema.Any, Schema.Schema.All, never>,
+      executionId: string
+    ) => Effect.Effect<void>
+
+    /**
+     * Try to retrieve the result of an activity execution.
+     */
+    readonly activityResult: (
+      options: {
+        readonly workflow: Workflow.Workflow<{}, Schema.Schema.Any, Schema.Schema.All, never>
+        readonly executionId: string
+        readonly activity: Activity.Activity<Schema.Schema.Any, Schema.Schema.All, never>
+        readonly attempt: number
+      }
+    ) => Effect.Effect<Option.Option<Exit.Exit<unknown, unknown>>>
+
+    /**
+     * Execute an activity from a workflow.
+     */
+    readonly activityExecute: (
+      options: {
+        readonly workflow: Workflow.Workflow<{}, Schema.Schema.Any, Schema.Schema.All, never>
+        readonly executionId: string
+        readonly activity: Activity.Activity<Schema.Schema.Any, Schema.Schema.All, never>
+        readonly attempt: number
+      }
+    ) => Effect.Effect<unknown, unknown>
   }
 >() {}
 
@@ -41,6 +75,7 @@ export class WorkflowInstance extends Context.Tag("@effect/workflow/WorkflowEngi
   WorkflowInstance,
   {
     readonly executionId: string
+    readonly payload: object
 
     /**
      * The workflow definition.
