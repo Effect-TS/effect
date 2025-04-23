2b514428810a3e4714b68129be498440c8525c29
Sebastian Lorenz <fubhy@fubhy.com>
2025-04-23 12:33:02 +0200

    fix imports

--- packages/platform-browser/test/KeyValueStore.test.ts
+++ packages/platform-browser/test/KeyValueStore.test.ts
@@ -1,7 +1,7 @@
 import * as Kv from "@effect/platform-browser/BrowserKeyValueStore"
-// @ts-ignore
-import { testLayer } from "@effect/platform/test/KeyValueStore.test"
 import { describe } from "@effect/vitest"
+// @ts-ignore
+import { testLayer } from "../../platform/test/KeyValueStore.test.js"
 
 describe("KeyValueStore / layerLocalStorage", () => testLayer(Kv.layerLocalStorage))
 describe("KeyValueStore / layerSessionStorage", () => testLayer(Kv.layerSessionStorage))
