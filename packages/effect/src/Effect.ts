2db99322b5bc238f3fd6fb99bda24d9e32aee981
André König <andre.koenig@openformation.io>
2025-04-23 18:43:41 +0200

    refactor: makes sure that the typedef matches the existing conventions

--- packages/effect/src/Effect.ts
+++ packages/effect/src/Effect.ts
@@ -3955,7 +3955,7 @@ export const catchTags: {
   <
     E,
     Cases extends
-      { [K in Extract<E, { _tag: string }>['_tag']]?: (error: Extract<E, { _tag: K }>) => Effect<any, any, any> }
+      { [K in Extract<E, { _tag: string }>["_tag"]]+?: ((error: Extract<E, { _tag: K }>) => Effect<any, any, any>) }
       & (unknown extends E ? {} : { [K in Exclude<keyof Cases, Extract<E, { _tag: string }>['_tag']>]: never })
   >(
     cases: Cases,
@@ -3964,18 +3964,18 @@ export const catchTags: {
     self: Effect<A, E, R>
   ) => Effect<
     | A
-    | { [K in keyof Cases]: Cases[K] extends (...args: any[]) => Effect<infer A1, any, any> ? A1 : never }[keyof Cases],
+    | { [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Effect<infer A1, any, any> ? A1 : never }[keyof Cases],
     | Exclude<E, { _tag: keyof Cases }>
-    | { [K in keyof Cases]: Cases[K] extends (...args: any[]) => Effect<any, infer E1, any> ? E1 : never }[keyof Cases],
+    | { [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Effect<any, infer E1, any> ? E1 : never }[keyof Cases],
     | R
-    | { [K in keyof Cases]: Cases[K] extends (...args: any[]) => Effect<any, any, infer R1> ? R1 : never }[keyof Cases]
+    | { [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Effect<any, any, infer R1> ? R1 : never }[keyof Cases]
   >
   <
     R,
     E,
     A,
     Cases extends
-      & { [K in Extract<E, { _tag: string }>["_tag"]]+?: ((error: Extract<E, { _tag: K }>) => Effect<any, any, any>) }
+      { [K in Extract<E, { _tag: string }>["_tag"]]+?: ((error: Extract<E, { _tag: K }>) => Effect<any, any, any>) }
       & (unknown extends E ? {} : { [K in Exclude<keyof Cases, Extract<E, { _tag: string }>["_tag"]>]: never })
   >(
     self: Effect<A, E, R>,
@@ -4007,11 +4007,11 @@ export const catchTags: {
     onOther: (error: Exclude<E, { _tag: keyof Cases }>) => Effect<any, any, any>
   ): Effect<
     | A
-    | { [K in keyof Cases]: Cases[K] extends (...args: any[]) => Effect<infer A1, any, any> ? A1 : never }[keyof Cases],
+    | { [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Effect<infer A1, any, any> ? A1 : never }[keyof Cases],
     | Exclude<E, { _tag: keyof Cases }>
-    | { [K in keyof Cases]: Cases[K] extends (...args: any[]) => Effect<any, infer E1, any> ? E1 : never }[keyof Cases],
+    | { [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Effect<any, infer E1, any> ? E1 : never }[keyof Cases],
     | R
-    | { [K in keyof Cases]: Cases[K] extends (...args: any[]) => Effect<any, any, infer R1> ? R1 : never }[keyof Cases]
+    | { [K in keyof Cases]: Cases[K] extends (...args: Array<any>) => Effect<any, any, infer R1> ? R1 : never }[keyof Cases]
   >
 } = effect.catchTags
 
