a9387b148b305c8732f43b2362bcd0c0025524ba
Tim <hello@timsmart.co>
2025-04-29 09:18:26 +1200

    improve completion

--- packages/effect/src/internal/core-effect.ts
+++ packages/effect/src/internal/core-effect.ts
@@ -237,16 +237,16 @@ export const catchSomeDefect = dual<
 
 /* @internal */
 export const catchTag = dual<
-  <E, const K extends ReadonlyArray<E extends { _tag: string } ? E["_tag"] : never>, A1, E1, R1>(
+  <E, const K extends Arr.NonEmptyReadonlyArray<E extends { _tag: string } ? E["_tag"] : never>, A1, E1, R1>(
     ...args: [...tags: K, f: (e: Extract<NoInfer<E>, { _tag: K[number] }>) => Effect.Effect<A1, E1, R1>]
   ) => <A, R>(self: Effect.Effect<A, E, R>) => Effect.Effect<A | A1, Exclude<E, { _tag: K[number] }> | E1, R | R1>,
-  <A, E, R, const K extends ReadonlyArray<E extends { _tag: string } ? E["_tag"] : never>, R1, E1, A1>(
+  <A, E, R, const K extends Arr.NonEmptyReadonlyArray<E extends { _tag: string } ? E["_tag"] : never>, R1, E1, A1>(
     self: Effect.Effect<A, E, R>,
     ...args: [...tags: K, f: (e: Extract<NoInfer<E>, { _tag: K[number] }>) => Effect.Effect<A1, E1, R1>]
   ) => Effect.Effect<A | A1, Exclude<E, { _tag: K[number] }> | E1, R | R1>
 >(
   (args: any) => core.isEffect(args[0]),
-  <A, E, R, const K extends ReadonlyArray<E extends { _tag: string } ? E["_tag"] : never>, R1, E1, A1>(
+  <A, E, R, const K extends Arr.NonEmptyReadonlyArray<E extends { _tag: string } ? E["_tag"] : never>, R1, E1, A1>(
     self: Effect.Effect<A, E, R>,
     ...args: [...tags: K, f: (e: Extract<NoInfer<E>, { _tag: K[number] }>) => Effect.Effect<A1, E1, R1>]
   ): Effect.Effect<A | A1, Exclude<E, { _tag: K[number] }> | E1, R | R1> => {
