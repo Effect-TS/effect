e234c12263ae8dd661b492e7dd91d5c2997f43bf
Tim <hello@timsmart.co>
2025-05-06 09:33:43 +1200

    allow dies with response

--- packages/platform/src/internal/httpServerError.ts
+++ packages/platform/src/internal/httpServerError.ts
@@ -40,7 +40,7 @@ export const causeResponse = <E>(
           return Option.some([Respondable.toResponseOrElse(cause.error, internalServerError), cause] as const)
         }
         case "Die": {
-          return Option.some([Effect.succeed(internalServerError), cause] as const)
+          return Option.some([Respondable.toResponseOrElseDefect(cause.defect, internalServerError), cause] as const)
         }
         case "Interrupt": {
           if (acc[1]._tag !== "Empty") {
