140d34384ecc1cbcaf09566540fd4998621588de
Tim <hello@timsmart.co>
2025-05-09 12:20:02 +1200

    prevent key conflicts and add $match

--- packages/effect/test/Schema/Schema/taggedUnion.test.ts
+++ packages/effect/test/Schema/Schema/taggedUnion.test.ts
@@ -1,21 +1,40 @@
 import { describe, it } from "@effect/vitest"
+import * as Equal from "effect/Equal"
 import * as S from "effect/Schema"
 import * as Util from "effect/test/Schema/TestUtils"
+import { strictEqual } from "node:assert"
 
 describe("taggedUnion", () => {
-  it("should create a union of tagged structs", () => {
-    const schema = S.taggedUnion({
-      A: {},
-      B: { value: S.String },
-      C: { otherValue: S.Number }
+  const schema = S.taggedUnion({
+    A: {},
+    B: { value: S.String },
+    C: { otherValue: S.Number }
+  })
+
+  it("test roundtrip consistency", () => {
+    Util.assertions.testRoundtripConsistency(schema)
+  })
+
+  it("implements Equal", () => {
+    const value1 = S.decodeSync(schema)({ _tag: "A" })
+    const value2 = S.decodeSync(schema)({ _tag: "A" })
+    strictEqual(Equal.equals(value1, value2), true)
+  })
+
+  it("$match", () => {
+    const valueA = schema.members.A.make()
+    const valueB = schema.members.B.make({ value: "ok" })
+    let result = schema.$match(valueA, {
+      A: () => "A",
+      B: () => "B",
+      C: () => "C"
     })
-
-    const expected = S.Union(
-      S.TaggedStruct("A", {}),
-      S.TaggedStruct("B", { value: S.String }),
-      S.TaggedStruct("C", { otherValue: S.Number })
-    )
-
-    Util.assertions.ast.equals(schema, expected)
+    strictEqual(result, "A")
+    result = schema.$match(valueB, {
+      A: () => "A",
+      B: () => "B",
+      C: () => "C"
+    })
+    strictEqual(result, "B")
   })
 })
