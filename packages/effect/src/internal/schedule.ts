df3069de2e63b113db453cba7c77e6b7f71675d6
Maxim Khramtsov <khraks.mamtsov@gmail.com>
2025-05-16 11:48:46 +0200

    Add `Schedule.CurrentIterationMetadata` Reference  (#4828)
    Co-authored-by: Tim <hello@timsmart.co>

--- packages/effect/src/internal/schedule.ts
+++ packages/effect/src/internal/schedule.ts
@@ -45,6 +45,22 @@ export const ScheduleDriverTypeId: Schedule.ScheduleDriverTypeId = Symbol.for(
   ScheduleDriverSymbolKey
 ) as Schedule.ScheduleDriverTypeId
 
+/** @internal */
+const defaultIterationMetadata: Schedule.IterationMetadata = {
+  start: 0,
+  now: 0,
+  input: undefined,
+  elapsed: Duration.zero,
+  elapsedSincePrevious: Duration.zero,
+  recurrence: 0
+}
+
+/** @internal */
+export const CurrentIterationMetadata = Context.Reference<Schedule.CurrentIterationMetadata>()(
+  "effect/Schedule/CurrentIterationMetadata",
+  { defaultValue: () => defaultIterationMetadata }
+)
+
 const scheduleVariance = {
   /* c8 ignore next */
   _Out: (_: never) => _,
@@ -81,6 +97,31 @@ class ScheduleImpl<S, Out, In, R> implements Schedule.Schedule<Out, In, R> {
 }
 
 /** @internal */
+const updateInfo = (
+  iterationMetaRef: Ref.Ref<Schedule.IterationMetadata>,
+  now: number,
+  input: unknown
+) =>
+  ref.update(iterationMetaRef, (prev) =>
+    (prev.recurrence === 0) ?
+      {
+        now,
+        input,
+        recurrence: prev.recurrence + 1,
+        elapsed: Duration.zero,
+        elapsedSincePrevious: Duration.zero,
+        start: now
+      } :
+      {
+        now,
+        input,
+        recurrence: prev.recurrence + 1,
+        elapsed: Duration.millis(now - prev.start),
+        elapsedSincePrevious: Duration.millis(now - prev.now),
+        start: prev.start
+      })
+
+/** @internal */
 class ScheduleDriverImpl<Out, In, R> implements Schedule.ScheduleDriver<Out, In, R> {
   [ScheduleDriverTypeId] = scheduleDriverVariance
 
@@ -106,8 +147,12 @@ class ScheduleDriverImpl<Out, In, R> implements Schedule.ScheduleDriver<Out, In,
     })
   }
 
+  iterationMeta = ref.unsafeMake(defaultIterationMetadata)
+
   get reset(): Effect.Effect<void> {
-    return ref.set(this.ref, [Option.none(), this.schedule.initial])
+    return ref.set(this.ref, [Option.none(), this.schedule.initial]).pipe(
+      core.zipLeft(ref.set(this.iterationMeta, defaultIterationMetadata))
+    )
   }
 
   next(input: In): Effect.Effect<Out, Option.Option<never>, R> {
@@ -122,15 +167,22 @@ class ScheduleDriverImpl<Out, In, R> implements Schedule.ScheduleDriver<Out, In,
               core.flatMap(([state, out, decision]) => {
                 const setState = ref.set(this.ref, [Option.some(out), state] as const)
                 if (ScheduleDecision.isDone(decision)) {
-                  return core.zipRight(setState, core.fail(Option.none()))
+                  return setState.pipe(
+                    core.zipRight(core.fail(Option.none()))
+                  )
                 }
                 const millis = Intervals.start(decision.intervals) - now
                 if (millis <= 0) {
-                  return core.as(setState, out)
+                  return setState.pipe(
+                    core.zipRight(updateInfo(this.iterationMeta, now, input)),
+                    core.as(out)
+                  )
                 }
+                const duration = Duration.millis(millis)
                 return pipe(
                   setState,
-                  core.zipRight(effect.sleep(Duration.millis(millis))),
+                  core.zipRight(updateInfo(this.iterationMeta, now, input)),
+                  core.zipRight(effect.sleep(duration)),
                   core.as(out)
                 )
               })
@@ -1874,7 +1926,22 @@ export const repeatOrElse_Effect = dual<
   core.flatMap(driver(schedule), (driver) =>
     core.matchEffect(self, {
       onFailure: (error) => orElse(error, Option.none()),
-      onSuccess: (value) => repeatOrElseEffectLoop(self, driver, orElse, value)
+      onSuccess: (value) =>
+        repeatOrElseEffectLoop(
+          effect.provideServiceEffect(
+            self,
+            CurrentIterationMetadata,
+            ref.get(driver.iterationMeta)
+          ),
+          driver,
+          (error, option) =>
+            effect.provideServiceEffect(
+              orElse(error, option),
+              CurrentIterationMetadata,
+              ref.get(driver.iterationMeta)
+            ),
+          value
+        )
     })))
 
 /** @internal */
@@ -1883,8 +1950,8 @@ const repeatOrElseEffectLoop = <A, E, R, R1, B, C, E2, R2>(
   driver: Schedule.ScheduleDriver<B, A, R1>,
   orElse: (error: E, option: Option.Option<B>) => Effect.Effect<C, E2, R2>,
   value: A
-): Effect.Effect<B | C, E2, R | R1 | R2> => {
-  return core.matchEffect(driver.next(value), {
+): Effect.Effect<B | C, E2, R | R1 | R2> =>
+  core.matchEffect(driver.next(value), {
     onFailure: () => core.orDie(driver.last),
     onSuccess: (b) =>
       core.matchEffect(self, {
@@ -1892,7 +1959,6 @@ const repeatOrElseEffectLoop = <A, E, R, R1, B, C, E2, R2>(
         onSuccess: (value) => repeatOrElseEffectLoop(self, driver, orElse, value)
       })
   })
-}
 
 /** @internal */
 export const retry_Effect = dual<
@@ -1970,7 +2036,21 @@ export const retryOrElse_Effect = dual<
 >(3, (self, policy, orElse) =>
   core.flatMap(
     driver(policy),
-    (driver) => retryOrElse_EffectLoop(self, driver, orElse)
+    (driver) =>
+      retryOrElse_EffectLoop(
+        effect.provideServiceEffect(
+          self,
+          CurrentIterationMetadata,
+          ref.get(driver.iterationMeta)
+        ),
+        driver,
+        (e, out) =>
+          effect.provideServiceEffect(
+            orElse(e, out),
+            CurrentIterationMetadata,
+            ref.get(driver.iterationMeta)
+          )
+      )
   ))
 
 /** @internal */
@@ -2022,7 +2102,16 @@ export const scheduleFrom_Effect = dual<
 >(3, (self, initial, schedule) =>
   core.flatMap(
     driver(schedule),
-    (driver) => scheduleFrom_EffectLoop(self, initial, driver)
+    (driver) =>
+      scheduleFrom_EffectLoop(
+        effect.provideServiceEffect(
+          self,
+          CurrentIterationMetadata,
+          ref.get(driver.iterationMeta)
+        ),
+        initial,
+        driver
+      )
   ))
 
 /** @internal */
@@ -2033,7 +2122,11 @@ const scheduleFrom_EffectLoop = <In, E, R, R2, Out>(
 ): Effect.Effect<Out, E, R | R2> =>
   core.matchEffect(driver.next(initial), {
     onFailure: () => core.orDie(driver.last),
-    onSuccess: () => core.flatMap(self, (a) => scheduleFrom_EffectLoop(self, a, driver))
+    onSuccess: () =>
+      core.flatMap(
+        self,
+        (a) => scheduleFrom_EffectLoop(self, a, driver)
+      )
   })
 
 /** @internal */
