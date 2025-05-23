05cd1a946dc698b71f9cb9a137d7bbbf276e155e
Maxim Khramtsov <khraks.mamtsov@gmail.com>
2025-05-23 03:43:27 +0200

    `Config.port` and `Config.branded` functions have been added (#4892)
    Co-authored-by: Tim <hello@timsmart.co>

--- packages/effect/dtslint/Config.tst.ts
+++ packages/effect/dtslint/Config.tst.ts
@@ -1,11 +1,22 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Config, hole, pipe } from "effect"
+import { Brand, Config, hole, pipe } from "effect"
 import { describe, expect, it } from "tstyche"
 
 declare const string: Config.Config<string>
 declare const number: Config.Config<number>
 declare const array: Array<Config.Config<string>>
 declare const record: Record<string, Config.Config<number>>
+
+type Int = Brand.Branded<number, "Int">
+const Int = Brand.refined<Int>(
+  (n) => Number.isInteger(n),
+  (n) => Brand.error(`Expected ${n} to be an integer`)
+)
+
+type Str = Brand.Branded<string, "Str">
+const Str = Brand.refined<Str>(
+  (n) => n.length > 2,
+  (n) => Brand.error(`Expected "${n}" to be longer than 2`)
+)
 
 describe("Config", () => {
   describe("all", () => {
@@ -30,10 +41,24 @@ describe("Config", () => {
     })
   })
 
+  it("branded", () => {
+    // @ts-expect-error
+    Config.branded("NAME", Int)
+    // @ts-expect-error
+    Config.branded(number, Str)
+    // @ts-expect-error
+    number.pipe(Config.branded(Str))
+
+    expect(Config.branded(number, Int)).type.toBe<Config.Config<Int>>()
+    expect(Config.branded("NAME", Str)).type.toBe<Config.Config<Str>>()
+    expect(number.pipe(Config.branded(Int))).type.toBe<Config.Config<Int>>()
+    expect(pipe([string, number] as const, Config.all)).type.toBe<Config.Config<[string, number]>>()
+  })
+
   it("Config.Success helper type", () => {
     expect(hole<Config.Config.Success<typeof string>>()).type.toBe<string>()
     expect(hole<Config.Config.Success<typeof number>>()).type.toBe<number>()
-    const config = Config.all({ a: string, b: number })
-    expect(hole<Config.Config.Success<typeof config>>()).type.toBe<{ a: string; b: number }>()
+    const _config = Config.all({ a: string, b: number })
+    expect(hole<Config.Config.Success<typeof _config>>()).type.toBe<{ a: string; b: number }>()
   })
 })
