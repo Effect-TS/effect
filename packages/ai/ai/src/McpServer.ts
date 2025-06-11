06450a30dec73512e6745aa9d76cca9a4d3c59d6
Tim <hello@timsmart.co>
2025-06-11 17:36:32 +1200

    resource wip

--- packages/ai/ai/src/McpServer.ts
+++ packages/ai/ai/src/McpServer.ts
@@ -10,18 +10,23 @@ import * as Effect from "effect/Effect"
 import * as JsonSchema from "effect/JSONSchema"
 import * as Layer from "effect/Layer"
 import * as Option from "effect/Option"
+import * as Schema from "effect/Schema"
 import * as AST from "effect/SchemaAST"
 import type { Sink } from "effect/Sink"
 import type { Stream } from "effect/Stream"
+import * as FindMyWay from "find-my-way-ts"
 import * as AiTool from "./AiTool.js"
 import type * as AiToolkit from "./AiToolkit.js"
-import type { Implementation, ServerCapabilities } from "./McpSchema.js"
+import type { Implementation, Resource, ResourceTemplate, ServerCapabilities } from "./McpSchema.js"
 import {
   CallToolResult,
   ClientRpcs,
   InternalError,
   InvalidParams,
+  ListResourcesResult,
+  ListResourceTemplatesResult,
   ListToolsResult,
+  ReadResourceResult,
   TextContent,
   Tool,
   ToolAnnotations
@@ -50,6 +55,15 @@ export class Registry extends Context.Tag("@effect/ai/McpServer/Registry")<
       readonly tool: AiTool.AnyWithProtocol
       readonly handle: (payload: any) => Effect.Effect<CallToolResult>
     }>
+    readonly resources: Array<{
+      readonly resource: Resource
+      readonly handle: Effect.Effect<ReadResourceResult>
+    }>
+    readonly resourceTemplates: Array<{
+      readonly template: ResourceTemplate
+      readonly routerPath: string
+      readonly handle: (params: Array<string>) => Effect.Effect<ReadResourceResult>
+    }>
   }
 >() {
   /**
@@ -57,7 +71,9 @@ export class Registry extends Context.Tag("@effect/ai/McpServer/Registry")<
    */
   static readonly layer = Layer.sync(Registry, () =>
     Registry.of({
-      tools: new Map()
+      tools: new Map(),
+      resourceTemplates: [],
+      resources: []
     }))
 }
 
@@ -116,6 +132,31 @@ const ClientRpcHandlers = ClientRpcs.toLayer(
     const serverInfo = yield* McpServerImplementation
     const { capabilities = {} } = yield* McpServerOptions
     const registry = yield* Registry
+
+    // TODO: move into registry to support dynamic registration
+    const resourceRouter = makeUriMatcher<
+      {
+        readonly _tag: "Resource"
+        readonly effect: Effect.Effect<ReadResourceResult>
+      } | {
+        readonly _tag: "ResourceTemplate"
+        readonly handle: (params: Array<string>) => Effect.Effect<ReadResourceResult>
+      }
+    >()
+
+    for (const { handle, routerPath } of registry.resourceTemplates) {
+      resourceRouter.add(routerPath, {
+        _tag: "ResourceTemplate",
+        handle
+      })
+    }
+    for (const { handle, resource } of registry.resources) {
+      resourceRouter.add(resource.uri, {
+        _tag: "Resource",
+        effect: handle
+      })
+    }
+
     return {
       // Requests
       ping: () => Effect.succeed({}),
@@ -133,11 +174,34 @@ const ClientRpcHandlers = ClientRpcs.toLayer(
       "logging/setLevel": () => InternalError.notImplemented,
       "prompts/get": () => InternalError.notImplemented,
       "prompts/list": () => InternalError.notImplemented,
-      "resources/list": () => InternalError.notImplemented,
-      "resources/read": () => InternalError.notImplemented,
+      "resources/list": () =>
+        Effect.sync(() =>
+          new ListResourcesResult({
+            resources: registry.resources.map(({ resource }) => resource)
+          })
+        ),
+      "resources/read": ({ uri }) =>
+        Effect.suspend(() => {
+          const found = resourceRouter.find(uri)
+          if (!found) {
+            return Effect.succeed(
+              new ReadResourceResult({
+                contents: []
+              })
+            )
+          } else if (found.handler._tag === "Resource") {
+            return found.handler.effect
+          }
+          return found.handler.handle(found.params as any)
+        }),
       "resources/subscribe": () => InternalError.notImplemented,
       "resources/unsubscribe": () => InternalError.notImplemented,
-      "resources/templates/list": () => InternalError.notImplemented,
+      "resources/templates/list": () =>
+        Effect.sync(() =>
+          new ListResourceTemplatesResult({
+            resourceTemplates: registry.resourceTemplates.map(({ template }) => template)
+          })
+        ),
       "tools/call": ({ arguments: params, name }) =>
         Effect.suspend(() => {
           const tool = registry.tools.get(name)
@@ -266,3 +330,55 @@ export const toolkit = <Tools extends AiTool.Any>(
       })
     }
   })).pipe(Layer.provide(Registry.layer))
+
+const makeUriMatcher = <A>() => {
+  const protocols = new Map<string, FindMyWay.Router<A>>()
+  const add = (uri: string, value: A) => {
+    const url = new URL(uri)
+    let router = protocols.get(url.protocol)
+    if (!router) {
+      router = FindMyWay.make({
+        ignoreTrailingSlash: true,
+        ignoreDuplicateSlashes: true,
+        caseSensitive: true
+      })
+      protocols.set(url.protocol, router)
+    }
+    router.on("GET", `${url.host}${url.pathname}` as any, value)
+  }
+  const find = (uri: string) => {
+    const url = new URL(uri)
+    const router = protocols.get(url.protocol)
+    if (!router) {
+      return undefined
+    }
+    return router.find("GET", `${url.host}${url.pathname}`)
+  }
+
+  return { add, find } as const
+}
+
+const compileUriTemplate = (segments: TemplateStringsArray, ...schemas: ReadonlyArray<Schema.Schema.Any>) => {
+  let routerPath = segments[0]
+  let uriPath = segments[0]
+  let pathSchema = Schema.Tuple() as Schema.Schema.Any
+  if (schemas.length > 0) {
+    const arr: Array<Schema.Schema.Any> = []
+    for (let i = 0; i < schemas.length; i++) {
+      const schema = schemas[i]
+      const key = String(i)
+      arr.push(schema)
+      routerPath += `:${key}${segments[i + 1]}`
+      const paramName = AST.getIdentifierAnnotation(schema.ast).pipe(
+        Option.getOrElse(() => `param${key}`)
+      )
+      uriPath += `{${paramName}}`
+    }
+    pathSchema = Schema.Tuple(...arr)
+  }
+  return {
+    routerPath,
+    uriPath,
+    schema: pathSchema
+  } as const
+}
