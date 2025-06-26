8191302c63c646f4d5154f3fa14551508a66b87f
mrazauskas <tom@mrazauskas.de>
2025-06-26 17:00:50 +0300

    fix lint

--- packages/platform/dtslint/HttpApiEndpoint.tst.ts
+++ packages/platform/dtslint/HttpApiEndpoint.tst.ts
@@ -8,7 +8,6 @@ describe("HttpApiEndpoint", () => {
   it("should prevent duplicated params", () => {
     HttpApiEndpoint.get("test")`/${HttpApiSchema.param("id", Schema.NumberFromString)}/${
       // @ts-expect-error: Argument of type 'Param<"id", typeof NumberFromString>' is not assignable to parameter of type '"Duplicate param: id"'
-      HttpApiSchema.param("id", Schema.NumberFromString)
-    }`
+      HttpApiSchema.param("id", Schema.NumberFromString)}`
   })
 })
