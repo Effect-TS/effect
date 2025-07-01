8e3c565aad2b888badb0b62f109d9b4ec4049305
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-07-01 13:55:31 -0400

    Fix generated OpenAPI types for Anthropic AI provider (#5121)

--- packages/ai/anthropic/scripts/generate.sh
+++ packages/ai/anthropic/scripts/generate.sh
@@ -1,10 +1,30 @@
-#!/usr/bin/env sh
-mkdir -p tmp
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+temp_dir=$(mktemp -d)
+
+cleanup() {
+  rm -rf "$temp_dir"
+}
+
+trap cleanup EXIT
+
+
 anthropic_stats_url="https://raw.githubusercontent.com/anthropics/anthropic-sdk-typescript/refs/heads/main/.stats.yml"
-openapi_spec_url=$(curl -sSL $anthropic_stats_url | yq '.openapi_spec_url')
-curl $openapi_spec_url > tmp/anthropic.yaml
+
+openapi_spec_url="$(curl -sSL $anthropic_stats_url | yq ".openapi_spec_url")"
+
+curl "${openapi_spec_url}" > "${temp_dir}/anthropic.yaml"
+
 echo "/**
  * @since 1.0.0
  */" > src/Generated.ts
-pnpm openapi-gen -s tmp/anthropic.yaml >> src/Generated.ts
+
+pnpm openapi-gen -s "${temp_dir}/anthropic.yaml" >> src/Generated.ts
+
 pnpm eslint --fix src/Generated.ts
+
+git apply --reject --whitespace=fix "${SCRIPT_DIR}/generated.patch"
