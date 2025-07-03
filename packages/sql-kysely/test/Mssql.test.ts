c1684d7b72d080bee6886ca8a212568708bb6165
Kristian Notari <kristiannotari@icloud.com>
2025-07-03 19:26:25 +0200

    wip: kysely new proposal

--- packages/sql-kysely/test/Mssql.test.ts
+++ packages/sql-kysely/test/Mssql.test.ts
@@ -2,6 +2,7 @@ import * as MssqlKysely from "@effect/sql-kysely/Mssql"
 import { assert, describe, it } from "@effect/vitest"
 import { Context, Effect, Layer } from "effect"
 import type { Generated } from "kysely"
+import { execute } from "../src/internal/kysely.js"
 import { MssqlContainer } from "./utils.js"
 
 export interface User {
@@ -22,18 +23,20 @@ describe("MssqlKysely", () => {
   it.effect("queries", () =>
     Effect.gen(function*(_) {
       const db = yield* MssqlDB
-      yield* db.schema
-        .createTable("users")
-        .addColumn("id", "integer", (c) => c.primaryKey().identity())
-        .addColumn("name", "text", (c) => c.notNull())
-        .addColumn("nickname", "text")
+      yield* execute(
+        db.schema
+          .createTable("users")
+          .addColumn("id", "integer", (c) => c.primaryKey().identity())
+          .addColumn("name", "text", (c) => c.notNull())
+          .addColumn("nickname", "text")
+      )
 
-      yield* db.insertInto("users").values({ name: "Alice" })
-      const inserted = yield* db.selectFrom("users").selectAll()
-      yield* db.updateTable("users").set({ name: "Bob", nickname: "The Bobinator" })
-      const updated = yield* db.selectFrom("users").selectAll()
-      yield* db.deleteFrom("users")
-      const deleted = yield* db.selectFrom("users").selectAll()
+      yield* execute(db.insertInto("users").values({ name: "Alice" }))
+      const inserted = yield* execute(db.selectFrom("users").selectAll())
+      yield* execute(db.updateTable("users").set({ name: "Bob", nickname: "The Bobinator" }))
+      const updated = yield* execute(db.selectFrom("users").selectAll())
+      yield* execute(db.deleteFrom("users"))
+      const deleted = yield* execute(db.selectFrom("users").selectAll())
 
       assert.deepStrictEqual(inserted, [{ id: 1, name: "Alice", nickname: null }])
       assert.deepStrictEqual(updated, [{ id: 1, name: "Bob", nickname: "The Bobinator" }])
