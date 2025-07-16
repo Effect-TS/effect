dc7a04d24c5d40b352e8ba1191737fa722708d82
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-07-16 12:55:54 -0400

    update the generated openapi specification for openai

--- packages/ai/openai/scripts/generate.sh
+++ packages/ai/openai/scripts/generate.sh
@@ -1,10 +1,29 @@
-#!/usr/bin/env sh
-mkdir -p tmp
-SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-curl https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml > tmp/openapi.yaml
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+temp_dir=$(mktemp -d)
+
+cleanup() {
+  rm -rf "${temp_dir}"
+}
+
+trap cleanup EXIT
+
+openapi_spec_url="https://raw.githubusercontent.com/openai/openai-openapi/refs/heads/manual_spec/openapi.yaml"
+temp_file="${temp_dir}/openai.yaml"
+
+touch "${temp_file}"
+curl "${openapi_spec_url}" > "${temp_file}"
+
 echo "/**
  * @since 1.0.0
  */" > src/Generated.ts
-pnpm openapi-gen -s tmp/openapi.yaml >> src/Generated.ts
+
+pnpm openapi-gen -s "${temp_file}" >> src/Generated.ts
+
 pnpm eslint --fix src/Generated.ts
-git apply $SCRIPT_DIR/create-chat-completions-response.patch
+
+git apply --reject --whitespace=fix "${SCRIPT_DIR}/generated.patch"
