95c13b98efe1df826d51304cd65bf0c0ff008689
nikelborm <evadev@duck.com>
2025-07-27 06:09:13 +0300

    fixed parenthesis in scripts/clean.mjs

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,7 +1,7 @@
 import * as Glob from "glob"
 import * as Fs from "node:fs"
 
-const dirs = [".", ...Glob.sync("packages/*/", ...Glob.sync("packages/ai/*/"))]
+const dirs = [".", ...Glob.sync("packages/*/"), ...Glob.sync("packages/ai/*/")]
 dirs.forEach((pkg) => {
   const files = [".tsbuildinfo", "docs", "build", "dist", "coverage"]
 
