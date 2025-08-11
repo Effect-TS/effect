3f8537900189b70b4ad1011c8ba05b78f1527788
Patrick Roza <contact@patrickroza.com>
2025-08-11 20:40:32 +0200

    improve: add flatten option to RpcTest.makeClient

--- packages/rpc/src/RpcTest.ts
+++ packages/rpc/src/RpcTest.ts
@@ -12,24 +12,27 @@ import * as RpcServer from "./RpcServer.js"
  * @since 1.0.0
  * @category constructors
  */
-export const makeClient: <Rpcs extends Rpc.Any>(
-  group: RpcGroup.RpcGroup<Rpcs>
+export const makeClient: <Rpcs extends Rpc.Any, const Flatten extends boolean = false>(
+  group: RpcGroup.RpcGroup<Rpcs>,
+  options?: { flatten?: Flatten | undefined }
 ) => Effect.Effect<
-  RpcClient.RpcClient<Rpcs>,
+  Flatten extends true ? RpcClient.RpcClient.Flat<Rpcs> : RpcClient.RpcClient<Rpcs>,
   never,
   Scope.Scope | Rpc.ToHandler<Rpcs> | Rpc.Middleware<Rpcs> | Rpc.MiddlewareClient<Rpcs>
-> = Effect.fnUntraced(function*<Rpcs extends Rpc.Any>(
-  group: RpcGroup.RpcGroup<Rpcs>
+> = Effect.fnUntraced(function*<Rpcs extends Rpc.Any, const Flatten extends boolean = false>(
+  group: RpcGroup.RpcGroup<Rpcs>,
+  options?: { flatten?: Flatten | undefined }
 ) {
   // eslint-disable-next-line prefer-const
-  let client!: Effect.Effect.Success<ReturnType<typeof RpcClient.makeNoSerialization<Rpcs, never>>>
+  let client!: Effect.Effect.Success<ReturnType<typeof RpcClient.makeNoSerialization<Rpcs, never, Flatten>>>
   const server = yield* RpcServer.makeNoSerialization(group, {
     onFromServer(response) {
       return client.write(response)
     }
   })
-  client = yield* RpcClient.makeNoSerialization(group, {
+  client = yield* RpcClient.makeNoSerialization<Rpcs, never, Flatten>(group, {
     supportsAck: true,
+    flatten: options?.flatten,
     onFromClient({ message }) {
       return server.write(0, message)
     }
