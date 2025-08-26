ea4cba20465aaa04961ea9a89769736709e23f1c
Giulio Canti <giulio.canti@gmail.com>
2025-08-26 17:30:45 +0200

    add type level test for PR #5360

--- packages/effect/dtslint/Schema/Brand.tst.ts
+++ packages/effect/dtslint/Schema/Brand.tst.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { pipe, Schema } from "effect"
-import { describe, expect, it } from "tstyche"
+import { describe, expect, it, when } from "tstyche"
 
 const Int1 = Symbol.for("Int")
 const Int2 = Symbol.for("Int")
@@ -15,5 +15,14 @@ describe("SchemaBrand", () => {
   it("should differentiate between branded schema types", () => {
     expect<A1>().type.not.toBeAssignableTo<A2>()
     expect<A2>().type.not.toBeAssignableTo<A1>()
+  })
+
+  it("should raise an error when the brand is not assignable to the schema", () => {
+    when(pipe).isCalledWith(
+      Schema.Number,
+      expect(Schema.brand).type.not.toBeCallableWith("UserId", {
+        examples: ["a"]
+      })
+    )
   })
 })
