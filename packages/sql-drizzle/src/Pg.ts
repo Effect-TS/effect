c77428f99dc18dbe1c74ebd69f1f8c00d8a25f55
adr <andres@duarterengifo.com>
2025-09-05 13:44:25 -0400

    add support for drizzle relational queries

--- packages/sql-drizzle/src/Pg.ts
+++ packages/sql-drizzle/src/Pg.ts
@@ -47,6 +47,72 @@ export class PgDrizzle extends Context.Tag("@effect/sql-drizzle/Pg")<
 >() {}
 
 /**
+ *
+ * @example
+ * ```ts
+ * const schema = {
+ *     account: pgTable("account", {
+ *         id: integer()
+ *     })
+ * }
+ * 
+ * export const PgLive = PgClient.layerConfig({
+ * 	url: Config.redacted(""),
+ * })
+ * 
+ * export const DrizzeLive = layerWithConfigGeneric({
+ * 	casing: "snake_case",
+ * 	schema,
+ * }).pipe(Layer.provide(PgLive))
+ * 
+ * export const DBLive = Layer.mergeAll(
+ * 	PgLive,
+ * 	DrizzeLive,
+ * )
+ * 
+ * export const DrizzleInstance = PgDrizzleFactory<typeof schema>()
+ * 
+ * const eff = Effect.gen(function* () {
+ * 		const drizle = yield* DrizzleInstance
+ *         // before account wouldn't be inferred.
+ * 		const result = yield* drizle.query.account.findFirst()
+ * }).pipe(Effect.provide(DBLive))
+ * ```
+ *
+ */
+
+/**
+ * @since 1.0.0
+ * @category tags
+ */
+export const PgDrizzleFactory = <TSchema extends Record<string, unknown> = Record<string, never>>() =>
+  Context.GenericTag<
+  "@effect/sql-drizzle/Pg",
+  PgRemoteDatabase<TSchema>
+>("@effect/sql-drizzle/Pg")
+
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const makeWithConfigGeneric = <TSchema extends Record<string, unknown> = Record<string, never>>(config: DrizzleConfig<TSchema>) =>
+  Effect.gen(function*() {
+    const db = drizzle(yield* makeRemoteCallback, config)
+    return db
+  })
+
+/**
+ * @since 1.0.0
+ * @category layers
+ */
+export const layerWithConfigGeneric = <
+  TSchema extends Record<string, unknown> = Record<string, never>
+>(
+  config: DrizzleConfig<TSchema>
+): Layer.Layer<"@effect/sql-drizzle/Pg", never, Client.SqlClient> =>
+  Layer.effect(PgDrizzleFactory<TSchema>(), makeWithConfigGeneric(config))
+
+/**
  * @since 1.0.0
  * @category layers
  */
