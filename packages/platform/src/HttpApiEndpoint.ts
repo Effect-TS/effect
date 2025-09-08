8ac3f2232f190ef1ad93bc0bf43e6c85df6ac5d9
Aldwin Vlasblom <aldwin@altshift.win>
2025-09-08 18:04:56 +0200

    Update addError to give precedence to newly added schemas
    HttpApiSchema.UnionUnify creates a union whose members are listed in the
    order of `[...firstArgUnionMembers, ...secondArgUnionMembers]`. When
    encoding a union, Schema will try each member in the order that they
    appear.
    
    So the `addError` method on each of the HttpApi* classes, by passing the
    user-provided schema as a second argument, causes older schemas that
    encompass newer schemas to take precedence over the newer ones.
    
    By flipping the argument order, users are now able to use `addError`
    with a schema that (partly) encompasses an older one, to override the
    encoding behaviour of value conforming to that schema.

--- packages/platform/src/HttpApiEndpoint.ts
+++ packages/platform/src/HttpApiEndpoint.ts
@@ -771,8 +771,8 @@ const Proto = {
     return makeProto({
       ...this,
       errorSchema: HttpApiSchema.UnionUnify(
+        annotations?.status ? schema.annotations(HttpApiSchema.annotations({ status: annotations.status })) : schema,
         this.errorSchema,
-        annotations?.status ? schema.annotations(HttpApiSchema.annotations({ status: annotations.status })) : schema
       )
     })
   },
