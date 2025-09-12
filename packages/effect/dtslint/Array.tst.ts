1a1ead9e90f83b88b65d2751b7b36bb88778e40b
Oliver Joseph Ash <oliverjash@gmail.com>
2025-09-12 12:01:18 +0100

    Update `Array.match` to return `NonEmptyArray` when given `Array`

--- packages/effect/dtslint/Array.tst.ts
+++ packages/effect/dtslint/Array.tst.ts
@@ -1,7 +1,7 @@
 import { Array, Effect, Either, Option, Order, Predicate } from "effect"
 import { hole, identity, pipe } from "effect/Function"
 import { describe, expect, it, when } from "tstyche"
-import { NonEmptyReadonlyArray } from "effect/Array"
+import { NonEmptyArray, NonEmptyReadonlyArray } from "effect/Array"
 
 declare const nonEmptyReadonlyStrings: Array.NonEmptyReadonlyArray<string>
 declare const nonEmptyNumbers: Array.NonEmptyArray<number>
@@ -1246,15 +1246,16 @@ describe("Array", () => {
     const readonlyArray: ReadonlyArray<number> = [1, 2, 3];
 
     expect(pipe(array, Array.match({ onEmpty: () => 1, onNonEmpty: (xs) => {
-      expect(xs).type.toBe<NonEmptyReadonlyArray<never>>()
+      expect(xs).type.toBe<NonEmptyArray<never>>()
       return 1
     } })))
+    // TODO: not sure what to do about this
     expect(pipe(readonlyArray, Array.match({ onEmpty: () => 1, onNonEmpty: (xs) => {
       expect(xs).type.toBe<NonEmptyReadonlyArray<never>>()
       return 1
     } })))
     expect(Array.match(array, { onEmpty: () => 1, onNonEmpty: (xs) => {
-      expect(xs).type.toBe<NonEmptyReadonlyArray<never>>()
+      expect(xs).type.toBe<NonEmptyArray<never>>()
       return 1
     } }))
     expect(Array.match(readonlyArray, { onEmpty: () => 1, onNonEmpty: (xs) => {
