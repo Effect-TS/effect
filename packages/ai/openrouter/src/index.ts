9c42d7b783a5cc37a3757f3f2a837099d838a799
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-09-17 15:40:15 -0400

    begin openrouter provider

---
+++ packages/ai/openrouter/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * @since 1.0.0
+ */
+export * as Generated from "./Generated.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as OpenRouterClient from "./OpenRouterClient.js"
+
+/**
+ * @since 1.0.0
+ */
+export * as OpenRouterConfig from "./OpenRouterConfig.js"
+
+
+export * as OpenRouterLanguageModel from "./OpenRouterLanguageModel.js"
