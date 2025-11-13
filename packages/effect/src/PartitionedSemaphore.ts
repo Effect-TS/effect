82e872b80eadeffb5a3c5fdfbec54e524c6c58c9
Tim Smart <hello@timsmart.co>
2025-11-14 09:54:13 +1300

    add PartitionedSemaphore module

---
+++ packages/effect/src/PartitionedSemaphore.ts
@@ -0,0 +1,175 @@
+/**
+ * @since 3.20.0
+ * @experimental
+ */
+import * as Effect from "./Effect.js"
+import * as Iterable from "./Iterable.js"
+import * as MutableHashMap from "./MutableHashMap.js"
+import * as Option from "./Option.js"
+
+/**
+ * @since 3.20.0
+ * @category Models
+ * @experimental
+ */
+export const TypeId: TypeId = "~effect/PartitionedSemaphore"
+
+/**
+ * @since 3.20.0
+ * @category Models
+ * @experimental
+ */
+export type TypeId = "~effect/PartitionedSemaphore"
+
+/**
+ * A `PartitionedSemaphore` is a concurrency primitive that can be used to
+ * control concurrent access to a resource across multiple partitions identified
+ * by keys.
+ *
+ * The total number of permits is shared across all partitions, with waiting
+ * permits equally distributed among partitions using a round-robin strategy.
+ *
+ * This is useful when you want to limit the total number of concurrent accesses
+ * to a resource, while still allowing for fair distribution of access across
+ * different partitions.
+ *
+ * @since 3.20.0
+ * @category Models
+ * @experimental
+ */
+export interface PartitionedSemaphore<in K> {
+  readonly [TypeId]: TypeId
+
+  readonly withPermits: (
+    key: K,
+    permits: number
+  ) => <A, E, R>(effect: Effect.Effect<A, E, R>) => Effect.Effect<A, E, R>
+}
+
+/**
+ * A `PartitionedSemaphore` is a concurrency primitive that can be used to
+ * control concurrent access to a resource across multiple partitions identified
+ * by keys.
+ *
+ * The total number of permits is shared across all partitions, with waiting
+ * permits equally distributed among partitions using a round-robin strategy.
+ *
+ * This is useful when you want to limit the total number of concurrent accesses
+ * to a resource, while still allowing for fair distribution of access across
+ * different partitions.
+ *
+ * @since 3.20.0
+ * @category Constructors
+ * @experimental
+ */
+export const makeUnsafe = <K = unknown>(options: {
+  readonly permits: number
+}): PartitionedSemaphore<K> => {
+  let totalPermits = options.permits
+  let waitingPermits = 0
+
+  type Waiter = {
+    permits: number
+    readonly resume: () => void
+  }
+  const partitions = MutableHashMap.empty<K, Set<Waiter>>()
+
+  const take = <A, E, R>(key: K, permits: number, effect: Effect.Effect<A, E, R>) =>
+    Effect.uninterruptibleMask((restore) =>
+      Effect.async<A, E, R>((resume) => {
+        if (options.permits < permits) {
+          return resume(restore(Effect.never))
+        } else if (totalPermits >= permits) {
+          totalPermits -= permits
+          return resume(Effect.ensuring(restore(effect), release(permits)))
+        }
+
+        const needed = permits - totalPermits
+        const taken = permits - needed
+        if (totalPermits > 0) {
+          totalPermits = 0
+        }
+        waitingPermits += needed
+
+        const waiters = Option.getOrElse(
+          MutableHashMap.get(partitions, key),
+          () => {
+            const set = new Set<Waiter>()
+            MutableHashMap.set(partitions, key, set)
+            return set
+          }
+        )
+
+        const entry: Waiter = {
+          permits: needed,
+          resume() {
+            cleanup()
+            resume(Effect.ensuring(restore(effect), release(permits)))
+          }
+        }
+        function cleanup() {
+          waiters.delete(entry)
+          if (waiters.size === 0) {
+            MutableHashMap.remove(partitions, key)
+          }
+        }
+        waiters.add(entry)
+        return Effect.sync(() => {
+          cleanup()
+          waitingPermits -= entry.permits
+          if (taken > 0) {
+            releaseUnsafe(taken)
+          }
+        })
+      })
+    )
+
+  let iterator = partitions[Symbol.iterator]()
+  const releaseUnsafe = (permits: number) => {
+    while (permits > 0) {
+      if (waitingPermits === 0) {
+        totalPermits += permits
+        return
+      }
+
+      let state = iterator.next()
+      if (state.done) {
+        iterator = partitions[Symbol.iterator]()
+        state = iterator.next()
+        if (state.done) return
+      }
+
+      const entry = Iterable.unsafeHead(state.value[1])
+      entry.permits--
+      waitingPermits--
+      if (entry.permits === 0) entry.resume()
+      permits--
+    }
+  }
+  const release = (permits: number) => Effect.sync(() => releaseUnsafe(permits))
+
+  return {
+    [TypeId]: TypeId,
+    withPermits: (key, permits) => (effect) => take(key, permits, effect)
+  }
+}
+
+/**
+ * A `PartitionedSemaphore` is a concurrency primitive that can be used to
+ * control concurrent access to a resource across multiple partitions identified
+ * by keys.
+ *
+ * The total number of permits is shared across all partitions, with waiting
+ * permits equally distributed among partitions using a round-robin strategy.
+ *
+ * This is useful when you want to limit the total number of concurrent accesses
+ * to a resource, while still allowing for fair distribution of access across
+ * different partitions.
+ *
+ * @since 3.20.0
+ * @category Constructors
+ * @experimental
+ */
+export const make = <K = unknown>(options: {
+  readonly permits: number
+}): Effect.Effect<PartitionedSemaphore<K>> => Effect.sync(() => makeUnsafe<K>(options))
