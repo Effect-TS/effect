b3523de0f47398e3e10a19f232f4d74dfe75df98
Tim Smart <hello@timsmart.co>
2025-11-19 12:39:05 +1300

    make RpcClient socket retry schedule configurable

--- packages/rpc/src/RpcClient.ts
+++ packages/rpc/src/RpcClient.ts
@@ -934,6 +934,7 @@ export const layerProtocolHttp = (options: {
  */
 export const makeProtocolSocket = (options?: {
   readonly retryTransientErrors?: boolean | undefined
+  readonly retrySchedule?: Schedule.Schedule<any, unknown> | undefined
 }): Effect.Effect<
   Protocol["Type"],
   never,
@@ -1016,7 +1017,7 @@ export const makeProtocolSocket = (options?: {
           error: currentError
         })
       }),
-      Effect.retry(Schedule.spaced(1000)),
+      Effect.retry(options?.retrySchedule ?? defaultRetrySchedule),
       Effect.annotateLogs({
         module: "RpcClient",
         method: "makeProtocolSocket"
@@ -1038,6 +1039,10 @@ export const makeProtocolSocket = (options?: {
       supportsTransferables: false
     }
   }))
+
+const defaultRetrySchedule = Schedule.exponential(500, 1.5).pipe(
+  Schedule.union(Schedule.spaced(5000))
+)
 
 const makePinger = Effect.fnUntraced(function*<A, E, R>(writePing: Effect.Effect<A, E, R>) {
   let recievedPong = true
@@ -1239,6 +1244,7 @@ export const layerProtocolWorker = (
  */
 export const layerProtocolSocket = (options?: {
   readonly retryTransientErrors?: boolean | undefined
+  readonly retrySchedule?: Schedule.Schedule<any, unknown> | undefined
 }): Layer.Layer<
   Protocol,
   never,
