de59e71640572c23f284db4c1ae7883b89d3c824
mrazauskas <tom@mrazauskas.de>
2026-01-02 09:46:48 +0200

    bump version

--- packages/effect/dtslint/List.tst.ts
+++ packages/effect/dtslint/List.tst.ts
@@ -42,6 +42,7 @@ describe("List", () => {
       expect(numberOrString).type.toBe<List.Cons<string | number>>()
     }
     if (pipe(numberOrString, List.some(Predicate.isString))) {
+      // @tstyche fixme -- This doesn't work but it should
       expect(numberOrString).type.toBe<List.Cons<string | number>>()
     }
     if (List.some(Predicate.isString)(numberOrString)) {
