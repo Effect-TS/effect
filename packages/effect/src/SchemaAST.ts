ff58c8e75f6fe86162adba54c9aaf2218afa4958
Taylor Gregoire-Wright <taylor@ontologize.com>
2026-02-23 16:02:40 -0800

    Schema: fix getPropertySignatures crash on Struct with optionalWith({ default })

--- packages/effect/src/SchemaAST.ts
+++ packages/effect/src/SchemaAST.ts
@@ -2328,6 +2328,8 @@ export const getPropertyKeyIndexedAccess = (ast: AST, name: PropertyKey): Proper
       return getPropertyKeyIndexedAccess(ast.f(), name)
     case "Refinement":
       return getPropertyKeyIndexedAccess(ast.from, name)
+    case "Transformation":
+      return getPropertyKeyIndexedAccess(ast.to, name)
   }
   throw new Error(errors_.getASTUnsupportedSchemaErrorMessage(ast))
 }
