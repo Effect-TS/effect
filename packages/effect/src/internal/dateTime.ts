dddd4bca77cf24cfd98b85906bef4e12947d8a46
lihan3238 <lihan3238@163.com>
2026-05-05 23:32:57 +0800

    feat(DateTime): add toEpochSeconds method
    Adds `DateTime.toEpochSeconds` which returns the Unix timestamp in
    seconds (integer), complementing the existing `toEpochMillis`.
    
    Closes #6148

--- packages/effect/src/internal/dateTime.ts
+++ packages/effect/src/internal/dateTime.ts
@@ -614,6 +614,9 @@ export const zonedOffsetIso = (self: DateTime.Zoned): string => offsetToString(z
 export const toEpochMillis = (self: DateTime.DateTime): number => self.epochMillis
 
 /** @internal */
+export const toEpochSeconds = (self: DateTime.DateTime): number => Math.floor(self.epochMillis / 1000)
+
+/** @internal */
 export const removeTime = (self: DateTime.DateTime): DateTime.Utc =>
   withDate(self, (date) => {
     date.setUTCHours(0, 0, 0, 0)
