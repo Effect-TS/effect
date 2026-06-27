9003cd9c9aa5e35e23c3ec9287b82d094fda09ca
Nicoreia <nico.reiab@gmail.com>
2026-06-27 12:39:33 +0200

    test: add tests for Tuple mapFirst, mapSecond, getEquivalence, getOrder
    The exported functions `mapFirst`, `mapSecond`, `getEquivalence` and
    `getOrder` had no unit tests. Add cases following the existing Tuple test
    style: both data-first and data-last forms for `mapFirst`/`mapSecond`, and
    equal/less/greater checks for `getEquivalence` and `getOrder`.

--- packages/effect/test/Tuple.test.ts
+++ packages/effect/test/Tuple.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "@effect/vitest"
 import { deepStrictEqual, strictEqual } from "@effect/vitest/utils"
-import { pipe, Tuple } from "effect"
+import { Equivalence, Order, pipe, Tuple } from "effect"
 
 describe("Tuple", () => {
   it("make", () => {
@@ -39,5 +39,29 @@ describe("Tuple", () => {
 
   it("at", () => {
     deepStrictEqual(Tuple.at([1, "hello", true], 1), "hello")
+  })
+
+  it("mapFirst", () => {
+    deepStrictEqual(Tuple.mapFirst(Tuple.make("a", 1), (s) => s + "!"), ["a!", 1])
+    deepStrictEqual(pipe(Tuple.make("a", 1), Tuple.mapFirst((s) => s + "!")), ["a!", 1])
+  })
+
+  it("mapSecond", () => {
+    deepStrictEqual(Tuple.mapSecond(Tuple.make("a", 1), (n) => n * 2), ["a", 2])
+    deepStrictEqual(pipe(Tuple.make("a", 1), Tuple.mapSecond((n) => n * 2)), ["a", 2])
+  })
+
+  it("getEquivalence", () => {
+    const equivalence = Tuple.getEquivalence(Equivalence.string, Equivalence.number)
+    strictEqual(equivalence(Tuple.make("a", 1), Tuple.make("a", 1)), true)
+    strictEqual(equivalence(Tuple.make("a", 1), Tuple.make("a", 2)), false)
+    strictEqual(equivalence(Tuple.make("a", 1), Tuple.make("b", 1)), false)
+  })
+
+  it("getOrder", () => {
+    const order = Tuple.getOrder(Order.string, Order.number)
+    strictEqual(order(Tuple.make("a", 1), Tuple.make("a", 2)), -1)
+    strictEqual(order(Tuple.make("a", 1), Tuple.make("a", 1)), 0)
+    strictEqual(order(Tuple.make("b", 1), Tuple.make("a", 1)), 1)
   })
 })
