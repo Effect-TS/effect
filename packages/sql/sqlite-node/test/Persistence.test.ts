72475b470b1d7167698c44d2ac7c1f0c13e126c5
Tim <hello@timsmart.co>
2026-08-02 13:08:22 +1200

    Fix SQL cleanup test timing race (#6881)

--- packages/sql/sqlite-node/test/Persistence.test.ts
+++ packages/sql/sqlite-node/test/Persistence.test.ts
@@ -141,16 +141,7 @@ it.layer(ClientLayer)("Persistence SQL cleanup", (it) => {
 
       yield* Layer.build(Persistence.layerBackingSql).pipe(TestClock.withLive)
 
-      let expired = yield* SqlCleanupTest.waitForCount(
-        expiredCount,
-        (count) => count < SqlCleanupTest.expiredEntryCount
-      )
-      assert.strictEqual(expired, 1)
-
-      while (expired > 0) {
-        const previous = expired
-        expired = yield* SqlCleanupTest.waitForCount(expiredCount, (count) => count < previous)
-      }
+      const expired = yield* SqlCleanupTest.waitForCount(expiredCount, (count) => count === 0)
       assert.strictEqual(expired, 0)
       const live = yield* sql<{ readonly count: number }>`
         SELECT COUNT(*) AS count FROM ${table} WHERE store_id = 'live'
