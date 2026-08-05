97ef9f80652e24890ccc6e677cbc86a48fcb34e4
Sebastian Lorenz <fubhy@fubhy.com>
2026-08-05 21:18:21 +0000

    Fix logger shutdown is skipped after flush failure

--- packages/opentelemetry/test/OtelLogger.test.ts
+++ packages/opentelemetry/test/OtelLogger.test.ts
@@ -1,7 +1,9 @@
 import * as NodeSdk from "@effect/opentelemetry/NodeSdk"
+import * as OtelLogger from "@effect/opentelemetry/OtelLogger"
+import * as Resource from "@effect/opentelemetry/Resource"
 import { assert, describe, it } from "@effect/vitest"
 import { SeverityNumber } from "@opentelemetry/api-logs"
-import { InMemoryLogRecordExporter, SimpleLogRecordProcessor } from "@opentelemetry/sdk-logs"
+import { InMemoryLogRecordExporter, LoggerProvider, SimpleLogRecordProcessor } from "@opentelemetry/sdk-logs"
 import { InMemorySpanExporter, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-base"
 import * as Clock from "effect/Clock"
 import * as Effect from "effect/Effect"
@@ -9,6 +11,25 @@ import * as Layer from "effect/Layer"
 import * as References from "effect/References"
 
 describe("Logger", () => {
+  it.effect("shuts down the logger provider after forceFlush rejects", () =>
+    Effect.gen(function*() {
+      let shutdowns = 0
+      const originalFlush = LoggerProvider.prototype.forceFlush
+      const originalShutdown = LoggerProvider.prototype.shutdown
+      LoggerProvider.prototype.forceFlush = () => Promise.reject(new Error("flush failed"))
+      LoggerProvider.prototype.shutdown = () => {
+        shutdowns++
+        return Promise.resolve()
+      }
+      const processor = { onEmit() {}, shutdown: async () => {}, forceFlush: async () => {} } as any
+      yield* Effect.exit(
+        Effect.scoped(Layer.build(OtelLogger.layerLoggerProvider(processor)).pipe(Effect.provide(Resource.layerEmpty)))
+      )
+      LoggerProvider.prototype.forceFlush = originalFlush
+      LoggerProvider.prototype.shutdown = originalShutdown
+      assert.strictEqual(shutdowns, 1)
+    }))
+
   describe("provided", () => {
     const exporter = new InMemoryLogRecordExporter()
 
