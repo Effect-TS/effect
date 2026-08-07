d3debd35f4857f4a298662aa21389269502c2f94
Sebastian Lorenz <fubhy@fubhy.com>
2026-08-07 09:52:17 +0000

    Fix rcRef leaks resources acquired before acquisition failure

--- packages/effect/test/RcRef.test.ts
+++ packages/effect/test/RcRef.test.ts
@@ -2,6 +2,7 @@ import { assert, describe, it } from "@effect/vitest"
 import * as Effect from "effect/Effect"
 import * as Exit from "effect/Exit"
 import * as RcRef from "effect/RcRef"
+import * as Ref from "effect/Ref"
 import * as Scope from "effect/Scope"
 
 describe("RcRef", () => {
@@ -54,6 +55,30 @@ describe("RcRef", () => {
 
       const exit = yield* RcRef.get(ref).pipe(Effect.scoped, Effect.exit)
       assert.isTrue(Exit.hasInterrupts(exit))
+    }))
+
+  it.effect("releases resources acquired before acquisition failure", () =>
+    Effect.gen(function*() {
+      const acquired = yield* Ref.make(0)
+      const released = yield* Ref.make(0)
+      const refScope = yield* Scope.make()
+      const borrowerScope = yield* Scope.make()
+      const ref = yield* RcRef.make({
+        acquire: Effect.acquireRelease(
+          Ref.updateAndGet(acquired, (n) => n + 1),
+          () => Ref.update(released, (n) => n + 1)
+        ).pipe(Effect.andThen(Effect.fail("boom")))
+      }).pipe(Scope.provide(refScope))
+
+      const getExit = yield* RcRef.get(ref).pipe(Scope.provide(borrowerScope), Effect.exit)
+      const borrowerCloseExit = yield* Scope.close(borrowerScope, Exit.void).pipe(Effect.exit)
+      const refCloseExit = yield* Scope.close(refScope, Exit.void).pipe(Effect.exit)
+
+      assert.deepStrictEqual(getExit, Exit.fail("boom"))
+      assert.deepStrictEqual(borrowerCloseExit, Exit.void)
+      assert.deepStrictEqual(refCloseExit, Exit.void)
+      assert.strictEqual(yield* Ref.get(acquired), 1)
+      assert.strictEqual(yield* Ref.get(released), 1)
     }))
 
   // it.scoped("idleTimeToLive", () =>
