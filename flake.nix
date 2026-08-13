f4ba735bc450e5120800a4140f4f262a0cab2cae
Sebastian Lorenz <fubhy@fubhy.com>
2026-08-13 21:54:30 +0200

    upgrade pnpm (#7237)

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
         packages = with pkgs; [
           bun
           deno
-          (corepack.override {nodejs-slim = nodejs-slim_26;})
-          nodejs_26
+          nodejs_latest
+          pnpm
           python3
         ];
       };
