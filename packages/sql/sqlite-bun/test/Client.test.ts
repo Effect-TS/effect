05d2b1451637a3fcd31cf9142d1ee3c587a80d82
Denys Kashkovskyi <dannie.k@me.com>
2026-08-26 23:29:35 +0200

    Fix readonly SQLite transactions

--- packages/sql/sqlite-bun/test/Client.test.ts
+++ packages/sql/sqlite-bun/test/Client.test.ts
@@ -62,11 +62,13 @@ describe("Client", () => {
 
       const sql = yield* SqliteClient.make({ filename, readonly: true })
       assert.deepStrictEqual(yield* sql`SELECT * FROM test`, [])
-      assert.deepStrictEqual(yield* sql.withTransaction(sql`SELECT * FROM test`), [])
 
       const error = yield* Effect.flip(sql`INSERT INTO test DEFAULT VALUES`)
       assert.strictEqual(error._tag, "SqlError")
       assert(error.reason.cause instanceof Error)
       assert.match(error.reason.cause.message, /attempt to write a readonly database/i)
+
+      yield* sql`PRAGMA query_only = ON`
+      assert.deepStrictEqual(yield* sql.withTransaction(sql`SELECT * FROM test`), [])
     }).pipe(Effect.provide(Reactivity.layer)))
 })
