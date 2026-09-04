433d87c1cf23722317995c34b386961396c57eb3
Tim Smart <hello@timsmart.co>
2026-09-04 04:01:54 +0000

    test(bundle): remove fixture path coverage

--- packages/tools/bundle/test/fixtures/fixtures-runner.mjs
+++
@@ -1,6 +0,0 @@
-import * as Effect from "effect/Effect"
-import { pathToFileURL } from "node:url"
-
-const modulePath = process.argv[2]
-const { Fixtures } = await import(pathToFileURL(modulePath).href)
-console.log(JSON.stringify(await Effect.runPromise(Fixtures.make)))
