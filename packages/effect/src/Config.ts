3b34ddb125b1c7ecc30aaa93d89dd0b25d116af1
Giulio Canti <giulio.canti@gmail.com>
2026-09-24 15:56:27 +0200

    fix since tag

--- packages/effect/src/Config.ts
+++ packages/effect/src/Config.ts
@@ -245,7 +245,7 @@ export const map: {
  * ```
  *
  * @category mapping
- * @since 2.0.0
+ * @since 4.0.0
  */
 export const flatMap: {
   <A, B>(f: (a: A) => Config<B>): (self: Config<A>) => Config<B>
